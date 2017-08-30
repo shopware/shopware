53982c56d3e089b91f55d89e97207e55a756af17
Thomas Eiling <teiling88@gmail.com>
2017-08-30 11:42:57 +0200

    SW-19241 - adding small check requirements script

--- dev-ops/common/actions/init.sh
+++ dev-ops/common/actions/init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #DESCRIPTION: initialization of your environment
 
+INCLUDE: ./check_requirements.sh
+
 INCLUDE: ./.init_database.sh
 INCLUDE: ./.init_composer.sh
 
---
+++ dev-ops/common/templates/check_requirements.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * very small check for needed server requirements
+ */
+
+if (!version_compare(PHP_VERSION, '7.1', '>=')) {
+    throw new Exception('you need a php version 7.1 or higher');
+}
+
+$db = new PDO('mysql:host=__DB_HOST__;dbname=__DB_NAME__', '__DB_USER__', '__DB_PASSWORD__');
+
+$mysqlVersion = $db->query('SELECT VERSION();')->fetchColumn();
+
+if (!version_compare($mysqlVersion, '5.7', '>=')) {
+    throw new Exception('you need a mysql version 5.7 or higher');
+}

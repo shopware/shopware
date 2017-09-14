e126954f0a07b2bb2ed6bbbb92cbe641f94001d5
Jan Bücker <j.buecker@shopware.com>
2017-09-14 13:18:29 +0200

    Change environment variables back to default symfony names

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ require __DIR__.'/../vendor/autoload.php';
 // The check is to ensure we don't use .env in production
 (new Dotenv())->load(__DIR__.'/../.env');
 
-if ($_SERVER['SHOPWARE_DEBUG'] ?? true) {
+if ($_SERVER['APP_DEBUG'] ?? true) {
     // WARNING: You should setup permissions the proper way!
     // REMOVE the following PHP line and read
     // https://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
@@ -20,7 +20,7 @@ if ($_SERVER['SHOPWARE_DEBUG'] ?? true) {
 
 // Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 
-$kernel = new AppKernel($_SERVER['SHOPWARE_ENV'] ?? 'dev', $_SERVER['SHOPWARE_DEBUG'] ?? true);
+$kernel = new AppKernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? false);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();

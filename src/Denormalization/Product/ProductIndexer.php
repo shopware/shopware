acbc09299eba3e37959b6dabebe1ef281f8e2ee1
Oliver Skroblin <o.skroblin@shopware.com>
2017-09-27 11:06:56 +0200

    Fix listing price indexing process.

--- src/Denormalization/Product/ProductIndexer.php
+++ src/Denormalization/Product/ProductIndexer.php
@@ -3,8 +3,16 @@
 namespace Shopware\Denormalization\Product;
 
 use Doctrine\DBAL\Connection;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
 use Shopware\Context\Struct\TranslationContext;
+use Shopware\CustomerGroup\Repository\CustomerGroupRepository;
+use Shopware\ProductListingPrice\Struct\ProductListingPriceBasicCollection;
 use Shopware\ProductListingPrice\Struct\ProductListingPriceBasicStruct;
+use Shopware\Search\Criteria;
+use Shopware\Search\UuidSearchResult;
+use Shopware\Storefront\Context\StorefrontContextService;
 
 class ProductIndexer
 {
@@ -18,10 +26,26 @@ class ProductIndexer
      */
     private $listingPriceLoader;
 
-    public function __construct(Connection $connection, ListingPriceLoader $listingPriceLoader)
-    {
+    /**
+     * @var CustomerGroupRepository
+     */
+    private $customerGroupRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        Connection $connection,
+        ListingPriceLoader $listingPriceLoader,
+        CustomerGroupRepository $customerGroupRepository,
+        LoggerInterface $logger
+    ) {
         $this->connection = $connection;
         $this->listingPriceLoader = $listingPriceLoader;
+        $this->customerGroupRepository = $customerGroupRepository;
+        $this->logger = $logger;
     }
 
     public function index(array $uuids, TranslationContext $context): void
@@ -33,7 +57,9 @@ class ProductIndexer
 
     public function indexListingPrices(array $uuids, TranslationContext $context): void
     {
-        $this->connection->transactional(function () use ($uuids) {
+        $this->connection->transactional(function () use ($uuids, $context) {
+            $customerGroups = $this->customerGroupRepository->searchUuids(new Criteria(), $context);
+
             $listingPrices = $this->listingPriceLoader->load($uuids);
 
             $insert = $this->connection->prepare('
@@ -47,16 +73,57 @@ class ProductIndexer
                 ['uuids' => Connection::PARAM_STR_ARRAY]
             );
 
-            /** @var ProductListingPriceBasicStruct $price */
-            foreach ($listingPrices as $price) {
-                $insert->execute([
-                    'uuid' => $price->getUuid(),
-                    'product_uuid' => $price->getProductUuid(),
-                    'customer_group_uuid' => $price->getCustomerGroupUuid(),
-                    'price' => $price->getPrice(),
-                ]);
+            foreach ($uuids as $productUuid) {
+                $prices = $this->prepareProductPrices($productUuid, $listingPrices, $customerGroups);
+
+                /** @var ProductListingPriceBasicStruct $price */
+                foreach ($prices as $price) {
+                    $insert->execute([
+                        'uuid' => $price->getUuid(),
+                        'product_uuid' => $price->getProductUuid(),
+                        'customer_group_uuid' => $price->getCustomerGroupUuid(),
+                        'price' => $price->getPrice(),
+                    ]);
+                }
             }
         });
+    }
+
+    private function prepareProductPrices(
+        string $productUuid,
+        ProductListingPriceBasicCollection $listingPrices,
+        UuidSearchResult $customerGroups
+    ): ProductListingPriceBasicCollection {
+        $prices = $listingPrices->filterByProductUuid($productUuid);
+
+        $fallback = $prices->filterByCustomerGroupUuid(StorefrontContextService::FALLBACK_CUSTOMER_GROUP);
+
+        if ($fallback->count() <= 0) {
+            $this->logger->log(Logger::WARNING, sprintf('Product %s has no default customer group price', $productUuid));
+            return $prices;
+        }
+
+        $fallback = $fallback->first();
+
+        /** @var ProductListingPriceBasicStruct $fallback */
+        foreach ($customerGroups->getUuids() as $uuid) {
+            if ($uuid === StorefrontContextService::FALLBACK_CUSTOMER_GROUP) {
+                continue;
+            }
+
+            //check if customer prices exists
+            $customerPrice = $prices->filterByCustomerGroupUuid($uuid);
+            if ($customerPrice->count() > 0) {
+                continue;
+            }
+
+            $customerPrice = clone $fallback;
+            $customerPrice->setCustomerGroupUuid($uuid);
+            $customerPrice->setUuid(Uuid::uuid4()->toString());
+            $prices->add($customerPrice);
+        }
+
+        return $prices;
     }
 
     public function refreshCategoryAssignment(array $uuids, $context): void

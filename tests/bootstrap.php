63c470084a08519c19b945c62c7fcfb6d20b9a7b
Jeffrey Böhm <jeffrey.boehm@twt.de>
2017-10-19 23:24:41 +0200

    Always use test environment in testing mode

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
 
-(new Dotenv())->load(__DIR__.'/../.env');
+$dotEnv = new Dotenv();
+$values = $dotEnv->parse(file_get_contents(__DIR__ . '/../.env'));
+unset($values['APP_ENV']);
+$dotEnv->populate($values);

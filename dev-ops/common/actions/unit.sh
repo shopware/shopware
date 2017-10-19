189977552de1bc865a7a98e4bb880713ea15ce80
Jan Bücker <j.buecker@shopware.com>
2017-10-19 23:25:01 +0200

    Fix symfony onto 3.4.0-BETA1

--- dev-ops/common/actions/unit.sh
+++ dev-ops/common/actions/unit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: execute unit tests
 
-./phpunit -c phpunit.xml --stop-on-failure --stop-on-error --log-junit=build/artifacts/junit.xml
+php vendor/bin/phpunit -c phpunit.xml --stop-on-failure --stop-on-error --log-junit=build/artifacts/junit.xml

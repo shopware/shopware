910f2ab6c9b59a6ecf3cecaf073ec1cf637d01c6
Oliver Skroblin <o.skroblin@shopware.com>
2017-10-26 08:50:57 +0200

    Unify decoration of services in tracable compiler pass

--- src/Traceable/DependencyInjection/CartTracerCompilerPass.php
+++ src/Traceable/DependencyInjection/CartTracerCompilerPass.php
@@ -17,12 +17,12 @@ class CartTracerCompilerPass implements CompilerPassInterface
         $services = $container->findTaggedServiceIds('cart.processor');
 
         foreach ($services as $id => $tags) {
-            $this->replaceProcessor($container, $id);
+            $this->decorateService($container, $id, ProcessorTracer::class);
         }
 
         $services = $container->findTaggedServiceIds('cart.collector');
         foreach ($services as $id => $tags) {
-            $this->replaceCollector($container, $id);
+            $this->decorateService($container, $id, CollectorTracer::class);
         }
 
         $definition = new Definition(
@@ -36,20 +36,9 @@ class CartTracerCompilerPass implements CompilerPassInterface
         $container->setDefinition('cart.calculator.tracer', $definition);
     }
 
-    protected function replaceProcessor(ContainerBuilder $container, string $serviceId): void
+    protected function decorateService(ContainerBuilder $container, string $serviceId, string $class): void
     {
         $new = new Definition(ProcessorTracer::class, [
-            new Reference($serviceId . '.tracer.inner'),
-            new Reference('shopware.traceable.traced_cart_actions'),
-        ]);
-
-        $new->setDecoratedService($serviceId);
-        $container->setDefinition($serviceId . '.tracer', $new);
-    }
-
-    private function replaceCollector(ContainerBuilder $container, string $serviceId)
-    {
-        $new = new Definition(CollectorTracer::class, [
             new Reference($serviceId . '.tracer.inner'),
             new Reference('shopware.traceable.traced_cart_actions'),
         ]);

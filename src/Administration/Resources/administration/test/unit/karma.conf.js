bb19e4fcd8c0ab9b0e89a5d44407728ae287ea0e
Stephan Pohl <stp@shopware.com>
2017-11-14 09:10:31 +0100

    Remove PhantomJS from the default test browsers

--- src/Administration/Resources/administration/test/unit/karma.conf.js
+++ src/Administration/Resources/administration/test/unit/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
         // 1. install corresponding karma launcher
         //    http://karma-runner.github.io/0.13/config/browsers.html
         // 2. add it to the `browsers` array below.
-        browsers: [ 'ChromeHeadless', 'PhantomJS' ],
+        browsers: [ 'ChromeHeadless' ],
         frameworks: [ 'mocha', 'sinon-chai', 'chai' ],
         reporters: [ 'spec', 'coverage', 'junit' ],
         files: [

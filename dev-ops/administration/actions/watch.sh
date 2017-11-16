60aa9d8b26fc5d6850e32c233ddfc8d92687012e
Stephan Pohl <stp@shopware.com>
2017-11-16 08:48:57 +0100

    Added DEVPORT to define a hot module reloading port for the administration

--- dev-ops/administration/actions/watch.sh
+++ dev-ops/administration/actions/watch.sh
@@ -2,4 +2,4 @@
 #DESCRIPTION: starting administration dev server for hot module reloading
 
 bin/console administration:dump:plugins
-npm run --prefix src/Administration/Resources/administration/ dev -- __SW_HOST____SW_BASE_PATH__
+PORT=__DEVPORT__ npm run --prefix src/Administration/Resources/administration/ dev -- __SW_HOST____SW_BASE_PATH__

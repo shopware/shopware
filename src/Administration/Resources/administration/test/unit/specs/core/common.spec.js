ab11a6797c0d01a3c520553f5b24c31457eaa376
Stephan Pohl <stp@shopware.com>
2017-11-28 11:11:51 +0100

    NEXT-2 Add a simple test which ensures the api isn't changing

---
+++ src/Administration/Resources/administration/test/unit/specs/core/common.spec.js
@@ -0,0 +1,32 @@
+describe('core/common.js', () => {
+    it('should contain the necessary methods for the module factory', () => {
+        expect(Shopware.Module).to.have.property('register');
+        expect(Shopware.Module).to.have.property('getRegistry');
+        expect(Shopware.Module).to.have.property('getRoutes');
+    });
+
+    it('should contain the necessary methods for the component factory', () => {
+        expect(Shopware.Component).to.have.property('register');
+        expect(Shopware.Component).to.have.property('extend');
+        expect(Shopware.Component).to.have.property('override');
+        expect(Shopware.Component).to.have.property('build');
+        expect(Shopware.Component).to.have.property('getRegistry');
+        expect(Shopware.Component).to.have.property('getTemplate');
+    });
+
+    it('should contain the necessary methods for the template factory', () => {
+        expect(Shopware.Template).to.have.property('register');
+        expect(Shopware.Template).to.have.property('extend');
+        expect(Shopware.Template).to.have.property('override');
+        expect(Shopware.Template).to.have.property('getRenderedTemplate');
+        expect(Shopware.Template).to.have.property('getRegistry');
+        expect(Shopware.Template).to.have.property('getOverrideRegistry');
+        expect(Shopware.Template).to.have.property('find');
+        expect(Shopware.Template).to.have.property('findOverride');
+    });
+
+    it('should contain the utility collection & application', () => {
+        expect(Shopware).to.have.property('Utils');
+        expect(Shopware).to.have.property('Application');
+    });
+});

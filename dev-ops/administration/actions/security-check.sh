08625e2d30cce589053db3e2f719fc8449742353
Stephan Pohl <stp@shopware.com>
2018-01-02 14:24:36 +0100

    NTR - Update administration and add command for security check

---
+++ dev-ops/administration/actions/security-check.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#DESCRIPTION: runs an automated security check of vulnerabilities based Node Security Platform
+npm run --prefix src/Administration/Resources/administration/ security-check

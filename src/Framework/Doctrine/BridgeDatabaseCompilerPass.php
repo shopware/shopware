d15e0058ce4c3e370a55d07490e83437453312ad
Oliver Skroblin <o.skroblin@shopware.com>
2018-02-08 12:39:48 +0100

    NEXT-119 - Change service ids to class name

--- src/Framework/Doctrine/BridgeDatabaseCompilerPass.php
+++ src/Framework/Doctrine/BridgeDatabaseCompilerPass.php
@@ -34,6 +34,6 @@ class BridgeDatabaseCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $container->getDefinition('doctrine.dbal.connection_factory')
-                  ->addArgument(new Reference('db_connection'));
+                  ->addArgument(new Reference('PDO'));
     }
 }

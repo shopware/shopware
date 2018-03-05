fb07fc815134abc9fbcabc4c404a4f7ec3ba2d00
Oliver Skroblin <o.skroblin@shopware.com>
2018-03-05 13:11:42 +0100

    NEXT-153 - Fix date time type hints in api generator

--- dev-ops/api-generator/common/Util.php
+++ dev-ops/api-generator/common/Util.php
@@ -81,7 +81,7 @@ class Util
             case 'IntField':
                 return 'int';
             case 'DateField':
-                return \DateTime::class;
+                return "\\DateTime";
             case 'FloatField':
                 return 'float';
             case 'BoolField':

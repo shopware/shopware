cd651c050957bcbc162ff78151c94a44c049c7ed
Philipp Schuch <psc@shopware.com>
2018-03-27 16:19:24 +0200

    NEXT-206 - Refactored product detail page component

--- src/Administration/Resources/administration/src/module/sw-product/page/sw-product-detail/index.js
+++ src/Administration/Resources/administration/src/module/sw-product/page/sw-product-detail/index.js
@@ -3,7 +3,7 @@ import template from './sw-product-detail.html.twig';
 import './sw-product-detail.less';
 
 Component.register('sw-product-detail', {
-    inject: ['customerGroupService'],
+    template,
 
     mixins: [
         Mixin.getByName('product'),
@@ -12,54 +12,15 @@ Component.register('sw-product-detail', {
         Mixin.getByName('notification')
     ],
 
-    data() {
-        return {
-            customerGroups: []
-        };
-    },
-
-    computed: {
-        customerGroupOptions() {
-            const options = [];
-
-            this.customerGroups.forEach((item) => {
-                options.push({
-                    value: item.id,
-                    label: item.name
-                });
-            });
-
-            return options;
-        }
-    },
-
     created() {
         if (this.$route.params.id) {
             this.productId = this.$route.params.id;
         }
-
-        this.getData();
-    },
-
-    watch: {
-        $route: 'getData'
     },
 
     methods: {
-        getData() {
-            this.getCustomerGroupData();
-        },
-
-        getCustomerGroupData() {
-            this.customerGroupService.getList().then((response) => {
-                this.customerGroups = response.data;
-            });
-        },
-
         onSave() {
             this.saveProduct();
         }
-    },
-
-    template
+    }
 });

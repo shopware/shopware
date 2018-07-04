a1ad56d215a2826158fa9934f9a2f12b0595d7eb
Oliver Skroblin <o.skroblin@shopware.com>
2018-07-04 08:31:40 +0200

    NEXT-349 - Add test cases to modify price matrix

--- src/Core/Content/Test/Product/Repository/ProductRepositoryTest.php
+++ src/Core/Content/Test/Product/Repository/ProductRepositoryTest.php
@@ -1540,6 +1540,123 @@ class ProductRepositoryTest extends KernelTestCase
         /* @var PriceRuleStruct $price */
         $this->assertEquals(50, $price->getPrice()->getGross());
     }
+
+    public function testModifyProductPriceMatrix()
+    {
+        $ruleA = Uuid::uuid4()->getHex();
+        $ruleB = Uuid::uuid4()->getHex();
+
+        self::$container->get('rule.repository')->create([
+            ['id' => $ruleA, 'name' => 'test', 'payload' => new AndRule(), 'priority' => 1],
+            ['id' => $ruleB, 'name' => 'test', 'payload' => new AndRule(), 'priority' => 2],
+        ], Context::createDefaultContext(Defaults::TENANT_ID));
+
+        $id = Uuid::uuid4()->getHex();
+
+        $data = [
+            'id' => $id,
+            'name' => 'price test',
+            'price' => ['gross' => 15, 'net' => 10],
+            'manufacturer' => ['name' => 'test'],
+            'tax' => ['name' => 'test', 'rate' => 15],
+            'priceRules' => [
+                [
+                    'id' => $id,
+                    'currencyId' => Defaults::CURRENCY,
+                    'quantityStart' => 1,
+
+                    'ruleId' => $ruleA,
+                    'price' => ['gross' => 100, 'net' => 100],
+                ],
+            ],
+        ];
+
+        $context = Context::createDefaultContext(Defaults::TENANT_ID);
+        $this->repository->create([$data], $context);
+
+        $products = $this->repository->read(new ReadCriteria([$id]), $context);
+        $this->assertTrue($products->has($id));
+
+        /** @var ProductStruct $product */
+        $product = $products->get($id);
+
+        $this->assertCount(1, $product->getPriceRules());
+
+        /** @var ProductPriceRuleStruct $price */
+        $price = $product->getPriceRules()->first();
+        $this->assertEquals($ruleA, $price->getRuleId());
+
+        $data = [
+            'id' => $id,
+            'priceRules' => [
+                //update existing rule with new price and quantity end to add another graduation
+                [
+                    'id' => $id,
+                    'quantityEnd' => 20,
+                    'price' => ['gross' => 5000, 'net' => 4000],
+                ],
+
+                //add new graduation to existing rule
+                [
+                    'currencyId' => Defaults::CURRENCY,
+                    'quantityStart' => 21,
+                    'ruleId' => $ruleA,
+                    'price' => ['gross' => 10, 'net' => 50],
+                ],
+            ],
+        ];
+
+        $this->repository->upsert([$data], $context);
+
+        $products = $this->repository->read(new ReadCriteria([$id]), $context);
+        $this->assertTrue($products->has($id));
+
+        /** @var ProductStruct $product */
+        $product = $products->get($id);
+
+        $this->assertCount(2, $product->getPriceRules());
+
+        /** @var ProductPriceRuleStruct $price */
+        $price = $product->getPriceRules()->get($id);
+        $this->assertEquals($ruleA, $price->getRuleId());
+        $this->assertEquals(new PriceStruct(4000, 5000), $price->getPrice());
+
+        $this->assertEquals(1, $price->getQuantityStart());
+        $this->assertEquals(20, $price->getQuantityEnd());
+
+        $id3 = Uuid::uuid4()->getHex();
+
+        $data = [
+            'id' => $id,
+            'priceRules' => [
+                [
+                    'id' => $id3,
+                    'currencyId' => Defaults::CURRENCY,
+                    'quantityStart' => 1,
+                    'ruleId' => $ruleB,
+                    'price' => ['gross' => 50, 'net' => 50],
+                ],
+            ],
+        ];
+
+        $this->repository->upsert([$data], $context);
+
+        $products = $this->repository->read(new ReadCriteria([$id]), $context);
+        $this->assertTrue($products->has($id));
+
+        /** @var ProductStruct $product */
+        $product = $products->get($id);
+
+        $this->assertCount(3, $product->getPriceRules());
+
+        /** @var ProductPriceRuleStruct $price */
+        $price = $product->getPriceRules()->get($id3);
+        $this->assertEquals($ruleB, $price->getRuleId());
+        $this->assertEquals(new PriceStruct(50, 50), $price->getPrice());
+
+        $this->assertEquals(1, $price->getQuantityStart());
+        $this->assertNull($price->getQuantityEnd());
+    }
 }
 
 class CallableClass

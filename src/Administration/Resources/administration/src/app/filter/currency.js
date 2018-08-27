7c821b082f9c9faa65ae25bf088d9c6a7ed8b407
Timo Altholtmann <gitlab@altholtmann.com>
2018-08-27 10:25:14 +0200

    NEXT-788 - Fix qa suggestions in orders module

--- src/Administration/Resources/administration/src/app/filter/currency.js
+++ src/Administration/Resources/administration/src/app/filter/currency.js
@@ -2,5 +2,8 @@ import { currency } from 'src/core/service/utils/format.utils';
 import { Filter } from 'src/core/shopware';
 
 Filter.register('currency', (value, format = 'EUR') => {
+    if (value === null) {
+        return '-';
+    }
     return currency(value, format);
 });

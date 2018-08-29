13d44527f80ca84ba9ff0eabcef70fd842021235
Jan Pietrzyk <jp@shopware.com>
2018-08-29 16:02:00 +0200

    NTR- remove redundant memory cache

--- src/Core/Checkout/Context/CheckoutContextService.php
+++ src/Core/Checkout/Context/CheckoutContextService.php
@@ -1,26 +1,4 @@
 <?php declare(strict_types=1);
-/**
- * Shopware 5
- * Copyright (c) shopware AG
- *
- * According to our dual licensing model, this program can be used either
- * under the terms of the GNU Affero General Public License, version 3,
- * or under a proprietary license.
- *
- * The texts of the GNU Affero General Public License with an additional
- * permission and of our proprietary license can be found at and
- * in the LICENSE file you have received along with this program.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * "Shopware" is a registered trademark of shopware AG.
- * The licensing of the program under the AGPLv3 does not imply a
- * trademark license. Therefore any rights, title and interest in
- * our trademarks remain entirely with us.
- */
 
 namespace Shopware\Core\Checkout\Context;
 
@@ -58,11 +36,6 @@ class CheckoutContextService implements CheckoutContextServiceInterface
     private $factory;
 
     /**
-     * @var CheckoutContext[]
-     */
-    private $context = [];
-
-    /**
      * @var CheckoutRuleLoader
      */
     private $ruleLoader;
@@ -91,8 +64,6 @@ class CheckoutContextService implements CheckoutContextServiceInterface
 
     public function refresh(string $tenantId, string $salesChannelId, string $token): void
     {
-        $key = $salesChannelId . '-' . $token . '-' . $tenantId;
-        $this->context[$key] = null;
         $this->load($tenantId, $salesChannelId, $token, false);
     }
 

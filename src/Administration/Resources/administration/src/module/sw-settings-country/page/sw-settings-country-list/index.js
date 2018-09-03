f24e59cfe42c9f68ed85dd3e224fb16cd01412af
Timo Altholtmann <gitlab@altholtmann.com>
2018-09-03 14:59:16 +0200

    NEXT-788 - Fix active icon style in settings

--- src/Administration/Resources/administration/src/module/sw-settings-country/page/sw-settings-country-list/index.js
+++ src/Administration/Resources/administration/src/module/sw-settings-country/page/sw-settings-country-list/index.js
@@ -1,5 +1,6 @@
 import { Component, Mixin } from 'src/core/shopware';
 import template from './sw-settings-country-list.html.twig';
+import './sw-settings-country-list.less';
 
 Component.register('sw-settings-country-list', {
     template,
--- src/Administration/Resources/administration/src/module/sw-settings-payment/page/sw-settings-payment-list/index.js
+++ src/Administration/Resources/administration/src/module/sw-settings-payment/page/sw-settings-payment-list/index.js
@@ -1,5 +1,6 @@
 import { Component, Mixin, State } from 'src/core/shopware';
 import template from './sw-settings-payment-list.html.twig';
+import './sw-settings-payment-list.less';
 
 Component.register('sw-settings-payment-list', {
     template,

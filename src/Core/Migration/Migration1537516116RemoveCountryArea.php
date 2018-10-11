a46cb17d477770643a41c74b937f658c24a6d605
Jens Küper <J.Kueper@shopware.com>
2018-10-11 15:05:18 +0200

    Move migrations to new location

--- src/Core/Migration/Migration1537516116RemoveCountryArea.php
+++ src/Core/Migration/Migration1537516116RemoveCountryArea.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Core\Version;
+namespace Shopware\Core\Migration;
 
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;

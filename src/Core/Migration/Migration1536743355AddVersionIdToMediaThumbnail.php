78d72de1ffaa2ee15b2ed59e2a03eb796bd78986
Sebastian Franze <s.franze@shopware.com>
2018-10-15 10:00:51 +0200

    NEXT-679 - Extend Migration 1536743355 with trigger

--- src/Core/Migration/Migration1536743355AddVersionIdToMediaThumbnail.php
+++ src/Core/Migration/Migration1536743355AddVersionIdToMediaThumbnail.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Core\Version;
+namespace Shopware\Core\Migration;
 
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
 class Migration1536743355AddVersionIdToMediaThumbnail extends MigrationStep
 {
+    private const FORWARD_TRIGGER_NAME = 'forward_trigger_1536743355';
+
     public function getCreationTimestamp(): int
     {
         return 1536743355;
@@ -23,9 +25,19 @@ class Migration1536743355AddVersionIdToMediaThumbnail extends MigrationStep
             UPDATE `media_thumbnail`
                SET `version_id` = `media_version_id`
         ');
+
+        $this->addForwardTrigger(
+            $connection,
+            self::FORWARD_TRIGGER_NAME,
+            'media_thumbnail',
+            'BEFORE',
+            'INSERT',
+            'SET new.version_id = new.media_version_id'
+        );
     }
 
     public function updateDestructive(Connection $connection): void
     {
+        $this->removeTrigger($connection, self::FORWARD_TRIGGER_NAME);
     }
 }

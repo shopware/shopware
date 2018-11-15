e6e747d418a8f4469234cbeba9237cb32686460d
Daniel Meyer <d.meyer@shopware.com>
2018-11-15 14:03:15 +0100

    NEXT-975 - Finalize media upload test

--- src/Administration/Resources/e2e/repos/administration/specs/media/upload-media-item.spec.js
+++ src/Administration/Resources/e2e/repos/administration/specs/media/upload-media-item.spec.js
@@ -1,8 +1,9 @@
 module.exports = {
     '@tags': ['media-upload'],
-    'login to administration': (browser) => {
+    'open media listing': (browser) => {
         browser
-            .openMainMenuEntry('#/sw/media/index','Media');
+            .openMainMenuEntry('#/sw/media/index','Media')
+            .assert.urlContains('#/sw/media/index');
     },
     'open media index and change catalog': (browser) => {
         browser
@@ -11,7 +12,7 @@ module.exports = {
             .waitForElementVisible('div.sw-media-grid.sw-media-index__catalog-grid')
             .moveToElement('.sw-media-index__catalog-grid .sw-media-grid__content-cell', 15, 15)
             .click('.sw-media-index__catalog-grid .sw-media-grid__content-cell')
-            .waitForElementVisible('div.sw-page.sw-media-catalog')
+            .waitForElementVisible('div.sw-page.sw-media-catalog');
     },
     'upload and create first media item': (browser) => {
         browser
@@ -23,7 +24,7 @@ module.exports = {
             .fillField('input[name=sw-field--url]',`${process.env.APP_URL}/bundles/administration/static/img/sw-media-background.png`)
             .click('.sw-modal__footer .sw-button--primary')
             .waitForElementVisible('.sw-alert--success')
-            .click('.sw-alert__close')
+            .click('.sw-alert__close');
     },
     'upload and create second media item': (browser) => {
         browser
@@ -34,9 +35,9 @@ module.exports = {
             .fillField('input[name=sw-field--url]', `${process.env.APP_URL}/bundles/administration/static/img/sw-media-background.png`)
             .click('.sw-modal__footer .sw-button--primary')
             .waitForElementVisible('.sw-alert--success')
-            .click('.sw-alert__close')
+            .click('.sw-alert__close');
     },
-    'look for items in media index and default catalog': (browser) => {
+    'look for both items in media index and default catalog': (browser) => {
         browser
             .click('a.sw-admin-menu__navigation-link[href="#/sw/media/index"]')
             .waitForElementVisible('.sw-media-grid__content-cell:nth-of-type(2)')
@@ -47,7 +48,7 @@ module.exports = {
     'click preview thumbnail to open sidebar': (browser) => {
         browser
             .click('.sw-media-preview__item')
-            .waitForElementVisible('.sw-sidebar-item__content')
+            .waitForElementVisible('.sw-sidebar-item__content');
 
     },
     'verify meta data': (browser) => {
@@ -63,7 +64,7 @@ module.exports = {
             .assert.containsText('.sw-media-quickinfo-metadata-width', 'Width:')
             .assert.containsText('.sw-media-quickinfo-metadata-width', '16px')
             .assert.containsText('.sw-media-quickinfo-metadata-height', 'Height:')
-            .assert.containsText('.sw-media-quickinfo-metadata-height', '16px')
+            .assert.containsText('.sw-media-quickinfo-metadata-height', '16px');
     },
     'delete first item and check deletion verification': (browser) => {
         browser
@@ -74,7 +75,7 @@ module.exports = {
             .click('.sw-media-modal-delete__confirm')
             .waitForElementNotPresent('.sw-modal__footer')
             .waitForElementVisible ('.sw-alert--notification')
-            .click('.sw-alert__close')
+            .click('.sw-alert__close');
     },
     'delete second item and verify deletion': (browser) => {
         browser

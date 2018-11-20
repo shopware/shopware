0a681b315e2bd3cdd6033242b3d53df1c99eca27
Daniel Meyer <d.meyer@shopware.com>
2018-11-20 14:57:43 +0100

    NEXT-1071 - CRUD manufacturer

--- src/Administration/Resources/e2e/repos/administration/specs/product/create-manufacturer.spec.js
+++ src/Administration/Resources/e2e/repos/administration/specs/product/create-manufacturer.spec.js
@@ -1,18 +1,18 @@
 module.exports = {
     '@tags': ['manufacturer-create', 'manufacturer', 'create'],
-    'navigate to manufacturer module': (browser) => {
+    'navigate to manufacturer module and click on add manufacturer': (browser) => {
         browser
-            .openMainMenuEntry('#/sw/product/index', 'Product')
-            .openMainMenuEntry('#/sw/manufacturer/index', 'Manufacturer')
+            .openMainMenuEntry('#/sw/product/index', 'Product', '#/sw/manufacturer/index', 'Manufacturer')
+            .assert.urlContains('#/sw/manufacturer/index')
             .waitForElementPresent('.sw-button__content')
             .click('.sw-button__content');
     },
     'enter manufacturer information': (browser) => {
-    browser
+        browser
             .assert.containsText('.smart-bar__header', 'New manufacturer')
             .fillField('input[name=name]', 'MAN-U-FACTURE')
-            .fillField('input[name=link]', 'https://tomate.su/ist-seehofer-noch-im-amt/')
-            .setValue('.ql-editor.ql-blank', 'De-scribe THIS! \n \n Yours sincerely, \n \n The Manufacturer')
+            .fillField('input[name=link]', 'https://www.google.com/doodles')
+            .setValue('.ql-editor', 'De-scribe THIS! \n \n Yours sincerely, \n \n The Manufacturer')
             .click('.sw-button--primary')
             .waitForElementVisible('.sw-notifications .sw-alert')
             .click('.sw-alert button.sw-alert__close')
@@ -22,43 +22,61 @@ module.exports = {
             .fillField('input[name=sw-field--url]', `${process.env.APP_URL}/bundles/administration/static/img/sw-media-background.png`)
             .click('.sw-modal__footer .sw-button--primary')
             .waitForElementVisible('.sw-alert--success')
-            .click('.sw-alert__close')
+            .click('.sw-alert .sw-alert__close')
+            .waitForElementNotPresent('.sw-alert--success')
             .click('.sw-button--primary')
-            .waitForElementPresent('.sw-alert__close')
-            .click('.sw-alert button.sw-alert__close')
-            .click('.sw-button');
+            .waitForElementVisible('.sw-alert')
+            .waitForElementPresent('.sw-button__content')
+            .click('.sw-alert .sw-alert__close');
     },
     'check if new manufacturer exists in overview': (browser) => {
         browser
+            .openMainMenuEntry('#/sw/product/index', 'Product', '#/sw/manufacturer/index', 'Manufacturer')
+            .waitForElementPresent('.sw-button__content')
             .assert.urlContains('#/sw/manufacturer/index')
             .assert.containsText('.smart-bar__header', 'Manufacturer')
-            .waitForElementPresent('sw-sidebar__navigation')
-            .click('sw-sidebar-navigation-item')
-            .waitForElementPresent('.sw-grid-row:first-child', 'MAN-U-FACTURE');
+            .waitForElementVisible('.sw-button__content')
+            .assert.containsText('.sw-grid-row:first-child', 'MAN-U-FACTURE');
     },
-    'edit manufacturer information': (browser) => {
+    'open manufacturer details and change the given data': (browser) => {
         browser
-            .waitForElementPresent('.sw-context-button__button')
+            .waitForElementPresent('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .click('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .waitForElementVisible('.sw-grid-row:first-child .sw-context-button__button')
             .click('.sw-grid-row:first-child .sw-context-button__button')
-            .waitForElementPresent('.sw-context-menu')
+            .waitForElementVisible('.sw-context-menu')
             .click('.sw-context-menu .sw-context-menu-item__text')
-            .waitForElementPresent('.smart-bar__header', 'MAN-U-FACTURE')
+            .waitForElementVisible('.sw-card__title','Manufacturer information')
             .assert.containsText('.smart-bar__header','MAN-U-FACTURE')
-            .fillField('input[name=name]', 'Ramschladen')
+            .fillField('input[name=name]', 'Minnie\'s Haberdashery')
             .fillField('input[name=link]', 'https://google.com/doodles')
-            .setValue('.ql-editor', 'Bitch, get outta my way, bitch. ')
+            .setValue('.ql-editor', 'I would like to enter a meaningful description here. \n Ha, that was easy! \n Außerdem grüße ich \n Quentin Tarantino \n und \n meine Mama!!!!')
             .click('.sw-button--primary')
+            .waitForElementPresent('.sw-alert__close')
             .click('.sw-alert__close')
-            .click('.sw-button')
-            .waitForElementPresent('.smart-bar__header', 'Manufacturer')
-            .pause(2000)
-            .waitForElementVisible('sw-sidebar__navigation')
-            .click('sw-sidebar-navigation-item')
-            .pause(2000)
-            .assert.containsText('.sw-grid-row:first-child', 'Ramschladen')
-            .pause(5000);
+            .waitForElementNotPresent('.sw-alert__close')
+            .click('.sw-button__content');
     },
-
+    'open manufacturer overview again and delete manufacturer': (browser) => {
+        browser
+            .waitForElementPresent('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .click('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .waitForElementVisible('.sw-grid-row:first-child .sw-context-button__button')
+            .click('.sw-grid-row:first-child .sw-context-button__button')
+            .waitForElementVisible('.sw-context-menu')
+            .click('.sw-context-menu .sw-context-menu-item--danger')
+            .waitForElementVisible('.sw-modal')
+            .click('.sw-modal__footer button.sw-button--primary');
+    },
+    'check if manufacturer has been deleted': (browser) => {
+        browser
+            .openMainMenuEntry('#/sw/product/index', 'Product', '#/sw/manufacturer/index', 'Manufacturer')
+            .waitForElementVisible('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .click('.sw-sidebar__navigation .sw-sidebar-navigation-item')
+            .waitForElementVisible('.sw-grid-row:first-child')
+            .assert.containsText('.sw-grid-row:first-child', 'shopware AG')
+            .assert.containsText('.sw-page__smart-bar-amount', '(1)');
+    },
     after: (browser) => {
         browser.end();
     }

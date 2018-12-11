ab758d5ddacd4d9dccfbf2d955522ac4f66a4911
Stefan Sluiter <s.sluiter@shopware.com>
2018-12-11 09:33:11 +0100

    NEXT-1117 - test for write order changes

--- src/Core/Checkout/Test/Order/OrderRepositoryTest.php
+++ src/Core/Checkout/Test/Order/OrderRepositoryTest.php
@@ -14,7 +14,9 @@ use Shopware\Core\Checkout\Context\CheckoutContextService;
 use Shopware\Core\Checkout\Order\OrderDefinition;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Read\ReadCriteria;
 use Shopware\Core\Framework\DataAbstractionLayer\RepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Struct\Uuid;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 
@@ -54,6 +56,32 @@ class OrderRepositoryTest extends TestCase
         $this->customerRepository = $this->getContainer()->get('customer.repository');
         $this->processor = $this->getContainer()->get(Processor::class);
         $this->checkoutContextFactory = $this->getContainer()->get(CheckoutContextFactory::class);
+    }
+
+    public function testCreateOrder()
+    {
+        $orderId = Uuid::uuid4()->getHex();
+        $orderData = $this->getOrderData($orderId);
+        $defaultContext = Context::createDefaultContext();
+        $this->orderRepository->create($orderData, $defaultContext);
+        /*
+                $nestedCriteria2 = new Criteria();
+                $nestedCriteria2->addAssociation('customer.addresses');
+        
+                $nestedCriteria = new Criteria();
+                $nestedCriteria->addAssociation('order_customer.customer');
+                $nestedCriteria->addAssociation('order_customer.customer.addresses', $nestedCriteria2);
+        */
+        $criteria = new ReadCriteria([$orderId]);
+        //$criteria->addAssociation('order.orderCustomer', $nestedCriteria);
+
+        $order = $this->orderRepository->read($criteria, $defaultContext);
+
+        static::assertEquals($orderId, $order->first()->get('id'));
+        //static::assertNotNull($order->first()->getOrderCustomer());
+        //static::assertNotNull($order->first()->getOrderCustomer()->getCustomer());
+        //static::assertNotNull($order->first()->getOrderCustomer()->getCustomer()->getAddresses());
+        //static::assertEquals('OH', $order->first()->getOrderCustomer()->getCustomer()->getAddresses()->first()->getCountry()->getStates()->first()->getShortCode());
     }
 
     public function testDeleteOrder()
@@ -133,5 +161,128 @@ class OrderRepositoryTest extends TestCase
         $this->customerRepository->upsert([$customer], Context::createDefaultContext());
 
         return $customerId;
+    }
+
+    private function getOrderData($orderId)
+    {
+        $addressId = Uuid::uuid4()->getHex();
+        $orderLineItemId = Uuid::uuid4()->getHex();
+        $countryStateId = Uuid::uuid4()->getHex();
+
+        $order = [
+            [
+                'id' => $orderId,
+                'date' => date(DATE_ISO8601),
+                'amountTotal' => 0,
+                'amountNet' => 0,
+                'positionPrice' => 0,
+                'shippingTotal' => 0,
+                'shippingNet' => 0,
+                'isNet' => true,
+                'isTaxFree' => false,
+                'stateId' => Defaults::ORDER_STATE_OPEN,
+                'paymentMethodId' => Defaults::PAYMENT_METHOD_DEBIT,
+                'currencyId' => Defaults::CURRENCY,
+                'currencyFactor' => 1,
+                'salesChannelId' => Defaults::SALES_CHANNEL,
+                'deliveries' => [
+                    [
+                        'orderStateId' => Defaults::ORDER_STATE_OPEN,
+                        'shippingMethodId' => Defaults::SHIPPING_METHOD,
+                        'shippingDateEarliest' => date(DATE_ISO8601),
+                        'shippingDateLatest' => date(DATE_ISO8601),
+                        'shippingOrderAddress' => [
+                            'salutation' => 'mr',
+                            'firstName' => 'Floy',
+                            'lastName' => 'Glover',
+                            'zipcode' => '59438-0403',
+                            'city' => 'Stellaberg',
+                            'street' => 'street',
+                            'country' => [
+                                'name' => 'kasachstan',
+                                'id' => Defaults::COUNTRY,
+                            ],
+                        ],
+                        'positions' => [
+                            [
+                                'orderLineItemId' => $orderLineItemId,
+                                'unitPrice' => 1,
+                                'totalPrice' => 1,
+                                'quantity' => 1,
+                            ],
+                        ],
+                    ],
+                ],
+                'lineItems' => [
+                    [
+                        'id' => $orderLineItemId,
+                        'identifier' => 'test',
+                        'quantity' => 1,
+                        'unitPrice' => 1,
+                        'totalPrice' => 1,
+                        'type' => 'test',
+                        'label' => 'test',
+                        'priority' => 100,
+                        'good' => true,
+                    ],
+                ],
+                'deepLinkCode' => 'BwvdEInxOHBbwfRw6oHF1Q_orfYeo9RY',
+                'orderCustomer' => [
+                    'email' => 'test@example.com',
+                    'firstName' => 'Noe',
+                    'lastName' => 'Hill',
+                    'salutation' => 'Mr',
+                    'title' => 'Doc',
+                    'customerNumber' => 'Test',
+                    'customer' => [
+                        'email' => 'test@example.com',
+                        'firstName' => 'Noe',
+                        'lastName' => 'Hill',
+                        'salutation' => 'Mr',
+                        'title' => 'Doc',
+                        'customerNumber' => 'Test',
+                        'group' => ['name' => 'testse2323'],
+                        'defaultPaymentMethodId' => Defaults::PAYMENT_METHOD_DEBIT,
+                        'salesChannelId' => Defaults::SALES_CHANNEL,
+                        'defaultBillingAddressId' => $addressId,
+                        'defaultShippingAddressId' => $addressId,
+                        'addresses' => [
+                            [
+                                'salutation' => 'mr',
+                                'firstName' => 'Floy',
+                                'lastName' => 'Glover',
+                                'zipcode' => '59438-0403',
+                                'city' => 'Stellaberg',
+                                'street' => 'street',
+                                'countryStateId' => $countryStateId,
+                                'country' => [
+                                    'name' => 'kasachstan',
+                                    'id' => Defaults::COUNTRY,
+                                    'states' => [
+                                        [
+                                            'id' => $countryStateId,
+                                            'name' => 'oklahoma',
+                                            'shortCode' => 'OH',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'billingAddress' => [
+                    'salutation' => 'mr',
+                    'firstName' => 'Floy',
+                    'lastName' => 'Glover',
+                    'zipcode' => '59438-0403',
+                    'city' => 'Stellaberg',
+                    'street' => 'street',
+                    'countryId' => Defaults::COUNTRY,
+                    'id' => $addressId,
+                ],
+            ],
+        ];
+
+        return $order;
     }
 }

b9b60fa565ae0619e94e1e374fb7fc7110f2ec60
Ramona Schwering <R.Schwering@shopware.com>
2018-12-11 08:18:06 +0100

    NEXT-1255 - Add afterEach Hook for isolating tests

--- src/Administration/Resources/e2e/repos/administration/globals.js
+++ src/Administration/Resources/e2e/repos/administration/globals.js
@@ -3,11 +3,20 @@ require('./flags.js');
 
 const loginPage = require('./specs/before-scenario.js');
 
+const util = require('util');
+const path = require('path');
+const exec = util.promisify(require('child_process').exec);
+
+const resolve = (relativePath) => {
+    return path.resolve(__dirname, '..', relativePath);
+};
+
 module.exports = {
     waitForConditionTimeout: 30000,
     asyncHookTimeout: 30000,
 
     beforeEach: (browser, done) => {
+        // console.log(process.env);
         browser.url(browser.launch_url);
 
         browser.execute(function () {
@@ -21,6 +30,12 @@ module.exports = {
                 loginPage.login(browser, 'admin', 'shopware', done);
             }
 
+            done();
+        });
+    },
+    afterEach(done) {
+        console.log('Restoring clean state of database...');
+        exec(`mysql -u ${process.env.DB_USER} -p${process.env.DB_PASSWORD} -h mysql ${process.env.DB_NAME} < ${resolve('../temp/clean_db.sql')}`).then(() => {
             done();
         });
     }

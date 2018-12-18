4ae9388d23e7c301fed54948daaf827963cef80b
Oliver Skroblin <o.skroblin@shopware.com>
2018-12-18 16:47:16 +0100

    NEXT-1146 - Use decorator pattern for language loader caching

--- src/Core/System/Language/LanguageLoader.php
+++ src/Core/System/Language/LanguageLoader.php
@@ -3,71 +3,29 @@
 namespace Shopware\Core\System\Language;
 
 use Doctrine\DBAL\Connection;
-use Psr\Cache\CacheItemPoolInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Framework\Doctrine\FetchModeHelper;
 
-class LanguageLoader implements LanguageLoaderInterface, EventSubscriberInterface
+class LanguageLoader implements LanguageLoaderInterface
 {
-    private const CACHE_KEY = 'shopware.languages';
     /**
      * @var Connection
      */
     private $connection;
 
-    /**
-     * @var CacheItemPoolInterface
-     */
-    private $cache;
-
-    public function __construct(Connection $connection, CacheItemPoolInterface $cache)
+    public function __construct(Connection $connection)
     {
         $this->connection = $connection;
-        $this->cache = $cache;
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            LanguageEvents::LANGUAGE_DELETED_EVENT => 'invalidateCache',
-            LanguageEvents::LANGUAGE_WRITTEN_EVENT => 'invalidateCache',
-        ];
     }
 
     public function loadLanguages(): array
     {
-        $cacheItem = $this->cache->getItem(self::CACHE_KEY);
-        if ($cacheItem->isHit()) {
-            return $cacheItem->get();
-        }
-
         $data = $this->connection->createQueryBuilder()
-            ->select(['LOWER(HEX(language.id)) AS id, locale.code, LOWER(HEX(language.parent_id)) parentId'])
+            ->select(['LOWER(HEX(language.id)) AS array_key, LOWER(HEX(language.id)) AS id, locale.code, LOWER(HEX(language.parent_id)) parentId'])
             ->from('language')
             ->leftJoin('language', 'locale', 'locale', 'language.translation_code_id = locale.id')
             ->execute()
             ->fetchAll();
 
-        $languages = [];
-        foreach ($data as $row) {
-            $languages[$row['id']] = $row;
-            if ($row['code']) {
-                $languages[strtolower($row['code'])] = $row;
-            }
-        }
-        $cacheItem->set($languages);
-
-        return $languages;
-    }
-
-    /**
-     * @internal should only be called in response to a language update and delete event
-     */
-    public function invalidateCache(): void
-    {
-        $cacheItem = $this->cache->getItem(self::CACHE_KEY);
-        if (!$cacheItem->isHit()) {
-            return;
-        }
-        $this->cache->deleteItem($cacheItem->getKey());
+        return FetchModeHelper::groupUnique($data);
     }
 }

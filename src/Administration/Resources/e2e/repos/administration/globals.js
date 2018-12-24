546910cede593bca01c9544b9758644bf2334e3e
Patrick Weyck <p.weyck@shopware.com>
2018-12-24 08:34:01 +0100

    Use new `restore-db` command

--- src/Administration/Resources/e2e/repos/administration/globals.js
+++ src/Administration/Resources/e2e/repos/administration/globals.js
@@ -5,11 +5,7 @@ require('./service/fixture.service');
 const beforeScenarioActions = require('./specs/before-scenario.js');
 
 const util = require('util');
-const path = require('path');
 const exec = util.promisify(require('child_process').exec);
-const resolve = (relativePath) => {
-    return path.resolve(__dirname, '..', relativePath);
-};
 
 module.exports = {
     waitForConditionTimeout: 30000,
@@ -34,18 +30,10 @@ module.exports = {
         });
     },
     afterEach(done) {
-        console.log("### Setting database to clean state...");
-
-        if (process.env.E2E_ENV === "default") {
-            exec(`mysql -u ${process.env.DB_USER} -p${process.env.DB_PASSWORD} -h 127.0.0.1 --port=4406 ${process.env.DB_NAME} < ${resolve('../temp/clean_db.sql')}`).then(() => {
-                console.log('• ✓ - Done with reset');
-                done();
-            });
-        } else {
-            exec(`mysql -u ${process.env.DB_USER} -p${process.env.DB_PASSWORD} -h mysql --port=3306 ${process.env.DB_NAME} < ${resolve('../temp/clean_db.sql')}`).then(() => {
-                console.log('• ✓ - Done with reset');
-                done();
-            });
-        }
+        console.log("### Resetting database to clean state...");
+        exec(`${process.env.PROJECT_ROOT}psh.phar e2e:restore-db`).then(() => {
+            console.log('• ✓ - Done with reset');
+            done();
+        });
     }
 };

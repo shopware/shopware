eeeee12752c7f6084e7b14df4044029fc6c7d8d7
Tobias Berge <t.berge@shopware.com>
2019-01-15 08:44:48 +0100

    NTR - Add variants and more config for sw-label

--- src/Administration/Resources/administration/src/app/component/form/sw-label/index.js
+++ src/Administration/Resources/administration/src/app/component/form/sw-label/index.js
@@ -5,7 +5,7 @@ import template from './sw-label.html.twig';
 /**
  * @public
  * @status ready
- * @example-type code-only
+ * @example-type dynamic
  * @component-example
  * <sw-label>
  *     Text
@@ -15,10 +15,43 @@ Component.register('sw-label', {
     template,
 
     props: {
+        variant: {
+            type: String,
+            required: false,
+            default: '',
+            validValues: ['info', 'danger', 'success', 'warning'],
+            validator(value) {
+                if (!value.length) {
+                    return true;
+                }
+                return ['info', 'danger', 'success', 'warning'].includes(value);
+            }
+        },
         dismissAble: {
             type: Boolean,
             required: false,
             default: false
+        },
+        pill: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        ghost: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+
+    computed: {
+        labelClasses() {
+            return {
+                [`sw-label--${this.variant}`]: this.variant,
+                'sw-label--dismiss-able': this.dismissAble,
+                'sw-label--pill': this.pill,
+                'sw-label--ghost': this.ghost
+            };
         }
     }
 });

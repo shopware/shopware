f913f7bf6c9eaf6f9dcc95f340b397a42c68a3af
Timo Altholtmann <t.altholtmann@shopware.com>
2019-02-06 10:14:34 +0100

    NEXT-1610 - Fix text field placeholder

--- src/Administration/Resources/administration/src/app/component/form/sw-text-field/index.js
+++ src/Administration/Resources/administration/src/app/component/form/sw-text-field/index.js
@@ -97,7 +97,7 @@ export default {
             return this.label;
         },
         fieldPlaceholder() {
-            if (this.placeholder) {
+            if (!this.placeholder) {
                 return '';
             }
 

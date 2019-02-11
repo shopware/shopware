a95410211f63c1850c5e4959792251495629f25d
Marcel Brode <m.brode@shopware.com>
2019-02-11 14:55:58 +0100

    NEXT-1374 - Add snippet plugin documentation

---
+++ src/Docs/60-plugin-system/plugins/100-extend-snippets/SwagSnippetExample/Resources/SnippetFiles/SnippetFile_Tyrolean_de_DE.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace SwagSnippetExample\Resources\SnippetFiles;
+
+use Shopware\Core\Framework\Snippet\Files\SnippetFileInterface;
+
+class SnippetFile_Tyrolean_de_DE implements SnippetFileInterface
+{
+    public function getName(): string
+    {
+        return 'messages.tyrolean.de_DE';
+    }
+
+    public function getPath(): string
+    {
+        return __DIR__ . '/messages.tyrolean.de_DE.json';
+    }
+
+    public function getIso(): string
+    {
+        return 'de_DE';
+    }
+
+    public function getAuthor(): string
+    {
+        return 'exampleShop24AT';
+    }
+
+    public function isBase(): bool
+    {
+        return SnippetFileInterface::PLUGIN_SNIPPET_EXTENSION_FILE;
+    }
+}

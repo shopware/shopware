26bcbcd6a034057cf6f448d87ecf6184f0624b2f
barbieswimcrew <ms@dasistweb.de>
2019-02-15 10:01:51 +0100

    NEXT-1819 - configure stylelint
    Some stylelint configurations

--- src/Storefront/Resources/stylelint.config.js
+++ src/Storefront/Resources/stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     "extends": "stylelint-config-sass-guidelines",
     "rules": {
-        "indentation": 4
+        "indentation": 4,
+        "max-nesting-depth": 2,
+        "selector-no-qualifying-type": [
+            true, {
+                "ignore": ["attribute"]
+            }
+        ]
     }
 };

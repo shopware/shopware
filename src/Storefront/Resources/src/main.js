c2c8481ece4399810404203cc43b0bff31cb8eee
bott <bott@conexco.com>
2019-02-18 12:21:22 +0100

    NEXT-1879 - Updated scss variables, deactivated example js plugin

--- src/Storefront/Resources/src/main.js
+++ src/Storefront/Resources/src/main.js
@@ -5,23 +5,25 @@ import 'bootstrap';
 // Import styles
 import './assets/sass/main.scss';
 
-import Client from './service/http-client.service';
-import Plugin from './helper/plugin.helper';
+// import Client from './service/http-client.service';
+// import Plugin from './helper/plugin.helper';
 
-const client = new Client(window.accessKey, window.contextToken);
+// get listing example
+// const client = new Client(window.accessKey, window.contextToken);
 
-client.get('product?page=1&limit=10', function(response) {
-    console.log('client response', response);
-});
+// client.get('product?page=1&limit=10', function(response) {
+//     console.log('client response', response);
+// });
 
-const plugin = new Plugin('sw-simple-vanilla-plugin');
-plugin.on('initialized', () => {
-    console.log(
-        `Plugin %c"${plugin.name}" %cgot initialized`,
-        'font-weight: bold',
-        'font-weight: normal'
-    );
-});
+// plugin example
+// const plugin = new Plugin('sw-simple-vanilla-plugin');
+// plugin.on('initialized', () => {
+//     console.log(
+//         `Plugin %c"${plugin.name}" %cgot initialized`,
+//         'font-weight: bold',
+//         'font-weight: normal'
+//     );
+// });
 
 // Necessary for the webpack hot module reloading server
 if (module.hot) {

2f43504d3c73ba071ef6f1787aa89ee9e08b7ed2
Michael Telgmann <mt@shopware.com>
2019-02-19 16:11:51 +0100

    NEXT-1895 - Check old password before changing to the new one

--- src/Administration/Resources/administration/src/module/sw-profile/page/sw-profile-index/index.js
+++ src/Administration/Resources/administration/src/module/sw-profile/page/sw-profile-index/index.js
@@ -1,12 +1,13 @@
 import { Component, State, Mixin } from 'src/core/shopware';
 import { md5 } from 'src/core/service/utils/format.utils';
+import types from 'src/core/service/utils/types.utils';
 import template from './sw-profile-index.html.twig';
 import './sw-profile-index.scss';
 
 Component.register('sw-profile-index', {
     template,
 
-    inject: ['userService'],
+    inject: ['userService', 'loginService'],
 
     mixins: [
         Mixin.getByName('notification')
@@ -19,6 +20,7 @@ Component.register('sw-profile-index', {
                 isLoading: true
             },
             imageSize: 140,
+            oldPassword: null,
             newPassword: null,
             newPasswordConfirm: null
         };
@@ -56,26 +58,61 @@ Component.register('sw-profile-index', {
         },
 
         onSave() {
+            const passwordCheck = this.checkPassword();
+            if (passwordCheck === null) {
+                this.saveUser();
+            } else {
+                passwordCheck.then((validNewPassword) => {
+                    if (validNewPassword) {
+                        this.saveUser();
+                    }
+                });
+            }
+        },
+
+        checkPassword() {
             if (this.newPassword && this.newPassword.length > 0) {
-                if (this.newPassword !== this.newPasswordConfirm) {
-                    const errorTitle = this.$tc('sw-profile.index.notificationPasswordErrorTitle');
-                    const errorMessage = this.$tc('sw-profile.index.notificationPasswordErrorMessage');
+                return this.validateOldPassword().then((oldPasswordIsValid) => {
+                    if (oldPasswordIsValid === false) {
+                        this.createErrorMessage(this.$tc('sw-profile.index.notificationOldPasswordErrorMessage'));
+                        return false;
+                    }
 
-                    this.createNotificationError({
-                        title: errorTitle,
-                        message: errorMessage
-                    });
+                    if (this.newPassword !== this.newPasswordConfirm) {
+                        this.createErrorMessage(this.$tc('sw-profile.index.notificationPasswordErrorMessage'));
+                        return false;
+                    }
 
-                    return;
-                }
+                    this.user.password = this.newPassword;
 
-                this.user.password = this.newPassword;
+                    return true;
+                });
             }
 
+            return null;
+        },
+
+        validateOldPassword() {
+            return this.loginService.loginByUsername(this.user.username, this.oldPassword).then((response) => {
+                return types.isString(response.access);
+            }).catch(() => {
+                return false;
+            });
+        },
+
+        createErrorMessage(errorMessage) {
+            this.createNotificationError({
+                title: this.$tc('sw-profile.index.notificationPasswordErrorTitle'),
+                message: errorMessage
+            });
+        },
+
+        saveUser() {
             this.user.save().then(() => {
                 const successTitle = this.$tc('sw-profile.index.notificationSaveSuccessTitle');
                 const successMessage = this.$tc('sw-profile.index.notificationSaveSuccessMessage');
 
+                this.oldPassword = '';
                 this.newPassword = '';
                 this.newPasswordConfirm = '';
 

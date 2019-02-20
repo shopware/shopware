41034dbb7468e974e5cd6f41fc59377b07a7a05b
bott <bott@conexco.com>
2019-02-20 14:28:53 +0100

    NEXT-1879 - removed logo and search bar test because both are currently not integrated

--- src/Administration/Resources/e2e/repos/storefront/specs/homepage/homepage-open.spec.js
+++ src/Administration/Resources/e2e/repos/storefront/specs/homepage/homepage-open.spec.js
@@ -1,21 +1,5 @@
-const searchPage = require('storefront/page-objects/search.page-object.js');
-
 module.exports = {
     '@tags': ['homepage', 'homepage-open'],
-    'logo found': (browser) => {
-        browser
-            .waitForElementVisible('body')
-            .waitForElementVisible('.navbar')
-            .assert.visible('.navbar-brand');
-    },
-    'search bar found': (browser) => {
-        const page = searchPage(browser);
-
-        browser
-            .waitForElementVisible(page.elements.searchInput)
-            .checkIfElementExists(page.elements.searchInput)
-            .assert.visible(page.elements.searchInput);
-    },
     after: (browser) => {
         browser.end();
     }

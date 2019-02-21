dd9f60078ebeb7a968646d2ef3e3f618d7c2f678
Oliver Skroblin <o.skroblin@shopware.com>
2019-02-21 12:41:01 +0100

    Add test case to check if the storefront is rendered

--- src/Administration/Resources/e2e/repos/storefront/specs/homepage/homepage-open.spec.js
+++ src/Administration/Resources/e2e/repos/storefront/specs/homepage/homepage-open.spec.js
@@ -1,5 +1,9 @@
 module.exports = {
     '@tags': ['homepage', 'homepage-open'],
+    'storefront visible': (browser) => {
+        browser
+            .assert.visible('body');
+    },
     after: (browser) => {
         browser.end();
     }

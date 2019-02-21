5c3d793e9a9076ea8bf332824e4b7ed292fcd556
Jannis Leifeld <j.leifeld@shopware.com>
2019-02-21 15:04:14 +0100

    NEXT-2087 - Refactor the tab component with new design and vertical mode

--- src/Administration/Resources/administration/src/app/component/base/sw-tabs-item/index.js
+++ src/Administration/Resources/administration/src/app/component/base/sw-tabs-item/index.js
@@ -1,55 +1,96 @@
+import types from 'src/core/service/utils/types.utils';
 import template from './sw-tabs-item.html.twig';
 import './sw-tabs-item.scss';
 
 /**
- * @private
+ * @public
+ * @description Renders a tab item.
+ * @status ready
+ * @example-type static
+ * @component-example
+ * <sw-tabs>
+ *
+ *     <sw-tabs-item :to="{ name: 'sw.explore.index' }">
+ *         Explore
+ *     </sw-tabs-item>
+ *
+ *     <sw-tabs-item to="A link">
+ *         My Plugins
+ *     </sw-tabs-item>
+ *
+ * </sw-tabs>
  */
 export default {
     name: 'sw-tabs-item',
     template,
 
+    inheritAttrs: false,
+
     props: {
+        // The property "route" is deprecated and will be removed in the future. Use the new "to" property.
         route: {
-            type: [Object, String],
+            type: [String, Object],
             required: false,
             default: ''
         },
-        variant: {
+        active: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        activeTab: {
             type: String,
             required: false,
-            default: 'default',
-            validValues: ['default', 'minimal'],
-            validator(value) {
-                if (!value.length) {
-                    return true;
-                }
-                return ['default', 'minimal'].includes(value);
-            }
+            default: ''
         },
         name: {
             type: String,
             required: false,
             default: ''
-        },
-        active: {
-            type: String,
-            required: false,
-            default: ''
+        }
+    },
+
+    data() {
+        return {
+            isActive: false
+        };
+    },
+
+    mounted() {
+        if (this.$el.classList.contains('router-link-active')) {
+            this.isActive = true;
+        }
+        if (this.activeTab && this.activeTab === this.name) {
+            this.isActive = true;
+        }
+    },
+
+    created() {
+        this.$parent.$on('newActiveItem', this.checkIfActive);
+        if (this.active) {
+            this.isActive = true;
         }
     },
 
     computed: {
+        isNative() {
+            return types.isEmpty(this.route);
+        },
+
         tabsItemClasses() {
             return {
-                [`sw-tabs-item__${this.variant}`]: this.variant,
-                'is--active': this.name !== '' && this.name === this.active
+                'sw-tabs-item--active': this.isActive
             };
         }
     },
 
     methods: {
-        activate() {
-            this.$parent.active = this.name;
+        clickEvent() {
+            this.$parent.setActiveItem(this);
+            this.$emit('click');
+        },
+        checkIfActive(item) {
+            this.isActive = (item.$vnode === this.$vnode);
         }
     }
 };

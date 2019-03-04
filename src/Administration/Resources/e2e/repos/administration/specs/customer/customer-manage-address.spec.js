749cc4515a8619b0ae68ef088fec80a7cb283c66
Marcel Brode <m.brode@shopware.com>
2019-03-04 09:43:32 +0100

    NEXT-1825 - Fix Customer Address & Order Page

--- src/Administration/Resources/e2e/repos/administration/specs/customer/customer-manage-address.spec.js
+++ src/Administration/Resources/e2e/repos/administration/specs/customer/customer-manage-address.spec.js
@@ -20,8 +20,11 @@ module.exports = {
             .expect.element(page.elements.smartBarAmount).to.have.text.that.equals('(1)');
 
         browser
-            .clickContextMenuItem('.sw-customer-list__view-action', page.elements.contextMenuButton, `${page.elements.gridRow}--0`)
-            .expect.element(`${page.elements.customerMetaData}-customer-name`).to.have.text.that.equals('Mr Pep Eroni');
+            .clickContextMenuItem(
+                '.sw-customer-list__view-action',
+                page.elements.contextMenuButton,
+                `${page.elements.gridRow}--0`
+            ).expect.element(`${page.elements.customerMetaData}-customer-name`).to.have.text.that.equals('Mr Pep Eroni');
     },
     'open address tab with first address': (browser) => {
         const page = customerPage(browser);
@@ -77,15 +80,26 @@ module.exports = {
         const page = customerPage(browser);
 
         browser
+            .click('.sw-customer-detail__open-edit-mode-action')
+            .waitForElementVisible('.sw-customer-detail__save-action');
+
+        browser
             .click(`${page.elements.gridRow}--0 #defaultShippingAddress-0`)
-            .clickContextMenuItem(`${page.elements.contextMenu}-item--danger`, page.elements.contextMenuButton, `${page.elements.gridRow}--1`)
-            .expect.element('.sw-customer-detail-addresses__confirm-delete-text').to.have.text.that.equals('Are you sure you want to delete this address?');
+            .clickContextMenuItem(
+                `${page.elements.contextMenu}-item--danger`,
+                page.elements.contextMenuButton,
+                `${page.elements.gridRow}--1`
+            ).expect.element('.sw-customer-detail-addresses__confirm-delete-text').to.have.text.that.equals('Are you sure you want to delete this address?');
 
         browser
             .assert.containsText('.sw-address__full-name', 'Mr Harry Potter')
             .click(`${page.elements.modal}__footer button${page.elements.primaryButton}`)
             .waitForElementNotPresent(page.elements.modal)
             .waitForElementNotPresent(`${page.elements.gridRow}--1`);
+
+        browser
+            .click('.sw-customer-detail__save-action')
+            .checkNotification('Customer "Mr Pep Eroni" has been saved successfully.');
     },
     after: (browser) => {
         browser.end();

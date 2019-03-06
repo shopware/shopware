2743637de57575957e63aee6dfab50b5ab448fce
Jonas Elfering <jonas.elfering@web.de>
2019-03-06 14:59:31 +0100

    NEXT-1300 - Add delete operation to folder repository

---
+++ src/Core/Content/Media/DataAbstractionLayer/MediaFolderRepositoryDecorator.php
@@ -0,0 +1,141 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Media\DataAbstractionLayer;
+
+use Shopware\Core\Content\Media\Aggregate\MediaFolder\MediaFolderCollection;
+use Shopware\Core\Content\Media\Aggregate\MediaFolder\MediaFolderEntity;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregatorResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
+
+class MediaFolderRepositoryDecorator implements EntityRepositoryInterface
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $innerRepo;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $mediaRepository;
+
+
+    public function __construct(
+        EntityRepositoryInterface $innerRepo,
+        EntityRepositoryInterface $mediaRepository
+    ) {
+        $this->innerRepo = $innerRepo;
+        $this->mediaRepository = $mediaRepository;
+    }
+
+    public function delete(array $ids, Context $context): EntityWrittenContainerEvent
+    {
+        $criteria = new Criteria($this->getRawIds($ids));
+        $criteria->addAssociation('children');
+        $affectedFolders = $this->search($criteria, $context);
+
+        /** @var MediaFolderCollection $folders */
+        $folders = $affectedFolders->getEntities();
+        $this->deleteMediaAndSubfolders($folders, $context);
+
+        return $this->innerRepo->delete($ids, $context);
+    }
+
+    // Unchanged methods
+
+    public function aggregate(Criteria $criteria, Context $context): AggregatorResult
+    {
+        return $this->innerRepo->aggregate($criteria, $context);
+    }
+
+    public function searchIds(Criteria $criteria, Context $context): IdSearchResult
+    {
+        return $this->innerRepo->searchIds($criteria, $context);
+    }
+
+    public function clone(string $id, Context $context, ?string $newId = null): EntityWrittenContainerEvent
+    {
+        return $this->innerRepo->clone($id, $context, $newId);
+    }
+
+    public function search(Criteria $criteria, Context $context): EntitySearchResult
+    {
+        return $this->innerRepo->search($criteria, $context);
+    }
+
+    public function update(array $data, Context $context): EntityWrittenContainerEvent
+    {
+        return $this->innerRepo->update($data, $context);
+    }
+
+    public function upsert(array $data, Context $context): EntityWrittenContainerEvent
+    {
+        return $this->innerRepo->upsert($data, $context);
+    }
+
+    public function create(array $data, Context $context): EntityWrittenContainerEvent
+    {
+        return $this->innerRepo->create($data, $context);
+    }
+
+    public function createVersion(string $id, Context $context, ?string $name = null, ?string $versionId = null): string
+    {
+        return $this->innerRepo->createVersion($id, $context, $name, $versionId);
+    }
+
+    public function merge(string $versionId, Context $context): void
+    {
+        $this->innerRepo->merge($versionId, $context);
+    }
+
+    private function getRawIds(array $ids)
+    {
+        return array_map(
+            function ($idArray) {
+                return $idArray['id'];
+            },
+            $ids
+        );
+    }
+
+    private function deleteMediaAndSubfolders(MediaFolderCollection $folders, Context $context): void
+    {
+        /** @var MediaFolderEntity $folder */
+        foreach ($folders as $folder) {
+            $criteria = new Criteria();
+            $criteria->addFilter(new EqualsFilter('mediaFolderId', $folder->getId()));
+
+            $mediaResult = $this->mediaRepository->searchIds($criteria, $context);
+
+            if ($mediaResult->getTotal() > 0) {
+                $affectedMediaIds = array_map(function (string $id) {
+                    return ['id' => $id];
+                }, $mediaResult->getIds());
+
+                $this->mediaRepository->delete($affectedMediaIds, $context);
+            }
+
+
+            if ($folder->getChildren() === null) {
+                $this->loadChildFolders($folder, $context);
+            }
+
+            $this->deleteMediaAndSubfolders($folder->getChildren(), $context);
+        }
+    }
+
+    private function loadChildFolders(MediaFolderEntity $folder, Context $context): void
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('parentId', $folder->getId()));
+        /** @var MediaFolderCollection $childFolders */
+        $childFolders = $this->search($criteria, $context)->getEntities();
+        $folder->setChildren($childFolders);
+    }
+}

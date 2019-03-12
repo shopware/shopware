34c8586f772b48cea2353824864ca8c325fd7eec
Jens Küper <J.Kueper@shopware.com>
2019-03-12 08:52:31 +0100

    Restrict sorting direction

---
+++ src/Core/Framework/Test/DataAbstractionLayer/Dbal/CriteriaQueryHelperTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\DataAbstractionLayer\Dbal;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Exception\InvalidSortingDirectionException;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+
+class CriteriaQueryHelperTest extends TestCase
+{
+    use IntegrationTestBehaviour;
+
+    public function testInvalidSortingDirection()
+    {
+        $context = Context::createDefaultContext();
+        /** @var EntityRepositoryInterface $taxRepository */
+        $taxRepository = $this->getContainer()->get('tax.repository');
+
+        $criteria = new Criteria();
+
+        $criteria->addSorting(new FieldSorting('rate', 'invalid direction'));
+
+        static::expectException(InvalidSortingDirectionException::class);
+        $taxRepository->search($criteria, $context);
+    }
+}

45855efb83c01a21e9510db0338d190a579c30d0
Ronny Senf <rsenf@conexco.com>
2019-03-13 15:42:29 +0100

    NEXT-1924 - Added basic description, delivery information, price, gallery-slider

---
+++ src/Storefront/Resources/src/page/product-detail/product-detail.page.js
@@ -0,0 +1,8 @@
+import OffcanvasTabs from './offcanvas-tabs.page';
+import GallerySlider from './gallery-slider.page';
+
+// Product Detail - Offcanvas Tabs
+new OffcanvasTabs();
+
+// Product Detail - Gallery Slider
+new GallerySlider();

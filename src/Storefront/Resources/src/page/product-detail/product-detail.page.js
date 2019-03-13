332db3a4dd975090dd2a7c0ad51de9c0f2c651ea
barbieswimcrew <ms@dasistweb.de>
2019-03-13 17:27:37 +0100

    reorganize files

--- src/Storefront/Resources/src/page/product-detail/product-detail.page.js
+++
@@ -1,8 +0,0 @@
-import OffcanvasTabs from './offcanvas-tabs.page';
-import GallerySlider from './gallery-slider.page';
-
-// Product Detail - Offcanvas Tabs
-new OffcanvasTabs();
-
-// Product Detail - Gallery Slider
-new GallerySlider();

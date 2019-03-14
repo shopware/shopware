99e72c5715e025d277434c65d271dd2a5fbaf3af
bott <bott@conexco.com>
2019-03-14 10:39:04 +0100

    NEXT-1951 - added agb checkbox to confirm page

--- src/Storefront/Resources/asset/script/page/checkout/confirm.page.js
+++ src/Storefront/Resources/asset/script/page/checkout/confirm.page.js
@@ -1,12 +1,29 @@
 import Client from "../../service/http-client.service";
 import DomAccess from "../../helper/dom-access.helper";
+import DeviceDetection from "../../helper/device-detection.helper";
 import ButtonLoadingIndicator from "../../plugin/loading-indicator/button-loading-indicator.plugin";
 
 const client = new Client(window.accessKey, window.contextToken);
 
+// TODO: NEXT-2335: refactor and use device detection
+
 let confirmForm = document.getElementById('confirmForm');
 
-if (confirmForm){
+if (confirmForm) {
+    let confirmFormSubmit = document.getElementById('confirmFormSubmit');
+
+    // register click event on submit button (outside of form) as fallback for unsupported button form attribute
+    if (confirmFormSubmit && DeviceDetection.isNativeWindowsBrowser()) {
+        confirmFormSubmit.addEventListener('click', function(e) {
+            const button = e.srcElement;
+            const formId = DomAccess.getAttribute(button, 'form');
+            const form = document.getElementById(formId);
+
+            console.log(form);
+            // form.submit();
+        });
+    }
+
     confirmForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
@@ -15,7 +32,7 @@ if (confirmForm){
         const finishUrl = DomAccess.getAttribute(form, 'data-finish-url');
 
         let loader = new ButtonLoadingIndicator(
-            form.querySelector('button[type="submit"]')
+            document.querySelector('button[form="' + DomAccess.getAttribute(form, 'id') + '"]')
         );
         loader.create();
 

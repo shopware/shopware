434d5ea4fe19c97dc19d69e2455fdc96be76346f
Jonas Elfering <jonas.elfering@web.de>
2019-03-22 16:28:04 +0100

    NTR - Return error response when Context resolving fails

--- src/Core/Framework/Api/EventListener/ResponseExceptionListener.php
+++ src/Core/Framework/Api/EventListener/ResponseExceptionListener.php
@@ -2,13 +2,9 @@
 
 namespace Shopware\Core\Framework\Api\EventListener;
 
-use League\OAuth2\Server\Exception\OAuthServerException;
 use Shopware\Core\Framework\ShopwareHttpException;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ResponseExceptionListener extends ExceptionListener
@@ -27,32 +23,7 @@ class ResponseExceptionListener extends ExceptionListener
     {
         $exception = $event->getException();
 
-        if ($exception instanceof ShopwareHttpException) {
-            $errors = iterator_to_array($exception->getErrors($this->debug));
-
-            $response = new JsonResponse(
-                ['errors' => $this->convert($errors)],
-                $exception->getStatusCode()
-            );
-        } elseif ($exception instanceof OAuthServerException) {
-            $error = [
-                'code' => (string) $exception->getCode(),
-                'status' => (string) $exception->getHttpStatusCode(),
-                'title' => $exception->getMessage(),
-                'detail' => $exception->getHint(),
-            ];
-
-            $response = new JsonResponse(['errors' => [$error]], $exception->getHttpStatusCode(), $exception->getHttpHeaders());
-        } else {
-            $statusCode = 500;
-            if ($exception instanceof HttpException) {
-                $statusCode = $exception->getStatusCode();
-            }
-
-            $response = new JsonResponse(['errors' => $this->convertExceptionToError($exception)], $statusCode);
-        }
-
-        $event->setResponse($response);
+        $event->setResponse((new ErrorResponseFactory())->getResponseFromException($exception, $this->debug));
 
         return $event;
     }
@@ -68,45 +39,5 @@ class ResponseExceptionListener extends ExceptionListener
         } else {
             $this->logger->error($message, ['exception' => $exception]);
         }
-    }
-
-    private function convertExceptionToError(\Throwable $exception): array
-    {
-        $statusCode = 500;
-        if ($exception instanceof HttpException) {
-            $statusCode = $exception->getStatusCode();
-        }
-
-        $error = [
-            'code' => (string) $exception->getCode(),
-            'status' => (string) $statusCode,
-            'title' => Response::$statusTexts[$statusCode] ?? 'unknown status',
-            'detail' => $exception->getMessage(),
-        ];
-
-        if ($this->debug) {
-            $error['trace'] = $this->convert($exception->getTrace());
-        }
-
-        return [$error];
-    }
-
-    private function convert(array $array): array
-    {
-        foreach ($array as $key => $value) {
-            if (\is_array($value)) {
-                $array[$key] = $this->convert($value);
-            }
-
-            if (\is_string($value)) {
-                if (!ctype_print($value) && \strlen($value) === 16) {
-                    $array[$key] = sprintf('ATTENTION: Converted binary string by the "%s": %s', self::class, bin2hex($value));
-                } elseif (!mb_detect_encoding($value, mb_detect_order(), true)) {
-                    $array[$key] = utf8_encode($value);
-                }
-            }
-        }
-
-        return $array;
     }
 }

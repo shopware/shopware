29f33672f44f8270f2fd11ea41fd693a24ab8fbe
barbieswimcrew <ms@dasistweb.de>
2019-03-26 18:39:05 +0100

    NEXT-2063 - Cart Widget

--- src/Storefront/Resources/asset/script/plugin/actions/cart-widget.plugin.js
+++ src/Storefront/Resources/asset/script/plugin/actions/cart-widget.plugin.js
@@ -1,15 +1,17 @@
 import HttpClient from "../../service/http-client.service";
 import DomAccess from "../../helper/dom-access.helper";
+import Plugin from '../../helper/plugin.helper';
 
 const CART_WIDGET_ITEM_SELECTOR = '*[data-cart-widget=true]';
 const CART_WIDGET_STORAGE_KEY = 'cart-widget-template';
 
-export default class CartWidget {
+export default class CartWidget extends Plugin {
 
     /**
      * Constructor.
      */
     constructor() {
+        super();
         CartWidget.fetch();
     }
 

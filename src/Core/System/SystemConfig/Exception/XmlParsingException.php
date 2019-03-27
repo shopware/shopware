a8a845708f5f15dc3f9f17ad07c9ed02714c63c9
Jan Pietrzyk <jp@shopware.com>
2019-03-27 22:58:53 +0100

    NTR - move Xml-Exceptions

--- src/Core/System/SystemConfig/Exception/XmlParsingException.php
+++ src/Core/System/SystemConfig/Exception/XmlParsingException.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Core\Framework\Exception;
+namespace Shopware\Core\System\SystemConfig\Exception;
 
 use Shopware\Core\Framework\ShopwareHttpException;
 use Symfony\Component\HttpFoundation\Response;

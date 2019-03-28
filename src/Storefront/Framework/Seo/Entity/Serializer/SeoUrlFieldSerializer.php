6d4ff18596127768f3a1d02bc92fc7d0197d6993
Patrick Weyck <p.weyck@shopware.com>
2019-03-28 07:58:45 +0100

    NEXT-2602 - Remove pathInfo default generation

--- src/Storefront/Framework/Seo/Entity/Serializer/SeoUrlFieldSerializer.php
+++ src/Storefront/Framework/Seo/Entity/Serializer/SeoUrlFieldSerializer.php
@@ -7,26 +7,11 @@ use Shopware\Core\Framework\DataAbstractionLayer\Field\Field;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldSerializer\OneToManyAssociationFieldSerializer;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\DataStack\KeyValuePair;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\EntityExistence;
-use Shopware\Core\Framework\DataAbstractionLayer\Write\WriteCommandExtractor;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\WriteParameterBag;
-use Shopware\Core\Framework\Struct\Uuid;
 use Shopware\Storefront\Framework\Seo\Entity\Field\SeoUrlAssociationField;
-use Shopware\Storefront\Framework\Seo\SeoServiceInterface;
-use Shopware\Storefront\Framework\Seo\SeoUrl\SeoUrlEntity;
 
 class SeoUrlFieldSerializer extends OneToManyAssociationFieldSerializer
 {
-    /**
-     * @var SeoServiceInterface
-     */
-    private $seoService;
-
-    public function __construct(WriteCommandExtractor $writeExtractor, SeoServiceInterface $seoService)
-    {
-        parent::__construct($writeExtractor);
-        $this->seoService = $seoService;
-    }
-
     public function getFieldClass(): string
     {
         return SeoUrlAssociationField::class;
@@ -38,21 +23,10 @@ class SeoUrlFieldSerializer extends OneToManyAssociationFieldSerializer
             throw new InvalidSerializerFieldException(SeoUrlAssociationField::class, $field);
         }
 
-        $entityId = Uuid::fromBytesToHex($existence->getPrimaryKey()[$field->getLocalField()]);
-
         $seoUrls = $data->getValue();
         foreach ($seoUrls as $i => $seoUrl) {
             $seoUrl['routeName'] = $field->getRouteName();
             $seoUrl['isModified'] = true;
-
-            if (!isset($seoUrl['pathInfo']) && $existence->exists()) {
-                /** @var SeoUrlEntity|null $generated */
-                $generated = @current($this->seoService->generateSeoUrls($seoUrl['salesChannelId'], $field->getRouteName(), [$entityId]));
-
-                if ($generated) {
-                    $seoUrl['pathInfo'] = $generated->getPathInfo();
-                }
-            }
 
             $seoUrls[$i] = $seoUrl;
         }

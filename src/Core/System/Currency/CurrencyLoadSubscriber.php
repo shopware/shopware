e4fd3295c739d4227b62ca395308508b5c81fd7b
Sebastian Hölscher <s.hoelscher@shopware.com>
2019-04-01 10:48:24 +0200

    NTR - Currency default

---
+++ src/Core/System/Currency/CurrencyLoadSubscriber.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\System\Currency;
+
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CurrencyLoadSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [CurrencyEvents::CURRENCY_LOADED_EVENT => 'setDefault'];
+    }
+
+    public function setDefault(EntityLoadedEvent $event): void
+    {
+        /** @var CurrencyEntity $entity */
+        foreach ($event->getEntities() as $entity) {
+            $entity->setIsDefault($entity->getId() === Defaults::CURRENCY);
+        }
+    }
+}

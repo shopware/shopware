729e8f80dad64e5c1b8099c625984a51b23b6162
Uli Ehler <uehler@conexco.com>
2019-04-03 12:04:01 +0200

    NEXT-2603 - storefront submit order

---
+++ src/Core/Checkout/Order/Validation/OrderValidationService.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Order\Validation;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Validation\DataValidationDefinition;
+use Shopware\Core\Framework\Validation\ValidationServiceInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class OrderValidationService implements ValidationServiceInterface
+{
+    public function buildCreateValidation(Context $context): DataValidationDefinition
+    {
+        $definition = new DataValidationDefinition('order.create');
+
+        $definition->add('sAGB', new NotBlank());
+
+        return $definition;
+    }
+
+    public function buildUpdateValidation(Context $context): DataValidationDefinition
+    {
+        return $this->buildCreateValidation($context);
+    }
+}
--- src/Storefront/Resources/asset/script/page/checkout/confirm/confirm-order.page.js
+++ src/Storefront/Resources/asset/script/page/checkout/confirm/confirm-order.page.js
@@ -1,9 +1,7 @@
 import DomAccess from "../../../helper/dom-access.helper";
 import ButtonLoadingIndicator from "../../../util/loading-indicator/button-loading-indicator.util";
-import HttpClient from "../../../service/http-client.service";
 
 const CONFIRM_ORDER_FORM_ID = 'confirmOrderForm';
-const CONFIRM_ORDER_FINISH_ROUTE = window.router['frontend.checkout.finish.page'];
 
 export default class ConfirmOrder {
 
@@ -32,42 +30,14 @@ export default class ConfirmOrder {
 
     /**
      * Handle form submit event manually by preventing the usual form submission first.
-     * Show loading indicator while placing the order via POST request against the storefront-api
-     * and if successful redirect the user to the finish page.
-     * @param {Event} e
+     * Show loading indicator after submitting the order
      * @private
      */
-    _onFormSubmit(e) {
-        e.preventDefault();
-
-        const client = new HttpClient(window.accessKey, window.contextToken);
-        const url = DomAccess.getAttribute(this._form, 'action');
+    _onFormSubmit() {
         const submitButton = DomAccess.querySelector(this._form, `button[type=submit]`);
 
         // show loading indicator in submit button
         let loader = new ButtonLoadingIndicator(submitButton);
         loader.create();
-
-        client.post(url.toLowerCase(), '{}', (response) => {
-            let obj = JSON.parse(response);
-
-            if (obj.data.id) {
-                window.location.replace(this._createRedirectUrl(obj.data.id));
-            }
-
-            // remove the loading indicator at the end for correct visualization
-            loader.remove();
-        });
-
-    }
-
-    /**
-     * Build the route to redirect when order has been placed successfully
-     * @param {string} orderId
-     * @returns {string}
-     * @private
-     */
-    _createRedirectUrl(orderId) {
-        return `${CONFIRM_ORDER_FINISH_ROUTE}?orderId=${orderId}`;
     }
 }

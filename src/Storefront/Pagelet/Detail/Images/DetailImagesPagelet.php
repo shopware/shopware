9f15e67ccb15410751bf61fad81e8806f592deee
shvlmnns <sh@dasistweb.de>
2019-04-04 14:23:49 +0200

    NEXT-2066 - add detail image zoom
    Adding needed adjustments after review.

---
+++ src/Storefront/Pagelet/Detail/Images/DetailImagesPagelet.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storefront\Pagelet\Detail\Images;
+
+use Shopware\Core\Content\Product\Aggregate\ProductMedia\ProductMediaCollection;
+use Shopware\Core\Framework\Struct\Struct;
+
+class DetailImagesPagelet extends Struct
+{
+    /**
+     * @var ProductMediaCollection
+     */
+    private $productMedia;
+
+    public function getProductMedia(): ProductMediaCollection
+    {
+        return $this->productMedia;
+    }
+
+    public function setProductMedia(ProductMediaCollection $productMedia): void
+    {
+        $this->productMedia = $productMedia;
+    }
+}

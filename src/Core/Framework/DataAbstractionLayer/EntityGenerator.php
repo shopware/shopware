913220ecbfea0eecd23d0e56b7fa21c986c58475
Oliver Skroblin <o.skroblin@shopware.com>
2019-04-08 08:40:05 +0200

    Implement schema and struct generator

---
+++ src/Core/Framework/DataAbstractionLayer/EntityGenerator.php
@@ -0,0 +1,335 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer;
+
+use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
+use Shopware\Core\Checkout\Cart\Price\Struct\CartPrice;
+use Shopware\Core\Checkout\Cart\Price\Struct\QuantityPriceDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\EntityDefinitionQueryHelper;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\BlobField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\BoolField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\CalculatedPriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\CartPriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ChildrenAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\DateField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Field;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\FkField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\FloatField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IntField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\JsonField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextWithHtmlField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ParentAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PasswordField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceDefinitionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceRulesJsonField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ReferenceVersionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslatedField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\VersionField;
+use Shopware\Core\Framework\Pricing\Price;
+use Shopware\Core\Framework\Pricing\PriceRuleCollection;
+
+class EntityGenerator
+{
+    private $classTemplate = <<<EOF
+<?php declare(strict_types=1);
+
+namespace #domain#;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+#uses#
+
+class #entity#Entity extends Entity
+{
+    use EntityIdTrait;
+    
+    #properties#
+    #functions#
+}
+EOF;
+
+    private $propertyTemplate = <<<EOF
+    /**
+     * @var #type##nullable#
+     */
+    protected $#property#;
+EOF;
+
+    private $propertyFunctions = <<<EOF
+    public function get#propertyUc#(): #nullable##type#
+    {
+        return \$this->#propertyLc#;
+    }
+
+    public function set#propertyUc#(#nullable##type# $#propertyLc#): void
+    {
+        \$this->#propertyLc# = $#propertyLc#;
+    }
+EOF;
+
+    private $collectionTemplate = <<<EOF
+<?php declare(strict_types=1);
+
+namespace #domain#;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+
+/**
+ * @method void                add(#entityClass# \$entity)
+ * @method void                set(string \$key, #entityClass# \$entity)
+ * @method #entityClass#[]    getIterator()
+ * @method #entityClass#[]    getElements()
+ * @method #entityClass#|null get(string \$key)
+ * @method #entityClass#|null first()
+ * @method #entityClass#|null last()
+ */
+class #entity#Collection extends EntityCollection
+{
+    protected function getExpectedClass(): string
+    {
+        return #entityClass#::class;
+    }
+}
+EOF;
+
+    public function generate(string $definition): ?array
+    {
+        $instance = new $definition();
+        if ($instance instanceof MappingEntityDefinition) {
+            return null;
+        }
+        $entity = $definition::getEntityName();
+        $entity = explode('_', $entity);
+        $entity = array_map('ucfirst', $entity);
+        $entity = implode('', $entity);
+
+        $struct = $this->generateEntity($definition);
+
+        $collection = $this->generateCollection($definition);
+
+        return [
+            $entity . 'Entity.php' => $struct,
+            $entity . 'Collection.php' => $collection,
+        ];
+    }
+
+    private function generateEntity(string $definition)
+    {
+        $properties = [];
+
+        $uses = [];
+
+        /** @var string|EntityDefinition $definition */
+        foreach ($definition::getFields() as $field) {
+            $property = $this->generateProperty($definition, $field);
+            if (!$property) {
+                continue;
+            }
+            foreach ($property['uses'] as $use) {
+                $uses[] = $use;
+            }
+            $properties[] = $property;
+        }
+
+        $functions = array_column($properties, 'functions');
+        $properties = array_column($properties, 'property');
+
+        $domain = explode('\\', $definition);
+        $domain = array_slice($domain, 0, count($domain) - 1);
+        $domain = implode('\\', $domain);
+
+        $entity = $definition::getEntityName();
+        $entity = explode('_', $entity);
+        $entity = array_map('ucfirst', $entity);
+        $entity = implode('', $entity);
+
+        $parameters = [
+            '#domain#' => $domain,
+            '#uses#' => implode(";\n", $uses) . ';',
+            '#entity#' => lcfirst($entity),
+            '#property#' => implode("\n", $properties),
+            '#functions#' => implode("\n", $functions),
+        ];
+
+        return str_replace(
+            array_keys($parameters),
+            array_values($parameters),
+            $this->classTemplate
+        );
+    }
+
+    private function generateProperty(string $definition, Field $field): ?array
+    {
+        $nullable = '|null';
+        if ($field->is(Required::class)) {
+            $nullable = '';
+        }
+
+        $uses = [];
+
+        /* @var string|EntityDefinition $definition */
+        switch (true) {
+            case $field instanceof ParentAssociationField:
+                $uses[] = $this->getUsage($definition::getEntityClass());
+                $type = $this->getClassTypeHint($definition::getEntityClass());
+                break;
+            case $field instanceof ChildrenAssociationField:
+                $uses[] = $this->getUsage($definition::getCollectionClass());
+                $type = $this->getClassTypeHint($definition::getCollectionClass());
+                break;
+            case $field instanceof ManyToOneAssociationField:
+                $uses[] = $this->getUsage($field->getReferenceClass()::getEntityClass());
+                $type = $this->getClassTypeHint($field->getReferenceClass()::getEntityClass());
+                break;
+            case $field instanceof OneToManyAssociationField:
+                $uses[] = $this->getUsage($field->getReferenceClass()::getCollectionClass());
+                $type = $this->getClassTypeHint($field->getReferenceClass()::getCollectionClass());
+                break;
+            case $field instanceof ManyToManyAssociationField:
+                $uses[] = $this->getUsage($field->getReferenceDefinition()::getCollectionClass());
+                $type = $this->getClassTypeHint($field->getReferenceDefinition()::getCollectionClass());
+                break;
+            case $field instanceof OneToOneAssociationField:
+                $uses[] = $this->getUsage($field->getReferenceClass()::getEntityClass());
+                $type = $this->getClassTypeHint($field->getReferenceClass()::getEntityClass());
+                break;
+            case $field instanceof VersionField:
+                return null;
+            case $field instanceof TranslatedField:
+                return $this->generateProperty(
+                    $definition,
+                    EntityDefinitionQueryHelper::getTranslatedField($definition, $field)
+                );
+            case $field instanceof CartPriceField:
+                $type = 'CartPrice';
+                $uses[] = 'use ' . CartPrice::class;
+                break;
+            case $field instanceof CalculatedPriceField:
+                $type = 'CalculatedPrice';
+                $uses[] = 'use ' . CalculatedPrice::class;
+                break;
+            case $field instanceof ReferenceVersionField:
+                return null;
+            case $field instanceof PriceDefinitionField:
+                $type = 'QuantityPriceDefinition';
+                $uses[] = 'use ' . QuantityPriceDefinition::class;
+                break;
+            case $field instanceof PriceField:
+                $type = 'Price';
+                $uses[] = 'use ' . Price::class;
+                break;
+            case $field instanceof PriceRulesJsonField:
+                $type = 'PriceRuleCollection';
+                $uses[] = 'use ' . PriceRuleCollection::class;
+                break;
+            case $field instanceof LongTextField:
+            case $field instanceof LongTextWithHtmlField:
+                $type = 'string';
+                break;
+            case $field instanceof PasswordField:
+                $type = 'string';
+                break;
+            case $field instanceof FloatField:
+                $type = 'float';
+                break;
+            case $field instanceof IdField:
+                $type = 'string';
+                break;
+            case $field instanceof IntField:
+                $type = 'int';
+                break;
+            case $field instanceof JsonField:
+                $type = 'array';
+                break;
+            case $field instanceof FkField:
+                $type = 'string';
+                break;
+            case $field instanceof StringField:
+                $type = 'string';
+                break;
+            case $field instanceof BoolField:
+                $type = 'boolean';
+                break;
+            case $field instanceof DateField:
+                $type = "\DateTime";
+                break;
+            case $field instanceof BlobField:
+                $type = 'object';
+                break;
+            default:
+                throw new \RuntimeException(sprintf('Unknown field %s', get_class($field)));
+        }
+
+        $template = str_replace(
+            ['#property#', '#type#', '#nullable#'],
+            [$field->getPropertyName(), $type, $nullable],
+            $this->propertyTemplate
+        );
+
+        $nullable = '?';
+        if ($field->is(Required::class)) {
+            $nullable = '';
+        }
+
+        $functions = str_replace(
+            ['#propertyUc#', '#propertyLc#', '#nullable#', '#type#'],
+            [ucfirst($field->getPropertyName()), lcfirst($field->getPropertyName()), $nullable, $type],
+            $this->propertyFunctions
+        );
+
+        return [
+            'property' => trim($template),
+            'uses' => $uses,
+            'functions' => $functions,
+        ];
+    }
+
+    private function generateCollection(string $definition)
+    {
+        $entityClass = $definition::getEntityClass();
+        $entityClass = explode('\\', $entityClass);
+        $entityClass = array_pop($entityClass);
+
+        $entity = $definition::getEntityName();
+        $entity = explode('_', $entity);
+        $entity = array_map('ucfirst', $entity);
+        $entity = implode('', $entity);
+
+        $domain = explode('\\', $definition);
+        $domain = array_slice($domain, 0, count($domain) - 1);
+        $domain = implode('\\', $domain);
+
+        $parameters = [
+            '#domain#' => $domain,
+            '#entityClass#' => $entityClass,
+            '#entity#' => $entity,
+        ];
+
+        return str_replace(
+            array_keys($parameters),
+            array_values($parameters),
+            $this->collectionTemplate
+        );
+    }
+
+    private function getUsage(string $class)
+    {
+        return 'use ' . $class;
+    }
+
+    private function getClassTypeHint(string $class)
+    {
+        $parts = explode('\\', $class);
+
+        return array_pop($parts);
+    }
+}
---
+++ src/Core/Framework/DataAbstractionLayer/SchemaGenerator.php
@@ -0,0 +1,323 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\EntityDefinitionQueryHelper;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\AssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\BlacklistRuleField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\BlobField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\BoolField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\CalculatedPriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\CartPriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ChildCountField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ChildrenAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\CreatedAtField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\DateField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Field;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\FkField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\CascadeDelete;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Deferred;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\FloatField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IntField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\JsonField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ListField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextWithHtmlField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ParentAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ParentFkField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PasswordField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceDefinitionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\PriceRulesJsonField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ReferenceVersionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StorageAware;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslatedField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TreeLevelField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TreePathField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\UpdatedAtField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\VersionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\WhitelistRuleField;
+use Shopware\Storefront\Framework\Seo\Entity\Field\CanonicalUrlAssociationField;
+
+class SchemaGenerator
+{
+    private $tableTemplate = <<<EOL
+CREATE TABLE `#name#` (
+    #columns#,
+    #keys#
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+EOL;
+
+    private $columnTemplate = <<<EOL
+    `#name#` #type# #nullable# #default#
+EOL;
+
+    public function generate(string $definition)
+    {
+        $table = $definition::getEntityName();
+
+        $columns = [];
+
+        foreach ($definition::getFields() as $field) {
+            $columns[] = $this->generateFieldColumn($definition, $field);
+        }
+        $columns = array_filter($columns);
+
+        $primaryKey = $this->generatePrimaryKey($definition);
+
+        $foreignKeys = $this->generateForeignKeys($definition);
+
+        $jsonValidations = $this->generateJsonChecks($definition);
+
+        $keys = array_filter([$primaryKey, $jsonValidations, $foreignKeys]);
+        $keys = implode(",\n", $keys);
+
+        $template = str_replace(
+            ['#name#', '#columns#', '#keys#'],
+            [$table, implode(",\n    ", $columns), $keys],
+            $this->tableTemplate
+        );
+
+        return $template;
+    }
+
+    private function generateFieldColumn(string $definition, Field $field): ?string
+    {
+        if ($field->is(Deferred::class)) {
+            return null;
+        }
+        if ($field instanceof AssociationField) {
+            return null;
+        }
+        if (!$field instanceof StorageAware) {
+            return null;
+        }
+
+        $default = '';
+
+        $nullable = 'NULL';
+        if ($field->is(Required::class)) {
+            $nullable = 'NOT NULL';
+        }
+
+        switch (true) {
+            case $field instanceof VersionField:
+            case $field instanceof ReferenceVersionField:
+            case $field instanceof ParentFkField:
+            case $field instanceof IdField:
+            case $field instanceof FkField:
+                $type = 'BINARY(16)';
+                break;
+
+            case $field instanceof UpdatedAtField:
+            case $field instanceof CreatedAtField:
+            case $field instanceof DateField:
+                $type = 'DATETIME(3)';
+                break;
+
+            case $field instanceof TranslatedField:
+                return null;
+
+            case $field instanceof WhitelistRuleField:
+            case $field instanceof BlacklistRuleField:
+            case $field instanceof CartPriceField:
+            case $field instanceof CalculatedPriceField:
+            case $field instanceof PriceDefinitionField:
+            case $field instanceof PriceField:
+            case $field instanceof PriceRulesJsonField:
+            case $field instanceof ListField:
+            case $field instanceof JsonField:
+                $type = 'JSON';
+                break;
+
+            case $field instanceof ChildCountField:
+            case $field instanceof IntField:
+                $type = 'INT(11)';
+                break;
+
+            case $field instanceof TreePathField:
+            case $field instanceof LongTextField:
+            case $field instanceof LongTextWithHtmlField:
+                $type = 'LONGTEXT';
+                break;
+
+            case $field instanceof TreeLevelField:
+                $type = 'INT';
+                break;
+
+            case $field instanceof PasswordField:
+                $type = 'VARCHAR(1024)';
+                break;
+
+            case $field instanceof FloatField:
+                $type = 'DOUBLE';
+                break;
+
+            case $field instanceof StringField:
+                $type = 'VARCHAR(' . $field->getMaxLength() . ')';
+                break;
+
+            case $field instanceof BoolField:
+                $type = 'TINYINT(1)';
+                $default = "DEFAULT '0'";
+                break;
+
+            case $field instanceof BlobField:
+                $type = 'LONGBLOB';
+                break;
+            default:
+                throw new \RuntimeException(sprintf('Unknown field %s', get_class($field)));
+        }
+
+        $template = str_replace(
+            ['#name#', '#type#', '#nullable#', '#default#'],
+            [$field->getStorageName(), $type, $nullable, $default],
+            $this->columnTemplate
+        );
+
+        return trim($template);
+    }
+
+    private function generatePrimaryKey($definition): string
+    {
+        $keys = [];
+
+        /** @var string|EntityDefinition $definition */
+        foreach ($definition::getPrimaryKeys() as $primaryKey) {
+            /* @var StorageAware $primaryKey */
+            $keys[] = sprintf('`%s`', $primaryKey->getStorageName());
+        }
+
+        if (empty($keys)) {
+            throw new \RuntimeException(sprintf('No primary key detected for entity: %s', $definition::getEntityName()));
+        }
+
+        return 'PRIMARY KEY (' . implode(',', $keys) . ')';
+    }
+
+    private function generateForeignKeys(string $definition): string
+    {
+        $fields = $definition::getFields()->filter(
+            function (Field $field) {
+                if (!$field instanceof ManyToOneAssociationField) {
+                    return false;
+                }
+
+                return !$field instanceof CanonicalUrlAssociationField;
+            }
+        );
+
+        $referenceVersionFields = $definition::getFields()->filterInstance(ReferenceVersionField::class);
+
+        $indices = [];
+        $constraints = [];
+
+        /** @var ManyToOneAssociationField $field */
+        foreach ($fields as $field) {
+            $reference = $field->getReferenceClass();
+
+            $hasOneToMany = $definition::getFields()->filter(function (Field $field) use ($reference) {
+                if (!$field instanceof OneToManyAssociationField) {
+                    return false;
+                }
+                if ($field instanceof ChildrenAssociationField) {
+                    return false;
+                }
+
+                return $field->getReferenceClass() === $reference;
+            })->count() > 0;
+
+            $columns = [
+                EntityDefinitionQueryHelper::escape($field->getStorageName()),
+            ];
+
+            $referenceColumns = [
+                EntityDefinitionQueryHelper::escape($field->getReferenceField()),
+            ];
+
+            if ($reference::isVersionAware()) {
+                $versionField = null;
+
+                /** @var ReferenceVersionField $referenceVersionField */
+                foreach ($referenceVersionFields as $referenceVersionField) {
+                    if ($referenceVersionField->getVersionReference() === $reference) {
+                        $versionField = $referenceVersionField;
+                        break;
+                    }
+                }
+
+                if ($field instanceof ParentAssociationField) {
+                    $columns[] = '`version_id`';
+                } else {
+                    $columns[] = EntityDefinitionQueryHelper::escape($versionField->getStorageName());
+                }
+
+                $referenceColumns[] = '`version_id`';
+            }
+
+            $update = 'CASCADE';
+
+            $parameters = [
+                '#entity#' => $definition::getEntityName(),
+                '#column#' => $field->getStorageName(),
+                '#columns#' => implode(',', $columns),
+            ];
+            $indices[] = str_replace(
+                array_keys($parameters),
+                array_values($parameters),
+                '    KEY `fk.#entity#.#column#` (#columns#)'
+            );
+
+            if ($field->is(CascadeDelete::class)) {
+                $delete = 'CASCADE';
+            } else {
+                $delete = 'RESTRICT';
+            }
+
+            //skip foreign key to prevent bi-directional foreign key
+            if ($hasOneToMany) {
+                continue;
+            }
+
+            $parameters = [
+                '#entity#' => $definition::getEntityName(),
+                '#column#' => $field->getStorageName(),
+                '#columns#' => implode(',', $columns),
+                '#refEntity#' => $reference::getEntityName(),
+                '#refColumns#' => implode(',', $referenceColumns),
+                '#delete#' => $delete,
+                '#update#' => $update,
+            ];
+
+            $constraints[] = str_replace(
+                array_keys($parameters),
+                array_values($parameters),
+                '    CONSTRAINT `fk.#entity#.#column#` FOREIGN KEY (#columns#) REFERENCES `#refEntity#` (#refColumns#) ON DELETE #delete# ON UPDATE #update#'
+            );
+        }
+
+        $constraints = implode(",\n", array_filter($constraints));
+        $indices = implode(",\n", array_filter($indices));
+
+        return implode(",\n", array_filter([$indices, $constraints]));
+    }
+
+    private function generateJsonChecks(string $definition): string
+    {
+        $fields = $definition::getFields()->filterInstance(JsonField::class);
+
+        $template = '    CONSTRAINT `json.#column#` CHECK (JSON_VALID(`#column#`))';
+        $checks = [];
+        /** @var JsonField $field */
+        foreach ($fields as $field) {
+            $checks[] = str_replace('#column#', $field->getStorageName(), $template);
+        }
+
+        return implode(",\n", $checks);
+    }
+}

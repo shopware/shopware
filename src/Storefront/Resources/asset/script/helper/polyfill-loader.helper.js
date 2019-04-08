195d01d3c121d710b66544b9fab9601f583f9059
shvlmnns <sh@dasistweb.de>
2019-04-08 18:59:02 +0200

    NEXT-2736 - add needed polyfills for IE11

---
+++ src/Storefront/Resources/asset/script/helper/polyfill-loader.helper.js
@@ -0,0 +1,6 @@
+import 'nodelist-foreach-polyfill';
+import 'custom-event-polyfill';
+import 'form-association-polyfill/dist/form-association-polyfill-register-with-shims';
+import ElementClosestPolyfill from 'element-closest';
+
+ElementClosestPolyfill(window);

ab426ea3cb60e17a7f7794a864b631afa10ff005
Stefan Heppenheimer <stefan.heppenheimer@viison.com>
2019-04-12 10:08:04 +0200

    Add default value for available_stock to product definition

--- src/Core/Content/Product/ProductDefinition.php
+++ src/Core/Content/Product/ProductDefinition.php
@@ -89,6 +89,7 @@ class ProductDefinition extends EntityDefinition
             'minPurchase' => 1,
             'purchaseSteps' => 1,
             'shippingFree' => false,
+            'availableStock' => 0,
             'restockTime' => 1,
             'minDeliveryTime' => 1,
             'maxDeliveryTime' => 2,

09bfef6b9afaa77bd570eccd1e9f1d7efafeabfe
Anton Zoffmann <az@dasistweb.de>
2019-04-17 09:51:59 +0200

    NEXT-2488 add account controller logout action

--- src/Storefront/PageController/AccountPageController.php
+++ src/Storefront/PageController/AccountPageController.php
@@ -154,6 +154,20 @@ class AccountPageController extends StorefrontController
     }
 
     /**
+     * @Route("/account/logout", name="frontend.account.logout.page", methods={"GET"})
+     */
+    public function logout(InternalRequest $request, SalesChannelContext $context): Response
+    {
+        if ($context->getCustomer() === null) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
+
+        $this->accountService->logout($context);
+
+        return $this->redirectToRoute('frontend.account.login.page');
+    }
+
+    /**
      * @Route("/account/login", name="frontend.account.login", methods={"POST"})
      */
     public function loginCustomer(Request $request, RequestDataBag $data, SalesChannelContext $context): Response

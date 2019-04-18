b86278e91ffbc52f5b88711785cb6888b54212c4
bott <bott@conexco.com>
2019-04-18 11:19:12 +0200

    NEXT-2853 - screendesign updates

--- src/Storefront/Resources/src/script/helper/polyfill-loader.helper.js
+++ src/Storefront/Resources/src/script/helper/polyfill-loader.helper.js
@@ -1,6 +1,10 @@
-import 'nodelist-foreach-polyfill';
-import 'custom-event-polyfill';
 import 'form-association-polyfill/dist/form-association-polyfill-register-with-shims';
+import 'mdn-polyfills/Array.prototype.forEach';
+import 'mdn-polyfills/NodeList.prototype.forEach';
+import 'mdn-polyfills/CustomEvent';
+import 'src/script/helper/polyfill/object.foreach.polyfill';
+import 'picturefill';
+import 'picturefill/dist/plugins/mutation/pf.mutation';
 import ElementClosestPolyfill from 'element-closest';
 
 ElementClosestPolyfill(window);

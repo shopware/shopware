3fc501d7e25f59d0e466c49fdbdf701049bf34dc
Uli Ehler <uehler@conexco.com>
2019-04-18 15:40:54 +0200

    NEXT-2486 - redirect to login after session timeout

--- src/Storefront/PageController/AccountPageController.php
+++ src/Storefront/PageController/AccountPageController.php
@@ -134,7 +134,7 @@ class AccountPageController extends StorefrontController
     /**
      * @Route("/account/login", name="frontend.account.login.page", methods={"GET"})
      */
-    public function login(Request $request,RequestDataBag$data, SalesChannelContext $context): Response
+    public function login(Request $request, RequestDataBag $data, SalesChannelContext $context): Response
     {
         /** @var string $redirect */
         $redirect = $request->get('redirectTo', $this->generateUrl('frontend.account.home.page'));
@@ -217,12 +217,12 @@ class AccountPageController extends StorefrontController
 
     /**
      * @Route("/account/payment", name="frontend.account.payment.page", options={"seo"="false"}, methods={"GET"})
-     *
-     * @throws CustomerNotLoggedInExceptionAlias
      */
     public function paymentOverview(Request $request, SalesChannelContext $context): Response
     {
-        $this->denyAccessUnlessLoggedIn();
+        if (!$context->getCustomer()) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
 
         $page = $this->paymentMethodPageLoader->load($request, $context);
 
@@ -231,12 +231,12 @@ class AccountPageController extends StorefrontController
 
     /**
      * @Route("/account/order", name="frontend.account.order.page", options={"seo"="false"}, methods={"GET"})
-     *
-     * @throws CustomerNotLoggedInExceptionAlias
      */
     public function orderOverview(Request $request, SalesChannelContext $context): Response
     {
-        $this->denyAccessUnlessLoggedIn();
+        if (!$context->getCustomer()) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
 
         $page = $this->orderPageLoader->load($request, $context);
 
@@ -245,12 +245,12 @@ class AccountPageController extends StorefrontController
 
     /**
      * @Route("/account/profile", name="frontend.account.profile.page", methods={"GET"})
-     *
-     * @throws CustomerNotLoggedInExceptionAlias
      */
     public function profileOverview(Request $request, SalesChannelContext $context): Response
     {
-        $this->denyAccessUnlessLoggedIn();
+        if (!$context->getCustomer()) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
 
         $page = $this->profilePageLoader->load($request, $context);
 

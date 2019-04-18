a00065fb2104da48397c3b1f4be56366db9b2ebd
Ramona Schwering <R.Schwering@shopware.com>
2019-04-18 15:43:13 +0200

    NEXT-1694 - Add e2e order state tests

--- src/Administration/Resources/e2e/repos/administration/page-objects/module/sw-order.page-object.js
+++ src/Administration/Resources/e2e/repos/administration/page-objects/module/sw-order.page-object.js
@@ -11,6 +11,31 @@ class OrderPageObject extends GeneralPageObject {
             }
         };
     }
+
+    setOrderState({ stateTitle, type, signal = 'neutral', scope = 'select' }) {
+        const stateColor = `.sw-order-state__${signal}-select`;
+
+        this.browser
+            .waitForElementVisible(`.sw-order-state-${scope}__${type}-state select[name=sw-field--selectedActionName]`)
+            .click(`.sw-order-state-${scope}__${type}-state select[name=sw-field--selectedActionName]`)
+            .setValue(
+                `.sw-order-state-${scope}__${type}-state select[name=sw-field--selectedActionName]`,
+                stateTitle
+            )
+            .click(this.elements.smartBarHeader)
+            .waitForElementNotPresent('.sw-order-user-card .sw-loader__element');
+
+        scope === 'select' ? this.browser.waitForElementVisible(stateColor) : null;
+    }
+
+    checkOrderHistoryEntry({ type, stateTitle, signal = 'neutral', position = 0 }) {
+        const currentStatusIcon = `.sw-order-state__${signal}-icon-bg`;
+        const item = `.sw-order-state-history-card__${type}-state .sw-order-state-history__entry--${position}`;
+
+        this.browser
+            .waitForElementVisible(`${item} ${currentStatusIcon}`)
+            .assert.containsText(item, stateTitle);
+    }
 }
 
 module.exports = (browser) => {

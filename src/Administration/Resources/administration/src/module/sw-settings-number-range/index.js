4459e4065a6f5ab1fff47548650af121a2a943f2
Marcel Brode <m.brode@shopware.com>
2019-04-23 15:01:29 +0200

    NEXT-2816 - Add title to language, number range and payment

--- src/Administration/Resources/administration/src/module/sw-settings-number-range/index.js
+++ src/Administration/Resources/administration/src/module/sw-settings-number-range/index.js
@@ -10,7 +10,7 @@ import enGB from './snippet/en_GB.json';
 
 Module.register('sw-settings-number-range', {
     type: 'core',
-    name: 'Number Range settings',
+    name: 'sw-settings-number-range.general.mainMenuItemGeneral',
     description: 'Number Range section in the settings module',
     color: '#9AA8B5',
     icon: 'default-action-settings',

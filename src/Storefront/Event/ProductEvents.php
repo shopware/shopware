c00c3a88445cb52f72165a977cb3d63987901acc
Jens Küper <J.Kueper@shopware.com>
2019-04-24 16:36:55 +0200

    Add criteria events for product detail page

--- src/Storefront/Event/ProductEvents.php
+++ src/Storefront/Event/ProductEvents.php
@@ -2,6 +2,7 @@
 
 namespace Shopware\Storefront\Event;
 
+use Shopware\Storefront\Page\Product\ProductPageCriteriaEvent;
 use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
 
 class ProductEvents
@@ -10,4 +11,9 @@ class ProductEvents
      * @Event("Shopware\Storefront\Page\Product\ProductPageLoadedEvent")
      */
     public const PRODUCT_PAGE_LOADED_EVENT = ProductPageLoadedEvent::NAME;
+
+    /**
+     * @Event("Shopware\Storefront\Page\Product\ProductPageCriteriaEvent")
+     */
+    public const PRODUCT_PAGE_CRITERIA_EVENT = ProductPageCriteriaEvent::NAME;
 }

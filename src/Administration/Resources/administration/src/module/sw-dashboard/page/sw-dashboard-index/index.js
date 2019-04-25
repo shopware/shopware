d501a09d4232a4d45d8465e869917993af918bfd
Christian Rades <c.rades@shopware.com>
2019-04-25 16:39:00 +0200

    NEXT-2906 - Added Dashboard

--- src/Administration/Resources/administration/src/module/sw-dashboard/page/sw-dashboard-index/index.js
+++ src/Administration/Resources/administration/src/module/sw-dashboard/page/sw-dashboard-index/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'src/core/shopware';
 import template from './sw-dashboard-index.html.twig';
+import './sw-dashboard-index.scss';
 
 Component.register('sw-dashboard-index', {
     template,
@@ -8,5 +9,18 @@ Component.register('sw-dashboard-index', {
         return {
             title: this.$createTitle()
         };
+    },
+
+    computed: {
+        roadmapLink() {
+            return this.$tc('sw-dashboard.welcome.roadmapLink');
+        },
+
+        username() {
+            if (this.$store.state.adminUser.currentProfile) {
+                return this.$store.state.adminUser.currentProfile.firstName;
+            }
+            return '';
+        }
     }
 });

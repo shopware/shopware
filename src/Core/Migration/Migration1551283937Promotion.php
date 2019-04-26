16d0e92a80373309f9ade877632c8492af975c74
Sebastian Wieland <swieland@conexco.com>
2019-04-26 14:08:54 +0200

    NEXT-2373 - Unified Migrations

--- src/Core/Migration/Migration1551283937Promotion.php
+++ src/Core/Migration/Migration1551283937Promotion.php
@@ -29,7 +29,22 @@ class Migration1551283937Promotion extends MigrationStep
               `created_at` DATETIME(3) NOT NULL,
               `updated_at` DATETIME(3) NULL,
               PRIMARY KEY (`id`)
-              ) ENGINE = InnoDB');
+          ) ENGINE = InnoDB');
+
+        $connection->executeQuery('
+        CREATE TABLE `promotion_translation` (
+            `name` VARCHAR(255) NOT NULL,
+            `promotion_id` BINARY(16) NOT NULL,
+            `language_id` BINARY(16) NOT NULL,
+            `created_at` DATETIME(3) NOT NULL,
+            `updated_at` DATETIME(3) NULL,
+            PRIMARY KEY (`promotion_id`,`language_id`),
+            KEY `fk.promotion_translation.promotion_id` (`promotion_id`),
+            KEY `fk.promotion_translation.language_id` (`language_id`),
+            CONSTRAINT `fk.promotion_translation.promotion_id` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+            CONSTRAINT `fk.promotion_translation.language_id` FOREIGN KEY (`language_id`) REFERENCES `language` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        ');
     }
 
     public function updateDestructive(Connection $connection): void

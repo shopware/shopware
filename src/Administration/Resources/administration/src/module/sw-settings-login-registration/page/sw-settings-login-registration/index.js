86f393de62ec7bed02e05e81dcdcb7714b706b2e
Ramona Schwering <R.Schwering@shopware.com>
2019-05-01 20:31:49 +0200

    NTR - Add process button for login registration module

--- src/Administration/Resources/administration/src/module/sw-settings-login-registration/page/sw-settings-login-registration/index.js
+++ src/Administration/Resources/administration/src/module/sw-settings-login-registration/page/sw-settings-login-registration/index.js
@@ -8,14 +8,27 @@ Component.register('sw-settings-login-registration', {
         Mixin.getByName('notification')
     ],
 
+    data() {
+        return {
+            isLoading: false,
+            isSaveSuccessful: false
+        };
+    },
+
     methods: {
+        saveFinish() {
+            this.isSaveSuccessful = false;
+        },
+
         onSave() {
+            this.isSaveSuccessful = false;
+            this.isLoading = true;
+
             this.$refs.systemConfig.saveAll().then(() => {
-                this.createNotificationSuccess({
-                    title: this.$tc('sw-settings-login-registration.general.titleSaveSuccess'),
-                    message: this.$tc('sw-settings-login-registration.general.messageSaveSuccess')
-                });
+                this.isLoading = false;
+                this.isSaveSuccessful = true;
             }).catch((err) => {
+                this.isLoading = false;
                 this.createNotificationError({
                     title: this.$tc('sw-settings-login-registration.general.titleSaveError'),
                     message: err

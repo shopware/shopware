0f86736e3e7249bf9934197960eaee02d79b2ae8
Oliver Skroblin <o.skroblin@shopware.com>
2019-05-02 10:58:04 +0200

    NEXT-1396 - Fix property assignment styling

--- src/Administration/Resources/administration/src/module/sw-property/component/sw-property-option-detail/index.js
+++ src/Administration/Resources/administration/src/module/sw-property/component/sw-property-option-detail/index.js
@@ -1,4 +1,4 @@
-import { Component } from 'src/core/shopware';
+import { Component, State } from 'src/core/shopware';
 import template from './sw-property-option-detail.html.twig';
 
 Component.register('sw-property-option-detail', {
@@ -13,6 +13,16 @@ Component.register('sw-property-option-detail', {
         }
     },
 
+    computed: {
+        mediaStore() {
+            return State.getStore('media');
+        },
+
+        uploadStore() {
+            return State.getStore('upload');
+        }
+    },
+
     methods: {
         onCancel() {
             if (this.currentOption !== null) {
@@ -23,6 +33,33 @@ Component.register('sw-property-option-detail', {
 
         onSave() {
             this.$emit('save-option-edit', this.currentOption);
+        },
+
+        onUploadsAdded({ data }) {
+            if (data.length === 0) {
+                return;
+            }
+
+            this.mediaStore.sync().then(() => {
+                data.forEach((upload) => {
+                    this.currentOption.mediaId = upload.targetId;
+                });
+                this.uploadStore.runUploads(this.currentOption.id);
+            });
+        },
+
+        successfulUpload({ targetId }) {
+            this.mediaStore.getByIdAsync(targetId).then(() => {
+                this.$forceUpdate();
+            });
+        },
+
+        removeMedia() {
+            this.currentOption.mediaId = null;
+        },
+
+        setMedia(selection) {
+            this.currentOption.mediaId = selection[0].id;
         }
     }
 });

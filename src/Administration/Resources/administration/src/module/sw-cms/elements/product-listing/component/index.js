49a78fd1581fb87adff2e1b5f9dd9283114e9690
Arnold Stoba <a.stoba@shopware.com>
2019-05-03 11:45:12 +0200

    NTR - Update cms-listing block & product-box layout

--- src/Administration/Resources/administration/src/module/sw-cms/elements/product-listing/component/index.js
+++ src/Administration/Resources/administration/src/module/sw-cms/elements/product-listing/component/index.js
@@ -9,6 +9,37 @@ Component.register('sw-cms-el-product-listing', {
         Mixin.getByName('cms-element')
     ],
 
+    computed: {
+        demoProductElement() {
+            return {
+                config: {
+                    boxLayout: {
+                        source: 'static',
+                        value: 'minimal'
+                    }
+                },
+                data: {
+                    product: {
+                        name: 'Lorem Ipsum dolor',
+                        description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua.`.trim(),
+                        price: {
+                            gross: 19.90
+                        },
+                        cover: {
+                            media: {
+                                url: '/administration/static/img/cms/preview_glasses_large.jpg',
+                                alt: 'Lorem Ipsum dolor'
+                            }
+                        }
+                    }
+                }
+
+            };
+        }
+    },
+
     created() {
         this.createdComponent();
     },

0f6f87bb27e3cf780f9f117eaffd95b6ee95e8b7
Tobias Berge <t.berge@shopware.com>
2019-05-09 16:13:21 +0200

    NEXT-3227 - Fix sales channel language switch, stabilize select E2E commands

--- src/Administration/Resources/e2e/repos/administration/custom-commands/fillMultiSelect.js
+++ src/Administration/Resources/e2e/repos/administration/custom-commands/fillMultiSelect.js
@@ -10,7 +10,11 @@ exports.command = function fillMultiSelect(selector, searchTerm, value) {
         .waitForElementNotPresent(`${selector} .sw-loader`)
         .waitForElementVisible(`${selector} .sw-multi-select__results`)
         .assert.containsText(`${selector} .sw-multi-select__results .sw-multi-select-option--0`, value)
-        .click(`${selector} .sw-multi-select__results .sw-multi-select-option--0`);
+        .click(`${selector} .sw-multi-select__results .sw-multi-select-option--0`)
+        .waitForElementVisible(`${selector} .sw-multi-select__selections .sw-multi-select__selection-item-holder--0`)
+        .assert.containsText(`${selector} .sw-multi-select__selections .sw-multi-select__selection-item-holder--0 .sw-multi-select__selection-item`, value)
+        .setValue(`${selector} .sw-multi-select__input`, this.Keys.ESCAPE)
+        .waitForElementNotPresent(`${selector} .sw-multi-select__results`);
 
     return this;
 };

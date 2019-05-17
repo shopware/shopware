ee9f0ec4ee043ec3648f22868ca3fe4d5992b8d7
Anton Zoffmann <az@dasistweb.de>
2019-05-17 12:57:38 +0200

    NEXT-3422 - newest order on account overview

--- src/Storefront/Page/Account/Overview/AccountOverviewPage.php
+++ src/Storefront/Page/Account/Overview/AccountOverviewPage.php
@@ -2,8 +2,21 @@
 
 namespace Shopware\Storefront\Page\Account\Overview;
 
+use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Storefront\Page\Page;
 
 class AccountOverviewPage extends Page
 {
+    /** @var OrderEntity|null */
+    private $newestOrder;
+
+    public function setNewestOrder(OrderEntity $order)
+    {
+        $this->newestOrder = $order;
+    }
+
+    public function getNewestOrder(): ?OrderEntity
+    {
+        return $this->newestOrder;
+    }
 }
--- src/Storefront/Page/Account/Overview/AccountOverviewPageLoader.php
+++ src/Storefront/Page/Account/Overview/AccountOverviewPageLoader.php
@@ -3,6 +3,12 @@
 namespace Shopware\Storefront\Page\Account\Overview;
 
 use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Page\GenericPageLoader;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -20,17 +26,24 @@ class AccountOverviewPageLoader
      */
     private $eventDispatcher;
 
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $repository;
+
     public function __construct(
         GenericPageLoader $genericLoader,
-        EventDispatcherInterface $eventDispatcher
+        EventDispatcherInterface $eventDispatcher,
+        EntityRepositoryInterface $repository
     ) {
         $this->eventDispatcher = $eventDispatcher;
         $this->genericLoader = $genericLoader;
+        $this->repository = $repository;
     }
 
     public function load(Request $request, SalesChannelContext $context): AccountOverviewPage
     {
-        if (!$context->getCustomer()) {
+        if (!($customer = $context->getCustomer()) instanceof CustomerEntity) {
             throw new CustomerNotLoggedInException();
         }
 
@@ -38,11 +51,37 @@ class AccountOverviewPageLoader
 
         $page = AccountOverviewPage::createFrom($page);
 
+        $orderOrNull = $this->loadNewestOrder($context, $customer);
+
+        if ($orderOrNull instanceof OrderEntity) {
+            $page->setNewestOrder($orderOrNull);
+        }
+
         $this->eventDispatcher->dispatch(
             new AccountOverviewPageLoadedEvent($page, $context, $request),
             AccountOverviewPageLoadedEvent::NAME
         );
 
         return $page;
+    }
+
+    private function loadNewestOrder(SalesChannelContext $context, CustomerEntity $customer): ?OrderEntity
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('order.orderCustomer.customerId', $customer->getId()))
+            ->addSorting(new FieldSorting('orderDate', FieldSorting::DESCENDING))
+            ->addAssociationPath('transactions.paymentMethod')
+            ->addAssociationPath('deliveries.shippingMethod')
+            ->setLimit(1)
+            ->addAssociation('orderCustomer');
+
+        /** @var OrderEntity|null $order */
+        $order = $this->repository->search($criteria, $context->getContext())->first();
+
+        if (!$order instanceof OrderEntity) {
+            return null;
+        }
+
+        return $order;
     }
 }

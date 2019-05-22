76fbcea4c3084a14c966ef92b91d0fafbe5287ae
bott <bott@conexco.com>
2019-05-22 17:03:26 +0200

    NEXT-3462 - storefront styles cleanup

--- src/Storefront/Resources/src/script/plugin/header/account-menu.plugin.js
+++ src/Storefront/Resources/src/script/plugin/header/account-menu.plugin.js
@@ -4,20 +4,23 @@ import OffCanvas from 'src/script/plugin/offcanvas/offcanvas.plugin';
 import DeviceDetection from 'src/script/helper/device-detection.helper';
 import ViewportDetection from 'src/script/helper/viewport-detection.helper';
 
-export default class OffCanvasAccountMenu extends Plugin{
+export default class OffCanvasAccountMenu extends Plugin {
 
     static options = {
-
-
         /**
          * selector for the dropdown menu content which is inserted into the offcanvas
          */
-        dropdownMenuSelector: 'js-account-widget-dropdown',
+        dropdownMenuSelector: 'js-account-menu-dropdown',
 
         /**
          * additional class for the offcanvas
          */
-        additionalClass: 'offcanvas-account-menu',
+        additionalClass: 'account-menu-offcanvas',
+
+        /**
+         * class is used to hide the dropdown on viewports where the offcanvas is used
+         */
+        hiddenClass: 'd-none',
 
         /**
          * from which direction the
@@ -31,10 +34,10 @@ export default class OffCanvasAccountMenu extends Plugin{
     }
 
     /**
-   * Register events to handle opening the account menu OffCanvas
-   * by clicking a defined trigger selector
-   * @private
-   */
+     * Register events to handle opening the account menu OffCanvas
+     * by clicking a defined trigger selector
+     * @private
+     */
     _registerEventListeners() {
         const event = (DeviceDetection.isTouchDevice()) ? 'touchstart' : 'click';
         this.el.addEventListener(event, this._onClickAccountMenuTrigger.bind(this, this.el));
@@ -42,42 +45,50 @@ export default class OffCanvasAccountMenu extends Plugin{
         document.addEventListener(ViewportDetection.EVENT_VIEWPORT_HAS_CHANGED(), this._onViewportHasChanged.bind(this));
     }
 
-
     /**
-   * On clicking the trigger item the account menu OffCanvas shall open
-   * and the dropdown content may be fetched and shown inside the OffCanvas.
-   * @param trigger
-   * @private
-   */
+     * On clicking the trigger item the account menu OffCanvas shall open
+     * and the dropdown content may be fetched and shown inside the OffCanvas.
+     * @param trigger
+     * @private
+     */
     _onClickAccountMenuTrigger(trigger) {
 
         // if the current viewport is not allowed return
         if (this._isInAllowedViewports() === false) return;
 
-        const html = DomAccess.querySelector(trigger.parentNode, `.${this.options.dropdownMenuSelector}`);
+        this._dropdown = DomAccess.querySelector(trigger.parentNode, `.${this.options.dropdownMenuSelector}`);
 
-        OffCanvas.open(html.innerHTML, null, this.options.offcanvasPostion, true, OffCanvas.REMOVE_OFF_CANVAS_DELAY());
+        this._dropdown.classList.add(this.options.hiddenClass);
+
+        OffCanvas.open(this._dropdown.innerHTML, null, this.options.offcanvasPostion, true, OffCanvas.REMOVE_OFF_CANVAS_DELAY());
         OffCanvas.setAdditionalClassName(this.options.additionalClass);
     }
 
     /**
-   * If viewport has changed verify whether to close
-   * an already open account menu offcanvas or not
-   * @private
-   */
+     * If viewport has changed verify whether to close
+     * an already open account menu offcanvas/dropwdown or not
+     * @private
+     */
     _onViewportHasChanged() {
         if (this._isInAllowedViewports() === false && OffCanvas.exists()) {
             OffCanvas.close();
         }
+
+        if (this._dropdown) {
+            if (this._isInAllowedViewports() === false) {
+                this._dropdown.classList.remove(this.options.hiddenClass);
+            } else {
+                this._dropdown.classList.add(this.options.hiddenClass);
+            }
+        }
     }
 
     /**
-   * Returns if the browser is in the allowed viewports
-   * @returns {boolean}
-   * @private
-   */
+     * Returns if the browser is in the allowed viewports
+     * @returns {boolean}
+     * @private
+     */
     _isInAllowedViewports() {
         return (ViewportDetection.isXS() || ViewportDetection.isSM());
     }
-
 }

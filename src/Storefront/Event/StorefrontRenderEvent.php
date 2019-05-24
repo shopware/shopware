c39e6cbcaafbbd6fb55c6391d5e6911284bd7686
Oliver Skroblin <o.skroblin@shopware.com>
2019-05-24 11:51:22 +0200

    Implement storefront render event

---
+++ src/Storefront/Event/StorefrontRenderEvent.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storefront\Event;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Event\NestedEvent;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+
+class StorefrontRenderEvent extends NestedEvent
+{
+    public const NAME = 'storefront.render';
+
+    /**
+     * @var string
+     */
+    protected $view;
+
+    /**
+     * @var array
+     */
+    protected $parameters;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var SalesChannelContext
+     */
+    protected $context;
+
+    public function __construct(string $view, array $parameters, Request $request, SalesChannelContext $context)
+    {
+        $this->view = $view;
+        $this->parameters = $parameters;
+        $this->request = $request;
+        $this->context = $context;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSalesChannelContext(): SalesChannelContext
+    {
+        return $this->context;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context->getContext();
+    }
+
+    public function getView(): string
+    {
+        return $this->view;
+    }
+
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    public function setParameter(string $key, $value): void
+    {
+        $this->parameters[$key] = $value;
+    }
+}

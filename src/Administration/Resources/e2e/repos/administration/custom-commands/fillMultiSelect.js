78f5a885c10c295ad0c2f60297c06b288b8d7183
Ramona Schwering <R.Schwering@shopware.com>
2019-05-27 11:21:41 +0200

    NTR - Fix e2e SwSelect and checkout issues

--- src/Administration/Resources/e2e/repos/administration/custom-commands/fillMultiSelect.js
+++ src/Administration/Resources/e2e/repos/administration/custom-commands/fillMultiSelect.js
@@ -9,7 +9,7 @@ exports.command = function fillMultiSelect(selector, searchTerm, value, position
         .click(selector)
         .fillField(`${selector} .sw-multi-select__input`, searchTerm, true)
         .waitForElementNotPresent(`${selector} .sw-loader`)
-        .waitForElementVisible(`${selector} .sw-multi-select__results`)
+        .waitForElementVisible(`${selector} .sw-multi-select__results .sw-multi-select-option--0`)
         .assert.containsText(`${selector} .sw-multi-select__results .sw-multi-select-option--0`, value)
         .click(`${selector} .sw-multi-select__results .sw-multi-select-option--0`)
         .expect.element(`${selector} .sw-multi-select__selections .sw-multi-select__selection-item-holder--${position} .sw-multi-select__selection-item`).to.have.text.that.contains(value);
--- src/Administration/Resources/e2e/repos/administration/custom-commands/fillSingleSelect.js
+++ src/Administration/Resources/e2e/repos/administration/custom-commands/fillSingleSelect.js
@@ -8,11 +8,12 @@ exports.command = function fillSingleSelect(selector, value, resultPosition = 0)
         .click(selector)
         .waitForElementVisible(`${selector} .sw-single-select__results-list`)
         .waitForElementNotPresent(`${selector} .sw-loader__element`)
+        .waitForElementVisible(`${selector} .sw-single-select__results`)
         .fillField(`${selector} .sw-single-select__input-single`, value, true)
-        .assert.containsText(`${selector} .sw-single-select__results-list .sw-single-select-option--0`, value);
+        .assert.containsText(`${selector} .sw-single-select__results-list .sw-single-select-option--${resultPosition}`, value);
 
     this
-        .click(`${selector} .sw-single-select__results-list .sw-single-select-option--0`)
+        .click(`${selector} .sw-single-select__results-list .sw-single-select-option--${resultPosition}`)
         .waitForElementNotPresent(`${selector} .sw-single-select__results`);
     return this;
 };

08d8903e471e19e3813c6b7a0208a3d6fae1fb90
Stephan Pohl <stp@shopware.com>
2019-05-31 10:13:28 +0200

    NTR Replace underscore with minus

--- src/Administration/Resources/administration/src/app/service/locale-to-language.service.js
+++ src/Administration/Resources/administration/src/app/service/locale-to-language.service.js
@@ -17,7 +17,7 @@ export default function createLocaleToLanguageService() {
         const languageRepo = repoFactory.create('language', '/language');
         const localeCriteria = new Criteria();
 
-        localeCriteria.setTerm(locale.replace('-', '_'));
+        localeCriteria.setTerm(locale);
 
         return localeRepo.search(localeCriteria, contextService).then((data) => {
             return data.first().id;

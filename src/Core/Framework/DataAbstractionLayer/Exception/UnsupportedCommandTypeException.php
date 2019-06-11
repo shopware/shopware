0a288ec42ed34b5148f106dd341e0e561a33612c
Sebastian Franze <s.franze@shopware.com>
2019-06-11 15:32:32 +0200

    NEXT-3637 - Add parent field related Exceptions

---
+++ src/Core/Framework/DataAbstractionLayer/Exception/UnsupportedCommandTypeException.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer\Exception;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\WriteCommandInterface;
+use Shopware\Core\Framework\ShopwareHttpException;
+
+class UnsupportedCommandTypeException extends ShopwareHttpException
+{
+    public function __construct(WriteCommandInterface $command)
+    {
+        parent::__construct('Command of class {{ command }} is not supported by {{ definition }}',
+            [
+                'command' => \get_class($command),
+                'definition' => $command->getDefinition()->getClass(),
+            ]);
+    }
+
+    public function getErrorCode(): string
+    {
+        return 'FRAMEWORK__UNSUPPORTED_COMMAND_TYPE_EXCEPTION';
+    }
+}

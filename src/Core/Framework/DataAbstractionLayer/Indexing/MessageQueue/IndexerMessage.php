a2107e4ba160eb94051fb23e6ccf70adaf5508d6
Malte Janz <m.janz@shopware.com>
2019-06-17 11:48:45 +0200

    PT-10510 - Implement message queue IndexerMessageSender

---
+++ src/Core/Framework/DataAbstractionLayer/Indexing/MessageQueue/IndexerMessage.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer\Indexing\MessageQueue;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
+
+class IndexerMessage
+{
+    public const ACTION_INDEX = 'index';
+    public const ACTION_REFRESH = 'refresh';
+
+    /**
+     * @var string classname of the indexer
+     */
+    private $indexer;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $timestamp;
+
+    /**
+     * @var string
+     */
+    private $actionType = self::ACTION_INDEX;
+
+    /**
+     * @var EntityWrittenContainerEvent
+     */
+    private $entityWrittenContainerEvent;
+
+    public function getIndexer(): string
+    {
+        return $this->indexer;
+    }
+
+    public function getTimestamp(): \DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function getActionType(): string
+    {
+        return $this->actionType;
+    }
+
+    public function getEntityWrittenContainerEvent(): EntityWrittenContainerEvent
+    {
+        return $this->entityWrittenContainerEvent;
+    }
+
+    public function setIndexer(string $indexer): void
+    {
+        $this->indexer = $indexer;
+    }
+
+    public function setTimestamp(\DateTimeInterface $timestamp): void
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    public function setActionType(string $actionType): void
+    {
+        $this->actionType = $actionType;
+    }
+
+    public function setEntityWrittenContainerEvent(EntityWrittenContainerEvent $entityWrittenContainerEvent): void
+    {
+        $this->entityWrittenContainerEvent = $entityWrittenContainerEvent;
+    }
+}

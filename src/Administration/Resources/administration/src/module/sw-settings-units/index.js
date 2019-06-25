5ab00a4aaa3d25783e2714c507f48c30d79d3dcd
Jannis Leifeld <j.leifeld@shopware.com>
2019-06-25 09:40:37 +0200

    NEXT-3770 - create settings page for units

---
+++ src/Administration/Resources/administration/src/module/sw-settings-units/index.js
@@ -0,0 +1,33 @@
+import { Module } from 'src/core/shopware';
+
+import './extension/sw-settings-index';
+import './page/sw-settings-units';
+
+import deDE from './snippet/de-DE.json';
+import enGB from './snippet/en-GB.json';
+
+Module.register('sw-settings-units', {
+    type: 'core',
+    name: 'settings-units',
+    title: 'sw-settings-units.general.mainMenuItemGeneral',
+    description: 'Units section in the settings module',
+    color: '#9AA8B5',
+    icon: 'default-object-scale',
+    favicon: 'icon-module-settings.png',
+    entity: 'units',
+
+    snippets: {
+        'de-DE': deDE,
+        'en-GB': enGB
+    },
+
+    routes: {
+        index: {
+            component: 'sw-settings-units',
+            path: 'index',
+            meta: {
+                parentPath: 'sw.settings.index'
+            }
+        }
+    }
+});

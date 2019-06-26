1ce046455561e2734af1c85c566cd30cea7003bb
Uli Ehler <uehler@conexco.com>
2019-06-26 11:29:31 +0200

    NEXT-3719 - fixed birthday validation

--- src/Core/Checkout/Customer/Validation/CustomerProfileValidationService.php
+++ src/Core/Checkout/Customer/Validation/CustomerProfileValidationService.php
@@ -7,6 +7,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Validation\EntityExists;
 use Shopware\Core\Framework\Validation\DataValidationDefinition;
 use Shopware\Core\Framework\Validation\ValidationServiceInterface;
 use Shopware\Core\System\Salutation\SalutationDefinition;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,11 +18,17 @@ class CustomerProfileValidationService implements ValidationServiceInterface
      * @var SalutationDefinition
      */
     private $salutationDefinition;
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
 
     public function __construct(
-        SalutationDefinition $salutationDefinition
+        SalutationDefinition $salutationDefinition,
+        SystemConfigService $systemConfigService
     ) {
         $this->salutationDefinition = $salutationDefinition;
+        $this->systemConfigService = $systemConfigService;
     }
 
     public function buildCreateValidation(Context $context): DataValidationDefinition
@@ -47,9 +54,13 @@ class CustomerProfileValidationService implements ValidationServiceInterface
         $definition
             ->add('salutationId', new NotBlank(), new EntityExists(['entity' => $this->salutationDefinition->getEntityName(), 'context' => $context]))
             ->add('firstName', new NotBlank())
-            ->add('lastName', new NotBlank())
-            ->add('birthdayDay', new GreaterThanOrEqual(['value' => 1]), new LessThanOrEqual(['value' => 31]))
-            ->add('birthdayMonth', new GreaterThanOrEqual(['value' => 1]), new LessThanOrEqual(['value' => 12]))
-            ->add('birthdayYear', new LessThanOrEqual(['value' => date('Y')]));
+            ->add('lastName', new NotBlank());
+
+        if ($this->systemConfigService->get('core.loginRegistration.showBirthdayField') && $this->systemConfigService->get('core.loginRegistration.birthdayFieldRequired')) {
+            $definition
+                ->add('birthdayDay', new GreaterThanOrEqual(['value' => 1]), new LessThanOrEqual(['value' => 31]))
+                ->add('birthdayMonth', new GreaterThanOrEqual(['value' => 1]), new LessThanOrEqual(['value' => 12]))
+                ->add('birthdayYear', new LessThanOrEqual(['value' => date('Y')]));
+        }
     }
 }

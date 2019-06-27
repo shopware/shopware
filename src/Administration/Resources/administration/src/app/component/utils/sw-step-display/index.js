3008f444f9728e3843cc0823afb4595b93bef445
Malte Janz <m.janz@shopware.com>
2019-06-27 14:43:58 +0200

    NTR - Implement sw-step-display component

---
+++ src/Administration/Resources/administration/src/app/component/utils/sw-step-display/index.js
@@ -0,0 +1,102 @@
+import template from './sw-step-display.html.twig';
+
+/**
+ * @public
+ * @description This step display component need flow-items inside it's slot to work.
+ * To control the current position use the `itemIndex` property (zero-based index).
+ * To change the variant of the current position you can use the `itemVariant` property.
+ * To load specific variants for multiple items you can use the `initialItemVariants` property
+ * with an array of variants. Possible variants are 'disabled', 'info', 'error' and 'success'.
+ * @status ready
+ * @example-type dynamic
+ * @component-example
+ * <sw-step-display :itemIndex="flowChartItemIndex"
+ *                            :itemVariant="flowChartItemVariant"
+ *                            :initialItemVariants="flowChartInitialItemVariants">
+ *   <sw-step-item>
+ *      Check
+ *   </sw-step-item>
+ *   <sw-step-item>
+ *      Read
+ *   </sw-step-item>
+ *   <sw-step-item disabledIcon="small-default-checkmark-line-medium">
+ *      Finish
+ *   </sw-step-item>
+ * </sw-step-display>
+ */
+export default {
+    name: 'sw-step-display',
+
+    template,
+
+    props: {
+        itemIndex: {
+            type: Number,
+            required: true
+        },
+        itemVariant: {
+            type: String,
+            required: true
+        },
+        initialItemVariants: {
+            type: Array,
+            default() {
+                return [];
+            },
+            required: false
+        }
+    },
+
+    data() {
+        return {
+            items: []
+        };
+    },
+
+    mounted() {
+        // read child step items
+        this.$children.forEach((child) => {
+            if (child.$options._componentTag === 'sw-step-item') {
+                this.items.push(child);
+            }
+        });
+
+        this.setItemVariants(this.initialItemVariants);
+        this.setItemActive(this.itemIndex, true);
+        this.setVariantForCurrentItem(this.itemVariant);
+    },
+
+    watch: {
+        itemIndex(newIndex, oldIndex) {
+            this.setItemActive(oldIndex, false);
+            this.setItemActive(newIndex, true);
+            this.setVariantForCurrentItem(this.itemVariant);
+        },
+        itemVariant(newVariant) {
+            this.setVariantForCurrentItem(newVariant);
+        },
+        initialItemVariants: {
+            deep: true,
+            handler(newItemVariants) {
+                this.setItemVariants(newItemVariants);
+            }
+        }
+    },
+
+    methods: {
+        setItemVariants(itemVariants) {
+            const max = Math.min(this.items.length, itemVariants.length);
+            for (let i = 0; i < max; i += 1) {
+                this.items[i].setVariant(itemVariants[i]);
+            }
+        },
+
+        setVariantForCurrentItem(variant) {
+            this.items[this.itemIndex].setVariant(variant);
+        },
+
+        setItemActive(index, active) {
+            this.items[index].setActive(active);
+        }
+    }
+};
---
+++ src/Administration/Resources/administration/src/app/component/utils/sw-step-item/index.js
@@ -0,0 +1,70 @@
+import template from './sw-step-item.html.twig';
+import './sw-step-item.scss';
+
+
+/**
+ * @public
+ * @description Renders a step and must be used in the slot of the sw-step-display component.
+ * @status ready
+ * @example-type dynamic
+ * @component-example
+ * <sw-step-item disabledIcon="small-default-checkmark-line-medium">
+ *     Finish
+ * </sw-step-item>
+ */
+export default {
+    name: 'sw-step-item',
+
+    template,
+
+    props: {
+        disabledIcon: {
+            type: String,
+            default: 'small-default-circle-medium',
+            required: false
+        }
+    },
+
+    data() {
+        return {
+            variant: 'disabled',
+            active: false
+        };
+    },
+
+    computed: {
+        modifierClasses() {
+            return [
+                `sw-step-item--${this.variant}`,
+                {
+                    'sw-step-item--active': this.active
+                }
+            ];
+        },
+
+        icon() {
+            const iconConfig = {
+                disabled: this.disabledIcon,
+                info: 'small-default-circle-medium',
+                error: 'small-default-x-line-medium',
+                success: 'small-default-checkmark-line-medium'
+            };
+
+            return iconConfig[this.variant];
+        }
+    },
+
+    methods: {
+        setActive(active) {
+            this.active = active;
+        },
+
+        setVariant(variant) {
+            if (!['disabled', 'info', 'error', 'success'].includes(variant)) {
+                return;
+            }
+
+            this.variant = variant;
+        }
+    }
+};

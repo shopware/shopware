c0f1aa5da3579eefc24e6030aa94c780633f7109
Markus Velt <m.velt@shopware.com>
2019-07-09 14:31:59 +0200

    NEXT-3735 - fix finish page layout

--- src/Administration/Resources/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-finish/index.js
+++ src/Administration/Resources/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-finish/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'src/core/shopware';
 import template from './sw-first-run-wizard-finish.html.twig';
+import './sw-first-run-wizard-finish.scss';
 
 Component.register('sw-first-run-wizard-finish', {
     template

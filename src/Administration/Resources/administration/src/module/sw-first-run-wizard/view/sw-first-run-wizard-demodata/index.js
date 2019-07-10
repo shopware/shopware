58109561fb6829c187655d00876eb6a012934168
Markus Velt <m.velt@shopware.com>
2019-07-10 10:14:30 +0200

    NEXT-3735 - Fix demodata page layout

--- src/Administration/Resources/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-demodata/index.js
+++ src/Administration/Resources/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-demodata/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'src/core/shopware';
 import template from './sw-first-run-wizard-demodata.html.twig';
+import './sw-first-run-wizard-demodata.scss';
 
 Component.register('sw-first-run-wizard-demodata', {
     template

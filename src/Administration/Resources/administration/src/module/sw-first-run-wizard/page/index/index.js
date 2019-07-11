ad528a77766a8ab880c2b4ea5e5acb37200808c7
Markus Velt <m.velt@shopware.com>
2019-07-11 07:54:41 +0200

    NEXT-3735 - Remove unused vuex store

--- src/Administration/Resources/administration/src/module/sw-first-run-wizard/page/index/index.js
+++ src/Administration/Resources/administration/src/module/sw-first-run-wizard/page/index/index.js
@@ -1,13 +1,8 @@
 import { Component } from 'src/core/shopware';
-// import { mapState } from 'vuex';
-// import Criteria from 'src/core/data-new/criteria.data';
 import template from './sw-first-run-wizard.html.twig';
-import swFirstRunWizardState from './state';
 
 Component.register('sw-first-run-wizard', {
     template,
-
-    inject: ['repositoryFactory', 'context'],
 
     metaInfo() {
         return {
@@ -15,50 +10,9 @@ Component.register('sw-first-run-wizard', {
         };
     },
 
-    data() {
-        return {
-            repository: null
-        };
-    },
-
     computed: {
-        // ...mapState('swFirstRunWizardState', [
-        //     'currentLocale'
-        // ]),
-        currentLocale() {
-            return this.$store.state.adminLocale.currentLocale;
-        },
-
         title() {
             return `${this.$tc('sw-first-run-wizard.welcome.modalTitle')}`;
-        }
-    },
-
-    beforeCreate() {
-        this.$store.registerModule('swFirstRunWizardState', swFirstRunWizardState);
-    },
-
-    created() {
-        this.createdComponent();
-    },
-
-    methods: {
-        createdComponent() {
-            console.log('created');
-            // this.repository = this.repositoryFactory.create('plugin');
-
-            // const criteria = new Criteria();
-            // criteria.addFilter(
-            //     Criteria.equals('plugin.name', 'SwagPayPal')
-            // );
-
-            // this.repository
-            //     .search(criteria, this.context)
-            //     .then((result) => {
-            //         const plugin = result.first();
-
-            //         console.log(plugin);
-            //     });
         }
     }
 });

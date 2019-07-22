78ea318f7e00417a21799b5649b7d4d74fa76db3
Patrick Weyck <p.weyck@shopware.com>
2019-07-22 22:45:40 +0200

    NEXT-4121 - Fix profiler

--- src/Core/Profiling/Profiling.php
+++ src/Core/Profiling/Profiling.php
@@ -2,10 +2,15 @@
 
 namespace Shopware\Core\Profiling;
 
+use Shopware\Core\Framework\Bundle;
+use Shopware\Core\Kernel;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class Profiling extends Bundle
 {
@@ -16,7 +21,27 @@ class Profiling extends Bundle
     {
         parent::build($container);
 
+        $environment = $container->getParameter('kernel.environment');
+        $this->buildConfig($container, $environment);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection/'));
         $loader->load('services.xml');
+    }
+
+    private function buildConfig(ContainerBuilder $container, $environment): void
+    {
+        $locator = new FileLocator($this->getConfigPath());
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = $this->getPath() . '/' . $this->getConfigPath();
+
+        $configLoader->load($confDir . '/{packages}/*' . Kernel::CONFIG_EXTS, 'glob');
+        $configLoader->load($confDir . '/{packages}/' . $environment . '/*' . Kernel::CONFIG_EXTS, 'glob');
     }
 }

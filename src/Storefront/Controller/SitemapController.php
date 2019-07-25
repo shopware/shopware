b17c4ee41be90f2a0d1fece12f43e2915b20cbc4
Uli Ehler <uehler@conexco.com>
2019-07-25 18:17:25 +0200

    NEXT-1996 - implemented sitemap.xml

---
+++ src/Storefront/Controller/SitemapController.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storefront\Controller;
+
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Page\Sitemap\SitemapPageLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SitemapController extends StorefrontController
+{
+    /**
+     * @var SitemapPageLoader
+     */
+    private $sitemapPageLoader;
+
+    public function __construct(SitemapPageLoader $sitemapPageLoader)
+    {
+        $this->sitemapPageLoader = $sitemapPageLoader;
+    }
+
+    /**
+     * @Route("/sitemap.xml", name="frontend.sitemap.xml", methods={"GET"}, defaults={"_format"="xml"})
+     */
+    public function sitemapXml(SalesChannelContext $context, Request $request): Response
+    {
+        $page = $this->sitemapPageLoader->load($request, $context);
+
+        return $this->renderStorefront('@Storefront/page/sitemap/sitemap.xml.twig', ['page' => $page]);
+    }
+}

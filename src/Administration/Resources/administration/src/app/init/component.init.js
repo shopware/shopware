41287841b92b7fc0f2718736225b1e2c5df0266a
Tobias Berge <t.berge@shopware.com>
2019-08-19 14:42:11 +0200

    NEXT-4584 - Remove register logic from component initializer

--- src/Administration/Resources/administration/src/app/init/component.init.js
+++ src/Administration/Resources/administration/src/app/init/component.init.js
@@ -1,30 +1,5 @@
 import baseComponents from 'src/app/component/components';
 
-const { Component } = Shopware;
-const { warn } = Shopware.Utils.debug;
-
 export default function initializeBaseComponents() {
-    const components = baseComponents.filter((item) => {
-        return item !== undefined;
-    });
-
-    components.forEach((component) => {
-        if (component.extendsFrom && typeof component.extendsFrom !== 'string') {
-            warn(
-                'component.init',
-                'extendsFrom always must be a string.',
-                component
-            );
-            return;
-        }
-
-        const isExtendedComponent = (component.extendsFrom && component.extendsFrom.length);
-
-        if (isExtendedComponent) {
-            Component.extend(component.name, component.extendsFrom, component);
-            return;
-        }
-
-        Component.register(component.name, component);
-    });
+    return baseComponents;
 }

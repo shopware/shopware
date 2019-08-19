e4f4ea897aa2173f1256df8db673d57debb2783a
Stephan Pohl <stp@shopware.com>
2019-08-19 07:06:23 +0200

    NTR - Runtime chunk for the node modules

--- src/Administration/Resources/administration/build/webpack.base.conf.js
+++ src/Administration/Resources/administration/build/webpack.base.conf.js
@@ -17,6 +17,21 @@ module.exports = {
             ? config.build.performanceHints
             : config.dev.performanceHints
     },
+    optimization: {
+        runtimeChunk: {
+            name: 'runtime'
+        },
+        splitChunks: {
+            cacheGroups: {
+                'runtime-vendor': {
+                    test: utils.resolve('node_modules'),
+                    name: 'vendors-node',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    moduleIds: 'hashed',
     entry: {
         commons: [`${resolve('src')}/core/common.js`, `${resolve('src')}/core/shopware.js`],
         app: `${resolve('src')}/app/main.js`

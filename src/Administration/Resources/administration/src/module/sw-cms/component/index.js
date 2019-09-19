db7e2636a01480c017662e11403f461934d40dde
Thorben pantring <T.Pantring@shopware.com>
2019-09-19 09:05:42 +0200

    NEXT-3527 - Add CMS creation wizard

--- src/Administration/Resources/administration/src/module/sw-cms/component/index.js
+++ src/Administration/Resources/administration/src/module/sw-cms/component/index.js
@@ -13,3 +13,4 @@ import './sw-cms-section';
 import './sw-cms-stage-add-section';
 import './sw-cms-stage-section-selection';
 import './sw-cms-sidebar';
+import './sw-cms-create-wizard';
--- src/Administration/Resources/administration/src/module/sw-cms/component/sw-cms-create-wizard/index.js
+++ src/Administration/Resources/administration/src/module/sw-cms/component/sw-cms-create-wizard/index.js
@@ -1,25 +1,61 @@
 import template from './sw-cms-create-wizard.html.twig';
 import './sw-cms-create-wizard.scss';
 
-const { Component } = Shopware;
+const { Component, Application } = Shopware;
 
-// ToDo:
 Component.register('sw-cms-create-wizard', {
     template,
 
-    inject: ['cmsService'],
+    inject: [
+        'repositoryFactory',
+        'context'
+    ],
 
     props: {
-
+        page: {
+            type: Object,
+            required: true
+        }
     },
 
     data() {
         return {
+            step: 1,
+            pageTypeNames: {
+                page: this.$tc('sw-cms.detail.labelPageTypeShopPage'),
+                landingpage: this.$tc('sw-cms.detail.labelPageTypeShopPage'),
+                product_list: this.$tc('sw-cms.detail.labelPageTypeLandingpage'),
+                product_detail: this.$tc('sw-cms.detail.labelPageTypeProduct')
+            },
+
+            pageTypeIcons: {
+                page: 'default-object-lightbulb',
+                landingpage: 'default-web-dashboard',
+                product_list: 'default-shopping-basket',
+                product_detail: 'default-action-tags'
+            }
         };
     },
 
     computed: {
+        pagePreviewMedia() {
+            if (this.page.sections.length < 1) {
+                return '';
+            }
 
+            const initContainer = Application.getContainer('init');
+            const context = initContainer.contextService;
+            const imgPath = 'administration/static/img/cms';
+
+            return `url(${context.assetsPath}/${imgPath}/preview_${this.page.type}_${this.page.sections[0].type}.png)`;
+        },
+
+        pagePreviewStyle() {
+            return {
+                'background-image': this.pagePreviewMedia,
+                'background-size': 'cover'
+            };
+        }
     },
 
     created() {
@@ -29,6 +65,29 @@ Component.register('sw-cms-create-wizard', {
     methods: {
         createdComponent() {
 
+        },
+
+        getPageTypeName() {
+            return this.pageTypeNames[this.page.type];
+        },
+
+
+        getPageIconName() {
+            return this.pageTypeIcons[this.page.type];
+        },
+
+        onPageTypeSelect(type) {
+            this.page.type = type;
+            this.step = 2;
+        },
+
+        onSectionSelect(section) {
+            this.step = 3;
+            this.$emit('on-section-select', section);
+        },
+
+        onCompletePageCreation() {
+            this.$emit('wizard-complete');
         }
     }
 });

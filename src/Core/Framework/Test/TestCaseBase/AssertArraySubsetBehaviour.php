1a479aebe962aaa11597db328a231ae72bfb5392
Jonas Elfering <jonas.elfering@web.de>
2019-10-04 09:10:29 +0200

    NTR - Update phpunit to 8.4

--- src/Core/Framework/Test/TestCaseBase/AssertArraySubsetBehaviour.php
+++ src/Core/Framework/Test/TestCaseBase/AssertArraySubsetBehaviour.php
@@ -4,7 +4,6 @@ namespace Shopware\Core\Framework\Test\TestCaseBase;
 
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\Constraint\ArraySubset;
-use PHPUnit\Util\InvalidArgumentHelper;
 
 /**
  * @deprecated just a polyfill to remove the noise
@@ -17,17 +16,11 @@ trait AssertArraySubsetBehaviour
     public function silentAssertArraySubset($subset, $array, bool $checkForObjectIdentity = false, string $message = ''): void
     {
         if (!(\is_array($subset) || $subset instanceof \ArrayAccess)) {
-            throw InvalidArgumentHelper::factory(
-                1,
-                'array or ArrayAccess'
-            );
+            throw new \InvalidArgumentException('Parameter 1 is expected to be array or ArrayAccess');
         }
 
         if (!(\is_array($array) || $array instanceof \ArrayAccess)) {
-            throw InvalidArgumentHelper::factory(
-                2,
-                'array or ArrayAccess'
-            );
+            throw new \InvalidArgumentException('Parameter 2 is expected to be array or ArrayAccess');
         }
 
         $constraint = new ArraySubset($subset, $checkForObjectIdentity);

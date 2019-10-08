3934acbd9c53c898c7488f44994a1c70766a7547
Tobias Berge <t.berge@shopware.com>
2019-10-08 11:13:08 +0200

    NTR - Log error

--- src/Administration/Resources/e2e/routes/cypress.js
+++ src/Administration/Resources/e2e/routes/cypress.js
@@ -6,12 +6,15 @@ const app = express();
 app.get('/cleanup', (req, res) => {
     return childProcess.exec('./psh.phar e2e:cleanup', (err, stdin) => {
         if (err) {
-            res.status(500).send(err);
+            res.status(500).send(err.message);
+            return;
         }
 
         if (!stdin.includes('All commands successfully executed!')) {
             res.status(500).send(stdin);
+            return;
         }
+
         res.send(stdin);
     });
 });

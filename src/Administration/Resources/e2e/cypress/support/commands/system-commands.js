1aad31b9c67886af1e3948b2b79328d72e1c43a8
Tobias Berge <t.berge@shopware.com>
2019-10-08 15:14:55 +0200

    NTR - trigger

--- src/Administration/Resources/e2e/cypress/support/commands/system-commands.js
+++ src/Administration/Resources/e2e/cypress/support/commands/system-commands.js
@@ -39,6 +39,8 @@ Cypress.Commands.add('cleanUpPreviousState', () => {
         return cy.exec(`${Cypress.env('projectRoot')}/psh.phar e2e:restore-db`);
     }
 
+    cy.log('new');
+
     return cy.request(`http://${new URL(Cypress.config('baseUrl')).hostname}:8005/cleanup`)
         .its('body').should('eq', 'success');
 });

a876c72dee6b185e871c2a4916827a739767d958
Tobias Berge <t.berge@shopware.com>
2019-10-11 16:32:04 +0200

    NEXT-4966 - Add settings module for delivery times

---
+++ src/Administration/Resources/administration/src/module/sw-settings-delivery-times/page/sw-settings-delivery-time-create/index.js
@@ -0,0 +1,24 @@
+const { Component, State } = Shopware;
+
+Component.extend('sw-settings-delivery-time-create', 'sw-settings-delivery-time-detail', {
+    computed: {
+        languageStore() {
+            return State.getStore('language');
+        }
+    },
+
+    methods: {
+        saveFinish() {
+            this.isSaveSuccessful = false;
+            this.$router.push({
+                name: 'sw.settings.delivery.time.detail', params: { id: this.deliveryTime.id }
+            });
+        },
+
+        createdComponent() {
+            this.languageStore.setCurrentId(this.languageStore.systemLanguageId);
+
+            this.deliveryTime = this.deliveryTimeRepository.create(this.context);
+        }
+    }
+});

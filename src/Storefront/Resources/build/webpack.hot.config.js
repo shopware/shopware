639b4925da5e76827e4e1c83c31d19ef5268d58b
wkrawiec <wkrawiec@gmail.com>
2019-10-28 20:56:07 +0100

    NTR - fix local fonts source in hot reload mode
    Fixes https://github.com/shopware/platform/pull/265

--- src/Storefront/Resources/build/webpack.hot.config.js
+++ src/Storefront/Resources/build/webpack.hot.config.js
@@ -59,6 +59,18 @@ const modules = {
                 },
             ],
         },
+        {
+            test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                },
+            ],
+        },
     ],
 };
 

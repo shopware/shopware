b890da6119529715f2e0697d9ec3c00e6f3b81eb
Oliver Skroblin <o.skroblin@shopware.com>
2019-10-28 09:31:01 +0100

    NEXT-4148 - Move component library to administration/build

---
+++ src/Administration/Resources/administration/build/nuxt-component-library/component-library.conf.js
@@ -0,0 +1,4 @@
+module.exports = {
+    components: `${__dirname}/../../src/app/component/**/!(components).js`,
+    sassVariables: `${__dirname}/../../src/app/assets/scss/variables.scss`
+};
--- src/Administration/Resources/nuxt-component-library/component-library.conf.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    components: `${__dirname}/../administration/src/app/component/**/!(components).js`,
-    sassVariables: `${__dirname}/../administration/src/app/assets/scss/variables.scss`
-};

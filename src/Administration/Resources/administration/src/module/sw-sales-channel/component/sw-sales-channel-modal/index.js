1bd10ea14656bfe5e86c05b9598bc136fb4d2587
Stephan Pohl <stp@shopware.com>
2019-11-06 11:08:08 +0100

    NEXT-4998 Split up sales channel modal into two components

--- src/Administration/Resources/administration/src/module/sw-sales-channel/component/sw-sales-channel-modal/index.js
+++ src/Administration/Resources/administration/src/module/sw-sales-channel/component/sw-sales-channel-modal/index.js
@@ -1,58 +1,30 @@
 import template from './sw-sales-channel-modal.html.twig';
 import './sw-sales-channel-modal.scss';
 
-const { Component, State } = Shopware;
+const { Component } = Shopware;
 
 Component.register('sw-sales-channel-modal', {
     template,
 
-    inject: ['salesChannelService'],
-
     data() {
         return {
-            salesChannelTypes: [],
-            isLoading: false,
-            detailType: false,
-            total: 0
+            detailType: null
         };
     },
 
     computed: {
-        salesChannelStore() {
-            return State.getStore('sales_channel');
-        },
-
-        salesChannelTypeStore() {
-            return State.getStore('sales_channel_type');
-        },
-
         modalTitle() {
             if (this.detailType) {
-                return `${this.$tc('sw-sales-channel.modal.titleDetailPrefix')} ${this.detailType.name}`;
+                return this.$tc('sw-sales-channel.modal.titleDetailPrefix', 0, { name: this.detailType.name });
             }
 
             return this.$tc('sw-sales-channel.modal.title');
         }
     },
 
-    created() {
-        this.createdComponent();
-    },
-
     methods: {
-        createdComponent() {
-            const params = {
-                limit: 500,
-                page: 1
-            };
-
-            this.isLoading = true;
-
-            this.salesChannelTypeStore.getList(params).then((response) => {
-                this.total = response.total;
-                this.salesChannelTypes = response.items;
-                this.isLoading = false;
-            });
+        onGridOpenDetails(detailType) {
+            this.detailType = detailType;
         },
 
         onCloseModal() {

ec87147db5baea848af102ced9413ce3ce446b47
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2019-11-25 10:58:45 +0100

    NEXT-5043 - Export template contents to new files

--- src/Administration/Resources/app/administration/src/module/sw-sales-channel/product-export-templates/google-product-search-de/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/product-export-templates/google-product-search-de/index.js
@@ -1,47 +1,13 @@
-/* eslint-disable max-len */
+import header from './header.xml.twig';
+import body from './body.xml.twig';
+import footer from './footer.xml.twig';
+
 Shopware.Service('exportTemplateService').registerProductExportTemplate({
     name: 'google-product-search-de',
     translationKey: 'sw-sales-channel.detail.productComparison.templates.template-label.google-product-search-de',
-    headerTemplate: `
-<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0" xmlns:atom="http://www.w3.org/2005/Atom">
-<channel>
-    <atom:link href="{{ productExport.salesChannelDomain.url }}/export/{{ productExport.accessKey }}/{{ productExport.fileName }}" rel="self" type="application/rss+xml" />
-    <title>{{ context.salesChannel.name }}</title>
-    <description>{# change your shop\'s description #}</description>
-    <link>{{ productExport.salesChannelDomain.url }}</link>
-    <language>{{ productExport.salesChannelDomain.language.locale.code }}</language>
-    <image>
-        <url>{# add your logo URL #}</url>
-        <title>{{ context.salesChannel.name }}</title>
-        <link>{{ productExport.salesChannelDomain.url }}</link>
-    </image>
-        `.trim(),
-    bodyTemplate: `    <item> 
-        <g:id>{{ product.productNumber }}</g:id>
-        <title>{{ product.translated.name|escape }}</title>
-        <description>{{ product.translated.description|escape }}</description>
-        <g:google_product_category>950{# change your Google Shopping category #}</g:google_product_category>
-        <g:product_type>{{ product.categories.first.getBreadCrumb|slice(1)|join(\' > \')|raw|escape }}</g:product_type>
-        <link>{{ seoUrl(\'frontend.detail.page\', {\'productId\': product.id}) }}</link>
-        <g:image_link>{{ product.cover.media.url }}</g:image_link>
-        <g:condition>neu</g:condition>
-        <g:availability>{% if product.availableStock >= product.minPurchase and product.deliveryTime %}bestellbar{% elseif product.availableStock < product.minPurchase and product.deliveryTime and product.restockTime %}vorbestellt{% else %}nicht auf lager{% endif %}</g:availability>
-        <g:price>{{ product.calculatedListingPrice.from.unitPrice|currency }}</g:price>
-        <g:brand>{{ product.manufacturer.translated.name|escape }}</g:brand>
-        <g:gtin>{{ product.ean }}</g:gtin>
-        <g:mpn>{{ product.manufacturerNumber }}</g:mpn>
-        <g:shipping>
-            <g:country>DE</g:country>
-            <g:service>Standard</g:service>
-            <g:price>{{ 4.95|currency }}{# change your default delivery costs #}</g:price>
-        </g:shipping>
-        {% if product.updatedAt %}<pubDate>{{ product.updatedAt|date("%a, %d %b %Y %T %Z") }}</pubDate>{% endif %}
-    </item>`,
-    footerTemplate: `
-</channel>
-</rss>
-    `.trim(),
+    headerTemplate: header.trim(),
+    bodyTemplate: body,
+    footerTemplate: footer.trim(),
     fileName: 'google.xml',
     encoding: 'UTF-8',
     fileFormat: 'xml',
--- src/Administration/Resources/app/administration/src/module/sw-sales-channel/product-export-templates/idealo/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/product-export-templates/idealo/index.js
@@ -1,52 +1,11 @@
-/* eslint-disable max-len */
+import header from './header.csv.twig';
+import body from './body.csv.twig';
+
 Shopware.Service('exportTemplateService').registerProductExportTemplate({
     name: 'idealo',
     translationKey: 'sw-sales-channel.detail.productComparison.templates.template-label.idealo',
-    headerTemplate: `
-"categoryPath",{#- -#}
-"brand",{#- -#}
-"title",{#- -#}
-"price",{#- -#}
-"basePrice",{#- -#}
-"hans",{#- -#}
-"eans",{#- -#}
-"deliveryCosts",{#- -#}
-"url",{#- -#}
-"delivery",{#- -#}
-"sku",{#- -#}
-"imageUrls",{#- -#}
-"description",{#- -#}
-"paymentCosts_CashInAdvance",{#- Change or add your payment methods -#}
-"paymentCosts_CashOnDelivery",{#- Change or add your payment methods -#}
-"paymentCosts_Invoice"{#- Change or add your payment methods -#}
-        `.trim(),
-    bodyTemplate: `
-"{{ product.categories.first.getBreadCrumb|slice(1)|join(\' > \')|raw }}",{#- -#}
-"{{ product.manufacturer.translated.name }}",{#- -#}
-"{{ product.translated.name }}",{#- -#}
-"{{ product.calculatedListingPrice.from.unitPrice|currency }}",{#- -#}
-{% set price = product.calculatedListingPrice.from %}
-"{% if price.referencePrice is not null %}
-{{ price.referencePrice.price|currency }} / {{ price.referencePrice.referenceUnit }} {{ price.referencePrice.unitName }}{#- -#}
-{% endif %}",{#- -#}
-"{{ product.manufacturerNumber }}", {#- -#}
-"{{ product.ean }}",{#- -#}
-"{{ 4.95|currency }}",{#- Change to your delivery costs -#}
-"{{ seoUrl(\'frontend.detail.page\', {\'productId\': product.id}) }}",{#- -#}
-"{% if product.availableStock >= product.minPurchase and product.deliveryTime %}
-{{ "detail.deliveryTimeAvailable"|trans({\'%name%\': product.deliveryTime.translation(\'name\')}) }}{#- -#}
-{% elseif product.availableStock < product.minPurchase and product.deliveryTime and product.restockTime %}
-{{ "detail.deliveryTimeRestock"|trans({\'%restockTime%\': product.restockTime,\'%name%\': product.deliveryTime.translation(\'name\')}) }}{#- -#}
-{% else %}
-{{ "detail.soldOut"|trans }}{#- -#}
-{% endif %}",{#- -#}
-"{{ product.productNumber }}",{#- -#}
-"{{ product.cover.media.url }}",{#- -#}
-"{{ product.translated.description|raw|length > 300 ? product.translated.description|raw|slice(0,300) ~ \'...\' : product.translated.description|raw }}",{#- -#}
-"0.00",{#- Change or add your payment methods -#}
-"0.00",{#- Change or add your payment methods -#}
-"0.00"{#- Change or add your payment methods -#}
-        `.trim(),
+    headerTemplate: header.trim(),
+    bodyTemplate: body.trim(),
     footerTemplate: '',
     fileName: 'idealo.csv',
     encoding: 'UTF-8',

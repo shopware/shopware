686ade8761192e503e557e3fbaae562ef645d306
Patrick Weyck <p.weyck@shopware.com>
2019-12-11 16:57:41 +0100

    NTR - Fix docs job

--- src/Docs/Command/DocsPlatformUpdates.php
+++ src/Docs/Command/DocsPlatformUpdates.php
@@ -18,8 +18,8 @@ class DocsPlatformUpdates extends Command
 [__RAW__]: <>(__RAW__)
 
 <p class="alert is--warning" Recent information about technical updates and news regarding <a href="https://github.com/shopware/platform">shopware platform</a>
-    are now located in the changelog and upgrade file of the respective version in the github repository. For example <a href="https://github.com/shopware/platform/blob/6.1/CHANGELOG-6.0.md">CHANGELOG-6.0.md</a> and 
-    <a href="https://github.com/shopware/platform/blob/6.1/UPGRADE-6.1.md">UPGRADE-6.1.md</a>
+    are now located in the changelog and upgrade file of the respective version in the github repository. For example <a href="https://github.com/shopware/platform/blob/6.1/CHANGELOG-6.0.md">CHANGELOG-6.0.md</a> 
+    and <a href="https://github.com/shopware/platform/blob/6.1/UPGRADE-6.1.md">UPGRADE-6.1.md</a>
 </p>
 
 <p><strong>Our public admin component library for easy scaffolding of your admin modules</strong></p>

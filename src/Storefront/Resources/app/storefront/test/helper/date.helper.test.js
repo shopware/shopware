5df7a0c8fc7a3b08c097a4d8757875a65f16ee48
Sebastian Franze <s.franze@shopware.com>
2020-01-03 09:12:49 +0100

    NEXT-6088 - Test date.helper

---
+++ src/Storefront/Resources/app/storefront/test/helper/date.helper.test.js
@@ -0,0 +1,26 @@
+import DateHelper from 'src/helper/date.helper';
+
+const defaultFormatterOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+};
+
+describe('date.helper.js', () => {
+    test('it returns an empty string for non numbers', () => {
+        expect(DateHelper.format(null)).toStrictEqual('');
+        expect(DateHelper.format(1/0)).toStrictEqual('');
+        expect(DateHelper.format()).toStrictEqual('');
+        expect(DateHelper.format('parse me')).toStrictEqual('');
+
+    });
+
+    test('it returns formatted date from date object', () => {
+        const date = new Date(2020, 1, 3, 9, 0, 0);
+        const expectedDate = Intl.DateTimeFormat(navigator.language, defaultFormatterOptions).format(date);
+
+        expect(DateHelper.format(date.getTime())).toBe(expectedDate);
+        expect(DateHelper.format(date)).toBe(expectedDate);
+        expect(DateHelper.format(expectedDate)).toBe(expectedDate);
+    });
+});

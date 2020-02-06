294d02f9dfdb0b62030fddd726e0ac68fcb0f456
Soner Sayakci <s.sayakci@shopware.com>
2020-02-06 10:21:19 +0100

    NEXT-6646 - Fix template warmup

---
+++ src/Core/Framework/Adapter/Twig/TemplateIterator.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Adapter\Twig;
+
+use Shopware\Core\Framework\Bundle;
+use Symfony\Bundle\TwigBundle\TemplateIterator as TwigBundleIterator;
+
+class TemplateIterator implements \IteratorAggregate
+{
+    /**
+     * @var TwigBundleIterator
+     */
+    private $templateIterator;
+
+    /**
+     * @var array
+     */
+    private $kernelBundles;
+
+    public function __construct(TwigBundleIterator $templateIterator, array $kernelBundles)
+    {
+        $this->templateIterator = $templateIterator;
+        $this->kernelBundles = $kernelBundles;
+    }
+
+    public function getIterator(): iterable
+    {
+        $data = iterator_to_array($this->templateIterator, false);
+        $search = [];
+        $replace = [];
+
+        foreach ($this->kernelBundles as $bundleName => $bundle) {
+            $parents = class_parents($bundle);
+
+            if (!isset($parents[Bundle::class])) {
+                continue;
+            }
+
+            $search[] = sprintf('@%s/', $bundleName);
+            $replace[] = '';
+        }
+
+        foreach ($data as &$template) {
+            yield str_replace($search, $replace, $template);
+        }
+        unset($template);
+    }
+}

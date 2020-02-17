319b4d6e690c5a31ab3baacfe9721184ed49bb0c
Ramona Schwering <R.Schwering@shopware.com>
2020-02-17 09:12:49 +0100

    NTR - Use 1.2.0-beta e2e package

--- src/Administration/Resources/app/administration/test/e2e/cypress/plugins/index.js
+++ src/Administration/Resources/app/administration/test/e2e/cypress/plugins/index.js
@@ -14,12 +14,4 @@
 // The e2e-testsuite-pllatform version is not compatible with 4.0.0 yet so we comment it out for now
 // module.exports = require('@shopware-ag/e2e-testsuite-platform/cypress/plugins');
 
-require('@babel/register');
-const selectTestsWithGrep = require('cypress-select-tests/grep');
-const semver = require('semver');
-
-module.exports = (on, config) => {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
-    on('file:preprocessor', selectTestsWithGrep(config));
-};
+module.exports = require('@shopware-ag/e2e-testsuite-platform/cypress/plugins');

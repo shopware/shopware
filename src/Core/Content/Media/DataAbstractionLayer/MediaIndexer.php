e5b10981095e8571a4e07d99c475325d80b59095
Oliver Skroblin <o.skroblin@shopware.com>
2020-03-09 12:35:18 +0100

    NEXT-6133 - Refactor media thumbnail indexer

---
+++ src/Core/Content/Media/DataAbstractionLayer/MediaIndexer.php
@@ -0,0 +1,106 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Media\DataAbstractionLayer;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Content\Media\MediaCollection;
+use Shopware\Core\Content\Media\MediaDefinition;
+use Shopware\Core\Framework\Adapter\Cache\CacheClearer;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\IteratorFactory;
+use Shopware\Core\Framework\DataAbstractionLayer\Doctrine\RetryableQuery;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\Indexing\EntityIndexerInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Indexing\EntityIndexingMessage;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+class MediaIndexer implements EntityIndexerInterface
+{
+    /**
+     * @var IteratorFactory
+     */
+    private $iteratorFactory;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var CacheClearer
+     */
+    private $cacheClearer;
+
+    public function __construct(IteratorFactory $iteratorFactory, EntityRepositoryInterface $repository, Connection $connection, CacheClearer $cacheClearer)
+    {
+        $this->iteratorFactory = $iteratorFactory;
+        $this->repository = $repository;
+        $this->connection = $connection;
+        $this->cacheClearer = $cacheClearer;
+    }
+
+    public function getName(): string
+    {
+        return 'media.indexer';
+    }
+
+    public function iterate($offset): ?EntityIndexingMessage
+    {
+        $iterator = $this->iteratorFactory->createIterator($this->repository->getDefinition(), $offset);
+
+        $ids = $iterator->fetch();
+
+        if (empty($ids)) {
+            return null;
+        }
+
+        return new EntityIndexingMessage($ids, $iterator->getOffset());
+    }
+
+    public function update(EntityWrittenContainerEvent $event): ?EntityIndexingMessage
+    {
+        $updates = $event->getPrimaryKeys(MediaDefinition::ENTITY_NAME);
+
+        if (empty($updates)) {
+            return null;
+        }
+
+        return new EntityIndexingMessage($updates, null);
+    }
+
+    public function handle(EntityIndexingMessage $message): void
+    {
+        $ids = $message->getData();
+
+        if (empty($ids)) {
+            return;
+        }
+
+        $criteria = new Criteria($ids);
+        $criteria->addAssociation('thumbnails');
+
+        $context = Context::createDefaultContext();
+
+        $query = new RetryableQuery(
+            $this->connection->prepare('UPDATE `media` SET thumbnails_ro = :thumbnails_ro WHERE id = :id')
+        );
+
+        /** @var MediaCollection $medias */
+        $medias = $this->repository->search($criteria, $context);
+        foreach ($medias as $media) {
+            $query->execute([
+                'thumbnails_ro' => serialize($media->getThumbnails()),
+                'id' => Uuid::fromHexToBytes($media->getId()),
+            ]);
+        }
+
+        $this->cacheClearer->invalidateIds($ids, MediaDefinition::ENTITY_NAME);
+    }
+}

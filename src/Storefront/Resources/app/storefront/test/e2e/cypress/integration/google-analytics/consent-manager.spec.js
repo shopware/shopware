fac73ff8a26f75950d731baa687136c5fe1c1c0d
Ramona Schwering <R.Schwering@shopware.com>
2020-03-16 10:29:31 +0100

    NTR - Remove loop in google analytics e2e test

--- src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/google-analytics/consent-manager.spec.js
+++ src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/google-analytics/consent-manager.spec.js
@@ -1,51 +1,49 @@
 describe('Google Analytics: New analytics cookie in Cookie Consent Manager', () => {
-    for (let i = 0; i < 20; i++) {
-        it('@cookies: There is a new analytics cookie in the manager', () => {
-            cy.visit('/');
+    it('@cookies: There is a new analytics cookie in the manager', () => {
+        cy.visit('/');
 
-            cy.window().then((win) => {
-                win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
-                cy.get('.offcanvas-cookie').should('be.visible').then(() => {
-                    cy.get('.offcanvas-cookie').find('.offcanvas-cookie-group').should('have.length', 2);
-                    cy.get('.offcanvas-cookie-group').eq(1).contains('Statistic');
+        cy.window().then((win) => {
+            win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
+            cy.get('.offcanvas-cookie').should('be.visible').then(() => {
+                cy.get('.offcanvas-cookie').find('.offcanvas-cookie-group').should('have.length', 2);
+                cy.get('.offcanvas-cookie-group').eq(1).contains('Statistic');
 
-                    cy.get('.offcanvas-cookie-group').eq(1).find('.offcanvas-cookie-entry').contains('Google Analytics');
-                });
+                cy.get('.offcanvas-cookie-group').eq(1).find('.offcanvas-cookie-entry').contains('Google Analytics');
             });
         });
+    });
 
-        it('@cookies: Google Analytics cookies will be set and removed again', () => {
-            cy.addAnalyticsFixtureToSalesChannel();
-            cy.server();
-            cy.route({
-                url: '/cookie/offcanvas',
-                method: 'get'
-            }).as('cookieOffcanvas');
+    it('@cookies: Google Analytics cookies will be set and removed again', () => {
+        cy.addAnalyticsFixtureToSalesChannel();
+        cy.server();
+        cy.route({
+            url: '/cookie/offcanvas',
+            method: 'get'
+        }).as('cookieOffcanvas');
 
-            cy.visit('/');
-            cy.window().then((win) => {
-                cy.getCookie('_swag_ga_ga').should('be.null');
+        cy.visit('/');
+        cy.window().then((win) => {
+            cy.getCookie('_swag_ga_ga').should('be.null');
 
-                cy.wait('@cookieOffcanvas').then((xhr) => {
-                    expect(xhr).to.have.property('status', 200);
-                    cy.get('.offcanvas-cookie').should('be.visible');
-                    cy.get('.offcanvas-cookie-group').eq(1).find('.custom-control-label').first().click();
-                    cy.get('.js-offcanvas-cookie-submit').click();
+            cy.wait('@cookieOffcanvas').then((xhr) => {
+                expect(xhr).to.have.property('status', 200);
+                cy.get('.offcanvas-cookie').should('be.visible');
+                cy.get('.offcanvas-cookie-group').eq(1).find('.custom-control-label').first().click();
+                cy.get('.js-offcanvas-cookie-submit').click();
 
-                    cy.waitUntil(() => cy.getCookie('_swag_ga_ga').then(cookie => cookie && cookie.value !== null));
-                    cy.get('.offcanvas-cookie').should('not.exist').then(() => {
-                        win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
-                        cy.get('.offcanvas-cookie').should('be.visible').then(() => {
-                            cy.get('.offcanvas-cookie-group').eq(1).find('.custom-control-label').first().click();
-                            cy.get('.js-offcanvas-cookie-submit').click();
+                cy.waitUntil(() => cy.getCookie('_swag_ga_ga').then(cookie => cookie && cookie.value !== null));
+                cy.get('.offcanvas-cookie').should('not.exist').then(() => {
+                    win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
+                    cy.get('.offcanvas-cookie').should('be.visible').then(() => {
+                        cy.get('.offcanvas-cookie-group').eq(1).find('.custom-control-label').first().click();
+                        cy.get('.js-offcanvas-cookie-submit').click();
 
-                            cy.waitUntil(() => cy.getCookie('_swag_ga_ga').then(cookie => !cookie || cookie.value === null));
-                        });
+                        cy.waitUntil(() => cy.getCookie('_swag_ga_ga').then(cookie => !cookie || cookie.value === null));
                     });
                 });
-
-                win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
             });
+
+            win.PluginManager.getPluginInstances('CookieConfiguration')[0].openOffCanvas();
         });
-    }
+    });
 });

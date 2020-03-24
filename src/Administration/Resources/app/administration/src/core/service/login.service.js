63ff2b8f9aeda4f0af7d42313d80c774b6636b1a
Jannis Leifeld <j.leifeld@shopware.com>
2020-03-24 16:05:51 +0100

    NEXT-7554 - add unit tests for login service

--- src/Administration/Resources/app/administration/src/core/service/login.service.js
+++ src/Administration/Resources/app/administration/src/core/service/login.service.js
@@ -38,6 +38,7 @@ export default function createLoginService(httpClient, context, bearerAuth = nul
         addOnLogoutListener,
         addOnLoginListener,
         getLocalStorageKey,
+        getStorageKey,
         notifyOnLoginListener
     };
 

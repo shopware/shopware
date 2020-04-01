40f474841184a2465552ea24d207bac9104079a7
Tobias Berge <t.berge@shopware.com>
2020-04-01 09:03:51 +0200

    NEXT-6751 - Add delete profile E2E test

--- src/Administration/Resources/app/administration/test/e2e/cypress/integration/settings/sw-settings-import-export/crud-profile.spec.js
+++ src/Administration/Resources/app/administration/test/e2e/cypress/integration/settings/sw-settings-import-export/crud-profile.spec.js
@@ -26,7 +26,7 @@ describe('Import/Export - Profiles: Test crud operations', () => {
             method: 'post'
         }).as('saveData');
 
-        cy.get('.sw-import-export-view-profiles__toolbar').should('be.visible');
+        cy.get('.sw-import-export-view-profiles__listing').should('be.visible');
 
         // Perform create new profile action
         cy.get('.sw-import-export-view-profiles__create-action').click();
@@ -45,10 +45,10 @@ describe('Import/Export - Profiles: Test crud operations', () => {
         cy.get('.sw-import-export-edit-profile-modal-mapping__add-action').click();
 
         // Expect a new grid row to be visible
-        cy.get('.sw-data-grid__row--0').should('be.visible');
+        cy.get(`${page.elements.dataGridRow}--0`).should('be.visible');
 
         // Fill in mapping
-        cy.get('.sw-data-grid__row--0 [name="mappedKey-0"]').type('id');
+        cy.get(`${page.elements.dataGridRow}--0 [name="mappedKey-0"]`).type('id');
         cy.get('.sw-import-export-entity-path-select')
             .typeSingleSelectAndCheck('id', '.sw-import-export-entity-path-select');
 
@@ -64,7 +64,7 @@ describe('Import/Export - Profiles: Test crud operations', () => {
 
         // Verify that created profile is inside profile listing
         cy.get('.sw-import-export-view-profiles__search input[type="text"]').type('Basic');
-        cy.get('.sw-data-grid__row--0').should('contain', 'Basic');
+        cy.get(`${page.elements.dataGridRow}--0`).should('contain', 'Basic');
     });
 
     it('@settings: Update and read profile', () => {
@@ -74,19 +74,24 @@ describe('Import/Export - Profiles: Test crud operations', () => {
             method: 'patch'
         }).as('saveData');
 
-        cy.get('.sw-import-export-view-profiles__search input[type="text"]').type('E2E');
+        cy.get('.sw-import-export-view-profiles__listing').should('be.visible');
+
+        // Search for given profile
+        cy.get('.sw-import-export-view-profiles__search input[type="text"]').typeAndCheck('E2E');
         cy.get(`${page.elements.dataGridRow}--0`).should('contain', 'E2E');
 
+        // Perform open action
         cy.clickContextMenuItem(
             '.sw-import-export-view-profiles__listing-open-action',
             page.elements.contextMenuButton,
             `${page.elements.dataGridRow}--0`
         );
 
+        // Wait for detail modal
         cy.get('.sw-import-export-edit-profile-modal').should('be.visible');
 
+        // Update the profile with a new name
         cy.get('[name="sw-field--profile-name"]').clearTypeAndCheck('Extended');
-
         cy.get('.sw-import-export-edit-profile-modal__save-action').click();
 
         // Save request should be successful
@@ -95,5 +100,47 @@ describe('Import/Export - Profiles: Test crud operations', () => {
         });
 
         cy.get('.sw-import-export-edit-profile-modal').should('not.be.visible');
+
+        // Verify updated profile is in listing
+        cy.get('.sw-import-export-view-profiles__search input[type="text"]').clearTypeAndCheck('Extended');
+        cy.get(`${page.elements.dataGridRow}--0`).should('contain', 'Extended');
+    });
+
+    it('@settings: Delete profile', () => {
+        cy.server();
+        cy.route({
+            url: '/api/v1/import-export-profile/*',
+            method: 'delete'
+        }).as('deleteData');
+
+        cy.get('.sw-import-export-view-profiles__listing').should('be.visible');
+
+        // Search for given profile
+        cy.get('.sw-import-export-view-profiles__search input[type="text"]').typeAndCheck('E2E');
+        cy.get(`${page.elements.dataGridRow}--0`).should('contain', 'E2E');
+
+        // Perform delete action
+        cy.clickContextMenuItem(
+            '.sw-import-export-view-profiles__listing-delete-action',
+            page.elements.contextMenuButton,
+            `${page.elements.dataGridRow}--0`
+        );
+
+        // Wait for delete confirmation modal
+        cy.get('.sw-modal__dialog').should('be.visible');
+        cy.get('.sw-modal__dialog .sw-listing__confirm-delete-text')
+            .should('contain', 'Are you sure you want to delete this item?');
+
+        // Confirm deletion
+        cy.get('.sw-modal__dialog .sw-button--primary').click();
+
+        // Delete request should be successful
+        cy.wait('@deleteData').then((xhr) => {
+            expect(xhr).to.have.property('status', 204);
+        });
+
+        // Verify deleted item is not present
+        cy.get('.sw-import-export-view-profiles__search input[type="text"]').clearTypeAndCheck('E2E');
+        cy.get('.sw-import-export-view-profiles__listing .sw-data-grid__body').should('not.contain', 'E2E');
     });
 });

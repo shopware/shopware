fd1fda5edb10938bfa9ef110a31f70e53f80849a
Ramona Schwering <R.Schwering@shopware.com>
2020-04-15 10:54:51 +0200

    NEXT-7619 - Make e2e route versions more abstract

---
+++ src/Administration/Resources/app/administration/test/e2e/cypress/support/commands/commands.js
@@ -0,0 +1,27 @@
+
+/**
+ * Types in the global search field and verify search terms in url
+ * @memberOf Cypress.Chainable#
+ * @name typeAndCheckSearchField
+ * @function
+ * @param {String} value - The value to type
+ */
+Cypress.Commands.add('typeAndCheckSearchField', {
+    prevSubject: 'element'
+}, (subject, value) => {
+
+    // Request we want to wait for later
+    cy.server();
+    cy.route({
+        url: '/api/**/search/**',
+        method: 'post'
+    }).as('searchResultCall');
+
+    cy.wrap(subject).type(value).should('have.value', value);
+
+    cy.wait('@searchResultCall').then((xhr) => {
+        expect(xhr).to.have.property('status', 200);
+
+        cy.url().should('include', encodeURI(value));
+    });
+});

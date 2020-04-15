0c829d253733a85b4cab40b69736976e52cf69bf
Patrick Weyck <p.weyck@shopware.com>
2020-04-15 14:35:37 +0200

    NEXT-5038 - Refactor shipping trigger

--- src/Core/Migration/Migration1580218617RefactorShippingMethodPrice.php
+++ src/Core/Migration/Migration1580218617RefactorShippingMethodPrice.php
@@ -42,23 +42,25 @@ class Migration1580218617RefactorShippingMethodPrice extends MigrationStep
             FOR EACH ROW
             BEGIN
                 IF NEW.price != OLD.price AND (NEW.currency_price = OLD.currency_price OR (NEW.currency_price IS NULL AND OLD.currency_price IS NULL)) THEN
-                    SET NEW.currency_price = `{
-                        CONCAT("c", HEX(NEW.currency_id)): {
-                            "currencyId": HEX(NEW.currency_id),
-                            "net": NEW.price,
-                            "gross": NEW.price,
-                            "linked": false
-                        }
-                    }`;
-                ELSEIF NEW.price = OLD.price AND NEW.currency_id = OLD.currency_id AND NEW.currency_price != OLD.currency_price THEN
-                    SET NEW.price = JSON_EXTRACT(
-                        JSON_EXTRACT(NEW.currency_price, "$.*.gross"),
-                        "$[0]"
+                    SET NEW.currency_price = JSON_OBJECT(
+                        CONCAT("c", LOWER(HEX(NEW.currency_id))),
+                        JSON_OBJECT(
+                            "net", NEW.price,
+                            "gross", NEW.price,
+                            "linked", false,
+                            "currencyId", LOWER(HEX(NEW.currency_id))
+                        )
                     );
-                    SET NEW.currency_id = UNHEX(JSON_EXTRACT(
-                        JSON_EXTRACT(NEW.currency_price, "$.*.currencyId"),
-                        "$[0]"
-                    ));
+                ELSEIF NEW.price = OLD.price AND NEW.currency_id = OLD.currency_id AND NEW.currency_price != OLD.currency_price THEN
+                    SET NEW.price = JSON_UNQUOTE(JSON_EXTRACT(
+                        NEW.currency_price,
+                        CONCAT("$.", JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(NEW.currency_price), "$[0]")), ".gross")
+                    )) + 0.0;
+
+                    SET NEW.currency_id = UNHEX(JSON_UNQUOTE(JSON_EXTRACT(
+                        NEW.currency_price,
+                        CONCAT("$.", JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(NEW.currency_price), "$[0]")), ".currencyId")
+                    )));
                 END IF;
             END;';
 
@@ -72,23 +74,25 @@ class Migration1580218617RefactorShippingMethodPrice extends MigrationStep
             FOR EACH ROW
             BEGIN
                 IF NEW.price IS NOT NULL AND NEW.currency_id IS NOT NULL AND NEW.currency_price IS NULL THEN
-                    SET NEW.currency_price = `{
-                        CONCAT("c", NEW.currency_id): {
-                            "currencyId": NEW.currency_id,
-                            "net": NEW.price,
-                            "gross": NEW.price,
-                            "linked": false
-                        }
-                    }`;
+                    SET NEW.currency_price = JSON_OBJECT(
+                        CONCAT("c", LOWER(HEX(NEW.currency_id))),
+                        JSON_OBJECT(
+                            "net", NEW.price,
+                            "gross", NEW.price,
+                            "linked", false,
+                            "currencyId", LOWER(HEX(NEW.currency_id))
+                        )
+                    );
                 ELSEIF NEW.price IS NULL AND NEW.currency_id IS NULL AND NEW.currency_price IS NOT NULL THEN
-                    SET NEW.price = JSON_EXTRACT(
-                        JSON_EXTRACT(NEW.currency_price, "$.*.gross"),
-                        "$[0]"
-                    );
-                    SET NEW.currency_id = JSON_EXTRACT(
-                        JSON_EXTRACT(NEW.currency_price, "$.*.currencyId"),
-                        "$[0]"
-                    );
+                    SET NEW.price = JSON_UNQUOTE(JSON_EXTRACT(
+                        NEW.currency_price,
+                        CONCAT("$.", JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(NEW.currency_price), "$[0]")), ".gross")
+                    )) + 0.0;
+
+                    SET NEW.currency_id = UNHEX(JSON_UNQUOTE(JSON_EXTRACT(
+                        NEW.currency_price,
+                        CONCAT("$.", JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(NEW.currency_price), "$[0]")), ".currencyId")
+                    )));
                 END IF;
             END;';
 

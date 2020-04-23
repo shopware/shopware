d2eeb0b1839fe5b738e461505cd220ffd45be982
Oliver Skroblin <o.skroblin@shopware.com>
2020-04-23 09:06:39 +0200

    NEXT-8200 - Call request criteria builder in product routes

--- src/Core/Content/Product/SalesChannel/Listing/ProductListingRouteResponse.php
+++ src/Core/Content/Product/SalesChannel/Listing/ProductListingRouteResponse.php
@@ -2,22 +2,21 @@
 
 namespace Shopware\Core\Content\Product\SalesChannel\Listing;
 
-use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 use Shopware\Core\System\SalesChannel\StoreApiResponse;
 
 class ProductListingRouteResponse extends StoreApiResponse
 {
     /**
-     * @var EntitySearchResult
+     * @var ProductListingResult
      */
     protected $object;
 
-    public function __construct(EntitySearchResult $object)
+    public function __construct(ProductListingResult $object)
     {
         parent::__construct($object);
     }
 
-    public function getResult(): EntitySearchResult
+    public function getResult(): ProductListingResult
     {
         return $this->object;
     }

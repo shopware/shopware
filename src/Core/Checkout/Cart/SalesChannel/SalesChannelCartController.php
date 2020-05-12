8c49c9874c3fcdf71d5272e3eeaab6476091e761
Soner Sayakci <s.sayakci@shopware.com>
2020-05-12 08:49:10 +0200

    NEXT-8551 - Allow only price definitions with permission

--- src/Core/Checkout/Cart/SalesChannel/SalesChannelCartController.php
+++ src/Core/Checkout/Cart/SalesChannel/SalesChannelCartController.php
@@ -225,7 +225,7 @@ class SalesChannelCartController extends AbstractController
         }
 
         $lineItem = new LineItem($id, $type, null, $quantity);
-        $this->updateLineItemByRequest($lineItem, $requestDataBag, $context->getContext());
+        $this->updateLineItemByRequest($lineItem, $requestDataBag, $context);
 
         $cart = $this->cartService->add($this->cartService->getCart($token, $context), $lineItem, $context);
 
@@ -274,7 +274,7 @@ class SalesChannelCartController extends AbstractController
 
         $lineItem = $this->cartService->getCart($token, $context)->getLineItems()->get($id);
 
-        $this->updateLineItemByRequest($lineItem, $requestDataBag, $context->getContext());
+        $this->updateLineItemByRequest($lineItem, $requestDataBag, $context);
 
         $cart = $this->cartService->recalculate($cart, $context);
 
@@ -330,7 +330,7 @@ class SalesChannelCartController extends AbstractController
      * @throws LineItemNotStackableException
      * @throws InvalidPayloadException
      */
-    private function updateLineItemByRequest(LineItem $lineItem, RequestDataBag $requestDataBag, Context $context): void
+    private function updateLineItemByRequest(LineItem $lineItem, RequestDataBag $requestDataBag, SalesChannelContext $context): void
     {
         $payload = $requestDataBag->get('payload', []);
         $stackable = $requestDataBag->get('stackable');
@@ -363,7 +363,7 @@ class SalesChannelCartController extends AbstractController
         }
 
         if ($coverId !== null) {
-            $cover = $this->mediaRepository->search(new Criteria([$coverId]), $context)->get($coverId);
+            $cover = $this->mediaRepository->search(new Criteria([$coverId]), $context->getContext())->get($coverId);
 
             if (!$cover) {
                 throw new LineItemCoverNotFoundException($coverId, $lineItem->getId());
@@ -372,9 +372,9 @@ class SalesChannelCartController extends AbstractController
             $lineItem->setCover($cover);
         }
 
-        if ($requestDataBag->get('priceDefinition') !== null) {
+        if ($requestDataBag->get('priceDefinition') !== null && array_key_exists(ProductCartProcessor::ALLOW_PRODUCT_PRICE_OVERWRITES, $context->getPermissions())) {
             $priceDefinition = $requestDataBag->get('priceDefinition')->all();
-            $priceDefinitionType = $this->initPriceDefinition($context, $priceDefinition, $lineItem->getType());
+            $priceDefinitionType = $this->initPriceDefinition($context->getContext(), $priceDefinition, $lineItem->getType());
             $lineItem->setPriceDefinition($priceDefinitionType);
 
             if ($lineItem->getType() === LineItem::PRODUCT_LINE_ITEM_TYPE) {

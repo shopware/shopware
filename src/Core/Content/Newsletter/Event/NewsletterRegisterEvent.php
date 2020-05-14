23f8457c2759800e43b56bfc81ef3a6b55e64763
Alexander Schmidt <support@kiplingi.de>
2020-05-14 14:10:23 +0200

    NEXT-8828 - Remove required newsletter form fields and make fallback for email recipient

--- src/Core/Content/Newsletter/Event/NewsletterRegisterEvent.php
+++ src/Core/Content/Newsletter/Event/NewsletterRegisterEvent.php
@@ -83,17 +83,15 @@ class NewsletterRegisterEvent extends Event implements MailActionInterface
             return $this->mailRecipientStruct;
         }
 
+        $recipientName = $this->newsletterRecipient->getEmail();
+
         if ($this->newsletterRecipient->getFirstName() && $this->newsletterRecipient->getLastName()) {
-            return new MailRecipientStruct(
-                [
-                    $this->newsletterRecipient->getEmail() => $this->newsletterRecipient->getFirstName() . ' ' . $this->newsletterRecipient->getLastName(),
-                ]
-            );
+            $recipientName =  $this->newsletterRecipient->getFirstName() . ' ' . $this->newsletterRecipient->getLastName();
         }
 
         return new MailRecipientStruct(
             [
-                $this->newsletterRecipient->getEmail() => $this->newsletterRecipient->getEmail()
+                $this->newsletterRecipient->getEmail() => $recipientName
             ]
         );
     }

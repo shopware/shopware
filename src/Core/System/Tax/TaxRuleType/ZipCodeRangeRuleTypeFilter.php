0342a88ea44d21e5b0339c68fd0a3e3c8617b7f1
acris-cp <35556204+acris-cp@users.noreply.github.com>
2020-05-27 12:40:16 +0200

    Update ZipCodeRangeRuleTypeFilter.php

--- src/Core/System/Tax/TaxRuleType/ZipCodeRangeRuleTypeFilter.php
+++ src/Core/System/Tax/TaxRuleType/ZipCodeRangeRuleTypeFilter.php
@@ -36,8 +36,8 @@ class ZipCodeRangeRuleTypeFilter implements TaxRuleTypeFilterInterface
             return false;
         }
 
-        if ($customer !== null && $customer->getActiveBillingAddress()) {
-            return $customer->getActiveBillingAddress()->getCountryId() === $taxRuleEntity->getCountryId();
+        if ($customer !== null && $customer->getActiveShippingAddress()) {
+            return $customer->getActiveShippingAddress()->getCountryId() === $taxRuleEntity->getCountryId();
         }
 
         return $shippingLocation->getCountry()->getId() === $taxRuleEntity->getCountryId();
@@ -45,8 +45,8 @@ class ZipCodeRangeRuleTypeFilter implements TaxRuleTypeFilterInterface
 
     private function getZipCode(?CustomerEntity $customer, ShippingLocation $shippingLocation): ?string
     {
-        if ($customer !== null && $customer->getActiveBillingAddress() !== null) {
-            return $customer->getActiveBillingAddress()->getZipcode();
+        if ($customer !== null && $customer->getActiveShippingAddress() !== null) {
+            return $customer->getActiveShippingAddress()->getZipcode();
         }
 
         $address = $shippingLocation->getAddress();

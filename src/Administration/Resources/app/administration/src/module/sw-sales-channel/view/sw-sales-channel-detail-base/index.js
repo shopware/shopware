e4ec8dd5f987b0c76ef2a8a0fd0fdfbcc746359e
Oliver Skroblin <o.skroblin@shopware.com>
2020-05-28 12:29:34 +0200

    NEXT-8302 - Added new block and description property to sw-radio-field. Added tax calculation type selection in sales channel detail page

--- src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-base/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-base/index.js
@@ -276,6 +276,23 @@ Component.register('sw-sales-channel-detail-base', {
             return null;
         },
 
+        taxCalculationTypeOptions() {
+            return [
+                {
+                    name: 'Column by column (vertical) calculation',
+                    value: 'vertical',
+                    helpText: 'The net sum per tax rate for the entire invoice is calculated then the VAT sum is calculated and added.',
+                    description: 'The net sum per tax rate for the entire invoice is calculated then the VAT sum is calculated and added.'
+                },
+                {
+                    name: 'Line by line (horizontal) calculation',
+                    value: 'horizontal',
+                    helpText: 'This will add the VAT rate to the individual unit price of the item, and then multiply the gross by the quantity',
+                    description: 'This will add the VAT rate to the individual unit price of the item, and then multiply the gross by the quantity'
+                }
+            ];
+        },
+
         maintenanceIpWhitelist: {
             get() {
                 return this.salesChannel.maintenanceIpWhitelist ? this.salesChannel.maintenanceIpWhitelist : [];

949bea10198ca14f00b6e33339e82ae321dd1cc3
Michael Telgmann <mt@shopware.com>
2020-07-07 09:01:56 +0200

    NEXT-1797 - Update to Composer 2.0 and execute composer commands during plugin install, uninstall and update

---
+++ src/Core/Framework/Plugin/Exception/PluginComposerRemoveException.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Plugin\Exception;
+
+use Shopware\Core\Framework\ShopwareHttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+class PluginComposerRemoveException extends ShopwareHttpException
+{
+    public function __construct(string $pluginName, string $pluginComposerName, string $output)
+    {
+        parent::__construct(
+            sprintf('Could not execute "composer remove" for plugin "{{ pluginName }} ({{ pluginComposerName }}). Output:%s{{ output }}', PHP_EOL),
+            [
+                'pluginName' => $pluginName,
+                'pluginComposerName' => $pluginComposerName,
+                'output' => $output,
+            ]
+        );
+    }
+
+    public function getErrorCode(): string
+    {
+        return 'FRAMEWORK__PLUGIN_COMPOSER_REMOVE';
+    }
+
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_BAD_REQUEST;
+    }
+}

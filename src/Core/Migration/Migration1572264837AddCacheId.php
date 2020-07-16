0f4eab88b0e4f1161a4013a5ed1bbc8253a8ce2e
Lennart Tinkloh <l.tinkloh@shopware.com>
2020-07-16 15:54:09 +0200

    NEXT-9750 - added changelog, imported DBALException in missing migration and set --sql-require-primary-key=ON on gitlab ci

--- src/Core/Migration/Migration1572264837AddCacheId.php
+++ src/Core/Migration/Migration1572264837AddCacheId.php
@@ -3,6 +3,7 @@
 namespace Shopware\Core\Migration;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
 use Shopware\Core\Framework\Migration\MigrationStep;
 use Shopware\Core\Framework\Uuid\Uuid;
 
@@ -16,11 +17,13 @@ class Migration1572264837AddCacheId extends MigrationStep
     public function update(Connection $connection): void
     {
         $connection->executeUpdate('DELETE FROM app_config');
+
         try {
             $connection->exec('ALTER TABLE app_config ADD PRIMARY KEY (`key`)');
         } catch (DBALException $e) {
             // PK already exists
         }
+
         $connection->executeUpdate(
             '
             INSERT IGNORE INTO app_config (`key`, `value`)

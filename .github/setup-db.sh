f0baee63d1c1fd9bd3a1a7419dfa5fa1e335cb37
Simon Kunz <simon.kunz@pixolith.de>
2020-07-31 10:03:02 +0200

    * enable ansi quotes for testing

--- .github/setup-db.sh
+++ .github/setup-db.sh
@@ -5,7 +5,7 @@ STARTPARAMS=""
 DB=$1
 
 if [[ $DB == "mysql:8.0.19" ]]; then
-    STARTPARAMS="mysqld --default-authentication-plugin=mysql_native_password"
+    STARTPARAMS="mysqld --default-authentication-plugin=mysql_native_password --ansi"
 fi
 
 docker run -it --mount type=tmpfs,destination=/var/lib/mysql --name=mysqld -d -e MYSQL_ROOT_PASSWORD=shopware -e MYSQL_DATABASE=shopware -p3306:3306 ${DB} ${STARTPARAMS}

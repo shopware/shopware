dfbc5b8a255ade698693183aaad6daade12aeed7
Jannis Leifeld <j.leifeld@shopware.com>
2020-08-20 11:31:12 +0200

    NEXT-9685 - inject feature service to failing unit tests

--- src/Administration/Resources/app/administration/test/module/sw-cms/elements/product-slider/config/sw-cms-el-config-product-slider.spec.js
+++ src/Administration/Resources/app/administration/test/module/sw-cms/elements/product-slider/config/sw-cms-el-config-product-slider.spec.js
@@ -58,6 +58,9 @@ function createWrapper(customCmsElementConfig) {
             $tc: (value) => value
         },
         provide: {
+            feature: {
+                isActive: () => true
+            },
             cmsService: {
                 getCmsBlockRegistry: () => {
                     return {};

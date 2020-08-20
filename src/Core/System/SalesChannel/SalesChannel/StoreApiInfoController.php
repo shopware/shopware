b82892376fa62c613ff53ea85d52ce47c8373aa9
Soner Sayakci <s.sayakci@shopware.com>
2020-08-20 09:43:22 +0200

    NEXT-10392 - Fix swagger documentation errors with CSP

--- src/Core/System/SalesChannel/SalesChannel/StoreApiInfoController.php
+++ src/Core/System/SalesChannel/SalesChannel/StoreApiInfoController.php
@@ -5,7 +5,9 @@ namespace Shopware\Core\System\SalesChannel\SalesChannel;
 use Shopware\Core\Framework\Api\ApiDefinition\DefinitionService;
 use Shopware\Core\Framework\Api\ApiDefinition\Generator\OpenApi3Generator;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\PlatformRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -25,10 +27,16 @@ class StoreApiInfoController
      */
     private $twig;
 
-    public function __construct(DefinitionService $definitionService, Environment $twig)
+    /**
+     * @var array
+     */
+    private $cspTemplates;
+
+    public function __construct(DefinitionService $definitionService, Environment $twig, array $cspTemplates)
     {
         $this->definitionService = $definitionService;
         $this->twig = $twig;
+        $this->cspTemplates = $cspTemplates;
     }
 
     /**
@@ -56,10 +64,26 @@ class StoreApiInfoController
     /**
      * @Route("/store-api/v{version}/_info/swagger.html", defaults={"auth_required"="%shopware.api.api_browser.auth_required_str%"}, name="store-api.info.swagger", methods={"GET"})
      */
-    public function infoHtml(int $version): Response
+    public function infoHtml(Request $request, int $version): Response
     {
-        $content = $this->twig->render('@Framework/swagger.html.twig', ['schemaUrl' => 'store-api.info.openapi3', 'apiVersion' => $version]);
+        $nonce = $request->attributes->get(PlatformRequest::ATTRIBUTE_CSP_NONCE);
+        $response = new Response($this->twig->render(
+            '@Framework/swagger.html.twig',
+            [
+                'schemaUrl' => 'store-api.info.openapi3',
+                'apiVersion' => $version,
+                'cspNonce' => $nonce,
+            ]
+        ));
 
-        return new Response($content);
+        $cspTemplate = $this->cspTemplates['administration'] ?? '';
+        $cspTemplate = trim($cspTemplate);
+        if ($cspTemplate !== '') {
+            $csp = str_replace('%nonce%', $nonce, $cspTemplate);
+            $csp = str_replace(["\n", "\r"], ' ', $csp);
+            $response->headers->set('Content-Security-Policy', $csp);
+        }
+
+        return $response;
     }
 }

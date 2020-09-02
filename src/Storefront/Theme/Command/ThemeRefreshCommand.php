90252a216a7ccae2d8415e1c20b933a47d2f7e11
Peter Jaap Blaakmeer <peterjaap@blaakmeer.com>
2020-09-02 12:36:36 +0200

    Output list of refreshed themes in theme:refresh command

--- src/Storefront/Theme/Command/ThemeRefreshCommand.php
+++ src/Storefront/Theme/Command/ThemeRefreshCommand.php
@@ -32,7 +32,9 @@ class ThemeRefreshCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->themeLifecycleService->refreshThemes($this->context);
+        $refreshedThemes = $this->themeLifecycleService->refreshThemes($this->context);
+        
+        $output->writeln('Refreshed themes: ' . implode(', ', $refreshedThemes));
 
         return 0;
     }

286147d52349216c66b5b7bf8333803d3ea48217
Felix Brucker <felix@felixbrucker.com>
2020-09-03 15:02:52 +0200

    NEXT-11468 - Fix struct encoding of extensions which are not loaded
    fixes https://github.com/shopware/platform/pull/1305

--- src/Core/System/SalesChannel/Api/StructEncoder.php
+++ src/Core/System/SalesChannel/Api/StructEncoder.php
@@ -211,7 +211,12 @@ class StructEncoder
                 continue;
             }
 
-            $value[$name] = $this->encode($struct->getExtension($name), $apiVersion, $fields);
+            $extension = $struct->getExtension($name);
+            if ($extension === null) {
+                continue;
+            }
+
+            $value[$name] = $this->encode($extension, $apiVersion, $fields);
         }
 
         return $value;

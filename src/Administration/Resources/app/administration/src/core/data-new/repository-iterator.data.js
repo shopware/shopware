49694f20a2f41b6ef7cf5a32370b50616197d8fd
Joshua Behrens <behrens@heptacom.de>
2020-09-06 15:53:21 +0200

    Add iterate and iterateId methods to repository iterators for easier usage

--- src/Administration/Resources/app/administration/src/core/data-new/repository-iterator.data.js
+++ src/Administration/Resources/app/administration/src/core/data-new/repository-iterator.data.js
@@ -1,5 +1,20 @@
 import Criteria from './criteria.data';
 
+/**
+ * Callback for iterating entities
+ *
+ * @callback iterateEntitiesCallback
+ * @param {Array<Object>} entities
+ * @param {Object} response
+ */
+
+/**
+ * Callback for iterating entities
+ *
+ * @callback iterateIdsCallback
+ * @param {Array<String>} ids
+ */
+
 export default class RepositoryIterator {
     /**
      * @param {Repository} repository
@@ -41,6 +56,45 @@ export default class RepositoryIterator {
     }
 
     /**
+     * @param {iterateIdsCallback} callback
+     * @returns {Promise<Array<String>>}
+     */
+    iterateIds(callback = null) {
+        const fetch = (result) => {
+            return this.fetch().then(response => {
+                if (response === null) {
+                    return Promise.resolve(result);
+                }
+
+                result.push(...response);
+
+                if (callback !== null) {
+                    callback(response);
+                }
+
+                return fetch(result);
+            });
+        };
+
+        return fetch([]);
+    }
+
+    async * iterateIdsAsync() {
+        const fetchIds = async () => this.fetchIds();
+
+        do {
+            // eslint-disable-next-line no-await-in-loop
+            const ids = await fetchIds();
+
+            if (ids === null) {
+                break;
+            }
+
+            yield* ids;
+        } while (true);
+    }
+
+    /**
      * @return {Promise<Object>}
      */
     fetch() {
@@ -49,5 +103,46 @@ export default class RepositoryIterator {
         this.criteria.setPage(this.criteria.page + 1);
         return this.repository.search(criteria, this.context)
             .then(response => (response.data.length > 0 ? response : null));
+    }
+
+    /**
+     * @param {iterateEntitiesCallback} callback
+     * @returns {Promise<Array<Object>>}
+     */
+    iterate(callback = null) {
+        const fetch = (result) => {
+            return this.fetch().then(response => {
+                if (response === null) {
+                    return Promise.resolve(result);
+                }
+
+                result.push(...response.data);
+                result.criteria = response.criteria;
+                result.total = response.total;
+
+                if (callback !== null) {
+                    callback(response.data, response);
+                }
+
+                return fetch(result);
+            });
+        };
+
+        return fetch([]);
+    }
+
+    async * iterateAsync() {
+        const fetch = async () => this.fetch();
+
+        do {
+            // eslint-disable-next-line no-await-in-loop
+            const items = await fetch();
+
+            if (items === null) {
+                break;
+            }
+
+            yield* items.data;
+        } while (true);
     }
 }

1b30b36c632d6df74e0417a866096966432043f4
Oliver Skroblin <o.skroblin@shopware.com>
2020-09-30 13:41:55 +0200

    NEXT-10488 - Update order mails. Added trait to unify mail template updates

---
+++ src/Core/Migration/Traits/UpdateMailTrait.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\Traits;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+trait UpdateMailTrait
+{
+    use ImportTranslationsTrait;
+
+    final protected function updateMail(MailUpdate $update, Connection $connection): void
+    {
+        $this->updateEnMail($connection, $update);
+
+        $this->updateDeMail($connection, $update);
+    }
+
+    final private function updateDeMail(Connection $connection, MailUpdate $update): void
+    {
+        $languages = $this->getLanguageIds($connection, 'de-DE');
+        if (!$languages) {
+            return;
+        }
+
+        $translations = $this->getTranslationIds($connection, $languages, $update->getType());
+        if (empty($translations)) {
+            return;
+        }
+
+        foreach ($translations as $translation) {
+            $connection->executeUpdate(
+                '
+                    UPDATE mail_template_translation
+                    SET content_html = :html, content_plain = :plain
+                    WHERE language_id = :language_id AND mail_template_id = :template',
+                [
+                    'language_id' => $translation['language_id'],
+                    'template' => $translation['mail_template_id'],
+                    'html' => $update->getDeHtml(),
+                    'plain' => $update->getDePlain(),
+                ]
+            );
+        }
+    }
+
+    final private function updateEnMail(Connection $connection, MailUpdate $update): void
+    {
+        $languages = array_merge([Defaults::LANGUAGE_SYSTEM], $this->getLanguageIds($connection, 'en-GB'));
+        $languages = array_unique(array_filter($languages));
+
+        if (empty($languages)) {
+            return;
+        }
+
+        $translations = $this->getTranslationIds($connection, $languages, $update->getType());
+        if (empty($translations)) {
+            return;
+        }
+
+        foreach ($translations as $translation) {
+            $connection->executeUpdate(
+                'UPDATE mail_template_translation
+                 SET content_html = :html, content_plain = :plain
+                 WHERE language_id = :language_id AND mail_template_id = :template',
+                [
+                    'language_id' => $translation['language_id'],
+                    'template' => $translation['mail_template_id'],
+                    'html' => $update->getEnHtml(),
+                    'plain' => $update->getEnPlain(),
+                ]
+            );
+        }
+    }
+
+    final private function getTranslationIds(Connection $connection, array $languageIds, string $type): array
+    {
+        return $connection->fetchAll(
+            '
+            SELECT mail_template_translation.mail_template_id, mail_template_translation.language_id
+
+            FROM mail_template
+
+                INNER JOIN mail_template_translation
+                    ON mail_template.id = mail_template_translation.mail_template_id
+
+                INNER JOIN mail_template_type
+                    ON mail_template.mail_template_type_id = mail_template_type.id
+
+            WHERE mail_template_translation.language_id IN (:ids)
+            AND mail_template_type.technical_name = :type
+            AND mail_template.system_default = 1
+            AND mail_template_translation.updated_at IS NULL
+            AND mail_template.updated_at IS NULL',
+            ['ids' => Uuid::fromHexToBytesList($languageIds), 'type' => $type],
+            ['ids' => Connection::PARAM_STR_ARRAY]
+        );
+    }
+}

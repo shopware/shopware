978ac2fc13e009d8a4c8af207f21a15cdcba5946
Hendrik Söbbing <hendrik@soebbing.de>
2020-10-05 21:30:56 +0200

    NEXT-11267 - Add missing CustomFieldTypes constants
    fixes https://github.com/shopware/platform/pull/1395
    
    Signed-off-by: Hendrik Söbbing <hendrik@soebbing.de>

--- src/Core/System/CustomField/CustomFieldTypes.php
+++ src/Core/System/CustomField/CustomFieldTypes.php
@@ -5,14 +5,16 @@ namespace Shopware\Core\System\CustomField;
 final class CustomFieldTypes
 {
     public const BOOL = 'bool';
+    public const COLORPICKER = 'colorpicker';
     public const DATETIME = 'datetime';
     public const FLOAT = 'float';
     public const INT = 'int';
     public const JSON = 'json';
-    public const TEXT = 'text';
     public const HTML = 'html';
+    public const MEDIA = 'media';
     public const SELECT = 'select';
     public const SWITCH = 'switch';
+    public const TEXT = 'text';
 
     private function __construct()
     {

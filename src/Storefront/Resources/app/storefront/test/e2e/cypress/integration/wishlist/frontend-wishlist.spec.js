f6a72a2a83fa3c7856ab05285b25edc12d62b00c
Y Tran <y.tran@shopware.com>
2020-10-09 16:50:33 +0700

    NEXT-11281 - Implement Wishlist list page

---
+++ src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/wishlist/frontend-wishlist.spec.js
@@ -0,0 +1,110 @@
+import AccountPageObject from "../../support/pages/account.page-object";
+
+const page = new AccountPageObject();
+
+const customer = {
+    firstName: 'Y',
+    lastName: 'Tran',
+    email: "tester@example.com",
+    password: "shopware"
+};
+
+const product = {
+    "id": "6dfd9dc216ab4ac99598b837ac600368",
+    "name": "Test product 1",
+    "stock": 1,
+    "productNumber": "RS-1",
+    "descriptionLong": "Product description",
+    "price": [
+        {
+            "currencyId": "b7d2554b0ce847cd82f3ac9bd1c0dfca",
+            "net": 8.40,
+            "linked": false,
+            "gross": 10
+        }
+    ],
+    "url": "/product-name.html",
+    "manufacturer": {
+        "id": "b7d2554b0ce847cd82f3ac9bd1c0dfca",
+        "name": "Test variant manufacturer"
+    },
+};
+
+describe('Wishlist: for wishlist', () => {
+    beforeEach(() => {
+        cy.authenticate().then((result) => {
+            const requestConfig = {
+                headers: {
+                    Authorization: `Bearer ${result.access}`
+                },
+                method: 'post',
+                url: `api/${Cypress.env('apiVersion')}/_action/system-config/batch`,
+                body: {
+                    null: {
+                        'core.cart.wishlistEnabled': true // enable wishlist
+                    }
+                }
+            };
+
+            return cy.request(requestConfig);
+        });
+
+        return cy.createCustomerFixtureStorefront(customer).then(() => {
+            return cy.createProductFixture(product).then(() => {
+
+                // todo handle set product wishlist waiting apply store-api
+
+                // return cy.setProductWishlist({
+                //     productId: response.id,
+                //     customer: {
+                //         username: customer.email,
+                //         password: customer.password
+                //     }
+                // }).then(() => {
+                //     cy.visit('/wishlist');
+                // });
+
+                cy.visit('/');
+            })
+        })
+    });
+
+    it('@wishlist does some simple testing of the wishlist', () => {
+        it.skip();
+        // todo handle when @shopware-ag/e2e-testsuite-platform support call `/store-api/v*/*`
+
+        // cy.window().then((win) => {
+        //     if (!win.Feature.isActive('FEATURE_NEXT_10549')) {
+        //         return ;
+        //     }
+        //
+        //     cy.get('#loginMail').typeAndCheckStorefront(customer.email);
+        //     cy.get('#loginPassword').typeAndCheckStorefront(customer.password);
+        //     cy.get(`${page.elements.loginSubmit} [type="submit"]`).click();
+        //
+        //     cy.get('.cms-listing-row .cms-listing-col').contains(product.name);
+        //     cy.get(`.cms-listing-row .cms-listing-col`).contains(product.manufacturer.name);
+        // });
+    });
+
+    it('@wishlist remove product of wishlist', () => {
+        it.skip();
+        // todo handle when @shopware-ag/e2e-testsuite-platform support call `/store-api/v*/*`
+
+        // cy.window().then((win) => {
+        //     if (!win.Feature.isActive('FEATURE_NEXT_10549')) {
+        //         return ;
+        //     }
+        //
+        //     cy.get('#loginMail').typeAndCheckStorefront(customer.email);
+        //     cy.get('#loginPassword').typeAndCheckStorefront(customer.password);
+        //     cy.get(`${page.elements.loginSubmit} [type="submit"]`).click();
+        //
+        //     cy.get('.cms-listing-row .cms-listing-col').contains(product.name);
+        //
+        //     cy.get('.product-wishlist-form [type="submit"]').click();
+        //
+        //     cy.get('.alert-success').contains('You have successfully removed the product from the wishlist.');
+        // });
+    })
+});

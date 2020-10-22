49ae8abf9521043de78ac3fbfd65b662713742e8
Quynh Nguyen <q.nguyen@shopware.com>
2020-10-22 16:23:24 +0700

    NEXT-11503 - Add cms buy box element in admin

---
+++ src/Administration/Resources/app/administration/src/module/sw-cms/elements/buy-box/preview/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-el-preview-buy-box.html.twig';
+import './sw-cms-el-preview-buy-box.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-el-preview-buy-box', {
+    template
+});
---
+++ src/Administration/Resources/app/administration/test/module/sw-cms/elements/buy-box/component/sw-cms-el-buy-box.spec.js
@@ -0,0 +1,105 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import 'src/module/sw-cms/mixin/sw-cms-element.mixin';
+import 'src/module/sw-cms/elements/buy-box/component';
+
+const productMock = {
+    name: 'Lorem Ipsum dolor',
+    productNumber: '1234',
+    minPurchase: 1,
+    deliveryTime: {
+        name: '1-3 days'
+    },
+    price: [
+        { gross: 100 }
+    ]
+};
+
+function createWrapper() {
+    const localVue = createLocalVue();
+    localVue.filter('currency', key => key);
+
+    return shallowMount(Shopware.Component.build('sw-cms-el-buy-box'), {
+        localVue,
+        sync: false,
+        propsData: {
+            element: {
+                data: {},
+                config: {}
+            },
+            defaultConfig: {
+                alignment: {
+                    value: null
+                }
+            }
+        },
+        data() {
+            return {
+                cmsPageState: {
+                    currentPage: {
+                        type: 'ladingpage'
+                    }
+                }
+            };
+        },
+        stubs: {
+            'sw-block-field': true,
+            'sw-icon': true
+        },
+        mocks: {
+            $tc: (value) => value
+        },
+        provide: {
+            cmsService: {
+                getCmsBlockRegistry: () => {
+                    return {};
+                },
+                getCmsElementRegistry: () => {
+                    return {
+                        'buy-box': {
+                            defaultData: {}
+                        }
+                    };
+                }
+            }
+        }
+    });
+}
+
+describe('module/sw-cms/elements/buy-box/component', () => {
+    it('should show skeleton if page type is product page', async () => {
+        const wrapper = createWrapper();
+
+        await wrapper.setData({
+            cmsPageState: {
+                currentPage: {
+                    type: 'product_detail'
+                }
+            }
+        });
+
+        expect(wrapper.find('.sw-cms-el-buy-box__skeleton').exists()).toBeTruthy();
+    });
+
+    it('should show dummy data initally if page type is not product page and no product config', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper.find('.sw-cms-el-buy-box__content').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-cms-el-buy-box__price').text()).toBe('0');
+    });
+
+    it('should show product data if page type is not product page', async () => {
+        const wrapper = createWrapper();
+
+        await wrapper.setProps({
+            element: {
+                data: {
+                    product: productMock
+                },
+                config: {}
+            }
+        });
+
+        expect(wrapper.find('.sw-cms-el-buy-box__content').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-cms-el-buy-box__price').text()).toBe('100');
+    });
+});

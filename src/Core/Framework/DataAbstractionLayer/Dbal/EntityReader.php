0648f1fb9b3f790a12c8eea70bcedf804132b66d
Oliver Skroblin <o.skroblin@shopware.com>
2020-11-05 12:42:46 +0100

    NEXT-10437 - Added exception to prevent parent loading via DAL

--- src/Core/Framework/DataAbstractionLayer/Dbal/EntityReader.php
+++ src/Core/Framework/DataAbstractionLayer/Dbal/EntityReader.php
@@ -4,6 +4,7 @@ namespace Shopware\Core\Framework\DataAbstractionLayer\Dbal;
 
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Exception\ParentAssociationCanNotBeFetched;
 use Shopware\Core\Framework\DataAbstractionLayer\Doctrine\FetchModeHelper;
 use Shopware\Core\Framework\DataAbstractionLayer\Entity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
@@ -141,6 +142,10 @@ class EntityReader implements EntityReaderInterface
         }
 
         $fields = $this->addAssociationFieldsToCriteria($criteria, $definition, $fields);
+
+        if ($definition->isInheritanceAware() && $criteria->hasAssociation('parent')) {
+            throw new ParentAssociationCanNotBeFetched();
+        }
 
         $rows = $this->fetch($criteria, $definition, $context, $fields);
 
---
+++ src/Core/Framework/DataAbstractionLayer/Dbal/Exception/ParentAssociationCanNotBeFetched.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer\Dbal\Exception;
+
+use Shopware\Core\Framework\ShopwareHttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ParentAssociationCanNotBeFetched extends ShopwareHttpException
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'It is not possible to read the parent association directly. Please read the parents via a separate call over the repository'
+        );
+    }
+
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_BAD_REQUEST;
+    }
+
+    public function getErrorCode(): string
+    {
+        return 'FRAMEWORK__PARENT_ASSOCIATION_CAN_NOT_BE_FETCHED';
+    }
+}

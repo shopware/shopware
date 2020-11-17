7749feac600a3630016cee9c8f858653b7ceaed8
Binh Tran <t.binh@shopware.com>
2020-11-17 14:09:25 +0700

    NEXT-11817 - Fix bug type of customer account when submit validation failed

--- src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/account/register.spec.js
+++ src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/account/register.spec.js
@@ -122,4 +122,49 @@ describe('Account: Register via account menu', () => {
             expect(element).to.contain('Overview');
         });
     });
+
+    it('@registration: Trigger validation error with account type selection', () => {
+        cy.authenticate().then((result) => {
+            const requestConfig = {
+                headers: {
+                    Authorization: `Bearer ${result.access}`
+                },
+                method: 'post',
+                url: `api/${Cypress.env('apiVersion')}/_action/system-config/batch`,
+                body: {
+                    null: {
+                        'core.loginRegistration.showAccountTypeSelection': true
+                    }
+                }
+            };
+
+            return cy.request(requestConfig);
+        });
+
+        cy.createCustomerFixtureStorefront();
+
+        const page = new AccountPageObject();
+        cy.visit('/account/login');
+        cy.get(page.elements.registerCard).should('be.visible');
+
+        const accountTypeSelector = `${page.elements.registerForm} select[name="accountType"]`;
+        cy.get(accountTypeSelector).should('be.visible');
+        cy.get(accountTypeSelector).typeAndSelect('Commercial');
+
+        cy.get(`${page.elements.registerForm} select[name="salutationId"]`).select('Mr.');
+        cy.get(`${page.elements.registerForm} input[name="firstName"]`).type('John');
+        cy.get(`${page.elements.registerForm} input[name="lastName"]`).type('Doe');
+        cy.get(`${page.elements.registerForm} input[name="email"]`).type('test@example.com');
+        cy.get(`${page.elements.registerForm} input[name="password"]`).type('1234567890');
+
+        cy.get('.register-address input[name="billingAddress[company]"]').type('Test Company');
+        cy.get('.register-address input[name="billingAddress[street]"]').type('123 Main St');
+        cy.get('.register-address input[name="billingAddress[zipcode]"]').type('9876');
+        cy.get('.register-address input[name="billingAddress[city]"]').type('Anytown');
+
+        cy.get('.register-address select[name="billingAddress[countryId]"]').select('USA');
+        cy.get('.register-address select[name="billingAddress[countryStateId]"').should('be.visible');
+
+        cy.get(`${page.elements.registerSubmit} [type="submit"]`).click();
+    });
 });

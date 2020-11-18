62dd8b8a07bbbba039be7c98b81a2e7b2ab82b3b
Ramona Schwering <R.Schwering@shopware.com>
2020-11-18 10:26:43 +0100

    NEXT-11851 - Improve media-crud e2e test

--- src/Administration/Resources/app/administration/test/e2e/cypress/integration/media-marketing/sw-media/crud-media.spec.js
+++ src/Administration/Resources/app/administration/test/e2e/cypress/integration/media-marketing/sw-media/crud-media.spec.js
@@ -123,20 +123,26 @@ describe('Media: Test crud operations', () => {
             method: 'post'
         }).as('saveDataFileUpload');
 
-        page.uploadImageUsingFileUpload('img/sw-login-background.png', 'sw-login-background.png');
+        cy.route({
+            url: `${Cypress.env('apiPath')}/_action/media/**/upload?extension=png&fileName=sw_logo_white`,
+            method: 'post'
+        }).as('saveDataUrlUpload');
 
-        cy.wait('@saveDataFileUpload').then((xhr) => {
+        // Upload medium
+        cy.clickContextMenuItem(
+            '.sw-media-upload-v2__button-url-upload',
+            '.sw-media-upload-v2__button-context-menu'
+        );
+        page.uploadImageUsingUrl('http://assets.shopware.com/sw_logo_white.png');
+
+        cy.wait('@saveDataUrlUpload').then((xhr) => {
             cy.awaitAndCheckNotification('File has been saved.');
             expect(xhr).to.have.property('status', 204);
-
-            cy.get('.sw-media-media-item').should('be.visible');
             cy.get('.sw-media-media-item .sw-media-preview-v2__item')
                 .should('have.attr', 'src')
-                .and('match', /sw-login-background/);
+                .and('match', /sw_logo_white/);
         });
-        cy.get('.sw-media-base-item__name[title="sw-login-background.png"]')
-            .should('be.visible');
-        cy.get('.sw-media-media-item').should('be.visible')
-        page.deleteFile('sw-login-background.png');
+
+        page.deleteFile('sw_logo_white.png');
     });
 });

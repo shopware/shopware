ac6b9b7dcd1f64d3a610cad98055f038daf71ef1
Philip Gatzka <p.gatzka@shopware.com>
2020-11-20 13:41:37 +0100

    NEXT-11792 - Added config to make phone number input optional for contact form
    fixes #1456

---
+++ src/Core/Migration/Migration1604499476AddDefaultSettingConfigValueForContactForm.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+class Migration1604499476AddDefaultSettingConfigValueForContactForm extends MigrationStep
+{
+    private const CONFIG_KEYS = [
+        'core.basicInformation.firstNameFieldRequired',
+        'core.basicInformation.lastNameFieldRequired',
+        'core.basicInformation.phoneNumberFieldRequired',
+    ];
+
+    public function getCreationTimestamp(): int
+    {
+        return 1604499476;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $createdAt = (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT);
+
+        foreach (self::CONFIG_KEYS as $configKey) {
+            $configPresent = $connection->fetchColumn('SELECT 1 FROM `system_config` WHERE `configuration_key` = ?', [$configKey]);
+
+            if ($configPresent !== false) {
+                continue;
+            }
+
+            $connection->insert('system_config', [
+                'id' => Uuid::randomBytes(),
+                'configuration_key' => $configKey,
+                'configuration_value' => '{"_value": true}',
+                'created_at' => $createdAt,
+            ]);
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}

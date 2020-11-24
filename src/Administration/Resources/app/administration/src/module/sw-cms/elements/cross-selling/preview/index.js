c009658c6b71d81e2724c4f68ef5709332a5b54c
Vu Le <l.nguyenvu@shopware.com>
2020-11-24 09:25:20 +0700

    NEXT-12059 - Create "Cross Selling" element

---
+++ src/Administration/Resources/app/administration/src/module/sw-cms/elements/cross-selling/preview/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-el-preview-cross-selling.html.twig';
+import './sw-cms-el-preview-cross-selling.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-el-preview-cross-selling', {
+    template
+});

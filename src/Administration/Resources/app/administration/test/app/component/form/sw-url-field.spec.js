d078e88b48cc9517b1c3ebbf3bf603afd6cf0833
Dominik Makeprange <d.makeprange@shopware.com>
2020-11-24 15:42:36 +0100

    NEXT-9348 - Fix URL Punycode conversion

--- src/Administration/Resources/app/administration/test/app/component/form/sw-url-field.spec.js
+++ src/Administration/Resources/app/administration/test/app/component/form/sw-url-field.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import 'src/app/component/form/sw-url-field';
 import 'src/app/component/form/sw-text-field';
 import 'src/app/component/form/field-base/sw-contextual-field';
@@ -6,19 +6,26 @@ import 'src/app/component/form/field-base/sw-block-field';
 import 'src/app/component/form/field-base/sw-base-field';
 import 'src/app/component/base/sw-icon';
 import 'src/app/component/form/field-base/sw-field-error';
+import 'src/app/filter/unicode-uri';
 
 describe('components/form/sw-url-field', () => {
     let wrapper;
 
     beforeEach(() => {
+        const localVue = createLocalVue();
+        localVue.filter('unicodeUri', Shopware.Filter.getByName('unicodeUri'));
+
         wrapper = shallowMount(Shopware.Component.build('sw-url-field'), {
+            localVue,
             stubs: {
                 'sw-text-field': Shopware.Component.build('sw-text-field'),
                 'sw-contextual-field': Shopware.Component.build('sw-contextual-field'),
                 'sw-block-field': Shopware.Component.build('sw-block-field'),
                 'sw-base-field': Shopware.Component.build('sw-base-field'),
                 'sw-field-error': Shopware.Component.build('sw-field-error'),
-                'sw-icon': Shopware.Component.build('sw-icon')
+                'sw-icon': Shopware.Component.build('sw-icon'),
+                'icons-default-lock-closed': true,
+                'icons-default-lock-open': true
             },
             provide: {
                 validationService: {}
@@ -61,5 +68,22 @@ describe('components/form/sw-url-field', () => {
         expect(wrapper.text()).toBe('https://');
         await wrapper.find('.sw-field__url-input__prefix').trigger('click');
         expect(wrapper.text()).toBe('http://');
+    });
+
+    it('should display unicode format', async () => {
+        await wrapper.find('.sw-url-input-field__input').setValue('www.täst-shöp.de');
+        expect(wrapper.find('.sw-field__error').exists()).toBe(false);
+        expect(wrapper.find('.sw-url-input-field__input').element.value).toBe('www.täst-shöp.de');
+        expect(wrapper.vm.currentValue).toBe('www.täst-shöp.de');
+
+        await wrapper.find('.sw-url-input-field__input').setValue('www.täst-shöp.de/blä');
+        expect(wrapper.find('.sw-field__error').exists()).toBe(false);
+        expect(wrapper.find('.sw-url-input-field__input').element.value).toBe('www.täst-shöp.de/blä');
+        expect(wrapper.vm.currentValue).toBe('www.täst-shöp.de/bl%C3%A4');
+
+        await wrapper.find('.sw-url-input-field__input').setValue('www.täst-shöp.de/😋');
+        expect(wrapper.find('.sw-field__error').exists()).toBe(false);
+        expect(wrapper.find('.sw-url-input-field__input').element.value).toBe('www.täst-shöp.de/😋');
+        expect(wrapper.vm.currentValue).toBe('www.täst-shöp.de/%F0%9F%98%8B');
     });
 });

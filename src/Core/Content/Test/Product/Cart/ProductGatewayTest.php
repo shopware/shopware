c91f6ce1a9fd1d9a977469a6ddaa6727357bc656
Philip Gatzka <p.gatzka@shopware.com>
2020-11-26 13:56:44 +0100

    NEXT-12416 - Add missing product gateway criteria event
    fixes #1517

---
+++ src/Core/Content/Test/Product/Cart/ProductGatewayTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Test\Product\Cart;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Content\Product\Cart\ProductGateway;
+use Shopware\Core\Content\Product\Events\ProductGatewayCriteriaEvent;
+use Shopware\Core\Content\Product\ProductCollection;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepositoryInterface;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class ProductGatewayTest extends TestCase
+{
+    public function testSendCriteriaEvent(): void
+    {
+        $ids = [
+            Uuid::randomHex(),
+            Uuid::randomHex(),
+        ];
+
+        $context = $this->createMock(SalesChannelContext::class);
+
+        $repository = $this->createMock(SalesChannelRepositoryInterface::class);
+        $repository->method('search')->willReturn(new ProductCollection());
+
+        $validator = static::callback(static function ($subject) {
+            return $subject instanceof ProductGatewayCriteriaEvent;
+        });
+
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $eventDispatcher->expects(static::once())->method('dispatch')->with($validator);
+
+        $gateway = new ProductGateway(
+            $repository,
+            $eventDispatcher
+        );
+
+        $gateway->get($ids, $context);
+    }
+}

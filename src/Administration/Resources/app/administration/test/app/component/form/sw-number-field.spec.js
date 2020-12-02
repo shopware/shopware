ddc35a23ed6a1a8168788c94cb2d6646b7183056
Jannis Leifeld <jannis.leifeld@googlemail.com>
2020-12-02 13:29:28 +0100

    NEXT-11620 - allow empty values in number field

---
+++ src/Administration/Resources/app/administration/test/app/component/form/sw-number-field.spec.js
@@ -0,0 +1,82 @@
+import { shallowMount } from '@vue/test-utils';
+import 'src/app/component/form/sw-text-field';
+import 'src/app/component/form/sw-number-field';
+import 'src/app/component/form/field-base/sw-contextual-field';
+import 'src/app/component/form/field-base/sw-block-field';
+import 'src/app/component/form/field-base/sw-base-field';
+
+const createWrapper = () => {
+    return shallowMount(Shopware.Component.build('sw-number-field'), {
+        stubs: {
+            'sw-contextual-field': Shopware.Component.build('sw-contextual-field'),
+            'sw-block-field': Shopware.Component.build('sw-block-field'),
+            'sw-base-field': Shopware.Component.build('sw-base-field'),
+            'sw-field-error': {
+                template: '<div></div>'
+            }
+        },
+        provide: {
+            validationService: {}
+        },
+        propsData: {
+            value: null
+        }
+    });
+};
+
+describe('app/component/form/sw-number-field', () => {
+    it('should be a Vue.js component', async () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper.vm).toBeTruthy();
+    });
+
+    it('should set value 0 when user deletes everything', async () => {
+        const wrapper = createWrapper();
+
+        const input = wrapper.find('input');
+
+        // type "5"
+        await input.setValue('5');
+        await input.trigger('change');
+
+        // expect 5
+        expect(wrapper.emitted('change')[0]).toEqual([5]);
+        expect(input.element.value).toBe('5');
+
+        // clear input
+        await input.setValue('');
+        await input.trigger('change');
+
+        // expect 0
+        expect(wrapper.emitted('change')[1]).toEqual([0]);
+        expect(input.element.value).toBe('0');
+    });
+
+    it('should clear input field when user deletes everything and emits null', async () => {
+        const wrapper = createWrapper();
+
+        // set property allowEmpty to true
+        await wrapper.setProps({
+            allowEmpty: true
+        });
+
+        const input = wrapper.find('input');
+
+        // type "5"
+        await input.setValue('5');
+        await input.trigger('change');
+
+        // expect 5
+        expect(wrapper.emitted('change')[0]).toEqual([5]);
+        expect(input.element.value).toBe('5');
+
+        // clear input
+        await input.setValue('');
+        await input.trigger('change');
+
+        // expect null and empty input
+        expect(wrapper.emitted('change')[1]).toEqual([null]);
+        expect(input.element.value).toBe('');
+    });
+});

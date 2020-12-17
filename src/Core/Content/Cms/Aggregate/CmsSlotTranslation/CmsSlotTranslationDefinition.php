70bb257c0be09473280207b6cd8456b3f6adc6ab
OliverSkroblin <o.skroblin@shopware.com>
2020-12-17 13:31:25 +0100

    NEXT-10320 - Remove required flag of cms slot config

--- src/Core/Content/Cms/Aggregate/CmsSlotTranslation/CmsSlotTranslationDefinition.php
+++ src/Core/Content/Cms/Aggregate/CmsSlotTranslation/CmsSlotTranslationDefinition.php
@@ -6,7 +6,6 @@ use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotDefinition;
 use Shopware\Core\Content\Cms\DataAbstractionLayer\Field\SlotConfigField;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityTranslationDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\CustomFields;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 
 class CmsSlotTranslationDefinition extends EntityTranslationDefinition
@@ -36,7 +35,7 @@ class CmsSlotTranslationDefinition extends EntityTranslationDefinition
     protected function defineFields(): FieldCollection
     {
         return new FieldCollection([
-            (new SlotConfigField('config', 'config'))->setFlags(new Required()),
+            new SlotConfigField('config', 'config'),
             new CustomFields(),
         ]);
     }

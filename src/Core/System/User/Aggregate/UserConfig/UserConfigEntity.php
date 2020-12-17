b31d96d8df1d40b8922f481b45f58c7d8b953647
Trong Nguyen Van <n.trong@shopware.com>
2020-12-17 14:54:50 +0700

    NEXT-9589 - saving of grid settings at user - implement DAL for new table user_config

---
+++ src/Core/System/User/Aggregate/UserConfig/UserConfigEntity.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\System\User\Aggregate\UserConfig;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+use Shopware\Core\System\User\UserEntity;
+
+class UserConfigEntity extends Entity
+{
+    use EntityIdTrait;
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var array|null
+     */
+    protected $value;
+
+    /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * @var UserEntity|null
+     */
+    protected $user;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function getValue(): ?array
+    {
+        return $this->value;
+    }
+
+    public function setValue(?array $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(string $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUser(): ?UserEntity
+    {
+        return $this->user;
+    }
+
+    public function setUser(UserEntity $user): void
+    {
+        $this->user = $user;
+    }
+}

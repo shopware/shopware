a32a6b9045ce352075416a01abaee001ed972116
Niklas Büchner <niklas.buechner@pickware.de>
2021-01-18 16:18:24 +0100

    Load administration even if plugin js does not exist

---
+++ changelog/_unreleased/2021-01-15-prevent-missing-plugin-file-from-crashing-administration.md
@@ -0,0 +1,8 @@
+---
+title:              Prevent error in loading plugin administration files from crashing the administration.
+issue:              
+author:             Niklas Büchner
+author_email:       niklas.buechner@pickware.de
+---
+# Administration
+* Changed the plugin loading process to gracefully handle a missing javascript file of a plugin instead of crashing.
--- src/Administration/Resources/app/administration/src/core/application.js
+++ src/Administration/Resources/app/administration/src/core/application.js
@@ -508,23 +508,20 @@ class ApplicationBootstrapper {
      * @private
      * @returns {Promise<any[][]>}
      */
-    loadPlugins() {
+    async loadPlugins() {
         const isDevelopmentMode = process.env.NODE_ENV;
 
+        let plugins;
         // only in webpack dev mode
         if (isDevelopmentMode === 'development') {
-            return fetch('./sw-plugin-dev.json')
-                .then((rawResponse) => rawResponse.json())
-                .then((plugins) => {
-                    const injectAllPlugins = Object.values(plugins).map((plugin) => this.injectPlugin(plugin));
-                    return Promise.all(injectAllPlugins);
-                });
+            const response = await fetch('./sw-plugin-dev.json');
+            plugins = await response.json();
+        } else {
+            plugins = Shopware.Context.app.config.bundles;
         }
 
-        // in production
-        const plugins = Shopware.Context.app.config.bundles;
-
         const injectAllPlugins = Object.values(plugins).map((plugin) => this.injectPlugin(plugin));
+
         return Promise.all(injectAllPlugins);
     }
 
@@ -534,7 +531,7 @@ class ApplicationBootstrapper {
      * @param {Object} plugin
      * @returns {Promise<any[]>}
      */
-    injectPlugin(plugin) {
+    async injectPlugin(plugin) {
         let allScripts = [];
         let allStyles = [];
 
@@ -552,7 +549,13 @@ class ApplicationBootstrapper {
             allStyles.push(this.injectCss(plugin.css));
         }
 
-        return Promise.all([...allScripts, ...allStyles]);
+        try {
+            return await Promise.all([...allScripts, ...allStyles]);
+        } catch (_) {
+            console.log('Error while loading plugin', plugin);
+
+            return null;
+        }
     }
 
     /**

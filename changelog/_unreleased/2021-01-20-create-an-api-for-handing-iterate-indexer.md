abacc94d9bb5707de7e13a9821a5bb4aa537d5df
Huy Truong <h.truong@shopware.com>
2021-01-20 15:16:30 +0700

    NEXT-13105 - Create iterate indexer api

---
+++ changelog/_unreleased/2021-01-20-create-an-api-for-handing-iterate-indexer.md
@@ -0,0 +1,7 @@
+---
+title: Create an api for handing iterate indexer
+issue: NEXT-13105
+flag: FEATURE_NEXT_10552
+---
+# Core
+* Added iterate indexer handling API as `iterate` function at `Framework/Api/Controller/IndexingController`.
---
+++ src/Core/Framework/Test/Api/Controller/IndexingControllerTest.php
@@ -0,0 +1,108 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\Api\Controller;
+
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\Test\TestCaseBase\AdminFunctionalTestBehaviour;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\PlatformRequest;
+
+class IndexingControllerTest extends TestCase
+{
+    use AdminFunctionalTestBehaviour;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $productRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->productRepository = $this->getContainer()->get('product.repository');
+        $this->connection = $this->getContainer()->get(Connection::class);
+    }
+
+    public function testIterateIndexerApiShouldReturnFinishTrueWithInvalidIndexer(): void
+    {
+        $this->getBrowser()->request(
+            'POST',
+            '/api/v' . PlatformRequest::API_VERSION . '/_action/indexing/test.indexer',
+            ['offset' => 0]
+        );
+        $response = $this->getBrowser()->getResponse();
+        $response = json_decode($response->getContent(), true);
+
+        static::assertTrue($response['finish']);
+    }
+
+    /**
+     * @dataProvider provideOffsets
+     */
+    public function testIterateIndexerApiShouldReturnCorrectOffset(int $offset): void
+    {
+        $database = $this->connection->fetchColumn('select database();');
+        $autoIncrement = $this->connection->fetchColumn(
+            'SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = :database AND TABLE_NAME = "product"',
+            ['database' => $database]
+        );
+
+        $offset = $offset + $autoIncrement - 1;
+
+        $this->createProducts();
+
+        $this->getBrowser()->request(
+            'POST',
+            '/api/v' . PlatformRequest::API_VERSION . '/_action/indexing/product.indexer',
+            ['offset' => $offset]
+        );
+        $response = $this->getBrowser()->getResponse();
+        $response = json_decode($response->getContent(), true);
+
+        if ($offset - $autoIncrement + 1 === 100) {
+            static::assertTrue($response['finish']);
+        } else {
+            static::assertFalse($response['finish']);
+            static::assertEquals(['offset' => $offset + 50], $response['offset']);
+        }
+    }
+
+    public function provideOffsets(): array
+    {
+        return [
+            'offset 0' => [0],
+            'offset 50' => [50],
+            'offset 100' => [100],
+        ];
+    }
+
+    private function createProducts(): void
+    {
+        $data = [];
+        for ($i = 0; $i < 100; ++$i) {
+            $data[] = [
+                'id' => Uuid::randomHex(),
+                'name' => 'test',
+                'productNumber' => Uuid::randomHex(),
+                'stock' => 10,
+                'price' => [
+                    ['currencyId' => Defaults::CURRENCY, 'gross' => 15, 'net' => 10, 'linked' => false],
+                ],
+                'manufacturer' => ['name' => 'test'],
+                'tax' => ['name' => 'test', 'taxRate' => 15],
+            ];
+        }
+        $this->productRepository->create($data, Context::createDefaultContext());
+    }
+}

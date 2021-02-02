3cc159188cdd33f19fa034396a42659d01217089
Krispin Lütjann <k.luetjann@shopware.com>
2021-02-02 15:44:45 +0100

    NEXT-13293 - Add landing page to search bar

---
+++ src/Administration/Resources/app/administration/src/module/sw-landing-page/index.js
@@ -0,0 +1,24 @@
+import './../sw-category/page/sw-category-detail';
+
+const { Module } = Shopware;
+
+Module.register('sw-landing-page', {
+    type: 'core',
+    name: 'landing_page',
+    version: '1.0.0',
+    targetVersion: '1.0.0',
+    color: '#57D9A3',
+    icon: 'default-symbol-products',
+    favicon: 'icon-module-products.png',
+    entity: 'landing_page',
+
+    routes: {
+        index: {
+            component: 'sw-category-detail',
+            path: 'index',
+            redirect: {
+                name: 'sw.category.detail.base'
+            }
+        }
+    }
+});

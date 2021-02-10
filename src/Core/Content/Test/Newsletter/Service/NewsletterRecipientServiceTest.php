d0a66245ff15c55d44c4172c7dda5ad53fa45032
OliverSkroblin <o.skroblin@shopware.com>
2021-02-10 13:53:36 +0100

    NEXT-12037 - Fix newsletter test

--- src/Core/Content/Test/Newsletter/Service/NewsletterRecipientServiceTest.php
+++ src/Core/Content/Test/Newsletter/Service/NewsletterRecipientServiceTest.php
@@ -268,8 +268,5 @@ class NewsletterRecipientServiceTest extends TestCase
         $recipientSql = file_get_contents(__DIR__ . '/../fixtures/recipient.sql');
         $recipientSql = str_replace(':createdAt', (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT), $recipientSql);
         $this->getContainer()->get(Connection::class)->exec($recipientSql);
-
-        $templateSql = file_get_contents(__DIR__ . '/../fixtures/template.sql');
-        $this->getContainer()->get(Connection::class)->exec($templateSql);
     }
 }

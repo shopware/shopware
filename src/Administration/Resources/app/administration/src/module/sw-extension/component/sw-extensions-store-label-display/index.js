de1a2b312aae887f3c475877c22132f5a4f8728f
Niklas Limberg <n.limberg@shopware.com>
2021-02-22 14:47:47 +0100

    NEXT-12903 - show extension store labels - change text color

--- src/Administration/Resources/app/administration/src/module/sw-extension/component/sw-extensions-store-label-display/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-extension/component/sw-extensions-store-label-display/index.js
@@ -14,6 +14,21 @@ Component.register('sw-extensions-store-label-display', {
             type: Array,
             required: true
         }
-    }
+    },
 
+    methods: {
+        determineTextColor(backgroudColor) {
+            if (!backgroudColor) {
+                return '#000';
+            }
+
+            const hexColor = backgroudColor.charAt(0) === '#' ? backgroudColor.substring(1, 7) : backgroudColor;
+
+            const r = parseInt(hexColor.substring(0, 2), 16); // hexToR
+            const g = parseInt(hexColor.substring(2, 4), 16); // hexToG
+            const b = parseInt(hexColor.substring(4, 6), 16); // hexToB
+
+            return (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#000' : '#fff';
+        }
+    }
 });

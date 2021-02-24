5a8afa964a54744d7c239f566cd47fa39b7f092e
Jannis Leifeld <j.leifeld@shopware.com>
2021-02-24 13:19:46 +0100

    NEXT-13830 - only update extensions when user has the correct privileges for this

--- src/Administration/Resources/app/administration/src/module/sw-extension/store/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-extension/store/index.js
@@ -5,6 +5,10 @@ export default function initState(Shopware) {
 
     let languageId = Shopware.State.get('session').languageId;
     Shopware.State._store.subscribe(({ type }, state) => {
+        if (!Shopware.Service('acl').can('system.plugin_maintain')) {
+            return;
+        }
+
         if (type === 'setAdminLocale' && state.session.languageId !== '' && languageId !== state.session.languageId) {
             Shopware.Service('shopwareExtensionService').updateExtensionData();
             languageId = state.session.languageId;

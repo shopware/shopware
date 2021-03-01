4b03296aa7d3edaa97fb4093fb6bf08a4ac2acab
Jannis Leifeld <j.leifeld@shopware.com>
2021-03-01 11:21:19 +0100

    NEXT-13831 - add acl for extension store

---
+++ src/Administration/Resources/app/administration/src/module/sw-extension/acl/index.js
@@ -0,0 +1,14 @@
+Shopware.Service('privileges')
+    .addPrivilegeMappingEntry({
+        category: 'additional_permissions',
+        parent: null,
+        key: 'system',
+        roles: {
+            extension_store: {
+                privileges: [],
+                dependencies: [
+                    'system.plugin_maintain'
+                ]
+            }
+        }
+    });

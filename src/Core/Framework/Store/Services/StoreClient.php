06bda4aa8cd7b2caed7ff0c68b257fa52a5c7ed0
Soner Sayakci <s.sayakci@shopware.com>
2021-03-19 14:56:54 +0000

    NEXT-13469 - Fix styling of rating modal

--- src/Core/Framework/Store/Services/StoreClient.php
+++ src/Core/Framework/Store/Services/StoreClient.php
@@ -423,13 +423,17 @@ class StoreClient
 
     public function createRating(ReviewStruct $rating, Context $context): void
     {
+        if ($this->authenticationProvider === null) {
+            throw new \RuntimeException('App Store is not active');
+        }
+
         try {
             $this->getClient()->post(
                 sprintf($this->endpoints['create_rating'], $rating->getExtensionId()),
                 [
                     'query' => $this->storeService->getDefaultQueryParameters('en-GB', false),
                     'headers' => $this->getHeaders($this->authenticationProvider->getUserStoreToken($context)),
-                    'json' => $rating
+                    'json' => $rating,
                 ]
             );
         } catch (ClientException $e) {

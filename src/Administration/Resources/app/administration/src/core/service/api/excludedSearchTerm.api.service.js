2fa40d665a17c889fe62622a8d08b0dadaecdd46
Phuoc Cao <p.cao@shopware.com>
2021-03-30 16:25:26 +0700

    NEXT-14417 - Add reset to default for excluded search term

---
+++ src/Administration/Resources/app/administration/src/core/service/api/excludedSearchTerm.api.service.js
@@ -0,0 +1,21 @@
+import ApiService from '../api.service';
+
+export default class ExcludedSearchTermService extends ApiService {
+    constructor(httpClient, loginService, apiEndpoint = 'reset-excluded-search-term') {
+        super(httpClient, loginService, apiEndpoint);
+        this.name = 'excludedSearchTermService';
+    }
+
+    resetExcludedSearchTerm() {
+        const route = '/_admin/reset-excluded-search-term';
+        const headers = this._getHeader();
+        return this.httpClient.post(route, {}, { headers });
+    }
+
+    _getHeader() {
+        return {
+            ...super.getBasicHeaders(),
+            'sw-language-id': Shopware.Context.api.languageId
+        };
+    }
+}

8e65e1bb52f3003972ad615953da6877a875895a
Patrick Weyck <p.weyck@shopware.com>
2021-03-30 11:48:53 +0000

    Merge branch 'next-13939/fix-range-constraint' into 'trunk'
    NEXT-13939 - Fix validation of range constraints
    
    See merge request shopware/6/product/platform!4875
    
    (cherry picked from commit 371d1e255503581c86323b9a20c38ca16281c56e)
    
    2615a603 NEXT-13939 - Fix validation of range constraints

---
+++ src/Core/Framework/Test/Validation/HappyPathValidatorTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\Validation;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Validation\HappyPathValidator;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class HappyPathValidatorTest extends TestCase
+{
+    /**
+     * @dataProvider constraintDataProvider
+     */
+    public function testRangeFilter(Constraint $constraint, $value, bool $isValid): void
+    {
+        $inner = $this->createMock(ValidatorInterface::class);
+
+        if ($isValid) {
+            $inner->expects(static::never())->method('validate');
+        } else {
+            $inner->expects(static::atLeastOnce())->method('validate')->willReturn(new ConstraintViolationList([
+                $this->createMock(ConstraintViolationInterface::class),
+            ]));
+        }
+
+        $validator = new HappyPathValidator($inner);
+        $list = $validator->validate($value, $constraint);
+
+        $isEmpty = $list->count() === 0;
+        static::assertSame($isValid, $isEmpty);
+    }
+
+    public function constraintDataProvider(): \Generator
+    {
+        yield 'min range valid' => [
+            new Range(['min' => 11]),
+            11,
+            true,
+        ];
+
+        yield 'min range invalid' => [
+            new Range(['min' => 11]),
+            10,
+            false,
+        ];
+
+        yield 'max range valid' => [
+            new Range(['max' => 11]),
+            11,
+            true,
+        ];
+
+        yield 'max range invalid' => [
+            new Range(['max' => 11]),
+            12,
+            false,
+        ];
+
+        yield 'min max range valid' => [
+            new Range(['min' => 11, 'max' => 20]),
+            20,
+            true,
+        ];
+
+        yield 'min max range too low' => [
+            new Range(['min' => 11, 'max' => 20]),
+            10,
+            false,
+        ];
+
+        yield 'min max range too high' => [
+            new Range(['min' => 11, 'max' => 20]),
+            21,
+            false,
+        ];
+    }
+}

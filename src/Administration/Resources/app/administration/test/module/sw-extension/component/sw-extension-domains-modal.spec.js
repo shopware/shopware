5747a4a200b578197063f5f8e6035aa1d0263ff2
Jannis Leifeld <j.leifeld@shopware.com>
2021-04-08 11:36:00 +0200

    NEXT-14641 - add tests for domains modal and permissions modal

---
+++ src/Administration/Resources/app/administration/test/module/sw-extension/component/sw-extension-domains-modal.spec.js
@@ -0,0 +1,68 @@
+import { shallowMount } from '@vue/test-utils';
+import 'src/module/sw-extension/component/sw-extension-domains-modal';
+
+function createWrapper(propsData) {
+    return shallowMount(Shopware.Component.build('sw-extension-domains-modal'), {
+        propsData: {
+            extensionLabel: 'SEO Professional App',
+            ...propsData
+        },
+        mocks: {
+            $t: (...args) => JSON.stringify([...args]),
+            $tc: (...args) => JSON.stringify([...args])
+        },
+        stubs: {
+            'sw-modal': {
+                template: '<div class="sw-modal"><slot></slot></div>'
+            }
+        }
+    });
+}
+
+describe('src/module/sw-extension/component/sw-extension-domains-modal', () => {
+    /** @type Wrapper */
+    let wrapper;
+
+    afterEach(() => {
+        if (wrapper) wrapper.destroy();
+    });
+
+    it('should be a Vue.JS component', async () => {
+        wrapper = createWrapper({
+            domains: []
+        });
+
+        expect(wrapper.vm).toBeTruthy();
+    });
+
+    [
+        null,
+        undefined,
+        []
+    ].forEach(domains => {
+        it(`should not show any domains: ${domains}`, async () => {
+            wrapper = createWrapper({
+                domains
+            });
+
+            expect(wrapper.findAll('.sw-extension-domains-modal__list li').length).toBe(0);
+        });
+    });
+
+    [
+        ['htpps://www.google.com'],
+        ['https://www.google.com', 'https://bing.com']
+    ].forEach(domains => {
+        it(`should show the domains which are given via the property, domain count: ${domains.length}`, async () => {
+            wrapper = createWrapper({
+                domains
+            });
+
+            expect(wrapper.findAll('.sw-extension-domains-modal__list li').length).toBe(domains.length);
+
+            domains.forEach(domain => {
+                expect(wrapper.find('.sw-extension-domains-modal__list').text()).toContain(domain);
+            });
+        });
+    });
+});

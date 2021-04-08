54b996d9e29fbd28702a0a02e88dfaa0cdc8b046
rune.laenen <rune.laenen@intracto.com>
2021-04-08 17:36:55 +0200

    Make name column nullable

---
+++ src/Core/Migration/Migration1617896006MakeNameNullable.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @deprecated tag:v6.5.0 - Will be deleted. Migrations are now namespaced by major version
+ */
+class Migration1617896006MakeNameNullable extends \Shopware\Core\Migration\V6_4\Migration1617896006MakeNameNullable
+{}

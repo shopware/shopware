580e48825090db40bbe885cb9f64d5bf41bbf808
Jonas Elfering <j.elfering@shopware.com>
2021-04-16 13:38:20 +0000

    Merge branch 'next-14418/recursively-resolve-mixins' into 'trunk'
    NEXT-14418 - Recursively resolve mixins
    
    See merge request shopware/6/product/platform!5076
    
    (cherry picked from commit 1cf467dcfdc51002a40158adcf08b936f30b3d45)
    
    d9e7abb0 NEXT-14418 - Recursively resolve mixins

--- src/Administration/Resources/app/administration/test/app/adapter/view/vue.adapter.spec.js
+++ src/Administration/Resources/app/administration/test/app/adapter/view/vue.adapter.spec.js
@@ -79,6 +79,58 @@ describe('app/adapter/view/vue.adapter.js', () => {
         expect(Shopware.Service('localeHelper').setLocaleWithId).toHaveBeenCalledWith(expectedId);
     });
 
+    it('should resolve mixins by explicit Mixin get by name call', () => {
+        Shopware.Mixin.register('foo1', {
+            methods: {
+                fooBar() {
+                    return this.title;
+                }
+            }
+        });
+
+        Shopware.Component.register('test-component1', {
+            template: '<div></div>',
+            name: 'test-component1',
+            data() {
+                return {
+                    title: 'testComponent'
+                };
+            },
+            mixins: [
+                Shopware.Mixin.getByName('foo1')
+            ],
+            methods: {
+                bar() {}
+            }
+        });
+
+        const buildComp = vueAdapter.createComponent('test-component1');
+        let wrapper = shallowMount(buildComp);
+
+        expect(buildComp.sealedOptions.methods.fooBar).toBeDefined();
+        expect(buildComp.sealedOptions.methods.bar).toBeDefined();
+        expect(wrapper.vm.fooBar()).toBe('testComponent');
+
+        Shopware.Component.override('test-component1', {
+            data() {
+                return {
+                    title: 'testComponentOverride'
+                };
+            },
+            methods: {
+                buz() {}
+            }
+        });
+
+        const buildOverrideComp = vueAdapter.createComponent('test-component1');
+        wrapper = shallowMount(buildOverrideComp);
+
+        expect(buildOverrideComp.sealedOptions.methods.fooBar).toBeDefined();
+        expect(buildOverrideComp.sealedOptions.methods.bar).toBeDefined();
+        expect(buildOverrideComp.sealedOptions.methods.buz).toBeDefined();
+        expect(wrapper.vm.fooBar()).toBe('testComponentOverride');
+    });
+
     it('should resolve mixins by string', () => {
         Shopware.Mixin.register('foo', {
             methods: {
@@ -244,5 +296,45 @@ describe('app/adapter/view/vue.adapter.js', () => {
         expect(buildExtendedComponent.sealedOptions.methods.buz).toBeDefined();
         expect(wrapper.vm.fooBar()).toBe('testComponentExtended');
         expect(wrapper.vm.buz()).toBe('component');
+    });
+
+    it('should allow multi-inheritance with multiple mixins and lifecycle hooks are only executed once', () => {
+        const lifecycleSpy = jest.fn();
+        Shopware.Mixin.register('first-mixin', {
+            created() {
+                lifecycleSpy();
+            },
+            methods: {
+                foo() { return 'foo'; }
+            }
+        });
+
+        Shopware.Mixin.register('second-mixin', {
+            methods: {
+                bar() { return 'bar'; }
+            }
+        });
+
+        Shopware.Component.register('base-component', {
+            template: '<div class="base-component"></div>'
+        });
+
+        Shopware.Component.override('base-component', {
+            mixins: ['first-mixin']
+        });
+
+        Shopware.Component.override('base-component', {
+            mixins: ['second-mixin', 'first-mixin']
+        });
+
+        const buildExtendedComponent = vueAdapter.createComponent('base-component');
+        const wrapper = shallowMount(buildExtendedComponent);
+
+        expect(buildExtendedComponent.sealedOptions.methods.foo).toBeDefined();
+        expect(buildExtendedComponent.sealedOptions.methods.bar).toBeDefined();
+        expect(wrapper.vm.foo()).toBe('foo');
+        expect(wrapper.vm.bar()).toBe('bar');
+
+        expect(lifecycleSpy).toBeCalledTimes(1);
     });
 });

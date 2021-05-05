0a59a2d3cfdb8182d033edca3e2cfa2d0b00e22d
d.neustadt <d.neustadt@shopware.com>
2021-05-05 12:29:59 +0200

    NEXT-15008 - added test for message stats decrement through admin message consumption

--- src/Core/Framework/Test/MessageQueue/Api/ConsumeMessagesControllerTest.php
+++ src/Core/Framework/Test/MessageQueue/Api/ConsumeMessagesControllerTest.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Indexing\EntityIndexerRegistry;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\MessageQueue\MessageQueueStatsEntity;
@@ -14,6 +15,7 @@ use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskDefinition;
 use Shopware\Core\Framework\Test\TestCaseBase\AdminFunctionalTestBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\QueueTestBehaviour;
 use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\SalesChannel\DataAbstractionLayer\SalesChannelIndexingMessage;
 
 class ConsumeMessagesControllerTest extends TestCase
 {
@@ -53,12 +55,28 @@ class ConsumeMessagesControllerTest extends TestCase
         static::assertArrayHasKey('handledMessages', $response);
         static::assertIsInt($response['handledMessages']);
         static::assertEquals(1, $response['handledMessages']);
+    }
+
+    public function testMessageStatsDecrement(): void
+    {
+        $registry = $this->getContainer()->get(EntityIndexerRegistry::class);
+        $registry->index(true);
 
         /** @var EntityRepositoryInterface $queueRepo */
         $queueRepo = $this->getContainer()->get('message_queue_stats.repository');
         $context = Context::createDefaultContext();
         $criteria = new Criteria();
-        $criteria->setLimit(1)->addFilter(new EqualsFilter('name', RequeueDeadMessagesTask::class));
+        $criteria->setLimit(1)->addFilter(new EqualsFilter('name', SalesChannelIndexingMessage::class));
+
+        /** @var MessageQueueStatsEntity $queueStatus */
+        $queueStatus = $queueRepo->search($criteria, $context)->first();
+
+        static::assertGreaterThan(0, $queueStatus->getSize());
+
+        $url = '/api/_action/message-queue/consume';
+        $client = $this->getBrowser();
+        $client->request('POST', $url, ['receiver' => 'default']);
+
         /** @var MessageQueueStatsEntity $queueStatus */
         $queueStatus = $queueRepo->search($criteria, $context)->first();
 

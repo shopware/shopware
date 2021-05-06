a82b1b0065c9ae9cebad860fc57873a2cfa10d0b
Michael Telgmann <mt@shopware.com>
2021-05-06 14:32:40 +0200

    NEXT-14973 - Fix cs-fixer update issue

--- src/Recovery/Update/index.php
+++ src/Recovery/Update/index.php
@@ -1,24 +1,24 @@
 <?php declare(strict_types=1);
 
-if (\function_exists('opcache_reset')) {
+if (function_exists('opcache_reset')) {
     opcache_reset();
 }
 
 require_once __DIR__ . '/../autoload.php';
 
-\define('UPDATE_PATH', __DIR__);
+define('UPDATE_PATH', __DIR__);
 $isManual = is_dir(SW_PATH . '/update-assets');
 
 if ($isManual) {
-    \define('UPDATE_IS_MANUAL', true);
-    \define('UPDATE_FILES_PATH', null);
-    \define('UPDATE_ASSET_PATH', SW_PATH . '/update-assets');
-    \define('UPDATE_META_FILE', null);
+    define('UPDATE_IS_MANUAL', true);
+    define('UPDATE_FILES_PATH', null);
+    define('UPDATE_ASSET_PATH', SW_PATH . '/update-assets');
+    define('UPDATE_META_FILE', null);
 } else {
-    \define('UPDATE_IS_MANUAL', false);
-    \define('UPDATE_FILES_PATH', SW_PATH . '/files/update/files');
-    \define('UPDATE_ASSET_PATH', SW_PATH . '/files/update/update-assets');
-    \define('UPDATE_META_FILE', SW_PATH . '/files/update/update.json');
+    define('UPDATE_IS_MANUAL', false);
+    define('UPDATE_FILES_PATH', SW_PATH . '/files/update/files');
+    define('UPDATE_ASSET_PATH', SW_PATH . '/files/update/update-assets');
+    define('UPDATE_META_FILE', SW_PATH . '/files/update/update.json');
 }
 
 use Shopware\Recovery\Update\Console\Application;

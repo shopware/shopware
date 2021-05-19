fe038e7362384c644106eaaafde5f2c91a3b7eba
Patrick Weyck <p.weyck@shopware.com>
2021-05-19 16:09:07 +0000

    Merge branch 'next-15362/add-missing-pagination-to-user-list' into 'trunk'
    NEXT-15362 - Add missing pagination
    
    See merge request shopware/6/product/platform!5377
    
    (cherry picked from commit c74667b092738f12ea3f7d6e0e86b59d78cd13b5)
    
    4c084512 NEXT-15362 - Add missing pagination

---
+++ src/Core/Framework/Demodata/Generator/UserGenerator.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Demodata\Generator;
+
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\EntityWriterInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\WriteContext;
+use Shopware\Core\Framework\Demodata\DemodataContext;
+use Shopware\Core\Framework\Demodata\DemodataGeneratorInterface;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\Language\LanguageEntity;
+use Shopware\Core\System\User\UserDefinition;
+
+class UserGenerator implements DemodataGeneratorInterface
+{
+    private EntityWriterInterface $writer;
+
+    private UserDefinition $userDefinition;
+
+    private EntityRepository $languageRepository;
+
+    public function __construct(EntityWriterInterface $writer, UserDefinition $userDefinition, EntityRepository $languageRepository)
+    {
+        $this->writer = $writer;
+        $this->userDefinition = $userDefinition;
+        $this->languageRepository = $languageRepository;
+    }
+
+    public function getDefinition(): string
+    {
+        return UserDefinition::class;
+    }
+
+    public function generate(int $numberOfItems, DemodataContext $context, array $options = []): void
+    {
+        $writeContext = WriteContext::createFromContext($context->getContext());
+
+        $context->getConsole()->progressStart($numberOfItems);
+
+        $payload = [];
+        for ($i = 0; $i < $numberOfItems; ++$i) {
+            $id = Uuid::randomHex();
+            $firstName = $context->getFaker()->firstName;
+            $lastName = $context->getFaker()->lastName;
+            $title = $this->getRandomTitle();
+
+            $user = [
+                'id' => $id,
+                'title' => $title,
+                'firstName' => $firstName,
+                'lastName' => $lastName,
+                'username' => $context->getFaker()->userName,
+                'email' => $id . $context->getFaker()->safeEmail,
+                'password' => 'shopware',
+                'localeId' => $this->getLocaleId($context->getContext()),
+            ];
+
+            $payload[] = $user;
+
+            if (\count($payload) >= 100) {
+                $this->writer->upsert($this->userDefinition, $payload, $writeContext);
+
+                $context->getConsole()->progressAdvance(\count($payload));
+
+                $payload = [];
+            }
+        }
+
+        if (!empty($payload)) {
+            $this->writer->upsert($this->userDefinition, $payload, $writeContext);
+
+            $context->getConsole()->progressAdvance(\count($payload));
+        }
+
+        $context->getConsole()->progressFinish();
+    }
+
+    private function getRandomTitle(): string
+    {
+        $titles = ['', 'Dr.', 'Dr. med.', 'Prof.', 'Prof. Dr.'];
+
+        return $titles[array_rand($titles)];
+    }
+
+    private function getLocaleId(Context $context): string
+    {
+        /** @var LanguageEntity $first */
+        $first = $this->languageRepository->search(new Criteria([Defaults::LANGUAGE_SYSTEM]), $context)->first();
+
+        return $first->getLocaleId();
+    }
+}

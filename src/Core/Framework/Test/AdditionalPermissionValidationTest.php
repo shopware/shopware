d3f51ac7cb41630a07a3b08845eb4a63a9a845bc
David Neustadt <d.neustadt@shopware.com>
2021-05-19 08:42:57 +0000

    NEXT-15022 - Added test for validation of non-entity privileges

---
+++ src/Core/Framework/Test/AdditionalPermissionValidationTest.php
@@ -0,0 +1,153 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
+use Shopware\Core\Kernel;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @group slow
+ */
+class AdditionalPermissionValidationTest extends TestCase
+{
+    use IntegrationTestBehaviour;
+
+    /**
+     * blacklist file path segments for ignored paths
+     *
+     * @var array
+     */
+    private $blacklist = [
+        'Test/',
+        'node_modules/',
+        'Common/vendor/',
+        'Recovery/vendor',
+        'recovery/vendor',
+        'storefront/vendor',
+        'public/static/js',
+    ];
+
+    private string $rootDir;
+
+    public function setUp(): void
+    {
+        $this->rootDir = $this->getPathForClass(Kernel::class);
+    }
+
+    public function testSourceFilesForUnvalidatedPrivileges(): void
+    {
+        $additionalPermission = $this->getAdditionalPermissions();
+
+        if (empty($additionalPermission)) {
+            return;
+        }
+
+        $regexParts = [];
+
+        foreach ($additionalPermission as $permission) {
+            $regexParts[$permission] = '(\"|\')' . $permission . '(\"|\')';
+        }
+
+        $regex = sprintf('/%s/s', implode('|', $regexParts));
+
+        $finder = new Finder();
+        $finder->in([$this->rootDir . '/Administration', $this->rootDir . '/Core'])
+            ->files()
+            ->name('*.php')
+            ->contains($regex);
+
+        foreach ($this->blacklist as $path) {
+            $finder->notPath($path);
+        }
+
+        foreach ($finder->getIterator() as $file) {
+            $filePath = $file->getRealPath();
+            $content = file_get_contents($filePath);
+
+            foreach ($regexParts as $key => $regexPart) {
+                if (preg_match('/' . $regexPart . '/s', $content)) {
+                    unset($regexParts[$key]);
+                }
+            }
+        }
+
+        if (!empty($regexParts)) {
+            static::fail(sprintf(
+                'Found additional permission privileges not validated: %s',
+                implode(', ', array_keys($regexParts))
+            ));
+        }
+    }
+
+    private function getAdditionalPermissions(): array
+    {
+        $entityPermissions = [];
+
+        $registry = $this->getContainer()->get(DefinitionInstanceRegistry::class);
+        $entities = $registry->getDefinitions();
+
+        foreach ($entities as $entity) {
+            foreach ([':read', ':update', ':create', ':delete'] as $action) {
+                $entityPermissions[] = $entity->getEntityName() . $action;
+            }
+        }
+
+        $regex = '/additional_permissions(.*?)(\'|\"|)privileges(\'|\"|):(.*?)\[(.*?)]/s';
+        $finder = new Finder();
+        $finder->in([$this->rootDir . '/Administration', $this->rootDir . '/Core'])
+            ->files()
+            ->path('/acl/')
+            ->name('index.js')
+            ->contains($regex);
+
+        foreach ($this->blacklist as $path) {
+            $finder->notPath($path);
+        }
+
+        $additionalPermission = [];
+
+        foreach ($finder->getIterator() as $file) {
+            $filePath = $file->getRealPath();
+            $content = file_get_contents($filePath);
+
+            preg_match_all($regex, $content, $matches);
+
+            if (isset($matches[5][0])) {
+                $jsArray = '[' . $matches[5][0] . ']';
+
+                try {
+                    // use Yaml to try parsing reconstructed JavaScript array
+                    $results = Yaml::parse($jsArray);
+
+                    $additionalPermission = array_merge($additionalPermission, $results);
+                } catch (\Exception $exception) {
+                    continue;
+                }
+            }
+        }
+
+        foreach ($additionalPermission as $key => $permission) {
+            if (array_search($permission, $entityPermissions, true) !== false) {
+                unset($additionalPermission[$key]);
+            }
+        }
+
+        return array_unique($additionalPermission);
+    }
+
+    private function getPathForClass(string $className): string
+    {
+        $path = realpath(\dirname(KernelLifecycleManager::getClassLoader()->findFile($className)) . '/../');
+
+        if ($path === false) {
+            throw new \LogicException("could not locate filepath for class {$className}");
+        }
+
+        return $path;
+    }
+}

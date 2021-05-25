329344ea1d8d6c7f2fa8844f97e7b8c888349208
uwekock <uwekock@muenster.de>
2021-05-25 19:09:59 +0200

    Update MediaService.php
    The VBA WinHttp.WinHttpRequest.5.1 library from Microsoft Office 2016 adds unfortunately the string "; charset=utf-8" to the content-type of the request. This prevents shopware 6 from loading the media file via api call /api/_action/media/.../upload.

--- src/Core/Content/Media/MediaService.php
+++ src/Core/Content/Media/MediaService.php
@@ -121,7 +121,7 @@ class MediaService
         }
 
         $contentType = $request->headers->get('content_type');
-        if ($contentType === 'application/json') {
+        if (strpos($contentType,'application/json') !== false) {
             return $this->fileFetcher->fetchFileFromURL($request, $tempFile);
         }
 

106b1a62091fbb101f606b48e413333651a02ff3
leichteckig <29896429+leichteckig@users.noreply.github.com>
2021-05-31 19:57:12 +0200

    Add test data nd cleanup

--- src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/listing/filter.spec.js
+++ src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/listing/filter.spec.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
-import generalPageObject from "../../support/pages/general.page-object";
+import generalPageObject from '../../support/pages/general.page-object';
 
-describe("Filter on startpage", () => {
+describe('Filter on startpage', () => {
     beforeEach(() => {
         cy.setToInitialState()
             .then(() => {
@@ -11,36 +11,57 @@ describe("Filter on startpage", () => {
             })
             .then(() => {
                 // Creates a product with manufacturer
-                return cy.createProductFixture();
+                return cy.createProductFixture({
+                    name: 'First product',
+                    productNumber: 'RS-123'
+                });
             })
             .then(() => {
-                cy.visit("/");
+                // Creates a product with manufacturer
+                return cy.createProductFixture({
+                    name: 'Second product',
+                    manufacturerId: null,
+                    productNumber: 'RS-345'
+                });
+            })
+            .then(() => {
+                // Creates a product with manufacturer
+                return cy.createProductFixture({
+                    name: 'Third product',
+                    manufacturerId: null,
+                    productNumber: 'RS-234'
+                });
+            })
+            .then(() => {
+                cy.visit('/');
             });
     });
 
     it("Filter for manufacturer", () => {
         const page = new generalPageObject();
 
-        const actualItems = 24;
+        const actualItems = 3;
         const filteredItems = 1;
-        const manufacturer = "shopware AG";
+        const manufacturer = 'shopware AG';
 
-        cy.get(page.elements.productCard).as("productCard");
+        cy.get(page.elements.productCard).as('productCard');
 
-        cy.get("@productCard").should("have.length", actualItems);
+        cy.get('@productCard').should('have.length', actualItems);
 
         cy.get(page.elements.manufacturerFilter).click();
 
         cy.contains(page.elements.filterLabel, manufacturer).click({
             force: true,
         });
+        cy.url().should('contain', '?manufacturer=');
+        cy.get('.has-element-loader').should('not.exist');
 
-        cy.get("@productCard").should("have.length", filteredItems);
+        cy.get("@productCard").should('have.length', filteredItems);
 
         cy.get("@productCard").first().click();
 
         cy.get(page.elements.productDetailManufacturerLink).should(
-            "contain",
+            'contain',
             manufacturer
         );
     });

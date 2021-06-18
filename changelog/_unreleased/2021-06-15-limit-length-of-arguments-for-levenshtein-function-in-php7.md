4b78761147bfb3d281117ba995fa3d089bda162e
David Neustadt <d.neustadt@shopware.com>
2021-06-18 06:01:32 +0000

    NEXT-15559 - Limit length of arguments for levenshtein function in PHP7

---
+++ changelog/_unreleased/2021-06-15-limit-length-of-arguments-for-levenshtein-function-in-php7.md
@@ -0,0 +1,9 @@
+---
+title: Limit length of arguments for levenshtein function in PHP7
+issue: NEXT-15559
+author: d.neustadt
+author_email: d.neustadt@shopware.com 
+author_github: dneustadt
+---
+# Core
+* Changed string length of `levenshtein` function arguments to be limited to 255 characters in PHP7
--- src/Core/Content/Product/SearchKeyword/ProductSearchTermInterpreter.php
+++ src/Core/Content/Product/SearchKeyword/ProductSearchTermInterpreter.php
@@ -218,7 +218,12 @@ class ProductSearchTermInterpreter implements ProductSearchTermInterpreterInterf
             }
 
             foreach ($tokens as $token) {
-                $levenshtein = levenshtein($match, (string) $token);
+                if (\PHP_VERSION_ID < 80000) {
+                    $levenshtein = levenshtein(substr($match, 0, 255), substr((string) $token, 0, 255));
+                } else {
+                    $levenshtein = levenshtein($match, (string) $token);
+                }
+
                 if ($levenshtein === 0) {
                     $score += 6;
 

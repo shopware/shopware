79b2a5ff990f304196a3b15481a46ad863bc5f43
Jannis Leifeld <j.leifeld@shopware.com>
2021-07-29 08:23:52 +0200

    NEXT-16294 - develop logging for cypress retries

--- src/Administration/Resources/app/administration/test/e2e/cypress/plugins/index.js
+++ src/Administration/Resources/app/administration/test/e2e/cypress/plugins/index.js
@@ -13,6 +13,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const axios = require('axios');
 require('@babel/register');
 
 // TODO Check incompatibility and reintegrate as soon as possible
@@ -38,4 +39,62 @@ module.exports = (on, config) => {
     on('before:browser:launch', () => {
         config.env.projectRoot = config.env.projectRoot || config.env.shopwareRoot;
     });
+
+    // report flaky tests
+    on('after:spec', (spec, results) => {
+        if (!results) {
+            return;
+        }
+
+        // Find all failed tests which contains retry attempts
+        const failedTests = results.tests.filter((test) => {
+            return test.attempts.some((attempt) => attempt.state === 'failed')
+        })
+
+        // stop execution when no failing test exists
+        if (failedTests.length <= 0) {
+            return;
+        }
+
+        // stop execution when a test fails non-flaky
+        if (results.stats?.failures > 0) {
+            return;
+        }
+
+        // stop execution when no environment variable is defined
+        if (!config.env['DD_API_KEY']) {
+            return Promise.resolve();
+        }
+
+        // log each failing test
+        const requestForFailingTests = failedTests.map(failedTest => {
+            const failedAttempts = failedTest.attempts.filter(a => a.state === 'failed').length;
+
+            // report retried tests to datadog
+            return axios({
+                method: 'post',
+                url: 'https://http-intake.logs.datadoghq.eu/v1/input',
+                headers: {
+                    'DD-API-KEY': config.env['DD_API_KEY']
+                },
+                data: {
+                    ddsource: 'cypress-admin',
+                    ddtags: 'cypress:admin,cypress:retries',
+                    message: 'Potential flaky e2e test in admin',
+                    service: 'Cypress',
+                    'test-description': failedTest?.title?.[0],
+                    'test-it': failedTest?.title?.[1],
+                    'test-retries': failedAttempts
+                }
+            })
+        })
+
+        // Directly return Promise.all creates an error. Therefore I need to wrap the promise all
+        // in a separate Promise call.
+        return new Promise((resolve, reject) => {
+            Promise.all([...requestForFailingTests])
+                .then(() => resolve())
+                .catch((e) => reject(e));
+        });
+    })
 };

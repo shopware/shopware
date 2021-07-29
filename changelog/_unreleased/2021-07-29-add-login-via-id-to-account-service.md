f078edbde049d2cf18b1247a04fedc3a866f505a
Max Stegmeyer <m.stegmeyer@shopware.com>
2021-07-29 08:42:07 +0200

    NEXT-16453 - Add login via id to AccountService

---
+++ changelog/_unreleased/2021-07-29-add-login-via-id-to-account-service.md
@@ -0,0 +1,9 @@
+---
+title: Add login via id to AccountService
+issue: NEXT-16453
+author: m.stegmeyer
+author_email: m.stegmeyer@shopware.com
+---
+# Core
+* Added method for login via customer Id in `Shopware\Core\Checkout\Customer\SalesChannel\AccountService`
+
---
+++ src/Core/Checkout/Customer/Exception/CustomerNotFoundByIdException.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Customer\Exception;
+
+use Shopware\Core\Framework\ShopwareHttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerNotFoundByIdException extends ShopwareHttpException
+{
+    public function __construct(string $id)
+    {
+        parent::__construct(
+            'No matching customer for the id "{{ id }}" was found.',
+            ['id' => $id]
+        );
+    }
+
+    public function getErrorCode(): string
+    {
+        return 'CHECKOUT__CUSTOMER_NOT_FOUND_BY_ID';
+    }
+
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_NOT_FOUND;
+    }
+}

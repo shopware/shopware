ac8d0aaf3f8181e3b8699e5cae288837941d0494
Nils Haberkamp <n.haberkamp@shopware.com>
2021-08-12 15:36:37 +0200

    NEXT-16265 - Product variant name is not displayed in rule assingment

--- src/Administration/Resources/app/administration/test/module/sw-settings-rule/view/sw-settings-rule-detail-assignments.spec.js
+++ src/Administration/Resources/app/administration/test/module/sw-settings-rule/view/sw-settings-rule-detail-assignments.spec.js
@@ -19,7 +19,13 @@ function createWrapper(entitiesWithResults = []) {
             },
             'sw-loader': true,
             'sw-empty-state': true,
-            'sw-entity-listing': true
+            'sw-entity-listing': {
+                props: ['items'],
+                template: `
+<div class="sw-entity-listing-stub">
+    <li v-for="(item, index) in items" :key="index">{{ item.name }}</li>
+</div>`
+            }
         },
         propsData: {
             ruleId: 'uuid1',
@@ -34,13 +40,19 @@ function createWrapper(entitiesWithResults = []) {
             repositoryFactory: {
                 create: (entityName) => {
                     return {
-                        search: () => {
+                        search: (_, api) => {
+                            const entities = [
+                                { name: 'Foo' },
+                                { name: 'Bar' },
+                                { name: 'Baz' }
+                            ];
+
+                            if (api.inheritance) {
+                                entities.push({ name: 'Inherited' });
+                            }
+
                             if (entitiesWithResults.includes(entityName)) {
-                                return Promise.resolve(createEntityCollectionMock(entityName, [
-                                    { name: 'Foo' },
-                                    { name: 'Bar' },
-                                    { name: 'Baz' }
-                                ]));
+                                return Promise.resolve(createEntityCollectionMock(entityName, entities));
                             }
 
                             return Promise.resolve(createEntityCollectionMock(entityName));
@@ -131,11 +143,11 @@ describe('src/module/sw-settings-rule/view/sw-settings-rule-detail-assignments',
         await wrapper.vm.$nextTick();
 
         // Expect entity listings to be present
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-product sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-shipping_method sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-payment_method sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-promotion sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-event_action sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-product .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-shipping_method .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-payment_method .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-promotion .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-event_action .sw-entity-listing-stub').exists()).toBeTruthy();
 
         // Loader and empty-state should not be present
         expect(wrapper.find('sw-settings-rule-detail-assignments__empty-state').exists()).toBeFalsy();
@@ -157,13 +169,13 @@ describe('src/module/sw-settings-rule/view/sw-settings-rule-detail-assignments',
         await wrapper.vm.$nextTick();
 
         // Expect entity listings to be present
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-shipping_method sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-payment_method sw-entity-listing-stub').exists()).toBeTruthy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-promotion sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-shipping_method .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-payment_method .sw-entity-listing-stub').exists()).toBeTruthy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-promotion .sw-entity-listing-stub').exists()).toBeTruthy();
 
         // Expect entity listings to be not present for entities without result
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-product sw-entity-listing-stub').exists()).toBeFalsy();
-        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-event_action sw-entity-listing-stub').exists()).toBeFalsy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-product .sw-entity-listing-stub').exists()).toBeFalsy();
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-event_action .sw-entity-listing-stub').exists()).toBeFalsy();
 
         // Loader and empty-state should not be present
         expect(wrapper.find('sw-settings-rule-detail-assignments__empty-state').exists()).toBeFalsy();
@@ -182,5 +194,31 @@ describe('src/module/sw-settings-rule/view/sw-settings-rule-detail-assignments',
 
         expect(wrapper.find('.sw-settings-rule-detail-assignments__empty-state').exists()).toBeTruthy();
         expect(wrapper.find('.sw-settings-rule-detail-assignments__card-loader').exists()).toBeFalsy();
+    });
+
+    it('should render names of product variants', async () => {
+        const wrapper = await createWrapper(['product']);
+
+        // Wait for repository request
+        await wrapper.vm.$nextTick();
+
+        // Wait for loading to be disabled and re-render
+        await wrapper.vm.$nextTick();
+        await wrapper.vm.$nextTick();
+
+        // expect entity listing for products to be present
+        expect(wrapper.find('.sw-settings-rule-detail-assignments__card-product .sw-entity-listing-stub').exists()).toBeTruthy();
+
+        const productAssignments = wrapper.findAll('.sw-settings-rule-detail-assignments__entity-listing-product li');
+
+        // expect the right amount of items
+        expect(productAssignments.length).toBe(4);
+
+        const validNames = ['Foo', 'Bar', 'Baz', 'Inherited'];
+
+        // expect the correct names of the products
+        productAssignments.wrappers.forEach((assignment, index) => {
+            expect(assignment.text()).toBe(validNames[index]);
+        });
     });
 });

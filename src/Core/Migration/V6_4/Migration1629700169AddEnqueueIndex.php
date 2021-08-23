80037409d3952a14effb722ad286688a64929aac
Oliver Skroblin <o.skroblin@shopware.com>
2021-08-23 08:32:00 +0200

    NEXT-16568 - Add index for enqueue.delivery_id

---
+++ src/Core/Migration/V6_4/Migration1629700169AddEnqueueIndex.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_4;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1629700169AddEnqueueIndex extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1629700169;
+    }
+
+    public function update(Connection $connection): void
+    {
+        try {
+            $connection->executeStatement('ALTER TABLE `enqueue` ADD INDEX `delivery_id` (`delivery_id`);');
+        } catch (Exception $e) {
+            // index already exists
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}

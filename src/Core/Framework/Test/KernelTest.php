869ec68db22d0a92c333ba85179d8ee883ecc48e
Soner Sayakci <s.sayakci@shopware.com>
2021-08-26 09:10:36 +0200

    NEXT-15289 - Disable setting timezone in MySQL if not supported

--- src/Core/Framework/Test/KernelTest.php
+++ src/Core/Framework/Test/KernelTest.php
@@ -4,6 +4,7 @@ namespace Shopware\Core\Framework\Test;
 
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\DevOps\Environment\EnvironmentHelper;
 use Shopware\Core\Framework\Plugin\KernelPluginLoader\StaticKernelPluginLoader;
 use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
 use Shopware\Core\Framework\Test\TestCaseHelper\ReflectionHelper;
@@ -83,6 +84,12 @@ class KernelTest extends TestCase
 
     public function testDatabaseTimeZonesAreEqual(): void
     {
+        $env = (bool) EnvironmentHelper::getVariable('SHOPWARE_DBAL_TIMEZONE_SUPPORT_ENABLED', false);
+
+        if ($env === false) {
+            static::markTestSkipped('Database does not support timezones');
+        }
+
         $c = $this->getContainer()->get(Connection::class);
 
         static::assertSame(

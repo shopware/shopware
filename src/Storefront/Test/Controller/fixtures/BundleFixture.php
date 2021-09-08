5bfc7df1e7dd56adef093e4713831c7b8e7d9026
ssltg <s.sluiter@shopware.com>
2021-09-08 14:58:42 +0200

    NEXT-17075 - add test for StorefrontController twig inheritance

---
+++ src/Storefront/Test/Controller/fixtures/BundleFixture.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storefront\Test\Controller\fixtures;
+
+use Shopware\Core\Framework\Bundle;
+
+class BundleFixture extends Bundle
+{
+    public function __construct(string $name, string $path)
+    {
+        $this->name = $name;
+        $this->path = $path;
+    }
+}

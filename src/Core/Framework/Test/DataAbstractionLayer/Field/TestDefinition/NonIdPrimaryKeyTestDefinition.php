f1525504e82940e6779ebcc4a586631731544d41
Jonas Elfering <j.elfering@shopware.com>
2021-09-09 16:06:01 +0200

    NEXT-17105 - Fix EntitySearcher for multiple PKs

---
+++ src/Core/Framework/Test/DataAbstractionLayer/Field/TestDefinition/NonIdPrimaryKeyTestDefinition.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\DataAbstractionLayer\Field\TestDefinition;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\ApiAware;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+
+class NonIdPrimaryKeyTestDefinition extends EntityDefinition
+{
+    public const ENTITY_NAME = 'non_id_primary_key_test';
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;
+    }
+
+    public function since(): ?string
+    {
+        return '6.0.0.0';
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('test_field', 'testField'))->addFlags(new ApiAware(), new PrimaryKey()),
+
+            new StringField('name', 'name'),
+        ]);
+    }
+}

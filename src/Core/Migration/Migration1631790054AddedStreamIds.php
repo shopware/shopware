130b626d58d63c97ca410ff86ec5f6d4ea2bbe2d
Soner Sayakci <s.sayakci@shopware.com>
2021-09-24 10:40:18 +0200

    NEXT-8057 - Fix class name

--- src/Core/Migration/Migration1631790054AddedStreamIds.php
+++ src/Core/Migration/Migration1631790054AddedStreamIds.php
@@ -5,6 +5,6 @@ namespace Shopware\Core\Migration;
 /**
  * @deprecated tag:v6.5.0 - Will be deleted. Migrations are now namespaced by major version
  */
-class Migration1631790054 extends \Shopware\Core\Migration\V6_4\Migration1631790054AddedStreamIds
+class Migration1631790054AddedStreamIds extends \Shopware\Core\Migration\V6_4\Migration1631790054AddedStreamIds
 {
 }

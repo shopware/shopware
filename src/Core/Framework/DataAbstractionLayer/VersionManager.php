604b59e539ea41e5897fb8eacca0d04440c45ebc
BlackScorp <cccpmik@gmail.com>
2021-10-02 20:58:47 +0200

    Fix createdAt Date during clone of entities

--- src/Core/Framework/DataAbstractionLayer/VersionManager.php
+++ src/Core/Framework/DataAbstractionLayer/VersionManager.php
@@ -359,7 +359,12 @@ class VersionManager
 
         $data = $this->filterPropertiesForClone($definition, $data, $keepIds, $id, $definition, $context->getContext());
         $data['id'] = $newId;
-
+        if(isset($data['createdAt'])) {
+            $data['createdAt'] = new \DateTime();
+        }
+        if(isset($data['updatedAt'])) {
+            $data['updatedAt'] = null;
+        }
         $data = array_replace_recursive($data, $behavior->getOverwrites());
 
         $versionContext = $context->createWithVersionId($versionId);

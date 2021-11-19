0c2632097e5328afd511f8191169fbbb9128643a
Frederik Schmitt <f.schmitt@shopware.com>
2021-11-19 12:05:35 +0000

    NEXT-18549 - Separate storage of FRW and Store token

--- src/Core/Framework/Test/Store/Authentication/FrwRequestOptionsProviderTest.php
+++ src/Core/Framework/Test/Store/Authentication/FrwRequestOptionsProviderTest.php
@@ -3,7 +3,11 @@
 namespace Shopware\Core\Framework\Test\Store\Authentication;
 
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Api\Context\Exception\InvalidContextSourceException;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\Store\Authentication\FrwRequestOptionsProvider;
+use Shopware\Core\Framework\Store\Services\FirstRunWizardClient;
 use Shopware\Core\Framework\Test\Store\StoreClientBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 
@@ -12,23 +16,50 @@ class FrwRequestOptionsProviderTest extends TestCase
     use IntegrationTestBehaviour;
     use StoreClientBehaviour;
 
+    private Context $context;
+
     private FrwRequestOptionsProvider $optionsProvider;
+
+    private EntityRepositoryInterface $userConfigRepository;
 
     public function setUp(): void
     {
+        $this->context = $this->createAdminStoreContext();
         $this->optionsProvider = $this->getContainer()->get(FrwRequestOptionsProvider::class);
+        $this->userConfigRepository = $this->getContainer()->get('user_config.repository');
     }
 
-    public function testGetAuthenticationHeadersOverridesStoreToken(): void
+    public function testSetsFrwUserTokenIfPresentInUserConfig(): void
     {
-        $context = $this->createAdminStoreContext();
+        $frwUserToken = 'a84a653a57dc43a48ded4275524893cf';
 
-        $headers = $this->optionsProvider->getAuthenticationHeader($context);
+        $this->userConfigRepository->create([
+            [
+                'userId' => $this->context->getSource()->getUserId(),
+                'key' => FirstRunWizardClient::USER_CONFIG_KEY_FRW_USER_TOKEN,
+                'value' => [
+                    FirstRunWizardClient::USER_CONFIG_VALUE_FRW_USER_TOKEN => $frwUserToken,
+                ],
+            ],
+        ], Context::createDefaultContext());
+
+        $headers = $this->optionsProvider->getAuthenticationHeader($this->context);
 
         static::assertArrayHasKey('X-Shopware-Token', $headers);
-        static::assertEquals(
-            $this->getStoreTokenFromContext($context),
-            $headers['X-Shopware-Token']
-        );
+        static::assertEquals($frwUserToken, $headers['X-Shopware-Token']);
+    }
+
+    public function testRemovesEmptyAuthenticationHeaderIfFrwUserTokenIsNotSet(): void
+    {
+        $headers = $this->optionsProvider->getAuthenticationHeader($this->context);
+
+        static::assertEmpty($headers);
+    }
+
+    public function testThrowsInvalidContextSourceExceptionIfNotAdminApiSource(): void
+    {
+        static::expectException(InvalidContextSourceException::class);
+
+        $this->optionsProvider->getAuthenticationHeader(Context::createDefaultContext());
     }
 }

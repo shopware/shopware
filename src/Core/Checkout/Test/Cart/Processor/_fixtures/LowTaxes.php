2ac4c7971822064e9e3253c4ff290a466b055ad5
Oliver Skroblin <o.skroblin@shopware.com>
2021-11-23 13:32:54 +0100

    NEXT-18619 - Implement new discount and container processor. This processors are build for cart hooks.

---
+++ src/Core/Checkout/Test/Cart/Processor/_fixtures/LowTaxes.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Test\Cart\Processor\_fixtures;
+
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRule;
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRuleCollection;
+
+class LowTaxes extends TaxRuleCollection
+{
+    public function __construct()
+    {
+        parent::__construct([new TaxRule(7)]);
+    }
+}

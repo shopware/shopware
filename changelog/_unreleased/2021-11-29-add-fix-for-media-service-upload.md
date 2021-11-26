9c1234482ecf7c6c0eb58eda96cd6dcb3bc75222
Samuel Lembke <sal@yoyaba.com>
2021-11-26 14:57:41 +0100

    NEXT-19180 - Fixed incorrect implementation of Content-Type check in MediaService fixes #2206

---
+++ changelog/_unreleased/2021-11-29-add-fix-for-media-service-upload.md
@@ -0,0 +1,11 @@
+---
+title: MediaService.php Content-Type check
+issue: https://github.com/shopware/platform/issues/2196
+flag:
+author: Samuel Lembke
+author_email: sl@yoyaba.tech
+author_github: samuellembke
+___
+# API
+*  Fixed Content-Type check in admin API
+___
--- src/Core/Content/Media/MediaService.php
+++ src/Core/Content/Media/MediaService.php
@@ -121,7 +121,7 @@ class MediaService
         }
 
         $contentType = $request->headers->get('content_type');
-        if ($contentType === 'application/json') {
+        if (str_starts_with( $contentType, 'application/json')) {
             return $this->fileFetcher->fetchFileFromURL($request, $tempFile);
         }
 

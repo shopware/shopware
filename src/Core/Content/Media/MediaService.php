e6d292f8d4689b85a530a69556c879db88e6960e
Samuel Lembke <sal@yoyaba.com>
2021-11-26 14:57:41 +0100

    Fixed incorrect implementation of Content-Type check in MediaService

--- src/Core/Content/Media/MediaService.php
+++ src/Core/Content/Media/MediaService.php
@@ -121,7 +121,7 @@ class MediaService
         }
 
         $contentType = $request->headers->get('content_type');
-        if ($contentType === 'application/json') {
+        if (strpos( $contentType , 'application/json' ) === 0) {
             return $this->fileFetcher->fetchFileFromURL($request, $tempFile);
         }
 

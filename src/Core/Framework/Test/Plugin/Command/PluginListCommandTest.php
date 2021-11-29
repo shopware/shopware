f231bcd13247b386727cd8fdbb31292fc2ad49b1
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2021-11-29 10:17:28 +0100

    NEXT-10723 add a bunch of tests

---
+++ src/Core/Framework/Test/Plugin/Command/PluginListCommandTest.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\Plugin\Command;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\MultiFilter;
+use Shopware\Core\Framework\Plugin\Command\PluginListCommand;
+use Shopware\Core\Framework\Plugin\PluginEntity;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class PluginListCommandTest extends TestCase
+{
+    /**
+     * @var MockObject|EntityRepository
+     */
+    private $pluginRepoMock;
+    private PluginListCommand $command;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->pluginRepoMock = $this->createMock(EntityRepository::class);
+        $this->command = new PluginListCommand($this->pluginRepoMock);
+    }
+
+    public function testCommand(): void
+    {
+        $entities = [
+            $this->createMock(PluginEntity::class),
+            $this->createMock(PluginEntity::class)
+        ];
+        $config = [
+            [
+                'getActive'         => true,
+                'getInstalledAt'    => new \DateTimeImmutable('2004-01-01T00:00:00.000001Z'),
+                'getUpgradeVersion' => '3.0.1',
+                'getName'           => 'Plugin List Plugin',
+                'getLabel'          => 'plp',
+                'getVersion'        => '2.5.3',
+                'getAuthor'         => 'Fabian Blechschmidt',
+            ],
+            [
+                'getActive'         => false,
+                'getInstalledAt'    => new \DateTimeImmutable('2019-05-23T00:00:00.000001Z'),
+                'getUpgradeVersion' => '6.0.0',
+                'getName'           => 'Shopware Next',
+                'getLabel'          => 'swn',
+                'getVersion'        => '5.5.3',
+                'getAuthor'         => 'Shopware AG',
+            ],
+        ];
+
+        foreach ($config as $number => $methods) {
+            foreach ($methods as $method => $value) {
+                $plugin = $entities[$number];
+                $plugin->method($method)->willReturn($value);
+            }
+        }
+
+        $this->setupEntityCollection($entities);
+
+        [$statusCode, $output] = $this->executeCommand([]);
+        $this->assertSame(0, $statusCode);
+        $this->assertStringEqualsFile(
+            __DIR__ . '/../_assertion/PluginListCommandTest::testCommand.txt',
+            implode("\n", array_map('trim', explode("\n", $output))) . "\n"
+        );
+    }
+
+    public function testFilter(): void
+    {
+        $filterValue = 'shopware-is-love';
+
+        $criteria = $this->callback(function (Criteria $criteria) use ($filterValue): bool {
+            $filters = $criteria->getFilters();
+            // must be MultiFilter
+            if (!(count($filters) === 1 && $filters[0] instanceof MultiFilter)) {
+                return false;
+            }
+            $filter = $filters[0];
+            // must be OR
+            if ($filter->getOperator() !== MultiFilter::CONNECTION_OR) {
+                return false;
+            }
+            $fields = ['name', 'label'];
+            foreach ($filter->getQueries() as $query) {
+                if (!
+                ($query instanceof ContainsFilter
+                    && $query->getValue() === $filterValue
+                    // first test against name, then label
+                    && $query->getField() === array_shift($fields)
+                )
+                ) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        $this->pluginRepoMock->method('search')->with($criteria, $this->anything());
+
+        [$statusCode, $output] = $this->executeCommand(['--filter' => $filterValue]);
+        $this->assertSame(0, $statusCode);
+        $this->assertStringContainsString('Filtering for: ' . $filterValue, $output);
+    }
+
+    public function testJsonOutput(): void
+    {
+        $o1 = ['PLUGIN1'];
+        $o2 = ['PLUGIN2'];
+
+        $entities = [
+            $plugin1 = $this->createMock(PluginEntity::class),
+            $plugin2 = $this->createMock(PluginEntity::class)
+        ];
+
+        $plugin1->method('jsonSerialize')->willReturn($o1);
+        $plugin2->method('jsonSerialize')->willReturn($o2);
+
+        $this->setupEntityCollection($entities);
+
+        $options = ['--json' => true];
+        $json = json_encode([$o1, $o2], JSON_THROW_ON_ERROR);
+        $expected = [0, $json];
+        $this->assertSame($expected, $this->executeCommand($options));
+    }
+
+    private function executeCommand(array $options): array
+    {
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute($options);
+
+        return [$commandTester->getStatusCode(), trim($commandTester->getDisplay())];
+    }
+
+    private function setupEntityCollection(array $entities): void
+    {
+        $collection = $this->createMock(EntityCollection::class);
+        $collection->method('jsonSerialize')->willReturn($entities);
+        $collection->method('getIterator')->willReturn($this->arrayAsGenerator($entities));
+        $result = $this->createMock(EntitySearchResult::class);
+        $result->method('getEntities')->willReturn($collection);
+        $this->pluginRepoMock->method('search')->willReturn($result);
+    }
+
+    private function arrayAsGenerator(array $array): \Generator
+    {
+        foreach ($array as $item) {
+            yield $item;
+        }
+    }
+}

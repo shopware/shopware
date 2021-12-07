91e156fa125bc5c574ff24bdd87e2f16f7a8cf78
Maike Sestendrup <M.Sestendrup@shopware.com>
2021-12-07 16:00:56 +0100

    NEXT-19195 - Show extension name on config page

--- src/Administration/Resources/app/administration/src/module/sw-extension/page/sw-extension-config/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-extension/page/sw-extension-config/index.js
@@ -9,6 +9,10 @@ const { Component, Mixin } = Shopware;
 Component.register('sw-extension-config', {
     template,
 
+    inject: [
+        'shopwareExtensionService',
+    ],
+
     mixins: [
         Mixin.getByName('notification'),
     ],
@@ -23,6 +27,7 @@ Component.register('sw-extension-config', {
     data() {
         return {
             salesChannelId: null,
+            extension: null,
         };
     },
 
@@ -30,9 +35,47 @@ Component.register('sw-extension-config', {
         domain() {
             return `${this.namespace}.config`;
         },
+
+        myExtensions() {
+            return Shopware.State.get('shopwareExtensions').myExtensions;
+        },
+
+        defaultThemeAsset() {
+            return Shopware.Filter.getByName('asset')('administration/static/img/theme/default_theme_preview.jpg');
+        },
+
+        image() {
+            if (this.extension?.icon) {
+                return this.extension.icon;
+            }
+
+            if (this.extension?.iconRaw) {
+                return `data:image/png;base64, ${this.extension.iconRaw}`;
+            }
+
+            return this.defaultThemeAsset;
+        },
+
+        extensionLabel() {
+            return this.extension?.label ?? this.namespace;
+        },
+    },
+
+    created() {
+        this.createdComponent();
     },
 
     methods: {
+        async createdComponent() {
+            if (!this.myExtensions.data.length) {
+                await this.shopwareExtensionService.updateExtensionData();
+            }
+
+            this.extension = this.myExtensions.data.find((ext) => {
+                return ext.name === this.namespace;
+            });
+        },
+
         onSave() {
             this.$refs.systemConfig.saveAll().then(() => {
                 this.createNotificationSuccess({

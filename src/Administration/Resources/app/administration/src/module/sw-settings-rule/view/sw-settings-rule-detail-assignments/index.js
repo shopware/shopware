c60acbe6bdaf46ea26856eef471396ba5536cc08
Nils Haberkamp <n.haberkamp@shopware.com>
2021-12-10 14:47:30 +0100

    NEXT-18171 - remove feature flag for rule assignments

--- src/Administration/Resources/app/administration/src/module/sw-settings-rule/view/sw-settings-rule-detail-assignments/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-settings-rule/view/sw-settings-rule-detail-assignments/index.js
@@ -1,16 +1,13 @@
 import RuleAssignmentConfigurationService from 'src/module/sw-settings-rule/service/rule-assignment-configuration.service';
-/** @feature-deprecated (flag:FEATURE_NEXT_16902) Replaced with "templateFeatureNext16902"  */
 import template from './sw-settings-rule-detail-assignments.html.twig';
-import templateFeatureNext16902 from './sw-settings-rule-detail-assignments-feature-next-16902.html.twig';
 import './sw-settings-rule-detail-assignments.scss';
 
-const { Component, Mixin, Context, Feature, Utils } = Shopware;
+const { Component, Mixin, Context, Utils } = Shopware;
 const { Criteria } = Shopware.Data;
 
 Component.register('sw-settings-rule-detail-assignments', {
     // eslint-disable-next-line max-len
-    /** @feature-deprecated (flag:FEATURE_NEXT_16902) Rename "templateFeatureNext16902" to "template" and delete old template  */
-    template: Feature.isActive('FEATURE_NEXT_16902') ? templateFeatureNext16902 : template,
+    template,
 
     inject: [
         'repositoryFactory',
@@ -53,13 +50,6 @@ Component.register('sw-settings-rule-detail-assignments', {
             return RuleAssignmentConfigurationService(this.rule.id, this.associationLimit).getConfiguration();
         },
 
-        /** @feature-deprecated (flag:FEATURE_NEXT_16902) tag:v6.5.0 Unused method will be removed */
-        hasNoAssociations() {
-            return this.associationEntities.every((entity) => {
-                return entity.loadedData && entity.loadedData.total === 0;
-            });
-        },
-
         /* eslint-disable max-len */
         /**
          * Definition of the associated entities of the current rule.
@@ -71,162 +61,7 @@ Component.register('sw-settings-rule-detail-assignments', {
          */
         /* eslint-enable max-len */
         associationEntitiesConfig() {
-            if (!this.feature.isActive('FEATURE_NEXT_16902')) {
-                return [
-                    {
-                        entityName: 'product',
-                        label: 'sw-settings-rule.detail.associations.products',
-                        criteria: () => {
-                            const criteria = new Criteria();
-                            criteria.setLimit(this.associationLimit);
-                            criteria.addFilter(Criteria.equals('prices.rule.id', this.rule.id));
-
-                            return criteria;
-                        },
-                        api: () => {
-                            const api = Object.assign({}, Context.api);
-                            api.inheritance = true;
-
-                            return api;
-                        },
-                        detailRoute: 'sw.product.detail.prices',
-                        gridColumns: [
-                            {
-                                property: 'name',
-                                label: 'Name',
-                                rawData: true,
-                                sortable: false,
-                                routerLink: 'sw.product.detail.prices',
-                                allowEdit: false,
-                            },
-                        ],
-                    },
-                    {
-                        entityName: 'shipping_method',
-                        label: 'sw-settings-rule.detail.associations.shippingMethods',
-                        criteria: () => {
-                            const criteria = new Criteria();
-                            criteria.setLimit(this.associationLimit);
-                            criteria.addFilter(
-                                Criteria.multi(
-                                    'OR',
-                                    [
-                                        Criteria.equals('prices.ruleId', this.rule.id),
-                                        Criteria.equals('prices.calculationRuleId', this.rule.id),
-                                        Criteria.equals('availabilityRuleId', this.rule.id),
-                                    ],
-                                ),
-                            );
-
-                            return criteria;
-                        },
-                        detailRoute: 'sw.settings.shipping.detail',
-                        gridColumns: [
-                            {
-                                property: 'name',
-                                label: 'Name',
-                                rawData: true,
-                                sortable: false,
-                                routerLink: 'sw.settings.shipping.detail',
-                                allowEdit: false,
-                            },
-                        ],
-                    },
-                    {
-                        entityName: 'payment_method',
-                        label: 'sw-settings-rule.detail.associations.paymentMethods',
-                        criteria: () => {
-                            const criteria = new Criteria();
-                            criteria.setLimit(this.associationLimit);
-                            criteria.addFilter(Criteria.equals('availabilityRuleId', this.rule.id));
-
-                            return criteria;
-                        },
-                        detailRoute: 'sw.settings.payment.detail',
-                        gridColumns: [
-                            {
-                                property: 'name',
-                                label: 'Name',
-                                rawData: true,
-                                sortable: false,
-                                routerLink: 'sw.settings.payment.detail',
-                                allowEdit: false,
-                            },
-                        ],
-                    },
-                    {
-                        entityName: 'promotion',
-                        label: 'sw-settings-rule.detail.associations.promotions',
-                        criteria: () => {
-                            const criteria = new Criteria();
-                            criteria.setLimit(this.associationLimit);
-                            criteria.addFilter(
-                                Criteria.multi(
-                                    'OR',
-                                    [
-                                        Criteria.equals('personaRules.id', this.rule.id),
-                                        Criteria.equals('orderRules.id', this.rule.id),
-                                        Criteria.equals('cartRules.id', this.rule.id),
-                                        Criteria.equals('discounts.discountRules.id', this.rule.id),
-                                        Criteria.equals('setgroups.setGroupRules.id', this.rule.id),
-                                    ],
-                                ),
-                            );
-
-                            return criteria;
-                        },
-                        detailRoute: 'sw.promotion.v2.detail.conditions',
-                        gridColumns: [
-                            {
-                                property: 'name',
-                                label: 'Name',
-                                rawData: true,
-                                sortable: false,
-                                routerLink: 'sw.promotion.v2.detail.conditions',
-                            },
-                        ],
-                    },
-                    {
-                        entityName: 'event_action',
-                        label: 'sw-settings-rule.detail.associations.eventActions',
-                        criteria: () => {
-                            const criteria = new Criteria();
-                            criteria.setLimit(this.associationLimit);
-                            criteria.addFilter(Criteria.equals('rules.id', this.rule.id));
-
-                            return criteria;
-                        },
-                        detailRoute: 'sw.event.action.detail',
-                        gridColumns: [
-                            {
-                                property: 'eventName',
-                                label: 'Business Event',
-                                rawData: true,
-                                sortable: false,
-                                width: '50%',
-                                routerLink: 'sw.event.action.detail',
-                            },
-                            {
-                                property: 'title',
-                                label: 'Business Event Title',
-                                rawData: true,
-                                sortable: false,
-                                width: '50%',
-                                routerLink: 'sw.event.action.detail',
-                            },
-                        ],
-                    },
-                ];
-            }
-
             return Object.values(this.getRuleAssignmentConfiguration);
-        },
-
-        /** @feature-deprecated (flag:FEATURE_NEXT_16902) tag:v6.5.0 Unused method will be removed */
-        loadedAssociationEntities() {
-            return this.associationEntities.filter((item) => {
-                return item.loadedData && item.loadedData.total > 0;
-            });
         },
     },
 
@@ -373,9 +208,7 @@ Component.register('sw-settings-rule-detail-assignments', {
                     return item.repository.search(item.criteria(), api).then(async (result) => {
                         item.loadedData = result;
 
-                        if (this.feature.isActive('FEATURE_NEXT_16902')) {
-                            item.notAssignedDataTotal = await this.loadNotAssignedDataTotals(item, api);
-                        }
+                        item.notAssignedDataTotal = await this.loadNotAssignedDataTotals(item, api);
                     });
                 }))
                 .catch(() => {

068ec3364af4803df68a22c5d046b37c37decdd0
y.tuer <y.tuer@shopware.com>
2021-12-16 11:17:04 +0100

    NEXT-18030 - Added non-default login and registration settings e2e tests

--- src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/account/register.spec.js
+++ src/Storefront/Resources/app/storefront/test/e2e/cypress/integration/account/register.spec.js
@@ -494,5 +494,119 @@ describe('Account: Register via account menu', () => {
 
             cy.get(`${page.elements.registerSubmit} [type="submit"]`).click();
         });
+
+    });
+
+    it('@registration @package: Check non-default registration fields and allow customer deletion', () => {
+        cy.authenticate().then((result) => {
+            const requestConfig = {
+                headers: {
+                    Authorization: `Bearer ${result.access}`
+                },
+                method: 'POST',
+                url: `api/_action/system-config/batch`,
+                body: {
+                    null: {
+                        'core.loginRegistration.showTitleField': true,
+                        'core.loginRegistration.requireEmailConfirmation': true,
+                        'core.loginRegistration.requirePasswordConfirmation': true,
+                        'core.loginRegistration.showPhoneNumberField': true,
+                        'core.loginRegistration.phoneNumberFieldRequired': true,
+                        'core.loginRegistration.showBirthdayField': true,
+                        'core.loginRegistration.birthdayFieldRequired': true,
+                        'core.loginRegistration.showAdditionalAddressField1': true,
+                        'core.loginRegistration.additionalAddressField1Required': true,
+                        'core.loginRegistration.showAdditionalAddressField2': true,
+                        'core.loginRegistration.additionalAddressField2Required': true,
+                        'core.loginRegistration.requireDataProtectionCheckbox': true,
+                        'core.loginRegistration.allowCustomerDeletion': true
+                    }
+                }
+            };
+            return cy.request(requestConfig);
+        });
+
+        const page = new AccountPageObject();
+
+        // Register customer
+        cy.visit('/account/register');
+        cy.get(page.elements.registerCard).should('be.visible');
+        cy.get(`${page.elements.registerForm} [name="title"]`).typeAndCheckStorefront('Prof. Dr.');
+        cy.get(`${page.elements.registerForm} [name="salutationId"]`).select('Mr.');
+        cy.get(`${page.elements.registerForm} [name="firstName"]`).typeAndCheckStorefront('John');
+        cy.get(`${page.elements.registerForm} [name="lastName"]`).typeAndCheckStorefront('Doe');
+        cy.get(`${page.elements.registerForm} [for="personalBirthday"]`).contains('*').should('be.visible');
+        cy.get(`${page.elements.registerForm} [name="birthdayDay"]`).select('14');
+        cy.get(`${page.elements.registerForm} [name="birthdayMonth"]`).select('11');
+        cy.get(`${page.elements.registerForm} [name="birthdayYear"]`).select('1988');
+        cy.get(`${page.elements.registerForm} [name="email"]`).typeAndCheckStorefront('test@example.com');
+        cy.get(`${page.elements.registerForm} #personalMailConfirmation`).typeAndCheckStorefront('test@example.com');
+        cy.get(`${page.elements.registerForm} [name="password"]`).typeAndCheckStorefront('123456789');
+        cy.get(`${page.elements.registerForm} #personalPasswordConfirmation`).typeAndCheckStorefront('123456789');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressStreet`).typeAndCheckStorefront('Ansgarstr 4');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressZipcode`).typeAndCheckStorefront('49134');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCity`).typeAndCheckStorefront('Wallenhorst');
+        cy.get(`${page.elements.registerForm} [for="billingAddressAdditionalField1"]`).contains('*').should('be.visible');
+        cy.get(`${page.elements.registerForm} #billingAddressAdditionalField1`).typeAndCheckStorefront('Address line 1');
+        cy.get(`${page.elements.registerForm} [for="billingAddressAdditionalField2"]`).contains('*').should('be.visible');
+        cy.get(`${page.elements.registerForm} #billingAddressAdditionalField2`).typeAndCheckStorefront('Address line 2');
+        cy.get(`${page.elements.registerForm} [for="billingAddressAddressPhoneNumber"]`).contains('*').should('be.visible');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCountry`).select('Germany');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCountryState`).select('Berlin');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressPhoneNumber`).typeAndCheckStorefront('0123456789');
+        cy.get(`${page.elements.registerForm} .data-protection-information`).click();
+        cy.get(`${page.elements.registerSubmit} [type="submit"]`).click();
+
+        // Verify registration
+        cy.url().should('not.include', '/register');
+        cy.url().should('include', '/account');
+        cy.get('.account-aside [title="Your profile"]').click();
+        cy.url().should('include', '/profile');
+
+        // Delete account
+        cy.get('.account-deleting.py-4').contains('delete all your personal data').should('be.visible');
+        cy.get('.account-deleting.py-4 > a').click();
+        cy.get('#confirmDeleteAccountModal  .modal-content').should('be.visible');
+        cy.get('.btn.btn-outline-danger').click();
+        cy.url().should('not.include', '/account');
+        cy.get('.alert-content').contains('account was deleted').should('be.visible');
+    });
+    it('@registration @package: Trigger validation error with short password', () => {
+        cy.authenticate().then((result) => {
+            const requestConfig = {
+                headers: {
+                    Authorization: `Bearer ${result.access}`
+                },
+                method: 'POST',
+                url: `api/_action/system-config/batch`,
+                body: {
+                    null: {
+                        'core.loginRegistration.passwordMinLength': 9
+                    }
+                }
+            };
+            return cy.request(requestConfig);
+        });
+
+        const page = new AccountPageObject();
+
+        // Register customer with short password
+        cy.visit('/account/register');
+        cy.get(page.elements.registerCard).should('be.visible');
+        cy.get(`${page.elements.registerForm} [name="salutationId"]`).select('Mr.');
+        cy.get(`${page.elements.registerForm} [name="firstName"]`).typeAndCheckStorefront('John');
+        cy.get(`${page.elements.registerForm} [name="lastName"]`).typeAndCheckStorefront('Doe');
+        cy.get(`${page.elements.registerForm} [name="email"]`).typeAndCheckStorefront('test@example.com');
+        cy.get(`${page.elements.registerForm} [name="password"]`).typeAndCheckStorefront('12345678');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressStreet`).typeAndCheckStorefront('Ansgarstr 4');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressZipcode`).typeAndCheckStorefront('49134');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCity`).typeAndCheckStorefront('Wallenhorst');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCountry`).select('Germany');
+        cy.get(`${page.elements.registerForm} #billingAddressAddressCountryState`).select('Berlin');
+        cy.get(`${page.elements.registerSubmit} [type="submit"]`).click();
+
+        // Verify registration isn't completed
+        cy.url().should('include', '/register');
+        cy.get('[data-form-validation-length-text]').contains('9').should('be.visible');
     });
 });

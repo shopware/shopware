544f74dd4f5fd0ae763321123ff0258efb137b5c
Soner Sayakci <s.sayakci@shopware.com>
2021-12-23 09:46:16 +0100

    NEXT-19313 - Fix environment variable test

--- src/Core/System/Test/BlueGreenDeployment/BlueGreenEnvTest.php
+++ src/Core/System/Test/BlueGreenDeployment/BlueGreenEnvTest.php
@@ -3,27 +3,28 @@
 namespace Shopware\Core\System\Test\BlueGreenDeployment;
 
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\DevOps\Environment\EnvironmentHelper;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 
 class BlueGreenEnvTest extends TestCase
 {
     use IntegrationTestBehaviour;
 
-    private $DEFAULT_ENV_VALUE = '1';
+    private const DEFAULT_ENV_VALUE = '1';
 
     public function testHasCorrectDefaultValue(): void
     {
-        static::assertSame($this->DEFAULT_ENV_VALUE, getenv('BLUE_GREEN_DEPLOYMENT'));
+        static::assertSame(self::DEFAULT_ENV_VALUE, (string) EnvironmentHelper::getVariable('BLUE_GREEN_DEPLOYMENT', self::DEFAULT_ENV_VALUE));
     }
 
     public function testCanChangeValueOfEnvVariable(): void
     {
         // saving the initial value so it can be restored after the test
-        $initialValue = (int) getenv('BLUE_GREEN_DEPLOYMENT');
+        $initialValue = (string) EnvironmentHelper::getVariable('BLUE_GREEN_DEPLOYMENT', self::DEFAULT_ENV_VALUE);
 
-        putenv('BLUE_GREEN_DEPLOYMENT=0');
+        $_SERVER['BLUE_GREEN_DEPLOYMENT'] = '0';
 
-        static::assertSame('0', getenv('BLUE_GREEN_DEPLOYMENT'));
-        putenv('BLUE_GREEN_DEPLOYMENT=' . $initialValue);
+        static::assertSame('0', (string) EnvironmentHelper::getVariable('BLUE_GREEN_DEPLOYMENT', self::DEFAULT_ENV_VALUE));
+        $_SERVER['BLUE_GREEN_DEPLOYMENT'] = $initialValue;
     }
 }

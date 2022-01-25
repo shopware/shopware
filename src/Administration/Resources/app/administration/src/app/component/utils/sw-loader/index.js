75d90a3399cf930df27d68c9157a6f12a1d137a2
Sebastian Seggewiss <s.seggewiss@shopware.com>
2022-01-25 10:13:18 +0100

    NEXT-17344 - Increase sw-loader performance

--- src/Administration/Resources/app/administration/src/app/component/utils/sw-loader/index.js
+++ src/Administration/Resources/app/administration/src/app/component/utils/sw-loader/index.js
@@ -9,25 +9,49 @@ const { Component } = Shopware;
  * @status ready
  * @example-type dynamic
  * @component-example
- * <sw-loader></sw-loader>
+ * <sw-loader />
  */
 Component.register('sw-loader', {
     template,
 
     props: {
+        /**
+         * @deprecated tag:v6.5.0 - Will be validated to be a px value greater or equal to 12px.
+         */
         size: {
             type: String,
             required: false,
             default: '50px',
+            /* validator(value) {
+                return /^(12|[2-9][0-9]|[1-9][2-9]|[1-9]\d{2,})px$/.test(value);
+            }, */
         },
     },
 
     computed: {
         loaderSize() {
             return {
-                width: this.size,
-                height: this.size,
+                width: `${this.numericSize}px`,
+                height: `${this.numericSize}px`,
             };
+        },
+
+        numericSize() {
+            const numericSize = parseInt(this.size, 10);
+
+            if (Number.isNaN(numericSize)) {
+                return 50;
+            }
+
+            if (numericSize < 12) {
+                return 50;
+            }
+
+            return numericSize;
+        },
+
+        borderWidth() {
+            return `${Math.floor(this.numericSize / 12)}px`;
         },
     },
 });

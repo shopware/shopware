bc87735449cd0ec93606cd4266ae7079b14f4043
Marcel Brode <m.brode@shopware.com>
2022-02-18 14:28:35 +0100

    NEXT-17913 - Refactor striphtml.filter to typescript

--- src/Administration/Resources/app/administration/src/app/filter/striphtml.filter.ts
+++ src/Administration/Resources/app/administration/src/app/filter/striphtml.filter.ts
@@ -1,9 +1,11 @@
 const { Filter } = Shopware;
 
-Filter.register('striphtml', (value) => {
+Filter.register('striphtml', (value: string): string => {
     if (!value) {
         return '';
     }
 
     return value.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, '');
 });
+
+export default {};

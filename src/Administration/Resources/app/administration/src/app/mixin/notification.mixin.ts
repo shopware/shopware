423c2a63b60600e113b8af66154e26ac11ea7249
Marcel Brode <m.brode@shopware.com>
2022-02-18 14:14:04 +0100

    NEXT-17913 - Refactor sw-alert to typescript

---
+++ src/Administration/Resources/app/administration/src/app/mixin/notification.mixin.ts
@@ -0,0 +1,107 @@
+const { Mixin } = Shopware;
+
+export type NotificationType = 'info' | 'warning' | 'error' | 'success';
+
+interface notification {
+    variant?: NotificationType,
+    title?: string,
+    message?: string,
+    system?: string,
+    [key: string]: string | undefined,
+}
+
+Mixin.register('notification', {
+    methods: {
+        createNotification(notification: notification) {
+            return Shopware.State.dispatch('notification/createNotification', notification);
+        },
+
+        createNotificationSuccess(config: notification): void {
+            const notification = Object.assign({
+                variant: 'success',
+                // @ts-expect-error
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+                title: this.$tc('global.default.success'),
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createNotificationInfo(config: notification): void {
+            const notification = Object.assign({
+                variant: 'info',
+                // @ts-expect-error
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+                title: this.$tc('global.default.info'),
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createNotificationWarning(config: notification): void {
+            const notification = Object.assign({
+                variant: 'warning',
+                // @ts-expect-error
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+                title: this.$tc('global.default.warning'),
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createNotificationError(config: notification): void {
+            const notification = Object.assign({
+                variant: 'error',
+                // @ts-expect-error
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+                title: this.$tc('global.default.error'),
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createSystemNotificationSuccess(config: notification): void {
+            const notification = Object.assign({
+                variant: 'success',
+                system: true,
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createSystemNotificationInfo(config: notification): void {
+            const notification = Object.assign({
+                variant: 'info',
+                system: true,
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createSystemNotificationWarning(config: notification): void {
+            const notification = Object.assign({
+                variant: 'warning',
+                system: true,
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createSystemNotificationError(config: notification): void {
+            const notification = Object.assign({
+                variant: 'error',
+                system: true,
+            }, config);
+
+            void this.createNotification(notification);
+        },
+
+        createSystemNotification(config: notification): void {
+            const notification = Object.assign({
+                system: true,
+            }, config);
+
+            void this.createNotification(notification);
+        },
+    },
+});

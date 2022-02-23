41e1bb8235878b13568d6caf738a39d6495ab3c1
boxblinkracer <boxblinkracer@gmx.net>
2022-02-23 10:41:33 +0100

    NTR: Admin sort payments alphabetically in sales channels

--- src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-base/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-base/index.js
@@ -133,7 +133,9 @@ Component.register('sw-sales-channel-detail-base', {
         paymentMethodCriteria() {
             const criteria = new Criteria();
 
-            criteria.addSorting(Criteria.sort('position', 'ASC'));
+            // sort by name, because that is easier for
+            // maintenance in the administration
+            criteria.addSorting(Criteria.sort('name', 'ASC'));
 
             return criteria;
         },

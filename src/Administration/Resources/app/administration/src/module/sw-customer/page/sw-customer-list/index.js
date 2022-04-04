264a2b20b5028056b7427deec1f326ad76425064
Y Tran <y.tran@shopware.com>
2022-04-04 16:06:19 +0700

    NEXT-20627 - Fix customer filter does not work with paging

--- src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-list/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-list/index.js
@@ -212,7 +212,7 @@ Component.register('sw-customer-list', {
             const criteria = await Shopware.Service('filterService')
                 .mergeWithStoredFilters(this.storeKey, this.defaultCriteria);
 
-            const newCriteria = await this.addQueryScores(this.term, this.defaultCriteria);
+            const newCriteria = await this.addQueryScores(this.term, criteria);
 
             this.activeFilterNumber = criteria.filters.length;
 

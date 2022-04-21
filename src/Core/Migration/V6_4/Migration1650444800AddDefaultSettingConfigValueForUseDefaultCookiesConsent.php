0cef9a83b7da7553aaf09d1beef41207d4d09ebd
Le Nguyen <l.nguyen@shopware.com>
2022-04-21 14:41:51 +0700

    NEXT-9096 - Disable default cookie notification

---
+++ src/Core/Migration/V6_4/Migration1650444800AddDefaultSettingConfigValueForUseDefaultCookiesConsent.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_4;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+class Migration1650444800AddDefaultSettingConfigValueForUseDefaultCookiesConsent extends MigrationStep
+{
+    private const CONFIG_KEY = 'core.basicInformation.useDefaultCookieConsent';
+
+    public function getCreationTimestamp(): int
+    {
+        return 1650444800;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $configPresent = $connection->fetchColumn('SELECT 1 FROM `system_config` WHERE `configuration_key` = ?', [self::CONFIG_KEY]);
+
+        if ($configPresent !== false) {
+            return;
+        }
+
+        $connection->insert('system_config', [
+            'id' => Uuid::randomBytes(),
+            'configuration_key' => self::CONFIG_KEY,
+            'configuration_value' => '{"_value": true}',
+            'created_at' => (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+        ]);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}

ec44eb603e2382cdb29e36ce893a0085d0618731
max <max@swk-web.com>
2022-04-26 17:57:10 +0200

    fix: Only consider active users if more than one user was found

--- src/Core/Checkout/Test/Customer/SalesChannel/LoginRouteTest.php
+++ src/Core/Checkout/Test/Customer/SalesChannel/LoginRouteTest.php
@@ -112,6 +112,30 @@ class LoginRouteTest extends TestCase
         static::assertArrayHasKey('contextToken', $response);
     }
 
+    public function testValidLoginWithOneInactive(): void
+    {
+        $email = Uuid::randomHex() . '@example.com';
+        $password = 'shopware';
+        // Inactive user with different password
+        $this->createCustomer($password . 'fooBar', $email, null, false);
+        // Active user with correct password
+        $this->createCustomer($password, $email);
+
+        $this->browser
+            ->request(
+                'POST',
+                '/store-api/account/login',
+                [
+                    'email' => $email,
+                    'password' => $password,
+                ]
+            );
+
+        $response = json_decode($this->browser->getResponse()->getContent(), true);
+
+        static::assertArrayHasKey('contextToken', $response);
+    }
+
     public function testLoginWithInvalidBoundSalesChannelId(): void
     {
         static::expectException(UnauthorizedHttpException::class);
@@ -270,7 +294,7 @@ class LoginRouteTest extends TestCase
         );
     }
 
-    private function createCustomer(string $password, ?string $email = null, ?string $boundSalesChannelId = null): string
+    private function createCustomer(string $password, ?string $email = null, ?string $boundSalesChannelId = null, bool $active = true): string
     {
         $customerId = Uuid::randomHex();
         $addressId = Uuid::randomHex();
@@ -322,6 +346,7 @@ class LoginRouteTest extends TestCase
             'salutationId' => $this->getValidSalutationId(),
             'customerNumber' => '12345',
             'boundSalesChannelId' => $boundSalesChannelId,
+            'active' => $active,
         ];
 
         $this->customerRepository->create([$customer], $this->ids->context);

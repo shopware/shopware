d41448f7f9e312f25a9985a5aaf5e94edc3cfeee
Florian Robel <f.robel@shopware.com>
2022-05-02 15:14:46 +0200

    NEXT-21340 - add snapshots to themes

--- tests/e2e/cypress/integration/administration/content/sw-theme/visual.spec.js
+++ tests/e2e/cypress/integration/administration/content/sw-theme/visual.spec.js
@@ -28,11 +28,17 @@ describe('Theme: Visual tests', () => {
         cy.wait('@getData')
             .its('response.statusCode').should('equal', 200);
         cy.get('.sw-theme-list__list').should('be.visible');
+        cy.get('.sw-skeleton__gallery').should('not.exist');
+        cy.prepareAdminForScreenshot();
+        cy.takeSnapshot('[Theme] Listing', '.sw-theme-list__content', null, {percyCSS: '.sw-notification-center__context-button--new-available:after { display: none; }'});
 
         cy.get('.sw-theme-list-item')
             .get('.sw-theme-list-item__title')
             .contains('Shopware default theme')
             .click();
         cy.get('.sw-colorpicker').should('be.visible');
+        cy.get('.sw-skeleton__detail').should('not.exist');
+        cy.prepareAdminForScreenshot();
+        cy.takeSnapshot('[Theme] Shopware default theme', '.sw-theme-manager-detail__info', null, {percyCSS: '.sw-notification-center__context-button--new-available:after { display: none; }'});
     });
 });

fc5fd0974b8cd22c8a2b01d6d5dff66b28e71b5f
pkeil <p.keil@gmx.net>
2022-05-05 13:29:56 +0200

    NEXT-21539 - check deliveries for length fixes #2467

--- src/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/OrderSerializer.php
+++ src/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/OrderSerializer.php
@@ -41,7 +41,7 @@ class OrderSerializer extends EntitySerializer
             $entity['lineItems'] = implode('|', $modifiedLineItems);
         }
 
-        if (isset($entity['deliveries'])) {
+        if (isset($entity['deliveries']) && count($entity['deliveries']) > 0) {
             $entity['deliveries'] = $entity['deliveries']->first()->jsonSerialize();
             $entity['deliveries']['shippingOrderAddress'] = $entity['deliveries']['shippingOrderAddress']->jsonSerialize();
         }

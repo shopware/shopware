d890cd2dc196c47eebfbf51513543e503ec5ee1c
Oliver Skroblin <o.skroblin@shopware.com>
2022-05-20 10:59:04 +0200

    NEXT-21456 - Remove decoration of payment method repository

---
+++ src/Core/Checkout/Payment/DataAbstractionLayer/PaymentMethodValidator.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Payment\DataAbstractionLayer;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Checkout\Payment\Exception\PluginPaymentMethodsDeleteRestrictionException;
+use Shopware\Core\Checkout\Payment\PaymentMethodDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Validation\PreWriteValidationEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class PaymentMethodValidator implements EventSubscriberInterface
+{
+    private Connection $connection;
+
+    /**
+     * @internal
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            PreWriteValidationEvent::class => 'validate',
+        ];
+    }
+
+    public function validate(PreWriteValidationEvent $event): void
+    {
+        $ids = $event->getDeletedPrimaryKeys(PaymentMethodDefinition::ENTITY_NAME);
+
+        $ids = \array_column($ids, 'id');
+
+        if (empty($ids)) {
+            return;
+        }
+
+        $pluginIds = $this->connection->fetchOne(
+            'SELECT id FROM payment_method WHERE id IN (:ids) AND plugin_id IS NOT NULL',
+            ['ids' => $ids],
+            ['ids' => Connection::PARAM_STR_ARRAY]
+        );
+
+        if (!empty($pluginIds)) {
+            throw new PluginPaymentMethodsDeleteRestrictionException();
+        }
+    }
+}

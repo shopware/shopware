364455299c4a100261e030e32d7695f383b84ef1
Soner Sayakci <s.sayakci@shopware.com>
2022-07-13 09:59:08 +0200

    NEXT-22404 - Add not found page caching

---
+++ src/Storefront/Framework/Routing/NotFound/NotFoundPageTagsEvent.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storefront\Framework\Routing\NotFound;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Event\ShopwareEvent;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+
+class NotFoundPageTagsEvent implements ShopwareEvent
+{
+    private array $tags;
+
+    private Request $request;
+
+    private SalesChannelContext $context;
+
+    public function __construct(array $tags, Request $request, SalesChannelContext $context)
+    {
+        $this->tags = $tags;
+        $this->request = $request;
+        $this->context = $context;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context->getContext();
+    }
+
+    public function getSalesChannelContext(): SalesChannelContext
+    {
+        return $this->context;
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    public function addTags(array $tags): void
+    {
+        $this->tags = array_merge($this->tags, $tags);
+    }
+}

c32819a8ffbd31d65e61f1afa3846b78047dfd6e
Stefan Poensgen <mail@stefanpoensgen.de>
2022-08-09 09:01:32 +0200

    feat: add sales channel context restorer order criteria event

---
+++ changelog/_unreleased/2022-08-09-add-sales-channel-context-restorer-order-criteria-event.md
@@ -0,0 +1,9 @@
+---
+title: Add event to modify order criteria in SalesChannelContextRestorer
+author: Stefan Poensgen
+author_email: mail@stefanpoensgen.de
+author_github: @stefanpoensgen
+---
+
+# Core
+* Add `Shopware\Core\System\SalesChannel\Event\SalesChannelContextRestorerOrderCriteriaEvent.php`
---
+++ src/Core/System/SalesChannel/Event/SalesChannelContextRestorerOrderCriteriaEvent.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\System\SalesChannel\Event;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Event\NestedEvent;
+
+class SalesChannelContextRestorerOrderCriteriaEvent extends NestedEvent
+{
+    protected Context $context;
+
+    protected Criteria $criteria;
+
+    public function __construct(Criteria $criteria, Context $context)
+    {
+        $this->context = $context;
+        $this->criteria = $criteria;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+
+    public function getCriteria(): Criteria
+    {
+        return $this->criteria;
+    }
+}

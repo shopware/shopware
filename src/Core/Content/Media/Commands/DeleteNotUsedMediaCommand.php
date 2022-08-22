49bfefeef09feb08ee389a624fb6c5f4c38ebbe0
David Neustadt <d.neustadt@shopware.com>
2022-08-22 08:34:01 +0000

    NEXT-22467 - media:delete-unused option to restrict deletion by folder

--- src/Core/Content/Media/Commands/DeleteNotUsedMediaCommand.php
+++ src/Core/Content/Media/Commands/DeleteNotUsedMediaCommand.php
@@ -5,8 +5,10 @@ namespace Shopware\Core\Content\Media\Commands;
 use Shopware\Core\Content\Media\DeleteNotUsedMediaService;
 use Shopware\Core\Framework\Adapter\Console\ShopwareStyle;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Struct\ArrayStruct;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DeleteNotUsedMediaCommand extends Command
@@ -34,7 +36,13 @@ class DeleteNotUsedMediaCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Deletes all media files that are never used');
+            ->setDescription('Deletes all media files that are never used')
+            ->addOption(
+                'folder-entity',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Restrict deletion of not used media in default location folders of the provided entity name'
+            );
     }
 
     /**
@@ -45,6 +53,13 @@ class DeleteNotUsedMediaCommand extends Command
         $io = new ShopwareStyle($input, $output);
 
         $context = Context::createDefaultContext();
+
+        if (\is_string($input->getOption('folder-entity'))) {
+            $context->addExtension(
+                DeleteNotUsedMediaService::RESTRICT_DEFAULT_FOLDER_ENTITIES_EXTENSION,
+                new ArrayStruct([strtolower($input->getOption('folder-entity'))])
+            );
+        }
 
         $count = $this->deleteMediaService->countNotUsedMedia($context);
 

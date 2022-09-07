ee9b3d6056361cd93dcd0ed23f9da5afdb2b2af6
Fayti1703 <fayti1703@protonmail.com>
2022-09-07 21:47:21 +0200

    NEXT-24825 - Fix Plugin Template Order fixes #2675

---
+++ tests/unit/php/Storefront/Theme/Twig/ThemeInheritanceBuilderTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Storefront\Theme\Twig;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Storefront\Theme\StorefrontPluginConfiguration\StorefrontPluginConfiguration;
+use Shopware\Storefront\Theme\StorefrontPluginConfiguration\StorefrontPluginConfigurationCollection;
+use Shopware\Storefront\Theme\StorefrontPluginRegistryInterface;
+use Shopware\Storefront\Theme\Twig\ThemeInheritanceBuilder;
+
+/**
+ * @internal
+ *
+ * @covers \Shopware\Storefront\Theme\Twig\ThemeInheritanceBuilder
+ */
+class ThemeInheritanceBuilderTest extends TestCase
+{
+    private ThemeInheritanceBuilder $builder;
+
+    public function setUp(): void
+    {
+        $this->builder = new ThemeInheritanceBuilder(new TestStorefrontPluginRegistry(
+            new StorefrontPluginConfigurationCollection([
+                new StorefrontPluginConfiguration('Storefront'),
+            ])
+        ));
+    }
+
+    public function testBuildPreservesThePluginOrder(): void
+    {
+        $result = $this->builder->build([
+            'ExtensionPlugin' => [],
+            'BasePlugin' => [],
+            'Storefront' => [],
+        ], [
+            'Storefront' => [],
+        ]);
+
+        static::assertSame([
+            'ExtensionPlugin' => [],
+            'BasePlugin' => [],
+            'Storefront' => [],
+        ], $result);
+    }
+}
+
+/**
+ * @internal
+ */
+class TestStorefrontPluginRegistry implements StorefrontPluginRegistryInterface
+{
+    private StorefrontPluginConfigurationCollection $plugins;
+
+    public function __construct(StorefrontPluginConfigurationCollection $plugins)
+    {
+        $this->plugins = $plugins;
+    }
+
+    public function getConfigurations(): StorefrontPluginConfigurationCollection
+    {
+        return $this->plugins;
+    }
+}

c6c6aa8e1819c130dc0079e840bf7fa4340144d5
Linus Jolmes <l.jolmes@gmx.de>
2022-09-28 10:12:00 +0200

    NEXT-23477 - Add missing types to CustomFieldTypes
    Add:
    - checkbox
    - date
    - number
    fixes #2711

--- src/Core/System/CustomField/CustomFieldTypes.php
+++ src/Core/System/CustomField/CustomFieldTypes.php
@@ -5,12 +5,15 @@ namespace Shopware\Core\System\CustomField;
 final class CustomFieldTypes
 {
     public const BOOL = 'bool';
+    public const CHECKBOX = 'checkbox'
     public const COLORPICKER = 'colorpicker';
+    public const DATE = 'date';
     public const DATETIME = 'datetime';
     public const ENTITY = 'entity';
     public const FLOAT = 'float';
     public const INT = 'int';
     public const JSON = 'json';
+    public const NUMBER = 'number';
     public const PRICE = 'price';
     public const HTML = 'html';
     public const MEDIA = 'media';

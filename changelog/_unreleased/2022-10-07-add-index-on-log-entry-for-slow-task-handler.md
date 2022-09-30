c0e179c3be7f6362a46f60fb46ec2edf3704c443
Michiel Kalle <m.kalle@xsarus.nl>
2022-09-30 14:48:24 +0200

    NEXT-23615 - Fix slow cart.cleanup handler fixes #2712

---
+++ changelog/_unreleased/2022-10-07-add-index-on-log-entry-for-slow-task-handler.md
@@ -0,0 +1,9 @@
+---
+title: add-index-on-log-entry-for-slow-task-handler
+issue: 
+author: Michiel Kalle
+author_email: m.kalle@xsarus.nl
+author_github: @michielkalle
+___
+# Core
+* Add `idx.log_entry.created_at` on the `log_entry` table to fix slow `LogCleanupTaskHandler`.
---
+++ src/Core/Migration/Migration1664541794AddIndexForLogEntryTask.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @deprecated tag:v6.4.0 - Will be deleted. Migrations are now namespaced by major version
+ */
+class Migration1664541794AddIndexForLogEntryTask extends \Shopware\Core\Migration\V6_4\Migration1664541794AddIndexForLogEntryTask
+{
+}

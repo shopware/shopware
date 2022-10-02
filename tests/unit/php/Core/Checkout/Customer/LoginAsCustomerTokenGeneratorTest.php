852e6617325f0f28708074ae92864cb91fa796ad
Ugurkan Kaya <ugurkankya@gmail.com>
2022-10-03 01:07:37 +0200

    [NEXT-8593] add unit test for LoginAsCustomerTokenGenerator

---
+++ tests/unit/php/Core/Checkout/Customer/LoginAsCustomerTokenGeneratorTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Checkout\Customer;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Customer\Exception\InvalidLoginAsCustomerTokenException;
+use Shopware\Core\Checkout\Customer\LoginAsCustomerTokenGenerator;
+
+/**
+ * @internal
+ *
+ * @covers \Shopware\Core\Checkout\Customer\LoginAsCustomerTokenGenerator
+ */
+class LoginAsCustomerTokenGeneratorTest extends TestCase
+{
+    private LoginAsCustomerTokenGenerator $tokenGenerator;
+
+    private const SALES_CHANNEL_ID = '0146543d6a6241718da05d5ee6f6891a';
+
+    private const CUSTOMER_ID = 'bcf76884cb764eb2b9650bb2fcf1073e';
+
+    protected function setUp(): void
+    {
+        $this->tokenGenerator = new LoginAsCustomerTokenGenerator('testAppSecret');
+    }
+
+    public function testGenerate(): void
+    {
+        $token = $this->tokenGenerator->generate(self::SALES_CHANNEL_ID, self::CUSTOMER_ID);
+
+        $this->assertSame('88b0bd156c5b7b5e62bbad80da8c7f7195ded6f5', $token);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testValidate(): void
+    {
+        $this->tokenGenerator->validate('88b0bd156c5b7b5e62bbad80da8c7f7195ded6f5', self::SALES_CHANNEL_ID, self::CUSTOMER_ID);
+    }
+
+    public function testValidateWithInvalidToken(): void
+    {
+        $this->expectException(InvalidLoginAsCustomerTokenException::class);
+
+        $this->tokenGenerator->validate('invalidToken', self::SALES_CHANNEL_ID, self::CUSTOMER_ID);
+    }
+}

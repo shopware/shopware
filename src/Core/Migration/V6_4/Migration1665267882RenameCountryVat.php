08cee3cf5b83560e4af39ebfd1c4875a1a9f572e
FK <fk@bitsandlikes.de>
2022-10-10 19:50:15 +0200

    Migration and test were rewritten & extended according to the pull request comments.

--- src/Core/Migration/V6_4/Migration1665267882RenameCountryVat.php
+++ src/Core/Migration/V6_4/Migration1665267882RenameCountryVat.php
@@ -20,20 +20,51 @@ class Migration1665267882RenameCountryVat extends MigrationStep
 
     public function update(Connection $connection) : void
     {
-        $results = $connection->fetchFirstColumn('SELECT id FROM country WHERE iso = :iso AND iso3 = :iso3', ['iso' => 'VA', 'iso3' => 'VAT']);
+        $countryId = $connection->fetchOne('SELECT id FROM country WHERE iso = :iso AND iso3 = :iso3', ['iso' => 'VA', 'iso3' => 'VAT']);
+        if ($countryId === false) {
+            return;
+        }
 
-        $countryId = $results[0];
+        $sql = 'SELECT language.name, country_translation.name FROM country_translation
+        LEFT JOIN country ON country.id = country_translation.country_id
+        LEFT JOIN language ON language.id = country_translation.language_id
+        WHERE country.iso = :iso AND country.iso3 = :iso3';
 
-        $translations = new Translations(
-            [
+        $currentTranslations = $connection->fetchAllKeyValue($sql, ['iso' => 'VA', 'iso3' => 'VAT']);
+        if (empty($currentTranslations)) {
+            return;
+        }
+
+        $replacements = [];
+        if (($currentTranslations['English'] ?? null) === 'Holy See') {
+            $replacements['English'] = 'Vatican City';
+        }
+        if (($currentTranslations['Deutsch'] ?? null) === 'Heiliger Stuhl') {
+            $replacements['Deutsch'] = 'Staat Vatikanstadt';
+        }
+        if (empty($replacements)) {
+            return;
+        }
+
+        $languageIds = $connection->fetchAllKeyValue('SELECT language.name, language.id FROM language');
+        if (empty($languageIds)) {
+            return;
+        }
+
+        foreach ($replacements as $languageName => $newCountryName) {
+            $languageId = $languageIds[$languageName] ?? null;
+            if ($languageId === null) {
+                continue;
+            }
+            $data     = [
+                'name' => $newCountryName,
+            ];
+            $criteria = [
                 'country_id' => $countryId,
-                'name'       => 'Staat Vatikanstadt',
-            ], [
-                'country_id' => $countryId,
-                'name'       => 'Vatican City',
-            ]
-        );
-        $this->importTranslation('country_translation', $translations, $connection);
+                'language_id' => $languageId,
+            ];
+            $connection->update('country_translation', $data, $criteria);
+        }
     }
 
     public function updateDestructive(Connection $connection) : void
--- tests/migration/Core/V6_4/Migration1665267882RenameCountryVatTest.php
+++ tests/migration/Core/V6_4/Migration1665267882RenameCountryVatTest.php
@@ -4,43 +4,108 @@ namespace Shopware\Tests\Migration\Core\V6_4;
 
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
 use Shopware\Core\Migration\V6_4\Migration1665267882RenameCountryVat;
+use Shopware\Tests\Migration\MigrationTestTrait;
 
 class Migration1665267882RenameCountryVatTest extends TestCase
 {
-    use KernelTestBehaviour;
+    use MigrationTestTrait;
 
-    public function testChanges() : void
+    private const NAME_EN = 'Holy See';
+    private const NAME_DE = 'Heiliger Stuhl';
+    private Connection $connection;
+
+    public function testCountryVaticanCityExist()
     {
-        /** @var Connection $connection */
-        $connection = $this->getContainer()->get(Connection::class);
+        $countryId = $this->connection->fetchOne('SELECT id FROM country WHERE iso = :iso AND iso3 = :iso3', ['iso' => 'VA', 'iso3' => 'VAT']);
 
-        $sql = 'SELECT language.name, country_translation.name FROM country
-LEFT JOIN country_translation ON country.id = country_translation.country_id
-LEFT JOIN language ON language.id = country_translation.language_id
-WHERE country.iso = :iso AND country.iso3 = :iso3';
+        $this->assertIsNotBool($countryId, 'Country \'Vatican City\' not found');
+    }
 
-        $translations = $connection->fetchAllKeyValue($sql, ['iso' => 'VA', 'iso3' => 'VAT']);
-        $this->assertEquals(
-            [
-                'English' => 'Holy See',
-                'Deutsch' => 'Heiliger Stuhl',
-            ],
-            $translations
-        );
+    public function testFetchOfNonExistingCountryId()
+    {
+        $countryId = $this->connection->fetchOne('SELECT id FROM country WHERE iso = :iso AND iso3 = :iso3', ['iso' => 'VA3', 'iso3' => 'VAT3']);
+
+        $this->assertIsBool($countryId);
+        $this->assertEquals($countryId, false);
+    }
+
+    public function testPrepare()
+    {
+        $expected = [
+            'English' => 'Holy See',
+            'Deutsch' => 'Heiliger Stuhl',
+        ];
+        $this->assertEquals($expected, $this->fetchCurrentTranslations());
+    }
+
+    public function testUpdateCountryName() : void
+    {
+        $this->testPrepare();
+        $migration = new Migration1665267882RenameCountryVat();
+        $migration->update($this->connection);
+
+        $expected = [
+            'English' => 'Vatican City',
+            'Deutsch' => 'Staat Vatikanstadt',
+        ];
+        $this->assertEquals($expected, $this->fetchCurrentTranslations());
+    }
+
+    public function testSkipUpdateIfManuelEdited() : void
+    {
+        $this->testPrepare();
+
+        $manuelEditedName = 'Vatikanstadt';
+
+        $this->prepareCurrentName('Deutsch', $manuelEditedName);
 
         $migration = new Migration1665267882RenameCountryVat();
-        $migration->update($connection);
+        $migration->update($this->connection);
 
-        $translations = $connection->fetchAllKeyValue($sql, ['iso' => 'VA', 'iso3' => 'VAT']);
-        $this->assertEquals(
-            [
-                'English' => 'Vatican City',
-                'Deutsch' => 'Staat Vatikanstadt',
-            ],
-            $translations
-        );
+        $expected = [
+            'English' => 'Vatican City',
+            'Deutsch' => $manuelEditedName,
+        ];
+        $this->assertEquals($expected, $this->fetchCurrentTranslations());
+    }
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->connection = KernelLifecycleManager::getConnection();
+        $this->prepare();
+    }
+
+    protected function prepare()
+    {
+        $this->prepareCurrentName('English', self::NAME_EN);
+        $this->prepareCurrentName('Deutsch', self::NAME_DE);
+    }
+
+    protected function fetchCurrentTranslations()
+    {
+        $sql = 'SELECT language.name, country_translation.name FROM country_translation
+        LEFT JOIN country ON country.id = country_translation.country_id
+        LEFT JOIN language ON language.id = country_translation.language_id
+        WHERE country.iso = :iso AND country.iso3 = :iso3';
+
+        return $this->connection->fetchAllKeyValue($sql, ['iso' => 'VA', 'iso3' => 'VAT']);
+    }
+
+    protected function prepareCurrentName(string $languageName, string $countryName)
+    {
+        $sql = 'UPDATE `country_translation`
+LEFT JOIN country ON country.id = country_translation.country_id
+LEFT JOIN language ON language.id = country_translation.language_id
+SET country_translation.name = :name
+WHERE language.name = :language AND country.iso3 = \'VAT\'';
+
+        $this->connection->executeStatement($sql, [
+            'language' => $languageName,
+            'name' => $countryName,
+        ]);
     }
 
 }

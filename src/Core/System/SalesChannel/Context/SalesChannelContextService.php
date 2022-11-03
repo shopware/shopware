acf251bc81feaf41482903822533944f308c3b78
Martin Krzykawski <m.krzykawski@shopware.com>
2022-11-03 09:27:50 +0000

    NEXT-23355 - Dispatch event if sales channel context was created

--- src/Core/System/SalesChannel/Context/SalesChannelContextService.php
+++ src/Core/System/SalesChannel/Context/SalesChannelContextService.php
@@ -6,7 +6,9 @@ use Shopware\Core\Checkout\Cart\CartRuleLoader;
 use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
 use Shopware\Core\Framework\Util\Random;
 use Shopware\Core\Profiling\Profiler;
+use Shopware\Core\System\SalesChannel\Event\SalesChannelContextCreatedEvent;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class SalesChannelContextService implements SalesChannelContextServiceInterface
 {
@@ -46,6 +48,8 @@ class SalesChannelContextService implements SalesChannelContextServiceInterface
 
     private CartService $cartService;
 
+    private EventDispatcherInterface $eventDispatcher;
+
     /**
      * @internal
      */
@@ -53,12 +57,14 @@ class SalesChannelContextService implements SalesChannelContextServiceInterface
         AbstractSalesChannelContextFactory $factory,
         CartRuleLoader $ruleLoader,
         SalesChannelContextPersister $contextPersister,
-        CartService $cartService
+        CartService $cartService,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->factory = $factory;
         $this->ruleLoader = $ruleLoader;
         $this->contextPersister = $contextPersister;
         $this->cartService = $cartService;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function get(SalesChannelContextServiceParameters $parameters): SalesChannelContext
@@ -93,6 +99,7 @@ class SalesChannelContextService implements SalesChannelContextServiceInterface
             }
 
             $context = $this->factory->create($token, $parameters->getSalesChannelId(), $session);
+            $this->eventDispatcher->dispatch(new SalesChannelContextCreatedEvent($context, $token));
 
             $result = $this->ruleLoader->loadByToken($context, $token);
 

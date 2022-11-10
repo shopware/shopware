b10c09baa0a68db66c2abb8ea9f733d529885c29
Markus Velt <m.velt@shopware.com>
2022-11-10 13:53:08 +0100

    NEXT-23484 - Clean up versionIds when editing orders

--- src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
@@ -39,6 +39,7 @@ Component.register('sw-order-detail', {
             createdById: '',
             isDisplayingLeavePageWarning: false,
             nextRoute: null,
+            hasNewVersionId: false,
         };
     },
 
@@ -125,6 +126,8 @@ Component.register('sw-order-detail', {
     },
 
     beforeDestroy() {
+        this.beforeDestroyComponent();
+
         State.unregisterModule('swOrderDetail');
     },
 
@@ -151,6 +154,17 @@ Component.register('sw-order-detail', {
             if (Shopware.Feature.isActive('FEATURE_NEXT_7530')) {
                 State.commit('swOrderDetail/setVersionContext', Shopware.Context.api); // ?? do we need that anymore?
                 this.createNewVersionId();
+            }
+        },
+
+        async beforeDestroyComponent() {
+            if (Shopware.Feature.isActive('FEATURE_NEXT_7530') && this.hasNewVersionId) {
+                // clean up recently created version
+                await this.orderRepository.deleteVersion(
+                    this.orderId,
+                    this.versionContext.versionId,
+                    this.versionContext,
+                );
             }
         },
 
@@ -348,6 +362,8 @@ Component.register('sw-order-detail', {
 
         createNewVersionId() {
             return this.orderRepository.createVersion(this.orderId, this.versionContext).then((newContext) => {
+                this.hasNewVersionId = true;
+
                 State.commit('swOrderDetail/setVersionContext', newContext);
 
                 this.orderRepository.get(this.orderId, newContext, this.orderCriteria).then((response) => {

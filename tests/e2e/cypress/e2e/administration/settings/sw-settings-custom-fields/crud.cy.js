c57a3ba57ea4449d50d8300410f97a3bad540628
Patrick Weyck <p.weyck@shopware.com>
2022-11-23 14:08:45 +0000

    NEXT-22737 - Move @settings: create and read custom field into quarantine

--- tests/e2e/cypress/e2e/administration/settings/sw-settings-custom-fields/crud.cy.js
+++ tests/e2e/cypress/e2e/administration/settings/sw-settings-custom-fields/crud.cy.js
@@ -52,7 +52,7 @@ describe('Custom Fields: Test crud operations', () => {
         cy.wait('@saveData').its('response.statusCode').should('equal', 204);
     });
 
-    it('@settings: create and read custom field', { tags: ['pa-system-settings'] }, () => {
+    it('@settings: create and read custom field', { tags: ['pa-system-settings', 'quarantined'] }, () => {
         // Request we want to wait for later
         cy.intercept({
             url: `${Cypress.env('apiPath')}/custom-field-set`,

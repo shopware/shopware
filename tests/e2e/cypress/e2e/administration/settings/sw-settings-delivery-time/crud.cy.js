6df3ab7912f63f58d6b9b739e50530c2b6c4fb19
Patrick Weyck <p.weyck@shopware.com>
2022-11-25 07:29:57 +0000

    NEXT-22737 - Move @settings: Update and read delivery time into quarantine

--- tests/e2e/cypress/e2e/administration/settings/sw-settings-delivery-time/crud.cy.js
+++ tests/e2e/cypress/e2e/administration/settings/sw-settings-delivery-time/crud.cy.js
@@ -97,7 +97,7 @@ describe('Delivery times group: Test crud operations', () => {
             .should('contain', 'This field must not be empty.');
     });
 
-    it('@settings: Update and read delivery time', { tags: ['pa-customers-orders'] }, () => {
+    it('@settings: Update and read delivery time', { tags: ['pa-customers-orders', 'quarantined'] }, () => {
         const page = new SettingsPageObject();
 
         cy.intercept({

15cd07c3d82d1d8e216ebe2409eca0fb6bf8718b
Jeremy Kemmler <45293021+LostFeelings@users.noreply.github.com>
2022-11-30 09:44:24 +0100

    NEXT-24393 Fix Product Export for Product Comparison
    Added use statement for EqualsAnyFilter

--- src/Core/Content/ProductExport/ScheduledTask/ProductExportGenerateTaskHandler.php
+++ src/Core/Content/ProductExport/ScheduledTask/ProductExportGenerateTaskHandler.php
@@ -8,6 +8,7 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\MultiFilter;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
 use Shopware\Core\Framework\Uuid\Uuid;

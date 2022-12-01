b749096a78a293b8f389b69970b87d0a67a96b5a
Jan Matthiesen <jm@netinventors.de>
2022-12-01 12:17:49 +0100

    Add $definitaionClass type / extend constructor

---
+++ changelog/_unreleased/2022-12-01-optimize-entity-type.md
@@ -0,0 +1,13 @@
+---
+title: Optimize EntityType
+issue:
+flag:
+author: Jan Matthiesen
+author_email: jm@netinventors.de
+author_github: jmatthiesen81
+---
+
+# Core
+* Added more specifying type declaration PHP dockblocks.
+* Added class member definition `\Shopware\Core\Framework\Event\EventData\EntityType::$definitionClass` to improve code quality.
+* Changed class constructor `\Shopware\Core\Framework\Event\EventData\EntityType::__construct` to add the possibility to pass an object of `\Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition` to the constructor.
--- src/Core/Framework/Event/EventData/EntityType.php
+++ src/Core/Framework/Event/EventData/EntityType.php
@@ -2,17 +2,26 @@
 
 namespace Shopware\Core\Framework\Event\EventData;
 
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+
 class EntityType implements EventDataType
 {
     public const TYPE = 'entity';
 
     /**
-     * @var string
+     * @var class-string<EntityDefinition>
      */
-    private $definitionClass;
+    private string $definitionClass;
 
-    public function __construct(string $definitionClass)
+    /**
+     * @param class-string<EntityDefinition>|EntityDefinition $definitionClass
+     */
+    public function __construct(string|EntityDefinition $definitionClass)
     {
+        if ($definitionClass instanceof EntityDefinition) {
+            $definitionClass = $definitionClass::class;
+        }
+
         $this->definitionClass = $definitionClass;
     }
 

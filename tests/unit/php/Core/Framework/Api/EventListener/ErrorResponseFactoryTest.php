d51c2e1d024f7a506711756f1a44a6cadd6d3779
Krzykawski <martin.krzykawski1@gmail.com>
2022-12-12 15:26:44 +0100

    NEXT-24215 - Changed that testStackTraceForExceptionInDebugMode asserts on few first classes in stack trace

--- tests/unit/php/Core/Framework/Api/EventListener/ErrorResponseFactoryTest.php
+++ tests/unit/php/Core/Framework/Api/EventListener/ErrorResponseFactoryTest.php
@@ -4,6 +4,7 @@ namespace Shopware\Tests\Unit\Core\Framework\Api\EventListener;
 
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Api\EventListener\ErrorResponseFactory;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @internal
@@ -14,25 +15,39 @@ class ErrorResponseFactoryTest extends TestCase
 {
     public function testStackTraceForExceptionInDebugMode(): void
     {
+        $message = 'this is an error';
         $factory = new ErrorResponseFactory();
 
-        /* @var \Symfony\Component\HttpFoundation\JsonResponse $response */
-        $response = $factory->getResponseFromException(new \Exception('test'), true);
+        /* @var JsonResponse $response */
+        $response = $factory->getResponseFromException(new \Exception($message), true);
         $data = null;
         if ($response->getContent()) {
             $data = json_decode($response->getContent(), true);
         }
-        var_dump($data['stack']);
-        static::assertArrayHasKey('stack', $data);
-        static::assertIsArray($data['stack']);
+
+        $errors = $data['errors'];
+        static::assertCount(1, $errors);
+        static::assertSame($message, $errors[0]['detail']);
+
+        $stack = $data['stack'];
+        static::assertSame(self::class, $stack[0]['class']);
+        static::assertSame('testStackTraceForExceptionInDebugMode', $stack[0]['function']);
+
+        static::assertSame(TestCase::class, $stack[1]['class']);
+        static::assertSame('runTest', $stack[1]['function']);
+
+        static::assertSame(TestCase::class, $stack[2]['class']);
+        static::assertSame('runBare', $stack[2]['function']);
+
+        static::assertCount(12, $stack);
     }
 
     public function testNoStackTraceForExceptionNotInDebugMode(): void
     {
         $factory = new ErrorResponseFactory();
 
-        /* @var \Symfony\Component\HttpFoundation\JsonResponse $response */
-        $response = $factory->getResponseFromException(new \Exception('test'), false);
+        /* @var JsonResponse $response */
+        $response = $factory->getResponseFromException(new \Exception('test'));
         $data = null;
         if ($response->getContent()) {
             $data = json_decode($response->getContent(), true);

f0ecf4eab54071f5b8b700f51c9a1e07b238ca7f
Oliver Skroblin <o.skroblin@shopware.com>
2022-12-16 16:10:20 +0100

    NEXT-23894 - Added rector as dev dependency. Added phpstan and rector rule for package annotation

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/PackageAnnotationRule.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/PackageAnnotationRule.php
@@ -12,11 +12,64 @@ use PHPStan\Rules\RuleError;
  * @implements Rule<InClassNode>
  *
  * @internal
+ *
+ * @package core
  */
 class PackageAnnotationRule implements Rule
 {
-    private const PRODUCT_AREA_MAPPING = [
-        'business-ops' => '/Shopware\\\\.*\\\\(Rule|Flow|ProductStream)\\\\/',
+    /**
+     * @internal
+     */
+    public const PRODUCT_AREA_MAPPING = [
+        'core' => [
+            '/Shopware\\\\Core\\\\Framework\\\\(Adapter|Api|App|Changelog|DataAbstractionLayer|Demodata|DependencyInjection|)\\\\/',
+            '/Shopware\\\\Core\\\\Framework\\\\(Increment|Log|MessageQueue|Migration|Parameter|Plugin|RateLimiter|Script|Routing|Struct|Util|Uuid|Validation|Webhook)\\\\/',
+            '/Shopware\\\\Core\\\\DevOps\\\\/',
+            '/Shopware\\\\Core\\\\Installer\\\\/',
+            '/Shopware\\\\Core\\\\Maintenance\\\\/',
+            '/Shopware\\\\Core\\\\Migration\\\\/',
+            '/Shopware\\\\Core\\\\Profiling\\\\/',
+            '/Shopware\\\\Elasticsearch\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(Annotation|CustomEntity|DependencyInjection|SystemConfig)\\\\/',
+            '/Shopware\\\\.*\\\\(DataAbstractionLayer)\\\\/',
+        ],
+        'business-ops' => [
+            '/Shopware\\\\.*\\\\(Rule|Flow|ProductStream)\\\\/',
+            '/Shopware\\\\Core\\\\Framework\\\\(Event)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(Tag)\\\\/',
+        ],
+        'inventory' => [
+            '/Shopware\\\\Core\\\\Content\\\\(Product|ProductExport|Property)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(Currency|Unit)\\\\/',
+        ],
+        'content' => [
+            '/Shopware\\\\Core\\\\Content\\\\(Media|Category|Cms|ContactForm|LandingPage)\\\\/',
+        ],
+        'system-settings' => [
+            '/Shopware\\\\Core\\\\Content\\\\(ImportExport|Mail|)\\\\/',
+            '/Shopware\\\\Core\\\\Framework\\\\(Update)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(Country|CustomField|Integration|Language|Locale|Snippet|User)\\\\/',
+        ],
+        'sales-channel' => [
+            '/Shopware\\\\Core\\\\Content\\\\(MailTemplate|Seo|Sitemap)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(SalesChannel)\\\\/',
+        ],
+        'customer-order' => [
+            '/Shopware\\\\Core\\\\Content\\\\(Newsletter)\\\\/',
+            '/Shopware\\\\Core\\\\Checkout\\\\(Customer|Document|Order)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(DeliveryTime|Salutation|Tax)\\\\/',
+        ],
+        'checkout' => [
+            '/Shopware\\\\Core\\\\Checkout\\\\(Cart|Payment|Promotion|Shipping)\\\\/',
+            '/Shopware\\\\Core\\\\System\\\\(DeliveryTime|NumberRange|StateMachine)\\\\/',
+            '/Shopware\\\\Storefront\\\\(Checkout)\\\\/',
+        ],
+        'merchant-services' => [
+            '/Shopware\\\\Core\\\\Framework\\\\(Store)\\\\/',
+        ],
+        'storefront' => [
+            '/Shopware\\\\Storefront\\\\(DependencyInjection)\\\\/',
+        ],
     ];
 
     public function getNodeType(): string
@@ -31,31 +84,35 @@ class PackageAnnotationRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        if (!$area = $this->getProductArea($node)) {
+        if ($this->isTestClass($node)) {
             return [];
         }
 
-        if ($this->hasPackageAnnotation($node, $area)) {
+        $area = $this->getProductArea($node);
+
+        if ($this->hasPackageAnnotation($node)) {
             return [];
         }
 
-        return [sprintf('This class is missing the "@package %s" annotation', $area)];
+        return [sprintf('This class is missing the "@package" annotation (recommendation: %s)', $area ?? 'unknown')];
     }
 
     private function getProductArea(InClassNode $node): ?string
     {
         $namespace = $node->getClassReflection()->getName();
 
-        foreach (self::PRODUCT_AREA_MAPPING as $area => $regex) {
-            if (preg_match($regex, $namespace)) {
-                return $area;
+        foreach (self::PRODUCT_AREA_MAPPING as $area => $regexes) {
+            foreach ($regexes as $regex) {
+                if (preg_match($regex, $namespace)) {
+                    return $area;
+                }
             }
         }
 
         return null;
     }
 
-    private function hasPackageAnnotation(InClassNode $class, string $area): bool
+    private function hasPackageAnnotation(InClassNode $class): bool
     {
         $doc = $class->getDocComment();
 
@@ -63,6 +120,13 @@ class PackageAnnotationRule implements Rule
             return false;
         }
 
-        return \str_contains($doc->getText(), sprintf('@package %s', $area));
+        return \str_contains($doc->getText(), sprintf('@package'));
+    }
+
+    private function isTestClass(InClassNode $node): bool
+    {
+        $namespace = $node->getClassReflection()->getName();
+
+        return \str_contains($namespace, 'Shopware\\Tests\\') || \str_contains($namespace, '\\Test\\');
     }
 }

941b7fffd14776f64bfa7974de2c8df43c30ae26
Altay Akkus <altayakkus1993@googlemail.com>
2022-12-22 17:47:35 +0000

    Add blockResubmit feature to Cart Validation

--- src/Core/Checkout/Cart/Error/ErrorCollection.php
+++ src/Core/Checkout/Cart/Error/ErrorCollection.php
@@ -30,6 +30,17 @@ class ErrorCollection extends Collection
         return false;
     }
 
+    public function blockResubmit(): bool
+    {
+        foreach ($this->getIterator() as $error) {
+            if (method_exists($error, 'blockResubmit') and $error->blockResubmit()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function getErrors(): array
     {
         return $this->filterByErrorLevel(Error::LEVEL_ERROR);

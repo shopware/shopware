cc260cd376293aad028905f04d5a46eefe5d6c46
Krispin Lütjann <k.luetjann@shopware.com>
2023-01-05 11:22:25 +0100

    NEXT-21811 - Fix custom field demodata command

--- src/Core/Framework/Demodata/Generator/CustomFieldGenerator.php
+++ src/Core/Framework/Demodata/Generator/CustomFieldGenerator.php
@@ -197,7 +197,7 @@ class CustomFieldGenerator implements DemodataGeneratorInterface
         $attributeCount = random_int(1, 5);
         $attributes = [];
 
-        $setName = $context->getFaker()->unique()->format('category');
+        $setName = $context->getFaker()->unique()->format('customFieldSet');
         $prefix = 'custom_';
 
         for ($j = 0; $j < $attributeCount; ++$j) {

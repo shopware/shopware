48d850215b724873cce86c8ad38072a669f762a0
Aydin Hassan <a.hassan@shopware.com>
2023-01-26 15:07:55 +0100

    NEXT-24930 - Add PHPStan rule to prohibit using autoload in core entity definitions

---
+++ changelog/_unreleased/2023-01-27-phpstan-rule-prevent-autoload-in-entity-definitions.md
@@ -0,0 +1,6 @@
+---
+title: PHPStan rule prevent autoload in entity definitions
+issue: NEXT-24930
+---
+# Core
+* Added a PHPStan rule to prevent using autoload === true in core entity definition associations
---
+++ tests/unit/php/Core/DevOps/StaticAnalyse/PHPStan/Rules/data/no-dal-autoload/no-entity-name.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Entity;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToOneAssociationField;
+
+class Definition
+{
+    public function __construct()
+    {
+        new OneToOneAssociationField('prop', 'storageName', 'referenceField', 'referenceClass', true);
+        new ManyToOneAssociationField('prop2', 'storageName', 'referenceClass', 'referenceField', true);
+    }
+}

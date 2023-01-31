ed329081b5fc73c9451d18fcd702bc38624d198c
Krispin Lütjann <k.luetjann@shopware.com>
2023-01-31 14:17:58 +0100

    NEXT-25209 - Fix inherintance of stock properties with downloadable products

--- src/Administration/Resources/app/administration/src/module/sw-product/component/sw-product-variants/sw-product-modal-variant-generation/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-product/component/sw-product-variants/sw-product-modal-variant-generation/index.js
@@ -271,6 +271,9 @@ export default {
 
                 if (item.productStates.includes('is-download')) {
                     item.maxPurchase = 1;
+                    item.minPurchase = 1;
+                    item.isCloseout = false;
+                    item.shippingFree = false;
                 }
 
                 const mediaIds = [];

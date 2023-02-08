94a4f05c3481ac3a1f32d0b9afb61d03b56e601b
Soner Sayakci <s.sayakci@shopware.com>
2023-02-08 21:07:21 +0100

    NEXT-24538 - Update devenv

--- devenv.nix
+++ devenv.nix
@@ -6,70 +6,81 @@
     pkgs.nodePackages_latest.yalc
   ];
 
-  languages.javascript.enable = true;
-  languages.javascript.package = lib.mkDefault pkgs.nodejs-18_x;
+  languages.javascript = {
+    enable = lib.mkDefault true;
+    package = lib.mkDefault pkgs.nodejs-18_x;
+  };
 
-  languages.php.enable = true;
-  languages.php.package = lib.mkDefault (pkgs.php.buildEnv {
-    extensions = { all, enabled }: with all; enabled ++ [ amqp redis blackfire grpc ];
-    extraConfig = ''
+  languages.php = {
+    enable = lib.mkDefault true;
+    version = lib.mkDefault "8.1";
+    extensions = [ "grpc" ];
+
+    ini = ''
       memory_limit = 2G
-      pdo_mysql.default_socket=''${MYSQL_UNIX_PORT}
-      mysqli.default_socket=''${MYSQL_UNIX_PORT}
-      blackfire.agent_socket = "${config.services.blackfire.socket}";
-      realpath_cache_ttl=3600
-      session.gc_probability=0
+      realpath_cache_ttl = 3600
+      session.gc_probability = 0
+      ${lib.optionalString config.services.redis.enable ''
       session.save_handler = redis
       session.save_path = "tcp://127.0.0.1:6379/0"
+      ''}
       display_errors = On
       error_reporting = E_ALL
-      assert.active=0
-      opcache.memory_consumption=256M
-      opcache.interned_strings_buffer=20
+      assert.active = 0
+      opcache.memory_consumption = 256M
+      opcache.interned_strings_buffer = 20
       zend.assertions = 0
       short_open_tag = 0
-      zend.detect_unicode=0
-      realpath_cache_ttl=3600
+      zend.detect_unicode = 0
+      realpath_cache_ttl = 3600
     '';
-  });
 
-  languages.php.fpm.pools.web = {
-    settings = {
-      "clear_env" = "no";
-      "pm" = "dynamic";
-      "pm.max_children" = 10;
-      "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 10;
+    fpm.pools.web = lib.mkDefault {
+      settings = {
+        "clear_env" = "no";
+        "pm" = "dynamic";
+        "pm.max_children" = 10;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 10;
+      };
     };
   };
 
-  services.caddy.enable = lib.mkDefault true;
-  services.caddy.virtualHosts."http://localhost:8000" = {
-    extraConfig = ''
-      root * public
-      php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
-      file_server
-    '';
+  services.caddy = {
+    enable = lib.mkDefault true;
+
+    virtualHosts.":8000" = lib.mkDefault {
+      extraConfig = lib.mkDefault ''
+        @default {
+          not path /theme/* /media/* /thumbnail/* /bundles/* /css/* /fonts/* /js/* /sitemap/*
+        }
+        root * public
+        php_fastcgi @default unix/${config.languages.php.fpm.pools.web.socket} {
+            trusted_proxies private_ranges
+        }
+        file_server
+      '';
+    };
   };
 
-  services.mysql.enable = lib.mkDefault true;
-  services.mysql.initialDatabases = [
-    { name = "shopware"; }
-  ];
-  services.mysql.ensureUsers = [
-    {
-      name = "shopware";
-      password = "shopware";
-      ensurePermissions = {
-        "shopware.*" = "ALL PRIVILEGES";
-        "shopware_test.*" = "ALL PRIVILEGES";
+  services.mysql = {
+    enable = true;
+    initialDatabases = lib.mkDefault [{ name = "shopware"; }];
+    ensureUsers = lib.mkDefault [
+      {
+        name = "shopware";
+        password = "shopware";
+        ensurePermissions = {
+          "shopware.*" = "ALL PRIVILEGES";
+          "shopware_test.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+    settings = {
+      mysqld = {
+        log_bin_trust_function_creators = 1;
       };
-    }
-  ];
-  services.mysql.settings = {
-    mysqld = {
-      log_bin_trust_function_creators = 1;
     };
   };
 

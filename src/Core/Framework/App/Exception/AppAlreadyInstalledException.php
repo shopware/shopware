7bec95251a2724aabed7babfb532980921d147a4
Tim Goldbach <t.goldbach@shopware.com>
2023-02-23 13:36:42 +0000

    NEXT-19982 - edit AppAlreadyInstalledException

--- src/Core/Framework/App/Exception/AppAlreadyInstalledException.php
+++ src/Core/Framework/App/Exception/AppAlreadyInstalledException.php
@@ -3,15 +3,33 @@
 namespace Shopware\Core\Framework\App\Exception;
 
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\ShopwareHttpException;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @internal only for use by the app-system, will be considered internal from v6.4.0 onward
  */
 #[Package('core')]
-class AppAlreadyInstalledException extends \Exception
+class AppAlreadyInstalledException extends ShopwareHttpException
 {
-    public function __construct(string $appName)
+    public function __construct(
+        string $appName,
+        ?\Throwable $e = null
+    ) {
+        parent::__construct(
+            'App with name "{{appName}}" is already installed.',
+            ['appName' => $appName],
+            $e
+        );
+    }
+
+    public function getErrorCode(): string
     {
-        parent::__construct(sprintf('App with name "%s" is already installed.', $appName));
+        return 'FRAMEWORK__APP_ALREADY_INSTALLED';
+    }
+
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_BAD_REQUEST;
     }
 }

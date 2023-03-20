fb0f11043cdea6b97e6f4528035718720a698718
Krispin Lütjann <k.luetjann@shopware.com>
2023-03-20 11:56:03 +0100

    NEXT-25346 - Fix sw-price-rule-modal jest test

--- src/Administration/Resources/app/administration/src/module/sw-settings-shipping/component/sw-price-rule-modal/sw-price-rule-modal.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-settings-shipping/component/sw-price-rule-modal/sw-price-rule-modal.spec.js
@@ -1,15 +1,83 @@
 import { shallowMount } from '@vue/test-utils';
 import swPriceRuleModal from 'src/module/sw-settings-shipping/component/sw-price-rule-modal';
+import 'src/app/component/rule/sw-rule-modal';
 
 /**
  * @package checkout
  */
 
-Shopware.Component.register('sw-price-rule-modal', swPriceRuleModal);
+Shopware.Component.extend('sw-price-rule-modal', 'sw-rule-modal', swPriceRuleModal);
 
+function createRuleMock(isNew) {
+    return {
+        id: '1',
+        name: 'Test rule',
+        isNew: () => isNew,
+        conditions: [{
+            entity: 'rule',
+            source: 'foo/rule',
+            children: [{
+                id: 'some-id',
+                children: [{
+                    id: 'some-id'
+                }]
+            }]
+        }],
+        someRuleRelation: []
+    };
+}
 
 async function createWrapper() {
-    return shallowMount(await Shopware.Component.build('sw-price-rule-modal'));
+    return shallowMount(await Shopware.Component.build('sw-price-rule-modal'), {
+        provide: {
+            repositoryFactory: {
+                create: () => {
+                    return {
+                        create: () => {
+                            return createRuleMock(true);
+                        },
+                        get: () => Promise.resolve(createRuleMock(false)),
+                        save: () => Promise.resolve(),
+                        search: () => Promise.resolve([])
+                    };
+                }
+            },
+
+            ruleConditionDataProviderService: {
+                getModuleTypes: () => [],
+                addScriptConditions: () => {},
+                getRestrictedRuleTooltipConfig: () => ({
+                    disabled: true
+                })
+            },
+
+            ruleConditionsConfigApiService: {
+                load: () => Promise.resolve()
+            }
+        },
+
+        stubs: {
+            'sw-modal': {
+                template: `
+                    <div class="sw-modal">
+                      <slot name="modal-header"></slot>
+                      <slot></slot>
+                      <slot name="modal-footer"></slot>
+                    </div>
+                `
+            },
+            'sw-button': {
+                template: '<button @click="$emit(\'click\', $event)"><slot></slot></button>'
+            },
+            'sw-condition-tree': true,
+            'sw-container': true,
+            'sw-multi-select': true,
+            'sw-textarea-field': true,
+            'sw-number-field': true,
+            'sw-text-field': true,
+            'sw-field': true
+        }
+    });
 }
 
 describe('module/sw-settings-shipping/component/sw-price-rule-modal', () => {

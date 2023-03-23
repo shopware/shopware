95df98c5d5b668355bd814b01ffe38d4cb440cfd
Frederik Schmitt <f.schmitt@shopware.com>
2023-03-23 14:34:16 +0100

    NEXT-25582 - Add extension icon component

---
+++ src/Administration/Resources/app/administration/src/module/sw-extension/component/sw-extension-icon/index.ts
@@ -0,0 +1,17 @@
+import template from './sw-extension-icon.html.twig';
+import './sw-extension-icon.scss';
+
+/**
+ * @package merchant-services
+ * @private
+ */
+export default {
+    template,
+
+    props: {
+        iconSrc: {
+            type: String,
+            required: true,
+        },
+    }
+};

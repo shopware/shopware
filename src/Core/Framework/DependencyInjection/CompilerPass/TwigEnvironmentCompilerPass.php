41fe81a4c9914d939da088da6bc7713e4cbb2173
Jonas Elfering <j.elfering@shopware.com>
2023-04-04 16:13:44 +0200

    NEXT-25869 - Use configured twig.cache for twig cache

--- src/Core/Framework/DependencyInjection/CompilerPass/TwigEnvironmentCompilerPass.php
+++ src/Core/Framework/DependencyInjection/CompilerPass/TwigEnvironmentCompilerPass.php
@@ -8,6 +8,9 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 #[Package('core')]
+/**
+ * @codeCoverageIgnore This would be useless as a unit test. It is integration tested here: \Shopware\Tests\Integration\Core\Framework\DependencyInjection\CompilerPass\TwigEnvironmentCompilerPassTest
+ */
 class TwigEnvironmentCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
@@ -16,5 +19,18 @@ class TwigEnvironmentCompilerPass implements CompilerPassInterface
         // symfony service subscriber somehow don't work, therefore the service has to be public
         $twigEnvironment->setPublic(true);
         $twigEnvironment->setClass(TwigEnvironment::class);
+
+        // The twig extension directly compiles the config into the service, there is no other way to get it @see \Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension::load
+        $twigOptions = $twigEnvironment->getArgument(1);
+        \assert(\is_array($twigOptions));
+
+        $configuredTwigCache = $twigOptions['cache'] ?? false;
+        if (!\is_string($configuredTwigCache)) {
+            $container->setParameter('twig.cache', $container->getParameter('kernel.cache_dir') . '/twig');
+
+            return;
+        }
+
+        $container->setParameter('twig.cache', $configuredTwigCache);
     }
 }

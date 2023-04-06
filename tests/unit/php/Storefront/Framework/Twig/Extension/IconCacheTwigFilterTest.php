5424ef3db953c4b17231f9840abe5d75f2b4c222
ssltg <s.sluiter@shopware.com>
2023-04-06 11:23:29 +0200

    NEXT-26125 - quarantine icon cache test

--- tests/unit/php/Storefront/Framework/Twig/Extension/IconCacheTwigFilterTest.php
+++ tests/unit/php/Storefront/Framework/Twig/Extension/IconCacheTwigFilterTest.php
@@ -69,6 +69,11 @@ class IconCacheTwigFilterTest extends TestCase
         </span>', $rendered->getContent());
     }
 
+    /**
+     * NEXT-26125
+     *
+     * @group quarantined
+     */
     public function testStorefrontRenderIconCacheDisabled(): void
     {
         $twig = $this->createFinder([

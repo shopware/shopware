195c55f6442fb665527bdb65be50dc329a9f73ea
Philip Gatzka <p.gatzka@shopware.com>
2023-04-24 17:33:56 +0200

    NEXT-26373 - Add command to fetch classes per area

--- src/Core/DevOps/StaticAnalyze/Coverage/Command/GetClassesPerAreaCommand.php
+++ src/Core/DevOps/StaticAnalyze/Coverage/Command/GetClassesPerAreaCommand.php
@@ -1,18 +1,19 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Shopware\Core\DevOps\StaticAnalyze\Coverage\Command;
 
 use Composer\Autoload\ClassLoader;
+use Composer\Composer;
+use Shopware\Core\DevOps\StaticAnalyze\Coverage\CoveragePerArea;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\ErrorHandler\Error\ClassNotFoundError;
+use Symfony\Component\Finder\Finder;
 
-/**
- * @internal
- */
 #[AsCommand(
     name: 'coverage:classes-per-area',
     description: 'Output all classes of the Shopware-namespace aggregated by area.
@@ -24,18 +25,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[Package('core')]
 class GetClassesPerAreaCommand extends Command
 {
-    public const OPTION_JSON = 'json';
-    public const OPTION_PRETTY = 'pretty-print';
-
-    public const OPTION_GENERATE_PHPUNIT_TEST = 'generate-phpunit-test';
-
-    public const OPTION_NAMESPACE_PATTERN = 'ns-pattern';
-
-    public const NAMESPACE_PATTERN_DEFAULT = '#^Shopware\\\\(Core|Administration|Storefront|Elasticsearch)\\\\#';
+    private const OPTION_JSON = 'json';
+    private const OPTION_PRETTY = 'pretty-print';
 
     private ClassLoader $classLoader;
-
-    private string $nsPattern;
 
     /**
      * @internal
@@ -43,9 +36,9 @@ class GetClassesPerAreaCommand extends Command
     public function __construct(
         private readonly string $projectDir,
     ) {
-        parent::__construct();
-
         $this->classLoader = require $this->projectDir . '/vendor/autoload.php';
+
+        parent::__construct();
     }
 
     protected function configure(): void
@@ -63,84 +56,27 @@ class GetClassesPerAreaCommand extends Command
             InputOption::VALUE_NONE,
             'Format output to be human-readable'
         );
-
-        $this->addOption(
-            self::OPTION_GENERATE_PHPUNIT_TEST,
-            'g',
-            InputOption::VALUE_NONE,
-            'Generate phpunit.xml'
-        );
-
-        $this->addOption(
-            self::OPTION_NAMESPACE_PATTERN,
-            null,
-            InputOption::VALUE_REQUIRED,
-            'The pattern the namespace has to match',
-            self::NAMESPACE_PATTERN_DEFAULT,
-        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->nsPattern = $input->getOption(self::OPTION_NAMESPACE_PATTERN);
-
-        $classesPerArea = $this->getClassesPerArea();
         if ($input->getOption(self::OPTION_JSON)) {
             $output->write(
                 json_encode(
-                    $classesPerArea,
-                    $input->getOption(self::OPTION_PRETTY) ? \JSON_PRETTY_PRINT : 0
-                ) ?: ''
+                    $this->getClassesPerArea(),
+                    $input->getOption(self::OPTION_PRETTY) ? JSON_PRETTY_PRINT : 0
+                )
             );
         } else {
             $output->write(
-                var_export(
-                    $classesPerArea,
-                    true
-                )
+                var_export($this->getClassesPerArea(), true)
             );
-        }
-
-        if ($input->getOption(self::OPTION_GENERATE_PHPUNIT_TEST)) {
-            $unitFiles = [];
-            foreach ($classesPerArea as $area => $classToFile) {
-                $unitFile = new \DOMDocument();
-                // Load phpunit template
-                $unitFile->load('phpunit.xml.dist');
-                $unitDocument = $unitFile->documentElement;
-                if ($unitDocument === null) {
-                    return 1;
-                }
-                $coverage = $unitDocument->getElementsByTagName('coverage')->item(0);
-                if ($coverage === null) {
-                    return 1;
-                }
-                $includeChildElement = $coverage->getElementsByTagName('include')->item(0);
-                if ($includeChildElement === null) {
-                    return 1;
-                }
-                // Remove include from coverage to create our own includes
-                $coverage->removeChild($includeChildElement);
-                $includeElement = $unitFile->createElement('include');
-
-                foreach ($classToFile as $class => $file) {
-                    $fileElement = $unitFile->createElement('file', $file);
-                    $includeElement->appendChild($fileElement);
-                }
-                $coverage->appendChild($includeElement);
-
-                // Create phpunit file per area
-                file_put_contents("phpunit.$area.xml", $unitFile->saveXML());
-            }
         }
 
         return 0;
     }
 
-    /**
-     * @return array<string, array<string, string>>
-     */
-    private function getClassesPerArea(): array
+    private function getClassesPerArea()
     {
         $areas = [];
 
@@ -149,15 +85,18 @@ class GetClassesPerAreaCommand extends Command
                 $area = Package::getPackageName($class);
             } catch (\Throwable $e) {
                 $areas['unknown'][$class] = $path;
-
                 continue;
             }
 
-            if (!\is_string($area)) {
+            if (!is_string($area)) {
                 continue;
             }
 
-            $areaTrim = strstr($area, \PHP_EOL, true) ?: $area;
+            $areaTrim = strstr($area, PHP_EOL, true) ?: $area;
+
+            if (!is_string($areaTrim)) {
+                continue;
+            }
 
             $areas[trim($areaTrim)][$class] = $path;
         }
@@ -166,16 +105,12 @@ class GetClassesPerAreaCommand extends Command
     }
 
     /**
-     * @return array<string, string>
+     * @return array<array<string, string>>
      */
     private function getShopwareClasses(): array
     {
-        return array_filter($this->classLoader->getClassMap(), function (string $class): bool {
-            if (str_starts_with($class, 'Shopware\\')) {
-                return (bool) preg_match($this->nsPattern, $class);
-            }
-
-            return false;
-        }, \ARRAY_FILTER_USE_KEY);
+        return array_filter($this->classLoader->getClassMap(), static function (string $class): bool {
+            return str_starts_with($class, 'Shopware\\');
+        }, ARRAY_FILTER_USE_KEY);
     }
 }

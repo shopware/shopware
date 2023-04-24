460e92f7602d1a802410cd3dffff8a50bf89b7ee
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-04-24 10:12:04 +0200

    NEXT-26012 - Test app init folder till directive

---
+++ changelog/_unreleased/2023-04-24-add-jest-tests-for-init-folder.md
@@ -0,0 +1,18 @@
+---
+title: Add jest tests for init folder
+issue: NEXT-26012
+author: Sebastian Seggewiss
+author_email: s.seggewiss@shopware.com
+author_github: @seggewiss
+---
+# Administration
+* Added `src/app/init/action-button.init.spec.js`
+* Added `src/app/init/actions.init.spec.js`
+* Added `src/app/init/cms.init.spec.js`
+* Added `src/app/init/component.init.spec.js`
+* Added `src/app/init/component-helper.init.spec.js`
+* Added `src/app/init/context.init.spec.js`
+* Changed `src/app/init/component.init.js` to `src/app/init/component.init.ts`
+* Changed `src/app/init/component-helper.init.js` to `src/app/init/component-helper.init.ts`
+* Changed `src/app/init/context.init.js` to `src/app/init/context.init.ts`
+* Changed `src/app/init/directive.init.js` to `src/app/init/directive.init.ts`
---
+++ src/Administration/Resources/app/administration/src/app/init/actions.init.spec.js
@@ -0,0 +1,70 @@
+import initActions from 'src/app/init/actions.init';
+import { actionExecute } from '@shopware-ag/admin-extension-sdk/es/app/action';
+import ExtensionSdkService from '../../core/service/api/extension-sdk.service';
+import extensionsStore from '../state/extensions.store';
+
+describe('src/app/init/actions.init.ts', () => {
+    beforeAll(() => {
+        Shopware.Service().register('extensionSdkService', () => {
+            return new ExtensionSdkService();
+        });
+    });
+
+    beforeEach(() => {
+        if (Shopware.State.get('extensions')) {
+            Shopware.State.unregisterModule('extensions');
+        }
+
+        Shopware.State.registerModule('extensions', extensionsStore);
+    });
+
+    afterEach(() => {
+        Shopware.State.unregisterModule('extensions');
+    });
+
+    it('should handle actionExecute', async () => {
+        const appName = 'jestapp';
+        const mock = jest.fn();
+
+        Shopware.State.commit('extensions/addExtension', {
+            name: appName,
+            baseUrl: '',
+            permissions: [],
+            version: '1.0.0',
+            type: 'app',
+            integrationId: '123',
+            active: true,
+        });
+
+        Shopware.Service('extensionSdkService').runAction = mock;
+
+        initActions();
+        await actionExecute({
+            entity: 'customer',
+            url: 'https://example.com',
+            entityIds: ['123'],
+        });
+
+        expect(mock).toHaveBeenCalledWith(expect.objectContaining({
+            url: 'https://example.com',
+            entity: 'customer',
+            action: expect.any(String),
+            appName: appName,
+        }), ['123']);
+    });
+
+    it('should not handle actionExecute if extension is not found', async () => {
+        const mock = jest.fn();
+
+        Shopware.Service('extensionSdkService').runAction = mock;
+
+        initActions();
+        await actionExecute({
+            entity: 'customer',
+            url: 'https://example.com',
+            entityIds: ['123'],
+        });
+
+        expect(mock).toHaveBeenCalledTimes(0);
+    });
+});

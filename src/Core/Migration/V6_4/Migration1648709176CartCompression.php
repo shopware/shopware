704b3ecd27b303eba731e29b1cd6995008e28fc0
Frederik Schmitt <f.schmitt@shopware.com>
2023-05-24 07:28:16 +0000

    NEXT-26834 - Limit select to 1000

--- src/Core/Migration/V6_4/Migration1648709176CartCompression.php
+++ src/Core/Migration/V6_4/Migration1648709176CartCompression.php
@@ -3,6 +3,7 @@
 namespace Shopware\Core\Migration\V6_4;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
 use Shopware\Core\Framework\DataAbstractionLayer\Doctrine\RetryableQuery;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Migration\MigrationStep;
@@ -13,6 +14,8 @@ use Shopware\Core\Framework\Migration\MigrationStep;
 #[Package('core')]
 class Migration1648709176CartCompression extends MigrationStep
 {
+    private const UPDATE_LIMIT = 1000;
+
     public function getCreationTimestamp(): int
     {
         return 1648709176;
@@ -25,7 +28,7 @@ class Migration1648709176CartCompression extends MigrationStep
     public function updateDestructive(Connection $connection): void
     {
         if (!$this->columnExists($connection, 'cart', 'compressed')) {
-            $connection->executeStatement('ALTER TABLE `cart` ADD `compressed` tinyint(1) NOT NULL DEFAULT \'0\';');
+            $connection->executeStatement('ALTER TABLE `cart` ADD `compressed` tinyint(1) NOT NULL DEFAULT 0;');
         }
 
         // after adding the payload column, we may save carts as compressed serialized objects, there is no way of return at this point
@@ -37,11 +40,15 @@ class Migration1648709176CartCompression extends MigrationStep
             return;
         }
 
-        while ($token = $connection->fetchOne('SELECT token FROM cart WHERE `payload` IS NULL AND `cart` IS NOT NULL LIMIT 1')) {
-            RetryableQuery::retryable($connection, static function () use ($connection, $token): void {
-                $connection->executeStatement('UPDATE cart SET `payload` = `cart`, `compressed` = 0 WHERE token = :token', ['token' => $token]);
+        do {
+            $affectedRows = RetryableQuery::retryable($connection, static function () use ($connection): int {
+                return (int) $connection->executeStatement(
+                    'UPDATE cart SET `payload` = `cart` WHERE `payload` IS NULL AND `cart` IS NOT NULL LIMIT :limit',
+                    ['limit' => self::UPDATE_LIMIT],
+                    ['limit' => ParameterType::INTEGER]
+                );
             });
-        }
+        } while ($affectedRows === self::UPDATE_LIMIT);
 
         $connection->executeStatement('ALTER TABLE `cart` DROP COLUMN `cart`;');
     }

79cabc21d0bf21e1fbb5cbda54e8d6c9137fe75c
Joshua Behrens <behrens@heptacom.de>
2023-06-13 23:00:25 +0200

    Make databag consistently convert arrays to databags

--- tests/unit/Core/Framework/Validation/DataBag/DataBagTest.php
+++ tests/unit/Core/Framework/Validation/DataBag/DataBagTest.php
@@ -45,6 +45,53 @@ class DataBagTest extends TestCase
         static::assertEquals(['a' => 'b'], $bag->all('4'));
     }
 
+    public function testAddSetConvertsToDataBag(): void
+    {
+        $bag = new DataBag();
+
+        // test setting databag stays a databag, but converts to array
+        $bag->set('1', new DataBag(['a' => 'b']));
+
+        static::assertInstanceOf(DataBag::class, $bag->get('1'));
+        static::assertSame(['a' => 'b'], $bag->get('1')->all());
+
+        // test setting array becomes a databag, but converts to array
+        $bag->set('2', ['a' => 'b']);
+
+        static::assertInstanceOf(DataBag::class, $bag->get('2'));
+        static::assertSame(['a' => 'b'], $bag->get('2')->all());
+
+        // test adding databag parameter stays a databag, but converts to array
+        $bag->add([
+            '3' => new DataBag(['a' => 'b']),
+        ]);
+
+        static::assertInstanceOf(DataBag::class, $bag->get('3'));
+        static::assertSame(['a' => 'b'], $bag->get('3')->all());
+
+        // test adding array parameters becomes a databag, but converts to array
+        $bag->add([
+            '4' => ['a' => 'b'],
+        ]);
+
+        static::assertInstanceOf(DataBag::class, $bag->get('4'));
+        static::assertSame(['a' => 'b'], $bag->get('4')->all());
+    }
+
+    public function testEnsureCloningIsDeep(): void
+    {
+        $bag = new DataBag();
+        $innerBag = new DataBag();
+
+        $bag->set('key', $innerBag);
+
+        static::assertSame($innerBag, $bag->get('key'));
+
+        $clone = clone $bag;
+
+        static::assertNotSame($innerBag, $clone->get('key'));
+    }
+
     public function testAllWorksOnlyOnArrays(): void
     {
         $bag = new DataBag([

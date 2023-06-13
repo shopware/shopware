6e71eb7dd7c292c29294bc049c061d6abbc39c02
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-06-13 16:34:34 +0200

    NEXT-28477 - Fix webpack clean glob pattern

--- src/Administration/Resources/app/administration/webpack.config.js
+++ src/Administration/Resources/app/administration/webpack.config.js
@@ -551,6 +551,24 @@ const baseConfig = ({ pluginPath, pluginFilepath }) => ({
 
             return [];
         })(),
+
+        /**
+         * All files inside webpack's output.path directory will be removed once, but the
+         * directory itself will not be. If using webpack 4+'s default configuration,
+         * everything under <PROJECT_DIR>/dist/ will be removed.
+         * Use cleanOnceBeforeBuildPatterns to override this behavior.
+         *
+         * During rebuilds, all webpack assets that are not used anymore
+         * will be removed automatically.
+         *
+         * See `Options and Defaults` for information
+         */
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '!**/*',
+                'static/**/*',
+            ]
+        }),
     ],
 });
 

8aed19f4a16de63a08a3d5fde4c00cc40872ceae
ssltg <s.sluiter@shopware.com>
2023-06-15 11:53:04 +0200

    NEXT-28566 - Catch newsletter sub error

--- src/Core/Content/Newsletter/NewsletterException.php
+++ src/Core/Content/Newsletter/NewsletterException.php
@@ -8,11 +8,10 @@ use Shopware\Core\Framework\HttpException;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\HttpFoundation\Response;
 
-#[Package('buyers-experience')]
+#[Package('checkout')]
 class NewsletterException extends HttpException
 {
     public const NEWSLETTER_RECIPIENT_NOT_FOUND_CODE = 'CONTENT__NEWSLETTER_RECIPIENT_NOT_FOUND';
-    public const NEWSLETTER_RECIPIENT_THROTTLED = 'CONTENT__NEWSLETTER_RECIPIENT_THROTTLED';
 
     public static function recipientNotFound(
         string $identifier,
@@ -27,16 +26,6 @@ class NewsletterException extends HttpException
             self::NEWSLETTER_RECIPIENT_NOT_FOUND_CODE,
             'The NewsletterRecipient with the identifier "{{ identifier }}" - {{ value }} was not found.',
             ['identifier' => $identifier, 'value' => $value]
-        );
-    }
-
-    public static function newsletterThrottled(int $waitTime): NewsletterException
-    {
-        return new self(
-            Response::HTTP_TOO_MANY_REQUESTS,
-            self::NEWSLETTER_RECIPIENT_THROTTLED,
-            'Too many requests, try again in {{ seconds }} seconds.',
-            ['seconds' => $waitTime],
         );
     }
 }

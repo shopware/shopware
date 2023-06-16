4b3abd76dce94b9a6d981aad6bb5c0cba1d23ea9
David Neustadt <d.neustadt@shopware.com>
2023-06-16 13:30:54 +0000

    NEXT-28608 - make StaticEntityRepository return write results

--- src/Core/Test/Stub/DataAbstractionLayer/StaticEntityWriterGateway.php
+++ src/Core/Test/Stub/DataAbstractionLayer/StaticEntityWriterGateway.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Core\Test\Stub\DataAbstractionLayer;
+namespace Shopware\Tests\Unit\Common\Stubs\DataAbstractionLayer;
 
 use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\WriteCommandQueue;
@@ -10,7 +10,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Write\WriteContext;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\WriteParameterBag;
 
 /**
- * @final
+ * @internal
  */
 class StaticEntityWriterGateway implements EntityWriteGatewayInterface
 {

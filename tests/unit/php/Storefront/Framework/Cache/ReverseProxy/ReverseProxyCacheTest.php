52c513a119f39388eef6898994561b78e5d5cc32
Soner Sayakci <s.sayakci@shopware.com>
2023-06-26 15:39:16 +0200

    NEXT-28776 - Fix ReverseProxyCache to remove cache tags

--- tests/unit/php/Storefront/Framework/Cache/ReverseProxy/ReverseProxyCacheTest.php
+++ tests/unit/php/Storefront/Framework/Cache/ReverseProxy/ReverseProxyCacheTest.php
@@ -4,8 +4,12 @@ namespace Shopware\Tests\Unit\Storefront\Framework\Cache\ReverseProxy;
 
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Adapter\Cache\AbstractCacheTracer;
+use Shopware\Storefront\Framework\Cache\CacheStore;
 use Shopware\Storefront\Framework\Cache\ReverseProxy\AbstractReverseProxyGateway;
 use Shopware\Storefront\Framework\Cache\ReverseProxy\ReverseProxyCache;
+use Shopware\Storefront\Framework\Routing\RequestTransformer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @internal
@@ -24,5 +28,25 @@ class ReverseProxyCacheTest extends TestCase
 
         // this is the only way to call the destructor
         unset($cache);
+    }
+
+    public function testTagsFromResponseGetsMergedAndRemoved(): void
+    {
+        $gateway = $this->createMock(AbstractReverseProxyGateway::class);
+
+        $gateway
+            ->expects(static::once())
+            ->method('tag')
+            ->with(['foo']);
+
+        $cache = new ReverseProxyCache($gateway, $this->createMock(AbstractCacheTracer::class), []);
+
+        $response = new Response();
+        $response->headers->set(CacheStore::TAG_HEADER, '["foo"]');
+
+        $request = new Request();
+        $request->attributes->set(RequestTransformer::ORIGINAL_REQUEST_URI, 'test');
+        $cache->write($request, $response);
+        static::assertFalse($response->headers->has(CacheStore::TAG_HEADER));
     }
 }

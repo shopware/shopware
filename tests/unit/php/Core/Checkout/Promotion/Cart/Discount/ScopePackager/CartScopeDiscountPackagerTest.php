2b0c456cd2c3e89c487aace8ce735b22eefa78b3
David Neustadt <d.neustadt@shopware.com>
2023-06-30 15:32:35 +0000

    NEXT-23239 - Consider filter in discount packager

---
+++ tests/unit/php/Core/Checkout/Promotion/Cart/Discount/ScopePackager/CartScopeDiscountPackagerTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Checkout\Promotion\Cart\Discount\ScopePackager;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Cart\LineItem\Group\LineItemQuantity;
+use Shopware\Core\Checkout\Cart\LineItem\Group\LineItemQuantityCollection;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\LineItem\LineItemCollection;
+use Shopware\Core\Checkout\Cart\Price\Struct\AbsolutePriceDefinition;
+use Shopware\Core\Checkout\Cart\Price\Struct\PercentagePriceDefinition;
+use Shopware\Core\Checkout\Cart\Rule\LineItemRule;
+use Shopware\Core\Checkout\Promotion\Cart\Discount\DiscountLineItem;
+use Shopware\Core\Checkout\Promotion\Cart\Discount\DiscountPackage;
+use Shopware\Core\Checkout\Promotion\Cart\Discount\DiscountPackageCollection;
+use Shopware\Core\Checkout\Promotion\Cart\Discount\ScopePackager\CartScopeDiscountPackager;
+use Shopware\Core\Checkout\Test\Cart\Common\Generator;
+use Shopware\Core\Framework\Rule\Rule;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+/**
+ * @internal
+ *
+ * @covers \Shopware\Core\Checkout\Promotion\Cart\Discount\ScopePackager\CartScopeDiscountPackager
+ */
+class CartScopeDiscountPackagerTest extends TestCase
+{
+    public function testGetMatchingItems(): void
+    {
+        $context = Generator::createSalesChannelContext();
+
+        $matchingLineItem = (new LineItem(Uuid::randomHex(), LineItem::PRODUCT_LINE_ITEM_TYPE, Uuid::randomHex(), 2))->setStackable(true);
+        $cart = new Cart('foo');
+        $cart->setLineItems(
+            new LineItemCollection([
+                $matchingLineItem,
+                (new LineItem(Uuid::randomHex(), LineItem::PRODUCT_LINE_ITEM_TYPE, Uuid::randomHex()))->setStackable(true),
+            ])
+        );
+
+        $priceDefinition = new AbsolutePriceDefinition(42, new LineItemRule(Rule::OPERATOR_EQ, [$matchingLineItem->getReferencedId() ?? '']));
+        $discount = new DiscountLineItem('foo', $priceDefinition, ['discountScope' => 'foo', 'discountType' => 'bar'], null);
+
+        $packager = new CartScopeDiscountPackager();
+        $items = $packager->getMatchingItems($discount, $cart, $context);
+
+        $expected = new DiscountPackageCollection([
+            new DiscountPackage(
+                new LineItemQuantityCollection([
+                    new LineItemQuantity($matchingLineItem->getId(), 1),
+                    new LineItemQuantity($matchingLineItem->getId(), 1),
+                ])
+            ),
+        ]);
+
+        static::assertEquals($expected, $items);
+
+        $priceDefinition = new PercentagePriceDefinition(42, new LineItemRule(Rule::OPERATOR_EQ, [Uuid::randomHex()]));
+        $discount = new DiscountLineItem('foo', $priceDefinition, ['discountScope' => 'foo', 'discountType' => 'bar'], null);
+
+        $items = $packager->getMatchingItems($discount, $cart, $context);
+
+        static::assertEquals(new DiscountPackageCollection([]), $items);
+    }
+}

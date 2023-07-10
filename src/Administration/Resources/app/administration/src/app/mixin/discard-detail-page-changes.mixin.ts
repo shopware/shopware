b9638ed831fca1ee09da07c59aaadb3d41443726
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-07-10 09:22:50 +0200

    NEXT-28163 - Fix promotion listing

--- src/Administration/Resources/app/administration/src/app/mixin/discard-detail-page-changes.mixin.ts
+++ src/Administration/Resources/app/administration/src/app/mixin/discard-detail-page-changes.mixin.ts
@@ -1,5 +1,4 @@
 import type { NavigationGuardNext, Route } from 'vue-router';
-import { defineComponent } from 'vue';
 
 const { Mixin } = Shopware;
 const { types } = Shopware.Utils;
@@ -17,67 +16,65 @@ const { types } = Shopware.Utils;
  *   ],
  *
  */
-export default Mixin.register('discard-detail-page-changes', defineComponent(
-    (...entityNames: Array<string|Array<string>>) => {
-        const entities: string[] = [];
+export default Mixin.register('discard-detail-page-changes', (...entityNames: Array<string|Array<string>>) => {
+    const entities: string[] = [];
 
-        function tryAddEntity(name: string) {
-            if (types.isString(name)) {
-                entities.push(name);
-            }
+    function tryAddEntity(name: string) {
+        if (types.isString(name)) {
+            entities.push(name);
+        }
+    }
+
+    entityNames.forEach((name) => {
+        if (types.isArray(name)) {
+            name.forEach((item) => {
+                tryAddEntity(item);
+            });
+            return;
         }
 
-        entityNames.forEach((name) => {
-            if (types.isArray(name)) {
-                name.forEach((item) => {
-                    tryAddEntity(item);
-                });
-                return;
-            }
+        tryAddEntity(name);
+    });
 
-            tryAddEntity(name);
-        });
+    if (entities.length < 1) {
+        throw new Error('discard-detail-page-changes.mixin - You need to provide the entity names');
+    }
 
-        if (entities.length < 1) {
-            throw new Error('discard-detail-page-changes.mixin - You need to provide the entity names');
-        }
+    return Shopware.Component.wrapComponentConfig({
+        beforeRouteLeave(to: Route, from: Route, next: NavigationGuardNext) {
+            this.discardChanges();
 
-        return Shopware.Component.wrapComponentConfig({
-            beforeRouteLeave(to: Route, from: Route, next: NavigationGuardNext) {
+            next();
+        },
+
+        watch: {
+            '$route.params.id'() {
                 this.discardChanges();
-
-                next();
             },
+        },
 
-            watch: {
-                '$route.params.id'() {
-                    this.discardChanges();
-                },
-            },
+        methods: {
+            discardChanges() {
+                entities.forEach((entityName) => {
+                    // @ts-expect-error - we check if the entity exists on the component
+                    // eslint-disable-next-line max-len
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+                    const entity: EntitySchema.Entity<any> = this[entityName];
 
-            methods: {
-                discardChanges() {
-                    entities.forEach((entityName) => {
-                        // @ts-expect-error - we check if the entity exists on the component
-                        // eslint-disable-next-line max-len
-                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
-                        const entity: EntitySchema.Entity<any> = this[entityName];
-
-                        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                        if (entity && typeof entity.discardChanges === 'function') {
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    if (entity && typeof entity.discardChanges === 'function') {
                         // eslint-disable-next-line max-len
                         // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-                            entity.discardChanges();
-                            return;
-                        }
+                        entity.discardChanges();
+                        return;
+                    }
 
-                        Shopware.Utils.debug.warn(
-                            'Discard-detail-page-changes Mixin',
-                            `Could not discard changes for entity with name "${entityName}".`,
-                        );
-                    });
-                },
+                    Shopware.Utils.debug.warn(
+                        'Discard-detail-page-changes Mixin',
+                        `Could not discard changes for entity with name "${entityName}".`,
+                    );
+                });
             },
-        });
-    },
-));
+        },
+    });
+});

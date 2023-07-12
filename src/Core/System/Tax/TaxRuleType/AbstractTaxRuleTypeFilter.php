4e3dabe96693e6e2bfd88a423df1a511ef3fe1f3
Marcin Kaczor <marcink@codepro.space>
2023-07-12 17:06:05 +0200

    feat: add package type

--- src/Core/System/Tax/TaxRuleType/AbstractTaxRuleTypeFilter.php
+++ src/Core/System/Tax/TaxRuleType/AbstractTaxRuleTypeFilter.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Shopware\Core\System\Tax\TaxRuleType;
 
+use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\Tax\Aggregate\TaxRule\TaxRuleEntity;
 
+#[Package('customer-order')]
 abstract class AbstractTaxRuleTypeFilter implements TaxRuleTypeFilterInterface
 {
     protected function isTaxActive(TaxRuleEntity $taxRuleEntity): bool

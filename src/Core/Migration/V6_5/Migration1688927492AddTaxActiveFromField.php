01ee2a7e51b63701f449b0803ac67a0ca5a97422
Marcin Kaczor <marcink@codepro.space>
2023-07-13 15:23:58 +0200

    feat: add internal comment

--- src/Core/Migration/V6_5/Migration1688927492AddTaxActiveFromField.php
+++ src/Core/Migration/V6_5/Migration1688927492AddTaxActiveFromField.php
@@ -6,6 +6,9 @@ use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
+/**
+ * @internal
+ */
 #[Package('core')]
 class Migration1688927492AddTaxActiveFromField extends MigrationStep
 {

09fef4c6b30f64d8d03e799b356c7e192335feb6
Max Stegmeyer <m.stegmeyer@shopware.com>
2023-07-21 13:28:52 +0200

    NEXT-29451 - Add cron and date interval fields

---
+++ tests/unit/php/Core/Framework/DataAbstractionLayer/Field/DateIntervalFieldTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Framework\DataAbstractionLayer\Field;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\DateIntervalField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldSerializer\DateIntervalFieldSerializer;
+use Shopware\Core\Framework\Log\Package;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @covers \Shopware\Core\Framework\DataAbstractionLayer\Field\DateIntervalField
+ *
+ * @internal
+ */
+#[Package('checkout')]
+class DateIntervalFieldTest extends TestCase
+{
+    private DateIntervalField $field;
+
+    protected function setUp(): void
+    {
+        $this->field = new DateIntervalField('name', 'name');
+    }
+
+    public function testGetStorageName(): void
+    {
+        static::assertSame('name', $this->field->getStorageName());
+    }
+
+    public function testGetSerializerWillReturnFieldSerializerInterfaceInstance(): void
+    {
+        $registry = $this->createMock(DefinitionInstanceRegistry::class);
+        $registry
+            ->method('getSerializer')
+            ->willReturn(
+                new DateIntervalFieldSerializer(
+                    $this->createMock(ValidatorInterface::class),
+                    $registry
+                )
+            );
+        $registry->method('getResolver');
+        $registry->method('getAccessorBuilder');
+        $this->field->compile($registry);
+
+        static::assertInstanceOf(DateIntervalFieldSerializer::class, $this->field->getSerializer());
+    }
+}

ee770fbd4987a3593c4502a090e3416af04ee290
Sebastian Seggewiß <s.seggewiss@shopware.com>
2023-08-09 11:13:58 +0200

    NEXT-28991 - Fix sw-url-field models

--- src/Administration/Resources/app/administration/src/app/component/form/sw-url-field/index.js
+++ src/Administration/Resources/app/administration/src/app/component/form/sw-url-field/index.js
@@ -135,9 +135,7 @@ Component.extend('sw-url-field', 'sw-text-field', {
                 this.currentValue = validated;
 
                 if (this.feature.isActive('VUE3')) {
-                    if (this.value !== this.url) {
-                        this.$emit('update:value', this.url);
-                    }
+                    this.$emit('update:value', this.url);
                     return;
                 }
 
@@ -147,6 +145,13 @@ Component.extend('sw-url-field', 'sw-text-field', {
 
         handleEmptyUrl() {
             this.currentValue = '';
+
+            if (this.feature.isActive('VUE3')) {
+                this.$emit('update:value', '');
+                return;
+            }
+
+            this.$emit('input', '');
         },
 
         validateCurrentValue(value) {

6325100abff3bd1fa068408495e02ae9f199b737
Dinh Duy <d.dinh@shopware.com>
2023-08-09 14:05:04 +0700

    NEXT-29735 - Handle exception for country not found

--- tests/unit/Core/Checkout/Customer/Validation/CustomerZipcodeValidatorTest.php
+++ tests/unit/Core/Checkout/Customer/Validation/CustomerZipcodeValidatorTest.php
@@ -4,10 +4,12 @@ namespace Shopware\Tests\Unit\Core\Checkout\Customer\Validation;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Customer\CustomerException;
 use Shopware\Core\Checkout\Customer\Validation\Constraint\CustomerZipCode;
 use Shopware\Core\Checkout\Customer\Validation\Constraint\CustomerZipCodeValidator;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\Country\CountryEntity;
 use Shopware\Core\System\Country\Exception\CountryNotFoundException;
@@ -110,7 +112,12 @@ class CustomerZipcodeValidatorTest extends TestCase
 
     public function testValidateWithInvalidCountryId(): void
     {
-        static::expectException(CountryNotFoundException::class);
+        if (!Feature::isActive('v6.6.0.0')) {
+            static::expectException(CountryNotFoundException::class);
+        } else {
+            static::expectException(CustomerException::class);
+        }
+
         $result = $this->createMock(EntitySearchResult::class);
         $result->expects(static::once())->method('get')->with($this->constraint->countryId)->willReturn(null);
 

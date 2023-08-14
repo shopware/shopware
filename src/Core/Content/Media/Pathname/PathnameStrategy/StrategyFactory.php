daae4d5c21092432a445d309b21db33bb33b5d97
Oliver Skroblin <o.skroblin@shopware.com>
2023-08-14 14:57:05 +0200

    NEXT-25584 - Implement new media path strategies to get rid of the DAL dependency.

--- src/Core/Content/Media/Pathname/PathnameStrategy/StrategyFactory.php
+++ src/Core/Content/Media/Pathname/PathnameStrategy/StrategyFactory.php
@@ -5,6 +5,9 @@ namespace Shopware\Core\Content\Media\Pathname\PathnameStrategy;
 use Shopware\Core\Content\Media\MediaException;
 use Shopware\Core\Framework\Log\Package;
 
+/**
+ * @deprecated tag:v6.6.0 - reason:factory-for-deprecation - Use PathStrategyFactory instead
+ */
 #[Package('buyers-experience')]
 class StrategyFactory
 {

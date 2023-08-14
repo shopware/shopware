80bd5a0f8011e493029e23f05e489a1465b38622
Oliver Skroblin <o.skroblin@shopware.com>
2023-08-14 14:56:04 +0200

    NEXT-25584 - Implement events for the new path system to allow extension points and trigger path generation

---
+++ src/Core/Content/Media/Path/Contract/Event/ThumbnailLocationEvent.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Media\Path\Contract\Event;
+
+use Shopware\Core\Content\Media\Path\Contract\Struct\ThumbnailLocationStruct;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * @public
+ *
+ * The event is dispatched, when location for a thumbnail should be generated afterward and can be used
+ * to extend the data which is required for this process.
+ *
+ * @implements \IteratorAggregate<array-key, ThumbnailLocationStruct>
+ */
+#[Package('content')]
+class ThumbnailLocationEvent implements \IteratorAggregate
+{
+    /**
+     * @param array<string, ThumbnailLocationStruct> $locations
+     */
+    public function __construct(public array $locations)
+    {
+    }
+
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->locations);
+    }
+}

e6cb71c2615092b0c209c290dfc815d25aa2fcfc
Christian Rades <c.rades@shopware.com>
2023-08-15 15:28:41 +0200

    NEXT-27041 - Define WriteTypeException as 400 status

---
+++ tests/unit/Core/Framework/DataAbstractionLayer/Write/Command/WriteTypeIntendExceptionTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Framework\DataAbstractionLayer\Write\Command;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\InsertCommand;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\UpdateCommand;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\WriteTypeIntendException;
+use Shopware\Core\Framework\Test\Api\ApiDefinition\EntityDefinition\SimpleDefinition;
+
+/**
+ * @internal
+ *
+ * @covers \Shopware\Core\Framework\DataAbstractionLayer\Write\Command\WriteTypeIntendException
+ */
+class WriteTypeIntendExceptionTest extends TestCase
+{
+    public function testErrorSignalsBadRequest(): void
+    {
+        $exception = new WriteTypeIntendException(
+            new SimpleDefinition(),
+            'expected',
+            'actual'
+        );
+
+        static::assertSame(400, $exception->getStatusCode());
+    }
+
+    public function testDoesHintAtCorrectApiUsage(): void
+    {
+        $exception = new WriteTypeIntendException(
+            new SimpleDefinition(),
+            UpdateCommand::class,
+            InsertCommand::class
+        );
+
+        static::assertStringContainsString('Use POST method', $exception->getMessage());
+    }
+}

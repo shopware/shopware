ac2744dd3bd545b6dd13fb2f7251c0413bc1fc03
Soner Sayakci <s.sayakci@shopware.com>
2023-08-18 09:39:21 +0200

    NEXT-29980 - Allow running scheduled task handler as crontab

---
+++ tests/unit/Core/Framework/MessageQueue/Command/ScheduledTaskRunnerTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Framework\MessageQueue\Command;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemPoolInterface;
+use Shopware\Core\Framework\MessageQueue\Command\ScheduledTaskRunner;
+use Shopware\Core\Framework\MessageQueue\ScheduledTask\Scheduler\TaskScheduler;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @internal
+ *
+ * @covers \Shopware\Core\Framework\MessageQueue\Command\ScheduledTaskRunner
+ */
+class ScheduledTaskRunnerTest extends TestCase
+{
+    public function testScheduleDirectly(): void
+    {
+        $scheduler = $this->createMock(TaskScheduler::class);
+        $scheduler
+            ->expects(static::once())
+            ->method('queueScheduledTasks');
+
+        $runner = new ScheduledTaskRunner(
+            $scheduler,
+            $this->createMock(CacheItemPoolInterface::class)
+        );
+
+        $tester = new CommandTester($runner);
+
+        $tester->execute([
+            '--no-wait' => true,
+        ]);
+    }
+}

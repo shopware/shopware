977b3e19fe8a97b21ca73d73890cee8f6d683c65
Soner Sayakci <s.sayakci@shopware.com>
2023-08-30 10:17:02 +0000

    NEXT-30236 - Remove Cached SEO resolver

--- src/Core/Framework/Adapter/Cache/CacheInvalidationSubscriber.php
+++ src/Core/Framework/Adapter/Cache/CacheInvalidationSubscriber.php
@@ -32,7 +32,6 @@ use Shopware\Core\Content\Property\Aggregate\PropertyGroupOption\PropertyGroupOp
 use Shopware\Core\Content\Property\Aggregate\PropertyGroupOptionTranslation\PropertyGroupOptionTranslationDefinition;
 use Shopware\Core\Content\Property\Aggregate\PropertyGroupTranslation\PropertyGroupTranslationDefinition;
 use Shopware\Core\Content\Property\PropertyGroupDefinition;
-use Shopware\Core\Content\Seo\CachedSeoResolver;
 use Shopware\Core\Content\Seo\Event\SeoUrlUpdateEvent;
 use Shopware\Core\Content\Sitemap\Event\SitemapGeneratedEvent;
 use Shopware\Core\Content\Sitemap\SalesChannel\CachedSitemapRoute;
@@ -159,14 +158,11 @@ class CacheInvalidationSubscriber
         $this->cacheInvalidator->invalidate($logs);
     }
 
+    /**
+     * @deprecated tag:v6.6.0 - Will be removed without a replacement - reason:remove-subscriber
+     */
     public function invalidateSeoUrls(SeoUrlUpdateEvent $event): void
     {
-        // invalidates the cache for the seo url resolver based on the path infos which used for the new seo urls
-        $urls = $event->getSeoUrls();
-
-        $pathInfo = array_column($urls, 'pathInfo');
-
-        $this->cacheInvalidator->invalidate(array_map([CachedSeoResolver::class, 'buildName'], $pathInfo));
     }
 
     public function invalidateRules(): void

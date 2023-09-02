25fd41561cbc691f9678e650abe38f1d41b7230f
Joshua Behrens <behrens@heptacom.de>
2023-09-02 15:50:39 +0200

    NEXT-33773 - Add IPv6 CIDR suggestions to maintenance configuration
    fixes #3296

--- src/Administration/Framework/Routing/KnownIps/KnownIpsCollector.php
+++ src/Administration/Framework/Routing/KnownIps/KnownIpsCollector.php
@@ -12,18 +12,43 @@ class KnownIpsCollector implements KnownIpsCollectorInterface
      * The result is mapped as ip => name|snippet-key. So by default it will look like this:
      * <code>
      *     [
-     *         '127.0.0.1' => 'global.sw-multi-tag-ip-select.knownIps.you'
+     *         '127.0.0.1' => 'global.sw-multi-tag-ip-select.knownIps.you',
+     *         // or
+     *         '2001:0db8:0123:4567:89ab:cdef:1234:5678' => 'global.sw-multi-tag-ip-select.knownIps.you',
+     *         '2001:0db8:0123:4567::/64' => 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block64',
+     *         '2001:0db8:0123:4500::/56' => 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block56'
      *     ]
      * </code>
      */
     public function collectIps(Request $request): array
     {
         $result = [];
+        $clientIp = $request->getClientIp();
 
-        if ($request->getClientIp()) {
-            $result[$request->getClientIp()] = 'global.sw-multi-tag-ip-select.knownIps.you';
+        if ($clientIp) {
+            $result[$clientIp] = 'global.sw-multi-tag-ip-select.knownIps.you';
+
+            $isIpV6 = \str_contains($clientIp, ':') && \filter_var($clientIp, \FILTER_VALIDATE_IP, \FILTER_FLAG_IPV6);
+
+            if ($isIpV6 && $this->canPhpIpv6()) {
+                // replace last half (and slightly more for /56)
+                $cidr64 = \inet_ntop(\hex2bin(\substr(\bin2hex(\inet_pton($clientIp)), 0, 16) . \str_repeat('0', 16))) . '/64';
+                $cidr56 = \inet_ntop(\hex2bin(\substr(\bin2hex(\inet_pton($clientIp)), 0, 14) . \str_repeat('0', 18))) . '/56';
+
+                $result[$cidr64] = 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block64';
+                $result[$cidr56] = 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block56';
+            }
         }
 
         return $result;
+    }
+
+    private function canPhpIpv6(): bool
+    {
+        if (!((\extension_loaded('sockets') && \defined('AF_INET6')) || @\inet_pton('::1'))) {
+            return false;
+        }
+
+        return true;
     }
 }
---
+++ tests/unit/Administration/Framework/Routing/KnownIps/KnownIpsCollectorTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Administration\Framework\Routing\KnownIps;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Shopware\Administration\Framework\Routing\KnownIps\KnownIpsCollector;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @internal
+ */
+#[CoversClass(KnownIpsCollector::class)]
+final class KnownIpsCollectorTest extends TestCase
+{
+    public function testSuggestionsForIpv4(): void
+    {
+        $service = new KnownIpsCollector();
+        $suggestions = $service->collectIps(new Request(server: ['REMOTE_ADDR' => '127.0.0.1']));
+
+        static::assertSame([
+            '127.0.0.1' => 'global.sw-multi-tag-ip-select.knownIps.you',
+        ], $suggestions);
+    }
+
+    public function testSuggestionsForIpv6(): void
+    {
+        $service = new KnownIpsCollector();
+        $suggestions = $service->collectIps(new Request(server: ['REMOTE_ADDR' => '2001:0db8:0123:4567:89ab:cdef:1234:5678']));
+
+        static::assertEqualsCanonicalizing([
+            '2001:0db8:0123:4567:89ab:cdef:1234:5678' => 'global.sw-multi-tag-ip-select.knownIps.you',
+            '2001:0db8:0123:4567::/64' => 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block64',
+            '2001:0db8:0123:4500::/56' => 'global.sw-multi-tag-ip-select.knownIps.youIPv6Block56'
+        ], $suggestions);
+    }
+}

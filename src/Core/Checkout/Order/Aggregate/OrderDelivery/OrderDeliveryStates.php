427c933d453bbd14bb42de78a68322d6dc052ed5
lxregistry <32699348+lxregistry@users.noreply.github.com>
2023-09-11 15:22:51 +0300

    Update OrderDeliveryStates.php
    please add 2 more states
        public const STATE_PARTIALLY_DELIVERED = 'delivered_partially';
        public const STATE_DELIVERED = 'delivered';

--- src/Core/Checkout/Order/Aggregate/OrderDelivery/OrderDeliveryStates.php
+++ src/Core/Checkout/Order/Aggregate/OrderDelivery/OrderDeliveryStates.php
@@ -11,7 +11,9 @@ final class OrderDeliveryStates
     public const STATE_OPEN = 'open';
     public const STATE_PARTIALLY_SHIPPED = 'shipped_partially';
     public const STATE_SHIPPED = 'shipped';
-    public const STATE_RETURNED = 'returned';
+    public const STATE_PARTIALLY_DELIVERED = 'delivered_partially';
+    public const STATE_DELIVERED = 'delivered';
     public const STATE_PARTIALLY_RETURNED = 'returned_partially';
+    public const STATE_RETURNED = 'returned';
     public const STATE_CANCELLED = 'cancelled';
 }

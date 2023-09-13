dc3b11a50faaebed33be51a60a458a53e2b1c097
Soner Sayakci <s.sayakci@shopware.com>
2023-09-13 15:20:34 +0200

    NEXT-30476 - Make uasort faster when ordering countries

--- src/Core/System/Country/Aggregate/CountryState/CountryStateCollection.php
+++ src/Core/System/Country/Aggregate/CountryState/CountryStateCollection.php
@@ -11,6 +11,9 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('system-settings')]
 class CountryStateCollection extends EntityCollection
 {
+    /**
+     * @return array<string>
+     */
     public function getCountryIds(): array
     {
         return $this->fmap(fn (CountryStateEntity $countryState) => $countryState->getCountryId());
@@ -23,13 +26,18 @@ class CountryStateCollection extends EntityCollection
 
     public function sortByPositionAndName(): void
     {
-        uasort($this->elements, function (CountryStateEntity $a, CountryStateEntity $b) {
-            if ($a->getPosition() !== $b->getPosition()) {
-                return $a->getPosition() <=> $b->getPosition();
+        uasort($this->elements, static function (CountryStateEntity $a, CountryStateEntity $b) {
+            $aPosition = $a->getPosition();
+            $bPosition = $b->getPosition();
+
+            if ($aPosition !== $bPosition) {
+                return $aPosition <=> $bPosition;
             }
 
-            if ($a->getTranslation('name') !== $b->getTranslation('name')) {
-                return strnatcasecmp((string) $a->getTranslation('name'), (string) $b->getTranslation('name'));
+            $aName = (string) $a->getTranslation('name');
+            $bName = (string) $b->getTranslation('name');
+            if ($aName !== $bName) {
+                return strnatcasecmp($aName, $bName);
             }
 
             return 0;

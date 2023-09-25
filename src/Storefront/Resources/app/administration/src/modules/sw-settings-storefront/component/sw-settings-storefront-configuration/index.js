adb0d806e364bb06539af99d872c87f2b2b7d488
Nils Haberkamp <n.haberkamp@shopware.com>
2023-09-25 13:32:27 +0200

    NEXT-30533 - update code mapping for sw-settings-storefront module

--- src/Storefront/Resources/app/administration/src/modules/sw-settings-storefront/component/sw-settings-storefront-configuration/index.js
+++ src/Storefront/Resources/app/administration/src/modules/sw-settings-storefront/component/sw-settings-storefront-configuration/index.js
@@ -2,7 +2,7 @@ import template from './sw-settings-storefront-configuration.html.twig';
 import './sw-settings-storefront-configuration.scss';
 
 /**
- * @package services-settings
+ * @package system-settings
  */
 Shopware.Component.register('sw-settings-storefront-configuration', {
     template,
--- src/Storefront/Resources/app/administration/src/modules/sw-settings-storefront/index.js
+++ src/Storefront/Resources/app/administration/src/modules/sw-settings-storefront/index.js
@@ -2,7 +2,7 @@ import './page/sw-settings-storefront-index';
 import './component/sw-settings-storefront-configuration';
 
 /**
- * @package services-settings
+ * @package system-settings
  */
 Shopware.Module.register('sw-settings-storefront', {
     type: 'core',

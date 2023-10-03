7a5089769d7a537d6c717116d57e3d69b9e6ef22
Y Tran <y.tran@shopware.com>
2023-10-03 11:26:14 +0700

    NEXT-29060 - Fix generating documents very slow once a reasonable number of documents exists

--- src/Core/Checkout/Document/Service/DocumentGenerator.php
+++ src/Core/Checkout/Document/Service/DocumentGenerator.php
@@ -26,7 +26,6 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\NotFilter;
 use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Util\Random;

472e6f1f2027b1e091123aaf367f2c4e54088eae
Thuong Le <t.le@shopware.com>
2023-10-04 11:43:31 +0000

    NEXT-30040 - Sugar syntax for es definition

--- src/Elasticsearch/Product/Event/ElasticsearchProductCustomFieldsMappingEvent.php
+++ src/Elasticsearch/Product/Event/ElasticsearchProductCustomFieldsMappingEvent.php
@@ -2,13 +2,18 @@
 
 namespace Shopware\Elasticsearch\Product\Event;
 
+use Shopware\Core\Content\Product\ProductDefinition;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\Event\ShopwareEvent;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\CustomField\CustomFieldTypes;
+use Shopware\Elasticsearch\Event\ElasticsearchCustomFieldsMappingEvent;
 
+/**
+ * @deprecated tag:v6.6.0 - Will be removed, use \Shopware\Elasticsearch\Product\Event\ElasticsearchCustomFieldsMappingEvent instead
+ */
 #[Package('core')]
-class ElasticsearchProductCustomFieldsMappingEvent implements ShopwareEvent
+class ElasticsearchProductCustomFieldsMappingEvent extends ElasticsearchCustomFieldsMappingEvent
 {
     /**
      * @param array<string, string> $mapping
@@ -17,10 +22,16 @@ class ElasticsearchProductCustomFieldsMappingEvent implements ShopwareEvent
         protected array $mapping,
         protected Context $context
     ) {
+        parent::__construct(ProductDefinition::ENTITY_NAME, $this->mapping, $this->context);
     }
 
     public function getContext(): Context
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6.6.0.0',
+            Feature::deprecatedClassMessage(self::class, 'v6.6.0.0', 'use ElasticsearchCustomFieldsMappingEvent::getContext instead')
+        );
+
         return $this->context;
     }
 
@@ -29,6 +40,11 @@ class ElasticsearchProductCustomFieldsMappingEvent implements ShopwareEvent
      */
     public function setMapping(string $field, string $type): void
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6.6.0.0',
+            Feature::deprecatedClassMessage(self::class, 'v6.6.0.0', 'use ElasticsearchCustomFieldsMappingEvent::setMapping instead')
+        );
+
         $this->mapping[$field] = $type;
     }
 
@@ -38,11 +54,21 @@ class ElasticsearchProductCustomFieldsMappingEvent implements ShopwareEvent
      */
     public function getMapping(string $field)
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6.6.0.0',
+            Feature::deprecatedClassMessage(self::class, 'v6.6.0.0', 'use ElasticsearchCustomFieldsMappingEvent::getMapping instead')
+        );
+
         return $this->mapping[$field] ?? null;
     }
 
     public function removeMapping(string $field): void
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6.6.0.0',
+            Feature::deprecatedClassMessage(self::class, 'v6.6.0.0', 'use ElasticsearchCustomFieldsMappingEvent::removeMapping instead')
+        );
+
         if (isset($this->mapping[$field])) {
             unset($this->mapping[$field]);
         }
@@ -53,6 +79,11 @@ class ElasticsearchProductCustomFieldsMappingEvent implements ShopwareEvent
      */
     public function getMappings(): array
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6.6.0.0',
+            Feature::deprecatedClassMessage(self::class, 'v6.6.0.0', 'use ElasticsearchCustomFieldsMappingEvent::getMappings instead')
+        );
+
         return $this->mapping;
     }
 }

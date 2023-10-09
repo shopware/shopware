0b3c2c877b97b90aa5c5cb0c6df4afb99db6162e
Max Stegmeyer <m.stegmeyer@shopware.com>
2023-10-09 11:07:37 +0200

    NEXT-30753 - Fix inactive payment method load

--- src/Core/System/SalesChannel/Context/SalesChannelContextFactory.php
+++ src/Core/System/SalesChannel/Context/SalesChannelContextFactory.php
@@ -22,7 +22,6 @@ use Shopware\Core\System\Currency\Aggregate\CurrencyCountryRounding\CurrencyCoun
 use Shopware\Core\System\SalesChannel\BaseContext;
 use Shopware\Core\System\SalesChannel\Event\SalesChannelContextPermissionsChangedEvent;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\System\SalesChannel\SalesChannelException;
 use Shopware\Core\System\Tax\Aggregate\TaxRule\TaxRuleCollection;
 use Shopware\Core\System\Tax\Aggregate\TaxRule\TaxRuleEntity;
 use Shopware\Core\System\Tax\TaxCollection;
@@ -197,12 +196,14 @@ class SalesChannelContextFactory extends AbstractSalesChannelContextFactory
         $criteria = new Criteria([$id]);
         $criteria->addAssociation('media');
         $criteria->setTitle('context-factory::payment-method');
+        $criteria->addFilter(new EqualsFilter('active', 1));
+        $criteria->addFilter(new EqualsFilter('salesChannels.id', $context->getSalesChannel()->getId()));
 
         /** @var PaymentMethodEntity|null $paymentMethod */
         $paymentMethod = $this->paymentMethodRepository->search($criteria, $context->getContext())->get($id);
 
         if (!$paymentMethod) {
-            throw SalesChannelException::unknownPaymentMethod($id);
+            return $context->getPaymentMethod();
         }
 
         return $paymentMethod;

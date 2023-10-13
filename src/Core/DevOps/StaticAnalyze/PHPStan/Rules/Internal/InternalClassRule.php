2b673b28ceed3a0006aa34ec71b0114b4b1a9e8b
Jonas Elfering <j.elfering@shopware.com>
2023-10-13 11:54:00 +0200

    NEXT-31012 - Add script to dump ignored phpstan errors per area

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Internal/InternalClassRule.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Internal/InternalClassRule.php
@@ -6,7 +6,6 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Node\InClassNode;
 use PHPStan\Reflection\ClassReflection;
-use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleError;
 use PHPUnit\Framework\TestCase;
@@ -56,13 +55,6 @@ class InternalClassRule implements Rule
         DemodataCommand::class,
         DemodataRequestCreatedEvent::class,
     ];
-
-    private ReflectionProvider $reflectionProvider;
-
-    public function __construct(ReflectionProvider $reflectionProvider)
-    {
-        $this->reflectionProvider = $reflectionProvider;
-    }
 
     public function getNodeType(): string
     {
@@ -242,7 +234,9 @@ class InternalClassRule implements Rule
 
     private function isParentInternalAndAbstract(Scope $scope): bool
     {
-        $parent = $scope->getClassReflection()->getParentClass();
+        $class = $scope->getClassReflection();
+        \assert($class !== null);
+        $parent = $class->getParentClass();
 
         if ($parent === null) {
             return false;

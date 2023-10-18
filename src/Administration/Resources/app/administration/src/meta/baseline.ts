acc9cec216d3d408686a576f64ebea8e1e156620
Le Nguyen <l.nguyen@shopware.com>
2023-10-18 13:06:49 +0700

    NEXT-31021 - Rule builder date range condition broken

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -70,7 +70,6 @@ const missingTests = [
     'src/app/component/grid/sw-grid-row/index.js',
     'src/app/component/rule/condition-type/sw-condition-billing-zip-code/index.js',
     'src/app/component/rule/condition-type/sw-condition-customer-custom-field/index.js',
-    'src/app/component/rule/condition-type/sw-condition-date-range/index.js',
     'src/app/component/rule/condition-type/sw-condition-generic-line-item/index.js',
     'src/app/component/rule/condition-type/sw-condition-goods-count/index.js',
     'src/app/component/rule/condition-type/sw-condition-goods-price/index.js',

190def200e12f293fd574a8623f5e3955584a732
floriankeller <f.keller@shopware.com>
2023-11-02 10:43:11 +0100

    NEXT-29512 - Chance customer profile

--- src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-detail/index.js
@@ -169,7 +169,7 @@ export default {
     },
 
     methods: {
-        async createdComponent() {
+        async loadCustomer() {
             const defaultSalutationId = await this.getDefaultSalutation();
 
             Shopware.ExtensionAPI.publishData({

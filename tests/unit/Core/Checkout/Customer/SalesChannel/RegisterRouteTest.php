668733093cf6968699b0abb9e31876a720773818
Benny Poensgen <poensgen@vanwittlaer.de>
2023-11-03 14:01:41 +0100

    NEXT-31466 - Change RegisterRoute.php and RegisterController.php to facilitate passing of redirectParameters with double-optin confirmation request
    - NEXT-31466
    fixes #3402

--- tests/unit/Core/Checkout/Customer/SalesChannel/RegisterRouteTest.php
+++ tests/unit/Core/Checkout/Customer/SalesChannel/RegisterRouteTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use Shopware\Core\Checkout\Customer\CustomerDefinition;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\CustomerException;
+use Shopware\Core\Checkout\Customer\Event\CustomerDoubleOptInRegistrationEvent;
 use Shopware\Core\Checkout\Customer\SalesChannel\RegisterRoute;
 use Shopware\Core\Checkout\Customer\Validation\Constraint\CustomerZipCode;
 use Shopware\Core\Framework\Context;
@@ -480,6 +481,79 @@ class RegisterRouteTest extends TestCase
             ],
             'accountType' => CustomerEntity::ACCOUNT_TYPE_PRIVATE,
             'salutationId' => '',
+        ];
+
+        $salesChannelContext = $this->createMock(SalesChannelContext::class);
+        $salesChannelContext->method('getSalesChannelId')->willReturn(TestDefaults::SALES_CHANNEL);
+
+        $register->register(new RequestDataBag($data), $salesChannelContext, false);
+    }
+
+    public function testRedirectParameters(): void
+    {
+        $systemConfigService = new StaticSystemConfigService([
+            TestDefaults::SALES_CHANNEL => [
+                'core.loginRegistration.passwordMinLength' => '8',
+                'core.loginRegistration.doubleOptInRegistration' => true,
+                'core.cart.wishlistEnabled' => true,
+            ],
+            'core.systemWideLoginRegistration.isCustomerBoundToSalesChannel' => true,
+        ]);
+
+        $result = $this->createMock(EntitySearchResult::class);
+        $customerEntity = new CustomerEntity();
+        $customerEntity->setDoubleOptInRegistration(true);
+        $customerEntity->setId('customer-1');
+        $customerEntity->setGuest(false);
+        $customerEntity->setEmail('test@test.de');
+        $result->method('first')->willReturn($customerEntity);
+
+        $customerRepository = $this->createMock(EntityRepository::class);
+        $customerRepository->method('search')->willReturn($result);
+
+        $eventDispatcher = $this->createMock(EventDispatcher::class);
+        $eventDispatcher
+                ->expects(static::atLeast(1))
+                ->method('dispatch')
+                ->with(
+                    static::callback(function ($event): bool {
+                        if ($event instanceof CustomerDoubleOptInRegistrationEvent) {
+                            $query = [];
+                            $queryString = \parse_url($event->getConfirmUrl(), \PHP_URL_QUERY);
+                            self::assertIsString($queryString);
+                            \parse_str($queryString, $query);
+                            self::assertArrayHasKey('productId', $query);
+                            self::assertSame('018b906b869273fea7926f161dd23911', $query['productId']);
+                        }
+
+                        return true;
+                    })
+                );
+
+        $register = new RegisterRoute(
+            $eventDispatcher,
+            $this->createMock(NumberRangeValueGeneratorInterface::class),
+            $this->createMock(DataValidator::class),
+            $this->createMock(DataValidationFactoryInterface::class),
+            $this->createMock(DataValidationFactoryInterface::class),
+            $systemConfigService,
+            $customerRepository,
+            $this->createMock(SalesChannelContextPersister::class),
+            $this->createMock(SalesChannelRepository::class),
+            $this->createMock(Connection::class),
+            $this->createMock(SalesChannelContextService::class),
+            $this->createMock(StoreApiCustomFieldMapper::class),
+            $this->createMock(EntityRepository::class),
+        );
+
+        $data = [
+            'email' => 'test@test.de',
+            'billingAddress' => [
+                'countryId' => Uuid::randomHex(),
+            ],
+            'storefrontUrl' => 'http://localhost:8000',
+            'redirectTo' => 'frontend.wishlist.add.after.login',
+            'redirectParameters' => '{"productId":"018b906b869273fea7926f161dd23911"}',
         ];
 
         $salesChannelContext = $this->createMock(SalesChannelContext::class);

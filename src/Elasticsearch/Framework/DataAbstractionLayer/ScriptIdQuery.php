f1e6dafc34e375852336a5f28e60377cd18b6af4
Oliver Skroblin <o.skroblin@shopware.com>
2023-11-14 08:09:53 +0000

    NEXT-29212 - Do not store es scripts on server

--- src/Elasticsearch/Framework/DataAbstractionLayer/ScriptIdQuery.php
+++ src/Elasticsearch/Framework/DataAbstractionLayer/ScriptIdQuery.php
@@ -4,25 +4,41 @@ namespace Shopware\Elasticsearch\Framework\DataAbstractionLayer;
 
 use OpenSearchDSL\BuilderInterface;
 use OpenSearchDSL\ParametersTrait;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 
+/**
+ * @deprecated tag:v6.6.0 - Will be removed, use OpenSearchDSL\Sort\ScriptSort instead
+ */
 #[Package('core')]
 class ScriptIdQuery implements BuilderInterface
 {
     use ParametersTrait;
 
+    private ?string $id;
+
+    private ?string $source;
+
     /**
      * @param array<mixed> $parameters
      */
     public function __construct(
-        private readonly string $id,
-        array $parameters = []
+        ?string $id = null,
+        array $parameters = [],
+        ?string $source = null
     ) {
+        $this->id = $id;
+        $this->source = $source;
         $this->setParameters($parameters);
     }
 
     public function getType(): string
     {
+        Feature::triggerDeprecationOrThrow(
+            'v6_6_0_0',
+            Feature::deprecatedMethodMessage(self::class, __METHOD__, '6.6.0.0')
+        );
+
         return 'script';
     }
 
@@ -33,7 +49,22 @@ class ScriptIdQuery implements BuilderInterface
      */
     public function toArray(): array
     {
-        $query = ['id' => $this->id];
+        Feature::triggerDeprecationOrThrow(
+            'v6_6_0_0',
+            Feature::deprecatedMethodMessage(self::class, __METHOD__, '6.6.0.0')
+        );
+
+        $query = [];
+
+        if ($this->id) {
+            $query['id'] = $this->id;
+        }
+
+        if ($this->source) {
+            $query['source'] = $this->source;
+            $query['lang'] = 'painless';
+        }
+
         $output = $this->processArray($query);
 
         return [$this->getType() => ['script' => $output]];
--- tests/unit/Elasticsearch/Framework/DataAbstractionLayer/ScriptIdQueryTest.php
+++ tests/unit/Elasticsearch/Framework/DataAbstractionLayer/ScriptIdQueryTest.php
@@ -3,6 +3,7 @@
 namespace Shopware\Tests\Unit\Elasticsearch\Framework\DataAbstractionLayer;
 
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Test\Annotation\DisabledFeatures;
 use Shopware\Elasticsearch\Framework\DataAbstractionLayer\ScriptIdQuery;
 
 /**
@@ -12,9 +13,26 @@ use Shopware\Elasticsearch\Framework\DataAbstractionLayer\ScriptIdQuery;
  */
 class ScriptIdQueryTest extends TestCase
 {
-    public function testSerialize(): void
+    #[DisabledFeatures(['v6.6.0.0'])]
+    public function testSerializeUsingSource(): void
     {
-        $query = new ScriptIdQuery('foo', ['bar' => 'baz']);
+        $query = new ScriptIdQuery(null, ['bar' => 'baz'], 'foo');
+
+        static::assertSame([
+            'script' => [
+                'script' => [
+                    'source' => 'foo',
+                    'lang' => 'painless',
+                    'bar' => 'baz',
+                ],
+            ],
+        ], $query->toArray());
+    }
+
+    #[DisabledFeatures(['v6.6.0.0'])]
+    public function testSerializeUsingId(): void
+    {
+        $query = new ScriptIdQuery('foo', ['bar' => 'baz'], null);
 
         static::assertSame([
             'script' => [

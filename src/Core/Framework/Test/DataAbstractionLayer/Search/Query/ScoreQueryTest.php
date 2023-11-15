2a47a84369e4ee6941195e20cec834c7046ddbe7
Niklas Wolf <wolfniklas94@web.de>
2023-11-15 14:53:27 +0100

    fix: adjust JSON-serialization of ScoreQuery

---
+++ src/Core/Framework/Test/DataAbstractionLayer/Search/Query/ScoreQueryTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\DataAbstractionLayer\Search\Query;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Query\ScoreQuery;
+
+/**
+ * @internal
+ */
+class ScoreQueryTest extends TestCase
+{
+
+    public function testJsonSerialization(): void
+    {
+        $criteria = new Criteria();
+        $criteria->addQuery(new ScoreQuery(new ContainsFilter('productNumber', '123456'), 100));
+
+        /**
+         * @see \Shopware\Core\Framework\DataAbstractionLayer\Cache\EntityCacheKeyGenerator::getCriteriaHash
+         */
+        $json = json_encode($criteria->getQueries(), JSON_THROW_ON_ERROR);
+
+        $expected = '[{"extensions":[],"_class":"Shopware\\\\Core\\\\Framework\\\\DataAbstractionLayer\\\\Search\\\\Query\\\\ScoreQuery","query":{"extensions":[],"isPrimary":false,"resolved":null,"field":"productNumber","value":"123456","_class":"Shopware\\\\Core\\\\Framework\\\\DataAbstractionLayer\\\\Search\\\\Filter\\\\ContainsFilter"},"score":100,"scoreField":null}]';
+        static::assertEquals($expected, $json);
+
+    }
+}

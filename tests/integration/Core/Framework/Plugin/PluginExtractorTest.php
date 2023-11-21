8bbb9a25f8e4eb518934deb668d67bf3d342d720
Soner Sayakci <s.sayakci@shopware.com>
2023-11-21 11:57:49 +0100

    NEXT-28255 - Explicitly clear annotation cache

--- tests/integration/Core/Framework/Plugin/PluginExtractorTest.php
+++ tests/integration/Core/Framework/Plugin/PluginExtractorTest.php
@@ -2,6 +2,7 @@
 
 namespace Shopware\Tests\Integration\Core\Framework\Plugin;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\App\AppException;
 use Shopware\Core\Framework\Feature;
@@ -9,6 +10,7 @@ use Shopware\Core\Framework\Plugin\PluginExtractor;
 use Shopware\Core\Framework\Plugin\PluginManagementService;
 use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
 use Shopware\Core\System\SystemConfig\Exception\XmlParsingException;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -21,28 +23,33 @@ class PluginExtractorTest extends TestCase
 
     protected ContainerInterface $container;
 
-    /**
-     * @var Filesystem
-     */
-    private $filesystem;
+    private Filesystem $filesystem;
 
     private PluginExtractor $extractor;
+
+    private AdapterInterface&MockObject $annotationCache;
 
     protected function setUp(): void
     {
         $this->container = $this->getContainer();
         $this->filesystem = $this->container->get(Filesystem::class);
+        $this->annotationCache = $this->createMock(AdapterInterface::class);
         $this->extractor = new PluginExtractor(
             [
                 PluginManagementService::PLUGIN => __DIR__ . '/_fixtures/plugins',
                 PluginManagementService::APP => __DIR__ . '/_fixtures/apps',
             ],
-            $this->filesystem
+            $this->filesystem,
+            $this->annotationCache
         );
     }
 
     public function testExtractPlugin(): void
     {
+        $this->annotationCache
+            ->expects(static::once())
+            ->method('clear');
+
         $this->filesystem->copy(__DIR__ . '/_fixtures/archives/SwagFashionTheme.zip', __DIR__ . '/_fixtures/SwagFashionTheme.zip');
 
         $archive = __DIR__ . '/_fixtures/SwagFashionTheme.zip';

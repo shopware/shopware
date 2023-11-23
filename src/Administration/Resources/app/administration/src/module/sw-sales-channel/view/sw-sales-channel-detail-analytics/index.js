9e1088d57a253929cde326b64e6bb6997578ae17
Frederik Schmitt <f.schmitt@shopware.com>
2023-11-23 08:06:06 +0100

    NEXT-31909 - Remove deprecations

--- src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-analytics/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/view/sw-sales-channel-detail-analytics/index.js
@@ -9,10 +9,9 @@ import './sw-sales-channel-detail-analytics.scss';
 const { Context } = Shopware;
 
 /**
+ * @private
  * @package merchant-services
- * @deprecated tag:v6.6.0 - Will be private
  */
-// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
 export default {
     template,
 

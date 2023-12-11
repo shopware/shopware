73c8166cf207be637d9538e8c6f22038ea9a64f0
max <max@swk-web.com>
2023-12-11 16:15:30 +0100

    NEXT-21015 - Fixup: Implement StorefrontSearchResult deprecations

--- src/Storefront/Page/Account/Order/AccountOrderPageLoader.php
+++ src/Storefront/Page/Account/Order/AccountOrderPageLoader.php
@@ -13,6 +13,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Event\RouteRequest\OrderRouteRequestEvent;
@@ -50,7 +51,12 @@ class AccountOrderPageLoader
 
         $page->getMetaInformation()?->setRobots('noindex,follow');
 
-        $page->setOrders(StorefrontSearchResult::createFrom($this->getOrders($request, $salesChannelContext)));
+        $orders = $this->getOrders($request, $salesChannelContext);
+        if (!Feature::isActive('v6.7.0.0')) {
+            $orders = StorefrontSearchResult::createFrom($orders);
+        }
+
+        $page->setOrders($orders);
 
         $page->setDeepLinkCode($request->get('deepLinkCode'));
 
--- src/Storefront/Page/Product/Review/ReviewLoaderResult.php
+++ src/Storefront/Page/Product/Review/ReviewLoaderResult.php
@@ -5,101 +5,197 @@ namespace Shopware\Storefront\Page\Product\Review;
 use Shopware\Core\Content\Product\Aggregate\ProductReview\ProductReviewCollection;
 use Shopware\Core\Content\Product\Aggregate\ProductReview\ProductReviewEntity;
 use Shopware\Core\Content\Product\SalesChannel\Review\RatingMatrix;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Storefront\Framework\Page\StorefrontSearchResult;
 
-/**
- * @deprecated tag:v6.7.0 - Will inherit from EntitySearchResult<ProductReviewCollection>
- *
- * @template-extends StorefrontSearchResult<ProductReviewCollection>
- */
-#[Package('storefront')]
-class ReviewLoaderResult extends StorefrontSearchResult
-{
+if (Feature::isActive('v6.7.0.0')) {
     /**
-     * @var string|null
+     * @template-extends EntitySearchResult<ProductReviewCollection>
      */
-    protected $parentId;
-
-    /**
-     * @var string
-     */
-    protected $productId;
-
-    /**
-     * @var StorefrontSearchResult<ProductReviewCollection>
-     */
-    protected $reviews;
-
-    protected RatingMatrix $matrix;
-
-    /**
-     * @var ProductReviewEntity|null
-     */
-    protected $customerReview;
-
-    /**
-     * @var int
-     */
-    protected $totalReviews;
-
-    public function getProductId(): string
+    #[Package('storefront')]
+    class ReviewLoaderResult extends EntitySearchResult
     {
-        return $this->productId;
-    }
+        /**
+         * @var string|null
+         */
+        protected $parentId;
 
-    public function setProductId(string $productId): void
-    {
-        $this->productId = $productId;
-    }
+        /**
+         * @var string
+         */
+        protected $productId;
 
+        /**
+         * @var EntitySearchResult<ProductReviewCollection>
+         */
+        protected EntitySearchResult $reviews;
+
+        protected RatingMatrix $matrix;
+
+        /**
+         * @var ProductReviewEntity|null
+         */
+        protected $customerReview;
+
+        /**
+         * @var int
+         */
+        protected $totalReviews;
+
+        public function getProductId(): string
+        {
+            return $this->productId;
+        }
+
+        public function setProductId(string $productId): void
+        {
+            $this->productId = $productId;
+        }
+
+        /**
+         * @return EntitySearchResult<ProductReviewCollection>
+         */
+        public function getReviews(): EntitySearchResult
+        {
+            return $this->reviews;
+        }
+
+        public function getMatrix(): RatingMatrix
+        {
+            return $this->matrix;
+        }
+
+        public function setMatrix(RatingMatrix $matrix): void
+        {
+            $this->matrix = $matrix;
+        }
+
+        public function getCustomerReview(): ?ProductReviewEntity
+        {
+            return $this->customerReview;
+        }
+
+        public function setCustomerReview(?ProductReviewEntity $customerReview): void
+        {
+            $this->customerReview = $customerReview;
+        }
+
+        public function getTotalReviews(): int
+        {
+            return $this->totalReviews;
+        }
+
+        public function setTotalReviews(int $totalReviews): void
+        {
+            $this->totalReviews = $totalReviews;
+        }
+
+        public function getParentId(): ?string
+        {
+            return $this->parentId;
+        }
+
+        public function setParentId(?string $parentId): void
+        {
+            $this->parentId = $parentId;
+        }
+    }
+} else {
     /**
-     * @deprecated tag:v6.7.0 - Return type will change to EntitySearchResult<ProductReviewCollection>
+     * @deprecated tag:v6.7.0 - Will inherit from EntitySearchResult<ProductReviewCollection>
      *
-     * @return StorefrontSearchResult<ProductReviewCollection>
+     * @template-extends StorefrontSearchResult<ProductReviewCollection>
      */
-    public function getReviews(): StorefrontSearchResult
+    #[Package('storefront')]
+    class ReviewLoaderResult extends StorefrontSearchResult
     {
-        return $this->reviews;
-    }
+        /**
+         * @var string|null
+         */
+        protected $parentId;
 
-    public function getMatrix(): RatingMatrix
-    {
-        return $this->matrix;
-    }
+        /**
+         * @var string
+         */
+        protected $productId;
 
-    public function setMatrix(RatingMatrix $matrix): void
-    {
-        $this->matrix = $matrix;
-    }
+        /**
+         * @var StorefrontSearchResult<ProductReviewCollection>
+         */
+        protected $reviews;
 
-    public function getCustomerReview(): ?ProductReviewEntity
-    {
-        return $this->customerReview;
-    }
+        protected RatingMatrix $matrix;
 
-    public function setCustomerReview(?ProductReviewEntity $customerReview): void
-    {
-        $this->customerReview = $customerReview;
-    }
+        /**
+         * @var ProductReviewEntity|null
+         */
+        protected $customerReview;
 
-    public function getTotalReviews(): int
-    {
-        return $this->totalReviews;
-    }
+        /**
+         * @var int
+         */
+        protected $totalReviews;
 
-    public function setTotalReviews(int $totalReviews): void
-    {
-        $this->totalReviews = $totalReviews;
-    }
+        public function getProductId(): string
+        {
+            return $this->productId;
+        }
 
-    public function getParentId(): ?string
-    {
-        return $this->parentId;
-    }
+        public function setProductId(string $productId): void
+        {
+            $this->productId = $productId;
+        }
 
-    public function setParentId(?string $parentId): void
-    {
-        $this->parentId = $parentId;
+        /**
+         * @deprecated tag:v6.7.0 - Return type will change to EntitySearchResult<ProductReviewCollection>
+         *
+         * @return StorefrontSearchResult<ProductReviewCollection>
+         */
+        public function getReviews(): StorefrontSearchResult
+        {
+            return $this->reviews;
+        }
+
+        public function getMatrix(): RatingMatrix
+        {
+            return $this->matrix;
+        }
+
+        public function setMatrix(RatingMatrix $matrix): void
+        {
+            $this->matrix = $matrix;
+        }
+
+        public function getCustomerReview(): ?ProductReviewEntity
+        {
+            return $this->customerReview;
+        }
+
+        public function setCustomerReview(?ProductReviewEntity $customerReview): void
+        {
+            $this->customerReview = $customerReview;
+        }
+
+        public function getTotalReviews(): int
+        {
+            return $this->totalReviews;
+        }
+
+        public function setTotalReviews(int $totalReviews): void
+        {
+            $this->totalReviews = $totalReviews;
+        }
+
+        public function getParentId(): ?string
+        {
+            return $this->parentId;
+        }
+
+        public function setParentId(?string $parentId): void
+        {
+            $this->parentId = $parentId;
+        }
     }
 }

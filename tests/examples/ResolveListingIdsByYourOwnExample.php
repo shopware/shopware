721bafbb8142deb0e5d740c474d263b884ea7cb3
oskroblin Skroblin <o.skroblin@shopware.com>
2023-12-15 09:51:36 +0100

    NEXT-32330 - Added documentation examples for event based decorations

---
+++ tests/examples/ResolveListingIdsByYourOwnExample.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Shopware\Tests\Examples;
+
+use GuzzleHttp\Client;
+use Shopware\Core\Content\Product\Decoration\ResolveListingLoaderIds;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener(
+    event: 'listing-loader.resolve-listing-ids.pre',
+    method: '__invoke'
+)]
+#[Title('Example how you control the listing product ids')]
+#[Description('This example shows how you can control the listing product ids. It allows you to resolve the listing ids by your own over an API call or an own storage')]
+readonly class ResolveListingIdsByYourOwnExample
+{
+    public function __construct(
+        // you can inject your own services
+        private Client $client
+    ) {}
+
+    public function __invoke(ResolveListingLoaderIds $event): void
+    {
+        $criteria = $event->criteria;
+
+        $context = [
+            'salesChannelId' => $event->context->getSalesChannelId(),
+            'currencyId' => $event->context->getCurrency(),
+            'languageId' => $event->context->getLanguageId(),
+        ];
+
+        // use this client to do get request against: https://your-api.com/listing-ids?criteria=...&context=...
+
+        $ids = $this->client->get('https://your-api.com/listing-ids', [
+            'query' => [
+                'criteria' => json_encode($criteria),
+                'context' => json_encode($context),
+            ],
+        ]);
+
+        $data = json_decode($ids->getBody()->getContents());
+
+        $result = IdSearchResult::fromIds(
+            $data['ids'],
+            $event->criteria,
+            $event->context->getContext(),
+            $data['total']
+        );
+
+        $event->result = $result;
+    }
+}
---
+++ tests/unit/Content/Product/Decoration/ResolveListingLoaderIdsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Shopware\Tests\Unit\Content\Product\Decoration;
+
+use Shopware\Core\Content\Product\Decoration\ResolveListingLoaderIds;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Shopware\Core\Content\Product\Decoration\ResolveListingLoaderIds
+ */
+class ResolveListingLoaderIdsTest extends TestCase
+{
+    /**
+     * @covers \Shopware\Tests\Examples\ResolveListingIdsByYourOwnExample
+     */
+    public function testResolveListingIdsByYourOwnExample()
+    {
+        // add test for ResolveListingIdsByYourOwnExample.php to ensure documentation works
+    }
+}

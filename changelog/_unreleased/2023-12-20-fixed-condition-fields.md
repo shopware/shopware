c151efd1bfc67cd6036e13db24c1b18149a71316
Simon Fiebranz <s.fiebranz@shopware.com>
2023-12-20 10:53:19 +0100

    NEXT-32736 - changed awaited events for condition input fields in rule builder

---
+++ changelog/_unreleased/2023-12-20-fixed-condition-fields.md
@@ -0,0 +1,9 @@
+---
+title: Fixed condition fields in rule builder
+issue: NEXT-32736
+author: Simon Fiebranz
+author_email: s.fiebranz@shopware.com
+author_github: CR0YD
+---
+# Administration
+* Changed the awaited input events for the `sw-form-field-renderer` component in `sw-condition-order-custom-field` and `sw-condition-order-custom-field`
---
+++ src/Administration/Resources/app/administration/src/app/component/rule/condition-type/sw-condition-line-item-custom-field/sw-condition-line-item-custom-field.spec.js
@@ -0,0 +1,226 @@
+import { mount } from '@vue/test-utils';
+import ConditionDataProviderService from 'src/app/service/rule-condition.service';
+import EntityCollection from 'src/core/data/entity-collection.data';
+
+const mockCustomFields = new EntityCollection(
+    '/custom-field',
+    'custom_field',
+    null,
+    {},
+    [
+        {
+            id: '1',
+            name: '',
+            config: {
+                type: 'text',
+                label: 'foo',
+            },
+            customFieldSetId: '1',
+            customFieldSet: {
+                name: '',
+                config: {
+                    label: 'bar',
+                },
+            },
+            allowCartExpose: false,
+        },
+        {
+            id: '2',
+            name: '',
+            config: {
+                componentName: 'sw-field',
+                type: 'text',
+                label: 'foo2',
+            },
+            customFieldSetId: '2',
+            customFieldSet: {
+                name: '',
+                config: {
+                    label: 'bar',
+                },
+            },
+            allowCartExpose: true,
+        },
+    ],
+    2,
+    null,
+);
+
+async function createWrapper() {
+    return mount(await wrapTestComponent('sw-condition-line-item-custom-field', { sync: true }), {
+        global: {
+            stubs: {
+                'sw-base-field': await wrapTestComponent('sw-base-field'),
+                'sw-block-field': await wrapTestComponent('sw-block-field'),
+                'sw-condition-operator-select': await wrapTestComponent('sw-condition-operator-select'),
+                'sw-contextual-field': await wrapTestComponent('sw-contextual-field'),
+                'sw-entity-single-select': await wrapTestComponent('sw-entity-single-select'),
+                'sw-form-field-renderer': await wrapTestComponent('sw-form-field-renderer'),
+                'sw-popover': await wrapTestComponent('sw-popover'),
+                'sw-select-base': await wrapTestComponent('sw-select-base'),
+                'sw-select-result': await wrapTestComponent('sw-select-result'),
+                'sw-select-result-list': await wrapTestComponent('sw-select-result-list'),
+                'sw-single-select': await wrapTestComponent('sw-single-select'),
+                'sw-text-field': await wrapTestComponent('sw-text-field'),
+            },
+            provide: {
+                conditionDataProviderService: new ConditionDataProviderService(),
+                availableTypes: {},
+                availableGroups: [],
+                restrictedConditions: [],
+                childAssociationField: {},
+                validationService: {},
+                insertNodeIntoTree: () => ({}),
+                removeNodeFromTree: () => ({}),
+                createCondition: () => ({}),
+                conditionScopes: [],
+                unwrapAllLineItemsCondition: () => ({}),
+                repositoryFactory: {
+                    create: (param) => {
+                        if (param === 'custom_field') {
+                            return {
+                                search: () => Promise.resolve(mockCustomFields),
+                            };
+                        }
+                        return {
+                            search: () => Promise.resolve(),
+                            get: () => Promise.resolve(),
+                        };
+                    },
+                },
+            },
+        },
+        props: {
+            condition: {
+                value: {
+                    renderedField: '',
+                },
+            },
+        },
+    });
+}
+
+describe('components/rule/condition-type/sw-condition-line-item-custom-field', () => {
+    let wrapper;
+
+    beforeEach(async () => {
+        wrapper = await createWrapper();
+        await flushPromises();
+    });
+
+    it('should render custom field options', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        const listElements =
+            document.body.querySelector('.sw-select-result-list__item-list').querySelectorAll('li');
+
+        expect(listElements.item(0).querySelector('.sw-select-result__result-item-text').textContent)
+            .toBe('foo ');
+        expect(listElements.item(0).querySelector('.sw-select-result__result-item-description').textContent)
+            .toBe('bar');
+        expect(listElements.item(0).className).toContain('is--disabled');
+
+        expect(listElements.item(1).querySelector('.sw-select-result__result-item-text').textContent)
+            .toBe('foo2 ');
+        expect(listElements.item(1).querySelector('.sw-select-result__result-item-description').textContent)
+            .toBe('bar');
+        expect(listElements.item(1).className).not.toContain('is--disabled');
+    });
+
+    it('should get custom field option tooltip', async () => {
+        let tooltipConfig = wrapper.vm.getTooltipConfig(mockCustomFields.at(0));
+
+        expect(tooltipConfig).toEqual({
+            disabled: false,
+            width: 260,
+            message: 'global.sw-condition.condition.lineItemCustomField.field.customFieldSelect.tooltip',
+        });
+
+        tooltipConfig = wrapper.vm.getTooltipConfig(mockCustomFields.at(1));
+
+        expect(tooltipConfig).toEqual({ message: '', disabled: true });
+    });
+
+    it('should set data on field change with known id', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        wrapper.vm.onFieldChange('1');
+
+        expect(wrapper.vm.renderedField).toStrictEqual(
+            {
+                allowCartExpose: false,
+                config: {
+                    label: 'foo',
+                    type: 'text',
+                },
+                customFieldSet: {
+                    config: {
+                        label: 'bar',
+                    },
+                    name: '',
+                },
+                customFieldSetId: '1',
+                id: '1',
+                name: '',
+            },
+        );
+        expect(wrapper.vm.selectedFieldSet).toBe('1');
+    });
+
+    it('should not set data on field change with unknown id', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        wrapper.vm.onFieldChange('3');
+
+        expect(wrapper.vm.renderedField).toBeNull();
+        expect(wrapper.vm.selectedFieldSet).toBeUndefined();
+    });
+
+    it('should set custom field value on input', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        document.body.querySelector('li:nth-of-type(2)').click();
+        await flushPromises();
+
+        expect(wrapper.find('.sw-entity-single-select__selection-text').text()).toBe('foo2');
+    });
+
+    it('should set operator field value on input', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        document.body.querySelector('li:nth-of-type(2)').click();
+        await flushPromises();
+
+        await wrapper.find('.sw-single-select__selection-input').trigger('click');
+        await flushPromises();
+
+        document.body.querySelector('li').click();
+        await flushPromises();
+
+        expect(wrapper.find('.sw-single-select__selection-text').text()).toBe('global.sw-condition.operator.equals');
+    });
+
+    it('should set form field value on input', async () => {
+        await wrapper.find('.sw-entity-single-select .sw-select__selection').trigger('click');
+        await flushPromises();
+
+        document.body.querySelector('li:nth-of-type(2)').click();
+        await flushPromises();
+
+        await wrapper.find('.sw-single-select__selection-input').trigger('click');
+        await flushPromises();
+
+        document.body.querySelector('li').click();
+        await flushPromises();
+
+        await wrapper.find('.sw-form-field-renderer input').setValue('test123');
+        await flushPromises();
+
+        expect(wrapper.vm.renderedFieldValue).toBe('test123');
+    });
+});

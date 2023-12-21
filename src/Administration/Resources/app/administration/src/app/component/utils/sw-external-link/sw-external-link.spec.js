91aab2716d11a53f3216b26442afbf93f4ac43b4
Nils Haberkamp <n.haberkamp@shopware.com>
2023-12-21 13:52:09 +0100

    NEXT-32591 - migrate sw-external-link component

--- src/Administration/Resources/app/administration/src/app/component/utils/sw-external-link/sw-external-link.spec.js
+++ src/Administration/Resources/app/administration/src/app/component/utils/sw-external-link/sw-external-link.spec.js
@@ -2,62 +2,56 @@
  * @package admin
  */
 
-import { shallowMount } from '@vue/test-utils_v2';
+import { mount } from '@vue/test-utils';
 import 'src/app/component/utils/sw-external-link';
 
-// initial component setup
-const setup = async (propOverride) => {
-    const propsData = {
-        ...propOverride,
-    };
-
-    return shallowMount(await Shopware.Component.build('sw-external-link'), {
-        stubs: ['sw-icon'],
-        slots: {
-            default: 'test external link',
+const createWrapper = async (props = {}) => {
+    return mount(await wrapTestComponent('sw-external-link', { sync: true }), {
+        props,
+        global: {
+            stubs: ['sw-icon'],
+            slots: {
+                default: 'test external link',
+            },
         },
-        propsData,
     });
 };
 
 describe('components/utils/sw-external-link', () => {
-    it('should be a Vue.js component', async () => {
-        const wrapper = await setup({ href: 'https://google.com' });
-        expect(wrapper.vm).toBeTruthy();
-    });
+    it('should display the correct link', async () => {
+        const wrapper = await createWrapper({ href: 'https://google.com' });
+        const anchor = wrapper.find('a');
 
-    it('should render correctly', async () => {
-        const wrapper = await setup({ href: 'https://google.com' });
-        expect(wrapper.element).toMatchSnapshot();
+        expect(anchor.attributes('href')).toBe('https://google.com');
     });
 
     it('should display a custom icon', async () => {
-        const wrapper = await setup({
+        const wrapper = await createWrapper({
             href: 'https://google.com',
             icon: 'default-test-icon',
         });
 
+        expect(wrapper.find('sw-icon-stub').exists()).toBe(true);
         expect(wrapper.find('sw-icon-stub').attributes().name).toBe('default-test-icon');
         expect(wrapper.find('sw-icon-stub').attributes().size).toBe('10px');
     });
 
     it('should emit click event if no href is provided', async () => {
-        const wrapper = await setup();
+        const wrapper = await createWrapper();
 
         await wrapper.trigger('click');
-        await wrapper.vm.$nextTick();
+        await flushPromises();
 
         expect(wrapper.emitted().click).toBeTruthy();
     });
 
     it('should render small', async () => {
-        const wrapper = await setup({
+        const wrapper = await createWrapper({
             href: 'https://google.com',
             small: true,
         });
 
         expect(wrapper.find('sw-icon-stub').attributes().size).toBe('8px');
-
         expect(wrapper.classes()).toContain('sw-external-link--small');
     });
 });

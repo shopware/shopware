30c353bc45ad960ed957e66e6d9830a1a6b22fb9
Dinh Duy <d.dinh@shopware.com>
2023-12-26 10:59:15 +0700

    NEXT-31978 - Validate file name contains funky characters

--- tests/unit/Core/Content/Media/File/FileNameValidatorTest.php
+++ tests/unit/Core/Content/Media/File/FileNameValidatorTest.php
@@ -173,6 +173,19 @@ class FileNameValidatorTest extends TestCase
         $validator->validateFileName($name);
     }
 
+    public function testFilenameContainFunkyWhiteSpace(): void
+    {
+        $filename = 'pexels-jan-kopÅiva-3354648.jpg';
+
+        $this->expectException(MediaException::class);
+        $this->expectExceptionMessage(
+            "Provided filename \"$filename\" is not permitted: Filename must not contain funky whitespace"
+        );
+
+        $validator = new FileNameValidator();
+        $validator->validateFileName($filename);
+    }
+
     #[DoesNotPerformAssertions]
     public function testValidateFileNameDoesNothingIfFileNameHasValidLength(): void
     {

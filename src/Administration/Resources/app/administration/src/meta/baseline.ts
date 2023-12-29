09309bdaefdb3ef2cb4f378546194107e7968c5f
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-12-29 14:19:25 +0100

    NEXT-32624 - Sanitizer helper test

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -748,7 +748,6 @@ const missingTests = [
     'src/app/plugin/deprecation.plugin.js',
     'src/app/service/app-cms.service.js',
     'src/app/service/filter.service.js',
-    'src/core/helper/sanitizer.helper.js',
     'src/module/sw-bulk-edit/component/product/sw-bulk-edit-product-media/index.js',
     'src/module/sw-bulk-edit/component/product/sw-bulk-edit-product-media-form/index.js',
     'src/module/sw-bulk-edit/component/product/sw-bulk-edit-product-visibility/index.js',

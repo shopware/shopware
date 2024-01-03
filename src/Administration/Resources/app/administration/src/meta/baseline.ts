d5ca75602a5a035ee7c478fb3366c2a65f6c6f3f
Dennis Garding <d.garding@shopware.com>
2024-01-03 09:15:28 +0000

    NEXT-32410 - Move test vue2 to vue3

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -829,7 +829,6 @@ const missingTests = [
     'src/module/sw-flow/component/modals/sw-flow-set-order-state-modal/index.js',
     'src/module/sw-flow/component/modals/sw-flow-tag-modal/index.js',
     'src/module/sw-flow/component/sw-flow-sequence/index.js',
-    'src/module/sw-flow/component/sw-flow-sequence-action/index.js',
     'src/module/sw-flow/component/sw-flow-sequence-action-error/index.js',
     'src/module/sw-flow/component/sw-flow-sequence-condition/index.js',
     'src/module/sw-flow/component/sw-flow-sequence-selector/index.js',

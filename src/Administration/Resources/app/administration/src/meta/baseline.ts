c82df4bebfb1d96443a2243f12758a7815ab8ee4
Simon Fiebranz <s.fiebranz@shopware.com>
2024-01-04 16:29:28 +0100

    NEXT-32418 - added vue3 test for sw-flow-app-action-modal

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -720,7 +720,6 @@ const missingTests = [
     'src/module/sw-first-run-wizard/component/sw-first-run-wizard-modal/index.js',
     'src/module/sw-first-run-wizard/view/sw-first-run-wizard-paypal-info/index.js',
     'src/module/sw-first-run-wizard/view/sw-first-run-wizard-plugins/index.js',
-    'src/module/sw-flow/component/modals/sw-flow-app-action-modal/index.js',
     'src/module/sw-flow/component/modals/sw-flow-change-customer-group-modal/index.js',
     'src/module/sw-flow/component/modals/sw-flow-change-customer-status-modal/index.js',
     'src/module/sw-flow/component/modals/sw-flow-create-mail-template-modal/index.js',

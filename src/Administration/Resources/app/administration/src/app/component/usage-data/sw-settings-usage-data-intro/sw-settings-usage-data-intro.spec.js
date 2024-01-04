157c30c71a2276ada943952cd82ded47e64158b6
Dennis Garding <d.garding@shopware.com>
2024-01-04 10:27:46 +0100

    NEXT-32407 - Move test from vue2 to vue3 for compatibility

--- src/Administration/Resources/app/administration/src/app/component/usage-data/sw-settings-usage-data-intro/sw-settings-usage-data-intro.spec.js
+++ src/Administration/Resources/app/administration/src/app/component/usage-data/sw-settings-usage-data-intro/sw-settings-usage-data-intro.spec.js
@@ -1,13 +1,14 @@
-import { shallowMount } from '@vue/test-utils_v2';
-import 'src/app/component/usage-data/sw-settings-usage-data-intro';
-import 'src/app/component/base/sw-icon';
-import 'src/app/component/utils/sw-external-link';
+import { mount } from '@vue/test-utils';
 
 async function createWrapper() {
-    return shallowMount(await Shopware.Component.build('sw-settings-usage-data-intro'), {
-        stubs: {
-            'sw-icon': await Shopware.Component.build('sw-icon'),
-            'sw-external-link': await Shopware.Component.build('sw-external-link'),
+    return mount(await wrapTestComponent('sw-settings-usage-data-intro', {
+        sync: true,
+    }), {
+        global: {
+            stubs: {
+                'sw-icon': await wrapTestComponent('sw-icon'),
+                'sw-external-link': await wrapTestComponent('sw-external-link'),
+            },
         },
     });
 }
@@ -15,14 +16,9 @@ async function createWrapper() {
 describe('src/app/component/usage-data/sw-settings-usage-data-intro', () => {
     let wrapper = null;
 
-    afterEach(() => {
-        if (wrapper) {
-            wrapper.destroy();
-        }
-    });
-
     it('should show the solid-analytics icon', async () => {
         wrapper = await createWrapper();
+        await flushPromises();
 
         const icon = await wrapper.find('[data-testid="sw-icon__solid-analytics"]');
         expect(icon.exists()).toBe(true);
@@ -31,6 +27,7 @@ describe('src/app/component/usage-data/sw-settings-usage-data-intro', () => {
 
     it('should provide some contextual information', async () => {
         wrapper = await createWrapper();
+        await flushPromises();
 
         const contextualInformation = await wrapper.text();
         expect(contextualInformation).toContain('sw-settings-usage-data-intro.headline');

721654a73907b13d47329c8326832b0e195d1824
Dennis Garding <d.garding@shopware.com>
2024-01-05 10:40:14 +0100

    NEXT-32576 - Move test from vue2 to vue3 for compatibility

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -698,7 +698,6 @@ const missingTests = [
     'src/app/component/filter/sw-number-filter/index.js',
     'src/app/component/filter/sw-range-filter/index.js',
     'src/app/component/form/select/entity/advanced-selection-entities/sw-advanced-selection-rule/index.js',
-    'src/app/component/form/select/entity/sw-entity-advanced-selection-modal/index.js',
     'src/app/component/form/select/entity/sw-entity-many-to-many-select/index.js',
     'src/app/service/app-cms.service.js',
     'src/module/sw-bulk-edit/component/sw-bulk-edit-change-type/index.js',

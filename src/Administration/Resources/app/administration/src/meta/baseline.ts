c8614a12b27d0055a524f4955b2b9f554da037d7
Susanne Hartung <s.hartung@shopware.com>
2024-01-08 10:13:43 +0100

    NEXT-32577 - vue3 transition of plugin-card

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -757,7 +757,6 @@ const missingTests = [
     'src/module/sw-dashboard/component/sw-dashboard-statistics/index.ts',
     'src/module/sw-dashboard/page/sw-dashboard-index/index.js',
     'src/module/sw-first-run-wizard/component/sw-first-run-wizard-modal/index.js',
-    'src/module/sw-first-run-wizard/component/sw-plugin-card/index.ts',
     'src/module/sw-first-run-wizard/view/sw-first-run-wizard-mailer-selection/index.js',
     'src/module/sw-first-run-wizard/view/sw-first-run-wizard-paypal-info/index.js',
     'src/module/sw-first-run-wizard/view/sw-first-run-wizard-plugins/index.js',

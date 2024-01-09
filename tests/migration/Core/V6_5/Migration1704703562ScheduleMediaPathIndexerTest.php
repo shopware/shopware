f95622c60640b9cae0e0f26d87191376db25b099
Jonas Elfering <j.elfering@shopware.com>
2024-01-09 16:50:29 +0100

    NEXT-32919 - Adjust covers annotation

--- tests/migration/Core/V6_5/Migration1704703562ScheduleMediaPathIndexerTest.php
+++ tests/migration/Core/V6_5/Migration1704703562ScheduleMediaPathIndexerTest.php
@@ -3,7 +3,6 @@
 namespace Shopware\Tests\Migration\Core\V6_5;
 
 use Doctrine\DBAL\Connection;
-use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Migration\IndexerQueuer;
@@ -13,9 +12,10 @@ use Shopware\Core\Migration\V6_5\Migration1704703562ScheduleMediaPathIndexer;
 
 /**
  * @internal
+ *
+ * @covers \Shopware\Core\Migration\V6_5\Migration1704703562ScheduleMediaPathIndexer
  */
 #[Package('core')]
-#[CoversClass(Migration1704703562ScheduleMediaPathIndexer::class)]
 class Migration1704703562ScheduleMediaPathIndexerTest extends TestCase
 {
     use KernelTestBehaviour;

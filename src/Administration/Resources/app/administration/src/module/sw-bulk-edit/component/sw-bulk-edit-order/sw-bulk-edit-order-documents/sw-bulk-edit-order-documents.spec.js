59d9a70ebeb8dccb428d7a88b7aa7ba576fedf01
Max Stegmeyer <m.stegmeyer@shopware.com>
2024-01-09 21:51:52 +0100

    NEXT-32487 - Transition sw-bulk-edit-order-documents.spec.js

--- src/Administration/Resources/app/administration/src/module/sw-bulk-edit/component/sw-bulk-edit-order/sw-bulk-edit-order-documents/sw-bulk-edit-order-documents.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-bulk-edit/component/sw-bulk-edit-order/sw-bulk-edit-order-documents/sw-bulk-edit-order-documents.spec.js
@@ -1,28 +1,27 @@
 /**
  * @package system-settings
  */
-import { shallowMount } from '@vue/test-utils_v2';
-import swBulkEditOrderDocuments from 'src/module/sw-bulk-edit/component/sw-bulk-edit-order/sw-bulk-edit-order-documents';
-
-Shopware.Component.register('sw-bulk-edit-order-documents', swBulkEditOrderDocuments);
+import { mount } from '@vue/test-utils';
 
 async function createWrapper() {
-    return shallowMount(await Shopware.Component.build('sw-bulk-edit-order-documents'), {
-        stubs: {
-            'sw-container': true,
-            'sw-checkbox-field': true,
-            'sw-switch-field': true,
-        },
-        provide: {
-            repositoryFactory: {
-                create: () => {
-                    return {
-                        search: () => Promise.resolve([]),
-                    };
+    return mount(await wrapTestComponent('sw-bulk-edit-order-documents', { sync: true }), {
+        global: {
+            stubs: {
+                'sw-container': await wrapTestComponent('sw-container'),
+                'sw-checkbox-field': true,
+                'sw-switch-field': true,
+            },
+            provide: {
+                repositoryFactory: {
+                    create: () => {
+                        return {
+                            search: () => Promise.resolve([]),
+                        };
+                    },
                 },
             },
         },
-        propsData: {
+        props: {
             documents: {
                 disabled: false,
             },
@@ -39,10 +38,6 @@ describe('sw-bulk-edit-order-documents', () => {
 
     beforeEach(async () => {
         wrapper = await createWrapper();
-    });
-
-    afterEach(() => {
-        wrapper.destroy();
     });
 
     it('should be a Vue.js component', async () => {

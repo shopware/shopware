98bc2e6ca83e63e3ffa5a6789902710f0c06c3fd
Tobias Berge <t.berge@shopware.com>
2024-01-16 15:01:42 +0100

    NEXT-26302 - Improve alert contrast and accessibility

--- tests/integration/Storefront/Theme/ThemeTest.php
+++ tests/integration/Storefront/Theme/ThemeTest.php
@@ -319,7 +319,7 @@ class ThemeTest extends TestCase
         $themeInheritedConfig['baseThemeFields']['some-custom'] = ['value' => null, 'isInherited' => true];
 
         $themeInheritedConfig['currentFields']['sw-color-brand-primary']['value'] = '#ff00ff';
-        $themeInheritedConfig['currentFields']['sw-color-brand-secondary']['value'] = '#3d444d';
+        $themeInheritedConfig['currentFields']['sw-color-brand-secondary']['value'] = '#474a57';
 
         foreach ($themeInheritedConfig['fields'] as $key => $field) {
             if ($field['type'] === 'media') {
@@ -366,7 +366,7 @@ class ThemeTest extends TestCase
         $themeInheritedConfig['currentFields']['sw-color-brand-primary']['value'] = '#ff00ff';
         $themeInheritedConfig['currentFields']['sw-color-brand-primary']['isInherited'] = false;
 
-        $themeInheritedConfig['baseThemeFields']['sw-color-brand-primary']['value'] = '#0b539b';
+        $themeInheritedConfig['baseThemeFields']['sw-color-brand-primary']['value'] = '#0042a0';
 
         foreach ($themeInheritedConfig['fields'] as $key => $field) {
             if ($field['type'] === 'media') {
@@ -434,7 +434,7 @@ class ThemeTest extends TestCase
                 $themeInheritedConfig['fields'][$key]['value'] = $theme['fields'][$key]['value'];
             }
         }
-        $themeInheritedConfig['currentFields']['sw-color-brand-secondary']['value'] = '#3d444d';
+        $themeInheritedConfig['currentFields']['sw-color-brand-secondary']['value'] = '#474a57';
 
         static::assertEquals($themeInheritedConfig, $theme);
     }
@@ -689,7 +689,7 @@ class ThemeTest extends TestCase
         $_expectedColor = '#b1900f';
         $_expectedTheme = $childTheme->getId();
         $themeService->compileTheme(TestDefaults::SALES_CHANNEL, $childTheme->getId(), $this->context);
-        $_expectedColor = '#0b539b';
+        $_expectedColor = '#0042a0';
         $_expectedTheme = $baseTheme->getId();
         $themeService->compileTheme(TestDefaults::SALES_CHANNEL, $baseTheme->getId(), $this->context);
     }

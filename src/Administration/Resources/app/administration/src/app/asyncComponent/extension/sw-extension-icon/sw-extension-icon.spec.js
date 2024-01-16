db10ca6f19647d060e3cebb4602cc4b6e5620cde
Simon Fiebranz <s.fiebranz@shopware.com>
2024-01-16 11:54:39 +0100

    NEXT-32656 - added vue3 test for sw-extension-icon

---
+++ src/Administration/Resources/app/administration/src/app/asyncComponent/extension/sw-extension-icon/sw-extension-icon.spec.js
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils';
+
+/**
+ * @package services-settings
+ */
+async function createWrapper(props = {}) {
+    return mount(await wrapTestComponent('sw-extension-icon', { sync: true }), {
+        props: {
+            ...props,
+        },
+    });
+}
+
+describe('src/module/sw-extension/component/sw-extension-icon', () => {
+    it('passes correct props to image', async () => {
+        const wrapper = await createWrapper({ src: 'path-to-icon' });
+
+        const image = wrapper.get('img');
+
+        expect(image.classes('sw-extension-icon__icon')).toBe(true);
+        expect(image.attributes('src')).toBe('path-to-icon');
+        expect(image.attributes('alt')).toBe('');
+    });
+
+    it('can take an alt text', async () => {
+        const wrapper = await createWrapper({ src: 'path-to-icon', alt: 'description of an image' });
+
+        const image = wrapper.get('img');
+
+        expect(image.classes('sw-extension-icon__icon')).toBe(true);
+        expect(image.attributes('src')).toBe('path-to-icon');
+        expect(image.attributes('alt')).toBe('description of an image');
+    });
+});

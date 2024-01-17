cdf4caf091bea4619d6361a6276d23c9580f05dd
Sebastian Franze <s.franze@shopware.com>
2024-01-17 16:40:18 +0100

    NEXT-31870 - Deprecate ShopIdChangedException

--- src/Core/System/UsageData/Exception/ShopIdChangedException.php
+++ src/Core/System/UsageData/Exception/ShopIdChangedException.php
@@ -5,6 +5,9 @@ namespace Shopware\Core\System\UsageData\Exception;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\UsageData\UsageDataException;
 
+/**
+ * @deprecated tag:v6.7.0 will be removed with no replacement
+ */
 #[Package('data-services')]
 class ShopIdChangedException extends UsageDataException
 {

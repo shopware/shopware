a6a92a82fdfb852d36cbc1710c95823d807e71f1
Simon Fiebranz <s.fiebranz@shopware.com>
2024-01-18 14:29:22 +0100

    NEXT-32404 - added vue3 test for sw-first-run-wizard-modal

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -678,7 +678,6 @@ const missingTests = [
     'src/module/sw-users-permissions/components/sw-users-permissions-detailed-additional-permissions/index.js',
     'src/module/sw-users-permissions/index.js',
     'src/app/component/form/select/entity/sw-entity-many-to-many-select/index.js',
-    'src/module/sw-first-run-wizard/component/sw-first-run-wizard-modal/index.js',
     'src/module/sw-flow/page/sw-flow-detail/index.js',
     'src/module/sw-sales-channel/component/sw-sales-channel-modal/index.js',
 ];

af31eb757f837e72541f415f4c8f2c38f78d4739
Tuan Ngo <t.ngo@shopware.com>
2024-01-18 12:31:53 +0700

    NEXT-32440 - Vue2 Jest transition sw-review-list.spec.vue2.js

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -693,7 +693,6 @@ const missingTests = [
     'src/module/sw-product/view/sw-product-detail-context-prices/index.js',
     'src/module/sw-product/view/sw-product-detail-seo/index.js',
     'src/module/sw-product/view/sw-product-detail-specifications/index.js',
-    'src/module/sw-review/page/sw-review-list/index.js',
     'src/module/sw-sales-channel/component/sw-sales-channel-modal/index.js',
 ];
 

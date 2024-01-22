62c897d4c2a0c48e53231c1dc8accb27c4b742a5
Danil Schumin <danil80sch@gmail.com>
2024-01-22 17:54:21 +0100

    Update build-storefront.sh

--- src/WebInstaller/Resources/flex-config/bin/build-storefront.sh
+++ src/WebInstaller/Resources/flex-config/bin/build-storefront.sh
@@ -49,3 +49,4 @@ node "${STOREFRONT_ROOT}"/Resources/app/storefront/copy-to-vendor.js
 npm --prefix "${STOREFRONT_ROOT}"/Resources/app/storefront run production
 [[ ${SHOPWARE_SKIP_ASSET_COPY-""} ]] ||"${BIN_TOOL}" assets:install
 [[ ${SHOPWARE_SKIP_THEME_COMPILE-""} ]] || "${BIN_TOOL}" theme:compile
+"${BIN_TOOL}" cache:clear # Add config to skip

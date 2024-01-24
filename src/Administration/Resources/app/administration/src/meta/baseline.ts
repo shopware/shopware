1f7fcaf2f3476ec798862fd383e42e9a3a6cf96c
Tuan Ngo <t.ngo@shopware.com>
2024-01-24 08:48:24 +0000

    NEXT-32478 - Jest transition sw-product-variants-media-upload.spec

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -691,7 +691,6 @@ const missingTests = [
     'src/module/sw-product/component/sw-product-variants/sw-product-variants-configurator/sw-product-variants-configurator-selection/index.js',
     'src/module/sw-product/component/sw-product-variants/sw-product-variants-delivery/sw-product-variants-delivery-media/index.js',
     'src/module/sw-product/component/sw-product-variants/sw-product-variants-delivery/sw-product-variants-delivery-order/index.js',
-    'src/module/sw-product/component/sw-product-variants/sw-product-variants-media-upload/index.js',
     'src/module/sw-product/page/sw-product-list/index.js',
     'src/module/sw-product/view/sw-product-detail-context-prices/index.js',
     'src/module/sw-product/view/sw-product-detail-cross-selling/index.js',

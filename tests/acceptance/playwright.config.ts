0a0096916b2dcad1f927805492181ca40ac5d26f
Patrick Weyck <p.weyck@shopware.com>
2024-02-08 11:49:40 +0100

    NEXT-29322 - Only run acceptance job once

--- tests/acceptance/playwright.config.ts
+++ tests/acceptance/playwright.config.ts
@@ -109,8 +109,8 @@ export default defineConfig({
         baseURL: process.env['APP_URL'],
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace:  process.env.CI ? 'on-first-retry' : 'retain-on-failure',
-        video: 'on-first-retry',
+        trace:  process.env.CI ? 'on-all-retries' : 'retain-on-failure',
+        video: 'off',
         screenshot: 'only-on-failure',
     },
 

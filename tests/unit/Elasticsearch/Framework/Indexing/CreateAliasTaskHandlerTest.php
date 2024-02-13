48de146d85a35309ffa06b9f10db7882c8bd40ea
oskroblin Skroblin <o.skroblin@shopware.com>
2024-02-13 11:23:50 +0100

    NEXT-33708 - Provide test for available column default change

--- tests/unit/Elasticsearch/Framework/Indexing/CreateAliasTaskHandlerTest.php
+++ tests/unit/Elasticsearch/Framework/Indexing/CreateAliasTaskHandlerTest.php
@@ -206,14 +206,14 @@ class CreateAliasTaskHandlerTest extends TestCase
                 'body' => [
                     'actions' => [
                         [
-                            'add' => [
-                                'index' => 'index',
+                            'remove' => [
+                                'index' => 'old_index',
                                 'alias' => 'alias',
                             ],
                         ],
                         [
-                            'remove' => [
-                                'index' => 'old_index',
+                            'add' => [
+                                'index' => 'index',
                                 'alias' => 'alias',
                             ],
                         ],

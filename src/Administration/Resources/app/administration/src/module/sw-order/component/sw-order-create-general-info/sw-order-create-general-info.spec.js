b7881004f8f57cd0d7ae99fad4062f0e3d96e9d6
Quynh Nguyen <q.nguyen@shopware.com>
2024-02-13 19:52:36 +0700

    NEXT-33724 - Fix order currency

--- src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-create-general-info/sw-order-create-general-info.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-create-general-info/sw-order-create-general-info.spec.js
@@ -30,9 +30,7 @@ const context = {
         totalRounding: {
             decimals: 2,
         },
-        translated: {
-            isoCode: 'EUR',
-        },
+        isoCode: 'EUR',
     },
 };
 

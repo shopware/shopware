267e3d7927b5ac7c5425806a3a09ed9622fe6b44
Björn Meyer <b.meyer@shopware.com>
2024-03-04 15:40:28 +0100

    NEXT-34151 - Added Stoplight Elements for admin and store API

---
+++ changelog/_unreleased/2024-03-04-add-stoplight-elements.md
@@ -0,0 +1,10 @@
+---
+title: Add Stoplight Elements
+issue: NEXT-34151
+author: Björn Meyer
+author_email: b.meyer@shopware.com
+author_github: BrocksiNet
+---
+# Core
+* Added Stoplight Elements for the **admin** API, see `localhost:8000/api/_info/stoplightio.html`.
+* Added Stoplight Elements for the **store** API, see `localhost:8000/store-api/_info/stoplightio.html`.
--- src/Core/Framework/Api/Controller/InfoController.php
+++ src/Core/Framework/Api/Controller/InfoController.php
@@ -136,6 +136,31 @@ class InfoController extends AbstractController
         return $response;
     }
 
+    #[Route(path: '/api/_info/stoplightio.html', defaults: ['auth_required' => '%shopware.api.api_browser.auth_required_str%'], name: 'api.info.stoplightio', methods: ['GET'])]
+    public function stoplightIoInfoHtml(Request $request): Response
+    {
+        $nonce = $request->attributes->get(PlatformRequest::ATTRIBUTE_CSP_NONCE);
+        $apiType = $request->query->getAlpha('type', DefinitionService::TYPE_JSON);
+        $response = $this->render(
+            '@Framework/stoplightio.html.twig',
+            [
+                'schemaUrl' => 'api.info.openapi3',
+                'cspNonce' => $nonce,
+                'apiType' => $apiType,
+            ]
+        );
+
+        $cspTemplate = $this->cspTemplates['administration'] ?? '';
+        $cspTemplate = trim($cspTemplate);
+        if ($cspTemplate !== '') {
+            $csp = str_replace('%nonce%', $nonce, $cspTemplate);
+            $csp = str_replace(["\n", "\r"], ' ', $csp);
+            $response->headers->set('Content-Security-Policy', $csp);
+        }
+
+        return $response;
+    }
+
     #[Route(path: '/api/_info/config', name: 'api.info.config', methods: ['GET'])]
     public function config(Context $context, Request $request): JsonResponse
     {

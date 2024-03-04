12cd802ee543ee9d772a6531c325b6d9ca7d312b
Soner Sayakci <s.sayakci@shopware.com>
2024-03-04 10:30:46 +0100

    NEXT-34113 - Clear cookies on 404 pages

--- tests/unit/Storefront/Framework/Routing/NotFound/NotFoundPageCacheKeyEventTest.php
+++ tests/unit/Storefront/Framework/Routing/NotFound/NotFoundPageCacheKeyEventTest.php
@@ -1,19 +1,19 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Storefront\Test\Framework\Routing\NotFound;
+namespace Shopware\Tests\Unit\Storefront\Framework\Routing\NotFound;
 
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Storefront\Framework\Routing\NotFound\NotFoundPageTagsEvent;
+use Shopware\Storefront\Framework\Routing\NotFound\NotFoundPageCacheKeyEvent;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @internal
  *
- * @covers \Shopware\Storefront\Framework\Routing\NotFound\NotFoundPageTagsEvent
+ * @covers \Shopware\Storefront\Framework\Routing\NotFound\NotFoundPageCacheKeyEvent
  */
-class NotFoundPageTagsEventTest extends TestCase
+class NotFoundPageCacheKeyEventTest extends TestCase
 {
     public function testEvent(): void
     {
@@ -21,14 +21,14 @@ class NotFoundPageTagsEventTest extends TestCase
         $context = $this->createMock(SalesChannelContext::class);
         $context->method('getContext')->willReturn(Context::createDefaultContext());
 
-        $event = new NotFoundPageTagsEvent(['test'], $request, $context);
+        $event = new NotFoundPageCacheKeyEvent('test', $request, $context);
 
-        static::assertSame(['test'], $event->getTags());
+        static::assertSame('test', $event->getKey());
         static::assertSame($context->getContext(), $event->getContext());
         static::assertSame($context, $event->getSalesChannelContext());
         static::assertSame($request, $event->getRequest());
 
-        $event->addTags(['test2']);
-        static::assertSame(['test', 'test2'], $event->getTags());
+        $event->setKey('test2');
+        static::assertSame('test2', $event->getKey());
     }
 }

0ba045492941894b87612c2aeb645f43fac6b55c
Björn Meyer <b.meyer@shopware.com>
2024-03-05 11:01:36 +0100

    NEXT-34151 - Fix phpstan for VersionTest.php

--- src/Core/Framework/Test/Api/VersionTest.php
+++ src/Core/Framework/Test/Api/VersionTest.php
@@ -26,6 +26,9 @@ class VersionTest extends TestCase
         ]);
     }
 
+    /**
+     * @return array<array{string, string}>
+     */
     public static function protectedRoutesDataProvider(): array
     {
         return [
@@ -48,8 +51,10 @@ class VersionTest extends TestCase
             'Route should be protected. (URL: /api/oauth/token)'
         );
 
-        $response = json_decode($this->unauthorizedClient->getResponse()->getContent(), true, 512, \JSON_THROW_ON_ERROR);
+        $content = (string)$this->unauthorizedClient->getResponse()->getContent();
+        $response = json_decode($content, true, 512, \JSON_THROW_ON_ERROR);
 
+        static::assertNotEquals('false', $content);
         static::assertEquals('The authorization grant type is not supported by the authorization server.', $response['errors'][0]['title']);
         static::assertEquals('Check that all required parameters have been provided', $response['errors'][0]['detail']);
     }

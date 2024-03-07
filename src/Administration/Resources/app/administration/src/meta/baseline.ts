7462d46bca7c585e866a65b4534c88e3873074a5
Malte Janz <m.janz@shopware.com>
2024-03-07 16:42:13 +0100

    NEXT-34109 - Fix cherry-pick conflicts by adjusting the jest test to vue3

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -680,7 +680,6 @@ const missingTests = [
     'src/module/sw-flow/component/modals/sw-flow-create-mail-template-modal/index.js',
     'src/module/sw-flow/component/modals/sw-flow-mail-send-modal/index.js',
     'src/module/sw-flow/component/sw-flow-trigger/index.js',
-    'src/module/sw-flow/page/sw-flow-detail/index.js',
     'src/module/sw-flow/view/listing/sw-flow-list-flow-templates/index.ts',
     'src/module/sw-profile/view/sw-profile-index-general/index.js',
     'src/module/sw-sales-channel/component/sw-sales-channel-modal/index.js',

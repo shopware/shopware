7ca2b8920df9b50289fce9cbcf9c9622d7041607
Joshua Behrens <behrens@heptacom.de>
2024-03-10 00:39:31 +0100

    Check invalid rules with DAL before fetching

--- src/Core/Checkout/Cart/RuleLoader.php
+++ src/Core/Checkout/Cart/RuleLoader.php
@@ -7,6 +7,7 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\RepositoryIterator;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
@@ -36,14 +37,16 @@ class RuleLoader extends AbstractRuleLoader
         $criteria = new Criteria();
         $criteria->addSorting(new FieldSorting('priority', FieldSorting::DESCENDING));
         $criteria->addSorting(new FieldSorting('id'));
+        $criteria->addFilter(new EqualsFilter('invalid', false));
         $criteria->setLimit(500);
         $criteria->setTitle('cart-rule-loader::load-rules');
 
         $repositoryIterator = new RepositoryIterator($this->repository, $context, $criteria);
         $rules = new RuleCollection();
+
         while (($result = $repositoryIterator->fetch()) !== null) {
             foreach ($result->getEntities() as $rule) {
-                if (!$rule->isInvalid() && $rule->getPayload()) {
+                if ($rule->getPayload()) {
                     $rules->add($rule);
                 }
             }

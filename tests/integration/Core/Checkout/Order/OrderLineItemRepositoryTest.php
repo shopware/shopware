739769211fd2ef064932cb0e11731ebfbb63784c
xianyang <xianyang.lu@vcg.com>
2024-03-20 17:48:10 +0800

    fix product_version_id to nullable

---
+++ tests/integration/Core/Checkout/Order/OrderLineItemRepositoryTest.php
@@ -0,0 +1,133 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Integration\Core\Checkout\Order;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
+use Shopware\Core\Checkout\Cart\Price\Struct\QuantityPriceDefinition;
+use Shopware\Core\Checkout\Cart\Tax\Struct\CalculatedTaxCollection;
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRuleCollection;
+use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
+use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
+use Shopware\Core\Content\Test\Flow\OrderActionTrait;
+use Shopware\Core\Content\Test\Product\ProductBuilder;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Test\IdsCollection;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Test\TestDataCollection;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\Test\TestDefaults;
+
+#[Package('checkout')]
+class OrderLineItemRepositoryTest extends TestCase
+{
+    use IntegrationTestBehaviour;
+    use OrderActionTrait;
+
+   protected function setUp(): void
+   {
+       $this->ids = new TestDataCollection();
+   }
+
+    public function testProductVersionIdNullable(): void
+    {
+        $productId = Uuid::randomHex();
+        $context = Context::createDefaultContext();
+
+        $product = [
+            'id' => $productId,
+            'versionId' => $productId,
+            'name' => 'test',
+            'price' => [
+                ['currencyId' => Defaults::CURRENCY, 'gross' => 119.99, 'net' => 99.99, 'linked' => false],
+            ],
+            'productNumber' => Uuid::randomHex(),
+            'manufacturer' => ['name' => 'test'],
+            'tax' => ['taxRate' => 19, 'name' => 'test'],
+            'stock' => 10,
+            'active' => true,
+            'visibilities' => [
+                ['salesChannelId' => TestDefaults::SALES_CHANNEL, 'visibility' => ProductVisibilityDefinition::VISIBILITY_ALL],
+            ],
+        ];
+
+        $this->getContainer()->get('product.repository')
+            ->create([$product], $context);
+
+        $orderId = Uuid::randomHex();
+        $orderItemLineId = Uuid::randomHex();
+        $customerId = $this->createCustomer();
+        $this->createOrder($customerId, [
+            'id' => $orderId,
+            'lineItems' => [
+                [
+                    'id' => $orderItemLineId,
+                    'identifier' => $orderItemLineId,
+                    'productId' => $productId,
+                    'productVersionId' => $productId,
+                    'quantity' => 1,
+                    'label' => 'label',
+                    'type' => LineItem::CUSTOM_LINE_ITEM_TYPE,
+                    'price' => new CalculatedPrice(200, 200, new CalculatedTaxCollection(), new TaxRuleCollection()),
+                    'priceDefinition' => new QuantityPriceDefinition(200, new TaxRuleCollection(), 2),
+                ]
+            ],
+            'deliveries' => null
+        ]);
+
+        $this->getContainer()->get('product.repository')
+            ->delete([['id' => $productId, 'versionId' => $product]], $context);
+
+        /** @var OrderLineItemEntity $orderItemLine */
+        $orderItemLine = $this->getContainer()->get('order_line_item.repository')
+            ->search(new Criteria([$orderItemLineId]), $context)->first();
+        self::assertNull($orderItemLine->getProductId());
+        self::assertNull($orderItemLine->getProductVersionId());
+    }
+
+    private function createCustomer(): string
+    {
+        $customerId = Uuid::randomHex();
+        $addressId = Uuid::randomHex();
+
+        $customer = [
+            'id' => $customerId,
+            'number' => '1337',
+            'salutationId' => $this->getValidSalutationId(),
+            'firstName' => 'Max',
+            'lastName' => 'Mustermann',
+            'customerNumber' => '1337',
+            'email' => Uuid::randomHex() . '@example.com',
+            'password' => TestDefaults::HASHED_PASSWORD,
+            'defaultPaymentMethodId' => $this->getValidPaymentMethodId(),
+            'groupId' => TestDefaults::FALLBACK_CUSTOMER_GROUP,
+            'salesChannelId' => TestDefaults::SALES_CHANNEL,
+            'defaultBillingAddressId' => $addressId,
+            'defaultShippingAddressId' => $addressId,
+            'addresses' => [
+                [
+                    'id' => $addressId,
+                    'customerId' => $customerId,
+                    'countryId' => $this->getValidCountryId(),
+                    'salutationId' => $this->getValidSalutationId(),
+                    'firstName' => 'Max',
+                    'lastName' => 'Mustermann',
+                    'street' => 'Ebbinghoff 10',
+                    'zipcode' => '48624',
+                    'city' => 'Schöppingen',
+                ],
+            ],
+        ];
+
+        $this->getContainer()
+            ->get('customer.repository')
+            ->upsert([$customer], Context::createDefaultContext());
+
+        return $customerId;
+    }
+}

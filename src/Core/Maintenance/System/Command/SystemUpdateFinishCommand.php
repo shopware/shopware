3ea8e54beef68f40a2eeb9b00b44091d589f6283
oskroblin Skroblin <o.skroblin@shopware.com>
2024-03-22 10:20:39 +0100

    NEXT-34524 - Also execute destructive migrations in system:update:finish

--- src/Core/Maintenance/System/Command/SystemUpdateFinishCommand.php
+++ src/Core/Maintenance/System/Command/SystemUpdateFinishCommand.php
@@ -98,15 +98,22 @@ class SystemUpdateFinishCommand extends Command
         return self::SUCCESS;
     }
 
-    private function runMigrations(OutputInterface $output): int
+    private function runMigrations(OutputInterface $output): void
     {
         $application = $this->getApplication();
         \assert($application !== null);
         $command = $application->find('database:migrate');
 
-        return $this->runCommand($application, $command, [
+        $this->runCommand($application, $command, [
             'identifier' => 'core',
             '--all' => true,
+        ], $output);
+
+        $command = $application->find('database:migrate-destructive');
+        $this->runCommand($application, $command, [
+            'identifier' => 'core',
+            '--all' => true,
+            '--version-selection-mode' => 'blue-green',
         ], $output);
     }
 

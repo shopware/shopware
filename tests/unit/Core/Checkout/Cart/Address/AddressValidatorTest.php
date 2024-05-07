e417a65fe526e2c6b70d870e2d72756acf9edcba
Albert Scherman <p.scherman@shopware.com>
2024-05-07 09:19:59 +0200

    NEXT-36023 - Added tests for validator

--- tests/unit/Core/Checkout/Cart/Address/AddressValidatorTest.php
+++ tests/unit/Core/Checkout/Cart/Address/AddressValidatorTest.php
@@ -6,16 +6,23 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Checkout\Cart\Address\AddressValidator;
+use Shopware\Core\Checkout\Cart\Address\Error\BillingAddressCountryRegionMissingError;
+use Shopware\Core\Checkout\Cart\Address\Error\BillingAddressSalutationMissingError;
+use Shopware\Core\Checkout\Cart\Address\Error\ShippingAddressCountryRegionMissingError;
 use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\Delivery\Struct\ShippingLocation;
 use Shopware\Core\Checkout\Cart\Error\ErrorCollection;
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Content\Product\State;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
 use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\Country\Aggregate\CountryState\CountryStateCollection;
+use Shopware\Core\System\Country\Aggregate\CountryState\CountryStateEntity;
 use Shopware\Core\System\Country\CountryEntity;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
@@ -121,6 +128,188 @@ class AddressValidatorTest extends TestCase
         $context->method('getShippingLocation')->willReturn($shippingLocation);
 
         $cart->add((new LineItem('b', 'test'))->setStates([State::IS_DOWNLOAD]));
+
+        $errorCollection = new ErrorCollection();
+        $this->validator->validate($cart, $errorCollection, $context);
+
+        static::assertEquals(0, $errorCollection->count());
+    }
+
+    public function testValidateShippingAddressWithoutSalutation(): void
+    {
+        $cart = new Cart('test');
+
+        $context = $this->createMock(SalesChannelContext::class);
+
+        $country = new CountryEntity();
+        $country->setId(Uuid::randomHex());
+        $country->setActive(true);
+        $country->setShippingAvailable(true);
+        $country->setForceStateInRegistration(true);
+
+        $countryState = new CountryStateEntity();
+        $countryState->setId(Uuid::randomHex());
+        $countryState->setCountryId($country->getId());
+        $countryState->setCountry($country);
+        $countryState->setActive(true);
+
+        $countryStates = new CountryStateCollection();
+        $countryStates->add($countryState);
+        $country->setStates($countryStates);
+
+        $customerAddress = new CustomerAddressEntity();
+        $customerAddress->setId(Uuid::randomHex());
+        $customerAddress->setCountryId($country->getId());
+        $customerAddress->setFirstName('John');
+        $customerAddress->setLastName('Doe');
+        $customerAddress->setCity('ExampleCity');
+
+        $customer = new CustomerEntity();
+        $customer->setFirstName('John');
+        $customer->setLastName('Doe');
+        $customer->setId(Uuid::randomHex());
+        $customer->setActive(true);
+        $customer->setActiveBillingAddress($customerAddress);
+        $customer->setActiveShippingAddress($customerAddress);
+
+        $context->expects($this->once())->method('getCustomer')->willReturn($customer);
+
+        $idSearchResult = new IdSearchResult(
+            1,
+            [['data' => $country->getId(), 'primaryKey' => $country->getId()]],
+            new Criteria(),
+            Context::createDefaultContext()
+        );
+        $this->repository->method('searchIds')->willReturn($idSearchResult);
+
+        $shippingLocation = new ShippingLocation($country, null, null);
+        $context->method('getShippingLocation')->willReturn($shippingLocation);
+
+        $cart->add((new LineItem('b', 'test'))->setStates([State::IS_PHYSICAL]));
+
+        $errorCollection = new ErrorCollection();
+        $this->validator->validate($cart, $errorCollection, $context);
+
+        static::assertEquals(1, $errorCollection->count());
+        static::assertEquals(BillingAddressSalutationMissingError::class, get_class($errorCollection->first()));
+    }
+
+    public function testValidateAddressWithoutState(): void
+    {
+        $cart = new Cart('test');
+
+        $context = $this->createMock(SalesChannelContext::class);
+
+        $country = new CountryEntity();
+        $country->setId(Uuid::randomHex());
+        $country->setActive(true);
+        $country->setShippingAvailable(true);
+        $country->setForceStateInRegistration(true);
+
+        $countryState = new CountryStateEntity();
+        $countryState->setId(Uuid::randomHex());
+        $countryState->setCountryId($country->getId());
+        $countryState->setCountry($country);
+        $countryState->setActive(true);
+
+        $countryStates = new CountryStateCollection();
+        $countryStates->add($countryState);
+        $country->setStates($countryStates);
+
+        $customerAddress = new CustomerAddressEntity();
+        $customerAddress->setId(Uuid::randomHex());
+        $customerAddress->setCountryId($country->getId());
+        $customerAddress->setFirstName('John');
+        $customerAddress->setLastName('Doe');
+        $customerAddress->setCity('ExampleCity');
+        $customerAddress->setSalutationId(Uuid::randomHex());
+        $customerAddress->setCountry($country);
+
+        $customer = new CustomerEntity();
+        $customer->setFirstName('John');
+        $customer->setLastName('Doe');
+        $customer->setId(Uuid::randomHex());
+        $customer->setActive(true);
+        $customer->setActiveBillingAddress($customerAddress);
+        $customer->setActiveShippingAddress($customerAddress);
+
+        $context->expects($this->once())->method('getCustomer')->willReturn($customer);
+
+        $idSearchResult = new IdSearchResult(
+            1,
+            [['data' => $country->getId(), 'primaryKey' => $country->getId()]],
+            new Criteria(),
+            Context::createDefaultContext()
+        );
+        $this->repository->method('searchIds')->willReturn($idSearchResult);
+
+        $shippingLocation = new ShippingLocation($country, null, null);
+        $context->method('getShippingLocation')->willReturn($shippingLocation);
+
+        $cart->add((new LineItem('b', 'test'))->setStates([State::IS_PHYSICAL]));
+
+        $errorCollection = new ErrorCollection();
+        $this->validator->validate($cart, $errorCollection, $context);
+
+        static::assertEquals(2, $errorCollection->count());
+        static::assertEquals(BillingAddressCountryRegionMissingError::class, get_class($errorCollection->first()));
+        static::assertEquals(ShippingAddressCountryRegionMissingError::class, get_class($errorCollection->last()));
+    }
+
+    public function testValidateAddressWithState(): void
+    {
+        $cart = new Cart('test');
+
+        $context = $this->createMock(SalesChannelContext::class);
+
+        $country = new CountryEntity();
+        $country->setId(Uuid::randomHex());
+        $country->setActive(true);
+        $country->setShippingAvailable(true);
+        $country->setForceStateInRegistration(true);
+
+        $countryState = new CountryStateEntity();
+        $countryState->setId(Uuid::randomHex());
+        $countryState->setCountryId($country->getId());
+        $countryState->setCountry($country);
+        $countryState->setActive(true);
+
+        $countryStates = new CountryStateCollection();
+        $countryStates->add($countryState);
+        $country->setStates($countryStates);
+
+        $customerAddress = new CustomerAddressEntity();
+        $customerAddress->setId(Uuid::randomHex());
+        $customerAddress->setCountryId($country->getId());
+        $customerAddress->setFirstName('John');
+        $customerAddress->setLastName('Doe');
+        $customerAddress->setCity('ExampleCity');
+        $customerAddress->setSalutationId(Uuid::randomHex());
+        $customerAddress->setCountry($country);
+        $customerAddress->setCountryState($countryState);
+
+        $customer = new CustomerEntity();
+        $customer->setFirstName('John');
+        $customer->setLastName('Doe');
+        $customer->setId(Uuid::randomHex());
+        $customer->setActive(true);
+        $customer->setActiveBillingAddress($customerAddress);
+        $customer->setActiveShippingAddress($customerAddress);
+
+        $context->expects($this->once())->method('getCustomer')->willReturn($customer);
+
+        $idSearchResult = new IdSearchResult(
+            1,
+            [['data' => $country->getId(), 'primaryKey' => $country->getId()]],
+            new Criteria(),
+            Context::createDefaultContext()
+        );
+        $this->repository->method('searchIds')->willReturn($idSearchResult);
+
+        $shippingLocation = new ShippingLocation($country, null, null);
+        $context->method('getShippingLocation')->willReturn($shippingLocation);
+
+        $cart->add((new LineItem('b', 'test'))->setStates([State::IS_PHYSICAL]));
 
         $errorCollection = new ErrorCollection();
         $this->validator->validate($cart, $errorCollection, $context);

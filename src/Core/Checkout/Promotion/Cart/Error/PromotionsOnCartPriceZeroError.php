da7c3e0bd4f5c7e1187f7dbc2b0dd216e719216b
JasperP98 <jasper.peeters@meteor.be>
2024-05-08 12:58:41 +0200

    NEXT-36151 - Create error for when we try to add a promotion but cart price is zero fixes #3704

---
+++ src/Core/Checkout/Promotion/Cart/Error/PromotionsOnCartPriceZeroError.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Promotion\Cart\Error;
+
+use Shopware\Core\Checkout\Cart\Error\Error;
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('buyers-experience')]
+class PromotionsOnCartPriceZeroError extends Error
+{
+    private const KEY = 'promotions-on-cart-price-zero-error';
+
+    public function __construct(protected array $promotions)
+    {
+        $this->message = sprintf('Promotions %s were excluded for cart because the price of the cart is zero.', implode(', ', $this->promotions));
+
+        parent::__construct($this->message);
+    }
+
+    public function isPersistent(): bool
+    {
+        return false;
+    }
+
+    public function getId(): string
+    {
+        return self::KEY;
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL_NOTICE;
+    }
+
+    public function getMessageKey(): string
+    {
+        return self::KEY;
+    }
+
+    public function getPromotions(): array
+    {
+        return $this->promotions;
+    }
+
+    public function blockOrder(): bool
+    {
+        return false;
+    }
+
+    public function getParameters(): array
+    {
+        return [
+            'promotions' => implode(', ', $this->promotions),
+        ];
+    }
+}

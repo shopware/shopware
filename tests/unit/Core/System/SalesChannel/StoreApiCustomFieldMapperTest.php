74b9b7477f2395844865383a29eaa99d2d2b14c2
Jonas Elfering <j.elfering@shopware.com>
2024-05-10 15:34:07 +0200

    NEXT-35996 - Decode custom fields in StoreApiCustomFieldMapper

--- tests/unit/Core/System/SalesChannel/StoreApiCustomFieldMapperTest.php
+++ tests/unit/Core/System/SalesChannel/StoreApiCustomFieldMapperTest.php
@@ -7,7 +7,9 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\CustomField\CustomFieldTypes;
 use Shopware\Core\System\SalesChannel\StoreApiCustomFieldMapper;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * @internal
@@ -16,15 +18,56 @@ use Shopware\Core\System\SalesChannel\StoreApiCustomFieldMapper;
 #[CoversClass(StoreApiCustomFieldMapper::class)]
 class StoreApiCustomFieldMapperTest extends TestCase
 {
-    public function testMapping(): void
+    public function testMappingRemovesNotAllowedFields(): void
     {
         $connection = $this->createMock(Connection::class);
-        $connection
-            ->method('fetchAllAssociative')
-            ->willReturn([['entity_name' => 'customer', 'name' => 'allowed']]);
 
-        $mapper = new StoreApiCustomFieldMapper($connection);
+        $mapper = new StoreApiCustomFieldMapper($connection, ['customer' => [['name' => 'allowed', 'type' => 'string']]]);
         static::assertSame(['allowed' => 'yes'], $mapper->map('customer', new RequestDataBag(['bla' => 'foo', 'allowed' => 'yes'])));
+    }
+
+    public function testMappingDecodesFieldTypes(): void
+    {
+        $connection = $this->createMock(Connection::class);
+
+        $mapper = new StoreApiCustomFieldMapper($connection, ['customer' => [
+            ['name' => 'string', 'type' => CustomFieldTypes::TEXT],
+            ['name' => 'int', 'type' => CustomFieldTypes::INT],
+            ['name' => 'float', 'type' => CustomFieldTypes::FLOAT],
+            ['name' => 'bool', 'type' => CustomFieldTypes::BOOL],
+            ['name' => 'json', 'type' => CustomFieldTypes::JSON],
+            ['name' => 'singleSelect', 'type' => CustomFieldTypes::SELECT],
+            ['name' => 'multiSelect', 'type' => CustomFieldTypes::SELECT],
+            ['name' => 'date', 'type' => CustomFieldTypes::DATETIME],
+        ]]);
+
+        $mappedValues = $mapper->map('customer', new RequestDataBag([
+            'bla' => 'foo',
+            'string' => 'yes',
+            'int' => '1',
+            'float' => '1.1',
+            'bool' => 'true',
+            'json' => new ParameterBag(['foo' => 'bar']),
+            'singleSelect' => 'foo',
+            'multiSelect' => new ParameterBag(['foo', 'bar']),
+            'date' => '2020-01-01T00:00:00+00:00',
+        ]));
+
+        static::assertEquals(new \DateTimeImmutable('2020-01-01T00:00:00+00:00'), $mappedValues['date']);
+        unset($mappedValues['date']);
+
+        static::assertSame(
+            [
+                'string' => 'yes',
+                'int' => 1,
+                'float' => 1.1,
+                'bool' => true,
+                'json' => ['foo' => 'bar'],
+                'singleSelect' => 'foo',
+                'multiSelect' => ['foo', 'bar'],
+            ],
+            $mappedValues
+        );
     }
 
     public function testInternalStorageWorks(): void

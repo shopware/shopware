d71f169bece4d8e415cbcf4192051e29844ac870
Jonas Elfering <j.elfering@shopware.com>
2024-06-17 15:46:55 +0200

    NEXT-36799 - Add template content check in MailActionController

--- tests/unit/Core/Content/MailTemplate/Api/MailActionControllerTest.php
+++ tests/unit/Core/Content/MailTemplate/Api/MailActionControllerTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use Shopware\Core\Content\Mail\Service\AbstractMailService;
 use Shopware\Core\Content\Mail\Service\MailAttachmentsConfig;
 use Shopware\Core\Content\MailTemplate\Api\MailActionController;
+use Shopware\Core\Content\MailTemplate\MailTemplateException;
 use Shopware\Core\Framework\Adapter\Twig\StringTemplateRenderer;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Log\Package;
@@ -62,5 +63,92 @@ class MailActionControllerTest extends TestCase
         );
 
         $mailActionController->send($data, Context::createDefaultContext());
+    }
+
+    public function testBuild(): void
+    {
+        $templateData = [
+            'order' => [
+                'id' => Uuid::randomHex(),
+            ],
+        ];
+
+        $data = new RequestDataBag([
+            'mailTemplateType' => [
+                'templateData' => $templateData,
+            ],
+            'mailTemplate' => [
+                'contentHtml' => 'html',
+            ],
+        ]);
+
+        $context = Context::createDefaultContext();
+
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('enableTestMode');
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('disableTestMode');
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('render')
+            ->with('html', $templateData, $context)
+            ->willReturn('rendered');
+
+        $mailActionController = new MailActionController(
+            $this->mailService,
+            $this->stringTemplateRenderer
+        );
+
+        $response = $mailActionController->build($data, $context);
+        static::assertEquals('"rendered"', $response->getContent());
+    }
+
+    public function testBuildWithoutTemplateData(): void
+    {
+        $data = new RequestDataBag([
+            'mailTemplate' => [
+                'contentHtml' => 'html',
+            ],
+        ]);
+
+        $context = Context::createDefaultContext();
+
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('enableTestMode');
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('disableTestMode');
+        $this->stringTemplateRenderer->expects(static::once())
+            ->method('render')
+            ->with('html', [], $context)
+            ->willReturn('rendered');
+
+        $mailActionController = new MailActionController(
+            $this->mailService,
+            $this->stringTemplateRenderer
+        );
+
+        $response = $mailActionController->build($data, $context);
+        static::assertEquals('"rendered"', $response->getContent());
+    }
+
+    public function testBuildWithoutTemplateContentThrows(): void
+    {
+        $data = new RequestDataBag();
+
+        $context = Context::createDefaultContext();
+
+        $this->stringTemplateRenderer->expects(static::never())
+            ->method('enableTestMode');
+        $this->stringTemplateRenderer->expects(static::never())
+            ->method('disableTestMode');
+        $this->stringTemplateRenderer->expects(static::never())
+            ->method('render');
+
+        $mailActionController = new MailActionController(
+            $this->mailService,
+            $this->stringTemplateRenderer
+        );
+
+        $this->expectExceptionObject(MailTemplateException::invalidMailTemplateContent());
+        $mailActionController->build($data, $context);
     }
 }

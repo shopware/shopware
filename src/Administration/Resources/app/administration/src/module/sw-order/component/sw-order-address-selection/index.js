400afdf097e01ba2ed73346e02c6e3711ca0cd90
Y Tran <y.tran@shopware.com>
2024-07-22 08:16:19 +0700

    NEXT-37348 - Fix the warning message work does not correct in order detail page

--- src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-address-selection/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-address-selection/index.js
@@ -105,6 +105,7 @@ export default {
 
             return criteria;
         },
+
         addressOptions() {
             const addresses = (this.customer?.addresses || []).map(item => {
                 return {
@@ -148,9 +149,6 @@ export default {
         },
 
         onEditAddress(id) {
-            // set address selected
-            this.orderAddressId = id;
-
             if (id === this.address.id) {
                 this.currentAddress = this.address;
                 return;
@@ -181,6 +179,8 @@ export default {
             if (this.currentAddress.id === this.address.id) {
                 return this.orderRepository.save(this.order, this.versionContext).then(() => {
                     this.currentAddress = null;
+
+                    this.onAddressChange(this.address.id, true);
                 }).catch(() => {
                     this.createNotificationError({
                         message: this.$tc('sw-order.detail.messageSaveError'),
@@ -244,11 +244,12 @@ export default {
             return `${preFix.charAt(0).toUpperCase()}${preFix.slice(1)}`;
         },
 
-        onAddressChange(customerAddressId) {
+        onAddressChange(customerAddressId, edited = false) {
             this.$emit('change-address', {
                 orderAddressId: this.orderAddressId,
                 customerAddressId: customerAddressId,
                 type: this.type,
+                edited,
             });
         },
 

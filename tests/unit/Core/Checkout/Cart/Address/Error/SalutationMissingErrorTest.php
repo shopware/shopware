76a3a75f112609d2b99985cee2136e6e1318ce30
Lennart Tinkloh <l.tinkloh@shopware.com>
2024-07-24 09:14:18 +0200

    NEXT-37271 - Improve checkout coverage

---
+++ tests/unit/Core/Checkout/Cart/Address/Error/SalutationMissingErrorTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Checkout\Cart\Address\Error;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Cart\Address\Error\SalutationMissingError;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * @internal
+ */
+#[Package('checkout')]
+#[CoversClass(SalutationMissingError::class)]
+class SalutationMissingErrorTest extends TestCase
+{
+    public function testAPI(): void
+    {
+        $error = new class() extends SalutationMissingError {
+            public function getId(): string
+            {
+                return 'salutation-missing';
+            }
+        };
+
+        static::assertSame('salutation-missing', $error->getId());
+        static::assertSame('salutation-missing', $error->getMessageKey());
+        static::assertSame(10, $error->getLevel());
+        static::assertTrue($error->blockOrder());
+    }
+}

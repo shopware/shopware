3b40d9c7ee53fe8f8b8c18cbee1a8bda86b8a495
Soner Sayakci <s.sayakci@shopware.com>
2024-07-29 11:52:05 +0000

    NEXT-35453 - Revert Improve storefront watcher

--- src/Storefront/Resources/app/storefront/build/start-hot-reload.js
+++ src/Storefront/Resources/app/storefront/build/start-hot-reload.js
@@ -1,14 +1,8 @@
-const os = require('os');
-const crypto = require('crypto');
-
 const createLiveReloadServer = require('./live-reload-server/index');
 const createProxyServer = require('./proxy-server-hot/index');
 
-const socketName = crypto.randomBytes(4).readUInt32LE(0);
-const socketPath = `${os.tmpdir()}/shopware-${socketName}.sock`;
-
 // starting the live reload server
-const server = createLiveReloadServer(socketPath);
+const server = createLiveReloadServer();
 
 server.then(() => {
     const fullUrl = process.env.APP_URL;
@@ -27,15 +21,12 @@ server.then(() => {
         proxyHost: proxyUrl.hostname,
         proxyPort: parseInt(proxyPort || 9998),
         uri: uri.join('/'),
-        socketPath,
     };
 
     // starting the proxy server
     createProxyServer(proxyServerOptions).then(({ proxyUrl } ) => {
         console.log('############');
         console.log(`Storefront proxy server started at ${proxyUrl}`);
-        console.log(`Shopware storefront is available at ${fullUrl} (Use environment variable APP_URL to change)`);
-        console.log(`Replacing all links to ${proxyUrl} (Use environment variable PROXY_URL to change)`);
         console.log('############');
         console.log('\n');
     });

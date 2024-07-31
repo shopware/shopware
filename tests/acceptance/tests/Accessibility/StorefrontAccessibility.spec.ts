1a9119656ffbe44bfe8ca816b0acb1610d5fd20b
Philipp Schuch <p.schuch@shopware.com>
2024-07-31 07:35:54 +0200

    NEXT-37464 - Activate assertions in accessibility test

--- tests/acceptance/tests/Accessibility/StorefrontAccessibility.spec.ts
+++ tests/acceptance/tests/Accessibility/StorefrontAccessibility.spec.ts
@@ -27,58 +27,60 @@ test('The Storefront should implement accessibility best practices.', { tag: '@A
 
     await test.step('Login Page Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontAccountLogin.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Login', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Login', true));
     });
 
     await test.step('Account Page Accessibility', async () => {
         await ShopCustomer.attemptsTo(Login());
         await ShopCustomer.goesTo(StorefrontAccount.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Account', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Account', true));
     });
 
     await test.step('Category Page Accessibility', async () => {
-        await ShopCustomer.goesTo(StorefrontCategory.url(category.id));
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Category', false));
+        await ShopCustomer.goesTo(StorefrontCategory.url(category.name));
+
+        // Category has one valid violation, because first level headline can be configured via CMS.
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Category', 1));
     });
 
     await test.step('Product Detail Page Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Product', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Product', true));
     });
 
     await test.step('Cart Page Accessibility', async () => {
         await ShopCustomer.attemptsTo(AddProductToCart(product, '5'));
         await ShopCustomer.goesTo(StorefrontCheckoutCart.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Cart', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Cart', true));
     });
 
     await test.step('Checkout Accessibility', async () => {
         await ShopCustomer.attemptsTo(ProceedFromCartToCheckout());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Checkout Confirm', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Checkout Confirm', true));
 
         await ShopCustomer.attemptsTo(ConfirmTermsAndConditions());
         await ShopCustomer.attemptsTo(SubmitOrder());
 
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Checkout Finish', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Checkout Finish', true));
     });
 
     await test.step('Account Order Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontAccountOrder.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Order', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Order', true));
     });
 
     await test.step('Account Profile Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontAccountProfile.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Profile', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Profile', true));
     });
 
     await test.step('Account Addresses Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontAccountAddresses.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Addresses', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Addresses', true));
     });
 
     await test.step('Account Payment Accessibility', async () => {
         await ShopCustomer.goesTo(StorefrontAccountPayment.url());
-        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Payment', false));
+        await ShopCustomer.attemptsTo(ValidateAccessibility('Account Payment', true));
     });
 });

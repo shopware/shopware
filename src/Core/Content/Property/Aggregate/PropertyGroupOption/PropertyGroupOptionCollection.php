76a1eb5131c90c27c1e684b6a78ca561eba2eeda
Soner Sayakci <s.sayakci@gmail.com>
2024-08-08 10:01:56 +0200

    NEXT-37574 - Fix property group collection error

--- src/Core/Content/Property/Aggregate/PropertyGroupOption/PropertyGroupOptionCollection.php
+++ src/Core/Content/Property/Aggregate/PropertyGroupOption/PropertyGroupOptionCollection.php
@@ -51,10 +51,14 @@ class PropertyGroupOptionCollection extends EntityCollection
         $groups = new PropertyGroupCollection();
 
         foreach ($this->getIterator() as $element) {
+            if ($element->getGroup() === null) {
+                continue;
+            }
+
             if ($groups->has($element->getGroupId())) {
                 $group = $groups->get($element->getGroupId());
             } else {
-                $group = PropertyGroupEntity::createFrom($element->getGroup() ?? new PropertyGroupEntity());
+                $group = PropertyGroupEntity::createFrom($element->getGroup());
                 $groups->add($group);
 
                 $group->setOptions(new self());

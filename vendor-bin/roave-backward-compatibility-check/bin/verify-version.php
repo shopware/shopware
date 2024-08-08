a62ebbeed8dc041b1f68231c52101fb21e74fa44
Michael Telgmann <m.telgmann@shopware.com>
2024-08-08 10:12:29 +0200

    NEXT-37561 - Remove unnecessary PHP version check from BC checker

--- vendor-bin/roave-backward-compatibility-check/bin/verify-version.php
+++ vendor-bin/roave-backward-compatibility-check/bin/verify-version.php
@@ -1,21 +1,15 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+use Composer\Composer;
 
-/**
- * @deprecated tag:v6.5.0 - Script can be removed as 8.1 will be the min. version
- */
-if (version_compare(PHP_VERSION, '8.0.0', '<')) {
-    echo 'The BC Checker requires at least PHP 8.0, please update your PHP version to run the BC Checker locally.';
-    exit(1);
-}
+require_once __DIR__ . '/../vendor/autoload.php';
 
 if (!extension_loaded('bcmath')) {
     echo 'The BC Checker requires the bcmath extension, please install it to run the BC Checker locally.';
     exit(1);
 }
 
-if (version_compare(\Composer\Composer::getVersion(), '2.2.0', '<')) {
+if (version_compare(Composer::getVersion(), '2.2.0', '<')) {
     echo 'The BC Checker requires at least Composer 2.2.0, please update your composer version to run the BC Checker locally.';
     exit(1);
 }

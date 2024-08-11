5b1ba9a536d89d95d60efb2d4f69f374cf0e06d4
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2024-08-11 16:07:32 +0200

    NEXT-38115 - Add option to hide progress for indexing commands fixes #3844

---
+++ changelog/_unreleased/2024-08-11-add-no-progress-option-to-indexing-commands.md
@@ -0,0 +1,10 @@
+---
+title: Add no-progress option to indexing commands
+author: Marcus Müller
+author_email: 25648755+M-arcus@users.noreply.github.com
+author_github: @M-arcus
+issue: NEXT-00000
+---
+
+# Core
+* Added `--no-progress` option to commands `es:index`, `es:admin:index`, `dal:refresh:index` to disable progress bars
--- src/Elasticsearch/Framework/Command/ElasticsearchIndexingCommand.php
+++ src/Elasticsearch/Framework/Command/ElasticsearchIndexingCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
@@ -42,6 +43,7 @@ class ElasticsearchIndexingCommand extends Command
      */
     protected function configure(): void
     {
+        $this->addOption('no-progress', null, null, 'Do not output progress bar');
         $this->addOption('no-queue', null, null, 'Do not use the queue for indexing');
         $this->addOption('only', null, InputOption::VALUE_REQUIRED, 'Add entities separated by comma to indexing');
     }
@@ -58,7 +60,7 @@ class ElasticsearchIndexingCommand extends Command
             return self::FAILURE;
         }
 
-        $progressBar = new ProgressBar($output);
+        $progressBar = new ProgressBar($input->getOption('no-progress') ? new NullOutput() : $output);
         $progressBar->start();
 
         $entities = $input->getOption('only') ? explode(',', $input->getOption('only')) : [];

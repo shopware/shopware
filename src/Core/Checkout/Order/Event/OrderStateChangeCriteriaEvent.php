96d20639a96432b00cdc2a8ba1d3197ecb857886
Anders Gunder <ag@wexo.dk>
2024-08-15 14:14:54 +0200

    Add ShopwareEvent interface

--- src/Core/Checkout/Order/Event/OrderStateChangeCriteriaEvent.php
+++ src/Core/Checkout/Order/Event/OrderStateChangeCriteriaEvent.php
@@ -4,11 +4,12 @@ namespace Shopware\Core\Checkout\Order\Event;
 
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Event\ShopwareEvent;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Contracts\EventDispatcher\Event;
 
 #[Package('checkout')]
-class OrderStateChangeCriteriaEvent extends Event
+class OrderStateChangeCriteriaEvent extends Event implements ShopwareEvent
 {
     public function __construct(
         private readonly string $orderId,

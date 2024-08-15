3b2d3bb2928e7343b4f0f34a5abc34b32451fa60
Patrick Weyck <p.weyck@shopware.com>
2024-08-15 14:58:31 +0200

    NEXT-38071 - Refactor github workflows

--- tests/integration/Core/Framework/ApiRoutesHaveASchemaTest.php
+++ tests/integration/Core/Framework/ApiRoutesHaveASchemaTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Tests\Integration\Core;
+namespace Shopware\Tests\Integration\Core\Framework;
 
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Api\ApiDefinition\DefinitionService;

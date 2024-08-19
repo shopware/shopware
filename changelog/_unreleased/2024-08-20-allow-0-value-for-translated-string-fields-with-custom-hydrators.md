ab9b09c16ee2d3abfe6988a1ea46e7ab49c625af
max <max@swk-web.com>
2024-08-19 22:54:40 +0200

    NEXT-37993 - fix: Zero translated string field fixes #4519

---
+++ changelog/_unreleased/2024-08-20-allow-0-value-for-translated-string-fields-with-custom-hydrators.md
@@ -0,0 +1,9 @@
+---
+title: Allow "0" value for translated string fields with custom hydrators
+issue: NEXT-0000
+author: Max
+author_email: max@swk-web.com
+author_github: @aragon999
+---
+# Core
+* Changed hydration of translated fields in `Shopware\Core\Framework\DataAbstractionLayer\Dbal\EntityHydrator::translate` to check if the value is strictly equal to null and not just empty, to allow the value "0" for translated fields
---
+++ src/Core/Framework/Test/DataAbstractionLayer/Field/TestDefinition/TranslatableTestTranslationDefinition.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\Test\DataAbstractionLayer\Field\TestDefinition;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityTranslationDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\ApiAware;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+
+/**
+ * @internal
+ */
+class TranslatableTestTranslationDefinition extends EntityTranslationDefinition
+{
+    final public const ENTITY_NAME = 'translatable_test_translation';
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;
+    }
+
+    public function since(): ?string
+    {
+        return '6.0.0.0';
+    }
+
+    protected function getParentDefinitionClass(): string
+    {
+        return CustomFieldTestDefinition::class;
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new StringField('name', 'name'))->addFlags(new ApiAware(), new Required()),
+        ]);
+    }
+}

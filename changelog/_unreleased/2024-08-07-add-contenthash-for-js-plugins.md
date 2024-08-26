3e42cec6db69d381a29f68d6155cc137606b3614
Tobias Berge <t.berge@shopware.com>
2024-08-26 08:29:29 +0200

    NEXT-37279 - Add upgrade information

--- changelog/_unreleased/2024-08-07-add-contenthash-for-js-plugins.md
+++ changelog/_unreleased/2024-08-07-add-contenthash-for-js-plugins.md
@@ -12,8 +12,14 @@ author_github: BrocksiNet
   * This change will also clean up the `dist` folder in plugins/apps and core. So make sure all files below 
     `/dist/storefront` can be rebuild.
 * Added webpack Plugin `FilenameToChunkNamePlugin.js` to shorten the filename and remove the path in production.
+___
+# Upgrade Information
 
- **JS Filename before change in dist:**
+## Shortened filenames with hashes for async JS built files
+When building the Storefront JS-files for production using `composer run build:storefront`, the async bundle filenames no longer contain the filepath.
+Instead, only the filename is used with a chunkhash / dynamic version number. This also helps to identify which files have changed after build. Similar to the main entry file like e.g. `cms-extensions.js?1720776107`.
+
+**JS Filename before change in dist:**
 ```
 └── custom/apps/
     └── ExampleCmsExtensions/src/Resources/app/storefront/dist/storefront/js/
@@ -22,7 +28,7 @@ author_github: BrocksiNet
             └── custom_plugins_CmsExtensions_src_Resources_app_storefront_src_cms-extensions-quickview.js  <-- Complete path in filename
 ```
 
- **JS Filename after change in dist:**
+**JS Filename after change in dist:**
 ```
 └── custom/apps/
     └── ExampleCmsExtensions/src/Resources/app/storefront/dist/storefront/js/
--- src/Storefront/Resources/app/storefront/build/webpack/FilenameToChunkNamePlugin.js
+++ src/Storefront/Resources/app/storefront/build/webpack/FilenameToChunkNamePlugin.js
@@ -12,15 +12,15 @@ class FilenameToChunkNamePlugin {
                 chunks.forEach((chunk) => {
                     // do not change the name in development mode (it is using the original chunkIds: 'named')
                     if (!chunk.name && !this.isDevMode) {
-                        const chunkModule = compilation.chunkGraph.getChunkRootModules(chunk)[0]
-                        const rootModule = (chunkModule && chunkModule.rootModule) || chunkModule
-                        const rootPath = rootModule && rootModule.userRequest
-                        const name = rootPath && rootPath.split(path.sep).slice(-1)[0].replace('.js', '')
+                        const chunkModule = compilation.chunkGraph.getChunkRootModules(chunk)[0];
+                        const rootModule = (chunkModule && chunkModule.rootModule) || chunkModule;
+                        const rootPath = rootModule && rootModule.userRequest;
+                        const name = rootPath && rootPath.split(path.sep).slice(-1)[0].replace('.js', '');
                         // only set the name if it is not already used by another chunk
-                        if(name && !Object.values(this.allChunkNames).includes(name)) {
-                            chunk.name = name
+                        if (name && !Object.values(this.allChunkNames).includes(name)) {
+                            chunk.name = name;
                         }
-                        this.allChunkNames[chunk.id] = name
+                        this.allChunkNames[chunk.id] = name;
                     }
                 })
             })

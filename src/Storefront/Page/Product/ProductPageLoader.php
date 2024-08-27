ea4d6fbe2eaea1c4a61aa86139773da95ce980e9
Michael Telgmann <m.telgmann@shopware.com>
2024-08-27 12:04:45 +0000

    Merge branch 'next-38048/fix-product-page-cross-selling' into 'trunk'
    NEXT-38048 - Consider all sections of a CMS page to search for reviews and cross-sellings
    
    See merge request shopware/6/product/platform!14616

--- src/Storefront/Page/Product/ProductPageLoader.php
+++ src/Storefront/Page/Product/ProductPageLoader.php
@@ -3,6 +3,7 @@
 namespace Shopware\Storefront\Page\Product;
 
 use Shopware\Core\Content\Category\Exception\CategoryNotFoundException;
+use Shopware\Core\Content\Cms\Aggregate\CmsBlock\CmsBlockCollection;
 use Shopware\Core\Content\Cms\SalesChannel\Struct\CrossSellingStruct;
 use Shopware\Core\Content\Cms\SalesChannel\Struct\ProductDescriptionReviewsStruct;
 use Shopware\Core\Content\Product\Aggregate\ProductMedia\ProductMediaCollection;
@@ -172,9 +173,19 @@ class ProductPageLoader
         if ($cmsPage = $product->getCmsPage()) {
             $page->setCmsPage($cmsPage);
 
-            $blocks = $page->getCmsPage()?->getSections()?->first()?->getBlocks();
-            if ($blocks === null) {
+            $sections = $page->getCmsPage()?->getSections();
+            if ($sections === null) {
                 return;
+            }
+
+            $blocks = new CmsBlockCollection();
+            foreach ($sections as $section) {
+                $sectionBlocks = $section->getBlocks();
+                if ($sectionBlocks === null) {
+                    continue;
+                }
+
+                $blocks->merge($sectionBlocks);
             }
 
             $descriptionReviewsStruct = $blocks->filterByProperty('type', 'product-description-reviews')->first()?->getSlots()?->first()?->getData();

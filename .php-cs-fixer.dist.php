08944714a4c8abc4ce73e77b7921cd752cc849c7
Soner Sayakci <s.sayakci@gmail.com>
2024-08-28 22:13:03 +0200

    NEXT-38086 - Exchange ecs with cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Symfony\Component\Filesystem\Path;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'linebreak_after_opening_tag' => false,
+        'blank_line_after_opening_tag' => false,
+        'phpdoc_summary' => false,
+        'phpdoc_annotation_without_dot' => false,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => ['order' => ['param', 'throws', 'return']],
+        'class_attributes_separation' => ['elements' => ['property' => 'one', 'method' => 'one']],
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'concat_space' => ['spacing' => 'one'],
+        'native_function_invocation' => [
+            'scope' => 'namespaced',
+            'strict' => false,
+            'exclude' => ['ini_get'],
+        ],
+        'general_phpdoc_annotation_remove' => ['annotations' => ['copyright', 'category']],
+        'no_superfluous_phpdoc_tags' => ['allow_unused_params' => true, 'allow_mixed' => true],
+        'php_unit_dedicate_assert' => ['target' => 'newest'],
+        'single_quote' => ['strings_containing_single_quote_chars' => true],
+    ])
+    ->setUsingCache(true)
+    ->setCacheFile(Path::join($_SERVER['SHOPWARE_TOOL_CACHE_ECS'] ?? 'var/cache/cs_fixer', 'cs_fixer.cache'))
+    ->setFinder(
+        (new Finder())
+            ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+            ->exclude(['node_modules', '*/vendor/*', 'src/WebInstaller/Tests/_fixtures/Options.php'])
+    );

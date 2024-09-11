dc77ee033d88f1aa7334f92ffd8710e97c685f78
Soner Sayakci <s.sayakci@gmail.com>
2024-09-11 11:14:44 +0200

    NEXT-38263 - Add sales channel domain mapping exception page

--- tests/unit/Core/Framework/Adapter/Kernel/HttpKernelTest.php
+++ tests/unit/Core/Framework/Adapter/Kernel/HttpKernelTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Adapter\Kernel\HttpKernel;
 use Shopware\Core\Framework\Routing\CanonicalRedirectService;
 use Shopware\Core\Framework\Routing\RequestTransformerInterface;
+use Shopware\Storefront\Framework\Routing\Exception\SalesChannelMappingException;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,6 +16,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 /**
  * @internal
@@ -54,6 +56,60 @@ class HttpKernelTest extends TestCase
         $request->attributes->set('exception', new \Exception());
 
         $kernel->handle($request);
+    }
+
+    public function testTransformThrowsUnknownException(): void
+    {
+        $requestTransformer = $this->createMock(RequestTransformerInterface::class);
+        $requestTransformer
+            ->expects(static::once())
+            ->method('transform')
+            ->willThrowException(new \Exception());
+
+        $kernel = new HttpKernel(
+            new EventDispatcher(),
+            $this->controllerResolver,
+            $this->createMock(RequestStack::class),
+            $this->createMock(ArgumentResolverInterface::class),
+            $requestTransformer,
+            $this->createMock(CanonicalRedirectService::class)
+        );
+
+        $request = new Request();
+
+        $this->expectException(\Exception::class);
+
+        $kernel->handle($request);
+    }
+
+    public function testTransformThrowsDomainException(): void
+    {
+        $requestTransformer = $this->createMock(RequestTransformerInterface::class);
+        $requestTransformer
+            ->expects(static::once())
+            ->method('transform')
+            ->willThrowException(new SalesChannelMappingException('test'));
+
+        $dispatcher = new EventDispatcher();
+
+        $dispatcher->addListener('kernel.exception', static function (ExceptionEvent $event): void {
+            $event->setResponse(new Response('event'));
+        });
+
+        $kernel = new HttpKernel(
+            $dispatcher,
+            $this->controllerResolver,
+            $this->createMock(RequestStack::class),
+            $this->createMock(ArgumentResolverInterface::class),
+            $requestTransformer,
+            $this->createMock(CanonicalRedirectService::class)
+        );
+
+        $request = new Request();
+
+        $response = $kernel->handle($request);
+
+        static::assertSame('event', $response->getContent());
     }
 
     public function testHandleNormalRequest(): void

8d1b2a267339c2f1bb3f379e192280aa7d1ab737
tamvt <t.vo@shopware.com>
2024-09-11 16:14:34 +0700

    NEXT-38262 - Fix issue cart discount with total quantity rule not possible

--- src/Core/Checkout/Cart/Rule/LineItemGoodsTotalRule.php
+++ src/Core/Checkout/Cart/Rule/LineItemGoodsTotalRule.php
@@ -4,6 +4,7 @@ namespace Shopware\Core\Checkout\Cart\Rule;
 
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
 use Shopware\Core\Checkout\Cart\LineItem\LineItemCollection;
+use Shopware\Core\Checkout\Cart\Price\Struct\QuantityPriceDefinition;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Rule\Container\FilterRule;
 use Shopware\Core\Framework\Rule\Exception\UnsupportedOperatorException;
@@ -34,6 +35,23 @@ class LineItemGoodsTotalRule extends FilterRule
      */
     public function match(RuleScope $scope): bool
     {
+        if ($scope instanceof LineItemScope) {
+            $lineItem = $scope->getLineItem()->isGood() ? $scope->getLineItem() : null;
+
+            if ($lineItem === null) {
+                return false;
+            }
+
+            if ($this->filter !== null && !$this->filter->match($scope)) {
+                return false;
+            }
+
+            $quantityDefinition = $lineItem->getPriceDefinition();
+            $total = $quantityDefinition instanceof QuantityPriceDefinition ? $quantityDefinition->getQuantity() : $lineItem->getQuantity();
+
+            return RuleComparison::numeric($total, $this->count, $this->operator);
+        }
+
         if (!$scope instanceof CartRuleScope) {
             return false;
         }

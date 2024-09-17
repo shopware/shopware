767301342e558c94ea1ef2a959548259d9e0ff0a
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2024-09-17 11:42:56 +0200

    Add feature check to force option in es:index:cleanup

--- src/Elasticsearch/Framework/Command/ElasticsearchCleanIndicesCommand.php
+++ src/Elasticsearch/Framework/Command/ElasticsearchCleanIndicesCommand.php
@@ -3,11 +3,13 @@
 namespace Shopware\Elasticsearch\Framework\Command;
 
 use OpenSearch\Client;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Elasticsearch\Framework\ElasticsearchOutdatedIndexDetector;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -28,6 +30,17 @@ class ElasticsearchCleanIndicesCommand extends Command
         parent::__construct();
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        if (!Feature::isActive('v6.7.0.0')) {
+            $this
+                ->addOption('force', 'f', InputOption::VALUE_NONE, 'Do not ask for confirmation');
+        }
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
@@ -41,12 +54,14 @@ class ElasticsearchCleanIndicesCommand extends Command
 
         $io->table(['Indices to be deleted:'], array_map(static fn (string $name) => [$name], $indices));
 
-        $confirm = $io->confirm(\sprintf('Delete these %d indices?', \count($indices)));
+        if (Feature::isActive('v6.7.0.0') || !$input->getOption('force')) {
+            $confirm = $io->confirm(\sprintf('Delete these %d indices?', \count($indices)));
 
-        if (!$confirm) {
-            $io->caution('Deletion aborted.');
+            if (!$confirm) {
+                $io->caution('Deletion aborted.');
 
-            return self::SUCCESS;
+                return self::SUCCESS;
+            }
         }
 
         foreach ($indices as $index) {

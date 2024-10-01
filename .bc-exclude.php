026a129ffd6d7ec4fcddc34de9958a66fa4ab5c8
Aydin Hassan <a.hassan@shopware.com>
2024-10-01 13:29:20 +0200

    NEXT-38322 - Store services in more stable location

--- .bc-exclude.php
+++ .bc-exclude.php
@@ -23,6 +23,9 @@ return [
         '**/src/Core/Checkout/Cart/Event/CartChangedEvent.php', // duplicate class declarations for compatibility reasons,
         '**/src/Core/Framework/Changelog/**', // some missed internal
         '**/src/Core/Service/AllServiceInstaller.php', // missed internal (not released yet)
+        '**/src/Core/Framework/App/Source/AbstractTemporaryDirectoryFactory.php', // dropped (not released yet)
+        '**/src/Core/Framework/App/Source/TemporaryDirectoryFactory.php', // dropped decorator (not released yet)
+
     ],
     'errors' => [
         'Shopware\\\\Core\\\\System\\\\SystemConfig\\\\Util\\\\ConfigReader#\\$xsdFile', // Can not be inspected through reflection (__DIR__ constant)

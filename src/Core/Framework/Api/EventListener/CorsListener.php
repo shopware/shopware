89aa5f548bfdd1c8c2e1ea218d6fa81531b4be60
Richard Hülsberg <richard.huelsberg@twt.de>
2024-10-04 12:24:33 +0200

    feat(cors): optimize the cors preflights
    update to native database connections

--- src/Core/Framework/Api/EventListener/CorsListener.php
+++ src/Core/Framework/Api/EventListener/CorsListener.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Doctrine\DBAL\Connection;
 
 /**
  * @internal

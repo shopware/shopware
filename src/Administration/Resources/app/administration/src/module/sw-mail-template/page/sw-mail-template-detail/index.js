ea76e8e9e4173b3d658e130029edc0fb57c5c073
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2024-10-04 17:38:45 +0200

    #3103 es lint-fix

--- src/Administration/Resources/app/administration/src/module/sw-mail-template/page/sw-mail-template-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-mail-template/page/sw-mail-template-detail/index.js
@@ -115,6 +115,7 @@ export default {
                     });
                     return properties;
                 }
+
                 return completerFunction;
             })(this.entityMappingService, this.mailTemplateType);
         },

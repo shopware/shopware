fc3e02725648c5ace864f16b41fc7693a731aa89
momocode-de <moritz@momocode.de>
2024-10-05 10:07:34 +0200

    NEXT-36826 - Fix the selected customer when creating a new order fixes #4982

--- src/Administration/Resources/app/administration/src/module/sw-order/view/sw-order-create-initial/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/view/sw-order-create-initial/index.js
@@ -4,7 +4,8 @@ import template from './sw-order-create-initial.html.twig';
  * @package checkout
  */
 
-const { State } = Shopware;
+const { State, Data, Service } = Shopware;
+const { Criteria } = Data;
 
 // eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
 export default {
@@ -12,19 +13,53 @@ export default {
 
     compatConfig: Shopware.compatConfig,
 
+    inject: ['feature'],
+
+    computed: {
+        customerRepository() {
+            return Service('repositoryFactory').create('customer');
+        },
+
+        customerCriteria() {
+            const criteria = new Criteria(1, 25);
+            criteria
+                .addAssociation('addresses')
+                .addAssociation('group')
+                .addAssociation('salutation')
+                .addAssociation('salesChannel')
+                .addAssociation('lastPaymentMethod')
+                .addAssociation('defaultBillingAddress.country')
+                .addAssociation('defaultBillingAddress.countryState')
+                .addAssociation('defaultBillingAddress.salutation')
+                .addAssociation('defaultShippingAddress.country')
+                .addAssociation('defaultShippingAddress.countryState')
+                .addAssociation('defaultShippingAddress.salutation')
+                .addAssociation('tags');
+
+            if (!this.feature.isActive('v6.7.0.0')) {
+                criteria.addAssociation('defaultPaymentMethod');
+            }
+
+            return criteria;
+        },
+    },
+
     created() {
         this.createdComponent();
     },
 
     methods: {
-        createdComponent() {
-            const { customer } = this.$route.params;
+        async createdComponent() {
+            const customerId = this.$route.query?.customerId;
 
-            if (!customer) {
+            if (!customerId) {
                 return;
             }
 
-            State.commit('swOrder/setCustomer', customer);
+            const customer = await this.customerRepository.get(customerId, Shopware.Context.api, this.customerCriteria);
+            if (customer) {
+                State.commit('swOrder/setCustomer', customer);
+            }
         },
 
         onCloseCreateModal() {

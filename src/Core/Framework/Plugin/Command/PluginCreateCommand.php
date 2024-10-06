5fba3c8a74b3bf1bfb16575663a59fa0c3689fc2
Raffaele Carele <raffaele.carelle@algoritma.it>
2024-10-06 17:50:32 +0200

    fix coding style

--- src/Core/Framework/Plugin/Command/PluginCreateCommand.php
+++ src/Core/Framework/Plugin/Command/PluginCreateCommand.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
--- src/Storefront/Theme/Command/ThemeCreateCommand.php
+++ src/Storefront/Theme/Command/ThemeCreateCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;

f7a2805bf721e2f60c1d3d3fd6d03aee5840ff8f
Florian Robel <f.robel@shopware.com>
2024-10-14 15:09:04 +0200

    test: Visual test admin navigation-menu

---
+++ tests/acceptance/tests/Visual/VisualAdminNavigation.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@fixtures/AcceptanceTest';
+import path from 'path';
+
+test('Visual: Admin menu in the administration.', { tag: '@Visual' }, async ({
+ShopAdmin,
+AdminDashboard,
+}) => {
+
+    await test.step('Creates a screenshot and compare the admin menu.', async () => {
+
+        await ShopAdmin.goesTo(AdminDashboard.url());
+        await AdminDashboard.page.locator('.sw-catalogue').click();
+        await AdminDashboard.page.locator('.sw-settings').click();
+        await AdminDashboard.page.locator('.sw-dashboard').click();
+        await AdminDashboard.page.locator('.sw-order').click();
+        await AdminDashboard.page.locator('.sw-customer').click();
+        await AdminDashboard.page.locator('.sw-content').click();
+        await AdminDashboard.page.locator('.sw-marketing').click();
+        await AdminDashboard.page.locator('.sw-extension').click();
+        await AdminDashboard.page.locator('.sw-admin-menu__user-actions-indicator').click();
+
+        await AdminDashboard.page.setViewportSize({ width: 1280, height: 2048});
+
+        await expect(AdminDashboard.page.locator('.sw-admin-menu')).toHaveScreenshot({
+            stylePath: path.resolve('./tests/Visual/screenshot.css'),
+            mask: [
+                AdminDashboard.page.locator('.sw-admin-menu__user-name'),
+                AdminDashboard.page.locator('.sw-avatar'),
+            ],
+        });
+    });
+});

268ae2ef2205fbb640bf0512bcc04dd90f2c498b
Orkun Cavli <o.cavli@shopware.com>
2024-10-30 15:28:25 +0100

    NEXT-38356 - remove the version name

--- tests/acceptance/tests/Visual/VisualAdminSettingsShipping.spec.ts
+++ tests/acceptance/tests/Visual/VisualAdminSettingsShipping.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, TestDataService } from '@fixtures/AcceptanceTest';
+import { test, expect } from '@fixtures/AcceptanceTest';
 import path from 'path';
 
 test('Visual: Shipping listing page in administration', { tag: '@Visual' }, async ({
@@ -8,30 +8,17 @@ test('Visual: Shipping listing page in administration', { tag: '@Visual' }, asyn
     Login,
 }) => {
     await TestDataService.createBasicShippingMethod();
-
+    
     await ShopAdmin.attemptsTo(Login());
     await ShopAdmin.goesTo(AdminShippingListing.url());
     await ShopAdmin.expects(AdminShippingListing.header).toBeVisible();
     await ShopAdmin.expects(AdminShippingListing.contextMenu).toBeVisible();
-    await ShopAdmin.expects(AdminShippingListing.firstShippingMethodContextButton).toBeVisible();
-    AdminShippingListing.firstShippingMethodContextButton.click();
-    await ShopAdmin.expects(AdminShippingListing.editButton).toBeVisible();
-    await ShopAdmin.expects(AdminShippingListing.deleteButton).toBeVisible();
-    AdminShippingListing.deleteButton.click();
-    await ShopAdmin.expects(AdminShippingListing.modal).toBeVisible();
-    await ShopAdmin.expects(AdminShippingListing.modalCancelButton).toBeVisible();
-    await ShopAdmin.expects(AdminShippingListing.modalDeleteButton).toBeVisible();
-
-
-
-
 
     await test.step('Creates a screenshot and compare it on shipping listing page in admin.', async () => {
 
         await expect(AdminShippingListing.page).toHaveScreenshot({
             fullPage: true,
             stylePath: path.resolve('./tests/Visual/screenshot.css'),
-
         });
     });
 });

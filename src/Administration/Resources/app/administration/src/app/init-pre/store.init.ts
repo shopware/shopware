c2eb0895a46bf94638d07eb90d4f912a90e3714d
Iván Tajes Vidal <tajespasarela@gmail.com>
2024-11-11 11:28:10 +0100

    NEXT-38630 - Migrate menu-item.store to pinia.

--- src/Administration/Resources/app/administration/src/app/init-pre/store.init.ts
+++ src/Administration/Resources/app/administration/src/app/init-pre/store.init.ts
@@ -15,6 +15,7 @@ import 'src/app/store/settings-item.store';
 import 'src/app/store/shopware-apps.store';
 import 'src/app/store/system.store';
 import 'src/app/store/modals.store';
+import 'src/app/store/menu-item.store';
 
 /**
  * @package admin
--- src/Administration/Resources/app/administration/src/app/state/index.js
+++ src/Administration/Resources/app/administration/src/app/state/index.js
@@ -5,7 +5,6 @@
 import notification from './notification.store';
 import session from './session.store';
 import tabs from './tabs.store';
-import menuItem from './menu-item.store';
 import usageData from './usage-data.store';
 
 // eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
@@ -13,6 +12,5 @@ export default {
     notification,
     session,
     tabs,
-    menuItem,
     usageData,
 };

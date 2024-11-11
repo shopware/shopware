e584df61e2fd9e90773d54481095db069ec62e0e
Iván Tajes Vidal <tajespasarela@gmail.com>
2024-11-11 11:28:10 +0100

    NEXT-38630 - Migrate menu-item.store to pinia.

--- src/Administration/Resources/app/administration/src/app/init-pre/state.init.spec.js
+++ src/Administration/Resources/app/administration/src/app/init-pre/state.init.spec.js
@@ -25,7 +25,6 @@ describe('src/app/init-pre/state.init.ts', () => {
         expect(Shopware.State.get('session')).toBeDefined();
         expect(Shopware.Store.get('shopwareApps')).toBeDefined();
         expect(Shopware.State.get('tabs')).toBeDefined();
-        expect(Shopware.State.get('menuItem')).toBeDefined();
         expect(Shopware.State.get('usageData')).toBeDefined();
     });
 

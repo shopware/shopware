4fc3c61b801575afdb5605c68f8d3857e688cdbc
Jannis Leifeld <j.leifeld@shopware.com>
2024-11-13 13:59:11 +0100

    NEXT-38634 - transition sdk location store to pinia

--- src/Administration/Resources/app/administration/src/meta/baseline.ts
+++ src/Administration/Resources/app/administration/src/meta/baseline.ts
@@ -136,7 +136,6 @@ const missingTests = [
     'src/app/state/modals.store.ts',
     'src/app/state/notification.store.js',
     'src/app/state/rule-conditions-config.store.js',
-    'src/app/state/sdk-location.store.ts',
     'src/app/state/settings-item.store.js',
     'src/app/state/shopware-apps.store.ts',
     'src/app/state/system.store.js',

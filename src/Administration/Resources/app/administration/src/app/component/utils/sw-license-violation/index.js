dd37437aea00a792e95c398a9d9d60edc0389028
Jannis Leifeld <j.leifeld@shopware.com>
2024-11-14 16:25:19 +0100

    NEXT-38627 - Fix useStore error

--- src/Administration/Resources/app/administration/src/app/component/utils/sw-license-violation/index.js
+++ src/Administration/Resources/app/administration/src/app/component/utils/sw-license-violation/index.js
@@ -33,10 +33,13 @@ Shopware.Component.register('sw-license-violation', {
     },
 
     computed: {
-        ...mapState(Shopware.Store.get('licenseViolation'), [
-            'violations',
-            'warnings',
-        ]),
+        ...mapState(
+            () => Shopware.Store.get('licenseViolation'),
+            [
+                'violations',
+                'warnings',
+            ],
+        ),
 
         visible() {
             if (!this.showViolation) {

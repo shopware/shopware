e20547bbd54396e06052353f42e6b774ed6e5a94
Jannis Leifeld <j.leifeld@shopware.com>
2024-11-15 09:45:31 +0100

    NEXT-38640 - Transition tabs stores to pinia

--- src/Administration/Resources/app/administration/src/app/init-pre/store.init.ts
+++ src/Administration/Resources/app/administration/src/app/init-pre/store.init.ts
@@ -16,6 +16,7 @@ import 'src/app/store/shopware-apps.store';
 import 'src/app/store/system.store';
 import 'src/app/store/modals.store';
 import 'src/app/store/menu-item.store';
+import 'src/app/store/tabs.store';
 
 /**
  * @package admin

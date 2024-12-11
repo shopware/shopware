2d6f9966fb5d97d3f9c9b0d289f5117c88d27f4f
Iván Tajes Vidal <tajespasarela@gmail.com>
2024-12-11 10:05:52 +0100

    NEXT-39904 - Migrate swOrderDetail store to pinia.

--- src/Administration/Resources/app/administration/test/_setup/prepare_environment.js
+++ src/Administration/Resources/app/administration/test/_setup/prepare_environment.js
@@ -79,6 +79,7 @@ import '../../src/app/store/usage-data.store';
 import '../../src/app/store/session.store';
 import '../../src/module/sw-category/page/sw-category-detail/store';
 import '../../src/module/sw-extension/store/extensions.store';
+import '../../src/module/sw-order/store/order-detail.store';
 
 // Setup Vue Test Utils configuration
 config.showDeprecationWarnings = true;

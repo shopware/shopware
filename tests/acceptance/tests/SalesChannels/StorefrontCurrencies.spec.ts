a4da92441f028978026646043b592064dabb1ad4
a.okko <14863712+ravenokko@users.noreply.github.com>
2024-12-12 19:00:48 +0100

    NEXT-39289 - changed test currency from GBP to custom generated

--- tests/acceptance/tests/SalesChannels/StorefrontCurrencies.spec.ts
+++ tests/acceptance/tests/SalesChannels/StorefrontCurrencies.spec.ts
@@ -7,7 +7,7 @@ test ('Shop customers should be able to view products in different currencies.',
 }) => {
 
     const salesChannelId = TestDataService.defaultSalesChannel.id;
-    const currency = await TestDataService.getCurrency('GBP');
+    const currency = await TestDataService.createCurrency();
     await TestDataService.assignSalesChannelCurrency(salesChannelId, currency.id);
     
     const product = await TestDataService.createBasicProduct();
@@ -21,8 +21,8 @@ test ('Shop customers should be able to view products in different currencies.',
 
     await test.step('Customer can select a different currency', async () => {
         await StorefrontHome.currenciesDropdown.click();
-        await StorefrontHome.currenciesMenuOptions.getByText('£ GBP').click();
-        await ShopCustomer.expects(StorefrontHome.currenciesDropdown).toContainText('Pound');
-        await ShopCustomer.expects(productListing.productPrice).toContainText('£');
+        await StorefrontHome.currenciesMenuOptions.getByText(currency.shortName).click();
+        await ShopCustomer.expects(StorefrontHome.currenciesDropdown).toContainText(currency.name);
+        await ShopCustomer.expects(productListing.productPrice).toContainText(currency.isoCode);
     });
 })

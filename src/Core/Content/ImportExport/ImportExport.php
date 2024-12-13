26335c859b74d84e204e143eefd4e8af31466306
Andrii Havryliuk <a.havryliuk@shopware.com>
2024-12-13 17:35:52 +0100

    NEXT-39353 - removed usages of setNestTransactionsWithSavepoints, part 1

--- src/Core/Content/ImportExport/ImportExport.php
+++ src/Core/Content/ImportExport/ImportExport.php
@@ -105,7 +105,6 @@ class ImportExport
         $context->addState(Context::SKIP_TRIGGER_FLOW);
 
         if ($this->logEntity->getActivity() === ImportExportLogEntity::ACTIVITY_DRYRUN) {
-            $this->connection->setNestTransactionsWithSavepoints(true);
             $this->connection->beginTransaction();
         }
 

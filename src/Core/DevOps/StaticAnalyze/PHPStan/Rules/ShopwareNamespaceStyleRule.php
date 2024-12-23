904f77c6a87e57a464e5debc3a0c792ceeca295e
Nicolas Fortier <n.fortier@shopware.com>
2024-12-23 15:04:41 +0100

    NEXT-39832 - replace getLine by getStartLine (deprecation)

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/ShopwareNamespaceStyleRule.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/ShopwareNamespaceStyleRule.php
@@ -44,7 +44,7 @@ class ShopwareNamespaceStyleRule implements Rule
         if (\count($namespaceParts) > 0 && $namespaceParts[0] !== 'Shopware') {
             return [
                 RuleErrorBuilder::message('Namespace must start with Shopware')
-                    ->line($namespaceNode->getLine())
+                    ->line($namespaceNode->getStartLine())
                     ->identifier('shopware.namespace')
                     ->build(),
             ];
@@ -57,7 +57,7 @@ class ShopwareNamespaceStyleRule implements Rule
         if ($namespaceParts[2] === 'Command') {
             return [
                 RuleErrorBuilder::message('No global Command directories allowed, put your commands in the right domain directory')
-                    ->line($namespaceNode->getLine())
+                    ->line($namespaceNode->getStartLine())
                     ->identifier('shopware.namespace')
                     ->build(),
             ];
@@ -66,7 +66,7 @@ class ShopwareNamespaceStyleRule implements Rule
         if ($namespaceParts[2] === 'Exception') {
             return [
                 RuleErrorBuilder::message('No global Exception directories allowed, put your exceptions in the right domain directory')
-                    ->line($namespaceNode->getLine())
+                    ->line($namespaceNode->getStartLine())
                     ->identifier('shopware.namespace')
                     ->build(),
             ];

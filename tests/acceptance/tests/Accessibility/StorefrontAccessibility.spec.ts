6bf23c07462aba63ff0995b011a1f69773e2db07
Yusuf  Tür <yusufttur@gmail.com>
2024-12-23 15:28:47 +0100

    add ticket to storefront acccessibility

--- tests/acceptance/tests/Accessibility/StorefrontAccessibility.spec.ts
+++ tests/acceptance/tests/Accessibility/StorefrontAccessibility.spec.ts
@@ -21,7 +21,7 @@ test('The Storefront should implement accessibility best practices.', { tag: '@A
     InstanceMeta,
 }) => {
 
-    test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0');
+    test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0, ticket: https://shopware.atlassian.net/browse/NEXT-40163');
 
     test.slow();
 

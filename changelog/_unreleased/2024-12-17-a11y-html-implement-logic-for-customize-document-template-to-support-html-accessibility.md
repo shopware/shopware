7a52e95838847385a76ff4b5c76d6b3e4bb90001
YTN <nguyenytran06@gmail.com>
2025-01-03 19:30:09 +0700

    feat: [A11y-HTML] Implement logic (#5975)

--- changelog/_unreleased/2024-12-17-a11y-html-implement-logic-for-customize-document-template-to-support-html-accessibility.md
+++ changelog/_unreleased/2024-12-17-a11y-html-implement-logic-for-customize-document-template-to-support-html-accessibility.md
@@ -1,18 +1,23 @@
 ---
-title: [A11y-HTML] Implement Logic for customize Document Template to Support HTML Accessibility
-issue: NEXT-40063
+title: [A11y-HTML] Offer HTML alternative to our pdf standard documents
+issue: NEXT-40059
 ---
 # Core
-* Changed some files to call `RenderedDocument::setHtmlA11y` to set the HTML accessibility content for the rendered document.
+* Changed some files to call `RenderedDocument::setTemplateOptions` to provide template options to render document template.
   * `Shopware\Core\Checkout\Document\Renderer\CreditNoteRenderer::render`
   * `Shopware\Core\Checkout\Document\Renderer\DeliveryNoteRenderer::render`
   * `Shopware\Core\Checkout\Document\Renderer\InvoiceRenderer::render`
   * `Shopware\Core\Checkout\Document\Renderer\StornoRenderer::render`
-* Added parameter `htmlA11y` in `Shopware\Core\Checkout\Document\Renderer\RenderedDocument` to the provide The HTML accessibility content for the rendered document.
+* Added parameter `templateOptions` in `Shopware\Core\Checkout\Document\Renderer\RenderedDocument` to the provide the config to render template.
 * Changed `Shopware\Core\Checkout\Document\SalesChannel\DocumentRoute::download` to add the `fileType` configuration to the `DocumentGenerator`.
 * Changed `Shopware\Core\Checkout\Document\Service\DocumentGenerator::readDocument` to load the media based on `fileType`.
 * Changed `Shopware\Core\Checkout\Document\Service\DocumentGenerator::generate` to save `documentMediaFileIds`.
 * Changed `Shopware\Core\Checkout\Document\Service\DocumentGenerator::preview` to set the content based on `fileType`.
+* Added `Shopware\Core\Checkout\Document\Service\DocumentFileRendererRegistry` to callable render by contentType.
+* Added `Shopware\Core\Checkout\Document\Service\HtmlRenderer` to render the document file.
+* Changed `Shopware\Core\Checkout\Document\Service\PdfRenderer` to add the function `templateRenderer` to add document template. 
+* Changed `Shopware\Core\Checkout\Document\Controller\DocumentController::downloadDocuments` to add the attribute `fileType` to the request.
+* Changed `Shopware\Core\Checkout\Document\Service\DocumentMerger::merge` to implement a case zip archive to merge documents.
 ___
 # Administration
 * Changed method `getDocumentPreview` in `document.api.service` service to add the `fileType` like <html or pdf> attributes.
--- src/Core/Checkout/Document/Service/PdfRenderer.php
+++ src/Core/Checkout/Document/Service/PdfRenderer.php
@@ -7,11 +7,14 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 use Shopware\Core\Checkout\Document\Extension\PdfRendererExtension;
 use Shopware\Core\Checkout\Document\Renderer\RenderedDocument;
+use Shopware\Core\Checkout\Document\Twig\DocumentTemplateRenderer;
 use Shopware\Core\Framework\Extensions\ExtensionDispatcher;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 
 #[Package('checkout')]
-final class PdfRenderer
+final class PdfRenderer extends AbstractDocumentTypeRenderer
 {
     public const FILE_EXTENSION = 'pdf';
 
@@ -24,6 +27,7 @@ final class PdfRenderer
      */
     public function __construct(
         private readonly array $dompdfOptions,
+        private readonly DocumentTemplateRenderer $documentTemplateRenderer,
         private readonly ExtensionDispatcher $extensions
     ) {
     }
@@ -42,6 +46,11 @@ final class PdfRenderer
         );
     }
 
+    public function getDecorated(): AbstractDocumentTypeRenderer
+    {
+        throw new DecorationPatternException(self::class);
+    }
+
     private function _render(RenderedDocument $document): string
     {
         $dompdf = new Dompdf();
@@ -50,7 +59,7 @@ final class PdfRenderer
 
         $dompdf->setOptions($options);
         $dompdf->setPaper($document->getPageSize(), $document->getPageOrientation());
-        $dompdf->loadHtml($document->getHtml());
+        $dompdf->loadHtml($this->getHtml($document->getTemplateOptions(), $document->getHtml()));
 
         /*
          * Dompdf creates and destroys a lot of objects. The garbage collector slows the process down by ~50% for
@@ -70,7 +79,32 @@ final class PdfRenderer
             gc_enable();
         }
 
+        if (Feature::isActive('v6.7.0.0')) {
+            $document->setContentType(self::FILE_CONTENT_TYPE);
+            $document->setFileExtension(self::FILE_EXTENSION);
+        }
+
         return (string) $dompdf->output();
+    }
+
+    /**
+     * @decrecated tag:v6.7.0 - reason:parameter-change - $html will be removed
+     *
+     * @param array<mixed> $options
+     */
+    private function getHtml(array $options, string $html = ''): string
+    {
+        if (!Feature::isActive('v6.7.0.0')) {
+            return $html;
+        }
+
+        if (empty($options)) {
+            return '';
+        }
+
+        return $this->documentTemplateRenderer->render(
+            ...$options,
+        );
     }
 
     /**

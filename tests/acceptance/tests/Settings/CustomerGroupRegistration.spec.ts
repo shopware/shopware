a1cb2caec36116c7f929b98ebbafbac92b59e370
Yusuf  Tür <yusufttur@gmail.com>
2025-01-03 15:46:18 +0100

    fix: Unskip and add package-lock

--- tests/acceptance/tests/Settings/CustomerGroupRegistration.spec.ts
+++ tests/acceptance/tests/Settings/CustomerGroupRegistration.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '@fixtures/AcceptanceTest';
-import console from 'console';
+
 test('As an admin, I can create and verify customer groups in the admin.', { tag: '@CustomerGroups' }, async ({
     TestDataService,
     ShopAdmin,
@@ -41,11 +41,8 @@ test('As a customer, I must be able to register under a customer group in the St
     IdProvider,
     Register,
     CustomerGroupActivation,
-    InstanceMeta,
 }) => {
 
-    // test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0, ticket: https://shopware.atlassian.net/browse/NEXT-40163');
-    console.log(InstanceMeta.version);
     const customer = { email: IdProvider.getIdPair().uuid + '@test.com' };
     const customerGroup = await TestDataService.createCustomerGroup();
 
@@ -75,10 +72,7 @@ test('As a commercial customer, I must be able to register under a customer grou
     IdProvider,
     Register,
     CustomerGroupActivation,
-    InstanceMeta,
 }) => {
-
-    // test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0, ticket: https://shopware.atlassian.net/browse/NEXT-40163');
 
     const uuid = IdProvider.getIdPair().uuid;
     const customer = { email: uuid + '@test.com', vatRegNo: uuid + '-VatId'};

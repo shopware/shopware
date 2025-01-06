18b560ab8be55e818c52d4000cfd117267fe25e8
Fabian Boensch <f.boensch@shopware.com>
2025-01-06 12:37:35 +0100

    fix namespace

--- tests/migration/Core/V6_6/Migration1736154963FixCyprusVatIdPatternTest.php
+++ tests/migration/Core/V6_6/Migration1736154963FixCyprusVatIdPatternTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace migration\Core\V6_6;
+namespace Shopware\Tests\Migration\Core\V6_6;
 
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Attributes\CoversClass;

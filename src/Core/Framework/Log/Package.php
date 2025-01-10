e5da21fc1b5078ab3c7130170acaf32d4f39f9d9
akf-bw <benjamin.wittwer@a-k-f.de>
2025-01-10 10:57:02 +0100

    Fix b2b package phpstan-type

--- src/Core/Framework/Log/Package.php
+++ src/Core/Framework/Log/Package.php
@@ -5,7 +5,7 @@ namespace Shopware\Core\Framework\Log;
 /**
  * @internal
  *
- * @phpstan-type PackageString 'buyers-experience'|'services-settings'|'inventory'|'content'|'sales-channel'|'customer-order'|'checkout'|'merchant-services'|'storefront'|'core'|'administration'|'data-services'|'innovation'|'discovery'|b2b|'fundamentals@discovery'
+ * @phpstan-type PackageString 'buyers-experience'|'services-settings'|'inventory'|'content'|'sales-channel'|'customer-order'|'checkout'|'merchant-services'|'storefront'|'core'|'administration'|'data-services'|'innovation'|'discovery'|'b2b'|'fundamentals@discovery'
  */
 #[\Attribute(\Attribute::TARGET_CLASS)]
 #[Package('core')]

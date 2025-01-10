3255cc5bc89cf5a97fd882968927160fd350d097
Aydin Hassan <a.hassan@shopware.com>
2025-01-10 15:00:22 +0100

    feat: Service webhook retry strategy

--- src/Core/Service/Service.php
+++ src/Core/Service/Service.php
@@ -4,7 +4,9 @@ namespace Shopware\Core\Service;
 
 use Shopware\Core\Framework\Bundle;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Service\DependencyInjection\CompilerPass\MessengerCompilerPass;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
@@ -24,5 +26,7 @@ class Service extends Bundle
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection'));
         $loader->load('services.xml');
         parent::build($container);
+
+        $container->addCompilerPass(new MessengerCompilerPass(), PassConfig::TYPE_BEFORE_REMOVING);
     }
 }

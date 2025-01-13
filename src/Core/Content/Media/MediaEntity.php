a1d79f143d3d8026172a0cfbcaafdcba686b989b
YTN <nguyenytran06@gmail.com>
2025-01-13 12:05:43 +0700

    feat: [A11y-HTML] implement multiple media for document (#6079)

--- src/Core/Content/Media/MediaEntity.php
+++ src/Core/Content/Media/MediaEntity.php
@@ -3,6 +3,7 @@
 namespace Shopware\Core\Content\Media;
 
 use Shopware\Core\Checkout\Document\Aggregate\DocumentBaseConfig\DocumentBaseConfigCollection;
+use Shopware\Core\Checkout\Document\Aggregate\DocumentMedia\DocumentMediaCollection;
 use Shopware\Core\Checkout\Document\DocumentCollection;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemCollection;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItemDownload\OrderLineItemDownloadCollection;
@@ -322,6 +323,8 @@ class MediaEntity extends Entity
     protected ?ProductDownloadCollection $productDownloads = null;
 
     protected ?OrderLineItemDownloadCollection $orderLineItemDownloads = null;
+
+    protected ?DocumentMediaCollection $documentMediaFiles = null;
 
     /**
      * @experimental stableVersion:v6.8.0 feature:SPATIAL_BASES
@@ -836,5 +839,15 @@ class MediaEntity extends Entity
     public function isSpatialObject(): bool
     {
         return $this->mediaType instanceof SpatialObjectType;
+    }
+
+    public function getDocumentMediaFiles(): ?DocumentMediaCollection
+    {
+        return $this->documentMediaFiles;
+    }
+
+    public function setDocumentMediaFiles(DocumentMediaCollection $mediaCollection): void
+    {
+        $this->documentMediaFiles = $mediaCollection;
     }
 }

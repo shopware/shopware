f89a66f52d2b2fa5bd5948e23285b686532fb141
Dennis Garding <d.garding@shopware.com>
2025-01-14 08:04:30 +0100

    chore: Change package annotation for FirstRunWizard to after-sales

--- src/Administration/Resources/app/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-defaults/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-first-run-wizard/view/sw-first-run-wizard-defaults/index.js
@@ -2,7 +2,8 @@ import template from './sw-first-run-wizard-defaults.html.twig';
 import './sw-first-run-wizard-defaults.scss';
 
 /**
- * @package checkout
+ * @sw-package fundamentals@after-sales
+ *
  * @private
  */
 export default {

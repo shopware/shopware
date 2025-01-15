4d01884094740c99ca528cec76f33e8783884ab4
Jonas Elfering <j.elfering@shopware.com>
2025-01-15 13:36:20 +0100

    Don't generate new ID for non PK id fields

--- tests/unit/Core/Framework/DataAbstractionLayer/FieldSerializer/IdFieldSerializerTest.php
+++ tests/unit/Core/Framework/DataAbstractionLayer/FieldSerializer/IdFieldSerializerTest.php
@@ -34,21 +34,11 @@ class IdFieldSerializerTest extends TestCase
     #[DataProvider('idShouldNotBeGeneratedCasesProvider')]
     public function testSerializerAcceptsValue(Field $field, ?string $value, ?string $expected): void
     {
-        $validator = $this->createMock(ValidatorInterface::class);
-
-        $serializer = new IdFieldSerializer(
-            $validator,
-            new StaticDefinitionInstanceRegistry([], $validator, $this->createMock(EntityWriteGatewayInterface::class))
-        );
+        $serializer = $this->getIdFieldSerializer();
 
         $existence = EntityExistence::createEmpty();
         $kv = new KeyValuePair($field->getPropertyName(), $value, true);
-        $params = new WriteParameterBag(
-            new ProductDefinition(),
-            WriteContext::createFromContext(Context::createDefaultContext()),
-            '',
-            new WriteCommandQueue()
-        );
+        $params = $this->getWriteParameterBag();
 
         $encoded = iterator_to_array($serializer->encode($field, $existence, $kv, $params));
 
@@ -89,21 +79,11 @@ class IdFieldSerializerTest extends TestCase
     #[DataProvider('idShouldBeGeneratedCasesProvider')]
     public function testSerializerGeneratesValueWhenNullIsPassed(Field $field): void
     {
-        $validator = $this->createMock(ValidatorInterface::class);
-
-        $serializer = new IdFieldSerializer(
-            $validator,
-            new StaticDefinitionInstanceRegistry([], $validator, $this->createMock(EntityWriteGatewayInterface::class))
-        );
+        $serializer = $this->getIdFieldSerializer();
 
         $existence = EntityExistence::createEmpty();
         $kv = new KeyValuePair($field->getPropertyName(), null, true);
-        $params = new WriteParameterBag(
-            new ProductDefinition(),
-            WriteContext::createFromContext(Context::createDefaultContext()),
-            '',
-            new WriteCommandQueue()
-        );
+        $params = $this->getWriteParameterBag();
 
         $encoded = iterator_to_array($serializer->encode($field, $existence, $kv, $params));
 
@@ -125,5 +105,82 @@ class IdFieldSerializerTest extends TestCase
         yield 'field is pk and required and not explicitly passed' => [
             'field' => (new IdField('media_id', 'mediaId'))->addFlags(new PrimaryKey(), new Required()),
         ];
+    }
+
+    public function testNormalizeForPrimaryKeyWithProvidedValue(): void
+    {
+        $serializer = $this->getIdFieldSerializer();
+        $params = $this->getWriteParameterBag();
+
+        $field = (new IdField('media_id', 'mediaId'))->addFlags(new PrimaryKey());
+        $data = ['mediaId' => Uuid::randomHex()];
+
+        $normalized = $serializer->normalize($field, $data, $params);
+
+        static::assertSame($data, $normalized);
+        static::assertSame($data['mediaId'], $params->getContext()->get('product', 'mediaId'));
+    }
+
+    public function testNormalizeForPrimaryKeyGeneratesIdIfNotPassed(): void
+    {
+        $serializer = $this->getIdFieldSerializer();
+        $params = $this->getWriteParameterBag();
+
+        $field = (new IdField('media_id', 'mediaId'))->addFlags(new PrimaryKey());
+        $data = [];
+
+        $normalized = $serializer->normalize($field, $data, $params);
+
+        static::assertArrayHasKey('mediaId', $normalized);
+        static::assertTrue(Uuid::isValid($normalized['mediaId']));
+        static::assertSame($normalized['mediaId'], $params->getContext()->get('product', 'mediaId'));
+    }
+
+    public function testNormalizeForNonPrimaryKeyWithProvidedValue(): void
+    {
+        $serializer = $this->getIdFieldSerializer();
+        $params = $this->getWriteParameterBag();
+
+        $field = new IdField('media_id', 'mediaId');
+        $data = ['mediaId' => Uuid::randomHex()];
+
+        $normalized = $serializer->normalize($field, $data, $params);
+
+        static::assertSame($data, $normalized);
+        static::assertFalse($params->getContext()->has('product', 'mediaId'));
+    }
+
+    public function testNormalizeForNonPrimaryKeyDoesNotGenerateValue(): void
+    {
+        $serializer = $this->getIdFieldSerializer();
+        $params = $this->getWriteParameterBag();
+
+        $field = new IdField('media_id', 'mediaId');
+        $data = [];
+
+        $normalized = $serializer->normalize($field, $data, $params);
+
+        static::assertSame($data, $normalized);
+        static::assertFalse($params->getContext()->has('product', 'mediaId'));
+    }
+
+    private function getIdFieldSerializer(): IdFieldSerializer
+    {
+        $validator = $this->createMock(ValidatorInterface::class);
+
+        return new IdFieldSerializer(
+            $validator,
+            new StaticDefinitionInstanceRegistry([], $validator, $this->createMock(EntityWriteGatewayInterface::class))
+        );
+    }
+
+    private function getWriteParameterBag(): WriteParameterBag
+    {
+        return new WriteParameterBag(
+            new ProductDefinition(),
+            WriteContext::createFromContext(Context::createDefaultContext()),
+            '',
+            new WriteCommandQueue()
+        );
     }
 }

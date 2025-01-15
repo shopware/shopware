7d634a708adf155bff4100e8f912501420b48db1
Yusuf  Tür <yusufttur@gmail.com>
2025-01-15 14:31:06 +0100

    fix: add again todo

--- tests/acceptance/tests/Account/CommercialCustomerRegistration.spec.ts
+++ tests/acceptance/tests/Account/CommercialCustomerRegistration.spec.ts
@@ -59,6 +59,7 @@ test('As a new customer, I should not be able to register as a commercial custom
 }) => {
     test.skip(InstanceMeta.isSaaS, 'This test is incompatible with SaaS');
     test.skip(InstanceMeta.features['V6_7_0_0'], 'This test has a bug: https://shopware.atlassian.net/browse/NEXT-40297');
+    // TODO: Use a new country created by the TestDataService after this bug is fixed: https://shopware.atlassian.net/browse/NEXT-40285
 
     await TestDataService.setSystemConfig({ 'core.loginRegistration.showAccountTypeSelection': true });
     const country = await TestDataService.getCountryId('DE');

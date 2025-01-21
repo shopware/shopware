712486cb8ea327fed6efea3e6d866943e0c914dc
BrocksiNet <b.meyer@shopware.com>
2025-01-21 14:32:08 +0100

    feat: add UnitTest for addMacroResultCall

--- tests/unit/Core/Framework/Adapter/Twig/TwigEnvironmentTest.php
+++ tests/unit/Core/Framework/Adapter/Twig/TwigEnvironmentTest.php
@@ -4,6 +4,7 @@ namespace Shopware\Tests\Unit\Core\Framework\Adapter\Twig;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Adapter\Twig\TokenParser\ReturnNodeTokenParser;
 use Shopware\Core\Framework\Adapter\Twig\TwigEnvironment;
 use Twig\Loader\ArrayLoader;
 use Twig\Source;
@@ -22,5 +23,26 @@ class TwigEnvironmentTest extends TestCase
 
         static::assertStringContainsString('use Shopware\Core\Framework\Adapter\Twig\SwTwigFunction;', $code);
         static::assertStringContainsString('SwTwigFunction::getAttribute', $code);
+    }
+
+    public function testAddMacroResultCall(): void
+    {
+        $templateContent = <<<TWIG
+{% macro example_macro() %}
+    {% set foo = 'bar' %}
+    {% return foo %}
+{% endmacro %}
+{{ _self.example_macro() }}
+TWIG;
+
+        $twig = new TwigEnvironment(new ArrayLoader(['template' => $templateContent]));
+        $twig->addTokenParser(new ReturnNodeTokenParser());
+        $source = new Source($templateContent, 'template');
+        $stream = $twig->tokenize($source);
+        $nodes = $twig->parse($stream);
+
+        $code = $twig->compile($nodes);
+
+        static::assertStringContainsString('yield SwTwigFunction::$macroResult;', $code);
     }
 }

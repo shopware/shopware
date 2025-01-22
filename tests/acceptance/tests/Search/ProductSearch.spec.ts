fda87ec2fbb6b900a39cde03f4b71f982d291057
Orkun Cavli <o.cavli@shopware.com>
2025-01-22 10:28:29 +0100

    test: add new verifications

--- tests/acceptance/tests/Search/ProductSearch.spec.ts
+++ tests/acceptance/tests/Search/ProductSearch.spec.ts
@@ -7,8 +7,8 @@ test ('Customer is able to search products in shop', {tag: '@Search'}, async ({
     StorefrontSearchSuggest,
     SalesChannelBaseConfig,
 }) => {
-    const product = await TestDataService.createBasicProduct({
-        name: 'Shopware',
+    const product1 = await TestDataService.createBasicProduct({
+        name: 'Bottle',
         price: [{
             currencyId: SalesChannelBaseConfig.defaultCurrencyId,
             gross: 10.00,
@@ -16,27 +16,42 @@ test ('Customer is able to search products in shop', {tag: '@Search'}, async ({
             net: 7.55,
         }],
     });
-    const productListing = await StorefrontHome.getListingItemByProductId(product.id);
-
+    const product2 = await TestDataService.createBasicProduct({
+        name: 'Bowl',
+        price: [{
+            currencyId: SalesChannelBaseConfig.defaultCurrencyId,
+            gross: 10.00,
+            linked: false,
+            net: 7.55,
+        }],
+    });
 
     await ShopCustomer.goesTo(StorefrontHome.url());
 
     await test.step('Customer tries search with invalid input', async () => {
-        await StorefrontSearchSuggest.searchInput.fill('Sf');
+        await StorefrontSearchSuggest.searchInput.fill('Be');
         await ShopCustomer.expects(StorefrontSearchSuggest.searchSuggestNoResult).toBeVisible();
     });
 
     await test.step('Customer tries search with valid input', async () => {
-        await StorefrontSearchSuggest.searchInput.fill('Sh');
-        await ShopCustomer.expects(StorefrontSearchSuggest.searchSuggestLineItemName).toHaveText(product.name);
-        await ShopCustomer.expects(StorefrontSearchSuggest.searchSuggestLineItemPrice).toContainText('€10.00*');
+        await StorefrontSearchSuggest.searchInput.fill('Bo');
+
+        const totalCount = await StorefrontSearchSuggest.getTotalSearchResultCount();
+        await ShopCustomer.expects(totalCount).toBe(2);
+
+        await StorefrontSearchSuggest.searchInput.fill('Bow');
+        await ShopCustomer.expects(StorefrontSearchSuggest.searchResultTotal).toBeVisible();
+        await ShopCustomer.expects(totalCount).toBe(1);
+
     
     });
 
     await test.step('Customer navigates to result page and wants to see all result', async () => {
         await StorefrontSearchSuggest.searchSuggestTotalLink.click();
-        await ShopCustomer.expects(StorefrontSearchSuggest.searchHeadline).toContainText('Sh');
-        await ShopCustomer.expects(productListing.productName).toHaveText(product.name);
-        await ShopCustomer.expects(productListing.productPrice).toContainText('€10.00*');     
+        await ShopCustomer.expects(StorefrontSearchSuggest.searchHeadline).toContainText('Bow');
+
+        const productListing2 = await StorefrontSearchSuggest.getListingItemByProductId(product2.id);
+        await ShopCustomer.expects(productListing2.productName).toHaveText(product2.name);
+        await ShopCustomer.expects(productListing2.productPrice).toContainText('€10.00*');     
     });
 });

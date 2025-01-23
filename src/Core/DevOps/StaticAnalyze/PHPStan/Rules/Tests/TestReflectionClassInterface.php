2984dad05186eeffed25c1f2e798c8313bc0e37a
Nicolas Fortier <n.fortier@shopware.com>
2025-01-23 10:11:17 +0100

    TestReflectionClassInterface is only used in the helper and its tests, to be able to mock and resolve it

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/TestReflectionClassInterface.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/TestReflectionClassInterface.php
@@ -4,6 +4,11 @@ namespace Shopware\Core\DevOps\StaticAnalyze\PHPStan\Rules\Tests;
 
 /**
  * @internal
+ *
+ * This interface is used to mock the PHPstan ClassReflection class in the test cases.
+ * Their class is final and cannot be mocked directly.
+ *
+ * @see Shopware\Tests\DevOps\Core\DevOps\StaticAnalyse\PHPStan\Rules\Tests\TestRuleHelperTest
  */
 interface TestReflectionClassInterface
 {

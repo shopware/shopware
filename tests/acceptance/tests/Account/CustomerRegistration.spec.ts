975c599ed70101a951aec2b52225bcf4243bd29e
Yusuf  Tür <yusufttur@gmail.com>
2025-01-24 10:23:16 +0100

    test: add postal code required for a country

--- tests/acceptance/tests/Account/CustomerRegistration.spec.ts
+++ tests/acceptance/tests/Account/CustomerRegistration.spec.ts
@@ -13,3 +13,19 @@ test('As a new customer, I must be able to register in the Storefront.', { tag: 
     await ShopCustomer.attemptsTo(Register(customer));
     await ShopCustomer.expects(StorefrontAccount.page.getByText(customer.email, { exact: true })).toBeVisible();
 });
+
+test('As a new customer, I should not be able to register with empty postal code when it is required', { tag: '@Registration' }, async ({
+    ShopCustomer,
+    StorefrontAccountLogin,
+    Register,
+    TestDataService,
+    DefaultSalesChannel,
+}) => {
+    const country = await TestDataService.createCountry({ postalCodeRequired: true });
+    await TestDataService.assignSalesChannelCountry(DefaultSalesChannel.salesChannel.id, country.id);
+    const customer = { country: country.name, postalCode: '' };
+    await ShopCustomer.goesTo(StorefrontAccountLogin.url());
+    await ShopCustomer.attemptsTo(Register(customer));
+    await ShopCustomer.expects(StorefrontAccountLogin.postalCodeInput).toHaveCSS('border-color', 'rgb(194, 0, 23)');
+    await ShopCustomer.expects(StorefrontAccountLogin.page.getByText('I\'m a new customer!')).toBeVisible();
+});

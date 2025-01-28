3ab22765dc721844541771d5d9b85b76ec656b4a
Jonas Elfering <j.elfering@shopware.com>
2025-01-28 11:25:14 +0100

    Specify private key type in AccessToken

--- src/Core/Framework/Api/OAuth/AccessToken.php
+++ src/Core/Framework/Api/OAuth/AccessToken.php
@@ -13,6 +13,8 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('framework')]
 class AccessToken implements AccessTokenEntityInterface
 {
+    private FakeCryptKey $privateKey;
+
     use AccessTokenTrait;
     use EntityTrait;
     use RefreshTokenTrait;
@@ -79,11 +81,6 @@ class AccessToken implements AccessTokenEntityInterface
 
     public function initJwtConfiguration(): void
     {
-        if (!$this->privateKey instanceof FakeCryptKey) {
-            $jwtConfig = JWTConfigurationFactory::createJWTConfiguration();
-            $this->privateKey = new FakeCryptKey($jwtConfig);
-        }
-
         $this->jwtConfiguration = $this->privateKey->configuration;
     }
 }

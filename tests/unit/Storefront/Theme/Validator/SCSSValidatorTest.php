3a4e1f8cada390a81aa64c3cbb86963f1b752dfb
Philipp Schuch <p.schuch@shopware.com>
2025-01-30 09:45:10 +0100

    NEXT-40385 - Allow empty theme config fields
    Fixes #6359

--- tests/unit/Storefront/Theme/Validator/SCSSValidatorTest.php
+++ tests/unit/Storefront/Theme/Validator/SCSSValidatorTest.php
@@ -21,7 +21,7 @@ class SCSSValidatorTest extends TestCase
      * @param array<string, string> $data
      */
     #[DataProvider('sanitizeDataProvider')]
-    public function testValidateSanitize(array $data, string $expected): void
+    public function testValidateSanitize(array $data, ?string $expected): void
     {
         $returned = SCSSValidator::validate(new ScssPhpCompiler(), $data, [], true);
 
@@ -32,7 +32,7 @@ class SCSSValidatorTest extends TestCase
      * @param array<string, string> $data
      */
     #[DataProvider('validateDataProvider')]
-    public function testValidateNoSanitize(array $data, string $expected, bool $throwsException = false): void
+    public function testValidateNoSanitize(array $data, ?string $expected, bool $throwsException = false): void
     {
         if ($throwsException) {
             self::expectException(ThemeException::class);
@@ -173,7 +173,28 @@ class SCSSValidatorTest extends TestCase
             ],
             'rgba(4, 4, 4, 0.5)',
         ];
-        // incorrect but valid(no error in compilation)
+        // Empty values (are valid and will be set to null)
+        yield 'color empty' => [
+            [
+                'type' => 'color',
+                'value' => '',
+            ],
+            null,
+        ];
+        yield 'color value missing' => [
+            [
+                'type' => 'color',
+            ],
+            null,
+        ];
+        yield 'font family empty' => [
+            [
+                'type' => 'fontFamily',
+                'value' => '',
+            ],
+            null,
+        ];
+        // incorrect but valid (no error in compilation)
         yield 'color incorrect but valid hex 3' => [
             [
                 'type' => 'color',
@@ -529,6 +550,27 @@ class SCSSValidatorTest extends TestCase
                 'value' => 'rgba(4, 4, 4, 5)',
             ],
             '#040404',
+        ];
+        // Empty values (are valid and will be set to null)
+        yield 'color empty' => [
+            [
+                'type' => 'color',
+                'value' => '',
+            ],
+            null,
+        ];
+        yield 'color value missing' => [
+            [
+                'type' => 'color',
+            ],
+            null,
+        ];
+        yield 'font family empty' => [
+            [
+                'type' => 'fontFamily',
+                'value' => '',
+            ],
+            null,
         ];
         // Incorrect and sanitized
         yield 'color incorrect name' => [

af2384ccf7a4cfc8fb80178b41196bfa97f07979
Philipp Schuch <p.schuch@shopware.com>
2025-01-31 08:07:54 +0100

    NEXT-40385 - Fix ThemeCompilerTest

--- tests/unit/Storefront/Theme/ThemeCompilerTest.php
+++ tests/unit/Storefront/Theme/ThemeCompilerTest.php
@@ -406,9 +406,9 @@ $sw-asset-theme-url: \'http://localhost\';
 
 \$theme-id: themeId;
 \$sw-zero-margin: 0;
-\$sw-null-margin: 0;
-\$sw-unset-margin: 0;
-\$sw-empty-margin: 0;
+\$sw-null-margin: null;
+\$sw-unset-margin: null;
+\$sw-empty-margin: null;
 \$sw-asset-theme-url: 'http://localhost';
 
 PHP_EOL,

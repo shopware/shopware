7bd3f9dd4b1d716ee71667b6d626ed31ece57f2b
Orkun Cavli <o.cavli@shopware.com>
2025-02-03 08:56:08 +0100

    test: review feedback and assertion after adding to cart

--- tests/acceptance/tests/Settings/WishlistOnListingPage.spec.ts
+++ tests/acceptance/tests/Settings/WishlistOnListingPage.spec.ts
@@ -5,7 +5,8 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
     ShopCustomer,
     StorefrontHome,
     AddProductToWishlist,
-    Login, RemoveProductFromWishlist,
+    Login,
+    RemoveProductFromWishlist,
     StorefrontWishlist,
     AddProductToCartFromWishlist,
 }) => {
@@ -15,17 +16,17 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
     const product2 = await TestDataService.createBasicProduct();
     const product3 = await TestDataService.createBasicProduct();
 
-    const product1Data = await StorefrontHome.getListingItemByProductId(product1.id);
-    const product2Data = await StorefrontHome.getListingItemByProductId(product2.id);
-    const product3Data = await StorefrontHome.getListingItemByProductId(product3.id);
+    const product1Locators = await StorefrontHome.getListingItemByProductId(product1.id);
+    const product2Locators = await StorefrontHome.getListingItemByProductId(product2.id);
+    const product3Locators = await StorefrontHome.getListingItemByProductId(product3.id);
 
     await test.step('Add to products to the wishlist and check the basket count', async () => {
         await ShopCustomer.attemptsTo(Login());
         await ShopCustomer.goesTo(StorefrontHome.url());
         await ShopCustomer.attemptsTo(AddProductToWishlist(product1));
-        await ShopCustomer.expects(product1Data.wishlistAddedIcon).toBeVisible();
+        await ShopCustomer.expects(product1Locators.wishlistAddedIcon).toBeVisible();
         await ShopCustomer.attemptsTo(AddProductToWishlist(product2));
-        await ShopCustomer.expects(product2Data.wishlistAddedIcon).toBeVisible();
+        await ShopCustomer.expects(product2Locators.wishlistAddedIcon).toBeVisible();
         await ShopCustomer.attemptsTo(AddProductToWishlist(product3));
         await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('3');
     });
@@ -33,7 +34,7 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
     await test.step('Remove a product from the wishlist and check the basket count', async () => {
         await ShopCustomer.attemptsTo(RemoveProductFromWishlist(product3));
         await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('2');
-        await ShopCustomer.expects(product3Data.wishlistNotAddedIcon).toBeVisible();
+        await ShopCustomer.expects(product3Locators.wishlistNotAddedIcon).toBeVisible();
     });
 
     await test.step('Navigate to wishlist and verify the last added product is the fist in listing', async () => {
@@ -41,12 +42,15 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
         await ShopCustomer.expects(StorefrontWishlist.wishlistIcon).toBeVisible();
         const firstProductInListing = StorefrontWishlist.productListItems.first();
         const firstProductName = await firstProductInListing.locator('.product-name').innerText();
-        const expectedProductName = await product2Data.productName.innerText();
-        await ShopCustomer.expects(firstProductName).toBe(expectedProductName);
+        const expectedProductName = await product2Locators.productName.innerText();
+        ShopCustomer.expects(firstProductName).toBe(expectedProductName);
     });
 
     await test.step('Add product to cart from wishlist and verify it is added', async () => {
-        await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1))
+        await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
+        const productPrice = await product1Locators.productPrice.innerText();
+        const offCanvasSubtotal = await StorefrontWishlist.offCanvasSummaryTotalPrice.innerText();
+        ShopCustomer.expects(offCanvasSubtotal).toBe(productPrice);
     });
 });
 

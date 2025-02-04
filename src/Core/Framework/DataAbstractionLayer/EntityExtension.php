faa19e4c582b215097a43a70465dcb58891e0e85
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-02-04 16:15:22 +0100

    NEXT-40450 - removing deprecations in dal - root namespace, part 2

--- src/Core/Framework/DataAbstractionLayer/EntityExtension.php
+++ src/Core/Framework/DataAbstractionLayer/EntityExtension.php
@@ -38,17 +38,5 @@ abstract class EntityExtension
      */
     abstract public function getDefinitionClass(): string;
 
-    /**
-     * @deprecated tag:v6.7.0 - reason:visibility-change - Becomes abstract
-     *
-     * @abstract
-     */
-    public function getEntityName(): string
-    {
-        if (EnvironmentHelper::getVariable('APP_ENV') === 'dev') {
-            Feature::triggerDeprecationOrThrow('v6.7.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.7.0.0', 'Method will be abstract'));
-        }
-
-        return '';
-    }
+    abstract public function getEntityName(): string;
 }

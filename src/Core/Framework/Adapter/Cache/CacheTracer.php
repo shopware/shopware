29ed66ffae75d775a4403e12553fbd445b3b1c42
Michael Telgmann <m.telgmann@shopware.com>
2025-02-04 16:40:46 +0100

    Deprecate CacheTracer

--- src/Core/Framework/Adapter/Cache/CacheTracer.php
+++ src/Core/Framework/Adapter/Cache/CacheTracer.php
@@ -8,6 +8,8 @@ use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 /**
+ * @deprecated tag:v6.7.0 - Will be removed, cache tags are collected via AddCacheTagEvent
+ *
  * @extends AbstractCacheTracer<mixed|null>
  */
 #[Package('framework')]

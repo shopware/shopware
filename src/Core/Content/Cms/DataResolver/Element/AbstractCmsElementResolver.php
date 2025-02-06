86d9f089fe8ccf7c9989be6000f6e1da9531c34d
Krzykawski <m.krzykawski@shopware.com>
2025-02-06 09:56:57 +0100

    NEXT-38373 - Deprecated BC code

--- src/Core/Content/Cms/DataResolver/Element/AbstractCmsElementResolver.php
+++ src/Core/Content/Cms/DataResolver/Element/AbstractCmsElementResolver.php
@@ -2,9 +2,19 @@
 
 namespace Shopware\Core\Content\Cms\DataResolver\Element;
 
+use Shopware\Core\Content\Cms\DataResolver\FieldConfig;
 use Shopware\Core\Content\Cms\DataResolver\ResolverContext\EntityResolverContext;
 use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Exception\PropertyNotFoundException;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\AssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Field;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Struct\Struct;
 
@@ -94,6 +104,79 @@ abstract class AbstractCmsElementResolver implements CmsElementResolverInterface
         return $path;
     }
 
+    /**
+     * @deprecated tag:v6.8.0 - Will be removed without replacement
+     */
+    protected function resolveDefinitionField(EntityDefinition $definition, string $path): ?Field
+    {
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', 'Function will be removed without replacement');
+
+        $value = null;
+        $parts = explode('.', $path);
+        $fields = $definition->getFields();
+
+        // if property does not exist, try to omit the first key as it may contain the entity name.
+        // E.g. `product.description` does not exist, but will be found if the first part is omitted.
+        $smartDetect = true;
+
+        while (\count($parts) > 0) {
+            $part = array_shift($parts);
+            $value = $fields->get($part);
+
+            if ($value === null && !$smartDetect) {
+                break;
+            }
+
+            $smartDetect = false;
+
+            if ($value instanceof AssociationField) {
+                $fields = $value->getReferenceDefinition()->getFields();
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * @deprecated tag:v6.8.0 - Will be removed without replacement
+     */
+    protected function resolveCriteriaForLazyLoadedRelations(
+        EntityResolverContext $resolverContext,
+        FieldConfig $config
+    ): ?Criteria {
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', 'Function will be removed without replacement');
+
+        $field = $this->resolveDefinitionField($resolverContext->getDefinition(), $config->getStringValue());
+        if ($field === null) {
+            return null;
+        }
+
+        $key = null;
+        $refDef = null;
+
+        // resolve reverse side to fetch data afterwards
+        if ($field instanceof ManyToManyAssociationField) {
+            $key = $this->getKeyByManyToMany($field);
+            $refDef = $field->getToManyReferenceDefinition();
+        } elseif ($field instanceof OneToManyAssociationField) {
+            $key = $this->getKeyByOneToMany($field);
+            $refDef = $field->getReferenceDefinition();
+        }
+
+        if (!$key || !$refDef) {
+            return null;
+        }
+
+        $key = $refDef->getEntityName() . '.' . $key;
+
+        $criteria = new Criteria();
+        $criteria->addFilter(
+            new EqualsFilter($key, $resolverContext->getEntity()->getUniqueIdentifier())
+        );
+
+        return $criteria;
+    }
+
     protected function resolveEntityValues(EntityResolverContext $resolverContext, string $content): ?string
     {
         // https://regex101.com/r/idIfbk/1
@@ -108,5 +191,37 @@ abstract class AbstractCmsElementResolver implements CmsElementResolverInterface
             },
             $content
         );
+    }
+
+    private function getKeyByManyToMany(ManyToManyAssociationField $field): ?string
+    {
+        $referenceDefinition = $field->getReferenceDefinition();
+
+        $manyToMany = $field->getToManyReferenceDefinition()->getFields()
+            ->filterInstance(ManyToManyAssociationField::class)
+            ->filter(static fn (ManyToManyAssociationField $field) => $field->getReferenceDefinition() === $referenceDefinition)
+            ->first();
+
+        if (!$manyToMany instanceof ManyToManyAssociationField) {
+            return null;
+        }
+
+        return $manyToMany->getPropertyName() . '.' . $manyToMany->getReferenceField();
+    }
+
+    private function getKeyByOneToMany(OneToManyAssociationField $field): ?string
+    {
+        $referenceDefinition = $field->getReferenceDefinition();
+
+        $manyToOne = $field->getReferenceDefinition()->getFields()
+            ->filterInstance(ManyToOneAssociationField::class)
+            ->filter(static fn (ManyToOneAssociationField $field) => $field->getReferenceDefinition() === $referenceDefinition)
+            ->first();
+
+        if (!$manyToOne instanceof ManyToOneAssociationField) {
+            return null;
+        }
+
+        return $manyToOne->getPropertyName() . '.' . $manyToOne->getReferenceField();
     }
 }

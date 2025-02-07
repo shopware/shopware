8cfdcb4f3efaeeeb13d74c90fd53e397693f1043
Aydin Hassan <a.hassan@shopware.com>
2025-02-07 14:05:49 +0100

    chore: remove usages of deprecated service

--- tests/integration/Core/Content/Newsletter/DataAbstractionLayer/Indexing/CustomerNewsletterSalesChannelsUpdaterTest.php
+++ tests/integration/Core/Content/Newsletter/DataAbstractionLayer/Indexing/CustomerNewsletterSalesChannelsUpdaterTest.php
@@ -287,7 +287,7 @@ class CustomerNewsletterSalesChannelsUpdaterTest extends TestCase
             ->get('newsletter_recipient.repository')
             ->delete([$newsletterRecipient], $context);
 
-        $messageBus = static::getContainer()->get('messenger.bus.shopware');
+        $messageBus = static::getContainer()->get('messenger.bus.default');
         static::assertInstanceOf(TraceableMessageBus::class, $messageBus);
 
         foreach ($messageBus->getDispatchedMessages() as $message) {

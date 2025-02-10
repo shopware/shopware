0a567f7b9524b28dfbb285203d3e7107c7d49a5f
Orkun Cavli <o.cavli@shopware.com>
2025-02-10 11:46:45 +0100

    test: fix review feedbacks

--- tests/acceptance/tests/Settings/WishlistOnListingPage.spec.ts
+++ tests/acceptance/tests/Settings/WishlistOnListingPage.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@fixtures/AcceptanceTest';
 
-test('Customer is able to add and remove products to the wishlist',{ tag: '@Wishlist' }, async ({ 
+test('Customers can add or remove products from their wishlist.',{ tag: '@Wishlist' }, async ({ 
     TestDataService,
     ShopCustomer,
     StorefrontHome,
@@ -9,6 +9,7 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
     RemoveProductFromWishlist,
     StorefrontWishlist,
     AddProductToCartFromWishlist,
+    StorefrontOffCanvasCart,
 }) => {
     await TestDataService.setSystemConfig({ 'core.cart.wishlistEnabled': true });
 
@@ -20,7 +21,7 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
     const product2Locators = await StorefrontHome.getListingItemByProductId(product2.id);
     const product3Locators = await StorefrontHome.getListingItemByProductId(product3.id);
 
-    await test.step('Add to products to the wishlist and check the basket count', async () => {
+    await test.step('Add three products to the wishlist and verify the basket count updates to 3', async () => {
         await ShopCustomer.attemptsTo(Login());
         await ShopCustomer.goesTo(StorefrontHome.url());
         await ShopCustomer.attemptsTo(AddProductToWishlist(product1));
@@ -31,13 +32,13 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
         await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('3');
     });
 
-    await test.step('Remove a product from the wishlist and check the basket count', async () => {
+    await test.step('Remove a product from the wishlist and verify that the basket updates to 2', async () => {
         await ShopCustomer.attemptsTo(RemoveProductFromWishlist(product3));
         await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('2');
         await ShopCustomer.expects(product3Locators.wishlistNotAddedIcon).toBeVisible();
     });
 
-    await test.step('Navigate to wishlist and verify the last added product is the fist in listing', async () => {
+    await test.step('Navigate to the wishlist and verify the lastly added product appears the first in the listing', async () => {
         await StorefrontHome.wishlistIcon.click();
         await ShopCustomer.expects(StorefrontWishlist.wishlistIcon).toBeVisible();
         const firstProductInListing = StorefrontWishlist.productListItems.first();
@@ -46,12 +47,19 @@ test('Customer is able to add and remove products to the wishlist',{ tag: '@Wish
         ShopCustomer.expects(firstProductName).toBe(expectedProductName);
     });
 
-    await test.step('Add product to cart from wishlist and verify it is added', async () => {
-        await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
-        const productPrice = await product1Locators.productPrice.innerText();
-        const offCanvasSubtotal = await StorefrontWishlist.offCanvasSummaryTotalPrice.innerText();
-        ShopCustomer.expects(offCanvasSubtotal).toBe(productPrice);
+    await test.step('Remove product2 from the wishlist page and verify that the basket updates to 1', async () => {
+        const listedItemInWishlist = await StorefrontWishlist.getListingItemByProductId(product2.id);
+        await listedItemInWishlist.removeFromWishlistButton.click();
+        await ShopCustomer.expects(StorefrontWishlist.removeAlert).toBeVisible();
+        await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('1');
     });
+
+    await test.step('Add product to cart from wishlist and verify it is added and wishlist icon is visible on offcanvas', async () => {
+        await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
+        const offCanvasSubtotal = await StorefrontOffCanvasCart.subTotalPrice.innerText();
+        const expectedPrice = await product1Locators.productPrice.innerText();
+        ShopCustomer.expects(offCanvasSubtotal).toBe(expectedPrice);
+        const offcanvasItem = await StorefrontOffCanvasCart.getLineItemByProductNumber(product1.productNumber);
+        await ShopCustomer.expects(offcanvasItem.wishlistAddedButton).toBeVisible();
+    }); 
 });
-
-

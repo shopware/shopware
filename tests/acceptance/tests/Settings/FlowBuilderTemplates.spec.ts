ec77984c85e958f9afa5ab9e1087a8521e17798f
Daniel Meyer <d.meyer@shopware.com>
2025-02-10 21:26:39 +0100

    add test steps

--- tests/acceptance/tests/Settings/FlowBuilderTemplates.spec.ts
+++ tests/acceptance/tests/Settings/FlowBuilderTemplates.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@fixtures/AcceptanceTest';
+import { test } from '@fixtures/AcceptanceTest';
 import { getFlowId, compareFlowTemplateWithFlow } from "@shopware-ag/acceptance-test-suite";
 
 test('As an admin, I want to create new flows from templates, so that I can easily create new ones based on the default flows.', { tag: '@Flow' }, async ({
@@ -19,30 +19,28 @@ test('As an admin, I want to create new flows from templates, so that I can easi
     const uniqueId = IdProvider.getIdPair().uuid;
     const flowName = 'Test flow - ' + uniqueId;
 
-    // go to flow template detail page and retrieve template's UUID
+    await test.step('Go to flow template detail page and retrieve template UUID', async () => {
     // await ShopAdmin.goesTo(AdminFlowBuilderTemplates.url(`?limit=25&page=1&term=${flowTemplateSearchTerm}&sortBy=createdAt&sortDirection=DESC&naturalSorting=false`));
     // todo: the line above replaces the following two lines as soon as NEXT-40094 is resolved
-    await ShopAdmin.goesTo(AdminFlowBuilderTemplates.url());
-    // todo: wait for sth
-    await AdminFlowBuilderTemplates.searchBar.fill(flowTemplateSearchTerm);
-    const adminFlowBuilderTemplatesRow = await AdminFlowBuilderTemplates.getLineItemByFlowName(flowTemplateName);
-    await adminFlowBuilderTemplatesRow.templateDetailLink.click();
-    await ShopAdmin.expects(AdminFlowBuilderDetail.generalTab).toBeVisible();
-    await ShopAdmin.expects(AdminFlowBuilderDetail.templateName).toHaveValue(flowTemplateName);
-    await ShopAdmin.expects(AdminFlowBuilderDetail.alertWarning).toContainText('Flow templates cannot be edited.');
-    const flowTemplateUrl = AdminFlowBuilderDetail.page.url().split('/');
-    const flowTemplateId = flowTemplateUrl[flowTemplateUrl.length - 2];
+        await ShopAdmin.goesTo(AdminFlowBuilderTemplates.url());
+        await AdminFlowBuilderTemplates.searchBar.fill(flowTemplateSearchTerm);
+        const adminFlowBuilderTemplatesRow = await AdminFlowBuilderTemplates.getLineItemByFlowName(flowTemplateName);
+        await adminFlowBuilderTemplatesRow.templateDetailLink.click();
+        await ShopAdmin.expects(AdminFlowBuilderDetail.generalTab).toBeVisible();
+        await ShopAdmin.expects(AdminFlowBuilderDetail.templateName).toHaveValue(flowTemplateName);
+        await ShopAdmin.expects(AdminFlowBuilderDetail.alertWarning).toContainText('Flow templates cannot be edited.');
+        });
 
-    // create flow from template
-    await ShopAdmin.goesTo(AdminFlowBuilderCreate.url(`${flowTemplateId}`));
-    // todo: wait for sth
-    await ShopAdmin.expects(AdminFlowBuilderCreate.smartBarHeader).toContainText(flowTemplateName);
-    await AdminFlowBuilderCreate.nameField.fill(flowName);
-    await AdminFlowBuilderCreate.saveButton.click();
-    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-alert__title')).toHaveText('Success');
-
-    // compare flow template and flow
-    const flowId = await getFlowId(flowName, AdminApiContext);
-    const isEqual = await compareFlowTemplateWithFlow(flowId, flowTemplateId, AdminApiContext);
-    expect(isEqual).toBe(true);
+    await test.step('Create flow from template and compare it with template', async () => {
+        const flowTemplateUrl = AdminFlowBuilderDetail.page.url().split('/');
+        const flowTemplateId = flowTemplateUrl[flowTemplateUrl.length - 2];
+        await ShopAdmin.goesTo(AdminFlowBuilderCreate.url(`${flowTemplateId}`));
+        await ShopAdmin.expects(AdminFlowBuilderCreate.smartBarHeader).toContainText(flowTemplateName);
+        await AdminFlowBuilderCreate.nameField.fill(flowName);
+        await AdminFlowBuilderCreate.saveButton.click();
+        await ShopAdmin.expects(AdminFlowBuilderDetail.alertMessage).toHaveText('Success');
+        const flowId = await getFlowId(flowName, AdminApiContext);
+        const flowEqualsTemplate = await compareFlowTemplateWithFlow(flowId, flowTemplateId, AdminApiContext);
+        await ShopAdmin.expects(flowEqualsTemplate).toBe(true);
+        });
 });

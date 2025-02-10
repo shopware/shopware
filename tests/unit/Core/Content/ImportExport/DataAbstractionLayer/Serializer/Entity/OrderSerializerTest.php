bf66cee99c3c3d6abf9647ef5e3f8915046f50db
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-02-10 13:48:59 +0100

    NEXT-40450 - removing deprecations in dal - fixes for uninitialized ids typed as string (can't be serialized)

--- tests/unit/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/OrderSerializerTest.php
+++ tests/unit/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/OrderSerializerTest.php
@@ -205,7 +205,6 @@ class OrderSerializerTest extends TestCase
                     'order' => null,
                     'stateMachineState' => [
                         'extensions' => [],
-                        '_uniqueIdentifier' => null,
                         'versionId' => null,
                         'translated' => [],
                         'createdAt' => null,
@@ -222,7 +221,6 @@ class OrderSerializerTest extends TestCase
                         'fromStateMachineHistoryEntries' => null,
                         'toStateMachineHistoryEntries' => null,
                         'customFields' => null,
-                        'id' => null,
                         'translations' => null,
                     ],
                     'captures' => null,
@@ -316,7 +314,6 @@ class OrderSerializerTest extends TestCase
                     'order' => null,
                     'stateMachineState' => [
                         'extensions' => [],
-                        '_uniqueIdentifier' => null,
                         'versionId' => null,
                         'translated' => [],
                         'createdAt' => null,
@@ -333,7 +330,6 @@ class OrderSerializerTest extends TestCase
                         'fromStateMachineHistoryEntries' => null,
                         'toStateMachineHistoryEntries' => null,
                         'customFields' => null,
-                        'id' => null,
                         'translations' => null,
                     ],
                     'captures' => null,
@@ -470,11 +466,9 @@ class OrderSerializerTest extends TestCase
     private static function getExpected(array $overrided = []): array
     {
         return array_merge([
-            'id' => null,
             'updatedBy' => null,
             'updatedById' => null,
             'customFields' => null,
-            '_uniqueIdentifier' => null,
             'translated' => [],
             'extensions' => [],
             'versionId' => null,

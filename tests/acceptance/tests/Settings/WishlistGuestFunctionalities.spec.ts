14402efb0e68b61fbb2edcb83d63122946f1ae63
Orkun Cavli <o.cavli@shopware.com>
2025-02-11 08:57:48 +0100

    test: fix review feedback

--- tests/acceptance/tests/Settings/WishlistGuestFunctionalities.spec.ts
+++ tests/acceptance/tests/Settings/WishlistGuestFunctionalities.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@fixtures/AcceptanceTest';
 
-test('Guest Customer is able to add and remove products to the wishlist',{ tag: '@Wishlist' }, async ({ 
+test('Guest customer is able to add and remove products to the wishlist',{ tag: '@Wishlist' }, async ({ 
     TestDataService,
     ShopCustomer,
     StorefrontHome,

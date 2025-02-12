b0b86f51387f65246063ca92689be6d9ff1d2a97
Yusuf  Tür <106602505+yusufttur@users.noreply.github.com>
2025-02-12 08:47:51 +0100

    fix: Update TaxFreeCartForCurrency.spec.ts

--- tests/acceptance/tests/Settings/TaxFreeCartForCurrency.spec.ts
+++ tests/acceptance/tests/Settings/TaxFreeCartForCurrency.spec.ts
@@ -20,7 +20,8 @@ test(
 }) => {
 
     test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0 Ticket already created: https://shopware.atlassian.net/browse/NEXT-40119');
-    test.skip(InstanceMeta.features['V6_7_0_0'], 'Blocked by accessibility flag: https://shopware.atlassian.net/browse/NEXT-40636');
+    test.skip(InstanceMeta.features['ACCESSIBILITY_TWEAKS'], 'Blocked by accessibility flag: https://shopware.atlassian.net/browse/NEXT-40636');
+
     const product = await TestDataService.createBasicProduct();
     const currency = await TestDataService.createCurrency({ taxFreeFrom: 5 });
     const customer = await TestDataService.createCustomer();

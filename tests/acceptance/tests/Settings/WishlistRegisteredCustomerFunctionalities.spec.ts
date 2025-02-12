d93d755657c263cec781e649f1ca13dd23aa6d72
Orkun Cavli <o.cavli@shopware.com>
2025-02-12 15:39:46 +0100

    test: skip add to shopping cart action from wishlist

--- tests/acceptance/tests/Settings/WishlistRegisteredCustomerFunctionalities.spec.ts
+++ tests/acceptance/tests/Settings/WishlistRegisteredCustomerFunctionalities.spec.ts
@@ -55,26 +55,17 @@ test('Customers can add or remove products from their wishlist.',{ tag: '@Wishli
         await ShopCustomer.expects(StorefrontWishlist.removeAlert).toBeVisible();
         await ShopCustomer.expects(StorefrontHome.wishlistBasket).toContainText('1');
     });
-/*
-    //This step is skipped, please check the details from ticket : https://shopware.atlassian.net/browse/NEXT-40639
-    await test.step.skip('Add product to cart from wishlist and verify it is added and wishlist icon is visible on offcanvas', async () => {
-        await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
-        const offCanvasSubtotal = await StorefrontOffCanvasCart.subTotalPrice.innerText();
-        const expectedPrice = await product1Locators.productPrice.innerText();
-        ShopCustomer.expects(offCanvasSubtotal).toBe(expectedPrice);
-        const offcanvasItem = await StorefrontOffCanvasCart.getLineItemByProductNumber(product1.productNumber);
-        await ShopCustomer.expects(offcanvasItem.wishlistAddedButton).toBeVisible();
-    }); 
-*/          
-if (satisfies(InstanceMeta.version, '<6.7')) {
 
-    await test.step('Add product to cart from wishlist and verify it is added and wishlist icon is visible on offcanvas', async () => {       
-          await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
-          const offCanvasSubtotal = await StorefrontOffCanvasCart.subTotalPrice.innerText();
-          const expectedPrice = await product1Locators.productPrice.innerText();
-          ShopCustomer.expects(offCanvasSubtotal).toBe(expectedPrice);
-          const offcanvasItem = await StorefrontOffCanvasCart.getLineItemByProductNumber(product1.productNumber);
-          await ShopCustomer.expects(offcanvasItem.wishlistAddedButton).toBeVisible();      
+    // TO-DO: This step is skipped, please check the details from ticket : https://shopware.atlassian.net/browse/NEXT-40639
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    if (satisfies(InstanceMeta.version, '<6.7')) {
+        await test.step('Add product to cart from wishlist and verify it is added and wishlist icon is visible on offcanvas', async () => {       
+            await ShopCustomer.attemptsTo(AddProductToCartFromWishlist(product1));
+            const offCanvasSubtotal = await StorefrontOffCanvasCart.subTotalPrice.innerText();
+            const expectedPrice = await product1Locators.productPrice.innerText();
+            ShopCustomer.expects(offCanvasSubtotal).toBe(expectedPrice);
+            const offcanvasItem = await StorefrontOffCanvasCart.getLineItemByProductNumber(product1.productNumber);
+            await ShopCustomer.expects(offcanvasItem.wishlistAddedButton).toBeVisible();      
     });
-}
+    }
 });

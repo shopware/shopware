4ce95b263354129acdbed0c7ef56e87d2ea49dd5
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-02-18 12:39:14 +0100

    NEXT-40450 - rolled back changed to FilteredBulkEntityExtension

--- src/Core/Framework/DataAbstractionLayer/FilteredBulkEntityExtension.php
+++ src/Core/Framework/DataAbstractionLayer/FilteredBulkEntityExtension.php
@@ -18,25 +18,15 @@ class FilteredBulkEntityExtension extends EntityExtension
 
     public function extendFields(FieldCollection $collection): void
     {
-        try {
-            foreach ($this->bulkExtension->collect() as $entity => $fields) {
-                if ($entity !== $this->entityName) {
-                    continue;
-                }
-
-                foreach ($fields as $field) {
-                    $collection->add($field);
-                }
-            }
-        } catch (\Throwable $e) {
-            if (str_starts_with($e->getMessage(), 'Class "') && str_ends_with($e->getMessage(), '" not found')) {
-                // Ignore autoload issues, as they might happen during plugin lifecycle
-                return;
+        foreach ($this->bulkExtension->collect() as $entity => $fields) {
+            if ($entity !== $this->entityName) {
+                continue;
             }
 
-            throw $e;
+            foreach ($fields as $field) {
+                $collection->add($field);
+            }
         }
-
     }
 
     public function getEntityName(): string

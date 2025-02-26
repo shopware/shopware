25719a3b2aa504f4941ec14493f933aaa2da9180
Aydin Hassan <a.hassan@shopware.com>
2025-02-26 14:26:32 +0100

    remove useless tests

--- src/Core/Framework/App/Lifecycle/Parameters/AppUpdateParameters.php
+++ src/Core/Framework/App/Lifecycle/Parameters/AppUpdateParameters.php
@@ -6,6 +6,7 @@ use Shopware\Core\Framework\Log\Package;
 
 /**
  * @internal
+ * @codeCoverageIgnore This is a simple DTO and does not require tests
  */
 #[Package('framework')]
 final class AppUpdateParameters

bfd21086f3e449934326b4c940139dcdb6291149
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-02-27 09:16:25 +0100

    fix: allow reset inheritance in plugin configuration required fields (#7028)

--- tests/unit/Core/System/SystemConfig/Validation/SystemConfigValidatorTest.php
+++ tests/unit/Core/System/SystemConfig/Validation/SystemConfigValidatorTest.php
@@ -134,7 +134,8 @@ class SystemConfigValidatorTest extends TestCase
      * @param array<string, mixed> $elementConfig
      * @param array<int, mixed> $expected
      */
-    public function testBuildConstraintsWithConfigs(array $elementConfig, array $expected): void
+    #[DataProvider('dataProviderTestGetRuleByKey')]
+    public function testBuildConstraintsWithConfigs(array $elementConfig, array $expected, bool $allowNulls): void
     {
         $configurationServiceMock = $this->createMock(ConfigurationService::class);
         $dataValidatorMock = $this->createMock(DataValidator::class);
@@ -143,7 +144,7 @@ class SystemConfigValidatorTest extends TestCase
 
         $refMethod = ReflectionHelper::getMethod(SystemConfigValidator::class, 'buildConstraintsWithConfigs');
 
-        $result = $refMethod->invoke($systemConfigValidation, $elementConfig);
+        $result = $refMethod->invoke($systemConfigValidation, $elementConfig, $allowNulls);
 
         static::assertEquals($expected, $result);
     }
@@ -153,6 +154,7 @@ class SystemConfigValidatorTest extends TestCase
         yield 'element config is empty' => [
             'elementConfig' => [],
             'expected' => [],
+            'allowNulls' => false,
         ];
 
         yield 'element config with type string' => [
@@ -168,6 +170,7 @@ class SystemConfigValidatorTest extends TestCase
                 new Assert\Type('string'),
                 new Assert\NotBlank(),
             ],
+            'allowNulls' => false,
         ];
 
         yield 'element config with type int' => [
@@ -183,6 +186,23 @@ class SystemConfigValidatorTest extends TestCase
                 new Assert\Type('int'),
                 new Assert\NotBlank(),
             ],
+            'allowNulls' => false,
+        ];
+
+        yield 'element config with type string, nulls allowed' => [
+            'elementConfig' => [
+                'required' => true,
+                'dataType' => 'string',
+                'minLength' => 1,
+                'maxLength' => 255,
+            ],
+            'expected' => [
+                new Assert\Length(['min' => 1]),
+                new Assert\Length(['max' => 255]),
+                new Assert\Type('string'),
+                new Assert\NotBlank(null, null, true),
+            ],
+            'allowNulls' => true,
         ];
     }
 

00d44372b4ba2b07a66268a3652ebc6b6549cd2e
Aydin Hassan <a.hassan@shopware.com>
2025-02-27 09:44:32 +0100

    wip

--- src/Administration/Resources/app/administration/src/module/sw-extension/page/sw-extension-my-extensions-listing/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-extension/page/sw-extension-my-extensions-listing/index.js
@@ -14,6 +14,7 @@ export default {
         return {
             filterByActiveState: false,
             sortingOption: 'updated-at',
+            hasPermissionRequests: true,
         };
     },
 

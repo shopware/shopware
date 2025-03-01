5c1c9d66b1d1ab825ea8a51a4a21018fe7946cba
Soner Sayakci <s.sayakci@gmail.com>
2025-03-01 08:13:43 +0100

    refactor: Improve type handling in InstanceOfExtension Twig filter
    Modify the isInstanceOf method to accept more flexible input types while maintaining core functionality. This change allows for more lenient type checking in Twig templates while preserving the original method's intent.

--- src/Core/Framework/Adapter/Twig/Extension/InstanceOfExtension.php
+++ src/Core/Framework/Adapter/Twig/Extension/InstanceOfExtension.php
@@ -17,9 +17,10 @@ class InstanceOfExtension extends AbstractExtension
     }
 
     /**
+     * @param object $var
      * @param class-string $class
      */
-    public function isInstanceOf(object $var, string $class): bool
+    public function isInstanceOf($var, $class): bool
     {
         return (new \ReflectionClass($class))->isInstance($var);
     }

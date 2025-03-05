94212cd0ede411a170b5d34765c386418bef2392
Yusuf  Tür <yusufttur@gmail.com>
2025-03-05 14:59:07 +0100

    fix: unskip VariantGeneration.spec.ts

--- tests/acceptance/tests/Product/VariantGeneration.spec.ts
+++ tests/acceptance/tests/Product/VariantGeneration.spec.ts
@@ -5,10 +5,7 @@ test('Shop administrator should be able to create product variants.', { tag: '@P
     TestDataService,
     AdminProductDetail,
     GenerateVariants,
-    InstanceMeta,
 }) => {
-    test.skip(InstanceMeta.features['V6_7_0_0'], 'This test is incompatible with V6_7_0_0. Ticket: https://shopware.atlassian.net/browse/NEXT-40158');
-
     const product = await TestDataService.createBasicProduct();
     await TestDataService.createColorPropertyGroup();
     await TestDataService.createTextPropertyGroup();

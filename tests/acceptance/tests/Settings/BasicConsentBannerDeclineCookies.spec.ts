925cc56a11105a3eb7a9d3cf286bd299c18d5868
Yusuf  Tür <yusufttur@gmail.com>
2025-03-07 14:45:35 +0100

    test: remove comments

--- tests/acceptance/tests/Settings/BasicConsentBannerDeclineCookies.spec.ts
+++ tests/acceptance/tests/Settings/BasicConsentBannerDeclineCookies.spec.ts
@@ -8,8 +8,6 @@ test(
     InstanceMeta,
 }) => {
     test.skip(InstanceMeta.isSaaS, 'Cache invalidation does not happen immediately on SaaS');
-    // test.skip(InstanceMeta.features['ACCESSIBILITY_TWEAKS'], 'Blocked by https://shopware.atlassian.net/browse/NEXT-40635, ' +
-    //     'https://shopware.atlassian.net/browse/NEXT-40156');
 
     await TestDataService.setSystemConfig({'core.basicInformation.acceptAllCookies': true});
     const product = await TestDataService.createBasicProduct();

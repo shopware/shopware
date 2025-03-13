1dd25db7319bbadc05f4c7cb7b615f16deb89857
Aydin Hassan <a.hassan@shopware.com>
2025-03-13 15:43:11 +0100

    fix permission persister

--- src/Core/Framework/App/Lifecycle/AppLifecycle.php
+++ src/Core/Framework/App/Lifecycle/AppLifecycle.php
@@ -239,7 +239,7 @@ class AppLifecycle extends AbstractAppLifecycle
 
         $this->updateCustomEntities($app, $manifest);
 
-        $this->permissionPersister->updatePrivileges($manifest->getPermissions(), $roleId);
+        $this->permissionPersister->updatePrivileges($manifest->getPermissions(), $id);
 
         // If the app has no secret yet, but now specifies setup data we do a registration to get an app secret
         // this mostly happens during install, but may happen in the update case if the app previously worked without an external server

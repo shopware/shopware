a5223f3717b32ed9a02ea1d29964c3d69d9a7a41
Fabian Boensch <f.boensch@shopware.com>
2025-03-14 11:55:34 +0100

    fix: Show new customer address after saving admin modal

--- src/Administration/Resources/app/administration/src/module/sw-customer/view/sw-customer-detail-addresses/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-customer/view/sw-customer-detail-addresses/index.js
@@ -230,13 +230,11 @@ export default {
 
             Object.assign(address, this.currentAddress);
 
-            if (this.customer.addresses.has(address.id)) {
-                this.customer.addresses.remove(address.id);
-            }
+            this.customerAddressRepository.save(address).then(() => {
+                this.currentAddress = null;
 
-            this.customer.addresses.push(address);
-
-            this.currentAddress = null;
+                this.refreshList();
+            });
         },
 
         isValidAddress(address) {
@@ -279,12 +277,7 @@ export default {
         },
 
         onEditAddress(id) {
-            const currentAddress = this.addressRepository.create(Shopware.Context.api, id);
-
-            // assign values and id to new address
-            Object.assign(currentAddress, this.activeCustomer.addresses.get(id));
-
-            this.currentAddress = currentAddress;
+            this.currentAddress = this.activeCustomer.addresses.get(id);
             this.showEditAddressModal = id;
         },
 

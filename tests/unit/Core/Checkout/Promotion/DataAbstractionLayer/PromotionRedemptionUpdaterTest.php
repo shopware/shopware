c1cf210a19ac8b31ef05d94e5dd33144a64ace99
Albert Scherman <1457444+Bird87ZA@users.noreply.github.com>
2025-03-18 09:31:47 +0100

    fix: fixed updating of order_count in promotions table (#7492)

--- tests/unit/Core/Checkout/Promotion/DataAbstractionLayer/PromotionRedemptionUpdaterTest.php
+++ tests/unit/Core/Checkout/Promotion/DataAbstractionLayer/PromotionRedemptionUpdaterTest.php
@@ -7,16 +7,22 @@ use Doctrine\DBAL\Statement;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Checkout\Cart\Event\CheckoutOrderPlacedEvent;
 use Shopware\Core\Checkout\Order\Aggregate\OrderCustomer\OrderCustomerEntity;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemCollection;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemDefinition;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
+use Shopware\Core\Checkout\Order\OrderCollection;
+use Shopware\Core\Checkout\Order\OrderDefinition;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Promotion\Cart\PromotionProcessor;
 use Shopware\Core\Checkout\Promotion\DataAbstractionLayer\PromotionRedemptionUpdater;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityWriteResult;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWriteEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\DeleteCommand;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\InsertCommand;
 use Shopware\Core\Framework\DataAbstractionLayer\Write\Command\UpdateCommand;
@@ -40,10 +46,13 @@ class PromotionRedemptionUpdaterTest extends TestCase
 
     private PromotionRedemptionUpdater $promotionRedemptionUpdater;
 
+    private EntityRepository&MockObject $orderRepositoryMock;
+
     protected function setUp(): void
     {
         $this->connectionMock = $this->createMock(Connection::class);
-        $this->promotionRedemptionUpdater = new PromotionRedemptionUpdater($this->connectionMock);
+        $this->orderRepositoryMock = $this->createMock(EntityRepository::class);
+        $this->promotionRedemptionUpdater = new PromotionRedemptionUpdater($this->connectionMock, $this->orderRepositoryMock);
     }
 
     public function getDefinition(): OrderLineItemDefinition
@@ -59,7 +68,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
 
     public function testUpdateEmptyIds(): void
     {
-        $this->connectionMock->expects($this->never())->method('fetchAllAssociative');
+        $this->connectionMock->expects(static::never())->method('fetchAllAssociative');
         $this->promotionRedemptionUpdater->update([], Context::createDefaultContext());
     }
 
@@ -73,7 +82,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
         ]);
 
         $statementMock = $this->createMock(Statement::class);
-        $statementMock->expects($this->once())
+        $statementMock->expects(static::once())
             ->method('executeStatement')
             ->with(static::equalTo([
                 'id' => Uuid::fromHexToBytes($promotionId),
@@ -93,7 +102,20 @@ class PromotionRedemptionUpdaterTest extends TestCase
         $event = $this->createOrderPlacedEvent($promotionId, $customerId);
 
         $statementMock = $this->createMock(Statement::class);
-        $statementMock->expects($this->once())
+        $params = [
+            ['id', Uuid::fromHexToBytes($promotionId)],
+            ['customerCount', json_encode([$customerId => 1], \JSON_THROW_ON_ERROR)],
+            ['count', 0],
+        ];
+        $matcher = static::exactly(\count($params));
+        $statementMock->expects($matcher)
+            ->method('bindValue')
+            ->willReturnCallback(function (string $key, $value) use ($matcher, $params): void {
+                self::assertSame($params[$matcher->numberOfInvocations() - 1][0], $key);
+                self::assertSame($params[$matcher->numberOfInvocations() - 1][1], $value);
+            });
+
+        $statementMock->expects(static::once())
             ->method('executeStatement')
             ->with(static::equalTo([
                 'id' => Uuid::fromHexToBytes($promotionId),
@@ -102,15 +124,15 @@ class PromotionRedemptionUpdaterTest extends TestCase
 
         $this->connectionMock->method('prepare')->willReturn($statementMock);
 
-        $this->promotionRedemptionUpdater->orderPlaced($event);
+        $this->promotionRedemptionUpdater->orderUpdated($event);
     }
 
     public function testOrderPlacedNoLineItemsOrCustomer(): void
     {
         $event = $this->createOrderPlacedEvent(null, null);
 
-        $this->connectionMock->expects($this->never())->method('fetchAllAssociative');
-        $this->promotionRedemptionUpdater->orderPlaced($event);
+        $this->connectionMock->expects(static::never())->method('fetchAllAssociative');
+        $this->promotionRedemptionUpdater->orderUpdated($event);
     }
 
     public function testUpdateCalledBeforeOrderPlacedDoesNotRepeatUpdate(): void
@@ -128,14 +150,20 @@ class PromotionRedemptionUpdaterTest extends TestCase
             ],
             [
                 [
+                    'promotion_id' => $promotionId,
+                    'count' => 1,
+                ],
+            ],
+            [
+                [
                     'id' => Uuid::fromHexToBytes($promotionId),
                     'orders_per_customer_count' => json_encode([$customerId => 1]),
                 ],
-            ]
+            ],
         );
 
         $statementMock = $this->createMock(Statement::class);
-        $statementMock->expects($this->once())->method('executeStatement')->with([
+        $statementMock->expects(static::once())->method('executeStatement')->with([
             'id' => Uuid::fromHexToBytes($promotionId),
             'customerCount' => json_encode([$customerId => 1], \JSON_THROW_ON_ERROR),
             'count' => 1,
@@ -148,10 +176,10 @@ class PromotionRedemptionUpdaterTest extends TestCase
 
         // Expect no further update calls during orderPlaced
         $statementMock = $this->createMock(Statement::class);
-        $statementMock->expects($this->never())->method('executeStatement');
+        $statementMock->expects(static::never())->method('executeStatement');
         $this->connectionMock->method('prepare')->willReturn($statementMock);
 
-        $this->promotionRedemptionUpdater->orderPlaced($event);
+        $this->promotionRedemptionUpdater->orderUpdated($event);
     }
 
     public function testBeforeDeletePromotionLineItems(): void
@@ -189,7 +217,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
         );
 
         $statementMock = $this->createMock(Statement::class);
-        $statementMock->expects($this->once())
+        $statementMock->expects(static::once())
             ->method('executeStatement')
             ->with(static::equalTo([
                 'id' => Uuid::fromHexToBytes($promotionId),
@@ -198,7 +226,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
             ]));
 
         $this->connectionMock->method('prepare')->willReturn($statementMock);
-        $this->connectionMock->expects($this->once())
+        $this->connectionMock->expects(static::once())
             ->method('executeStatement')
             ->with(static::equalTo('UPDATE promotion_individual_code set payload = NULL WHERE code IN (:codes)'))
             ->willReturnCallback(function ($query, $params): void {
@@ -224,7 +252,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
             [$command],
         );
 
-        $this->connectionMock->expects($this->once())->method('fetchAllAssociative')->willReturn([]);
+        $this->connectionMock->expects(static::once())->method('fetchAllAssociative')->willReturn([]);
 
         $this->promotionRedemptionUpdater->beforeDeletePromotionLineItems($event);
     }
@@ -244,7 +272,7 @@ class PromotionRedemptionUpdaterTest extends TestCase
             [$command],
         );
 
-        $this->connectionMock->expects($this->never())->method('fetchAllAssociative');
+        $this->connectionMock->expects(static::never())->method('fetchAllAssociative');
         $this->promotionRedemptionUpdater->beforeDeletePromotionLineItems($event);
     }
 
@@ -263,14 +291,59 @@ class PromotionRedemptionUpdaterTest extends TestCase
             [$command],
         );
 
-        $this->connectionMock->expects($this->never())->method('fetchAllAssociative');
+        $this->connectionMock->expects(static::never())->method('fetchAllAssociative');
         $this->promotionRedemptionUpdater->beforeDeletePromotionLineItems($event);
     }
 
-    private function createOrderPlacedEvent(?string $promotionId, ?string $customerId): CheckoutOrderPlacedEvent
+    public function testOrderUpdated(): void
+    {
+        $orderId = Uuid::randomHex();
+        $promotionId = Uuid::randomHex();
+        $customerId = Uuid::randomHex();
+
+        $orderLineItem = new OrderLineItemEntity();
+        $orderLineItem->setId(Uuid::randomHex());
+        $orderLineItem->setType(PromotionProcessor::LINE_ITEM_TYPE);
+        $orderLineItem->setPromotionId($promotionId);
+
+        $orderLineItems = new OrderLineItemCollection([$orderLineItem]);
+
+        $orderCustomer = new OrderCustomerEntity();
+        $orderCustomer->setCustomerId($customerId);
+
+        $order = new OrderEntity();
+        $order->setId($orderId);
+        $order->setLineItems($orderLineItems);
+        $order->setOrderCustomer($orderCustomer);
+
+        $entityWriteResult = new EntityWriteResult($orderId, [], 'order', EntityWriteResult::OPERATION_UPDATE);
+
+        $orderCollection = new OrderCollection([$order]);
+
+        $entitySearchResult = $this->createMock(EntitySearchResult::class);
+        $entitySearchResult->method('getEntities')->willReturn($orderCollection);
+
+        $this->orderRepositoryMock->method('search')->willReturn($entitySearchResult);
+
+        $event = new EntityWrittenEvent(
+            'order',
+            [$entityWriteResult],
+            Context::createDefaultContext()
+        );
+
+        $this->connectionMock->expects(static::once())
+            ->method('prepare')
+            ->with('UPDATE promotion SET order_count = :count, orders_per_customer_count = :customerCount WHERE id = :id')
+            ->willReturn($this->createMock(Statement::class));
+
+        $this->promotionRedemptionUpdater->orderUpdated($event);
+    }
+
+    private function createOrderPlacedEvent(?string $promotionId, ?string $customerId): EntityWrittenEvent
     {
         $lineItems = new OrderLineItemCollection();
         $order = new OrderEntity();
+        $order->setId(Uuid::randomHex());
         if ($promotionId !== null) {
             $lineItem = new OrderLineItemEntity();
             $lineItem->setId(Uuid::randomHex());
@@ -289,6 +362,20 @@ class PromotionRedemptionUpdaterTest extends TestCase
 
         $context = Generator::generateSalesChannelContext();
 
-        return new CheckoutOrderPlacedEvent($context, $order);
+        $criteria = new Criteria([$order->getId()]);
+        $criteria->addAssociations(['lineItems', 'orderCustomer']);
+        $result = new EntitySearchResult(OrderDefinition::ENTITY_NAME, 1, new OrderCollection([$order]), null, $criteria, $context->getContext());
+
+        $this->orderRepositoryMock->expects(static::once())
+            ->method('search')
+            ->willReturn($result);
+
+        $entityWriteResult = new EntityWriteResult($order->getId(), [], OrderDefinition::ENTITY_NAME, EntityWriteResult::OPERATION_UPDATE);
+
+        return new EntityWrittenEvent(
+            OrderDefinition::ENTITY_NAME,
+            [$entityWriteResult],
+            Context::createDefaultContext()
+        );
     }
 }

cd1289b2f3a6e2db899c258ace4307b67bd7ec4c
Aydin Hassan <a.hassan@shopware.com>
2025-03-21 12:01:04 +0100

    use file renamed status

--- .danger.php
+++ .danger.php
@@ -238,7 +238,7 @@ return (new Config())
     })
     ->useRule(function (Context $context): void {
         $addedUnitTests = $context->platform->pullRequest->getFiles()
-            //->filter(fn (File $file) => in_array($file->status, [File::STATUS_ADDED, File::STATUS_MODIFIED], true))
+            ->filter(fn (File $file) => in_array($file->status, [File::STATUS_ADDED, File::STATUS_MODIFIED, File::STATUS_RENAMED], true))
             ->matches('tests/unit/**/*Test.php');
 
         $addedSrcFiles = $context->platform->pullRequest->getFiles()->filterStatus(File::STATUS_ADDED)->matches('src/**/*.php');
@@ -345,13 +345,6 @@ return (new Config())
                 $missingUnitTests[] = $file->name;
             }
         }
-
-        $context->warning(
-            implode(
-                ", ",
-                array_map(fn ($f) => $f->name . '-' . $f->status, $addedUnitTests),
-            )
-        );
 
         if (\count($missingUnitTests) > 0) {
             $context->warning(

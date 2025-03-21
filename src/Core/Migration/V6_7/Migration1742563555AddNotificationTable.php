5d57c9b5b0b2a9cb2ac024732bd635463cba8040
Aydin Hassan <a.hassan@shopware.com>
2025-03-21 14:56:42 +0100

    fix test

--- src/Core/Migration/V6_7/Migration1742563555AddNotificationTable.php
+++ src/Core/Migration/V6_7/Migration1742563555AddNotificationTable.php
@@ -9,7 +9,7 @@ use Shopware\Core\Framework\Migration\MigrationStep;
 /**
  * @internal
  */
-#[Package('core')]
+#[Package('framework')]
 class Migration1742563555AddNotificationTable extends MigrationStep
 {
     public function getCreationTimestamp(): int

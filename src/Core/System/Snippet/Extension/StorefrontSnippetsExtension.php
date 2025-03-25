16a38863188366353a717bfe738562667774427c
Sascha <sascha.heilmeier@netlogix.de>
2025-03-25 13:27:10 +0100

    fix: PHPStan

--- src/Core/System/Snippet/Extension/StorefrontSnippetsExtension.php
+++ src/Core/System/Snippet/Extension/StorefrontSnippetsExtension.php
@@ -8,11 +8,18 @@ use Shopware\Core\Framework\Extensions\Extension;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\Translation\MessageCatalogueInterface;
 
+/**
+ * @extends Extension<array<string, string>>
+ */
 #[Package('discovery')]
-class StorefrontSnippetsExtension extends Extension
+final class StorefrontSnippetsExtension extends Extension
 {
     public const NAME = 'storefront.snippets';
 
+    /**
+     * @param array<string, string> $snippets
+     * @param string[] $unusedThemes
+     */
     public function __construct(
         public array $snippets,
         public readonly string $locale,

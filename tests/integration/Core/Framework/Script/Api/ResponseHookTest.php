8ad88c40a9d95acb0cc5cfb173df3e96bc6a96c5
Jonas Elfering <j.elfering@shopware.com>
2025-03-27 16:28:38 +0100

    feat: add possibility to override response headers from app scripts
    Fixes https://github.com/shopware/shopware/issues/7947

---
+++ tests/integration/Core/Framework/Script/Api/ResponseHookTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Integration\Core\Framework\Script\Api;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Test\TestCaseBase\SalesChannelApiTestBehaviour;
+use Shopware\Core\Test\AppSystemTestBehaviour;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @internal
+ */
+class ResponseHookTest extends TestCase
+{
+    use AppSystemTestBehaviour;
+    use IntegrationTestBehaviour;
+    use SalesChannelApiTestBehaviour;
+
+    private KernelBrowser $browser;
+
+    protected function setUp(): void
+    {
+        $this->browser = $this->getSalesChannelBrowser();
+    }
+
+    public function testResponseHook(): void
+    {
+        $this->loadAppsFromDir(__DIR__ . '/_fixtures');
+
+        $this->browser->request('GET', '/account/login');
+        static::assertNotFalse($this->browser->getResponse()->getContent());
+
+        $response = $this->browser->getResponse();
+        static::assertSame(Response::HTTP_OK, $response->getStatusCode(), $response->getContent());
+
+        static::assertSame('deny', $response->headers->get('X-Frame-Options'));
+
+        $this->browser->request('GET', '/');
+        static::assertNotFalse($this->browser->getResponse()->getContent());
+
+        $response = $this->browser->getResponse();
+        static::assertSame(Response::HTTP_OK, $response->getStatusCode(), $response->getContent());
+
+        static::assertSame('SAMEORIGIN', $response->headers->get('X-Frame-Options'));
+    }
+}

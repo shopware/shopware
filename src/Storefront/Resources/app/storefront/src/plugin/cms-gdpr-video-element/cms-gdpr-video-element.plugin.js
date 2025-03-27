7d9a8d885a64c4e298d13c1056a5a34c0c3cf0b0
Dumea Alexandru <a.dumea@shopware.com>
2025-03-27 10:07:23 +0200

    fix: Add iframe title (#7636)
    Co-authored-by: Nicolas Fortier <164332867+nfortier-shopware@users.noreply.github.com>

--- src/Storefront/Resources/app/storefront/src/plugin/cms-gdpr-video-element/cms-gdpr-video-element.plugin.js
+++ src/Storefront/Resources/app/storefront/src/plugin/cms-gdpr-video-element/cms-gdpr-video-element.plugin.js
@@ -121,7 +121,6 @@ export default class CmsGdprVideoElement extends Plugin {
         const videoElement = document.createElement('iframe');
         videoElement.setAttribute('src', this.options.videoUrl);
         videoElement.setAttribute('title', this.options.iframeTitle);
-        videoElement.setAttribute('allowfullscreen', 'allowfullscreen');
 
         this.options.iframeClasses.forEach((cls) => {
             videoElement.classList.add(cls);

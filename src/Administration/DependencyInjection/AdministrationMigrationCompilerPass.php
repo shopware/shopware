995416afa8528e7887aca0e28a2504b59d5fa92f
Dennis Garding <d.garding@shopware.com>
2025-04-01 09:22:56 +0200

    Adjust versions in compiler pass

--- src/Administration/DependencyInjection/AdministrationMigrationCompilerPass.php
+++ src/Administration/DependencyInjection/AdministrationMigrationCompilerPass.php
@@ -15,7 +15,7 @@ class AdministrationMigrationCompilerPass implements CompilerPassInterface
         $migrationPath = \dirname(__DIR__) . '/Migration';
 
         // configure migration directories
-        $majors = ['V6_4', 'V6_5', 'V6_6', 'V6_7', 'V6_8'];
+        $majors = ['V6_4', 'V6_5', 'V6_6', 'V6_7'];
         foreach ($majors as $major) {
             $migrationPathV5 = $migrationPath . '/' . $major;
 

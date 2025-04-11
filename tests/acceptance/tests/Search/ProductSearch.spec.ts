97bdbbb791f3beb9a17fda9d1856c74378546c01
Daniel Meyer <d.meyer@shopware.com>
2025-04-11 12:31:32 +0200

    fix: add InstanceMeta check

--- tests/acceptance/tests/Search/ProductSearch.spec.ts
+++ tests/acceptance/tests/Search/ProductSearch.spec.ts
@@ -6,44 +6,52 @@ test('Customer is able to search products in shop', { tag: '@Search' }, async ({
     StorefrontHome,
     StorefrontSearchSuggest,
     SearchForTerm,
-    IdProvider
+    IdProvider,
+    InstanceMeta,
 }) => {
         const productNameSuffix1 = IdProvider.getIdPair().uuid;
-        const productNameSuffix2 = IdProvider.getIdPair().uuid;
         await TestDataService.createBasicProduct({
-            name: `Product1 - ${productNameSuffix1}`,
+            name: `Bottle${productNameSuffix1}`,
         });
         await TestDataService.createBasicProduct({
-            name: `Product2 - ${productNameSuffix2}`,
+            name: `Bowl${productNameSuffix1}`,
         });
 
         await test.step('Customer searches with an invalid input and sees no results', async () => {
             await ShopCustomer.goesTo(StorefrontHome.url());
-            await ShopCustomer.attemptsTo(SearchForTerm('thisProductDoesNotExist'));
+            await ShopCustomer.attemptsTo(SearchForTerm('thisShouldNotFindAnything'));
             await ShopCustomer.expects(StorefrontSearchSuggest.searchSuggestNoResult).toBeVisible();
         });
 
         await test.step('Customer searches term and sees a single matching product', async () => {
-            await ShopCustomer.attemptsTo(SearchForTerm(productNameSuffix2));
-            const totalCount1 = await StorefrontSearchSuggest.getTotalSearchResultCount();
-            await ShopCustomer.expects(totalCount1).toBe(1);
+            await ShopCustomer.attemptsTo(SearchForTerm(`Bottle${productNameSuffix1}`));
+            // eslint-disable-next-line playwright/no-conditional-in-test
+            if (InstanceMeta.isSaaS) {
+                let productFound = false;
+                for (const lineItem of await StorefrontSearchSuggest.searchSuggestLineItemName.all()) {
+                    const lineItemText = await lineItem.textContent();
+                    // eslint-disable-next-line playwright/no-conditional-in-test
+                    if (lineItemText.includes(`Bottle${productNameSuffix1}`)) {
+                        productFound = true;
+                        break;
+                    }
+                }
+                ShopCustomer.expects(productFound).toBe(true);
+            } else {
+                const totalCount1 = await StorefrontSearchSuggest.getTotalSearchResultCount();
+                await ShopCustomer.expects(totalCount1).toBe(1);
+            }
         });
 
         await test.step('Customer searches for a partial term and sees multiple matching products', async () => {
-            await ShopCustomer.attemptsTo(SearchForTerm(productNameSuffix1 + ' ' + productNameSuffix2));
-            // todo: The test needs to have something to wait for in order to get the correct totalCount2. Option 1 is 2 lines and needs a new locator. Option 2 is 1 line, uses an existing locator, but is similar to the actual assertion.
-            // option1:
-            // await ShopCustomer.expects(StorefrontSearchSuggest.page.locator('.header-search-btn')).toBeDisabled();
-            // await ShopCustomer.expects(StorefrontSearchSuggest.page.locator('.header-search-btn')).not.toBeDisabled();
-            // option2:
-            // await ShopCustomer.expects(StorefrontSearchSuggest.searchSuggestLineItemName).toHaveCount(2);
+            await ShopCustomer.attemptsTo(SearchForTerm(productNameSuffix1));
             const totalCount2 = await StorefrontSearchSuggest.getTotalSearchResultCount();
             await ShopCustomer.expects(totalCount2).toBe(2);
         });
 
         await test.step('Customer navigates to the results page to view all matching products', async () => {
             await StorefrontSearchSuggest.searchSuggestTotalLink.click();
-            await ShopCustomer.expects(StorefrontSearchSuggest.searchHeadline).toContainText(productNameSuffix1 + ' ' + productNameSuffix2);
+            await ShopCustomer.expects(StorefrontSearchSuggest.searchHeadline).toContainText(productNameSuffix1);
             const listedItemsCount = await StorefrontSearchSuggest.productListItems.count();
             await ShopCustomer.expects(listedItemsCount).toBe(2);
         });

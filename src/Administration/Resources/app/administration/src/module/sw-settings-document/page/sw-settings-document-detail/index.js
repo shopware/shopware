1b80c5d8d5814516c72a7cbe7c78d8219651889d
Dennis Garding <d.garding@shopware.com>
2025-04-15 14:19:54 +0200

    Adjust initial config

--- src/Administration/Resources/app/administration/src/module/sw-settings-document/page/sw-settings-document-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-settings-document/page/sw-settings-document-detail/index.js
@@ -49,6 +49,7 @@ export default {
             documentConfig: {
                 config: {
                     displayAdditionalNoteDelivery: false,
+                    fileTypes: [],
                 },
             },
             documentConfigSalesChannelOptionsCollection: [],

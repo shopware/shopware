8d1ae10da0f57c3163331fc6d4aea46e57132797
Dominik Grothaus <d.grothaus@shopware.com>
2025-04-15 08:51:15 +0200

    fix: Check valid category ids or throw exception

--- src/Core/Content/Category/Event/SalesChannelCategoryIdsFetchedEvent.php
+++ src/Core/Content/Category/Event/SalesChannelCategoryIdsFetchedEvent.php
@@ -2,6 +2,7 @@
 
 namespace Shopware\Core\Content\Category\Event;
 
+use Shopware\Core\Content\Category\CategoryException;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Event\ShopwareEvent;
 use Shopware\Core\Framework\Log\Package;
@@ -28,14 +29,14 @@ final class SalesChannelCategoryIdsFetchedEvent extends Event implements Shopwar
     private array $categoryIds = [];
 
     /**
-     * @param list<string> $categoryIds
+     * @param list<string> $categoryIds Category ids **must** be provided as hex strings
      */
     public function __construct(
         array $categoryIds,
         private readonly SalesChannelContext $context
     ) {
         foreach ($categoryIds as $categoryId) {
-            \assert(Uuid::isValid($categoryId));
+            $this->checkValidCategoryIdOrThrow($categoryId);
             $this->categoryIds[$categoryId] = $categoryId;
         }
     }
@@ -48,8 +49,12 @@ final class SalesChannelCategoryIdsFetchedEvent extends Event implements Shopwar
         return \array_values($this->categoryIds);
     }
 
+    /**
+     * @param string $categoryId Category ID to check as hex string
+     */
     public function hasId(string $categoryId): bool
     {
+        $this->checkValidCategoryIdOrThrow($categoryId);
         return \array_key_exists($categoryId, $this->categoryIds);
     }
 
@@ -61,15 +66,22 @@ final class SalesChannelCategoryIdsFetchedEvent extends Event implements Shopwar
         return \array_values($this->filteredIds);
     }
 
+    /**
+     * @param string $categoryId Category ID to remove from IDs as hex string
+     */
     public function filterId(string $categoryId): void
     {
-        \assert(Uuid::isValid($categoryId));
+        $this->checkValidCategoryIdOrThrow($categoryId);
         $this->filteredIds[$categoryId] = $categoryId;
         unset($this->categoryIds[$categoryId]);
     }
 
+    /**
+     * @param string $categoryId Category ID to check for being removed from IDs as hex string
+     */
     public function isFiltered(string $categoryId): bool
     {
+        $this->checkValidCategoryIdOrThrow($categoryId);
         return \array_key_exists($categoryId, $this->filteredIds);
     }
 
@@ -81,5 +93,13 @@ final class SalesChannelCategoryIdsFetchedEvent extends Event implements Shopwar
     public function getContext(): Context
     {
         return $this->context->getContext();
+    }
+
+    private function checkValidCategoryIdOrThrow(string $categoryId): void
+    {
+        if (Uuid::isValid($categoryId)) {
+            return;
+        }
+        throw CategoryException::categoryIdIsNotValidHex($categoryId);
     }
 }

25f5ba5a55200c888f36c929680e1940d80b3709
Dennis Garding <d.garding@shopware.com>
2025-04-28 13:05:28 +0200

    Change exception to domain exception

--- src/Core/Framework/Webhook/BusinessEventEncoder.php
+++ src/Core/Framework/Webhook/BusinessEventEncoder.php
@@ -111,7 +111,7 @@ class BusinessEventEncoder
             case ArrayType::TYPE:
                 return $this->encodeArray($dataType, $property);
             default:
-                throw new \RuntimeException('Unknown EventDataType: ' . $dataType['type']);
+                throw WebhookException::unknownEventDataType($dataType['type']);
         }
     }
 
@@ -138,13 +138,7 @@ class BusinessEventEncoder
             return $object[$propertyName];
         }
 
-        throw new \RuntimeException(
-            \sprintf(
-                'Invalid available DataMapping, could not get property "%s" on instance of %s',
-                $propertyName,
-                \is_object($object) ? $object::class : 'array'
-            )
-        );
+        throw WebhookException::invalidDataMapping($propertyName, \is_object($object) ? $object::class : 'array');
     }
 
     /**

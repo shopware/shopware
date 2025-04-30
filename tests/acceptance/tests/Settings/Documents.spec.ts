226b7e92852ca0e8460e8c4a50ba020529719fc6
Daniel Meyer <d.meyer@shopware.com>
2025-04-30 16:53:12 +0200

    add document creation via API

--- tests/acceptance/tests/Settings/Documents.spec.ts
+++ tests/acceptance/tests/Settings/Documents.spec.ts
@@ -29,7 +29,6 @@ test('As an admin, I want to create documents and make sure they contain certain
         'position': 2,
         'states': [],
         'price': {
-            'extensions': [],
             'unitPrice': -1.0,
             'totalPrice': -1.0,
             'calculatedTaxes': [
@@ -38,7 +37,7 @@ test('As an admin, I want to create documents and make sure they contain certain
                     'tax': -0.16,
                     'taxRate': 19.0,
                     'price': -1.0,
-                    'label': null,
+
                 },
             ],
             'taxRules': [
@@ -49,31 +48,26 @@ test('As an admin, I want to create documents and make sure they contain certain
                 },
             ],
             'quantity': 1,
-            'referencePrice': null,
-            'listPrice': null,
-            'regulationPrice': null,
         },
-        'priceDefinition': {
-            'extensions': [],
-            'price': -1.0,
-            'filter': {
-                '_name': 'cartLineItemOfType',
-                'operator': '!=',
-                'lineItemType': 'credit',
-            },
-            'type': 'absolute',
-        },
-        'unitPrice': -1.0,
-        'totalPrice': -1.0,
-        'description': 'credit line item',
-        'type': 'credit',
-        'customFields': null,
-        'apiAlias': 'order_line_item_foreign_keys_extension',
     }
-    const ruleResponse = await AdminApiContext.post('order-line-item', {
+    const productResponse = await AdminApiContext.post('order-line-item', {
         data: creditItem,
     });
-    ShopAdmin.expects(ruleResponse.ok()).toBeTruthy();
+    ShopAdmin.expects(productResponse.ok()).toBeTruthy();
+
+    const orderInvoice = {
+        'data':
+        {
+            'orderId': order.id,
+            'config': {
+                'name': 'invoice',
+            },
+        },
+    }
+    const orderResponse = await AdminApiContext.post('_action/order/document/invoice/create', {
+        data: orderInvoice,
+    });
+    ShopAdmin.expects(orderResponse.ok()).toBeTruthy();
 
     await test.step('Go to documents settings page and activate documents in customer accounts', async () => {
         await ShopAdmin.goesTo(AdminDocumentListing.url());
@@ -86,22 +80,14 @@ test('As an admin, I want to create documents and make sure they contain certain
         await ShopAdmin.goesTo(AdminOrderDetail.url(order.id, 'general'));
         await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-data-grid__row--1')).toContainText('CreditItem');
         });
-    await test.step('Go to documents tab and create invoice', async () => {
+    await test.step('Go to documents tab and send invoice', async () => {
         await ShopAdmin.goesTo(AdminOrderDetail.url(order.id, 'documents'));
-        await ShopAdmin.expects(AdminOrderDetail.page.locator('.sw-empty-state')).toContainText('No documents created yet');
-        await AdminOrderDetail.page.getByRole('button', { name: 'Create new document' }).click();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-modal__body')).toContainText('Invoice');
-        await AdminDocumentDetail.page.locator('.sw-modal__body').getByRole('radio', { name: 'Invoice', exact: true }).click();
-        await AdminDocumentDetail.page.locator('.sw-modal__footer').getByRole('button', { name: 'Create document' }).click();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-modal__header')).toHaveText('Create document - Invoice');
-        //await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-context-button__menu-position')).toBeVisible();
-        await AdminDocumentDetail.page.getByTestId('mt-icon__regular-chevron-down-xs').nth(1).click();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-context-menu')).toBeVisible();
-        await AdminDocumentDetail.page.locator('.sw-context-menu-item__text').getByText('Create and send').click();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.mt-button__loader')).not.toBeVisible();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.sw-modal__header')).toHaveText('Send document');
-        await AdminDocumentDetail.page.locator('.sw-modal__footer').getByRole('button', { name: 'Send document' }).click();
-        await ShopAdmin.expects(AdminDocumentDetail.page.locator('.mt-button__loader')).not.toBeVisible();
+        await ShopAdmin.expects(AdminOrderDetail.page.locator('.sw-data-grid__table')).toContainText('Invoice');
+        await AdminOrderDetail.page.getByLabel('Open actions menu').click();
+        await ShopAdmin.expects(AdminOrderDetail.page.locator('.sw-context-menu')).toBeVisible();
+        await AdminOrderDetail.page.locator('.sw-context-menu').getByText('Send document').click();
+        await ShopAdmin.expects(AdminOrderDetail.page.locator('.sw-order-send-document-modal')).toBeVisible();
+        await AdminOrderDetail.page.getByRole('button').getByText('Send document').click();
         });
     await test.step('Go to customer account in Storefront and check the order document', async () => {
         await ShopCustomer.goesTo(StorefrontAccountLogin.url());

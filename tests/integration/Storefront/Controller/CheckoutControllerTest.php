d824e64f324834ecfed7df71c5fa8af99bc9dd07
YTN <nguyenytran06@gmail.com>
2025-05-05 09:09:39 +0000

    fix: sales-channel-measurement-system entity (#8892)

--- tests/integration/Storefront/Controller/CheckoutControllerTest.php
+++ tests/integration/Storefront/Controller/CheckoutControllerTest.php
@@ -609,6 +609,9 @@ class CheckoutControllerTest extends TestCase
             'shippingMethods' => [['id' => $shippingMethodId]],
             'countries' => [['id' => $countryId]],
             'customerGroupId' => TestDefaults::FALLBACK_CUSTOMER_GROUP,
+            'measurementSystemId' => Uuid::fromStringToHex('metric'),
+            'lengthUnitId' => Uuid::fromStringToHex('metric-mm'),
+            'massUnitId' => Uuid::fromStringToHex('metric-kg'),
             'domains' => [
                 [
                     'id' => $salesChannelId,
@@ -616,6 +619,9 @@ class CheckoutControllerTest extends TestCase
                     'snippetSetId' => $snippetSetId,
                     'currencyId' => Defaults::CURRENCY,
                     'url' => 'http://test.123',
+                    'measurementSystemId' => Uuid::fromStringToHex('metric'),
+                    'lengthUnitId' => Uuid::fromStringToHex('metric-mm'),
+                    'massUnitId' => Uuid::fromStringToHex('metric-kg'),
                 ],
             ],
         ];

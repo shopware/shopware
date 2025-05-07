634607f211a8069730a6e324e84276287542be3e
Philipp Dinkhoff <130650198+philippdinkhoff@users.noreply.github.com>
2025-05-07 18:05:34 +0200

    fix: mail templates without sender name are not send (#8740)

--- tests/unit/Core/Content/Mail/Service/MailServiceTest.php
+++ tests/unit/Core/Content/Mail/Service/MailServiceTest.php
@@ -226,7 +226,7 @@ class MailServiceTest extends TestCase
         $data = [
             'recipients' => [],
             'subject' => 'Test email',
-            'senderName' => 'me@shopware.com',
+            'senderName' => null,
             'contentPlain' => 'Content plain',
             'contentHtml' => 'Content html',
             'salesChannelId' => $salesChannelId,

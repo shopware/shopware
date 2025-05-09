9a2a46fdbfa4ab93f9ea41f9566807454a285dac
Lee Nguyen <l.nguyen@shopware.com>
2025-05-09 10:46:25 +0700

    feat: Add measurement settings into sales channel configuration (#8841)

---
+++ src/Administration/Resources/app/administration/src/module/sw-sales-channel/component/sw-sales-channel-measurement/index.js
@@ -0,0 +1,135 @@
+/**
+ * @sw-package inventory
+ */
+
+import template from './sw-sales-channel-measurement.html.twig';
+
+const { Criteria } = Shopware.Data;
+
+// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
+export default Shopware.Component.wrapComponentConfig({
+    template,
+
+    props: {
+        salesChannel: {
+            type: Object,
+            required: true,
+        },
+
+        labelUnitSystem: {
+            type: String,
+            required: false,
+        },
+
+        labelLengthUnit: {
+            type: String,
+            required: false,
+        },
+
+        labelWeightUnit: {
+            type: String,
+            required: false,
+        },
+    },
+
+    emits: [
+        'measurement-system-change',
+    ],
+
+    data() {
+        return {
+            measurementSystem: null,
+            defaultMeasurementSystem: null,
+        };
+    },
+
+    computed: {
+        measurementSystemCriteria() {
+            const criteria = new Criteria(1, null);
+            criteria.addAssociation('units');
+
+            criteria.getAssociation('units').addFilter(Criteria.equals('default', true));
+
+            return criteria;
+        },
+
+        lengthUnitCriteria() {
+            const criteria = new Criteria(1, null);
+            criteria.addFilter(Criteria.equals('type', 'length'));
+            if (this.salesChannel?.measurementSystemId) {
+                criteria.addFilter(Criteria.equals('measurementSystem.id', this.salesChannel.measurementSystemId));
+            }
+
+            return criteria;
+        },
+
+        massUnitCriteria() {
+            const criteria = new Criteria(1, null);
+            criteria.addFilter(Criteria.equals('type', 'mass'));
+            if (this.salesChannel.measurementSystemId) {
+                criteria.addFilter(Criteria.equals('measurementSystem.id', this.salesChannel.measurementSystemId));
+            }
+
+            return criteria;
+        },
+
+        unitSystemLabel() {
+            return this.labelUnitSystem || this.$t('sw-sales-channel.detail.measurementSystem.labelUnitSystem');
+        },
+
+        dimensionUnitLabel() {
+            return this.labelLengthUnit || this.$t('sw-sales-channel.detail.measurementSystem.labelLengthUnit');
+        },
+
+        weightUnitLabel() {
+            return this.labelWeightUnit || this.$t('sw-sales-channel.detail.measurementSystem.labelWeightUnit');
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    methods: {
+        createdComponent() {
+            this.defaultMeasurementSystem = {
+                measurementSystemId: this.salesChannel.measurementSystemId,
+                lengthUnitId: this.salesChannel.lengthUnitId,
+                massUnitId: this.salesChannel.massUnitId,
+            };
+        },
+
+        onMeasurementSystemChange(measurementSystemId) {
+            const measurementSystemSelect = this.$refs.measurementSystemSelect;
+
+            const measurementSystem = measurementSystemSelect?.results?.get(measurementSystemId);
+
+            this.$emit('measurement-system-change', measurementSystemId, measurementSystem);
+
+            // Update the measurementSystem property on the sales channel
+            this.salesChannel.measurementSystem = measurementSystem;
+
+            if (measurementSystemId === this.defaultMeasurementSystem.measurementSystemId) {
+                this.salesChannel.lengthUnitId = this.defaultMeasurementSystem.lengthUnitId;
+                this.salesChannel.massUnitId = this.defaultMeasurementSystem.massUnitId;
+
+                return;
+            }
+
+            this.salesChannel.lengthUnitId = measurementSystem?.units?.filter((unit) => unit.type === 'length').first()?.id;
+
+            this.salesChannel.massUnitId = measurementSystem?.units?.filter((unit) => unit.type === 'mass').first()?.id;
+        },
+
+        formatUnitLabel(item) {
+            if (!item) {
+                return '';
+            }
+
+            const name = item.translated?.name || item.name;
+            const shortName = item.shortName || item.name;
+
+            return `${name} (${shortName})`.trim();
+        },
+    },
+});

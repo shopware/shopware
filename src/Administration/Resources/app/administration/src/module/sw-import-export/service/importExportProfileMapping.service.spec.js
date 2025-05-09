d7b76adce51f882c676909af35337f609c22f38f
akf-bw <benjamin.wittwer@a-k-f.de>
2025-05-09 08:32:12 +0000

    Remove invalid fields from import export

--- src/Administration/Resources/app/administration/src/module/sw-import-export/service/importExportProfileMapping.service.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-import-export/service/importExportProfileMapping.service.spec.js
@@ -329,7 +329,6 @@ describe('module/sw-import-export/service/importExportProfileMapping.service.spe
 
         expect(Object.keys(systemRequiredFields)).toEqual([
             'id',
-            'combinable',
             'group.id',
             'translations.DEFAULT.name',
         ]);
@@ -340,7 +339,6 @@ describe('module/sw-import-export/service/importExportProfileMapping.service.spe
 
         expect(Object.keys(systemRequiredFields)).toEqual([
             'id',
-            'combinable',
             'group.id',
             'group.displayType',
             'group.sortingType',
@@ -381,7 +379,6 @@ describe('module/sw-import-export/service/importExportProfileMapping.service.spe
             'product.tax.position',
             'product.translations.DEFAULT.name',
             'option.id',
-            'option.combinable',
             'option.group.id',
             'option.group.displayType',
             'option.group.sortingType',

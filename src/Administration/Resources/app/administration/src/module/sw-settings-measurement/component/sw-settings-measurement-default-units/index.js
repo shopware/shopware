3d3f66b0848228b2f39728f09e4c8d8259a4cec9
Sy Dinh <s.dinh@shopware.com>
2025-05-09 10:38:30 +0700

    feat: add sw-settings-measurement module (#9156)

---
+++ src/Administration/Resources/app/administration/src/module/sw-settings-measurement/component/sw-settings-measurement-default-units/index.js
@@ -0,0 +1,55 @@
+/**
+ * @sw-package inventory
+ */
+import template from './sw-settings-measurement-default-units.html.twig';
+import './sw-settings-measurement-default-units.scss';
+
+// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
+export default {
+    template,
+
+    emits: ['measurement-system-change'],
+
+    props: {
+        measurementSystem: {
+            type: Object,
+            required: true,
+        },
+        lengthUnitCriteria: {
+            type: Object,
+            required: true,
+        },
+        massUnitCriteria: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    methods: {
+        onChangeMeasurementSystem() {
+            this.$emit('measurement-system-change');
+        },
+
+        labelSystemCallback(item) {
+            if (!item) {
+                return '';
+            }
+
+            const name = item.translated?.name || item.name;
+            const systemLabel = this.$t('sw-settings-measurement.defaultUnits.system');
+
+            return `${name} ${systemLabel}`.trim();
+        },
+
+        labelUnitCallback(item) {
+            if (!item) {
+                return '';
+            }
+
+            const name = item.translated?.name || item.name;
+            const shortName = item.shortName || item.name;
+
+            return `${name} (${shortName})`.trim();
+        },
+    },
+};

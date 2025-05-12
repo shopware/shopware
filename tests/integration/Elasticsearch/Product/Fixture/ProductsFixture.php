3bb16c3b707dc1530bba986bc5f4c96395fbea48
Michael Telgmann <m.telgmann@shopware.com>
2025-05-12 16:37:15 +0200

    improve array type

--- tests/integration/Elasticsearch/Product/Fixture/ProductsFixture.php
+++ tests/integration/Elasticsearch/Product/Fixture/ProductsFixture.php
@@ -16,7 +16,7 @@ use Shopware\Core\Test\TestDefaults;
 class ProductsFixture
 {
     /**
-     * @return array<mixed>
+     * @return list<array<mixed>>
      */
     public static function get(IdsCollection $ids, string $secondLanguage, string $thirdLanguage): array
     {

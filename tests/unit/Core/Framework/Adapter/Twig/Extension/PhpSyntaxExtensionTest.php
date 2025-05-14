a12aecd8692508aacf42bf888a7544863e03b158
Nicolas Fortier <n.fortier@shopware.com>
2025-05-14 19:02:58 +0200

    move test to unit

--- tests/unit/Core/Framework/Adapter/Twig/Extension/PhpSyntaxExtensionTest.php
+++ tests/unit/Core/Framework/Adapter/Twig/Extension/PhpSyntaxExtensionTest.php
@@ -1,25 +1,45 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Tests\Integration\Core\Framework\Adapter\Twig\Extension;
+namespace Shopware\Tests\Unit\Core\Framework\Adapter\Twig\Extension;
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Adapter\Twig\Extension\PhpSyntaxExtension;
 use Shopware\Core\Framework\Adapter\Twig\StringTemplateRenderer;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Struct\ArrayStruct;
-use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
 
 /**
  * @internal
  */
+#[CoversClass(PhpSyntaxExtension::class)]
 class PhpSyntaxExtensionTest extends TestCase
 {
-    use IntegrationTestBehaviour;
+    public function testEmptyOperators(): void
+    {
+        $extension = new PhpSyntaxExtension();
+
+        // Since Twig 3.21 using operators is deprecated, but still supported
+        static::assertSame([[], []], $extension->getOperators());
+
+        // The operators are replaced by expression parsers
+        static::assertCount(4, $extension->getExpressionParsers());
+        static::assertSame('||', $extension->getExpressionParsers()[0]->getName());
+        static::assertSame('&&', $extension->getExpressionParsers()[1]->getName());
+        static::assertSame('===', $extension->getExpressionParsers()[2]->getName());
+        static::assertSame('!==', $extension->getExpressionParsers()[3]->getName());
+    }
 
     public function testSyntax(): void
     {
         $template = file_get_contents(__DIR__ . '/fixture/php-syntax-extension.html.twig');
         static::assertIsString($template);
-        $renderer = static::getContainer()->get(StringTemplateRenderer::class);
+
+        $environment = new Environment(new ArrayLoader());
+        $environment->addExtension(new PhpSyntaxExtension());
+        $renderer = new StringTemplateRenderer($environment, sys_get_temp_dir());
 
         $jsonEncodeData = [
             -4,

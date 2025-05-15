e6522e24fc3cf799f5d443f4f9d5f32b970ff0bf
Felix Frank <f.frank@shopware.com>
2025-05-15 10:22:51 +0200

    feat: Added dive as a 3D viewer for the storefront.

--- src/Storefront/Resources/app/storefront/src/plugin/spatial/spatial-gallery-slider-viewer.plugin.ts
+++ src/Storefront/Resources/app/storefront/src/plugin/spatial/spatial-gallery-slider-viewer.plugin.ts
@@ -1,12 +1,6 @@
 import SpatialBaseViewerPlugin from './spatial-base-viewer.plugin';
 import SpatialProductSliderRenderUtil from './utils/spatial-product-slider-render-util';
-import SpatialCanvasSizeUpdateUtil from './utils/spatial-canvas-size-update-util';
-import SpatialObjectLoaderUtil from './utils/spatial-object-loader-util';
-import SpatialOrbitControlsUtil from './utils/spatial-orbit-controls-util';
-import SpatialMovementNoteUtil from './utils/spatial-movement-note-util';
-import { type Object3D } from 'three';
-import SpatialLightCompositionUtil from './utils/composition/spatial-light-composition-util';
-import { loadThreeJs } from './utils/spatial-threejs-load-util';
+import { loadDIVE } from './utils/spatial-dive-load-util';
 
 /**
  * @package innovation
@@ -16,24 +10,16 @@ import { loadThreeJs } from './utils/spatial-threejs-load-util';
 export default class SpatialGallerySliderViewerPlugin extends SpatialBaseViewerPlugin {
 
     private spatialProductSliderRenderUtil: SpatialProductSliderRenderUtil | undefined;
-    private spatialCanvasSizeUpdateUtil: SpatialCanvasSizeUpdateUtil | undefined;
-    private spatialObjectLoaderUtil: SpatialObjectLoaderUtil | undefined;
-    private spatialOrbitControlsUtil: SpatialOrbitControlsUtil | undefined;
-    private spatialLightCompositionUtil: SpatialLightCompositionUtil | undefined;
-
-    private model: Object3D | undefined;
-
     public sliderIndex: number | undefined;
 
     public el: HTMLElement | undefined;
-    private spatialMovementNoteUtil: SpatialMovementNoteUtil | undefined;
 
     /**
      * initialize plugin
      * does not initialize the 3d scene
      */
     async init() {
-        await loadThreeJs();
+        await loadDIVE();
 
         if (!this.el) {
             return;
@@ -45,89 +31,23 @@ export default class SpatialGallerySliderViewerPlugin extends SpatialBaseViewerP
 
         this.spatialProductSliderRenderUtil.removeDisabled();
 
-        this.initViewer(true);
+        await this.initViewer();
     }
 
     /**
      * initialize the 3d scene
      * @param force - Will reinitialize the viewer. Otherwise, only the canvas and renderer will be reinitialized.
      */
-    public initViewer(force: boolean) {
-        super.initViewer(force);
-
-        // Set up the renderer
-        this.renderer?.setClearColor(0xffffff, 0);
-
-        // Set up the camera
-        this.camera?.position.set(0, 0.6, 1.2);
-        // @ts-ignore
-        this.camera?.lookAt(0, 0, 0);
-
-        // Set up the orbit controls
-        // We need to dispose of the old orbit controls if they exist
-        this.spatialOrbitControlsUtil?.dispose();
-        if (this.camera && this.renderer) {
-            this.spatialOrbitControlsUtil = new SpatialOrbitControlsUtil(this.camera, this.renderer.domElement);
-        }
-
-        // Set up move note
-        this.spatialMovementNoteUtil = new SpatialMovementNoteUtil(this);
-
-        // Set up canvas size updater
-        this.spatialCanvasSizeUpdateUtil = new SpatialCanvasSizeUpdateUtil(this);
-
-        // Set up the lights
-        if (this.spatialLightCompositionUtil == undefined || force) {
-            this.spatialLightCompositionUtil?.dispose();
-            if (this.scene) {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
-                this.spatialLightCompositionUtil = new SpatialLightCompositionUtil(this.scene, this.options.lightIntensity);
-            }
-        }
-
-        // Set up the object loader
-        if (this.spatialObjectLoaderUtil == undefined || force) {
-            this.spatialObjectLoaderUtil = new SpatialObjectLoaderUtil(this);
-        }
-
-        if (this.model == undefined || force) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            const modelUrl: string = this.options.modelUrl;
-            if (modelUrl == null) {
-                return;
-            }
-            this.spatialObjectLoaderUtil.loadSingleObjectByUrl(
-                modelUrl,
-                {
-                    center: true,
-                    clampSize: true,
-                    clampMaxSize: { x: 4 / 3, y: 1, z: 4 / 3 },
-                }
-            ).then((model) => {
-                this.model = model;
-                if (this.scene) {
-                    this.scene.add(this.model);
-                }
-
-                this.setReady(true);
-            }).catch(() => {
-                this.el?.parentElement?.parentElement?.classList.add('gallery-slider-canvas-disabled');
-            });
-        } else {
-            // set ready if the model is already loaded
+    public async initViewer() {
+        try {
+            await super.initViewer();
+        } catch (e) {
+            this.el?.parentElement?.parentElement?.classList.add('gallery-slider-canvas-disabled');
+        } finally {
             this.setReady(true);
+
+            // start rendering when on the correct slide
+            this.spatialProductSliderRenderUtil?.initRender();
         }
-
-        // start rendering when on the correct slide
-        this.spatialProductSliderRenderUtil?.initRender();
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    protected preRender(delta: number) {
-        this.spatialCanvasSizeUpdateUtil?.update();
-        this.spatialOrbitControlsUtil?.update();
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-    protected postRender(delta: number) { }
 }

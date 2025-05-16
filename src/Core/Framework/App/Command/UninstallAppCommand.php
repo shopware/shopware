d81f7ef0a043905db7389556c9b92e67d236a960
Jonas Elfering <j.elfering@shopware.com>
2025-05-16 10:38:04 +0200

    Fix codestyle

--- src/Core/Framework/App/Command/UninstallAppCommand.php
+++ src/Core/Framework/App/Command/UninstallAppCommand.php
@@ -11,7 +11,6 @@ use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Log\Package;
-use Shopware\Core\Framework\Plugin\PluginLifecycleService;
 use Shopware\Storefront\Theme\ThemeLifecycleHandler;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -83,9 +82,18 @@ class UninstallAppCommand extends Command
     protected function configure(): void
     {
         $this->addArgument('name', InputArgument::REQUIRED, 'The name of the app');
-        $this->addOption('keep-user-data', null, InputOption::VALUE_NONE, 'Keep user data of the app');
-        $this->addOption('skip-theme-recompile', null, InputOption::VALUE_NONE, 'Use this option to skip recompiling of all themes'
-    );
+        $this->addOption(
+            'keep-user-data',
+            null,
+            InputOption::VALUE_NONE,
+            'Keep user data of the app'
+        );
+        $this->addOption(
+            'skip-theme-recompile',
+            null,
+            InputOption::VALUE_NONE,
+            'Use this option to skip recompiling of all themes'
+        );
     }
 
     private function getAppByName(string $name, Context $context): ?AppEntity

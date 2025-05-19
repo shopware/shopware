a1cfc040932a870473dafb9ea23acb50d46f6dfb
Dennis Garding <d.garding@shopware.com>
2025-05-19 07:45:57 +0200

    fix: tests

--- src/Core/Framework/Api/Controller/Exception/AuthThrottledException.php
+++ src/Core/Framework/Api/Controller/Exception/AuthThrottledException.php
@@ -23,21 +23,21 @@ class AuthThrottledException extends ShopwareHttpException
 
     public function getErrorCode(): string
     {
-        Feature::triggerDeprecationOrThrow('v6.7.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.7.0.0'));
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.8.0.0'));
 
         return 'FRAMEWORK__AUTH_THROTTLED';
     }
 
     public function getStatusCode(): int
     {
-        Feature::triggerDeprecationOrThrow('v6.7.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.7.0.0'));
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.8.0.0'));
 
         return Response::HTTP_TOO_MANY_REQUESTS;
     }
 
     public function getWaitTime(): int
     {
-        Feature::triggerDeprecationOrThrow('v6.7.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.7.0.0'));
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', Feature::deprecatedMethodMessage(__CLASS__, __METHOD__, 'v6.8.0.0'));
 
         return $this->waitTime;
     }

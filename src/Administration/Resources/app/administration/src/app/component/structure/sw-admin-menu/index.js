a57c6f130984357a904450c8437d0d6050235a6e
Nils Haberkamp <haberkamp.n@gmail.com>
2025-05-19 16:30:20 +0200

    wip

--- src/Administration/Resources/app/administration/src/app/component/structure/sw-admin-menu/index.js
+++ src/Administration/Resources/app/administration/src/app/component/structure/sw-admin-menu/index.js
@@ -1,6 +1,7 @@
 import template from './sw-admin-menu.html.twig';
 import './sw-admin-menu.scss';
 import { MtText } from '@shopware-ag/meteor-component-library';
+const { Criteria } = Shopware.Data;
 
 /**
  * @sw-package framework
@@ -14,9 +15,14 @@ export default {
         MtText,
     },
 
+    inject: [
+        'repositoryFactory',
+    ],
+
     data() {
         return {
-            isDarkMode: false
+            isDarkMode: false,
+            salesChannels: []
         };
     },
 
@@ -31,5 +37,42 @@ export default {
             },
             immediate: true
         }
+    },
+
+    created() {
+        this.salesChannelRepository.search(this.salesChannelCriteria).then((response) => {
+            this.salesChannels = response;
+        });
+    },
+
+    computed: {
+        salesChannelRepository() {
+            return this.repositoryFactory.create('sales_channel');
+        },
+
+        salesChannelCriteria() {
+            const criteria = new Criteria(1, 7);
+
+            criteria.addIncludes({
+                sales_channel: [
+                    'name',
+                    'type',
+                    'active',
+                    'translated',
+                    'domains',
+                ],
+                sales_channel_type: ['iconName'],
+                sales_channel_domain: [
+                    'url',
+                    'languageId',
+                ],
+            });
+
+            criteria.addSorting(Criteria.sort('sales_channel.name', 'ASC'));
+            criteria.addAssociation('type');
+            criteria.addAssociation('domains');
+
+            return criteria;
+        },
     }
 };

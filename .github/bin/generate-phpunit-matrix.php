4fbdbab0c4d38c4f08064574b545bab6e6b50a23
Patrick Weyck <p.weyck@shopware.com>
2025-05-19 10:00:12 +0200

    tmp: next try

--- .github/bin/generate-phpunit-matrix.php
+++ .github/bin/generate-phpunit-matrix.php
@@ -7,7 +7,7 @@ $nightly = $_SERVER['argv'][1] ?? false;
 
 if ($nightly || true) {
     $php = ['8.4'];
-    $db = ['mariadb:11.7.2', 'mariadb:11.6.2', 'mariadb:11.4', 'mariadb:11.8-rc'];
+    $db = ['mariadb:11.7.2', 'mariadb:11.6.1', 'mariadb:11.4.4', 'mariadb:11.8-rc'];
 }
 
 echo \json_encode([

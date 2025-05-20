989859b9dc4f8ec46a3cc6e5b60f0cb6aa82dc8c
Max Stegmeyer <m.stegmeyer@shopware.com>
2025-05-20 14:50:28 +0200

    fix: assign function

--- src/Core/Checkout/Cart/Order/OrderConversionContext.php
+++ src/Core/Checkout/Cart/Order/OrderConversionContext.php
@@ -6,6 +6,9 @@ use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Struct\Struct;
 
+/**
+ * @codeCoverageIgnore
+ */
 #[Package('checkout')]
 class OrderConversionContext extends Struct
 {

6e2b1890d2f53371933b0bb91ef82e57c46e51d9
Max Stegmeyer <40475653+mstegmeyer@users.noreply.github.com>
2025-05-22 08:25:47 +0200

    fix: overwrite of CreatedByField with non-live version (#9666)

--- src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
@@ -27,7 +27,7 @@ export default {
 
     provide() {
         return {
-            swOrderDetailOnIdentifierChange: this.updateIdentifier,
+            /** @deprecated tag:v6.8.0 - swOrderDetailOnCreatedByIdChange will be removed */
             swOrderDetailOnCreatedByIdChange: this.updateCreatedById,
             swOrderDetailOnLoadingChange: this.onUpdateLoading,
             swOrderDetailOnEditingChange: this.onUpdateEditing,
@@ -63,6 +63,7 @@ export default {
             isEditing: false,
             isLoading: true,
             isSaveSuccessful: false,
+            /** @deprecated tag:v6.8.0 - createdById will be removed */
             createdById: '',
             isDisplayingLeavePageWarning: false,
             nextRoute: null,
@@ -239,10 +240,9 @@ export default {
             }
         },
 
-        updateIdentifier(identifier) {
-            this.identifier = identifier;
-        },
-
+        /**
+         * @deprecated tag:v6.8.0 - createdById will be removed (there is a template usage that needs to be removed as well)
+         */
         updateCreatedById(createdById) {
             this.createdById = createdById;
         },

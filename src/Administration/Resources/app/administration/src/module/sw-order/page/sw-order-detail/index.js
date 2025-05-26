93086fe9a093fdea300939f821f5cd1ef97889fc
Michel <m.bade@shopware.com>
2025-05-26 08:35:43 +0200

    fix: admin promotion notifications (#9783)

--- src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/page/sw-order-detail/index.js
@@ -33,7 +33,7 @@ export default {
             swOrderDetailOnSaveAndRecalculate: this.onSaveAndRecalculate,
             swOrderDetailOnRecalculateAndReload: this.onRecalculateAndReload,
             swOrderDetailOnReloadEntityData: this.reloadEntityData,
-            swOrderDetailOnSaveAndReload: this.onSaveAndReload,
+            swOrderDetailOnSaveAndReload: this.saveAndReload,
             swOrderDetailOnSaveEdits: this.onSaveEdits,
             swOrderDetailAskAndSaveEdits: this.askAndSaveEdits,
             swOrderDetailOnError: this.onError,
@@ -390,19 +390,11 @@ export default {
         },
 
         async onSaveAndRecalculate() {
-            Store.get('swOrderDetail').setLoading(['recalculation', true]);
-
-            try {
-                await this.orderRepository.save(this.order, this.versionContext);
-                await this.orderService
+            await this.saveAndReload(() => {
+                return this.orderService
                     .recalculateOrder(this.orderId, this.versionContext.versionId, {}, {})
                     .then(this.handleCartErrors.bind(this));
-                await this.reloadEntityData();
-            } catch (error) {
-                this.onError('error', error);
-            } finally {
-                Store.get('swOrderDetail').setLoading(['recalculation', false]);
-            }
+            });
         },
 
         async onRecalculateAndReload() {
@@ -420,18 +412,27 @@ export default {
             }
         },
 
+        /**
+         * @deprecated tag:v6.8.0 - Will be replaced by `saveAndReload`
+         */
         onSaveAndReload() {
+            return this.saveAndReload();
+        },
+
+        async saveAndReload(afterSaveFn = null) {
             Store.get('swOrderDetail').setLoading(['recalculation', true]);
 
-            return this.orderRepository
-                .save(this.order, this.versionContext)
-                .then(() => this.reloadEntityData())
-                .catch((error) => {
-                    this.onError('error', error);
-                })
-                .finally(() => {
-                    Store.get('swOrderDetail').setLoading(['recalculation', false]);
-                });
+            try {
+                await this.orderRepository.save(this.order, this.versionContext);
+                if (afterSaveFn) {
+                    await afterSaveFn();
+                }
+                await this.reloadEntityData();
+            } catch (error) {
+                this.onError('error', error);
+            } finally {
+                Store.get('swOrderDetail').setLoading(['recalculation', false]);
+            }
         },
 
         /**

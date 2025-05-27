59aff57e94d5e333d964d1bf86e0c1888546dc63
Sebastian Franze <s.franze@shopware.com>
2025-05-27 17:46:02 +0200

    feat(services): Save dashboard-banner state in user_config

--- src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-dashboard-banner/index.ts
+++ src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-dashboard-banner/index.ts
@@ -19,4 +19,28 @@ export default Shopware.Component.wrapComponentConfig({
             servicesGraphics,
         };
     },
+
+    created() {
+        Shopware.Service('userConfigService')
+            .search(['core.show-services-dashboard-banner'])
+            .then((response) => {
+                if (typeof response === 'undefined') {
+                    this.isHidden = false;
+                    return;
+                }
+
+                this.isHidden = (response.data['core.show-services-dashboard-banner']?.[0] as boolean|undefined) ?? false;
+            }).catch(() => { this.isHidden = false; });
+    },
+
+    methods: {
+        async hideBanner() {
+            await Shopware.Service('userConfigService')
+                .upsert({
+                    'core.show-services-dashboard-banner': [true],
+                });
+
+            this.isHidden = true;
+        },
+    },
 })

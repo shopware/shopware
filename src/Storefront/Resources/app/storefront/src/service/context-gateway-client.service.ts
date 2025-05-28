8fc4b41a469c4233199ba8c191259167af527d87
Lennart Tinkloh <l.tinkloh@shopware.com>
2025-05-28 14:57:37 +0200

    feat: context gateway js client

---
+++ src/Storefront/Resources/app/storefront/src/service/context-gateway-client.service.ts
@@ -0,0 +1,56 @@
+interface ContextTokenResponse {
+    token: string;
+    redirectUrl?: string;
+}
+
+/**
+ * @sw-package framework
+ */
+export default class ContextGatewayClient {
+    private readonly name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    /**
+     * Calls the context gateway to apply context changes triggered by app servers
+     *
+     * @param data - custom data sent to the app server
+     * @param autoNavigate - automatically reload page or navigate to redirectUrl (happens typically via sales channel switching) if provided
+     */
+    public async call(data: Record<string, unknown> = {}, autoNavigate = false): Promise<ContextTokenResponse> {
+        const body = { ...data, appName: this.name };
+
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        const gatewayRoute = window['router']['frontend.gateway.context'] as string;
+        const response = await fetch(gatewayRoute, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            const err = await response.text();
+            throw new Error(`Context gateway request failed for app '${this.name}': ${response.status} ${response.statusText} - ${err}`);
+        }
+
+        const tokenResponse = await response.json() as ContextTokenResponse;
+
+        if (!autoNavigate) {
+            return tokenResponse;
+        }
+
+        if (tokenResponse.redirectUrl) {
+            window.location.href = tokenResponse.redirectUrl;
+            return tokenResponse;
+        }
+
+        window.location.reload();
+        return tokenResponse;
+    }
+}
---
+++ src/Storefront/Resources/app/storefront/test/service/context-gateway-client.service.test.js
@@ -0,0 +1,101 @@
+import ContextGatewayClient from '../../src/service/context-gateway-client.service';
+
+/**
+ * @package framework
+ */
+describe('Context gateway client service', () => {
+    beforeEach(() => {
+        delete window.location;
+
+        window.location = { href: '', reload: jest.fn() };
+        window['router']['frontend.gateway.context'] = 'https://example.com/gateway/context';
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should handle token response', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve({
+                    token: '12345678',
+                    redirectUrl: 'https://example.com/redirect',
+                }),
+            })
+        );
+
+        const contextGatewayClient = new ContextGatewayClient('test');
+        const result = await contextGatewayClient.call({}, false);
+
+        expect(result).toEqual({
+            token: '12345678',
+            redirectUrl: 'https://example.com/redirect',
+        });
+
+        expect(global.fetch).toHaveBeenCalledWith('https://example.com/gateway/context', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            body: JSON.stringify({ appName: 'test' }),
+        });
+
+        expect(window.location.href).toBe('');
+        expect(window.location.reload).not.toHaveBeenCalled();
+    });
+
+    it('should handle window reload on autonavigate', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve({
+                    token: '12345678',
+                }),
+            })
+        );
+
+        const contextGatewayClient = new ContextGatewayClient('test');
+        await contextGatewayClient.call({}, true);
+
+        expect(window.location.href).toBe('');
+        expect(window.location.reload).toHaveBeenCalled();
+    });
+
+    it('should handle window redirect on autonavigate', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve({
+                    token: '12345678',
+                    redirectUrl: 'https://example.com/redirect',
+                }),
+            })
+        );
+
+        const contextGatewayClient = new ContextGatewayClient('test');
+        await contextGatewayClient.call({}, true);
+
+        expect(window.location.href).toBe('https://example.com/redirect');
+        expect(window.location.reload).not.toHaveBeenCalled();
+    });
+
+    it('should handle bad requests', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: false,
+                status: 400,
+                statusText: 'Bad Request',
+                text: () => Promise.resolve('Error'),
+            })
+        );
+
+        const contextGatewayClient = new ContextGatewayClient('test');
+        await expect(contextGatewayClient.call()).rejects.toThrow('Context gateway request failed for app \'test\': 400 Bad Request - Error');
+    });
+});

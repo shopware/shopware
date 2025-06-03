c975f6efa7cd96fa1c2508957a40c25308322449
Nicolas Fortier <n.fortier@shopware.com>
2025-06-03 16:25:00 +0200

    unicity check for MissingDomain

--- tests/integration/Core/Framework/MissingDomain/SecondNotDescribedTest.php
+++ tests/integration/Core/Framework/MissingDomain/SecondNotDescribedTest.php
@@ -13,6 +13,6 @@ class SecondNotDescribedTest extends TestCase
 {
     public function testNothing(): void
     {
-        static::markTestSkipped('This is not a test, but a placeholder to ensure that the test suite fails Danger.');
+        static::markTestSkipped('This is not a test, but a placeholder to ensure that the test suite fails Danger (but MissingDomain shows only once).');
     }
 }

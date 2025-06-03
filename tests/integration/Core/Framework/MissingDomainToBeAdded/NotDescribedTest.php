cca57b25ee59fa6ebebee4fd381aeb8f21b2fc5d
Nicolas Fortier <n.fortier@shopware.com>
2025-06-03 16:08:09 +0200

    check multiple

---
+++ tests/integration/Core/Framework/MissingDomainToBeAdded/NotDescribedTest.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Integration\Core\Framework\MissingDomainToBeAdded;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * @internal
+ */
+#[Package('framework')]
+class NotDescribedTest extends TestCase
+{
+    public function testNothing(): void
+    {
+        static::markTestSkipped('This is not a test, but a placeholder to ensure that the test suite fails Danger.');
+    }
+}

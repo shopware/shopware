93ee772aed82baa92617bc916a3236d113ca797e
Michel <m.bade@shopware.com>
2025-06-04 13:39:52 +0200

    fix: add mt-link to vue adapter (#10246)

--- src/Administration/Resources/app/administration/src/app/adapter/view/vue.adapter.ts
+++ src/Administration/Resources/app/administration/src/app/adapter/view/vue.adapter.ts
@@ -382,6 +382,7 @@ export default class VueAdapter extends ViewAdapter {
             'MtModalClose',
             'MtUrlField',
             'MtSearch',
+            'MtLink',
         ];
 
         meteorComponents.forEach((componentName) => {

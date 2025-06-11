4002eca8c5ea715ea7fc132bf53d71f09aaf57ca
Soner Sayakci <s.sayakci@gmail.com>
2025-06-11 09:22:04 +0200

    fix: update test

--- src/Administration/Resources/app/administration/src/app/component/media/sw-media-upload-v2/sw-media-upload-v2.spec.js
+++ src/Administration/Resources/app/administration/src/app/component/media/sw-media-upload-v2/sw-media-upload-v2.spec.js
@@ -58,14 +58,6 @@ async function createWrapper(customOptions = {}) {
                     removeByTag: () => {},
                     removeListener: () => null,
                 },
-                configService: {
-                    getConfig: () =>
-                        Promise.resolve({
-                            settings: {
-                                enableUrlFeature: true,
-                            },
-                        }),
-                },
             },
         },
         ...customOptions,
@@ -82,6 +74,29 @@ describe('src/app/component/media/sw-media-upload-v2', () => {
     let wrapper;
 
     beforeEach(async () => {
+        if (Shopware.Store.get('context')) {
+            Shopware.Store.unregister('context');
+        }
+
+        Shopware.Store.register({
+            id: 'context',
+            state: () => ({
+                app: {
+                    config: {
+                        settings: {
+                            enableUrlFeature: true,
+                        },
+                    },
+                },
+                api: {
+                    assetPath: 'http://localhost:8000/bundles/administration/',
+                    authToken: {
+                        token: 'testToken',
+                    },
+                },
+            }),
+        });
+
         wrapper = await createWrapper();
         await flushPromises();
 

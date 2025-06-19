f63cebb2a85d613b156423ce82212d44acf44c6c
Lee Nguyen <l.nguyen@shopware.com>
2025-06-19 18:19:16 +0700

    fix: update product and property option names to use translated values (#10714)

--- src/Administration/Resources/app/administration/src/module/sw-property/component/sw-property-option-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-property/component/sw-property-option-detail/index.js
@@ -4,7 +4,7 @@
 
 import template from './sw-property-option-detail.html.twig';
 
-const { Component } = Shopware;
+const { Component, Mixin } = Shopware;
 const { mapPropertyErrors } = Component.getComponentHelper();
 
 // eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
@@ -16,6 +16,10 @@ export default {
     inject: [
         'repositoryFactory',
         'acl',
+    ],
+
+    mixins: [
+        Mixin.getByName('placeholder'),
     ],
 
     props: {
@@ -41,6 +45,20 @@ export default {
     computed: {
         mediaRepository() {
             return this.repositoryFactory.create('media');
+        },
+
+        colorHexCode: {
+            set(value) {
+                this.currentOption.colorHexCode = value;
+            },
+
+            get() {
+                return this.currentOption?.colorHexCode || '';
+            },
+        },
+
+        modalTitle() {
+            return this.currentOption?.translated?.name || this.$tc('sw-property.detail.textOptionHeadline');
         },
 
         ...mapPropertyErrors('currentOption', ['name']),

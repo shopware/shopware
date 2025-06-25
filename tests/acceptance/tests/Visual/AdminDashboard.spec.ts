2f70bcd38f09e41fa63921fb126390e170f38f04
Orkun Cavli <o.cavli@shopware.com>
2025-06-25 14:42:26 +0200

    add new snapshot and styletag

--- tests/acceptance/tests/Visual/AdminDashboard.spec.ts
+++ tests/acceptance/tests/Visual/AdminDashboard.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from '@fixtures/AcceptanceTest';
-import path from 'path';
-test('Visual: Administration dashboard', { tag: '@Visual' }, async ({
-    ShopAdmin,
-    AdminDashboard,
-}) => {
 
+test('Visual: Administration dashboard', { tag: '@Visual' }, async ({ ShopAdmin, AdminDashboard }) => {
     await test.step('Creates a screenshot of the Administration dashboard.', async () => {
-
         await ShopAdmin.goesTo(AdminDashboard.url());
         await ShopAdmin.expects(AdminDashboard.welcomeHeadline).toBeVisible();
         await AdminDashboard.welcomeHeadline.hover();
 
-        await AdminDashboard.page.setViewportSize({ width: 1440, height: 2240});
-
-        await expect(AdminDashboard.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
+        await AdminDashboard.page.setViewportSize({ width: 1440, height: 2240 });
+        await AdminDashboard.page.addStyleTag({
+            content: `
+                .sw-admin-menu__user-name,
+                .sw-avatar,
+                .sw-dashboard-index__welcome-text 
+                {display: none !important;}`,
         });
+        await expect(AdminDashboard.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 });

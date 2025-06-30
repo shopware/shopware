57703fcd5370fd6462a78d7407199ab4483dd93d
Vin Le <t.le@shopware.com>
2025-06-30 11:11:55 +0700

    feat: separate measurement_system migrations files (#10812)

---
+++ src/Core/Migration/V6_7/Migration1742199551SalesChannelDomainMeasurementUnits.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_7;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Content\MeasurementSystem\MeasurementUnits;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @internal
+ */
+#[Package('inventory')]
+class Migration1742199551SalesChannelDomainMeasurementUnits extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1742199551;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->addMeasurementUnitsColumn($connection);
+    }
+
+    private function addMeasurementUnitsColumn(Connection $connection): void
+    {
+        if ($this->columnExists($connection, 'sales_channel_domain', 'measurement_units')) {
+            return;
+        }
+
+        $defaultUnits = \json_encode([
+            'system' => MeasurementUnits::DEFAULT_MEASUREMENT_SYSTEM,
+            'units' => [
+                'length' => MeasurementUnits::DEFAULT_LENGTH_UNIT,
+                'weight' => MeasurementUnits::DEFAULT_WEIGHT_UNIT,
+            ],
+        ]);
+
+        $connection->executeStatement('
+            ALTER TABLE `sales_channel_domain`
+            ADD COLUMN `measurement_units` JSON NULL;
+        ');
+
+        // Set default measurement units for existing domains
+        $connection->executeStatement('
+            UPDATE `sales_channel_domain`
+            SET `measurement_units` = \'' . $defaultUnits . '\'
+        ');
+    }
+}

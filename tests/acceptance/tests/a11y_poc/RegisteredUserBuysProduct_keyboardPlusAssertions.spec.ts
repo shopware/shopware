33ade00c3150d81ad2ba5be7446e006ca64ca66d
a.okko <14863712+ravenokko@users.noreply.github.com>
2025-07-01 12:14:19 +0200

    test: a11y refactorings of RegisteredUserBuysProduct.spc.ts to include keyboard focus and custom assertions

---
+++ tests/acceptance/tests/a11y_poc/RegisteredUserBuysProduct_keyboardPlusAssertions.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@fixtures/AcceptanceTest';
+
+test('Registered shop customer buys a product.', { tag: '@Checkout' }, async ({
+    ShopCustomer,
+    TestDataService,
+    DefaultSalesChannel,
+    AdminApiContext,
+    StorefrontProductDetail,
+    StorefrontCheckoutConfirm,
+    StorefrontCheckoutFinish,
+    Login_a11yAssert,
+    AddProductToCart_a11yAssert,
+    ProceedFromProductToCheckout_a11yAssert,
+    ConfirmTermsAndConditions_a11yAssert,
+    SelectInvoicePaymentOption_a11yAssert,
+    SelectStandardShippingOption_a11yAssert,
+    SubmitOrder_a11yAssert,
+}) => {
+    const product = await TestDataService.createBasicProduct();
+
+    await ShopCustomer.attemptsTo(Login_a11yAssert());
+
+    await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
+    await ShopCustomer.expects(StorefrontProductDetail.page).toHaveTitle(
+        `${product.translated.name} | ${product.productNumber}`
+    );
+
+    await ShopCustomer.attemptsTo(AddProductToCart_a11yAssert(product));
+    await ShopCustomer.attemptsTo(ProceedFromProductToCheckout_a11yAssert());
+
+    await ShopCustomer.attemptsTo(ConfirmTermsAndConditions_a11yAssert());
+    await ShopCustomer.attemptsTo(SelectInvoicePaymentOption_a11yAssert());
+    await ShopCustomer.attemptsTo(SelectStandardShippingOption_a11yAssert());
+
+    await ShopCustomer.expects(StorefrontCheckoutConfirm.grandTotalPrice).toContainText('€10.00');
+
+    await ShopCustomer.attemptsTo(SubmitOrder_a11yAssert());
+    await ShopCustomer.expects(StorefrontCheckoutFinish.grandTotalPrice).toContainText('€10.00');
+    
+    const orderId = StorefrontCheckoutFinish.getOrderId();
+
+    TestDataService.addCreatedRecord('order', orderId);
+
+    await test.step('Validate that the order was submitted successfully.', async () => {
+        const orderResponse = await AdminApiContext.get(`order/${orderId}`);
+
+        expect(orderResponse.ok()).toBeTruthy();
+
+        const order = await orderResponse.json();
+
+        expect(order.data).toEqual(
+            expect.objectContaining({
+                price: expect.objectContaining({
+                    totalPrice: 10,
+                }),
+                orderCustomer: expect.objectContaining({
+                    email: DefaultSalesChannel.customer.email,
+                }),
+            })
+        );
+    });
+});
---
+++ tests/acceptance/tests/a11y_poc/RegisteredUserBuysProduct_keyboardPlusTabbed.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@fixtures/AcceptanceTest';
+
+test('Registered shop customer buys a product.', { tag: '@Checkout' }, async ({
+    ShopCustomer,
+    TestDataService,
+    DefaultSalesChannel,
+    AdminApiContext,
+    StorefrontProductDetail,
+    StorefrontCheckoutConfirm,
+    StorefrontCheckoutFinish,
+    Login_a11y,
+    AddProductToCart_a11y,
+    ProceedFromProductToCheckout_a11y,
+    ConfirmTermsAndConditions_a11y,
+    SelectInvoicePaymentOption_a11y,
+    SelectStandardShippingOption_a11y,
+    SubmitOrder,
+}) => {
+    const product = await TestDataService.createBasicProduct();
+
+    await ShopCustomer.attemptsTo(Login_a11y());
+
+    await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
+    await ShopCustomer.expects(StorefrontProductDetail.page).toHaveTitle(
+        `${product.translated.name} | ${product.productNumber}`
+    );
+
+    await ShopCustomer.attemptsTo(AddProductToCart_a11y(product));
+
+
+    for(let i = 0; i < 10; i++)
+        await StorefrontProductDetail.page.keyboard.press('Tab');
+
+    await ShopCustomer.attemptsTo(ProceedFromProductToCheckout_a11y());
+
+    await ShopCustomer.attemptsTo(ConfirmTermsAndConditions_a11y());
+
+    for(let i = 0; i < 10; i++)
+        await StorefrontProductDetail.page.keyboard.press('Tab');
+
+    //could actually skip SelectInvoicePaymentOption and SelectStandardShippingOption bc they are selected by default
+    await ShopCustomer.attemptsTo(SelectInvoicePaymentOption_a11y());
+    await ShopCustomer.attemptsTo(SelectStandardShippingOption_a11y());
+
+    await ShopCustomer.expects(StorefrontCheckoutConfirm.grandTotalPrice).toContainText('€10.00');
+
+    await ShopCustomer.attemptsTo(SubmitOrder());
+    await ShopCustomer.expects(StorefrontCheckoutFinish.grandTotalPrice).toContainText('€10.00');
+
+    const orderId = StorefrontCheckoutFinish.getOrderId();
+
+    TestDataService.addCreatedRecord('order', orderId);
+
+    await test.step('Validate that the order was submitted successfully.', async () => {
+        const orderResponse = await AdminApiContext.get(`order/${orderId}`);
+
+        expect(orderResponse.ok()).toBeTruthy();
+
+        const order = await orderResponse.json();
+
+        expect(order.data).toEqual(
+            expect.objectContaining({
+                price: expect.objectContaining({
+                    totalPrice: 10,
+                }),
+                orderCustomer: expect.objectContaining({
+                    email: DefaultSalesChannel.customer.email,
+                }),
+            })
+        );
+    });
+});

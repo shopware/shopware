07c11798320e9124bcceec9008908d8e15a4492f
Sebastian Franze <s.franze@shopware.com>
2025-07-01 17:41:33 +0200

    feat(services): Send admin notification after services got installed

--- tests/unit/Core/Service/MessageHandler/InstallServicesHandlerTest.php
+++ tests/unit/Core/Service/MessageHandler/InstallServicesHandlerTest.php
@@ -5,9 +5,13 @@ namespace Shopware\Tests\Unit\Core\Service\MessageHandler;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Notification\NotificationCollection;
+use Shopware\Core\Framework\Notification\NotificationService;
 use Shopware\Core\Service\LifecycleManager;
 use Shopware\Core\Service\Message\InstallServicesMessage;
 use Shopware\Core\Service\MessageHandler\InstallServicesHandler;
+use Shopware\Core\Service\Notification;
+use Shopware\Core\Test\Stub\DataAbstractionLayer\StaticEntityRepository;
 
 /**
  * @internal
@@ -20,9 +24,18 @@ class InstallServicesHandlerTest extends TestCase
         $lifecycleManager = $this->createMock(LifecycleManager::class);
         $lifecycleManager->expects($this->once())
             ->method('install')
-            ->with(static::isInstanceOf(Context::class));
+            ->with(static::isInstanceOf(Context::class))
+            ->willReturn(['some-cool-service']);
 
-        $handler = new InstallServicesHandler($lifecycleManager);
+        /** @var StaticEntityRepository<NotificationCollection> $notificationRepo */
+        $notificationRepo = new StaticEntityRepository([]);
+
+        $handler = new InstallServicesHandler(
+            $lifecycleManager,
+            new Notification(new NotificationService($notificationRepo))
+        );
         $handler->__invoke(new InstallServicesMessage());
+
+        static::assertNotEmpty($notificationRepo->creates[0]);
     }
 }

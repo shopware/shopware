c0e5f64fcd9cd92c607ef748c031a63f3fd6033e
Dennis Garding <d.garding@shopware.com>
2025-07-02 08:52:11 +0200

    Remove comment

--- src/Core/Framework/Test/TestCaseBase/AdminApiTestBehaviour.php
+++ src/Core/Framework/Test/TestCaseBase/AdminApiTestBehaviour.php
@@ -85,7 +85,6 @@ trait AdminApiTestBehaviour
 
         $apiBrowser->followRedirects();
         $apiBrowser->setServerParameters([
-//            'CONTENT_TYPE' => 'application/json', // TODO: Test check if contentType is required
             'HTTP_ACCEPT' => ['application/vnd.api+json,application/json'],
         ]);
 

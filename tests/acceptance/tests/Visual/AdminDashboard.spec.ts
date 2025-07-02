b99288e6b5a56acfdab0b741a58bcb6fbac17d95
Orkun Cavli <o.cavli@shopware.com>
2025-07-02 16:55:38 +0200

    add HideElementsForScreenshot

--- tests/acceptance/tests/Visual/AdminDashboard.spec.ts
+++ tests/acceptance/tests/Visual/AdminDashboard.spec.ts
@@ -1,21 +1,22 @@
 import { test, expect } from '@fixtures/AcceptanceTest';
 
-test('Visual: Administration dashboard', { tag: '@Visual' }, async ({ ShopAdmin, AdminDashboard }) => {
+test('Visual: Administration dashboard', { tag: '@Visual' }, async ({ 
+    ShopAdmin, 
+    AdminDashboard,
+    HideElementsForScreenshot
+ }) => {
     await test.step('Creates a screenshot of the Administration dashboard.', async () => {
         await ShopAdmin.goesTo(AdminDashboard.url());
         await ShopAdmin.expects(AdminDashboard.welcomeHeadline).toBeVisible();
         await AdminDashboard.welcomeHeadline.hover();
 
         await AdminDashboard.page.setViewportSize({ width: 1440, height: 2240 });
-        await AdminDashboard.page.addStyleTag({
-            content: `
-                .sw-admin-menu__user-name,
-                .sw-avatar,
-                .sw-dashboard-index__welcome-text,
-                .mt-card__subtitle,
-                .apexcharts-xaxis-texts-g
-                {display: none !important;}`,
-        });
+        await HideElementsForScreenshot(AdminDashboard.page, [
+            '.sw-dashboard-index__welcome-text',
+            '.mt-card__subtitle',
+            '.apexcharts-xaxis-texts-g',
+        ]);
+
         await expect(AdminDashboard.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 });

584857dd5b47cdeeaa3baa051b575e1c60e87e44
Benjamin Wittwer <Discord.Benjamin@web.de>
2025-07-03 07:18:45 +0000

    Lint fix sw-time-ago component

--- src/Administration/Resources/app/administration/src/app/component/utils/sw-time-ago/index.ts
+++ src/Administration/Resources/app/administration/src/app/component/utils/sw-time-ago/index.ts
@@ -25,7 +25,7 @@ export default Shopware.Component.wrapComponentConfig({
         dateTimeFormat: {
             type: Object as PropType<Intl.DateTimeFormatOptions>,
             required: false,
-        }
+        },
     },
 
     data(): {

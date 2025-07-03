0603403f84aade5fc18427eb0a0b05da14c4c1a4
Max Stegmeyer <m.stegmeyer@shopware.com>
2025-07-03 13:59:29 +0200

    revert: unnecessary change in meta.spec.js

--- src/Administration/Resources/app/administration/src/meta/meta.spec.js
+++ src/Administration/Resources/app/administration/src/meta/meta.spec.js
@@ -109,7 +109,7 @@ describe('Administration meta tests', () => {
 
     describe('check extension sdk public api', () => {
         it('should not break position identifiers', () => {
-            let result = [];
+            const result = [];
             templateFiles.forEach((file) => {
                 const fileContent = fs.readFileSync(file, {
                     encoding: 'utf-8',

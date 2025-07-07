6681a89f5582d7e90d53ebfc4b3017d117dc31cb
Orkun Cavli <o.cavli@shopware.com>
2025-07-07 14:55:58 +0200

    update handling dymamic elements

--- tests/acceptance/tests/Visual/AdminDashboard.spec.ts
+++ tests/acceptance/tests/Visual/AdminDashboard.spec.ts
@@ -3,17 +3,19 @@ import { test, expect } from '@fixtures/AcceptanceTest';
 test('Visual: Administration dashboard', { tag: '@Visual' }, async ({ 
     ShopAdmin, 
     AdminDashboard,
-    HideElementsForScreenshot
+    ReplaceElementsForScreenshot,
+    HideElementsForScreenshot,
  }) => {
     await test.step('Creates a screenshot of the Administration dashboard.', async () => {
         await ShopAdmin.goesTo(AdminDashboard.url());
         await ShopAdmin.expects(AdminDashboard.welcomeHeadline).toBeVisible();
-        await AdminDashboard.welcomeHeadline.hover();
-
-        await AdminDashboard.page.setViewportSize({ width: 1440, height: 2240 });
-        await HideElementsForScreenshot(AdminDashboard.page, [
+        await AdminDashboard.page.setViewportSize({ width: 1440, height: 2300 });
+        await ReplaceElementsForScreenshot(AdminDashboard.page, [
             '.sw-dashboard-index__welcome-text',
             '.mt-card__subtitle',
+        ]);
+
+        await HideElementsForScreenshot(AdminDashboard.page, [
             '.apexcharts-xaxis-texts-g',
         ]);
 

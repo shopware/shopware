9f323f93b9355f3f380c5ffe3e93986f370ad36b
Orkun Cavli <o.cavli@shopware.com>
2025-07-08 14:48:18 +0200

    add replace method for dynamic name

--- tests/acceptance/tests/Visual/VisualStorefrontHomepage.spec.ts
+++ tests/acceptance/tests/Visual/VisualStorefrontHomepage.spec.ts
@@ -4,11 +4,16 @@ test('Visual: Homepage in the Storefront.', { tag: '@Visual' }, async ({
     ShopCustomer,
     StorefrontHome,
     Login,
+    ReplaceElementsForScreenshot,
 }) => {
     await ShopCustomer.attemptsTo(Login());
     await ShopCustomer.goesTo(StorefrontHome.url());
 
     await test.step('Creates a screenshot and compare it on homepage in storefront.', async () => {
+
+        ReplaceElementsForScreenshot(StorefrontHome.page, [
+            '.cms-element-text h1',
+        ]);
 
         await expect(StorefrontHome.page).toHaveScreenshot({
             fullPage: true,

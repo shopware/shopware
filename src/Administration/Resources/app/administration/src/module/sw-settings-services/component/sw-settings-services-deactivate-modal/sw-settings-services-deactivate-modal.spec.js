abd31060ad51e8f80407c44a8cefaf0455891068
Sebastian Franze <s.franze@shopware.com>
2025-07-08 10:38:16 +0200

    feat(services): Use modal components in deactivate-modal

--- src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-deactivate-modal/sw-settings-services-deactivate-modal.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-deactivate-modal/sw-settings-services-deactivate-modal.spec.js
@@ -1,28 +1,16 @@
 import { mount } from '@vue/test-utils';
-import AxiosMockAdapter from "axios-mock-adapter";
-import { MtModal, MtModalRoot, MtModalClose, MtModalAction } from '@shopware-ag/meteor-component-library';
-import ShopwareServicesService from "../../service/shopware-services.service";
-import createHTTPClient from "../../../../core/factory/http.factory";
-import createLoginService from "../../../../core/service/login.service";
-import SystemConfigApiService from "../../../../core/service/api/system-config.api.service";
+import { MtModal, MtModalClose, MtModalAction } from '@shopware-ag/meteor-component-library';
+import SwSettingsServicesDeactivateModal from "./index";
 
 describe('src/module/sw-settings-services/component/sw-settings-services-deactivate-modal', () => {
-    let axiosMock;
     const location = window.location;
 
     beforeAll(() => {
-        const httpClient = createHTTPClient(Shopware.Context.api);
-        const loginService = createLoginService(httpClient, Shopware.Context.api);
-
-        axiosMock = new AxiosMockAdapter(httpClient);
-
         Shopware.Service().register(
             'shopwareServicesService',
-            () => new ShopwareServicesService(
-                httpClient,
-                loginService,
-                new SystemConfigApiService(httpClient, loginService),
-            )
+            () => ({
+                disableAllServices: jest.fn(),
+            })
         )
     });
 
@@ -38,20 +26,7 @@ describe('src/module/sw-settings-services/component/sw-settings-services-deactiv
     })
 
     it ('can be opened and closed', async () => {
-        const deactivateModal = await mount(
-            await wrapTestComponent(
-                'sw-settings-services-deactivate-modal',
-                { sync: true, },
-            ),
-            {
-                global: {
-                    stubs: {
-                        MtModalRoot,
-                        MtModal,
-                    }
-                }
-            }
-        )
+        const deactivateModal = await mount(SwSettingsServicesDeactivateModal);
         await flushPromises();
 
         let modal = deactivateModal.getComponent(MtModal);
@@ -76,32 +51,14 @@ describe('src/module/sw-settings-services/component/sw-settings-services-deactiv
         const notificationStore = Shopware.Store.get('notification');
         const notificationSpy = jest.spyOn(notificationStore, 'createNotification');
 
-        const deactivateModal = await mount(
-            await wrapTestComponent(
-                'sw-settings-services-deactivate-modal',
-                { sync: true, },
-            ),
-            {
-                global: {
-                    stubs: {
-                        MtModalRoot,
-                        MtModal,
-                    }
-                }
-            }
-        )
+        Shopware.Service('shopwareServicesService').disableAllServices.mockImplementationOnce(() => ({
+            disabled: true,
+        }));
+
+        const deactivateModal = await mount(SwSettingsServicesDeactivateModal);
         await flushPromises();
 
         await deactivateModal.get('button').trigger('click');
-
-        axiosMock.onPost('services/disable').replyOnce(204);
-        axiosMock.onGet('_action/system-config', {
-            params: {
-                domain: 'core.services',
-                salesChannelId: null,
-            }
-        }).replyOnce(200, {});
-
         const modal = deactivateModal.getComponent(MtModal);
         await modal.getComponent(MtModalAction).trigger('click');
         await flushPromises();
@@ -114,24 +71,14 @@ describe('src/module/sw-settings-services/component/sw-settings-services-deactiv
         const notificationStore = Shopware.Store.get('notification');
         const notificationSpy = jest.spyOn(notificationStore, 'createNotification');
 
-        const deactivateModal = await mount(
-            await wrapTestComponent(
-                'sw-settings-services-deactivate-modal',
-                { sync: true, },
-            ),
-            {
-                global: {
-                    stubs: {
-                        MtModalRoot,
-                        MtModal,
-                    }
-                }
-            }
-        )
+        Shopware.Service('shopwareServicesService').disableAllServices.mockImplementationOnce(() => {
+            throw new Error('Deactivation failed');
+        })
+
+        const deactivateModal = await mount(SwSettingsServicesDeactivateModal);
         await flushPromises();
 
         await deactivateModal.get('button').trigger('click');
-
         const modal = deactivateModal.getComponent(MtModal);
         await modal.getComponent(MtModalAction).trigger('click');
         await flushPromises();
@@ -140,7 +87,7 @@ describe('src/module/sw-settings-services/component/sw-settings-services-deactiv
         expect(notificationSpy).toHaveBeenCalledWith({
             title: 'global.default.error',
             variant: 'critical',
-            message: 'Request failed with status code 404',
+            message: 'Deactivation failed',
             autoClose: false,
         });
     })

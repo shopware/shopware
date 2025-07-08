b5e2dd32018a697abe6367386ec08773aadb135e
Sebastian Franze <s.franze@shopware.com>
2025-07-08 13:31:56 +0200

    feat(services): Finalize sw-settings-services-grant-permissions-modal

--- src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-grant-permissions-modal/index.ts
+++ src/Administration/Resources/app/administration/src/module/sw-settings-services/component/sw-settings-services-grant-permissions-modal/index.ts
@@ -1,11 +1,12 @@
 /**
  * @sw-package framework
  */
-import { MtModalAction, MtModalClose } from '@shopware-ag/meteor-component-library';
+import { MtModal, MtModalRoot, MtModalAction, MtModalClose } from '@shopware-ag/meteor-component-library';
 import useSession from 'src/app/composables/use-session';
 import template from './sw-settings-services-grant-permissions-modal.html.twig';
 import './sw-settings-services-grant-permissions-modal.scss';
 import { useShopwareServicesStore } from '../../store/shopware-services.store';
+import extractErrorMessage from '../../composables/extract-error';
 
 /**
  * @private
@@ -15,6 +16,8 @@ export default Shopware.Component.wrapComponentConfig({
     template,
 
     components: {
+        MtModal,
+        MtModalRoot,
         MtModalAction,
         MtModalClose,
     },
@@ -26,6 +29,7 @@ export default Shopware.Component.wrapComponentConfig({
             grantPermissionsBackground: assetFilter(
                 '/administration/administration/static/img/services/grant-permissions-background.svg',
             ),
+            isLoading: false,
         };
     },
 
@@ -58,8 +62,31 @@ export default Shopware.Component.wrapComponentConfig({
             }
         },
 
-        grantPermissions(done: () => void) {
-            done();
+        async grantPermissions(done: () => void) {
+            try {
+                const shopwareServiceStore = useShopwareServicesStore();
+                const currentRevision = shopwareServiceStore.currentRevision?.revision;
+
+                if (!currentRevision) {
+                    throw new Error('No revision available');
+                }
+
+                this.isLoading = true;
+
+                shopwareServiceStore.config =
+                    await Shopware.Service('shopwareServicesService').acceptRevision(currentRevision);
+
+                this.$emit('service-permissions-granted');
+            } catch (exception) {
+                Shopware.Store.get('notification').createNotification({
+                    variant: 'critical',
+                    title: this.$t('global.default.error'),
+                    message: extractErrorMessage(exception),
+                });
+            } finally {
+                this.isLoading = false;
+                done();
+            }
         },
     },
 });

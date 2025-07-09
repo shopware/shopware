af20bac54d419b7ba71d880630e117a093dd4cf1
Christoph Pötz (ACRIS) <35556204+acris-cp@users.noreply.github.com>
2025-07-09 09:21:25 +0200

    Fix wrong customer context on login if entry from sales_channel_api_context is expired

--- src/Core/System/SalesChannel/Context/SalesChannelContextPersister.php
+++ src/Core/System/SalesChannel/Context/SalesChannelContextPersister.php
@@ -144,7 +144,7 @@ class SalesChannelContextPersister
         $now = new \DateTimeImmutable();
         if ($expiredTime < $now) {
             // context is expired
-            $payload = ['expired' => true];
+            $payload['expired'] = true;
         } else {
             $payload['expired'] = false;
         }

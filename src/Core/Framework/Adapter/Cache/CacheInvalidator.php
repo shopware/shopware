ca833e23f9ac0c9c0f11b66acda1c29e09dd4109
Nicolas Fortier <n.fortier@shopware.com>
2025-07-10 14:17:45 +0200

    remove stop

--- src/Core/Framework/Adapter/Cache/CacheInvalidator.php
+++ src/Core/Framework/Adapter/Cache/CacheInvalidator.php
@@ -39,9 +39,6 @@ class CacheInvalidator
     {
         $this->logger->critical(\sprintf('Invalidating cache with: %s', $this->environment));
 
-        var_dump($this->environment);
-        throw new \RuntimeException(\sprintf('Invalidating cache with: %s', $this->environment));
-
         $tags = array_filter(array_unique($tags));
 
         if (empty($tags)) {

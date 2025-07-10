1bf044cfdead0393f1ff89ae5788463d393c6b2c
Daniel Meyer <d.meyer@shopware.com>
2025-07-10 11:17:11 +0200

    unify waiting and remove stylePath

--- tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
+++ tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
@@ -7,61 +7,39 @@ test('Visual: Administration category page', { tag: '@Visual' }, async ({
 }) => {
 
     await test.step('Creates a screenshot of the category page on the general tab.', async () => {
-
         await ShopAdmin.goesTo(AdminCategories.url());
         await AdminCategories.page.locator('.sw-category-tree').locator('.tree-link').getByText('Home').click();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-media-upload-v2').locator('.sw-context-button__button')).toBeVisible();
-
+        await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 2200});
-
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
-        });
+        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
+
     await test.step('Creates a screenshot of the "configure home page" modal.', async () => {
-
         await AdminCategories.page.getByRole('button', { name: 'Configure home page' }).click();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-modal__dialog')).toBeVisible();
-
+        await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 1440});
-
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
-        });
+        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
         await AdminCategories.page.getByLabel('Configure home page').getByRole('button', { name: 'Cancel' }).click();
     });
+
     await test.step('Creates a screenshot of the category page on the products tab.', async () => {
-
         await AdminCategories.page.locator('.sw-tabs__content').getByText('Products').click();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-data-grid-skeleton')).toBeVisible();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-data-grid-skeleton')).not.toBeVisible();
-
+        await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
-
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
-        });
+        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
+
     await test.step('Creates a screenshot of the category page on the layout tab.', async () => {
-
         await AdminCategories.page.locator('.sw-tabs__content').getByText('Layout').click();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-tabs__slider')).toBeVisible();
-
+        await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 3660});
-
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
-        });
+        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
+
     await test.step('Creates a screenshot of the category page on the SEO tab.', async () => {
-
         await AdminCategories.page.locator('.sw-tabs__content').getByText('SEO').click();
-        await ShopAdmin.expects(AdminCategories.page.locator('.sw-seo-url')).toBeVisible();
-
+        await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
-
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot({
-            stylePath: path.resolve('./tests/Visual/screenshot.css'),
-        });
+        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 });

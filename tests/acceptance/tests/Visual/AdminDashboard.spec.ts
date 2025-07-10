aae880c40d8a09a793c8875b1f73cbb145d34d95
Orkun Cavli <o.cavli@shopware.com>
2025-07-10 08:50:55 +0200

    add waitforload

--- tests/acceptance/tests/Visual/AdminDashboard.spec.ts
+++ tests/acceptance/tests/Visual/AdminDashboard.spec.ts
@@ -1,18 +1,16 @@
 import { test, expect } from '@fixtures/AcceptanceTest';
 
-test('Visual: Administration dashboard', { tag: '@Visual' }, async ({ 
+test('Administration dashboard', { tag: '@Visual' }, async ({ 
     ShopAdmin, 
     AdminDashboard,
     ReplaceElementsForScreenshot,
     HideElementsForScreenshot,
  }) => {
     await test.step('Creates a screenshot of the Administration dashboard.', async () => {
-
-        const response = await AdminDashboard.page.waitForResponse(response => response.url().includes('/api/search/order') && response.status() === 200);
-        await ShopAdmin.goesTo(AdminDashboard.url());
-        await ShopAdmin.expects(response).toBeTruthy()  ;
+        await ShopAdmin.goesTo(AdminDashboard.url()),
+        await AdminDashboard.page.waitForLoadState('load');
         await AdminDashboard.page.setViewportSize({ width: 1440, height: 2300 });
-        
+
         await ReplaceElementsForScreenshot(AdminDashboard.page, [
             '.sw-dashboard-index__welcome-text',
             '.mt-card__subtitle',

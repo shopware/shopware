940adb17b80f7cca7c7e55ffebf9ea037f5fc038
Daniel Meyer <d.meyer@shopware.com>
2025-07-10 11:46:31 +0200

    replace category tree elements

--- tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
+++ tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
@@ -4,13 +4,17 @@ import path from 'path';
 test('Visual: Administration category page', { tag: '@Visual' }, async ({
     ShopAdmin,
     AdminCategories,
+    ReplaceElementsForScreenshot,
 }) => {
 
     await test.step('Creates a screenshot of the category page on the general tab.', async () => {
         await ShopAdmin.goesTo(AdminCategories.url());
-        await AdminCategories.page.locator('.sw-category-tree').locator('.tree-link').getByText('Home').click();
+        await AdminCategories.page.locator('.sw-category-tree').locator('.tree-link').first().click();
         await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 2200});
+        await ReplaceElementsForScreenshot(AdminCategories.page, [
+            '.tree-link',
+        ]);
         await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 
@@ -26,6 +30,9 @@ test('Visual: Administration category page', { tag: '@Visual' }, async ({
         await AdminCategories.page.locator('.sw-tabs__content').getByText('Products').click();
         await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
+        await ReplaceElementsForScreenshot(AdminCategories.page, [
+            '.tree-link',
+        ]);
         await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 
@@ -33,6 +40,9 @@ test('Visual: Administration category page', { tag: '@Visual' }, async ({
         await AdminCategories.page.locator('.sw-tabs__content').getByText('Layout').click();
         await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 3660});
+        await ReplaceElementsForScreenshot(AdminCategories.page, [
+            '.tree-link',
+        ]);
         await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 
@@ -40,6 +50,9 @@ test('Visual: Administration category page', { tag: '@Visual' }, async ({
         await AdminCategories.page.locator('.sw-tabs__content').getByText('SEO').click();
         await AdminCategories.page.waitForLoadState('load');
         await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
+        await ReplaceElementsForScreenshot(AdminCategories.page, [
+            '.tree-link',
+        ]);
         await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
     });
 });

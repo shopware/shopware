d80f01dd3f465c46a41fecf83389c6d473772394
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-07-16 16:03:53 +0200

    fix: added locking to cart mutating routes to avoid race conditions (#11229)

---
+++ src/Core/Checkout/Cart/CartLocker.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\Cart;
+
+use Shopware\Core\Framework\Log\Package;
+use Symfony\Component\Lock\LockFactory;
+
+/**
+ * @internal
+ */
+#[Package('checkout')]
+class CartLocker
+{
+    private const LOCK_TTL = 5;
+
+    public function __construct(private readonly LockFactory $lockFactory)
+    {
+    }
+
+    /**
+     * @param \Closure(): \T $closure
+     *
+     * @return \T
+     *
+     * @template \T
+     */
+    public function locked(string $token, \Closure $closure)
+    {
+        $lockKey = $this->getLockKey($token);
+        $lock = $this->lockFactory->createLock($lockKey, self::LOCK_TTL);
+
+        if (!$lock->acquire()) {
+            throw CartException::cartLocked($token);
+        }
+
+        try {
+            return $closure();
+        } finally {
+            $lock->release();
+        }
+    }
+
+    public function getLockKey(string $token): string
+    {
+        return 'cart-lock' . $token;
+    }
+}

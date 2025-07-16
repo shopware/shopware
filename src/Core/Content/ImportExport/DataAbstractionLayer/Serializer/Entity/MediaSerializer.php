ab4b92c859583a9396450284a5cf71df1f692bcc
p.dinkhoff <p.dinkhoff@shopware.com>
2025-07-16 09:05:42 +0200

    Fix phpstan

--- src/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/MediaSerializer.php
+++ src/Core/Content/ImportExport/DataAbstractionLayer/Serializer/Entity/MediaSerializer.php
@@ -4,6 +4,7 @@ namespace Shopware\Core\Content\ImportExport\DataAbstractionLayer\Serializer\Ent
 
 use Shopware\Core\Content\ImportExport\Exception\InvalidMediaUrlException;
 use Shopware\Core\Content\ImportExport\Exception\MediaDownloadException;
+use Shopware\Core\Content\ImportExport\ImportExportException;
 use Shopware\Core\Content\ImportExport\Struct\Config;
 use Shopware\Core\Content\Media\Aggregate\MediaFolder\MediaFolderCollection;
 use Shopware\Core\Content\Media\File\FileSaver;
@@ -93,7 +94,7 @@ class MediaSerializer extends AbstractMediaSerializer implements ResetInterface
 
             $parsed = parse_url((string) $url);
             if (!$parsed) {
-                throw new \RuntimeException('Error parsing media URL: ' . $url);
+                throw ImportExportException::failedParsingMediaUrl($url);
             }
 
             $pathInfo = pathinfo($parsed['path'] ?? '');
@@ -181,7 +182,7 @@ class MediaSerializer extends AbstractMediaSerializer implements ResetInterface
 
         $fallbackFolderId = $this->mediaFolderRepository->searchIds($criteria, $context)->firstId();
         if ($fallbackFolderId === null) {
-            throw new \RuntimeException('Failed to find default media folder for import_export_profile');
+            throw ImportExportException::mediaFolderNotFoundForImportExportProfile();
         }
 
         return $fallbackFolderId;

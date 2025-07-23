d62956ea88536978413c68b983939ad7d7153826
Aaron Sommer <a.sommer@arvenio.de>
2025-07-23 10:25:30 +0200

    Allowed checking for text custom field value to be nullable

--- src/Core/Framework/Rule/CustomFieldRule.php
+++ src/Core/Framework/Rule/CustomFieldRule.php
@@ -66,11 +66,11 @@ class CustomFieldRule
         }
 
         if (self::isFloat($renderedField)) {
-            return FloatComparator::compare((float) $actual, (float) $expected, $operator);
+            return FloatComparator::compare((float)$actual, (float)$expected, $operator);
         }
 
         if (self::isArray($renderedField)) {
-            return ArrayComparator::compare((array) $actual, (array) $expected, $operator);
+            return ArrayComparator::compare((array)$actual, (array)$expected, $operator);
         }
 
         return match ($operator) {
@@ -175,7 +175,7 @@ class CustomFieldRule
             return [new NotBlank()];
         }
 
-        if ($renderedField['type'] !== CustomFieldTypes::BOOL) {
+        if ($renderedField['type'] !== CustomFieldTypes::BOOL && $renderedField['type'] !== CustomFieldTypes::TEXT) {
             $constraints[] = new NotBlank();
         }
 

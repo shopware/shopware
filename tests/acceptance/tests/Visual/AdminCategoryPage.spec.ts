3fd54312f2d20cbb001c44ec2be8ca843bcad5d1
Daniel Meyer <d.meyer@shopware.com>
2025-07-24 23:56:28 +0200

    use helpers

--- tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
+++ tests/acceptance/tests/Visual/AdminCategoryPage.spec.ts
@@ -1,57 +1,67 @@
 import { test, expect } from '@fixtures/AcceptanceTest';
+import { setViewport, replaceElements } from '@shopware-ag/acceptance-test-suite';
 
 test('Visual: Administration category page', { tag: '@Visual' }, async ({
     ShopAdmin,
     AdminCategories,
-    ReplaceElementsForScreenshot,
 }) => {
 
     await test.step('Creates a screenshot of the category page on the general tab.', async () => {
         await ShopAdmin.goesTo(AdminCategories.url());
         await AdminCategories.categoryItems.first().click();
         await AdminCategories.page.waitForLoadState('load');
-        await AdminCategories.page.setViewportSize({ width: 1440, height: 2200});
-        await ReplaceElementsForScreenshot(AdminCategories.page, [
-            '.tree-link',
+        await setViewport(AdminCategories.page, {
+            requestURL: 'api/search/category',
+        });
+        await replaceElements(AdminCategories.page, [
+            AdminCategories.categoryItems,
         ]);
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
+        await expect(AdminCategories.contentView).toHaveScreenshot();
     });
 
     await test.step('Creates a screenshot of the "configure home page" modal.', async () => {
         await AdminCategories.configureHomePageButton.click();
         await AdminCategories.page.waitForLoadState('load');
-        await AdminCategories.page.setViewportSize({ width: 1440, height: 1440});
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
+        await setViewport(AdminCategories.page, {
+            requestURL: 'api/search/category',
+        });
+        await expect(AdminCategories.contentView).toHaveScreenshot();
         await AdminCategories.configureModalCancelButton.click();
     });
 
     await test.step('Creates a screenshot of the category page on the products tab.', async () => {
         await AdminCategories.productsTab.click();
         await AdminCategories.page.waitForLoadState('load');
-        await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
-        await ReplaceElementsForScreenshot(AdminCategories.page, [
-            '.tree-link',
+        await setViewport(AdminCategories.page, {
+            requestURL: 'api/search/category',
+        });
+        await replaceElements(AdminCategories.page, [
+            AdminCategories.categoryItems,
         ]);
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
+        await expect(AdminCategories.contentView).toHaveScreenshot();
     });
 
     await test.step('Creates a screenshot of the category page on the layout tab.', async () => {
         await AdminCategories.layoutTab.click();
         await AdminCategories.page.waitForLoadState('load');
-        await AdminCategories.page.setViewportSize({ width: 1440, height: 3660});
-        await ReplaceElementsForScreenshot(AdminCategories.page, [
-            '.tree-link',
+        await setViewport(AdminCategories.page, {
+            requestURL: 'api/search/category',
+        });
+        await replaceElements(AdminCategories.page, [
+            AdminCategories.categoryItems,
         ]);
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
+        await expect(AdminCategories.contentView).toHaveScreenshot();
     });
 
     await test.step('Creates a screenshot of the category page on the SEO tab.', async () => {
         await AdminCategories.seoTab.click();
         await AdminCategories.page.waitForLoadState('load');
-        await AdminCategories.page.setViewportSize({ width: 1440, height: 1280});
-        await ReplaceElementsForScreenshot(AdminCategories.page, [
-            '.tree-link',
+        await setViewport(AdminCategories.page, {
+            requestURL: 'api/search/category',
+        });
+        await replaceElements(AdminCategories.page, [
+            AdminCategories.categoryItems,
         ]);
-        await expect(AdminCategories.page.locator('.sw-desktop__content')).toHaveScreenshot();
+        await expect(AdminCategories.contentView).toHaveScreenshot();
     });
 });

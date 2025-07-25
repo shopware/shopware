ec5aa39ab12a198d93f4e13e9d171fd5a4d4e5e4
Daniel Meyer <d.meyer@shopware.com>
2025-07-25 14:06:08 +0200

    test: add dynamic viewport function to existing tests (#11333)

--- tests/acceptance/tests/Visual/AdminMenu.spec.ts
+++ tests/acceptance/tests/Visual/AdminMenu.spec.ts
@@ -1,31 +1,30 @@
 import { test, expect } from '@fixtures/AcceptanceTest';
+import { setViewport, hideElements, replaceElements } from '@shopware-ag/acceptance-test-suite';
 
 test('Visual: Administration menu', { tag: '@Visual' }, async ({
 ShopAdmin,
 AdminDashboard,
-HideElementsForScreenshot,
-ReplaceElementsForScreenshot,
 }) => {
 
     await test.step('Creates a screenshot of the fully expanded admin menu.', async () => {
         await ShopAdmin.goesTo(AdminDashboard.url());
-        await AdminDashboard.page.locator('.sw-catalogue').click();
-        await AdminDashboard.page.locator('.sw-order').click();
-        await AdminDashboard.page.locator('.sw-customer').click();
-        await AdminDashboard.page.locator('.sw-content').click();
-        await AdminDashboard.page.locator('.sw-marketing').click();
-        await AdminDashboard.page.locator('.sw-extension').click();
-        await AdminDashboard.page.locator('.sw-admin-menu__user-actions-indicator').click();
-        await AdminDashboard.page.setViewportSize({ width: 1280, height: 2048});
+        await AdminDashboard.adminMenuCatalog.click();
+        await AdminDashboard.adminMenuOrder.click();
+        await AdminDashboard.adminMenuCustomer.click();
+        await AdminDashboard.adminMenuContent.click();
+        await AdminDashboard.adminMenuMarketing.click();
+        await AdminDashboard.adminMenuExtension.click();
+        await AdminDashboard.adminMenuUserChevron.click();
 
-        await HideElementsForScreenshot(AdminDashboard.page, [
-            '.sw-avatar',
+        await setViewport(AdminDashboard.page, {
+            contentHeight: 1796,
+        });
+        await hideElements(AdminDashboard.page, [
+            AdminDashboard.adminMenuUserIcon,
         ]);
-
-        await ReplaceElementsForScreenshot(AdminDashboard.page, [
-            '.sw-admin-menu__user-name',
+        await replaceElements(AdminDashboard.page, [
+            AdminDashboard.adminMenuUserName,
         ]);
-        
-        await expect(AdminDashboard.page.locator('.sw-admin-menu')).toHaveScreenshot();
+        await expect(AdminDashboard.adminMenuView).toHaveScreenshot('AdminMenu-Expanded.png');
     });
 });

d65d854e192be74ff1b7e2f6a4cd4da4266217f5
Jonas Elfering <j.elfering@shopware.com>
2025-07-29 16:55:54 +0200

    fix: Set state machine history FK constraint to integration to SET NULL
    fixes https://github.com/shopware/shopware/issues/11583

---
+++ src/Core/Migration/V6_7/Migration1753799632FixStateMachineHistoryIntegrationConstraint.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_7;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @internal
+ *
+ * @codeCoverageIgnore
+ */
+#[Package('checkout')]
+class Migration1753799632FixStateMachineHistoryIntegrationConstraint extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1753799632;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->dropForeignKeyIfExists($connection, 'state_machine_history', 'fk.state_machine_history.integration_id');
+
+        $connection->executeStatement('
+            ALTER TABLE `state_machine_history`
+            ADD CONSTRAINT `fk.state_machine_history.integration_id` FOREIGN KEY (`integration_id`)
+                REFERENCES `integration` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+        ');
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}

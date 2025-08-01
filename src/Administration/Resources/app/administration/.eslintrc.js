089c63a403babc75b672ef86384160916d398b62
Lars Kemper <l.kemper@shopware.com>
2025-08-01 13:27:44 +0200

    feat: use jest/no-restricted-matchers instead of jest/no-truthy-falsy

--- src/Administration/Resources/app/administration/.eslintrc.js
+++ src/Administration/Resources/app/administration/.eslintrc.js
@@ -256,7 +256,13 @@ module.exports = {
                 'sw-test-rules/await-async-functions': 'error',
                 'max-len': 0,
                 'sw-deprecation-rules/private-feature-declarations': 0,
-                'jest/no-truthy-falsy': 'error',
+                "jest/no-restricted-matchers": [
+                    "error",
+                    {
+                        "toBeTruthy": "Use `toBe(true)` to assert a boolean is exactly true",
+                        "toBeFalsy": "Use `toBe(false)` to assert a boolean is exactly false",
+                    },
+                ],
                 'jest/expect-expect': 'error',
                 'jest/no-duplicate-hooks': 'error',
                 'jest/no-test-return-statement': 'error',

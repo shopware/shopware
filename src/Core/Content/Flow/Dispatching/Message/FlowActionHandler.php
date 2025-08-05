357338c92a16a5b11bd7ef8a87f447ee3fad7759
Jozsef Damokos <j.damokos@shopware.com>
2025-08-05 14:52:29 +0300

    simplify handler

--- src/Core/Content/Flow/Dispatching/Message/FlowActionHandler.php
+++ src/Core/Content/Flow/Dispatching/Message/FlowActionHandler.php
@@ -2,12 +2,8 @@
 
 namespace Shopware\Core\Content\Flow\Dispatching\Message;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception as DBALException;
 use Shopware\Core\Content\Flow\Dispatching\Action\FlowAction;
 use Shopware\Core\Content\Flow\Dispatching\FlowFactory;
-use Shopware\Core\Content\Flow\Dispatching\TransactionalAction;
-use Shopware\Core\Content\Flow\FlowException;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
@@ -27,7 +23,6 @@ final readonly class FlowActionHandler
      * @internal
      */
     public function __construct(
-        private readonly Connection $connection,
         private readonly FlowFactory $flowFactory,
         $actions
     ) {
@@ -43,33 +38,9 @@ final readonly class FlowActionHandler
         }
 
         $event = $message->event;
-
         $event = $this->flowFactory->restore($event->getName(), $message->context, $event->stored(), $event->data());
-
         $event->setConfig($message->sequence->config);
 
-        if (!$action instanceof TransactionalAction) {
-            $action->handleFlow($event);
-
-            return;
-        }
-
-        $this->connection->beginTransaction();
-
-        try {
-            $action->handleFlow($event);
-        } catch (\Throwable $e) {
-            $this->connection->rollBack();
-
-            throw FlowException::transactionFailed($e);
-        }
-
-        try {
-            $this->connection->commit();
-        } catch (DBALException $e) {
-            $this->connection->rollBack();
-
-            throw FlowException::transactionFailed($e);
-        }
+        $action->handleFlow($event);
     }
 }

b6f9ba5112dc45689ff1fc40ac5f9b843c222991
Tobias Berge <tobiasberge@users.noreply.github.com>
2025-08-18 10:53:05 +0200

    Web components (#11951)

---
+++ src/Storefront/Resources/app/storefront/dist/assets/web-components/index.js
@@ -0,0 +1,9 @@
+import SwProductCard from './sw-product-card/index.js';
+import SwButton from './sw-button/index.js';
+import SwBuyButton from './sw-buy-button/index.js';
+import SwAjaxModal from './sw-ajax-modal/index.js';
+
+customElements.define('sw-button', SwButton);
+customElements.define('sw-buy-button', SwBuyButton);
+customElements.define('sw-ajax-modal', SwAjaxModal);
+customElements.define('sw-product-card', SwProductCard);

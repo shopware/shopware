a22b2cc42997b1fa84d2307188a10b0ceb968d96
Philipp Schuch <p.schuch@shopware.com>
2025-08-19 09:16:26 +0200

    Revert "Web components (#11951)"
    This reverts commit b6f9ba5112dc45689ff1fc40ac5f9b843c222991.

--- src/Storefront/Resources/app/storefront/dist/assets/web-components/index.js
+++
@@ -1,9 +0,0 @@
-import SwProductCard from './sw-product-card/index.js';
-import SwButton from './sw-button/index.js';
-import SwBuyButton from './sw-buy-button/index.js';
-import SwAjaxModal from './sw-ajax-modal/index.js';
-
-customElements.define('sw-button', SwButton);
-customElements.define('sw-buy-button', SwBuyButton);
-customElements.define('sw-ajax-modal', SwAjaxModal);
-customElements.define('sw-product-card', SwProductCard);

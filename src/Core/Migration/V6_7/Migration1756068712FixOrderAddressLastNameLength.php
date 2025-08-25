27a46d319979c50925788a25568175d690a89756
Martin Bens <m.bens@shopware.com>
2025-08-25 18:10:46 +0200

    refactor(migration): split address field length migration (#12064)

--- src/Core/Migration/V6_7/Migration1756068712FixOrderAddressLastNameLength.php
+++ src/Core/Migration/V6_7/Migration1756068712FixOrderAddressLastNameLength.php
@@ -10,24 +10,17 @@ use Shopware\Core\Framework\Migration\MigrationStep;
  * @internal
  */
 #[Package('checkout')]
-class Migration1756068709FixCustomerAddressNameFieldLengths extends MigrationStep
+class Migration1756068712FixOrderAddressLastNameLength extends MigrationStep
 {
     public function getCreationTimestamp(): int
     {
-        return 1756068709;
+        return 1756068712;
     }
 
     public function update(Connection $connection): void
     {
         $connection->executeStatement('
-            ALTER TABLE `customer_address`
-            MODIFY COLUMN `first_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-            MODIFY COLUMN `last_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL
-        ');
-
-        $connection->executeStatement('
             ALTER TABLE `order_address`
-            MODIFY COLUMN `first_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
             MODIFY COLUMN `last_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL
         ');
     }

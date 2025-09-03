7dd3eb48244b15b421ff1135839c870ea9245c0b
Jonas Elfering <j.elfering@shopware.com>
2025-09-03 09:03:01 +0200

    Fix phpstan after backport

--- src/Core/Framework/Store/Services/ExtensionDownloader.php
+++ src/Core/Framework/Store/Services/ExtensionDownloader.php
@@ -8,6 +8,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Plugin\PluginCollection;
 use Shopware\Core\Framework\Plugin\PluginEntity;
 use Shopware\Core\Framework\Plugin\PluginManagementService;
 use Shopware\Core\Framework\Store\StoreException;

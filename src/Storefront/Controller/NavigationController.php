fb1f46f93efdf5d5a77353cea737d9f20a4767b3
Philipp Schuch <p.schuch@shopware.com>
2025-09-03 08:51:14 +0200

    Add first listing prototype based on new data with slots

--- src/Storefront/Controller/NavigationController.php
+++ src/Storefront/Controller/NavigationController.php
@@ -5,8 +5,10 @@ namespace Shopware\Storefront\Controller;
 use Shopware\Core\Content\Category\CategoryDefinition;
 use Shopware\Core\Content\Category\CategoryException;
 use Shopware\Core\Content\Category\Service\AbstractCategoryUrlGenerator;
+use Shopware\Core\Content\Media\MediaEntity;
 use Shopware\Core\Content\Seo\SeoUrlPlaceholderHandlerInterface;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Framework\Routing\RequestTransformer;
@@ -57,6 +59,19 @@ class NavigationController extends StorefrontController
         $page = $this->navigationPageLoader->load($request, $context);
 
         $this->hook(new NavigationPageLoadedHook($page, $context));
+
+        // TODO: Remove this once the new cms structure is fully implemented.
+        if (Feature::isActive('STOREFRONT_COMPONENTS')) {
+            $elements = $page->getCmsPage()->getAllElements();
+            $listing = $elements[4];
+            $listingData = $listing->getData()->getListing();
+    
+            $cmsPage = $this->getNewCmsStructure($listingData);
+
+            return $this->renderStorefront(
+                '@Storefront/storefront/page/content/index.html.twig', 
+                ['page' => $page, 'cmsPage' => $cmsPage]);
+        }
 
         return $this->renderStorefront('@Storefront/storefront/page/content/index.html.twig', ['page' => $page]);
     }
@@ -147,5 +162,110 @@ class NavigationController extends StorefrontController
             'footer' => $footer,
             'footerParameters' => $request->get('footerParameters') ?? [],
         ]);
+    }
+
+    /**
+     * TODO: Remove after final CMS structure is implemented.
+     */
+    private static function getNewCmsStructure($listingData) {
+
+        $media = new MediaEntity();
+        $media->setId('123');
+        $media->setMimeType('image/webp');
+        $media->setFileExtension('webp');
+        $media->setFileSize(1203165);
+        $media->setFileName('inspire-connect-riseup-mood.webp');
+        $media->setTitle('Test');
+        $media->setAlt('Test');
+        $media->setUrl('https://www.shopware.com/media/pages/products/shopping-experiences/inspire-connect-riseup-mood.webp');
+
+        return [
+            'id' => '123',
+            'name' => 'Home',
+            'elements' => [
+                [
+                    'id' => '123',
+                    'component' => 'grid:container',
+                    'properties' => [
+                        'columns' => '2',
+                        'columnsLg' => '1',
+                        'gap' => '24',
+                        'align' => 'start',
+                        'alignContent' => 'start',
+                        'justify' => 'stretch',
+                        'justifyContent' => 'stretch',
+                    ],
+                    'slots' => [
+                        'column-1' => [
+                            [
+                                'id' => '123',
+                                'component' => 'grid:column',
+                                'properties' => [
+                                    'start' => null,
+                                    'span' => null,
+                                ],
+                                'slots' => [
+                                    'content' => [
+                                        [
+                                            'id' => '123',
+                                            'component' => 'media:image',
+                                            'properties' => [
+                                                'media' => $media,
+                                            ],
+                                        ]
+                                    ],
+                                ]
+                            ],
+                        ],
+                        'column-2' => [
+                            [
+                                'id' => 'ABC',
+                                'component' => 'grid:column',
+                                'properties' => [
+                                    'start' => null,
+                                    'span' => null,
+                                ],
+                                'slots' => [
+                                    'default' => [
+                                        [
+                                            'id' => '123',
+                                            'component' => 'content:text',
+                                            'properties' => [
+                                                'text' => '<h1>Lorem ipsum dolor sit amet.</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>',
+                                            ],
+                                        ],
+                                        [
+                                            'id' => '123',
+                                            'component' => 'alert:alert',
+                                            'properties' => [
+                                                'text' => 'Hello World',
+                                            ],
+                                            'slots' => [
+                                                'content' => [
+                                                    [
+                                                        'id' => '123',
+                                                        'component' => 'button:button',
+                                                        'properties' => [
+                                                            'text' => 'Click Me!',
+                                                        ],
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ],
+                                ]
+                            ]
+                        ]
+                    ],
+                ],
+                [
+                    'id' => '123',
+                    'component' => 'product:listing',
+                    'properties' => [
+                        'listing' => $listingData
+                    ],
+                ]
+            ]
+        ];
     }
 }

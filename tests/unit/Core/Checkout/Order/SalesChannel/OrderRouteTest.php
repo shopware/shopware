e6ebd25ce1c54c74b0a8061571fed95602470fc4
Max Stegmeyer <m.stegmeyer@shopware.com>
2025-09-05 12:15:25 +0200

    fix: ecs

--- tests/unit/Core/Checkout/Order/SalesChannel/OrderRouteTest.php
+++ tests/unit/Core/Checkout/Order/SalesChannel/OrderRouteTest.php
@@ -5,7 +5,6 @@ namespace Shopware\Tests\Unit\Core\Checkout\Order\SalesChannel;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\CustomerException;
 use Shopware\Core\Checkout\Customer\SalesChannel\AccountService;

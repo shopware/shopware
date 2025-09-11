92d8f6c67b212ba5d1b39b82de0cf59196b0f0d6
Dominik Grothaus <d.grothaus@shopware.com>
2025-09-11 09:28:53 +0200

    feat: Improve the restrictive filter for private media

--- src/Core/Content/Media/Subscriber/MediaVisibilityRestrictionSubscriber.php
+++ src/Core/Content/Media/Subscriber/MediaVisibilityRestrictionSubscriber.php
@@ -112,14 +112,19 @@ class MediaVisibilityRestrictionSubscriber implements EventSubscriberInterface
 
     private function getMediaRestriction(): MultiFilter
     {
-        return new MultiFilter('OR', [
-            new EqualsFilter('private', false),
-            new MultiFilter('AND', [
-                new EqualsFilter('private', true),
-                new EqualsFilter('mediaFolder.defaultFolder.entity', 'product_download'),
+        return new MultiFilter('AND', [
+            new MultiFilter('OR', [
+                new EqualsFilter('private', false),
+                new MultiFilter('AND', [
+                    new EqualsFilter('private', true),
+                    new EqualsFilter('mediaFolder.defaultFolder.entity', 'product_download'),
+                ]),
             ]),
-        ]);
-    }
+            new MultiFilter('OR', [
+                new EqualsFilter('mediaFolder.configuration.private', false),
+                new EqualsFilter('mediaFolder.configuration.private', null),
+            ]),
+        ]);    }
 
     private function getMediaFolderRestriction(): MultiFilter
     {

c85ba5621cdfb3e50423bc4703ff0cca6678baa5
thuong.le <levienthuong@gmail.com>
2025-09-15 17:07:43 +0700

    feat: make CMS product streams loading compatible with ES

--- src/Core/Content/Product/Cms/ProductSlider/ProductStreamProcessor.php
+++ src/Core/Content/Product/Cms/ProductSlider/ProductStreamProcessor.php
@@ -172,14 +172,15 @@ class ProductStreamProcessor extends AbstractProductSliderProcessor
 
     private function addRandomSort(Criteria $criteria): void
     {
+        // these fields should be compatible with Elasticsearch mapped fields for sorting, see: \Shopware\Elasticsearch\Product\ElasticsearchProductDefinition::getMapping
         $fields = [
             'id',
             'stock',
             'releaseDate',
-            'manufacturer.id',
-            'unit.id',
-            'tax.id',
-            'cover.id',
+            'manufacturerId',
+            'deliveryTimeId',
+            'taxId',
+            'coverId',
         ];
         shuffle($fields);
         $fields = \array_slice($fields, 0, 2);

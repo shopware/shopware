c032fa8b07bf48002af4d0cebbf8f635927ecd90
Simon Fiebranz <s.fiebranz@shopware.com>
2025-09-25 11:05:28 +0200

    refactor: adjusting twig variable parser

--- src/Core/Framework/Adapter/Twig/TwigVariableParser.php
+++ src/Core/Framework/Adapter/Twig/TwigVariableParser.php
@@ -5,13 +5,17 @@ namespace Shopware\Core\Framework\Adapter\Twig;
 use Shopware\Core\Framework\Log\Package;
 use Twig\Environment;
 use Twig\Loader\ArrayLoader;
-use Twig\Node\Expression\AssignNameExpression;
+use Twig\Node\EmptyNode;
 use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Expression\FilterExpression;
 use Twig\Node\Expression\GetAttrExpression;
 use Twig\Node\Expression\NameExpression;
+use Twig\Node\Expression\Variable\AssignContextVariable;
 use Twig\Node\ForNode;
 use Twig\Node\Node;
+use Twig\Node\Nodes;
 use Twig\Node\SetNode;
+use Twig\Node\TextNode;
 
 #[Package('framework')]
 class TwigVariableParser
@@ -40,66 +44,79 @@ class TwigVariableParser
     }
 
     /**
-     * @param array<Node> $nodes
-     * @param array<mixed> $aliases
+     * @param array<Nodes> $nodes
+     * @param array<string, string> $aliases
      *
-     * @return array<mixed>
+     * @return array<string,string>
      */
-    private function getVariables(iterable $nodes, array $aliases = [], bool $excludeAssignNameExpression = true): array
+    private function getVariables(iterable $nodes, array $aliases = []): array
     {
         $variables = [];
+
         foreach ($nodes as $node) {
-            if ($excludeAssignNameExpression && $node instanceof AssignNameExpression) {
+            if ($node instanceof EmptyNode || $node instanceof TextNode) {
                 continue;
             }
 
-            if ($node instanceof NameExpression) {
+            if ($node instanceof NameExpression || $nodes instanceof AssignContextVariable) {
                 $name = $node->getAttribute('name');
-
-                if (isset($aliases[$name])) {
-                    $name = $aliases[$name];
-                }
-
-                $variables[$name] = $name;
-
+                $variables[$name] = $aliases[$name] ?? $name;
                 continue;
-            }
-
-            if ($node instanceof SetNode) {
-                $target = implode('.', $this->getVariables([$node->getNode('names')], $aliases, false));
-                $aliases[$target] = '';
             }
 
             if ($node instanceof ConstantExpression && $nodes instanceof GetAttrExpression) {
                 $value = $node->getAttribute('value');
-                if (!empty($value)) {
+                $variables[$value] = $value;
+                continue;
+            }
+
+            if ($node instanceof ConstantExpression && $nodes instanceof FilterExpression) {
+                $value = $node->getAttribute('value');
+                if ($value === 'first' || $value === 'last') {
                     $variables[$value] = $value;
                 }
-
                 continue;
             }
 
             if ($node instanceof GetAttrExpression) {
-                $path = implode('.', $this->getVariables($node, $aliases, $excludeAssignNameExpression));
+                $path = implode('.', $this->getVariables($node, $aliases));
                 if (!empty($path)) {
                     $variables[$path] = $path;
                 }
+                continue;
+            }
 
+            if ($node instanceof SetNode) {
+                $i = 0;
+                $names = $node->getNode('names');
+                $values = $node->getNode('values');
+                while ($names->hasNode((string) $i)) {
+                    $alias = implode('.', $this->getVariables([$names->getNode((string) $i)], $aliases));
+                    $valueNode = $values->getNode((string) $i);
+                    if ($valueNode instanceof GetAttrExpression || $valueNode instanceof NameExpression) {
+                        $aliases[$alias] = implode('.', $this->getVariables([$valueNode], $aliases));
+                    } else {
+                        $variables = array_merge($variables, $this->getVariables($valueNode, $aliases));
+                        $aliases[$alias] = '';
+                    }
+                    $i++;
+                }
                 continue;
             }
 
             if ($node instanceof ForNode) {
-                $target = implode('.', $this->getVariables([$node->getNode('seq')], $aliases, $excludeAssignNameExpression));
-                $source = $node->getNode('value_target')->getAttribute('name');
-
-                $aliases[$source] = $target . '.0';
+                $path = implode('.', $this->getVariables([$node->getNode('seq')], $aliases));
+                $loopAliases = [...$aliases];
+                $loopAliases[$node->getNode('value_target')->getAttribute('name')] = $path . '.0';
+                $variables = array_merge($variables, $this->getVariables([$node->getNode('body')], $loopAliases));
+                continue;
             }
 
             if ($node instanceof Node) {
-                $variables += $this->getVariables($node, $aliases, $excludeAssignNameExpression);
+                $variables = array_merge($variables, $this->getVariables($node, $aliases));
             }
         }
 
-        return array_filter($variables);
+        return array_filter($variables, fn (string $variable) => !empty($variable) && !\str_starts_with($variable, '.'));
     }
 }

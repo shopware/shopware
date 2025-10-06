8b04707e4ba8db62e729fdacf64d29c6327d87a5
Florian Keller <f.keller@shopware.com>
2025-10-06 10:46:06 +0200

    fix: ecs

--- src/Core/Checkout/Cart/CartRuleLoader.php
+++ src/Core/Checkout/Cart/CartRuleLoader.php
@@ -223,7 +223,7 @@ class CartRuleLoader implements ResetInterface
         $country = $context->getShippingLocation()->getCountry();
         $customer = $context->getCustomer();
 
-        if ($customer?->getAccountType() == CustomerEntity::ACCOUNT_TYPE_BUSINESS) {
+        if ($customer?->getAccountType() === CustomerEntity::ACCOUNT_TYPE_BUSINESS) {
             $isReachedCompanyTaxFreeAmount = $this->taxDetector->isCompanyTaxFree($context, $country)
                 && $this->isReachedCountryTaxFreeAmount($context, $country, $cartNetAmount, CountryDefinition::TYPE_COMPANY_TAX_FREE);
 

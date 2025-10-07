9fc5141c5e838c64ef204ab02e7f76760671d585
a.okko <14863712+ravenokko@users.noreply.github.com>
2025-10-07 09:59:43 +0200

    test: a11y compatible RegisteredUserBuysProduct

--- tests/acceptance/tests/Checkout/RegisteredUserBuysProduct.spec.ts
+++ tests/acceptance/tests/Checkout/RegisteredUserBuysProduct.spec.ts
@@ -12,8 +12,8 @@ test('Registered shop customer buys a product.', { tag: ['@Checkout', '@Storefro
     AddProductToCart,
     ProceedFromProductToCheckout,
     ConfirmTermsAndConditions,
-    SelectInvoicePaymentOption,
-    SelectStandardShippingOption,
+    SelectPaymentMethod,
+    SelectShippingMethod,
     SubmitOrder,
 }) => {
     const product = await TestDataService.createBasicProduct();
@@ -29,9 +29,8 @@ test('Registered shop customer buys a product.', { tag: ['@Checkout', '@Storefro
     await ShopCustomer.attemptsTo(ProceedFromProductToCheckout());
 
     await ShopCustomer.attemptsTo(ConfirmTermsAndConditions());
-    await ShopCustomer.attemptsTo(SelectInvoicePaymentOption());
-    await ShopCustomer.attemptsTo(SelectStandardShippingOption());
-
+    await ShopCustomer.attemptsTo(SelectPaymentMethod('Paid in advance'));
+    await ShopCustomer.attemptsTo(SelectShippingMethod('Standard'));
     await ShopCustomer.expects(StorefrontCheckoutConfirm.grandTotalPrice).toContainText('€10.00');
 
     await ShopCustomer.attemptsTo(SubmitOrder());

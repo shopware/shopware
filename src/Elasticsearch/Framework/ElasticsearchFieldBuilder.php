a4ef946a702007abf2f70aafec03bfb1a1294c24
Vin Le <t.le@shopware.com>
2025-10-13 18:03:47 +0700

    feat: compatibility with OpenSearch 3.x (#12979)

--- src/Elasticsearch/Framework/ElasticsearchFieldBuilder.php
+++ src/Elasticsearch/Framework/ElasticsearchFieldBuilder.php
@@ -122,6 +122,10 @@ class ElasticsearchFieldBuilder
             $mapping['properties'][$name] = $esType;
         }
 
+        if ($mapping['properties'] === []) {
+            unset($mapping['properties']);
+        }
+
         return $mapping;
     }
 }

2edc68a97225c8a59fb75ef341177a0b003011e1
max <max@swk-web.com>
2025-10-14 12:43:25 +0200

    fixup: Remove Symfony validator of the honeypot captcha completly

--- src/Storefront/Framework/Captcha/HoneypotCaptcha.php
+++ src/Storefront/Framework/Captcha/HoneypotCaptcha.php
@@ -2,9 +2,11 @@
 
 namespace Shopware\Storefront\Framework\Captcha;
 
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Blank;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Package('framework')]
@@ -13,7 +15,9 @@ class HoneypotCaptcha extends AbstractCaptcha
     final public const CAPTCHA_NAME = 'honeypot';
     final public const CAPTCHA_REQUEST_PARAMETER = 'shopware_surname_confirm';
 
-    #[Blank]
+    /**
+     * @deprecated tag:v6.8.0 - Will be removed, as the Symfony validator is not used anymore to validate the honeypot captcha
+     */
     protected ?string $honeypotValue = null;
 
     /**
@@ -24,13 +28,29 @@ class HoneypotCaptcha extends AbstractCaptcha
     }
 
     /**
+     * Default method for determining constraints when using the Symfony validator.
+     *
+     * @deprecated tag:v6.8.0 - Will be removed, as the Symfony validator is not used anymore to validate the honeypot captcha
+     */
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        Feature::triggerDeprecationOrThrow('v6.8.0.0', Feature::deprecatedMethodMessage(self::class, __METHOD__, 'v6.8.0.0'));
+
+        $metadata->addPropertyConstraint('honeypotValue', new Blank());
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function isValid(Request $request, array $captchaConfig): bool
     {
-        $this->honeypotValue = $request->get(self::CAPTCHA_REQUEST_PARAMETER, '');
+        if (!Feature::isActive('v6.8.0.0')) {
+            $this->honeypotValue = $request->get(self::CAPTCHA_REQUEST_PARAMETER, '');
 
-        return \count($this->validator->validate($this)) < 1;
+            return \count($this->validator->validate($this)) < 1;
+        }
+
+        return $request->get(self::CAPTCHA_REQUEST_PARAMETER, '') === '';
     }
 
     /**

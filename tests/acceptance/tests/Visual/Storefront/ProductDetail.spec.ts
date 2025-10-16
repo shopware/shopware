09cba1ec38e48e27f3bd6bc045d8e0b5475945cc
Orkun Cavli <o.cavli@shopware.com>
2025-10-16 11:35:40 +0200

    add checkvisibility and screenshot

--- tests/acceptance/tests/Visual/Storefront/ProductDetail.spec.ts
+++ tests/acceptance/tests/Visual/Storefront/ProductDetail.spec.ts
@@ -4,37 +4,17 @@ test('Visual: Storefront Product Detail Page', { tag: '@Visual' }, async ({
     ShopCustomer,
     TestDataService,
     StorefrontProductDetail,
-    StorefrontHome,
+    CheckVisibilityInHome,
     }) => {
 
-    const currency = await TestDataService.getCurrency('EUR');
     const product = await TestDataService.createBasicProduct({
-        name: 'Test Product',
-        productNumber: 'TEST-123',
-        description: null,
-        stock: 10,
-        price: [
-            {
-                currencyId: currency.id,
-                gross: 10,
-                linked: false,
-                net: 8.4,
-            },
-        ],
+        name: 'TestProduct',
+        productNumber: '123',
     });
-    await TestDataService.clearCaches();
     await TestDataService.setSystemConfig({ 'core.basicInformation.useDefaultCookieConsent': false });
 
-    await ShopCustomer.expects(async () => {
-        await test.step('Wait for products to be visible.', async () => {
-            await ShopCustomer.goesTo(`${StorefrontHome.url()}?a=${Date.now()}`);
-            const productLocator1 = await StorefrontHome.getListingItemByProductName(product.name);
-            await ShopCustomer.expects(productLocator1.productName).toBeVisible();
-        });
-    }).toPass({
-        intervals: [1_000, 2_500], // retry after 1 seconds, then every 2.5 seconds
-    });
     await test.step('Creates a screenshot of the product detail page General tab.', async () => {
+        await CheckVisibilityInHome(product.name);
         await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
         ShopCustomer.expects(StorefrontProductDetail.addToCartButton).toBeVisible();
         await expect(StorefrontProductDetail.page).toHaveScreenshot('Product-Detail-Page.png', {

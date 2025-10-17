ad352d1f6c502f60065521a1991ff061e43c8078
moshimorschi <lena.forlin@gmail.com>
2025-10-17 12:51:26 +0200

    fixed offcanvas screenshot

--- tests/acceptance/tests/Visual/Storefront/ShoppingCart.spec.ts
+++ tests/acceptance/tests/Visual/Storefront/ShoppingCart.spec.ts
@@ -2,22 +2,25 @@ import { test, expect } from '@fixtures/AcceptanceTest';
 import { hideElements, replaceElements } from '@shopware-ag/acceptance-test-suite';
 
 test('Visual: Storefront Shopping Cart.', { tag: '@Visual' }, async ({
-    ShopCustomer,
-    TestDataService,
-    StorefrontProductDetail,
     AddProductToCart,
+    ShopCustomer,
+    StorefrontProductDetail,
     StorefrontOffCanvasCart,
     StorefrontCheckoutCart,
+    TestDataService,
 }) => {
     const product = await TestDataService.createBasicProduct();
-    await TestDataService.setSystemConfig({ 'core.basicInformation.useDefaultCookieConsent': false });
 
     await test.step('Creates a screenshot of off-canvas shopping cart in storefront.', async () => {
         await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
         await ShopCustomer.attemptsTo(AddProductToCart(product));
-        await ShopCustomer.expects(StorefrontOffCanvasCart.headline).toBeVisible();
+        await ShopCustomer.expects(StorefrontOffCanvasCart.goToCartButton).toBeVisible();
 
-        await replaceElements(StorefrontOffCanvasCart.page, [
+        await hideElements(StorefrontProductDetail.page, [
+            '.cookie-permission-container',
+        ]);
+
+        await replaceElements(StorefrontProductDetail.page, [
             '.product-detail-name',
             '.product-detail-description-title',
             '.line-item-label',
@@ -25,16 +28,18 @@ test('Visual: Storefront Shopping Cart.', { tag: '@Visual' }, async ({
             '.line-item-delivery-date',
         ]);
 
-        await expect(StorefrontOffCanvasCart.page).toHaveScreenshot('OffcanvasCart.png', {
-            fullPage: true,
-        });
+        await StorefrontProductDetail.page.setViewportSize({ width: 1440, height: 800 });
+
+        await expect(StorefrontProductDetail.page.locator('.offcanvas')).toHaveScreenshot('OffcanvasCart.png', {});
     });
 
     await test.step('Creates a screenshot of shopping cart (checkout/cart) in storefront.', async () => {
-        await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
-        await ShopCustomer.attemptsTo(AddProductToCart(product));
-        await ShopCustomer.expects(StorefrontOffCanvasCart.headline).toBeVisible();
         await StorefrontOffCanvasCart.goToCartButton.click();
+        await ShopCustomer.expects(StorefrontCheckoutCart.goToCheckoutButton).toBeVisible();
+
+        await hideElements(StorefrontCheckoutCart.page, [
+            '.cookie-permission-container',
+        ]);
 
         await replaceElements(StorefrontCheckoutCart.page, [
             '.line-item-label',

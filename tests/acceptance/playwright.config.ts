64e4430c4c9d6d7ff35749eb587573d7bfbf0922
Florian Robel <f.robel@shopware.com>
2025-10-20 12:47:21 +0200

    fix: run product analytics tests before overall tests are executed

--- tests/acceptance/playwright.config.ts
+++ tests/acceptance/playwright.config.ts
@@ -79,6 +79,14 @@ export default defineConfig({
     /* Configure projects for major browsers */
     projects: [
         {
+            name: 'Product Analytics',
+            use: {
+                ...devices['Desktop Chrome'],
+            },
+            dependencies: ['Setup'],
+            grep: /@ProductAnalytics/,
+        },
+        {
             name: 'Setup',
             use: {
                 ...devices['Desktop Chrome'],
@@ -93,8 +101,8 @@ export default defineConfig({
                     args: ['--remote-debugging-port=9222'],
                 },
             },
-            dependencies: ['Setup'],
-            grepInvert: /@Install|@Update|@Visual|@Setup.*/,
+            dependencies: ['Product Analytics'],
+            grepInvert: /@Install|@Update|@Visual|@Setup|@ProductAnalytics.*/,
         },
         {
             name: 'Install',

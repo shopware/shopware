4b19f244a8efbfa0484d65b3da484dcc2663f2a8
moshimorschi <lena.forlin@gmail.com>
2025-10-23 15:12:56 +0200

    refactor: update DownloadDigitalProductFromOrder task (backport-6.6.x)

--- tests/acceptance/tasks/ShopCustomer/Account/DownloadDigitalProductFromOrder.ts
+++ tests/acceptance/tasks/ShopCustomer/Account/DownloadDigitalProductFromOrder.ts
@@ -2,7 +2,7 @@ import { test as base, expect } from '@playwright/test';
 import type { FixtureTypes, Task } from '@fixtures/AcceptanceTest';
 
 export const DownloadDigitalProductFromOrderAndExpectContentToBe = base.extend<{ DownloadDigitalProductFromOrderAndExpectContentToBe: Task }, FixtureTypes>({
-    DownloadDigitalProductFromOrderAndExpectContentToBe: async ({ StorefrontAccountOrder }, use)=> {
+    DownloadDigitalProductFromOrderAndExpectContentToBe: async ({ ShopCustomer, StorefrontAccountOrder }, use)=> {
         const task = (contentOfFile: string) => {
             return async function DownloadDigitalProductFromOrder() {
                 // TODO: Migrate to StorefrontAccountOrder.orderExpandButton.click(); when https://github.com/shopware/acceptance-test-suite/pull/126 is released.
@@ -10,7 +10,7 @@ export const DownloadDigitalProductFromOrderAndExpectContentToBe = base.extend<{
 
                 const [newTab] = await Promise.all([
                     StorefrontAccountOrder.page.waitForEvent('popup'),
-                    await StorefrontAccountOrder.digitalProductDownloadButton.click(),
+                    await ShopCustomer.presses(StorefrontAccountOrder.digitalProductDownloadButton),
                 ]);
                 const tabContent = await newTab.content();
                 expect(tabContent).toContain(contentOfFile);

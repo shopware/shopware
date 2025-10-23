c7fff1163cd9a56bc860a137d177cef45d0c38fc
moshimorschi <lena.forlin@gmail.com>
2025-10-23 16:53:37 +0200

    backport: ChangeOrderPaymentMethod test

--- tests/acceptance/tests/Account/ChangeOrderPaymentMethod.spec.ts
+++ tests/acceptance/tests/Account/ChangeOrderPaymentMethod.spec.ts
@@ -6,6 +6,7 @@ test('Customers can update the payment method for an existing order in the store
     StorefrontCheckoutOrderEdit,
     TestDataService,
     Login,
+    SelectPaymentMethod,
 }) => {
     const product = await TestDataService.createBasicProduct();
     const customer = await TestDataService.createCustomer();
@@ -22,12 +23,12 @@ test('Customers can update the payment method for an existing order in the store
     const orderItemLocators = await StorefrontAccountOrder.getOrderByOrderNumber(order.orderNumber);
     await ShopCustomer.expects(orderItemLocators.orderPaymentMethod).toContainText('Invoice');
     
-    await orderItemLocators.orderActionsButton.click();
-    await orderItemLocators.orderChangePaymentMethodButton.click();
+    await ShopCustomer.presses(orderItemLocators.orderActionsButton);
+    await ShopCustomer.presses(orderItemLocators.orderChangePaymentMethodButton);
 
-    await StorefrontCheckoutOrderEdit.getPaymentMethodButton(newPaymentMethod.name).click();
-    await StorefrontCheckoutOrderEdit.completePaymentButton.click();
-
+    await ShopCustomer.attemptsTo(SelectPaymentMethod(newPaymentMethod.name));
+    await ShopCustomer.presses(StorefrontCheckoutOrderEdit.completePaymentButton);
+    
     await ShopCustomer.goesTo(StorefrontAccountOrder.url());
     await ShopCustomer.expects(orderItemLocators.orderPaymentMethod).toContainText(newPaymentMethod.name);
 });

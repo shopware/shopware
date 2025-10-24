6b5dfa1df130ec4ff84df0b3f0d48d545d7aecea
moshimorschi <lena.forlin@gmail.com>
2025-10-24 12:59:11 +0200

    backport: test PageNotFound

--- tests/acceptance/tests/Categories/PageNotFound.spec.ts
+++ tests/acceptance/tests/Categories/PageNotFound.spec.ts
@@ -11,7 +11,7 @@ test(
         await ShopCustomer.expects(StorefrontPageNotFound.pageNotFoundMessage).toHaveText(
             `We are sorry, the page you're looking for could not be found. It may no longer exist or may have been moved.`
         );
-        await StorefrontPageNotFound.backToShopButton.click();
+        await ShopCustomer.presses(StorefrontPageNotFound.backToShopButton);
         await ShopCustomer.expects(StorefrontHome.mainNavigationLink).toContainText('Home');
     }
 );

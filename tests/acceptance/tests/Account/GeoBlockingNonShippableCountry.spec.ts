95c464187c0cba530123253e09137dde64022d41
moshimorschi <lena.forlin@gmail.com>
2025-10-28 12:03:20 +0100

    fix leftover .click() method

--- tests/acceptance/tests/Account/GeoBlockingNonShippableCountry.spec.ts
+++ tests/acceptance/tests/Account/GeoBlockingNonShippableCountry.spec.ts
@@ -137,7 +137,7 @@ test(
                 await ShopCustomer.expects(StorefrontAccountAddresses.useDefaultShippingAddressButton).toBeDisabled();
                 await ShopCustomer.expects(StorefrontAccountAddresses.deliveryNotPossibleAlert).toBeVisible();
             } else {
-                await StorefrontAccountAddresses.addressDropdownButton.click();
+                await ShopCustomer.presses(StorefrontAccountAddresses.addressDropdownButton);
                 await ShopCustomer.expects(StorefrontAccountAddresses.availableAddressesUseAsBillingAddress).toBeEnabled();
                 await ShopCustomer.expects(StorefrontAccountAddresses.availableAddressesUseAsShippingAddress).toBeDisabled();
             }

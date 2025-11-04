374d0c53eb2fcc9c1effe6bfee8c13a4cd9c1106
Jonas Elfering <j.elfering@shopware.com>
2025-11-04 09:26:09 +0100

    Reset unrelated change

--- src/Storefront/Framework/Routing/RequestTransformer.php
+++ src/Storefront/Framework/Routing/RequestTransformer.php
@@ -206,9 +206,8 @@ class RequestTransformer implements RequestTransformerInterface
             );
         }
 
-        $transformedRequest->headers->set(PlatformRequest::HEADER_LANGUAGE_ID, $salesChannel['languageId']);
-        // add all headers from the original request, overrides the headers from the domain mapping if they are passed on the request directly
         $transformedRequest->headers->add($request->headers->all());
+        $transformedRequest->headers->set(PlatformRequest::HEADER_LANGUAGE_ID, $salesChannel['languageId']);
         $transformedRequest->attributes->set(self::ORIGINAL_REQUEST_URI, $currentRequestUri);
 
         return $transformedRequest;

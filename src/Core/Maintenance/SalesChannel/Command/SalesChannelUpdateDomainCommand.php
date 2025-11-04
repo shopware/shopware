db04f0ef1085101fc8422eaf643cc4964b9f832f
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2025-11-04 10:20:46 +0100

    Update src/Core/Maintenance/SalesChannel/Command/SalesChannelUpdateDomainCommand.php
    Co-authored-by: Michael Telgmann <mitelg@users.noreply.github.com>

--- src/Core/Maintenance/SalesChannel/Command/SalesChannelUpdateDomainCommand.php
+++ src/Core/Maintenance/SalesChannel/Command/SalesChannelUpdateDomainCommand.php
@@ -22,7 +22,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 #[AsCommand(
     name: 'sales-channel:update:domain',
-    description: 'Updates a sales channel domain with a new domain for all or specific sales channels',
+    description: 'Updates the sales channel domain with a new domain for all or specific sales channels matching the previous domain, except headless sales channels',
 )]
 #[Package('discovery')]
 class SalesChannelUpdateDomainCommand extends Command

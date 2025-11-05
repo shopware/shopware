6db1b613dc4281489487a8ac98754e9dd0b02125
a.okko <14863712+ravenokko@users.noreply.github.com>
2025-11-05 21:17:19 +0100

    test: update StorefrontLanguages for language agnostic locators

--- tests/acceptance/tests/SalesChannels/StorefrontLanguages.spec.ts
+++ tests/acceptance/tests/SalesChannels/StorefrontLanguages.spec.ts
@@ -9,22 +9,21 @@ test(
         const salesChannelId = TestDataService.defaultSalesChannel.id;
         const language = await getLanguageData(TestDataService.AdminApiClient, 'de-DE');
         const snippetSetId = await getSnippetSetId(TestDataService.AdminApiClient, 'de-DE');
+        const languageDropdownDE = StorefrontHeader.page.locator('.top-bar-language').getByRole('button', { name: 'Deutsch' });
+        const addToShoppingCartButtonDE = StorefrontHome.page.getByRole('listitem').filter({ hasText: product.name }).getByRole('button', { name: 'In den Warenkorb' });
 
         await TestDataService.assignSalesChannelLanguage(salesChannelId, language.id);
         await TestDataService.createSalesChannelDomain({ languageId: language.id, snippetSetId: snippetSetId });
 
         await TestDataService.clearCaches();
 
-        const productListing = StorefrontHome.productListItems.filter({
-            has: StorefrontHome.page.getByRole('link', { name: product.name }),
-        });
-        const addToCartButton = productListing.filter({ has: StorefrontHome.page.getByRole('button') });
+        const productListing = await StorefrontHome.getListingItemByProductName(product.name);
 
         await ShopCustomer.expects(async () => {
             await test.step('Customer can view languages menu', async () => {
                 await ShopCustomer.goesTo(`${StorefrontHome.url()}?a=${Date.now()}`);
                 await ShopCustomer.expects(StorefrontHeader.languagesDropdown).toContainText('English');
-                await ShopCustomer.expects(addToCartButton).toContainText('Add to shopping cart');
+                await ShopCustomer.expects(productListing.productAddToShoppingCart).toContainText('Add to shopping cart');
             });
         }).toPass({
             intervals: [1_000, 2_500], // retry after 1 seconds, then every 2.5 seconds
@@ -33,7 +32,7 @@ test(
         await test.step('Customer can select a different language', async () => {
             await ShopCustomer.presses(StorefrontHeader.languagesDropdown);
             await ShopCustomer.presses(StorefrontHeader.languagesMenuOptions.getByText('Deutsch'));
-            await ShopCustomer.expects(StorefrontHeader.languagesDropdown).toContainText('Deutsch');
-            await ShopCustomer.expects(addToCartButton).toContainText('In den Warenkorb');
+            await ShopCustomer.expects(languageDropdownDE).toBeVisible();
+            await ShopCustomer.expects(addToShoppingCartButtonDE).toBeVisible();
     });
 })

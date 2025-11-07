d90b5b22bb6a99efefb20eec53dc1f4dd7d38c06
Michael Telgmann <m.telgmann@shopware.com>
2025-11-07 11:31:50 +0100

    fix SEO route not found exception

--- src/Core/Content/Seo/Exception/SeoUrlRouteNotFoundException.php
+++ src/Core/Content/Seo/Exception/SeoUrlRouteNotFoundException.php
@@ -3,19 +3,28 @@
 namespace Shopware\Core\Content\Seo\Exception;
 
 use Shopware\Core\Content\Seo\SeoException;
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\HttpFoundation\Response;
 
 #[Package('inventory')]
 class SeoUrlRouteNotFoundException extends SeoException
 {
-    final public const ERROR_CODE = 'CONTENT__SEO_URL_ROUTE_NOT_FOUND';
+    /**
+     * @deprecated tag:v6.8.0 - Will be removed with the next major as it is unused
+     */
+    final public const ERROR_CODE = 'FRAMEWORK__SEO_URL_ROUTE_NOT_FOUND';
 
     public function __construct(string $routeName)
     {
+        $errorCode = self::SEO_URL_ROUTE_NOT_FOUND;
+        if (!Feature::isActive('v6.8.0.0')) {
+            $errorCode = self::ERROR_CODE;
+        }
+
         parent::__construct(
             Response::HTTP_NOT_FOUND,
-            self::ERROR_CODE,
+            $errorCode,
             self::$couldNotFindMessage,
             ['entity' => 'SEO URL route', 'field' => 'name', 'value' => $routeName]
         );

1d817379caf4ff401386d1185373b620f34f3af4
Yusuf Tür <yusufttur@gmail.com>
2025-11-19 16:49:05 +0100

    test: add product review visual test

--- tests/acceptance/tests/Visual/Storefront/ProductDetail.spec.ts
+++ tests/acceptance/tests/Visual/Storefront/ProductDetail.spec.ts
@@ -11,6 +11,8 @@ test('Visual: Storefront Product Detail Page', { tag: '@Visual' }, async ({
         name: 'TestProduct',
         productNumber: '123',
     });
+    await TestDataService.createProductReview(product.id, { title: 'Very Good', points: 4 });
+    await TestDataService.createProductReview(product.id, { title: 'Excellent', points: 5 });
     await TestDataService.setSystemConfig({ 'core.basicInformation.useDefaultCookieConsent': false });
 
     await test.step('Creates a screenshot of the product detail page General tab.', async () => {
@@ -22,3 +24,35 @@ test('Visual: Storefront Product Detail Page', { tag: '@Visual' }, async ({
         });
     });
 });
+
+test('Visual: Storefront Product Detail Review',{ tag: '@Visual' }, async ({ 
+    ShopCustomer, 
+    TestDataService, 
+    StorefrontProductDetail, 
+    CheckVisibilityInHome,
+    }) => {
+    const product = await TestDataService.createBasicProduct({
+        name: 'TestProduct',
+        productNumber: '123',
+    });
+
+    await TestDataService.createProductReview(product.id, { title: 'Very Good', points: 4 });
+    await TestDataService.createProductReview(product.id, { title: 'Excellent', points: 5 });
+    await TestDataService.setSystemConfig({ 'core.basicInformation.useDefaultCookieConsent': false });
+
+    await test.step('Creates a screenshot of the product detail page General tab.', async () => {
+        await CheckVisibilityInHome(product.name);
+        await ShopCustomer.goesTo(StorefrontProductDetail.url(product));
+        await StorefrontProductDetail.reviewsTab.click();
+
+        await ShopCustomer.expects(StorefrontProductDetail.productReviewRating).toBeVisible();
+        await ShopCustomer.expects(StorefrontProductDetail.productReviewsLink).toHaveText('2 Reviews');
+        await ShopCustomer.expects(StorefrontProductDetail.reviewCounter).toContainText('2 reviews');
+        await ShopCustomer.expects(StorefrontProductDetail.reviewListingItems).toHaveCount(2);
+
+        await expect(StorefrontProductDetail.page).toHaveScreenshot('Product-Detail-Review.png', {
+            fullPage: true,
+        });
+    });
+});
+

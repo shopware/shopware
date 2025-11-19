17a3c14c8843142e93967427e1861e6bc701c7fd
thuong.le <levienthuong@gmail.com>
2025-11-19 11:22:33 +0700

    feat: dispatch events when product type change

--- src/Core/Content/Product/DataAbstractionLayer/ProductTypeInitiator.php
+++ src/Core/Content/Product/DataAbstractionLayer/ProductTypeInitiator.php
@@ -4,17 +4,21 @@ namespace Shopware\Core\Content\Product\DataAbstractionLayer;
 
 use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
+use Shopware\Core\Content\Product\Events\ProductTypeBeforeChangeEvent;
+use Shopware\Core\Content\Product\Events\ProductTypeChangedEvent;
 use Shopware\Core\Content\Product\ProductTypeRegistry;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Uuid\Uuid;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[Package('inventory')]
 class ProductTypeInitiator
 {
     public function __construct(
         private readonly Connection $connection,
-        private readonly ProductTypeRegistry $productTypeRegistry
+        private readonly ProductTypeRegistry $productTypeRegistry,
+        private readonly EventDispatcherInterface $eventDispatcher
     ) {
     }
 
@@ -51,17 +55,55 @@ class ProductTypeInitiator
             return;
         }
 
-        $this->connection->executeStatement(
+        $idsToUpdate = $this->getIdsWithoutAssignedType($ids, $context);
+
+        if ($idsToUpdate === []) {
+            return;
+        }
+
+        $this->eventDispatcher->dispatch(new ProductTypeBeforeChangeEvent($idsToUpdate, $type, $context));
+
+        $updatedRows = $this->connection->executeStatement(
             'UPDATE `product`
              SET `type` = :type
              WHERE `id` IN (:ids)
                AND `version_id` = :version AND type IS NULL',
             [
                 'type' => $type,
+                'ids' => Uuid::fromHexToBytesList($idsToUpdate),
+                'version' => Uuid::fromHexToBytes($context->getVersionId()),
+            ],
+            ['ids' => ArrayParameterType::BINARY]
+        );
+
+        if ($updatedRows <= 0) {
+            return;
+        }
+
+        $this->eventDispatcher->dispatch(new ProductTypeChangedEvent($idsToUpdate, $type, $context));
+    }
+
+    /**
+     * @param list<string> $ids
+     *
+     * @return list<string>
+     */
+    private function getIdsWithoutAssignedType(array $ids, Context $context): array
+    {
+        /** @var list<string> $idsWithoutType */
+        $idsWithoutType = $this->connection->fetchFirstColumn(
+            'SELECT LOWER(HEX(`id`))
+             FROM `product`
+             WHERE `id` IN (:ids)
+               AND `version_id` = :version
+               AND `type` IS NULL',
+            [
                 'ids' => Uuid::fromHexToBytesList($ids),
                 'version' => Uuid::fromHexToBytes($context->getVersionId()),
             ],
             ['ids' => ArrayParameterType::BINARY]
         );
+
+        return array_values($idsWithoutType);
     }
 }

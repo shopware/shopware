f2c60bda0bf633a588d2dec70d45db0acef313be
Andrii Havryliuk <a.havryliuk@shopware.com>
2025-11-19 13:41:23 +0100

    fixed codestyles

--- src/Core/Content/Product/SalesChannel/Suggest/ResolvedCriteriaProductSuggestRoute.php
+++ src/Core/Content/Product/SalesChannel/Suggest/ResolvedCriteriaProductSuggestRoute.php
@@ -44,7 +44,7 @@ class ResolvedCriteriaProductSuggestRoute extends AbstractProductSuggestRoute
         path: '/store-api/search-suggest',
         name: 'store-api.search.suggest',
         methods: [Request::METHOD_POST, Request::METHOD_GET],
-        defaults: [PlatformRequest::ATTRIBUTE_ENTITY => ProductDefinition::ENTITY_NAME  , PlatformRequest::ATTRIBUTE_HTTP_CACHE => true]
+        defaults: [PlatformRequest::ATTRIBUTE_ENTITY => ProductDefinition::ENTITY_NAME, PlatformRequest::ATTRIBUTE_HTTP_CACHE => true]
     )]
     public function load(Request $request, SalesChannelContext $context, Criteria $criteria): ProductSuggestRouteResponse
     {

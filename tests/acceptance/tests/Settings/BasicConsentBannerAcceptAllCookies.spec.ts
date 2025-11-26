287f8081458eacf34c05c5e8a584090ee7f3448c
Florian Robel <f.robel@shopware.com>
2025-11-26 08:13:54 +0100

    fix: unskip BasicConsentBannerAcceptAllCookies

--- tests/acceptance/tests/Settings/BasicConsentBannerAcceptAllCookies.spec.ts
+++ tests/acceptance/tests/Settings/BasicConsentBannerAcceptAllCookies.spec.ts
@@ -4,9 +4,7 @@ test('As a shop customer, I want to use an "Accept All Cookies" button in the ba
     ShopCustomer,
     StorefrontHome,
     TestDataService,
-    InstanceMeta,
 }) => {
-    test.skip(InstanceMeta.isSaaS, 'Cache invalidation does not happen immediately on SaaS');
 
     await test.step('Enable "Accept All Cookies" button in system configuration', async () => {
         await TestDataService.setSystemConfig({ 'core.basicInformation.acceptAllCookies': true });

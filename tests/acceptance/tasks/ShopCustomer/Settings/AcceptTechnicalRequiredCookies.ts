07d20a95ec7d0c2c8ffef7ed4b74b1ac7c932ef9
a.okko <14863712+ravenokko@users.noreply.github.com>
2025-12-10 17:11:37 +0100

    style: remove unnecessary comment

--- tests/acceptance/tasks/ShopCustomer/Settings/AcceptTechnicalRequiredCookies.ts
+++ tests/acceptance/tasks/ShopCustomer/Settings/AcceptTechnicalRequiredCookies.ts
@@ -8,7 +8,7 @@ interface AcceptTechnicalRequiredCookies {
 export const AcceptTechnicalRequiredCookies = base.extend<AcceptTechnicalRequiredCookies, FixtureTypes>({
     acceptTechnicalRequiredCookies: async ({ StorefrontHome, ShopCustomer }, use) => {
         const acceptTechnicalRequiredCookies = async () => {
-            const cookiePermissionButton = StorefrontHome.page.getByRole('button', { name: 'Only technically required' }); //StorefrontHome.page.locator('.js-cookie-permission-button');
+            const cookiePermissionButton = StorefrontHome.page.getByRole('button', { name: 'Only technically required' });
             await ShopCustomer.expects(cookiePermissionButton).toBeVisible();
             await ShopCustomer.presses(cookiePermissionButton);
             await ShopCustomer.expects(StorefrontHome.page.getByRole('region', { name: 'Cookie preferences' })).not.toBeVisible();

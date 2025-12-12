1e8b2207d4f18f7d44c6f038888cdfa0c37fbfed
Sebastian Franze <s.franze@shopware.com>
2025-12-12 16:43:23 +0100

    feat(product-analytics): Sync consent changes between tabs

--- src/Administration/Resources/app/administration/src/app/init/consent.init.ts
+++ src/Administration/Resources/app/administration/src/app/init/consent.init.ts
@@ -4,6 +4,18 @@
 import useConsentStore from 'src/core/consent/consent.store';
 import ConsentApiService from '../../core/consent/consent.api.service';
 
+type ConsentChangedMessage = {
+    consent: string,
+    state: 'accepted' | 'revoked',
+}
+
+function isConsentChangedMessage(message: unknown): message is ConsentChangedMessage {
+    if (typeof message !== 'object' || message === null) {
+        return false;
+    }
+
+    return 'consent' in message && 'state' in message;
+}
 
 /**
  * @private
@@ -23,4 +35,32 @@ export default async function initConsentStore(): Promise<void> {
     const consentStore = useConsentStore();
 
     await consentStore.update();
+
+    setInterval(() => {
+        void consentStore.update();
+    }, 300000); // every 5 minutes
+
+    const bc = new BroadcastChannel('shopware-consent-channel');
+
+    bc.onmessage  = ({ data }) => {
+        if (!isConsentChangedMessage(data)) {
+            return;
+        }
+
+        const { consent, state } = data;
+
+        if (consentStore.consents[consent]) {
+            consentStore.consents[consent].status = state;
+        }
+    }
+
+    consentStore.$onAction(({ name, args, after}) => {
+        if (name !== 'accept' && name !== 'revoke') {
+            return;
+        }
+
+        after(() => {
+            bc.postMessage({ consent: args[0], state: name === 'accept' ? 'accepted' : 'revoked' })
+        });
+    })
 }

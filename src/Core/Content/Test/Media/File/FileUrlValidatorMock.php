94ad5e6670e9aa7e775750aa8a2f4a93ec845b65
Dennis Garding <d.garding@shopware.com>
2025-12-18 10:34:40 +0100

    chore: replace FileUrlValidator with mock for testing

---
+++ src/Core/Content/Test/Media/File/FileUrlValidatorMock.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Test\Media\File;
+
+use Shopware\Core\Content\Media\File\FileUrlValidatorInterface;
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('discovery')]
+class FileUrlValidatorMock implements FileUrlValidatorInterface
+{
+    public function isValid(string $source): bool
+    {
+        $host = parse_url($source, \PHP_URL_HOST);
+
+        if ($host === false || $host === null) {
+            return false;
+        }
+
+        return true;
+    }
+}

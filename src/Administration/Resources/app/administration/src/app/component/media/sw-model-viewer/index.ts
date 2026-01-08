210a56c429c0f8463ef85d7784a6b80edb82bd23
Felix Frank <f.frank@shopware.com>
2026-01-08 11:53:57 +0100

    feat: add sw-media-model-viewer component to Administration.

---
+++ src/Administration/Resources/app/administration/src/app/component/media/sw-model-viewer/index.ts
@@ -0,0 +1,107 @@
+import template from './sw-model-viewer.html.twig';
+import './sw-model-viewer.scss';
+import { QuickView } from '@shopware-ag/dive/quickview';
+
+const { EventBus } = Shopware.Utils;
+
+/**
+ * @status ready
+ * @description The <u>sw-model-viewer</u> component is used to show a preview of model objects.
+ * @sw-package discovery
+ * @example-type code-only
+ * @component-example
+ * <sw-model-viewer
+ *      :source="mediaEntity"
+ * </sw-model-viewer>
+ */
+export default Shopware.Component.wrapComponentConfig({
+    template,
+
+    props: {
+        source: {
+            type: Object,
+            required: true,
+            validator(value: any) {
+                return value?.getEntityName() === 'media';
+            },
+        },
+    },
+
+    data() {
+        return {
+            canvas: null,
+            isLoading: true,
+            modelEntity: null,
+        } as {
+            canvas: HTMLCanvasElement | null;
+            isLoading: boolean;
+            modelEntity: EntitySchema.Entity<'media'> | null;
+        };
+    },
+
+    watch: {
+        async source() {
+            this.modelEntity = this.source as EntitySchema.Entity<'media'>;
+            await this.initializeQuickView();
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    beforeUnmount() {
+        this.beforeUnmountedComponent();
+    },
+
+    mounted() {
+        this.mountedComponent();
+    },
+
+    methods: {
+        createdComponent(): void {
+            EventBus.on('sw-media-library-item-updated', this.onMediaLibraryItemUpdated);
+        },
+
+        beforeUnmountedComponent(): void {
+            EventBus.off('sw-media-library-item-updated', this.onMediaLibraryItemUpdated);
+        },
+
+        async mountedComponent(): Promise<void> {
+            this.canvas = this.$el?.querySelector?.('.sw-model-viewer-canvas') || null;
+
+            this.modelEntity = this.source as EntitySchema.Entity<'media'>;
+            await this.initializeQuickView();
+        },
+
+        async initializeQuickView(): Promise<void> {
+            this.isLoading = true;
+
+            if(!this.canvas || !this.modelEntity || !this.modelEntity.url) {
+                this.isLoading = false;
+                return;
+            };
+
+            await QuickView(this.modelEntity.url, {
+                canvas: this.canvas,
+            });
+
+            this.isLoading = false;
+        },
+
+        async onMediaLibraryItemUpdated(mediaId: string): Promise<void> {
+            const currentMediaId = this.getCurrentMediaId();
+
+            if (!currentMediaId || currentMediaId !== mediaId) {
+                return;
+            }
+
+            await this.initializeQuickView();
+        },
+
+        getCurrentMediaId() : string | null {
+            const entity = Array.isArray(this.source) ? this.source[0] : this.source;
+            return entity?.id ?? null;
+        },
+    },
+});

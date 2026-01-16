b367fd345acce2dfb4a805038386caff6311eae5
Jonas Elfering <j.elfering@shopware.com>
2026-01-16 15:22:26 +0100

    Revert static analyze kernel change

--- src/Core/DevOps/StaticAnalyze/StaticAnalyzeKernel.php
+++ src/Core/DevOps/StaticAnalyze/StaticAnalyzeKernel.php
@@ -19,18 +19,4 @@ class StaticAnalyzeKernel extends Kernel
             $this->getEnvironment(),
         );
     }
-
-    /**
-     * @return array<string, array<string, mixed>|bool|string|int|float|\UnitEnum|null>
-     */
-    protected function getKernelParameters(): array
-    {
-        $parameters = parent::getKernelParameters();
-
-        // Perf improvement: skip dumping debug container info in static analysis
-        // see: https://symfony.com/doc/current/performance.html#disable-dumping-the-container-as-xml-in-debug-mode
-        $parameters['debug.container.dump'] = false;
-
-        return $parameters;
-    }
 }

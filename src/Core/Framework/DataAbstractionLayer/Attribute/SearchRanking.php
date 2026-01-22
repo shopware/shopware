8a141af0bd32261804e6d342d702cb0dc596f528
Teun Koster <teun@letstalk.nl>
2026-01-22 20:17:52 +0100

    feat: use constants from existing flag in search ranking attribute instead of hardcoded values

--- src/Core/Framework/DataAbstractionLayer/Attribute/SearchRanking.php
+++ src/Core/Framework/DataAbstractionLayer/Attribute/SearchRanking.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace Shopware\Core\Framework\DataAbstractionLayer\Attribute;
 
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\SearchRanking as SearchRankingFlag;
 use Shopware\Core\Framework\Log\Package;
 
 #[Package('framework')]
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 class SearchRanking
 {
-    final public const ASSOCIATION_SEARCH_RANKING = 0.25;
-    final public const MIDDLE_SEARCH_RANKING = 250;
-    final public const LOW_SEARCH_RANKING = 80;
-    final public const HIGH_SEARCH_RANKING = 500;
+    final public const ASSOCIATION_SEARCH_RANKING = SearchRankingFlag::ASSOCIATION_SEARCH_RANKING;
+    final public const MIDDLE_SEARCH_RANKING = SearchRankingFlag::MIDDLE_SEARCH_RANKING;
+    final public const LOW_SEARCH_RANKING = SearchRankingFlag::LOW_SEARCH_RANKING;
+    final public const HIGH_SEARCH_RANKING = SearchRankingFlag::HIGH_SEARCH_RANKING;
 
     public function __construct(
         public float $ranking,

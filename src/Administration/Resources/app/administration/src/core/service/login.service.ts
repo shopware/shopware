b6ec309c198f41c13395b60a25103e2439d0a09f
David Traum <d.traum@shopware.com>
2026-01-22 10:02:56 +0100

    fix: reset retry count

--- src/Administration/Resources/app/administration/src/core/service/login.service.ts
+++ src/Administration/Resources/app/administration/src/core/service/login.service.ts
@@ -180,6 +180,8 @@ export default function createLoginService(
                     refresh: response.data.refresh_token,
                 });
 
+                refreshRetryCount = 0
+
                 return response.data.access_token;
             })
             .catch((error) => {

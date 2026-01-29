d8bcc21fd872509553eff8dbef72c6e920b16d49
lxregistry <32699348+lxregistry@users.noreply.github.com>
2026-01-29 19:18:20 +0200

    Change constants to use 'string' type in OrderDeliveryStates
    add state STATE_DELIVERED

--- src/Core/Checkout/Order/Aggregate/OrderDelivery/OrderDeliveryStates.php
+++ src/Core/Checkout/Order/Aggregate/OrderDelivery/OrderDeliveryStates.php
@@ -7,11 +7,13 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('checkout')]
 final class OrderDeliveryStates
 {
-    public const STATE_MACHINE = 'order_delivery.state';
-    public const STATE_OPEN = 'open';
-    public const STATE_PARTIALLY_SHIPPED = 'shipped_partially';
-    public const STATE_SHIPPED = 'shipped';
-    public const STATE_RETURNED = 'returned';
-    public const STATE_PARTIALLY_RETURNED = 'returned_partially';
-    public const STATE_CANCELLED = 'cancelled';
+    public const string STATE_MACHINE = 'order_delivery.state';
+    public const string STATE_OPEN = 'open';
+    public const string STATE_PARTIALLY_SHIPPED = 'shipped_partially';
+    public const string STATE_SHIPPED = 'shipped';
+    public const string STATE_PARTIALLY_DELIVERED = 'delivered_partially';
+    public const string STATE_DELIVERED = 'delivered';
+    public const string STATE_PARTIALLY_RETURNED = 'returned_partially';
+    public const string STATE_RETURNED = 'returned';
+    public const string STATE_CANCELLED = 'cancelled';
 }

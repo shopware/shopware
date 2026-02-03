3fb38c14cd7b0cc128482ae7d67cabc5b50739bb
Florian Robel <f.robel@shopware.com>
2026-02-03 12:24:58 +0100

    fix: add custom audit script for storefront audit check

--- .github/bin/js/package-discovery.ts
+++ .github/bin/js/package-discovery.ts
@@ -48,7 +48,8 @@ export const EXPECTED_PACKAGE_JSON_FILES: readonly PackageInfo[] = [
   },
   {
     path: './src/Storefront/Resources/app/storefront/package.json',
-    name: 'Storefront Main'
+    name: 'Storefront Main',
+    hasCustomAuditScript: true
   },
   {
     path: './tests/acceptance/package.json',

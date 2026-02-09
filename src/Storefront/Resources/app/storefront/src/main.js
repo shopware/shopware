428b223151d990430a5c78e7144b7117e412ba91
Tobias Berge <tobiasberge@users.noreply.github.com>
2026-02-09 09:23:08 +0100

    fix(a11y): alerts on initial page load not read by screenreader (#14575)
    (cherry picked from commit e0e7799d841ac9988d68e3b805f94b33d838fbfb)

--- src/Storefront/Resources/app/storefront/src/main.js
+++ src/Storefront/Resources/app/storefront/src/main.js
@@ -33,6 +33,7 @@ import (synchronously) plugins
  */
 import SetBrowserClassPlugin from 'src/plugin/set-browser-class/set-browser-class.plugin';
 import SpeculationRulesPlugin from 'src/plugin/speculation-rules/speculation-rules.plugin';
+import AlertAriaPlugin from 'src/plugin/alert-aria/alert-aria.plugin';
 
 window.Feature = Feature;
 window.eventEmitter = new NativeEventEmitter();
@@ -120,6 +121,7 @@ PluginManager.register('BuyBox', () => import('src/plugin/buy-box/buy-box.plugin
 PluginManager.register('BasicCaptcha', () => import('src/plugin/captcha/basic-captcha.plugin'), '[data-basic-captcha]');
 PluginManager.register('QuantitySelector', () => import('src/plugin/quantity-selector/quantity-selector.plugin'), '[data-quantity-selector]');
 PluginManager.register('AjaxModal', () => import('src/plugin/ajax-modal/ajax-modal.plugin'), '[data-ajax-modal][data-url]');
+PluginManager.register('AlertAria', AlertAriaPlugin, '[data-alert-aria]'); // Plugin not async to prevent unreliable load time for the screenreader.
 
 /**
  * @experimental stableVersion:v6.8.0 feature:SPATIAL_BASES

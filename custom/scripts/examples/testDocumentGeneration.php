acd2db40f51dac8aae7e2a6b08bb74c4c51309b9
Malte Janz <m.janz@shopware.com>
2026-02-09 17:08:55 +0100

    chore: add temporary test script

---
+++ custom/scripts/examples/testDocumentGeneration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Scripts\Examples;
+
+use Shopware\Core\Checkout\DocumentV2\DocumentFormat;
+use Shopware\Core\Checkout\DocumentV2\DocumentGenerator;
+use Shopware\Core\Checkout\DocumentV2\DocumentType;
+
+require_once __DIR__ . '/base-script.php';
+
+$env = 'prod'; // by default, kernel gets booted in dev
+
+$kernel = require __DIR__ . '/../boot/boot.php';
+
+class TestDocumentGeneration extends BaseScript
+{
+    public function run()
+    {
+        $documentGenerator = $this->getContainer()->get(DocumentGenerator::class);
+        $documentGenerator->generate(DocumentType::Invoice->value, [DocumentFormat::EmbeddedZugferd->value]);
+        //$documentGenerator->generate(DocumentType::Invoice->value, [DocumentFormat::Pdf->value]);
+    }
+}
+
+
+(new TestDocumentGeneration($kernel))->run();

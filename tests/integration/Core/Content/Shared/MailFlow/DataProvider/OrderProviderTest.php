3363898dbb3a8c7e669be86dd13b47d9f19ddcf5
Jozsef Damokos <j.damokos@shopware.com>
2026-02-11 20:00:31 +0200

    Add provider unit test

---
+++ tests/integration/Core/Content/Shared/MailFlow/DataProvider/OrderProviderTest.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Integration\Core\Content\Shared\MailFlow\DataProvider;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Order\OrderDefinition;
+use Shopware\Core\Content\Shared\MailFlow\DataProvider\OrderProvider;
+use Shopware\Core\Content\Shared\MailFlow\Event\MailFlowDataCriteriaEvent;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\Log\Package;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @internal
+ */
+#[Package('after-sales')]
+class OrderProviderTest extends TestCase
+{
+    public function testCanGetRepository(): void
+    {
+        $repository = $this->createMock(EntityRepository::class);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container
+            ->expects(static::once())
+            ->method('get')
+            ->with(OrderDefinition::ENTITY_NAME . '.repository')
+            ->willReturn($repository);
+
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+
+        $provider = new OrderProvider($eventDispatcher, $container);
+
+        $context = Context::createDefaultContext();
+
+        // Trigger repository usage via getData()
+        $repository
+            ->expects(static::once())
+            ->method('search');
+
+        $provider->getData('some-id', $context);
+    }
+
+    public function testDispatchesCriteriaEvent()
+    {
+        $repository = $this->createMock(EntityRepository::class);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container
+            ->method('get')
+            ->willReturn($repository);
+
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+
+        $eventDispatcher
+            ->expects(static::once())
+            ->method('dispatch')
+            ->with(
+                static::callback(function ($event) {
+                    return $event instanceof MailFlowDataCriteriaEvent
+                        && $event->getEntityName() === OrderDefinition::ENTITY_NAME;
+                }),
+                'mail-flow.data.order.criteria.event'
+            );
+
+        $provider = new OrderProvider($eventDispatcher, $container);
+
+        $context = Context::createDefaultContext();
+
+        $provider->getCriteria('some-id', $context);
+    }
+}

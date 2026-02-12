697a7b24153a8819a0753ff48e3c15dfbbeeae8d
Fabian Boensch <f.boensch@shopware.com>
2026-02-12 11:26:16 +0100

    fix tests

--- src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-address-selection/sw-order-address-selection.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-address-selection/sw-order-address-selection.spec.js
@@ -114,7 +114,7 @@ async function createWrapper(propsData) {
                             }),
                         create: () => ({
                             _isNew: true,
-                            getEntityName: () => 'customer_address'
+                            getEntityName: () => 'customer_address',
                         }),
                     }),
                 },

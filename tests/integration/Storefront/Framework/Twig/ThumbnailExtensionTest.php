3f3e135c248323937c19e3e5dacf9acb2a0daaf9
Philipp <p.schuch@shopware.com>
2026-02-17 09:18:44 +0000

    Fix thumbnail extension test

--- tests/integration/Storefront/Framework/Twig/ThumbnailExtensionTest.php
+++ tests/integration/Storefront/Framework/Twig/ThumbnailExtensionTest.php
@@ -20,6 +20,7 @@ use Shopware\Core\Kernel;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Core\Test\Generator;
 use Shopware\Core\Test\Stub\Framework\BundleFixture;
+use Shopware\Storefront\Framework\Twig\Components\TwigComponentCollection;
 use Shopware\Storefront\Framework\Twig\Components\TwigComponentHelper;
 use Shopware\Storefront\Framework\Twig\Extension\ConfigExtension;
 use Shopware\Storefront\Framework\Twig\Extension\UrlEncodingTwigFilter;
@@ -239,7 +240,7 @@ class ThumbnailExtensionTest extends TestCase
 
         $packages = $this->createMock(Packages::class);
         $twigComponentHelper = $this->createMock(TwigComponentHelper::class);
-        $twigComponentHelper->method('getComponents')->willReturn([]);
+        $twigComponentHelper->method('getComponents')->willReturn(new TwigComponentCollection());
 
         // Needed for the ConfigExtension, so the theme_config('breakpoint.sm') calls return the actual breakpoints.
         $templateConfigAccessor = new TemplateConfigAccessor(

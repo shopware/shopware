59cc4a3d5f5e524bd3994a55f18f50d576228428
max <max@swk-web.com>
2026-02-21 23:54:59 +0100

    fixup: Add helper method

--- src/Storefront/Controller/RegisterController.php
+++ src/Storefront/Controller/RegisterController.php
@@ -250,13 +250,7 @@ class RegisterController extends StorefrontController
     )]
     public function confirmRegistration(SalesChannelContext $context, QueryDataBag $queryDataBag): Response
     {
-        /*
-         * Because some email-clients try to fetch previews for links in mails, they send a HEAD-request.
-         * But because Symfony is routing HEAD-requests as GET-requests, a subscriber would be confirmed without
-         * clicking the link, only by the HEAD-request.
-         * Beware: $request->getMethod() or $request->getRealMethod() will both return "GET"
-         */
-        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'HEAD') {
+        if ($this->isHeadRequest()) {
             return new Response(status: Response::HTTP_NO_CONTENT);
         }
 

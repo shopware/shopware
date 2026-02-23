a2f5fd4b513fe9aead6b3f0091bdf758070ba223
Dennis Garding <d.garding@shopware.com>
2026-02-23 16:01:24 +0100

    fix test

--- tests/acceptance/tests/Forms/RevocationRequestForm.spec.ts
+++ tests/acceptance/tests/Forms/RevocationRequestForm.spec.ts
@@ -9,14 +9,14 @@ test(
         await test.step('Visit the home page to check there is no revocation button', async () => {
             await TestDataService.setSystemConfig({ 'core.basicInformation.showRevocationButton': false });
             await ShopCustomer.goesTo(StorefrontHome.url());
-            const revocationFormButton = StorefrontHome.page.getByText(/Revoke a Contract|Vertrag widerrufen/);
+            const revocationFormButton = StorefrontHome.page.getByText(/Revoke a contract|Vertrag widerrufen/);
             await expect(revocationFormButton).toBeHidden();
         });
 
         await test.step('Enables the revocation button and check if it is visible', async () => {
             await TestDataService.setSystemConfig({ 'core.basicInformation.showRevocationButton': true });
             await ShopCustomer.goesTo(StorefrontHome.url());
-            const revocationFormButton = StorefrontHome.page.getByText(/Revoke a Contract|Vertrag widerrufen/);
+            const revocationFormButton = StorefrontHome.page.getByText(/Revoke a contract|Vertrag widerrufen/);
             await expect(revocationFormButton).toBeVisible();
         });
     }

7b620cf478c1280632ee3f8352be98e95a4fb77d
Gerrit Weiermann <g.weiermann@shopware.com>
2026-02-23 13:03:17 +0100

    refactor: move loading indicator removal to a separate method for better readability

--- src/Administration/Resources/app/administration/src/app/adapter/view/vue.adapter.ts
+++ src/Administration/Resources/app/administration/src/app/adapter/view/vue.adapter.ts
@@ -75,22 +75,7 @@ export default class VueAdapter extends ViewAdapter {
         this.app = createApp({
             name: 'ShopwareAdministration',
             template: '<sw-admin />',
-            mounted() {
-                // remove loading indicator:
-
-                // `DELAY` matches animation-delay that is used in `administration/index.html`
-                const DELAY = 2000;
-                const MIN_VISIBLE_TIME = 300;
-
-                const startTime = window._pageLoadTime_;
-                const elapsedTime = Date.now() - startTime;
-                // prevent flickering, show loading indicator longer than necessary:
-                const buffer = elapsedTime < DELAY ? 0 : Math.max(DELAY + MIN_VISIBLE_TIME - elapsedTime, 0);
-
-                setTimeout(() => {
-                    document.getElementById('page-loading-screen')?.remove();
-                }, buffer);
-            },
+            mounted: () => this.removeLoadingIndicator(),
         });
     }
 
@@ -766,5 +751,25 @@ export default class VueAdapter extends ViewAdapter {
             // @ts-expect-error - extends can be a string or a component config
             this.resolveMixins(componentConfig.extends);
         }
+    }
+
+    /**
+     * Removes the loading indicator (potentially with a delay to prevent flickering)
+     *
+     * @private
+     */
+    removeLoadingIndicator() {
+        // `DELAY` matches animation-delay that is used in `administration/index.html`
+        const DELAY = 2000;
+        const MIN_VISIBLE_TIME = 300;
+
+        const startTime = window._pageLoadTime_;
+        const elapsedTime = Date.now() - startTime;
+        // prevent flickering, show loading indicator longer than necessary:
+        const buffer = elapsedTime < DELAY ? 0 : Math.max(DELAY + MIN_VISIBLE_TIME - elapsedTime, 0);
+
+        setTimeout(() => {
+            document.getElementById('page-loading-screen')?.remove();
+        }, buffer);
     }
 }

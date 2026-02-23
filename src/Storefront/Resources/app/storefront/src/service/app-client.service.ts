498e0f82a5285d958499b3e8d699a9ec6a4e515e
BrocksiNet <b.meyer@shopware.com>
2026-02-23 14:48:49 +0100

    refactor: clean up app-client.service.ts by removing unnecessary whitespace and comments
    - Removed redundant whitespace and comments in the AppClientService class to improve code readability.
    - Ensured that the logic for fetching headers remains intact while enhancing overall code clarity.

--- src/Storefront/Resources/app/storefront/src/service/app-client.service.ts
+++ src/Storefront/Resources/app/storefront/src/service/app-client.service.ts
@@ -63,23 +63,19 @@ export default class AppClientService {
             return data.headers;
         }
 
-         
         const data = JSON.parse(this.storage.getItem(key));
 
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         if (new Date(data.expires) < new Date()) {
             this.storage.removeItem(key);
 
-             
             return await this.getHeaders();
         }
 
-         
         return data.headers;
     }
 
     private async fetchHeaders(): Promise<{ headers: { [key: string]: string }, expires: string }> {
-         
         const url = window['router']['frontend.app-system.generate-token'].replace('Placeholder', encodeURIComponent(this.name));
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         const response = await fetch(url, {
@@ -106,7 +102,6 @@ export default class AppClientService {
             options.headers = {};
         }
 
-         
         options.headers = {...options.headers, ...await this.getHeaders()};
 
         return fetch(url, options);

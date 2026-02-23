14f69bb20420be81763881a91bf50f0ffbb48601
Jonas Elfering <j.elfering@shopware.com>
2026-02-23 15:52:45 +0100

    Use new ATS version

--- src/Core/Framework/Api/Controller/CacheController.php
+++ src/Core/Framework/Api/Controller/CacheController.php
@@ -31,6 +31,7 @@ class CacheController extends AbstractController
         private readonly CacheInvalidator $cacheInvalidator,
         private readonly AdapterInterface $adapter,
         private readonly EntityIndexerRegistry $indexerRegistry,
+        /** @phpstan-ignore phpat.restrictNamespacesInCore (only injected conditionally if ES bundle is installed) */
         private readonly ?IndexManager $indexManager,
     ) {
     }

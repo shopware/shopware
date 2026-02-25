bf67f2d4b2ca65724c6ab2c12185404591399ec8
Jonas Elfering <j.elfering@shopware.com>
2026-02-25 16:12:19 +0100

    fix: use correct delimiter in refresh migration command
    fixes: https://github.com/shopware/shopware/issues/14904

---
+++ tests/unit/Core/Framework/Migration/Command/_fixtures/InvalidMigration.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Core\Framework\Migration\Command\_fixtures;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class InvalidMigration extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1772030791;
+    }
+
+    public function update(Connection $connection): void
+    {
+    }
+}

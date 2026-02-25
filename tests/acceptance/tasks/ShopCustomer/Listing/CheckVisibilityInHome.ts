84f0f90ffbd9ac2eefb356d4691da8f0cbcf276a
Jonas Elfering <j.elfering@shopware.com>
2026-02-25 17:06:38 +0100

    Remove cache clear in CheckVisibility task

--- tests/acceptance/tasks/ShopCustomer/Listing/CheckVisibilityInHome.ts
+++ tests/acceptance/tasks/ShopCustomer/Listing/CheckVisibilityInHome.ts
@@ -2,19 +2,14 @@ import type { FixtureTypes, Task } from '@fixtures/AcceptanceTest';
 import { test as base } from '@playwright/test';
 
 export const CheckVisibilityInHome = base.extend<{ CheckVisibilityInHome: Task }, FixtureTypes>({
-    CheckVisibilityInHome: async ({ ShopCustomer, StorefrontHome, TestDataService }, use) => {
+    CheckVisibilityInHome: async ({ ShopCustomer, StorefrontHome }, use) => {
         const task = (productName: string) => {
             return async () => {
 
-                await TestDataService.clearCaches();
                 const productLocators = await StorefrontHome.getListingItemByProductName(productName);
 
-                await ShopCustomer.expects(async () => {
-                    await ShopCustomer.goesTo(`${StorefrontHome.url()}?a=${Date.now()}`);
-                    await ShopCustomer.expects(productLocators.productName).toBeVisible();
-                }).toPass({
-                    intervals: [1_000, 2_500], // retry after 1 seconds, then every 2.5 seconds
-                });
+                await ShopCustomer.goesTo(`${StorefrontHome.url()}?a=${Date.now()}`);
+                await ShopCustomer.expects(productLocators.productName).toBeVisible();
             }
         };
 

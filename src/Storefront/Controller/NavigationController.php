ee52a44e99a3e2ee9ef01b45f20cc2b291f36751
Philipp <p.schuch@shopware.com>
2026-02-26 10:15:38 +0100

    Remove content system from controller

--- src/Storefront/Controller/NavigationController.php
+++ src/Storefront/Controller/NavigationController.php
@@ -5,17 +5,13 @@ namespace Shopware\Storefront\Controller;
 use Shopware\Core\Content\Category\CategoryDefinition;
 use Shopware\Core\Content\Category\CategoryException;
 use Shopware\Core\Content\Category\Service\AbstractCategoryUrlGenerator;
-use Shopware\Core\Content\Media\MediaEntity;
-use Shopware\Core\Content\ContentSystem\SalesChannel\AbstractContentRoute;
 use Shopware\Core\Content\Seo\SeoUrlPlaceholderHandlerInterface;
-use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Framework\Routing\RequestTransformer;
 use Shopware\Storefront\Framework\Routing\StorefrontRouteScope;
 use Shopware\Storefront\Framework\Seo\SeoUrlRoute\NavigationPageSeoUrlRoute;
-use Shopware\Storefront\Page\Navigation\NavigationPage;
 use Shopware\Storefront\Page\Navigation\NavigationPageLoadedHook;
 use Shopware\Storefront\Page\Navigation\NavigationPageLoaderInterface;
 use Shopware\Storefront\Pagelet\Footer\FooterPageletLoadedHook;
@@ -63,31 +59,6 @@ class NavigationController extends StorefrontController
 
         $this->hook(new NavigationPageLoadedHook($page, $context));
 
-        // TODO: Remove this once the new cms structure is fully implemented.
-        if (Feature::isActive('STOREFRONT_COMPONENTS')) {
-            $elements = $page->getCmsPage()->getAllElements();
-            $listing = $elements[4];
-            $listingData = $listing->getData()->getListing();
-
-            $cmsPage = $this->getNewCmsStructure($listingData);
-
-            return $this->renderStorefront(
-                '@Storefront/storefront/page/content/index.html.twig',
-                ['page' => $page, 'cmsPage' => $cmsPage, 'isNewContentStructure' => true]);
-            $contentPage = $this->loadCategoryContentPage($page, $request, $context);
-
-            if ($contentPage !== null) {
-                return $this->renderStorefront(
-                    '@Storefront/storefront/page/content/page.html.twig',
-                    [
-                        'page' => $page,
-                        'contentPage' => $contentPage,
-                        'isNewContentStructure' => true,
-                    ]
-                );
-            }
-        }
-
         return $this->renderStorefront('@Storefront/storefront/page/content/index.html.twig', ['page' => $page]);
     }
 
@@ -118,45 +89,7 @@ class NavigationController extends StorefrontController
             return new RedirectResponse($this->seoUrlReplacer->replace($urlPlaceholder, $host, $context));
         }
 
-        if (Feature::isActive('STOREFRONT_COMPONENTS')) {
-            $contentPage = $this->loadCategoryContentPage($page, $request, $context);
-
-            if ($contentPage !== null) {
-                return $this->renderStorefront(
-                    '@Storefront/storefront/page/content/page.html.twig',
-                    [
-                        'page' => $page,
-                        'contentPage' => $contentPage,
-                        'isNewContentStructure' => true,
-                    ]
-                );
-            }
-        }
-
         return $this->renderStorefront('@Storefront/storefront/page/content/index.html.twig', ['page' => $page]);
-    }
-
-    #[Route(
-        path: '/content/{path}',
-        name: 'frontend.content.layout',
-        options: ['seo' => false],
-        defaults: ['_httpCache' => true],
-        requirements: ['path' => '.+'],
-        methods: ['GET'],
-    )]
-    public function content(string $path, Request $request, SalesChannelContext $context): Response
-    {
-        $page = $this->navigationPageLoader->load($request, $context);
-        $contentPage = $this->loadContentPage($path, $request, $context);
-
-        return $this->renderStorefront(
-            '@Storefront/storefront/page/content/raw.html.twig',
-            [
-                'page' => $page,
-                'contentPage' => $contentPage,
-                'isNewContentStructure' => true,
-            ]
-        );
     }
 
     #[Route(
@@ -202,14 +135,6 @@ class NavigationController extends StorefrontController
 
         $headerParameters = $request->get('headerParameters') ?? [];
 
-        if (array_key_exists('isNewContentStructure', $headerParameters)) {
-            return $this->renderStorefront('@Storefront/storefront/page/content/header.html.twig', [
-                'header' => $header,
-                'headerParameters' => $headerParameters,
-                'isNewContentStructure' => true
-            ]);
-        }
-
         return $this->renderStorefront('@Storefront/storefront/layout/header.html.twig', [
             'header' => $header,
             'headerParameters' => $headerParameters,
@@ -236,148 +161,5 @@ class NavigationController extends StorefrontController
             'footer' => $footer,
             'footerParameters' => $request->query->all()['footerParameters'] ?? [],
         ]);
-    }
-
-    /**
-     * TODO: Remove after final CMS structure is implemented.
-     */
-    private static function getNewCmsStructure($listingData) {
-
-        $media = new MediaEntity();
-        $media->setId('123');
-        $media->setMimeType('image/webp');
-        $media->setFileExtension('webp');
-        $media->setFileSize(1203165);
-        $media->setFileName('inspire-connect-riseup-mood.webp');
-        $media->setTitle('Test');
-        $media->setAlt('Test');
-        $media->setUrl('https://www.shopware.com/media/pages/products/shopping-experiences/inspire-connect-riseup-mood.webp');
-
-        return [
-            'id' => '123',
-            'name' => 'Home',
-            'elements' => [
-                [
-                    'id' => '123',
-                    'component' => 'Sw:Grid:Container',
-                    'properties' => [
-                        'columns' => '2',
-                        'columnsLg' => '1',
-                        'gap' => 60,
-                        'align' => 'start',
-                        'alignContent' => 'start',
-                        'justify' => 'stretch',
-                        'justifyContent' => 'stretch',
-                    ],
-                    'slots' => [
-                        'column-1' => [
-                            [
-                                'id' => '123',
-                                'component' => 'Sw:Grid:Column',
-                                'properties' => [
-                                    'start' => null,
-                                    'span' => null,
-                                ],
-                                'slots' => [
-                                    'content' => [
-                                        [
-                                            'id' => '123',
-                                            'component' => 'Sw:Media:Image',
-                                            'properties' => [
-                                                'media' => $media,
-                                            ],
-                                        ]
-                                    ],
-                                ]
-                            ],
-                        ],
-                        'column-2' => [
-                            [
-                                'id' => 'ABC',
-                                'component' => 'Sw:Grid:Column',
-                                'properties' => [
-                                    'start' => null,
-                                    'span' => null,
-                                ],
-                                'slots' => [
-                                    'default' => [
-                                        [
-                                            'id' => '123',
-                                            'component' => 'Sw:Content:Text',
-                                            'properties' => [
-                                                'text' => "<h1>Discover Your Summer Style</h1><p>Step into the sunshine with our Summer Fashion Collection! From breezy dresses and linen shirts to stylish sandals and bold accessories — everything you need to stay cool and look effortlessly chic all season long. Embrace vibrant colors, light fabrics, and timeless designs perfect for beach days, city strolls, and evening get-togethers. Your next favorite outfit is waiting — explore now and make this summer your most stylish one yet.</p><p>Each piece in our collection is carefully selected to combine comfort and elegance, using high-quality materials that feel as good as they look. Whether you're planning a weekend getaway or updating your everyday wardrobe, our summer essentials will keep you glowing with confidence and ready for every adventure under the sun.</p>",
-                                            ],
-                                        ],
-                                        [
-                                            'id' => '123',
-                                            'component' => 'Sw:Alert',
-                                            'properties' => [
-                                                'text' => 'Hello World',
-                                            ],
-                                            'slots' => [
-                                                'content' => [
-                                                    [
-                                                        'id' => '123',
-                                                        'component' => 'Sw:Content:Text',
-                                                        'properties' => [
-                                                            'text' => 'Hello World',
-                                                            'inline' => true,
-                                                        ],
-                                                    ]
-                                                ]
-                                            ]
-                                        ]
-                                    ],
-                                ]
-                            ]
-                        ]
-                    ],
-                ],
-                [
-                    'id' => '123',
-                    'component' => 'Sw:Product:Listing',
-                    'properties' => [
-                        'listing' => $listingData
-                    ],
-                    'slots' => [
-                        'filters-panel' => [
-                            [
-                                'id' => '123',
-                                'component' => 'Sw:Filter:Panel',
-                                'properties' => [
-                                    'totalCount' => $listingData->getTotal(),
-                                    'currentSorting' => $listingData->getSorting(),
-                                    'filterAggregations' => $listingData->getAggregations(),
-                                    'availableSortings' => $listingData->getAvailableSortings(),
-                                ],
-                                'slots' => []
-                            ]
-                        ],
-                        'product-card' => []
-                    ]
-                ]
-            ]
-        ];
-    private function loadCategoryContentPage($page, Request $request, SalesChannelContext $context): ?ContentPage
-    {
-        $category = $page->getCategory();
-        \assert($category !== null);
-
-        $categoryId = $category->getId();
-        $path = '/category/' . $categoryId;
-
-        return $this->loadContentPage($path, $request, $context);
-    }
-
-    private function loadContentPage(string $path, Request $request, SalesChannelContext $context): ?ContentPage
-    {
-        try {
-            $contentPageResponse = $this->contentRoute->load($path, $request, $context);
-            $contentPage = $contentPageResponse->getContentPage();
-        } catch (\Exception $e) {
-            $contentPage = null;
-        }
-
-        return $contentPage;
     }
 }

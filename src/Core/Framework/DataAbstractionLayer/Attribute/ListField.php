1e1e45c3697356a9cd9a980fb3be6626d1ff03ab
Oliver Skroblin <oliver@goblin-coders.de>
2026-03-04 12:10:31 +0100

    feat: added password, list and email field types

---
+++ src/Core/Framework/DataAbstractionLayer/Attribute/ListField.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DataAbstractionLayer\Attribute;
+
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('framework')]
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
+final class ListField extends Field
+{
+    public const TYPE = 'list';
+
+    /**
+     * @param class-string|null $fieldType
+     */
+    public function __construct(
+        public ?string $fieldType = null,
+        public bool|array $api = false,
+        public bool $translated = false,
+        public ?string $column = null,
+    ) {
+        parent::__construct(type: self::TYPE, translated: $translated, api: $api, column: $column);
+    }
+}

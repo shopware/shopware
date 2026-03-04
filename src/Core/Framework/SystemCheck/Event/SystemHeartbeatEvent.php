cf49382bdfc57b1ada0a8b9011a5e08ba6ddac91
Ghaith Olabi <m.olabi@shopware.com>
2026-03-04 09:36:56 +0100

    fix circular reference

--- src/Core/Framework/SystemCheck/Event/SystemHeartbeatEvent.php
+++ src/Core/Framework/SystemCheck/Event/SystemHeartbeatEvent.php
@@ -3,25 +3,17 @@
 namespace Shopware\Core\Framework\SystemCheck\Event;
 
 use Shopware\Core\Framework\App\AppEntity;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\Event\ShopwareEvent;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Webhook\AclPrivilegeCollection;
 use Shopware\Core\Framework\Webhook\Hookable;
-use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * @internal
  */
 #[Package('framework')]
-class SystemHeartbeatEvent extends Event implements ShopwareEvent, Hookable
+class SystemHeartbeatEvent implements Hookable
 {
     final public const NAME = 'system.health.heartbeat';
-
-    public function getContext(): Context
-    {
-        return $this->getContext();
-    }
 
     public function getName(): string
     {

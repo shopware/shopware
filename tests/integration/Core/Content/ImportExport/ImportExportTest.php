faa5f39f72fa7b55b01d1c7fdcd06b97788c9255
Soner Sayakci <s.sayakci@gmail.com>
2026-03-09 08:05:06 +0100

    fix: remove unnecessary profiler toggling in tests
    TraceableEventDispatcher tracks called listeners via its internal
    callStack independently of the Symfony profiler. The profiler
    enable/disable was unnecessary and caused PHPStan errors since
    Container::get('profiler') returns untyped object.
    
    Also removes PermissionsServiceTest integration test which is
    already covered by the unit test.

--- tests/integration/Core/Content/ImportExport/ImportExportTest.php
+++ tests/integration/Core/Content/ImportExport/ImportExportTest.php
@@ -137,11 +137,6 @@ class ImportExportTest extends AbstractImportExportTestCase
 
     public function testExportEvents(): void
     {
-        $profilerNeedsToBeDisabledAgain = !self::getContainer()->get('profiler')->isEnabled();
-        if ($profilerNeedsToBeDisabledAgain) {
-            self::getContainer()->get('profiler')->enable();
-        }
-
         $this->listener->addSubscriber(new StockSubscriber());
 
         $productId = Uuid::randomHex();
@@ -158,19 +153,10 @@ class ImportExportTest extends AbstractImportExportTestCase
 
         $csv = $this->getCsvContent($progress->getLogId());
         static::assertStringContainsString(\sprintf(';%s;', $newStock), $csv);
-
-        if ($profilerNeedsToBeDisabledAgain) {
-            self::getContainer()->get('profiler')->disable();
-        }
     }
 
     public function testImportEvents(): void
     {
-        $profilerNeedsToBeDisabledAgain = !self::getContainer()->get('profiler')->isEnabled();
-        if ($profilerNeedsToBeDisabledAgain) {
-            self::getContainer()->get('profiler')->enable();
-        }
-
         $this->listener->addSubscriber(new TestSubscriber());
         $this->importCategoryCsv();
         $events = array_column($this->listener->getCalledListeners(), 'event');
@@ -178,10 +164,6 @@ class ImportExportTest extends AbstractImportExportTestCase
         static::assertContains(ImportExportBeforeImportRecordEvent::class, $events);
         static::assertContains(ImportExportAfterImportRecordEvent::class, $events);
         static::assertNotContains(ImportExportExceptionImportRecordEvent::class, $events);
-
-        if ($profilerNeedsToBeDisabledAgain) {
-            self::getContainer()->get('profiler')->disable();
-        }
     }
 
     public function testImportExport(): void

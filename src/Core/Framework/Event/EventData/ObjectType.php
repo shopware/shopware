120f9d12c1cf16f1bc05f8190d21e2ca7f1639b5
Michael Telgmann <mitelg@users.noreply.github.com>
2026-03-10 09:56:50 +0100

    fix: Missing undefined array key checks in HookableBusinessEvent (#15420)

--- src/Core/Framework/Event/EventData/ObjectType.php
+++ src/Core/Framework/Event/EventData/ObjectType.php
@@ -4,7 +4,7 @@ namespace Shopware\Core\Framework\Event\EventData;
 
 use Shopware\Core\Framework\Log\Package;
 
-#[Package('fundamentals@after-sales')]
+#[Package('framework')]
 class ObjectType implements EventDataType
 {
     final public const TYPE = 'object';

097f987b101893177e0d24f01f62c137c4290adb
David Traum <d.traum@shopware.com>
2026-03-10 11:02:23 +0100

    chore: remove blank line

--- src/Administration/Resources/app/administration/src/core/service/login.service.ts
+++ src/Administration/Resources/app/administration/src/core/service/login.service.ts
@@ -187,7 +187,6 @@ export default function createLoginService(
             const currentAccessToken = getToken();
             if (currentAccessToken && currentAccessToken !== accessTokenBeforeLock) {
                 notifyRefreshSubscribers(currentAccessToken);
-
                 return currentAccessToken;
             }
 

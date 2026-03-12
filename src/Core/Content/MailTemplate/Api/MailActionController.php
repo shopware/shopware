9247381931f75e6d3ef2b2aba824011086076c23
Simon Fiebranz <s.fiebranz@shopware.com>
2026-03-12 08:50:36 +0100

    fix route method

--- src/Core/Content/MailTemplate/Api/MailActionController.php
+++ src/Core/Content/MailTemplate/Api/MailActionController.php
@@ -157,7 +157,7 @@ class MailActionController extends AbstractController
     #[Route(
         path: '/api/_action/mail-template/available-variables',
         name: 'api.action.mail_template.available_variables',
-        methods: [Request::METHOD_GET]
+        methods: [Request::METHOD_POST]
     )]
     public function availableVariables(RequestDataBag $post, Context $context): JsonResponse
     {

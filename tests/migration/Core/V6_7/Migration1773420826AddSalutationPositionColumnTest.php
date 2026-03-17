a16f61bfed1e0f5ecbf7e7b6af36005e5bc9d7de
Daniel Geike <daniel.geike@8mylez.com>
2026-03-17 22:28:22 +0100

    chore: add migration unit test

---
+++ tests/migration/Core/V6_7/Migration1773420826AddSalutationPositionColumnTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\Migration\Core\V6_7;
+
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
+use Shopware\Core\Migration\V6_7\Migration1773420826AddSalutationPositionColumn;
+
+/**
+ * @internal
+ */
+#[Package('framework')]
+#[CoversClass(Migration1773420826AddSalutationPositionColumn::class)]
+class Migration1773420826AddSalutationPositionColumnTest extends TestCase
+{
+    use KernelTestBehaviour;
+
+    public function testMigrationAddsColumnAndAssignsPositions(): void
+    {
+        $connection = self::getContainer()->get(Connection::class);
+
+        $this->dropPositionColumnIfExists($connection);
+
+        $migration = new Migration1773420826AddSalutationPositionColumn();
+        $migration->update($connection);
+        $migration->update($connection);
+
+        $columnDefinition = $connection->fetchAssociative('SHOW COLUMNS FROM `salutation` LIKE "position"');
+        static::assertNotFalse($columnDefinition);
+        static::assertSame('NO', $columnDefinition['Null']);
+
+        $positions = $connection->fetchAllKeyValue('SELECT salutation_key, position FROM salutation');
+
+        static::assertSame('1', $positions['not_specified']);
+        static::assertSame('2', $positions['mrs']);
+        static::assertSame('3', $positions['mr']);
+
+        foreach ($positions as $pos) {
+            static::assertSame(1, preg_match('/^\d+$/', (string) $pos));
+        }
+    }
+
+    private function dropPositionColumnIfExists(Connection $connection): void
+    {
+        try {
+            $connection->executeStatement('ALTER TABLE `salutation` DROP COLUMN `position`');
+        } catch (\Throwable $e) {
+            // column didn't exist – ignore
+        }
+    }
+}

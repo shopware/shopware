c9d85f4eb9788375103f780cda57b5d235ba2330
Martin Bens <m.bens@shopware.com>
2026-03-18 20:43:34 +0100

    test(content-system): cover multi-source lookup in data loader type map

--- tests/unit/Core/Framework/ContentSystem/Schema/ContentSystemDataLoaderTypeMapTest.php
+++ tests/unit/Core/Framework/ContentSystem/Schema/ContentSystemDataLoaderTypeMapTest.php
@@ -20,7 +20,7 @@ class ContentSystemDataLoaderTypeMapTest extends TestCase
     /**
      * @param list<string> $expected
      */
-    #[DataProvider('getSourcesForClassProvider')]
+    #[DataProvider('returnsSourcesForClassProvider')]
     #[TestDox('returns matching source identifiers for $_dataName')]
     public function testGetSourcesForClass(string $className, array $expected): void
     {
@@ -35,10 +35,22 @@ class ContentSystemDataLoaderTypeMapTest extends TestCase
     /**
      * @return iterable<string, array{string, list<string>}>
      */
-    public static function getSourcesForClassProvider(): iterable
+    public static function returnsSourcesForClassProvider(): iterable
     {
         yield 'product entity' => [ProductEntity::class, ['entity']];
         yield 'tree' => [Tree::class, ['navigation']];
         yield 'unknown class' => ['NonExistent', []];
+    }
+
+    #[TestDox('returns all source identifiers when class appears in multiple sources')]
+    public function testGetSourcesForClassWithMultipleSources(): void
+    {
+        $map = new ContentSystemDataLoaderTypeMap([
+            'listing' => [new ContentSystemDataLoaderTypeDescriptor(ProductEntity::class)],
+            'search' => [new ContentSystemDataLoaderTypeDescriptor(ProductEntity::class)],
+            'suggest' => [new ContentSystemDataLoaderTypeDescriptor(ProductEntity::class)],
+        ]);
+
+        static::assertSame(['listing', 'search', 'suggest'], $map->getSourcesFor(ProductEntity::class));
     }
 }

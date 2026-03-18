638154e4121fcd25d14e9f4a2039c7b150cdf9a0
Dennis Garding <d.garding@shopware.com>
2026-03-18 15:56:37 +0100

    Add test for '{{ salesChannel.name }}' occurrences in mail templates

--- tests/migration/SalesChannelTranslatedNameTest.php
+++ tests/migration/SalesChannelTranslatedNameTest.php
@@ -14,11 +14,36 @@ class SalesChannelTranslatedNameTest extends TestCase
         $mailTemplateCollectionFileContent = $fileSystem->readFile(
             __DIR__ . '/../../src/Core/Migration/Fixtures/MailTemplateContent.php'
         );
+
         static::assertStringNotContainsString(
             '{{ salesChannel.name }}',
             $mailTemplateCollectionFileContent,
             'Do not use the twig tag "{{ salesChannel.name }}" for mail templates and it translations. Use "{{ salesChannel.translated.name }}" instead.'
         );
+    }
+
+    public function testMailTemplateFiles(): void
+    {
+        $baseDirectory = realpath(__DIR__ . '/../../src/Core/Migration/Fixtures/mails');
+        $finder = new Finder();
+
+        $result = [];
+        $files = $finder->files()
+            ->depth('1')
+            ->in($baseDirectory)
+            ->contains('/^.*(\{\{ salesChannel.name \}\})+.*$/m')
+            ->sortByCaseInsensitiveName();
+
+        foreach ($files as $file) {
+            $result[] = $file->getRealPath();
+        }
+
+        $message = 'Do not use the twig tag "{{ salesChannel.name }}" for mail templates and it translations. Use "{{ salesChannel.translated.name }}" instead.';
+        foreach ($result as $file) {
+            $message .= \PHP_EOL . $file;
+        }
+
+        static::assertCount(0, $result, $message);
     }
 
     public function testMigrationFiles(): void
@@ -29,17 +54,6 @@ class SalesChannelTranslatedNameTest extends TestCase
         $baseDirectory = realpath(__DIR__ . '/../../src/Core/Migration');
         static::assertDirectoryExists($baseDirectory);
 
-        $migrationDirectories = [
-            $baseDirectory . '/V6_3',
-            $baseDirectory . '/V6_4',
-            $baseDirectory . '/V6_5',
-            $baseDirectory . '/V6_6',
-            $baseDirectory . '/V6_7',
-            $baseDirectory . '/V6_8',
-            $baseDirectory . '/V6_9',
-            $baseDirectory . '/V6_10',
-        ];
-
         $result = \json_decode(
             $fileSystem->readFile(__DIR__ . '/SalesChannelTranslatedNameResult.json'),
             true,
@@ -48,25 +62,20 @@ class SalesChannelTranslatedNameTest extends TestCase
         );
 
         $fileList = [];
-        foreach ($migrationDirectories as $migrationDirectory) {
-            if (!$fileSystem->exists($migrationDirectory)) {
-                continue;
-            }
+        $files = $finder
+            ->files()
+            ->depth('1')
+            ->in($baseDirectory)
+            ->contains('/^.*(\{\{ salesChannel.name \}\})+.*$/m')
+            ->sortByCaseInsensitiveName()
+            ->getIterator();
 
-            $files = $finder
-                ->files()
-                ->in($migrationDirectory)
-                ->contains('/^.*(\{\{ salesChannel.name \}\})+.*$/m')
-                ->sortByCaseInsensitiveName()
-                ->getIterator();
-
-            foreach ($files as $file) {
-                $fileList[] = $file->getFilename();
-            }
+        foreach ($files as $file) {
+            $fileList[] = $file->getFilename();
         }
 
         $fileList = \array_values(array_unique($fileList));
-        // to ensure find the allowed 22 files
+        // to ensure find the allowed 22 files. (Old migrations)
         static::assertGreaterThan(21, \count($fileList));
 
         foreach ($fileList as $fileName) {

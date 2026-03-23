a2cfb2a772a645a42fc070460a142e0a045da363
Michael Telgmann <m.telgmann@shopware.com>
2026-03-23 23:40:41 +0100

    add test for listener

---
+++ tests/unit/Administration/Framework/Asset/AssetUploadListenerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopware\Tests\Unit\Administration\Framework\Asset;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Shopware\Administration\Framework\Asset\AssetUploadListener;
+use Shopware\Core\Framework\Plugin\Event\AssetUploadEvent;
+
+/**
+ * @internal
+ */
+#[CoversClass(AssetUploadListener::class)]
+class AssetUploadListenerTest extends TestCase
+{
+    public function testInvoke(): void
+    {
+        $listener = new AssetUploadListener();
+        $event = new AssetUploadEvent(
+            '/test/foo',
+            '/test/bar',
+            ['foo' => 'bar'],
+            ['foo' => 'bar'],
+            [
+                'administration/.vite/manifest.json',
+                'administration/.vite/entrypoints.json',
+                'test.js',
+            ],
+            []
+        );
+
+        $listener($event);
+
+        static::assertSame([
+            'test.js',
+            'administration/.vite/entrypoints.json',
+            'administration/.vite/manifest.json',
+        ], $event->filesToUpload);
+    }
+}

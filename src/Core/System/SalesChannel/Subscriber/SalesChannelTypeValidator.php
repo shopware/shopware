7f8c24772ca8628f527f1b7d5432aaade7463f19
Krzykawski <m.krzykawski@shopware.com>
2026-03-24 09:46:04 +0100

    feat: Use array_key_exists approach in SalesChannelTypeValidator

--- src/Core/System/SalesChannel/Subscriber/SalesChannelTypeValidator.php
+++ src/Core/System/SalesChannel/Subscriber/SalesChannelTypeValidator.php
@@ -18,10 +18,10 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class SalesChannelTypeValidator implements EventSubscriberInterface
 {
     private const PROTECTED_SALES_CHANNEL_TYPE_IDS = [
-        Defaults::SALES_CHANNEL_TYPE_API,
-        Defaults::SALES_CHANNEL_TYPE_STOREFRONT,
-        Defaults::SALES_CHANNEL_TYPE_PRODUCT_COMPARISON,
-        Defaults::SALES_CHANNEL_TYPE_AGENTIC_COMMERCE,
+        Defaults::SALES_CHANNEL_TYPE_API => true,
+        Defaults::SALES_CHANNEL_TYPE_STOREFRONT => true,
+        Defaults::SALES_CHANNEL_TYPE_PRODUCT_COMPARISON => true,
+        Defaults::SALES_CHANNEL_TYPE_AGENTIC_COMMERCE => true,
     ];
 
     public static function getSubscribedEvents(): array
@@ -40,7 +40,7 @@ class SalesChannelTypeValidator implements EventSubscriberInterface
 
             $id = Uuid::fromBytesToHex($command->getPrimaryKey()['id']);
 
-            if (\in_array($id, self::PROTECTED_SALES_CHANNEL_TYPE_IDS, true)) {
+            if (\array_key_exists($id, self::PROTECTED_SALES_CHANNEL_TYPE_IDS)) {
                 $event->getExceptions()->add(new DefaultSalesChannelTypeCannotBeDeleted($id));
             }
         }

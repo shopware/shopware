b22de0b893cfe47ea3a4bfce2543835c61e67b2e
Noah <70114451+zFl4wless@users.noreply.github.com>
2026-03-25 11:27:52 +0100

    fix: backport cms media custom field mapping to 6.6 #14648 (#15781)

--- src/Administration/Resources/app/administration/src/module/sw-cms/service/cms.service.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-cms/service/cms.service.spec.js
@@ -613,6 +613,14 @@ describe('module/sw-cms/service/cms.service.spec.js', () => {
                         customFieldType: 'datetime',
                     },
                 },
+                {
+                    id: '16a2beeb80f041c29390efa3432760dd',
+                    name: 'custom_media_field',
+                    type: 'text',
+                    config: {
+                        customFieldType: 'media',
+                    },
+                },
             ];
 
             const mockResponses = global.repositoryFactoryMock.responses;
@@ -815,6 +823,9 @@ describe('module/sw-cms/service/cms.service.spec.js', () => {
             await cmsService.addCustomFieldsToMappingTypes('product', mappings);
 
             expect(mappings).toEqual({
+                entity: {
+                    media: ['product.customFields.custom_media_field'],
+                },
                 string: [
                     'product.customFields.custom_text_field',
                     'product.customFields.custom_textarea_field',

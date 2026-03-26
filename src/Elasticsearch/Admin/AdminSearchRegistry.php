902adbe3b871524613a0ea7d9b1bd3c2206862ed
Michael Telgmann <mitelg@users.noreply.github.com>
2026-03-26 10:16:16 +0100

    Apply suggestion from @mitelg

--- src/Elasticsearch/Admin/AdminSearchRegistry.php
+++ src/Elasticsearch/Admin/AdminSearchRegistry.php
@@ -266,7 +266,7 @@ class AdminSearchRegistry implements EventSubscriberInterface
 
         $result = $this->client->bulk($arguments);
 
-        if (\is_array($result) && ((bool) $result['errors'] ?? false) !== false) {
+        if (\is_array($result) && ((bool) ($result['errors'] ?? false)) !== false) {
             $errors = $this->parseErrors($result);
 
             throw ElasticsearchException::indexingError($errors);

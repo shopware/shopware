4b205decf2f50b85886eddd31b82cb74a8f184c2
thuong.le <levienthuong@gmail.com>
2026-03-30 15:00:23 +0700

    chore: fix ecs lint

--- src/Elasticsearch/Resources/config/packages/shopware.php
+++ src/Elasticsearch/Resources/config/packages/shopware.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Shopware\Core\DevOps\Environment\EnvironmentHelper;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container): void {
     $esIndexingEnabled = filter_var(

cab3608c5a7cb9d68e0f9ac4a238c868731a57dd
Nicolas Fortier <n.fortier@shopware.com>
2026-03-31 23:49:28 +0200

    move unit test to relevant namespace

--- tests/unit/Core/Test/StrictIsEmptyTest.php
+++ tests/unit/Core/Test/StrictIsEmptyTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Tests\Unit\Util;
+namespace Shopware\Tests\Unit\Core\Test;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;

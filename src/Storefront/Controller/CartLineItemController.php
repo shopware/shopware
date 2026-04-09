8a83af9ade0064def6784d2558f448b347edb789
Fabian Boensch <f.boensch@shopware.com>
2026-04-09 15:02:56 +0200

    Improve cart helper form accessibility

--- src/Storefront/Controller/CartLineItemController.php
+++ src/Storefront/Controller/CartLineItemController.php
@@ -131,7 +131,9 @@ class CartLineItemController extends StorefrontController
                 $code = mb_trim((string) $request->request->get('code'));
 
                 if ($code === '') {
-                    throw RoutingException::missingRequestParameter('code');
+                    $this->addFlash(self::DANGER, $this->trans('error.VIOLATION::IS_BLANK_ERROR'));
+
+                    return $this->createActionResponse($request);
                 }
 
                 $lineItem = $this->promotionItemBuilder->buildPlaceholderItem($code);
@@ -220,10 +222,12 @@ class CartLineItemController extends StorefrontController
     public function addProductByNumber(Request $request, SalesChannelContext $context): Response
     {
         return Profiler::trace('cart::add-product-by-number', function () use ($request, $context) {
-            $number = (string) $request->request->get('number');
+            $number = mb_trim((string) $request->request->get('number'));
 
-            if (!$number) {
-                throw RoutingException::missingRequestParameter('number');
+            if ($number === '') {
+                $this->addFlash(self::DANGER, $this->trans('error.VIOLATION::IS_BLANK_ERROR'));
+
+                return $this->createActionResponse($request);
             }
 
             $criteria = new Criteria();

4437f26526a92f35772419ddd25e62e71bef3f2e
Elias Lackner <lackner.elias@gmail.com>
2026-04-13 16:21:04 +0200

    fix: property typo in SalesChannelContext

--- src/Core/System/SalesChannel/SalesChannelContext.php
+++ src/Core/System/SalesChannel/SalesChannelContext.php
@@ -34,7 +34,7 @@ class SalesChannelContext extends Struct
      */
     protected array $permissions = [];
 
-    protected bool $permisionsLocked = false;
+    protected bool $permissionsLocked = false;
 
     protected ?string $imitatingUserId = null;
 
@@ -214,7 +214,7 @@ class SalesChannelContext extends Struct
 
     public function lockPermissions(): void
     {
-        $this->permisionsLocked = true;
+        $this->permissionsLocked = true;
     }
 
     public function getToken(): string
@@ -259,7 +259,7 @@ class SalesChannelContext extends Struct
      */
     public function setPermissions(array $permissions): void
     {
-        if ($this->permisionsLocked) {
+        if ($this->permissionsLocked) {
             throw SalesChannelException::contextPermissionsLocked();
         }
 
@@ -426,7 +426,7 @@ class SalesChannelContext extends Struct
      */
     public function withPermissions(array $permissions, callable $callback): mixed
     {
-        if ($this->permisionsLocked) {
+        if ($this->permissionsLocked) {
             return $callback($this);
         }
 

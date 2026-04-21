081a8f8cea7926631d2342c71edbfb9e7cb4f311
Lars Kemper <l.kemper@shopware.com>
2026-04-21 09:34:22 +0200

    feat: add generation context

---
+++ src/Core/Checkout/DocumentV2/Generation/DocumentGenerationContext.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Checkout\DocumentV2\Generation;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * @internal
+ */
+#[Package('after-sales')]
+final readonly class DocumentGenerationContext
+{
+    /**
+     * @param list<string> $formats
+     */
+    public function __construct(
+        private string $orderId,
+        private string $orderVersionId,
+        private string $documentType,
+        private array $formats,
+        public Context $context,
+        public ?string $documentNumber = null,
+    ) {
+    }
+
+    public function getOrderId(): string
+    {
+        return $this->orderId;
+    }
+
+    public function getOrderVersionId(): string
+    {
+        return $this->orderVersionId;
+    }
+
+    public function getDocumentType(): string
+    {
+        return $this->documentType;
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getFormats(): array
+    {
+        return $this->formats;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+
+    public function getDocumentNumber(): ?string
+    {
+        return $this->documentNumber;
+    }
+}
--- src/Core/Checkout/DocumentV2/Renderer/AbstractDocumentRenderer.php
+++ src/Core/Checkout/DocumentV2/Renderer/AbstractDocumentRenderer.php
@@ -2,6 +2,10 @@
 
 namespace Shopware\Core\Checkout\DocumentV2\Renderer;
 
+use Shopware\Core\Checkout\DocumentV2\DocumentFormat;
+use Shopware\Core\Checkout\DocumentV2\Struct\RenderInput;
+use Shopware\Core\Checkout\DocumentV2\Struct\RenderResult;
+use Shopware\Core\Checkout\DocumentV2\Struct\RenderState;
 use Shopware\Core\Framework\Log\Package;
 
 /**
@@ -10,4 +14,27 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('after-sales')]
 abstract class AbstractDocumentRenderer
 {
+    /**
+     * @see DocumentType
+     */
+    abstract public function supports(string $type): bool;
+
+    /**
+     * @see DocumentFormat
+     */
+    abstract public function getFormat(): string;
+
+    /**
+     * @see DocumentFormat
+     *
+     * @return list<string>
+     */
+    public function getDependencies(): array
+    {
+        return [];
+    }
+
+    abstract public function renderToString(RenderInput $input, RenderState $state): RenderResult;
+
+    abstract public function persistToFile(RenderInput $input, RenderState $state): string;
 }

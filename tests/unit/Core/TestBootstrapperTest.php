1c39e3eaabcff68329c853d60fe96a9032f8a06c
Nicolas Fortier <164332867+nfortier-shopware@users.noreply.github.com>
2026-04-27 09:13:07 +0200

    test: TestBootStrapper has Kernel leakage (#16443)

--- tests/unit/Core/TestBootstrapperTest.php
+++ tests/unit/Core/TestBootstrapperTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Shopware\Tests\Unit\Core;
 
+use Composer\Autoload\ClassLoader;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Result;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Test\TestCaseBase\EnvTestBehaviour;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
+use Shopware\Core\Kernel;
 use Shopware\Core\TestBootstrapper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @internal
@@ -47,5 +53,64 @@ class TestBootstrapperTest extends TestCase
         $testBootstrapper->setDatabaseUrl('test');
 
         static::assertSame('test', $testBootstrapper->getDatabaseUrl());
+    }
+
+    public function testAddCallingPlugin(): void
+    {
+        $testBootstrapper = new TestBootstrapper();
+        $testBootstrapper->addCallingPlugin(__DIR__ . '/Framework/Plugin/Util/_fixture/LocallyInstalledPlugins/SwagTest/composer.json');
+
+        $activePlugins = (new \ReflectionProperty($testBootstrapper, 'activePlugins'))->getValue($testBootstrapper);
+
+        static::assertSame(['Test'], $activePlugins);
+    }
+
+    public function testBootstrapShutsDownKernelBeforeReturning(): void
+    {
+        $previousKernel = KernelLifecycleAccessor::currentKernel();
+
+        $result = static::createStub(Result::class);
+        $result->method('fetchAllAssociative')->willReturn([]);
+
+        $connection = static::createStub(Connection::class);
+        $connection->method('executeQuery')->willReturn($result);
+
+        $container = static::createStub(ContainerInterface::class);
+        $container->method('get')->willReturn($connection);
+
+        $kernel = $this->createMock(Kernel::class);
+        $kernel->method('getContainer')->willReturn($container);
+        $kernel->expects($this->once())->method('shutdown');
+
+        KernelLifecycleAccessor::setKernel($kernel);
+
+        try {
+            $bootstrapper = (new TestBootstrapper())
+                ->setClassLoader(static::createStub(ClassLoader::class))
+                ->setDatabaseUrl('mysql://irrelevant')
+                ->setLoadEnvFile(false);
+
+            $bootstrapper->bootstrap();
+
+            static::assertNull(KernelLifecycleAccessor::currentKernel(), 'bootstrap() must leave no residual kernel');
+        } finally {
+            KernelLifecycleAccessor::setKernel($previousKernel);
+        }
+    }
+}
+
+/**
+ * @internal
+ */
+class KernelLifecycleAccessor extends KernelLifecycleManager
+{
+    public static function setKernel(?Kernel $kernel): void
+    {
+        static::$kernel = $kernel;
+    }
+
+    public static function currentKernel(): ?Kernel
+    {
+        return static::$kernel;
     }
 }

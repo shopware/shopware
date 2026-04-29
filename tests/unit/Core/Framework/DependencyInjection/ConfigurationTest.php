02327933e6a2902f16f0da706743a4fe62e07ed9
tamvt <t.vo@shopware.com>
2026-04-29 11:08:02 +0700

    fix: Fix issue reordered terms can return fewer obvious matches

--- tests/unit/Core/Framework/DependencyInjection/ConfigurationTest.php
+++ tests/unit/Core/Framework/DependencyInjection/ConfigurationTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\BooleanNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\IntegerNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\VariableNodeDefinition;
 
@@ -214,5 +215,13 @@ class ConfigurationTest extends TestCase
 
         static::assertArrayHasKey('allowed_types', $nodes);
         static::assertInstanceOf(ArrayNodeDefinition::class, $nodes['allowed_types']);
+
+        static::assertArrayHasKey('search_keyword', $nodes);
+        static::assertInstanceOf(ArrayNodeDefinition::class, $nodes['search_keyword']);
+
+        $nodes = $nodes['search_keyword']->getChildNodeDefinitions();
+
+        static::assertArrayHasKey('relevant_keyword_count', $nodes);
+        static::assertInstanceOf(IntegerNodeDefinition::class, $nodes['relevant_keyword_count']);
     }
 }

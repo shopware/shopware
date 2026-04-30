3b91e19e036d50c3c465cc3f27470b360ece0a1b
Gerrit Weiermann <g.weiermann@shopware.com>
2026-04-30 17:35:48 +0200

    fix: test case

--- src/Storefront/Resources/app/administration/src/modules/sw-theme-manager/page/sw-theme-manager-detail/sw-theme-manager-detail.spec.js
+++ src/Storefront/Resources/app/administration/src/modules/sw-theme-manager/page/sw-theme-manager-detail/sw-theme-manager-detail.spec.js
@@ -314,6 +314,7 @@ describe('sw-theme-manager-detail', () => {
                 inheritanceRemove: inheritance.removeInheritance,
                 inheritanceRestore: inheritance.restoreInheritance,
                 inheritedValue: false,
+                mapInheritance: inheritance
             }),
         });
     });

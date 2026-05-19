d0d0987e84cfeb82cdb8e088062d30cc502615a3
Krzykawski <m.krzykawski@shopware.com>
2026-05-19 10:05:05 +0200

    feat: migrate existing product_export google templates to include feed_label

---
+++ src/Core/Migration/V6_7/Migration1779177686UpdateProductComparisonGoogleTemplateForFeedLabel.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_7;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @internal
+ */
+#[Package('framework')]
+class Migration1779177686UpdateProductComparisonGoogleTemplateForFeedLabel extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1779177686;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $filesystem = new Filesystem();
+
+        $fixturePath = __DIR__ . '/../Fixtures/productComparison-export-profiles/gh-9421/';
+        $templateOld = $filesystem->readFile($fixturePath . 'google_old.xml.twig');
+        $templateNew = $filesystem->readFile($fixturePath . 'google_new.xml.twig');
+
+        $connection->update(
+            'product_export',
+            [
+                'body_template' => $templateNew,
+                'updated_at' => (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+            ],
+            [
+                'body_template' => $templateOld,
+            ]
+        );
+    }
+}

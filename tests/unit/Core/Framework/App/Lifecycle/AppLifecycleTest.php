789d0e93628a95a346fd3eab4ed446dd4c8cf4ae
Aydin Hassan <a.hassan@shopware.com>
2026-05-21 17:21:58 +0200

    refactor: use app lifecycle persister for MCP

--- tests/unit/Core/Framework/App/Lifecycle/AppLifecycleTest.php
+++ tests/unit/Core/Framework/App/Lifecycle/AppLifecycleTest.php
@@ -3,7 +3,6 @@
 namespace Shopware\Tests\Unit\Core\Framework\App\Lifecycle;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Shopware\Administration\Snippet\AppAdministrationSnippetPersister;
 use Shopware\Administration\Snippet\AppLifecycleSubscriber;
@@ -17,7 +16,6 @@ use Shopware\Core\Framework\App\Event\AppInstalledEvent;
 use Shopware\Core\Framework\App\Event\AppUpdatedEvent;
 use Shopware\Core\Framework\App\Lifecycle\AppFeatureValidator;
 use Shopware\Core\Framework\App\Lifecycle\AppLifecycle;
-use Shopware\Core\Framework\App\Lifecycle\McpAppSyncer;
 use Shopware\Core\Framework\App\Lifecycle\Parameters\AppInstallParameters;
 use Shopware\Core\Framework\App\Lifecycle\Parameters\AppUpdateParameters;
 use Shopware\Core\Framework\App\Lifecycle\PermissionLifecycleService;
@@ -286,98 +284,6 @@ class AppLifecycleTest extends TestCase
         static::assertSame('test', $appRepository->upserts[0][0]['name']);
     }
 
-    #[TestDox('skips MCP syncing when MCP_SERVER feature flag is off')]
-    public function testInstallSkipsMcpAppSyncerWhenFeatureFlagIsOff(): void
-    {
-        $_SERVER['MCP_SERVER'] = false;
-
-        try {
-            /** @var StaticEntityRepository<LanguageCollection> $languageRepository */
-            $languageRepository = new StaticEntityRepository([$this->getLanguageCollection()]);
-
-            $appEntities = [
-                [],
-                [[
-                    'id' => Uuid::randomHex(),
-                    'path' => '',
-                    'configurable' => false,
-                    'allowDisable' => true,
-                ]],
-                [[
-                    'id' => Uuid::randomHex(),
-                    'name' => 'test',
-                    'path' => '',
-                    'configurable' => false,
-                    'allowDisable' => true,
-                ]],
-            ];
-
-            $manifest = Manifest::createFromXmlFile(__DIR__ . '/../_fixtures/manifest.xml');
-            $sourceResolver = $this->getSourceResolver(__DIR__ . '/../_fixtures/manifest.xml');
-            $appRepository = $this->getAppRepositoryMock($appEntities);
-
-            $mcpAppSyncer = $this->createMock(McpAppSyncer::class);
-            $mcpAppSyncer->expects($this->never())->method('sync');
-
-            $this->registerSubscriber($sourceResolver, $appEntities[2]);
-
-            $appLifecycle = $this->getAppLifecycle(
-                $appRepository,
-                $languageRepository,
-                $sourceResolver,
-                mcpAppSyncer: $mcpAppSyncer,
-            );
-            $appLifecycle->install($manifest, new AppInstallParameters(activate: false), Context::createDefaultContext());
-        } finally {
-            $_SERVER['MCP_SERVER'] = '1';
-        }
-    }
-
-    #[TestDox('delegates MCP syncing to McpAppSyncer on install')]
-    public function testInstallDelegatesToMcpAppSyncer(): void
-    {
-        /** @var StaticEntityRepository<LanguageCollection> $languageRepository */
-        $languageRepository = new StaticEntityRepository([$this->getLanguageCollection()]);
-
-        $appEntities = [
-            [],
-            [
-                [
-                    'id' => Uuid::randomHex(),
-                    'path' => '',
-                    'configurable' => false,
-                    'allowDisable' => true,
-                ],
-            ],
-            [
-                [
-                    'id' => Uuid::randomHex(),
-                    'name' => 'test',
-                    'path' => '',
-                    'configurable' => false,
-                    'allowDisable' => true,
-                ],
-            ],
-        ];
-
-        $manifest = Manifest::createFromXmlFile(__DIR__ . '/../_fixtures/manifest.xml');
-        $sourceResolver = $this->getSourceResolver(__DIR__ . '/../_fixtures/manifest.xml');
-        $appRepository = $this->getAppRepositoryMock($appEntities);
-
-        $mcpAppSyncer = $this->createMock(McpAppSyncer::class);
-        $mcpAppSyncer->expects($this->once())->method('sync');
-
-        $this->registerSubscriber($sourceResolver, $appEntities[2]);
-
-        $appLifecycle = $this->getAppLifecycle(
-            $appRepository,
-            $languageRepository,
-            $sourceResolver,
-            mcpAppSyncer: $mcpAppSyncer,
-        );
-        $appLifecycle->install($manifest, new AppInstallParameters(activate: false), Context::createDefaultContext());
-    }
-
     public function testInstallThrowsWhenRequirementsNotMet(): void
     {
         $manifest = Manifest::createFromXmlFile(__DIR__ . '/../_fixtures/manifest.xml');
@@ -489,7 +395,6 @@ class AppLifecycleTest extends TestCase
         StaticSourceResolver $appSourceResolver,
         ?DeletedAppsGateway $deletedAppsGateway = null,
         ?AppRequirementsValidator $requirementsValidator = null,
-        ?McpAppSyncer $mcpAppSyncer = null,
     ): AppLifecycle {
         /** @var StaticEntityRepository<AclRoleCollection> $aclRoleRepo */
         $aclRoleRepo = new StaticEntityRepository([new AclRoleCollection()]);
@@ -522,7 +427,6 @@ class AppLifecycleTest extends TestCase
             $this->createMock(AppFeatureValidator::class),
             $appSourceResolver,
             $this->createMock(ConfigReader::class),
-            $mcpAppSyncer ?? $this->createMock(McpAppSyncer::class),
             $deletedAppsGateway,
             $requirementsValidator ?? static::createStub(AppRequirementsValidator::class)
         );

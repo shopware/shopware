lacknere/platform

Allow locked product sorting translation

src/Administration/Resources/app/administration/src/module/sw-settings-listing/component/sw-settings-listing-option-criteria-grid/sw-settings-listing-option-criteria-grid.spec.js
@@ -44,12 +44,17 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-cr
                     },
                     stubs: {
                         'sw-card': {
-                            template: '<div><slot></slot></div>',
+                            template: `
+                                <div class="sw-card">
+                                    <slot name="toolbar"></slot>
+                                    <slot></slot>
+                                </div>
+                            `,
                         },
                         'sw-empty-state': {
                             template: '<div class="sw-empty-state"></div>',
                         },
-                        'sw-data-grid': await wrapTestComponent('sw-data-grid'),
+                        'sw-data-grid': await wrapTestComponent('sw-data-grid', { sync: true }),
                         'sw-checkbox-field': await wrapTestComponent('sw-checkbox-field'),
                         'sw-checkbox-field-deprecated': await wrapTestComponent('sw-checkbox-field-deprecated', {
                             sync: true,
@@ -269,5 +274,33 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-cr
                 priority: 1,
             },
         ]);
+    });
+
+    it('should disable fields on locked product sorting', async () => {
+        await wrapper.setProps({
+            productSortingEntity: {
+                label: 'Top result',
+                key: 'score',
+                fields: [
+                    {
+                        field: '_score',
+                        naturalSorting: 0,
+                        order: 'desc',
+                        priority: 1,
+                    },
+                ],
+                locked: true,
+            },
+        });
+
+        const criteriaSelect = wrapper.find('.sw-settings-listing-option-criteria-grid__criteria-select');
+        const isDisabled = criteriaSelect.attributes('disabled');
+
+        expect(isDisabled).toBeTruthy();
+
+        const dataGrid = wrapper.findComponent('.sw-settings-listing-option-criteria-grid__data-grid');
+
+        expect(dataGrid.vm.showActions).toBeFalsy();
+        expect(dataGrid.vm.allowInlineEdit).toBeFalsy();
     });
 });

src/Administration/Resources/app/administration/src/module/sw-settings-listing/component/sw-settings-listing-option-general-info/sw-settings-listing-option-general-info.spec.js
@@ -66,7 +66,7 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     });
 
     it('should display the correct name', async () => {
-        const textField = wrapper.find('.sw-settings-listing-edit__general-input input');
+        const textField = wrapper.find('.sw-settings-listing-option-general-info__field-name input');
 
         expect(textField.element.value).toBe('Price descending');
     });
@@ -74,7 +74,7 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     it('should display name error', async () => {
         await wrapper.setProps({ labelError: {} });
 
-        expect(wrapper.find('.sw-settings-listing-edit__general-input .sw-field__error').exists()).toBe(true);
+        expect(wrapper.find('.sw-settings-listing-option-general-info__field-name .sw-field__error').exists()).toBe(true);
     });
 
     it('should display the correct technical name', async () => {
@@ -92,14 +92,14 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     });
 
     it('should display the correct active state', async () => {
-        const switchField = wrapper.find('.sw-field--switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isActive = switchField.element.value;
 
         expect(isActive).toBe('on');
     });
 
     it('should not disable active state switch on normal product sortings', async () => {
-        const switchField = wrapper.find('.sw-field--switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isDisabled = switchField.attributes('disabled');
 
         expect(isDisabled).toBeUndefined();
@@ -108,9 +108,38 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     it('should disable active state switch on default sortings', async () => {
         await wrapper.setProps({ isDefaultSorting: true });
 
-        const switchField = wrapper.find('.sw-field--switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isDisabled = switchField.attributes('disabled');
 
         expect(isDisabled).toBeDefined();
+    });
+
+    it('should disable untranslated fields on locked product sorting', async () => {
+        await wrapper.setProps({
+            sortingOption: {
+                label: 'Top result',
+                key: 'score',
+                fields: [
+                    {
+                        field: '_score',
+                        naturalSorting: 0,
+                        order: 'desc',
+                        priority: 1,
+                    },
+                ],
+                locked: true,
+            },
+        });
+
+        const untranslatedFields = [
+            '.sw-settings-listing-option-general-info__field-technical-name',
+            '.sw-settings-listing-option-general-info__field-active',
+        ];
+
+        untranslatedFields.forEach((field) => {
+            const isDisabled = wrapper.findComponent(field).vm.$attrs.disabled;
+
+            expect(isDisabled).toBe(true);
+        });
     });
 });

shopware/shopware

src/Administration/Resources/app/administration/src/module/sw-settings-listing/component/sw-settings-listing-option-criteria-grid/sw-settings-listing-option-criteria-grid.spec.js
@@ -44,12 +44,17 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-cr
                     },
                     stubs: {
                         'mt-card': {
-                            template: '<div><slot></slot></div>',
+                            template: `
+                                <div class="mt-card">
+                                    <slot name="toolbar"></slot>
+                                    <slot></slot>
+                                </div>
+                            `,
                         },
                         'sw-empty-state': {
                             template: '<div class="sw-empty-state"></div>',
                         },
-                        'sw-data-grid': await wrapTestComponent('sw-data-grid'),
+                        'sw-data-grid': await wrapTestComponent('sw-data-grid', { sync: true }),
                         'sw-checkbox-field': await wrapTestComponent('sw-checkbox-field'),
                         'sw-checkbox-field-deprecated': await wrapTestComponent('sw-checkbox-field-deprecated', {
                             sync: true,
@@ -262,5 +267,33 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-cr
                 priority: 1,
             },
         ]);
+    });
+
+    it('should disable fields on locked product sorting', async () => {
+        await wrapper.setProps({
+            productSortingEntity: {
+                label: 'Top result',
+                key: 'score',
+                fields: [
+                    {
+                        field: '_score',
+                        naturalSorting: 0,
+                        order: 'desc',
+                        priority: 1,
+                    },
+                ],
+                locked: true,
+            },
+        });
+
+        const criteriaSelect = wrapper.find('.sw-settings-listing-option-criteria-grid__criteria-select');
+        const isDisabled = criteriaSelect.attributes('disabled');
+
+        expect(isDisabled).toBeTruthy();
+
+        const dataGrid = wrapper.findComponent('.sw-settings-listing-option-criteria-grid__data-grid');
+
+        expect(dataGrid.vm.showActions).toBeFalsy();
+        expect(dataGrid.vm.allowInlineEdit).toBeFalsy();
     });
 });

src/Administration/Resources/app/administration/src/module/sw-settings-listing/component/sw-settings-listing-option-general-info/sw-settings-listing-option-general-info.spec.js
@@ -64,7 +64,7 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     });
 
     it('should display the correct name', async () => {
-        const textField = wrapper.find('.sw-settings-listing-edit__general-input input');
+        const textField = wrapper.find('.sw-settings-listing-option-general-info__field-name input');
 
         expect(textField.element.value).toBe('Price descending');
     });
@@ -72,7 +72,7 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     it('should display name error', async () => {
         await wrapper.setProps({ labelError: {} });
 
-        expect(wrapper.find('.sw-settings-listing-edit__general-input .mt-field__error').exists()).toBe(true);
+        expect(wrapper.find('.sw-settings-listing-option-general-info__field-name .mt-field__error').exists()).toBe(true);
     });
 
     it('should display the correct technical name', async () => {
@@ -90,14 +90,14 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     });
 
     it('should display the correct active state', async () => {
-        const switchField = wrapper.find('.mt-switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isActive = switchField.element.value;
 
         expect(isActive).toBe('on');
     });
 
     it('should not disable active state switch on normal product sortings', async () => {
-        const switchField = wrapper.find('.mt-switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isDisabled = switchField.attributes('disabled');
 
         expect(isDisabled).toBeUndefined();
@@ -106,9 +106,38 @@ describe('src/module/sw-settings-listing/component/sw-settings-listing-option-ge
     it('should disable active state switch on default sortings', async () => {
         await wrapper.setProps({ isDefaultSorting: true });
 
-        const switchField = wrapper.find('.mt-switch input');
+        const switchField = wrapper.find('.sw-settings-listing-option-general-info__field-active input');
         const isDisabled = switchField.attributes('disabled');
 
         expect(isDisabled).toBeDefined();
+    });
+
+    it('should disable untranslated fields on locked product sorting', async () => {
+        await wrapper.setProps({
+            sortingOption: {
+                label: 'Top result',
+                key: 'score',
+                fields: [
+                    {
+                        field: '_score',
+                        naturalSorting: 0,
+                        order: 'desc',
+                        priority: 1,
+                    },
+                ],
+                locked: true,
+            },
+        });
+
+        const untranslatedFields = [
+            '.sw-settings-listing-option-general-info__field-technical-name',
+            '.sw-settings-listing-option-general-info__field-active',
+        ];
+
+        untranslatedFields.forEach((field) => {
+            const isDisabled = wrapper.findComponent(field).vm.$attrs.disabled;
+
+            expect(isDisabled).toBe(true);
+        });
     });
 });
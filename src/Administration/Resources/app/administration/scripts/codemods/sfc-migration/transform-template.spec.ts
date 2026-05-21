b58d26ccf20692218bb36550059a488f1516265d
Gerrit Weiermann <g.weiermann@shopware.com>
2026-05-21 13:54:42 +0200

    test: add tests to prevent silent ignores if ast shape is unexpected

--- src/Administration/Resources/app/administration/scripts/codemods/sfc-migration/transform-template.spec.ts
+++ src/Administration/Resources/app/administration/scripts/codemods/sfc-migration/transform-template.spec.ts
@@ -190,4 +190,14 @@ describe('scripts/codemods/sfc-migration/transform-template', () => {
         `),
         ).toThrow('Twig extends is not supported by the SFC migration codemod.');
     });
+
+    it('throws when parent() is used without a migrated twig block instead of leaving a Vue method call', () => {
+        expect(() =>
+            transformTemplate(`
+<div class="sw-foo">
+    {{ parent() }}
+</div>
+        `),
+        ).toThrow('Twig template is not supported by the SFC migration codemod.');
+    });
 });

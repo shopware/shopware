d395e50176312533ea5c797b042e2a694ecd3428
Jonas Elfering <j.elfering@shopware.com>
2026-05-29 09:48:52 +0200

    test: use coverage ignore see annotation format

--- src/Core/Framework/Routing/ApiRequestContextResolver.php
+++ src/Core/Framework/Routing/ApiRequestContextResolver.php
@@ -17,10 +17,13 @@ use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\PlatformRequest;
+use Shopware\Tests\Integration\Core\Framework\Routing\ApiRequestContextResolverTest;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @codeCoverageIgnore Integration tested with \Shopware\Tests\Integration\Core\Framework\Routing\ApiRequestContextResolverTest
+ * @codeCoverageIgnore
+ *
+ * @see ApiRequestContextResolverTest
  */
 #[Package('framework')]
 class ApiRequestContextResolver implements RequestContextResolverInterface

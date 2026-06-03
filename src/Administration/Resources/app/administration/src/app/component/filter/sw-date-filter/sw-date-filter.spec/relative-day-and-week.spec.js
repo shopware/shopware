a58e2e4f44994d87e24cfc60cffe987cfa3a496f
Gerrit Weiermann <g.weiermann@shopware.com>
2026-06-03 16:36:38 +0200

    test: restore sw-date-filter timezone case

--- src/Administration/Resources/app/administration/src/app/component/filter/sw-date-filter/sw-date-filter.spec/relative-day-and-week.spec.js
+++ src/Administration/Resources/app/administration/src/app/component/filter/sw-date-filter/sw-date-filter.spec/relative-day-and-week.spec.js
@@ -2,6 +2,8 @@
  * @sw-package framework
  */
 
+import mockTimezone from 'test/_helper_/mock-timezone';
+
 import { createWrapper, setupDateFilterHooks } from './sw-date-filter.fixtures';
 
 describe('src/app/component/filter/sw-date-filter - relative day and week', () => {
@@ -48,6 +50,51 @@ describe('src/app/component/filter/sw-date-filter - relative day and week', () =
             ]);
 
             jest.setSystemTime(new Date(1337, 11, 31));
+        });
+
+        it('should compute today from the user timezone when the browser timezone is ahead of UTC', async () => {
+            jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
+
+            const wrapper = await createWrapper();
+
+            await wrapper.setProps({
+                filter: {
+                    property: 'releaseDate',
+                    name: 'releaseDate',
+                    label: 'Release Date',
+                    dateType: 'date',
+                    showTimeframe: true,
+                },
+            });
+
+            const resetTimezone = mockTimezone('Europe/Berlin');
+
+            try {
+                wrapper.vm.onTimeframeSelect('today');
+
+                expect(wrapper.emitted()['filter-update']).toEqual([
+                    [
+                        'releaseDate',
+                        [
+                            {
+                                field: 'releaseDate',
+                                parameters: {
+                                    gte: '2020-01-01T00:00:00.000Z',
+                                    lte: '2020-01-01T23:59:59.000Z',
+                                },
+                                type: 'range',
+                            },
+                        ],
+                        {
+                            from: '2020-01-01T00:00:00.000Z',
+                            timeframe: 'today',
+                            to: '2020-01-01T23:59:59.000Z',
+                        },
+                    ],
+                ]);
+            } finally {
+                resetTimezone();
+            }
         });
 
         it('should roll yesterday into the previous month at month boundaries', async () => {

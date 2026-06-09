3cee4b4b527a36182aed179e875fdc08a487b466
kyle <lenguyenky1801@gmail.com>
2026-06-09 09:55:32 +0700

    refactor: remove product document ui from administration and storefront

--- src/Administration/Resources/app/administration/src/module/sw-product/page/sw-product-detail/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-product/page/sw-product-detail/index.js
@@ -273,8 +273,6 @@ export default {
             criteria.getAssociation('media').addSorting(Criteria.sort('position', 'ASC'));
             criteria.addAssociation('media.media');
 
-            criteria.getAssociation('productDocuments').addSorting(Criteria.sort('position', 'ASC'));
-
             criteria.getAssociation('prices').addSorting(Criteria.sort('quantityStart', 'ASC', true));
 
             criteria.getAssociation('tags').addSorting(Criteria.sort('name', 'ASC'));
@@ -306,8 +304,7 @@ export default {
                 .addAssociation('featureSet')
                 .addAssociation('cmsPage')
                 .addAssociation('translations')
-                .addAssociation('downloads.media')
-                .addAssociation('productDocuments.media');
+                .addAssociation('downloads.media');
 
             criteria.getAssociation('manufacturer').addAssociation('media');
 
@@ -407,12 +404,6 @@ export default {
                 {
                     key: 'properties',
                     label: 'sw-product.specifications.cardTitleProperties',
-                    enabled: true,
-                    name: 'specifications',
-                },
-                {
-                    key: 'product_documents',
-                    label: 'sw-product.specifications.cardTitleDocuments',
                     enabled: true,
                     name: 'specifications',
                 },
@@ -745,7 +736,6 @@ export default {
 
             // create empty product
             Shopware.Store.get('swProductDetail').product = this.productRepository.create();
-            this.ensureProductDocumentAssociation(this.product);
 
             // fill empty data
             this.product.active = true;
@@ -941,8 +931,6 @@ export default {
             return this.productRepository
                 .get(this.product.parentId, Shopware.Context.api, this.productCriteria)
                 .then(async (parent) => {
-                    this.ensureProductDocumentAssociation(parent);
-
                     if (!parent.purchasePrices || parent.purchasePrices.length === 0) {
                         if (!this.defaultCurrency?.id) {
                             await this.loadCurrencies();
@@ -971,12 +959,6 @@ export default {
                         false,
                     ]);
                 });
-        },
-
-        ensureProductDocumentAssociation(product) {
-            if (!product.productDocuments) {
-                product.productDocuments = product.getAssociation('productDocuments');
-            }
         },
 
         loadCurrencies() {
--- src/Administration/Resources/app/administration/src/module/sw-product/page/sw-product-detail/store.ts
+++ src/Administration/Resources/app/administration/src/module/sw-product/page/sw-product-detail/store.ts
@@ -75,7 +75,6 @@ const swProductDetail = Shopware.Store.register({
                 'measurement',
                 'selling_packaging',
                 'properties',
-                'product_documents',
                 'essential_characteristics',
                 'custom_fields',
             ],

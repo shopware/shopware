103437301a000b0f62dcd7f43fd3405dff88c701
Lars Kemper <l.kemper@shopware.com>
2026-06-17 15:20:25 +0200

    fix: align product stream ui with rule builder

--- src/Administration/Resources/app/administration/src/app/service/state-style.service.ts
+++ src/Administration/Resources/app/administration/src/app/service/state-style.service.ts
@@ -2,10 +2,13 @@ const { deepCopyObject } = Shopware.Utils.object;
 
 type variantKeys = 'neutral' | 'progress' | 'done' | 'warning' | 'danger';
 
+type meteorVariantKeys = 'neutral' | 'info' | 'attention' | 'critical' | 'positive';
+
 type style = {
     icon: variantKeys;
     color: variantKeys;
     variant: variantKeys;
+    meteorVariant: meteorVariantKeys;
 };
 
 type storedStyle = {
@@ -13,6 +16,7 @@ type storedStyle = {
     iconBackgroundStyle: string;
     icon: string;
     variant: string;
+    meteorVariant: string;
     colorCode: string;
     iconStyle: string;
 };
@@ -49,6 +53,7 @@ export default class StateStyleService {
             iconBackgroundStyle: 'sw-order-state__bg-neutral-icon-bg',
             selectBackgroundStyle: 'sw-order-state__bg-neutral-select',
             variant: 'neutral',
+            meteorVariant: 'neutral',
             colorCode: 'var(--color-icon-secondary-default)',
         },
     };
@@ -85,6 +90,14 @@ export default class StateStyleService {
         danger: 'danger',
     };
 
+    $meteorVariantsMapping: Record<variantKeys, meteorVariantKeys> = {
+        neutral: 'neutral',
+        progress: 'info',
+        done: 'positive',
+        warning: 'attention',
+        danger: 'critical',
+    };
+
     getPlaceholder(): storedStyle {
         return this.$store.placeholder as storedStyle;
     }
@@ -112,6 +125,10 @@ export default class StateStyleService {
 
         if (style.variant in this.$variants) {
             entry.variant = this.$variants[style.variant];
+        }
+
+        if (style.variant in this.$meteorVariantsMapping) {
+            entry.meteorVariant = this.$meteorVariantsMapping[style.variant];
         }
 
         // @ts-expect-error

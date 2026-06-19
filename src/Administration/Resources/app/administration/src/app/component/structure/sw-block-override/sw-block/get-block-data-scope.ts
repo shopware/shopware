a03531d8c58debc444ccb6122eb5aff3d3b3ab15
Gerrit Weiermann <g.weiermann@shopware.com>
2026-06-19 17:51:36 +0200

    feat: expose native setup block data scope

--- src/Administration/Resources/app/administration/src/app/component/structure/sw-block-override/sw-block/get-block-data-scope.ts
+++ src/Administration/Resources/app/administration/src/app/component/structure/sw-block-override/sw-block/get-block-data-scope.ts
@@ -1,9 +1,17 @@
 import { getCurrentInstance } from 'vue';
+import type { ComponentInternalInstance } from 'vue';
+import { scriptSetupDataScopeKey } from 'src/app/adapter/composition-extension-system';
+
+type ComponentInstanceWithScriptSetupDataScope = ComponentInternalInstance & {
+    [scriptSetupDataScopeKey]?: Record<string, unknown>;
+};
 
 /**
  * @sw-package framework
  * @private
  */
 export default function getBlockDataScope() {
-    return getCurrentInstance()?.proxy ?? null;
+    const instance = getCurrentInstance() as ComponentInstanceWithScriptSetupDataScope | null;
+
+    return instance?.[scriptSetupDataScopeKey] ?? instance?.proxy ?? null;
 }

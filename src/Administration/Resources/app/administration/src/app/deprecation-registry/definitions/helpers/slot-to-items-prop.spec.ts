d73799016cabd16814d90c143d35aa7c580aaba9
Gerrit Weiermann <g.weiermann@shopware.com>
2026-06-22 13:07:10 +0200

    Cover items prop order before v-bind

--- src/Administration/Resources/app/administration/src/app/deprecation-registry/definitions/helpers/slot-to-items-prop.spec.ts
+++ src/Administration/Resources/app/administration/src/app/deprecation-registry/definitions/helpers/slot-to-items-prop.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
 import { slotToItemsProp } from './slot-to-items-prop';
-import { createAttribute, createFixer, createRuleApi, createSlot } from './test-utils';
+import { createAttribute, createDirectiveAttribute, createFixer, createRuleApi, createSlot } from './test-utils';
 
 describe('slotToItemsProp', () => {
     it('adds an items prop generated from slot children', () => {
@@ -54,5 +54,52 @@ describe('slotToItemsProp', () => {
 ]"`,
         });
         expect(fixes[1].text).toContain('Please use the "items" property instead.');
+    });
+
+    it('adds the items prop after the component name so object v-bind can overwrite it', () => {
+        const usage = slotToItemsProp({
+            prop: 'items',
+            itemComponent: 'sw-tabs-item',
+        });
+        const slot = createSlot('default', [
+            {
+                type: 'VElement',
+                name: 'sw-tabs-item',
+                startTag: {
+                    attributes: [createAttribute('name', { value: 'overview' })],
+                },
+                children: [
+                    {
+                        type: 'VText',
+                        value: 'Overview',
+                    },
+                ],
+            },
+        ]);
+        const node = {
+            name: 'mt-tabs',
+            startTag: {
+                range: [
+                    0,
+                    9,
+                ],
+                attributes: [createDirectiveAttribute('bind')],
+            },
+            children: [slot],
+        };
+        const api = createRuleApi({ usage, node, slot });
+
+        usage.eslint?.report(api);
+        const fixes = api.reports[0].fix(createFixer());
+
+        expect(fixes[0]).toEqual(
+            expect.objectContaining({
+                method: 'insertTextAfterRange',
+                range: [
+                    8,
+                    8,
+                ],
+            }),
+        );
     });
 });

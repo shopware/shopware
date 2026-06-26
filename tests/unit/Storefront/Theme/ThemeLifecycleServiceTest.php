c0654c85dbd35e4dc4598ec399bf7cbb2dba71c3
Jonas Elfering <j.elfering@shopware.com>
2026-06-26 11:22:07 +0200

    test: simplify theme lifecycle service setup

--- tests/unit/Storefront/Theme/ThemeLifecycleServiceTest.php
+++ tests/unit/Storefront/Theme/ThemeLifecycleServiceTest.php
@@ -4,6 +4,7 @@ namespace Shopware\Tests\Unit\Storefront\Theme;
 
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Content\Media\Aggregate\MediaFolder\MediaFolderCollection;
@@ -22,9 +23,9 @@ use Shopware\Core\System\Language\LanguageCollection;
 use Shopware\Core\System\Language\LanguageEntity;
 use Shopware\Core\System\Locale\LocaleEntity;
 use Shopware\Core\Test\Stub\DataAbstractionLayer\StaticEntityRepository;
-use Shopware\Storefront\Theme\StorefrontPluginConfiguration\AbstractStorefrontPluginConfigurationFactory;
 use Shopware\Storefront\Theme\StorefrontPluginConfiguration\StorefrontPluginConfiguration;
 use Shopware\Storefront\Theme\StorefrontPluginConfiguration\StorefrontPluginConfigurationCollection;
+use Shopware\Storefront\Theme\StorefrontPluginConfiguration\StorefrontPluginConfigurationFactory;
 use Shopware\Storefront\Theme\StorefrontPluginRegistry;
 use Shopware\Storefront\Theme\ThemeCollection;
 use Shopware\Storefront\Theme\ThemeDefinition;
@@ -40,90 +41,66 @@ use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
 #[CoversClass(ThemeLifecycleService::class)]
 class ThemeLifecycleServiceTest extends TestCase
 {
-    public function testRefreshThemeLogsAndSkipsMediaImportFailuresOutsideDev(): void
+    private Context $context;
+
+    private StorefrontPluginConfiguration $configuration;
+
+    private string $assetRoot;
+
+    private FileSaver&MockObject $fileSaver;
+
+    private LoggerInterface&MockObject $logger;
+
+    private ThemeRuntimeConfigService&MockObject $runtimeConfigService;
+
+    private ThemeLifecycleService $lifecycleService;
+
+    /**
+     * @var StaticEntityRepository<ThemeCollection>
+     */
+    private StaticEntityRepository $themeRepository;
+
+    /**
+     * @var StaticEntityRepository<MediaCollection>
+     */
+    private StaticEntityRepository $mediaRepository;
+
+    /**
+     * @var StaticEntityRepository<MediaFolderCollection>
+     */
+    private StaticEntityRepository $mediaFolderRepository;
+
+    /**
+     * @var StaticEntityRepository<EntityCollection<Entity>>
+     */
+    private StaticEntityRepository $themeMediaRepository;
+
+    /**
+     * @var StaticEntityRepository<LanguageCollection>
+     */
+    private StaticEntityRepository $languageRepository;
+
+    /**
+     * @var StaticEntityRepository<EntityCollection<Entity>>
+     */
+    private StaticEntityRepository $themeChildRepository;
+
+    private StorefrontPluginRegistry&MockObject $pluginRegistry;
+
+    private ThemeFilesystemResolver&MockObject $themeFilesystemResolver;
+
+    private Connection&MockObject $connection;
+
+    protected function setUp(): void
     {
-        $context = Context::createDefaultContext();
-        $configuration = $this->createThemeConfiguration();
-        $assetRoot = $this->createAssetRoot();
-        $exception = MediaException::invalidFile('Broken media');
+        $this->context = Context::createDefaultContext();
 
-        $fileSaver = $this->createMock(FileSaver::class);
-        $fileSaver->expects($this->once())->method('persistFileToMedia')->willThrowException($exception);
-
-        $failedMediaId = null;
-        $logger = $this->createMock(LoggerInterface::class);
-        $logger->expects($this->once())->method('error')->with(
-            'Could not import theme media file.',
-            static::callback(static function (array $logContext) use (&$failedMediaId, $configuration, $exception): bool {
-                $failedMediaId = $logContext['mediaId'] ?? null;
-
-                return $logContext['theme'] === $configuration->getTechnicalName()
-                    && $logContext['path'] === 'app/storefront/src/assets/image/shopware_logo.svg'
-                    && $failedMediaId !== null
-                    && $logContext['exception'] === $exception;
-            })
-        );
-
-        $runtimeConfigService = $this->createMock(ThemeRuntimeConfigService::class);
-        $runtimeConfigService->expects($this->once())->method('refreshRuntimeConfig');
-        $runtimeConfigService->expects($this->once())->method('resetCaches');
-
-        $repositories = $this->createRepositories();
-        $service = $this->createService($configuration, $assetRoot, $fileSaver, $logger, $runtimeConfigService, 'test', $repositories);
-
-        try {
-            $service->refreshTheme($configuration, $context);
-        } finally {
-            $this->removeDirectory($assetRoot);
-        }
-
-        static::assertIsString($failedMediaId);
-        static::assertSame($failedMediaId, $repositories['media']->creates[0][0]['id']);
-        static::assertNull($repositories['media']->creates[0][0]['mediaFolderId']);
-        static::assertSame([['id' => $failedMediaId]], $repositories['media']->deletes[0]);
-
-        $themePayload = $repositories['theme']->upserts[0][0];
-        static::assertSame([], $themePayload['media']);
-        static::assertNull($themePayload['baseConfig']['fields']['brokenMedia']['value']);
-    }
-
-    public function testRefreshThemeRethrowsMediaImportFailuresInDev(): void
-    {
-        $context = Context::createDefaultContext();
-        $configuration = $this->createThemeConfiguration();
-        $assetRoot = $this->createAssetRoot();
-        $exception = MediaException::invalidFile('Broken media');
-
-        $fileSaver = $this->createMock(FileSaver::class);
-        $fileSaver->expects($this->once())->method('persistFileToMedia')->willThrowException($exception);
-
-        $logger = $this->createMock(LoggerInterface::class);
-        $logger->expects($this->never())->method('error');
-
-        $runtimeConfigService = $this->createMock(ThemeRuntimeConfigService::class);
-        $runtimeConfigService->expects($this->never())->method('refreshRuntimeConfig');
-        $runtimeConfigService->expects($this->never())->method('resetCaches');
-
-        $repositories = $this->createRepositories();
-        $service = $this->createService($configuration, $assetRoot, $fileSaver, $logger, $runtimeConfigService, 'dev', $repositories);
-
-        $this->expectExceptionObject($exception);
-
-        try {
-            $service->refreshTheme($configuration, $context);
-        } finally {
-            $this->removeDirectory($assetRoot);
-        }
-    }
-
-    private function createThemeConfiguration(): StorefrontPluginConfiguration
-    {
-        $configuration = new StorefrontPluginConfiguration('TestTheme');
-        $configuration->setName('TestTheme');
-        $configuration->setAuthor('shopware AG');
-        $configuration->setIsTheme(true);
-        $configuration->setThemeJson([]);
-        $configuration->setThemeConfig([
+        $this->configuration = new StorefrontPluginConfiguration('TestTheme');
+        $this->configuration->setName('TestTheme');
+        $this->configuration->setAuthor('shopware AG');
+        $this->configuration->setIsTheme(true);
+        $this->configuration->setThemeJson([]);
+        $this->configuration->setThemeConfig([
             'fields' => [
                 'brokenMedia' => [
                     'type' => 'media',
@@ -132,95 +109,16 @@ class ThemeLifecycleServiceTest extends TestCase
             ],
         ]);
 
-        return $configuration;
-    }
+        $this->assetRoot = sys_get_temp_dir() . '/theme-lifecycle-' . Uuid::randomHex();
+        $path = $this->assetRoot . '/Resources/app/storefront/src/assets/image';
 
-    /**
-     * @return array{
-     *     theme: StaticEntityRepository<ThemeCollection>,
-     *     media: StaticEntityRepository<MediaCollection>,
-     *     mediaFolder: StaticEntityRepository<MediaFolderCollection>,
-     *     themeMedia: StaticEntityRepository<EntityCollection<Entity>>,
-     *     language: StaticEntityRepository<LanguageCollection>,
-     *     themeChild: StaticEntityRepository<EntityCollection<Entity>>
-     * }
-     */
-    private function createRepositories(): array
-    {
-        /** @var StaticEntityRepository<ThemeCollection> $themeRepository */
-        $themeRepository = new StaticEntityRepository([new ThemeCollection()], new ThemeDefinition());
-        /** @var StaticEntityRepository<MediaCollection> $mediaRepository */
-        $mediaRepository = new StaticEntityRepository([[]], new MediaDefinition());
-        /** @var StaticEntityRepository<MediaFolderCollection> $mediaFolderRepository */
-        $mediaFolderRepository = new StaticEntityRepository([[]]);
-        /** @var StaticEntityRepository<EntityCollection<Entity>> $themeMediaRepository */
-        $themeMediaRepository = new StaticEntityRepository([]);
-        /** @var StaticEntityRepository<LanguageCollection> $languageRepository */
-        $languageRepository = new StaticEntityRepository([new LanguageCollection([$this->createSystemLanguage()])]);
-        /** @var StaticEntityRepository<EntityCollection<Entity>> $themeChildRepository */
-        $themeChildRepository = new StaticEntityRepository([[]]);
+        static::assertTrue(mkdir($path, 0777, true));
+        static::assertNotFalse(file_put_contents($path . '/shopware_logo.svg', '<svg xmlns="http://www.w3.org/2000/svg"></svg>'));
 
-        return [
-            'theme' => $themeRepository,
-            'media' => $mediaRepository,
-            'mediaFolder' => $mediaFolderRepository,
-            'themeMedia' => $themeMediaRepository,
-            'language' => $languageRepository,
-            'themeChild' => $themeChildRepository,
-        ];
-    }
+        $this->fileSaver = $this->createMock(FileSaver::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->runtimeConfigService = $this->createMock(ThemeRuntimeConfigService::class);
 
-    /**
-     * @param array{
-     *     theme: StaticEntityRepository<ThemeCollection>,
-     *     media: StaticEntityRepository<MediaCollection>,
-     *     mediaFolder: StaticEntityRepository<MediaFolderCollection>,
-     *     themeMedia: StaticEntityRepository<EntityCollection<Entity>>,
-     *     language: StaticEntityRepository<LanguageCollection>,
-     *     themeChild: StaticEntityRepository<EntityCollection<Entity>>
-     * } $repositories
-     */
-    private function createService(
-        StorefrontPluginConfiguration $configuration,
-        string $assetRoot,
-        FileSaver $fileSaver,
-        LoggerInterface $logger,
-        ThemeRuntimeConfigService $runtimeConfigService,
-        string $environment,
-        array $repositories
-    ): ThemeLifecycleService {
-        $configurationCollection = new StorefrontPluginConfigurationCollection([$configuration]);
-
-        $pluginRegistry = $this->createMock(StorefrontPluginRegistry::class);
-        $pluginRegistry->method('getConfigurations')->willReturn($configurationCollection);
-
-        $themeFilesystemResolver = $this->createMock(ThemeFilesystemResolver::class);
-        $themeFilesystemResolver->method('getFilesystemForStorefrontConfig')->willReturn(new Filesystem($assetRoot));
-
-        $connection = $this->createMock(Connection::class);
-        $connection->method('fetchAllAssociative')->willReturn([]);
-
-        return new ThemeLifecycleService(
-            $pluginRegistry,
-            $repositories['theme'],
-            $repositories['media'],
-            $repositories['mediaFolder'],
-            $repositories['themeMedia'],
-            $fileSaver,
-            $this->createMock(FileNameProvider::class),
-            $themeFilesystemResolver,
-            $repositories['language'],
-            $repositories['themeChild'],
-            $connection,
-            $this->createMock(AbstractStorefrontPluginConfigurationFactory::class),
-            $runtimeConfigService,
-            $logger,
-            $environment,
-        );
-    }
-
-    private function createSystemLanguage(): LanguageEntity
-    {
         $locale = new LocaleEntity();
         $locale->setId(Uuid::randomHex());
         $locale->setCode('en-GB');
@@ -229,22 +127,124 @@ class ThemeLifecycleServiceTest extends TestCase
         $language->setId(Uuid::randomHex());
         $language->setTranslationCode($locale);
 
-        return $language;
+        /** @var StaticEntityRepository<ThemeCollection> $themeRepository */
+        $themeRepository = new StaticEntityRepository([new ThemeCollection()], new ThemeDefinition());
+        $this->themeRepository = $themeRepository;
+        /** @var StaticEntityRepository<MediaCollection> $mediaRepository */
+        $mediaRepository = new StaticEntityRepository([[]], new MediaDefinition());
+        $this->mediaRepository = $mediaRepository;
+        /** @var StaticEntityRepository<MediaFolderCollection> $mediaFolderRepository */
+        $mediaFolderRepository = new StaticEntityRepository([[]]);
+        $this->mediaFolderRepository = $mediaFolderRepository;
+        /** @var StaticEntityRepository<EntityCollection<Entity>> $themeMediaRepository */
+        $themeMediaRepository = new StaticEntityRepository([]);
+        $this->themeMediaRepository = $themeMediaRepository;
+        /** @var StaticEntityRepository<LanguageCollection> $languageRepository */
+        $languageRepository = new StaticEntityRepository([new LanguageCollection([$language])]);
+        $this->languageRepository = $languageRepository;
+        /** @var StaticEntityRepository<EntityCollection<Entity>> $themeChildRepository */
+        $themeChildRepository = new StaticEntityRepository([[]]);
+        $this->themeChildRepository = $themeChildRepository;
+
+        $configurationCollection = new StorefrontPluginConfigurationCollection([$this->configuration]);
+
+        $this->pluginRegistry = $this->createMock(StorefrontPluginRegistry::class);
+        $this->pluginRegistry->method('getConfigurations')->willReturn($configurationCollection);
+
+        $this->themeFilesystemResolver = $this->createMock(ThemeFilesystemResolver::class);
+        $this->themeFilesystemResolver->method('getFilesystemForStorefrontConfig')->willReturn(new Filesystem($this->assetRoot));
+
+        $this->connection = $this->createMock(Connection::class);
+        $this->connection->method('fetchAllAssociative')->willReturn([]);
+
+        $this->lifecycleService = new ThemeLifecycleService(
+            $this->pluginRegistry,
+            $this->themeRepository,
+            $this->mediaRepository,
+            $this->mediaFolderRepository,
+            $this->themeMediaRepository,
+            $this->fileSaver,
+            $this->createMock(FileNameProvider::class),
+            $this->themeFilesystemResolver,
+            $this->languageRepository,
+            $this->themeChildRepository,
+            $this->connection,
+            $this->createMock(StorefrontPluginConfigurationFactory::class),
+            $this->runtimeConfigService,
+            $this->logger,
+            'test',
+        );
     }
 
-    private function createAssetRoot(): string
+    protected function tearDown(): void
     {
-        $root = sys_get_temp_dir() . '/theme-lifecycle-' . Uuid::randomHex();
-        $path = $root . '/Resources/app/storefront/src/assets/image';
-
-        static::assertTrue(mkdir($path, 0777, true));
-        static::assertNotFalse(file_put_contents($path . '/shopware_logo.svg', '<svg xmlns="http://www.w3.org/2000/svg"></svg>'));
-
-        return $root;
+        (new SymfonyFilesystem())->remove($this->assetRoot);
     }
 
-    private function removeDirectory(string $path): void
+    public function testRefreshThemeLogsAndSkipsMediaImportFailuresOutsideDev(): void
     {
-        (new SymfonyFilesystem())->remove($path);
+        $exception = MediaException::invalidFile('Broken media');
+
+        $this->fileSaver->expects($this->once())->method('persistFileToMedia')->willThrowException($exception);
+
+        $failedMediaId = null;
+        $this->logger->expects($this->once())->method('error')->with(
+            'Could not import theme media file.',
+            static::callback(function (array $logContext) use (&$failedMediaId, $exception): bool {
+                $failedMediaId = $logContext['mediaId'] ?? null;
+
+                return $logContext['theme'] === $this->configuration->getTechnicalName()
+                    && $logContext['path'] === 'app/storefront/src/assets/image/shopware_logo.svg'
+                    && $failedMediaId !== null
+                    && $logContext['exception'] === $exception;
+            })
+        );
+
+        $this->runtimeConfigService->expects($this->once())->method('refreshRuntimeConfig');
+        $this->runtimeConfigService->expects($this->once())->method('resetCaches');
+
+        $this->lifecycleService->refreshTheme($this->configuration, $this->context);
+
+        static::assertIsString($failedMediaId);
+        static::assertSame($failedMediaId, $this->mediaRepository->creates[0][0]['id']);
+        static::assertNull($this->mediaRepository->creates[0][0]['mediaFolderId']);
+        static::assertSame([['id' => $failedMediaId]], $this->mediaRepository->deletes[0]);
+
+        $themePayload = $this->themeRepository->upserts[0][0];
+        static::assertSame([], $themePayload['media']);
+        static::assertNull($themePayload['baseConfig']['fields']['brokenMedia']['value']);
+    }
+
+    public function testRefreshThemeRethrowsMediaImportFailuresInDev(): void
+    {
+        $exception = MediaException::invalidFile('Broken media');
+
+        $this->fileSaver->expects($this->once())->method('persistFileToMedia')->willThrowException($exception);
+
+        $this->logger->expects($this->never())->method('error');
+
+        $this->runtimeConfigService->expects($this->never())->method('refreshRuntimeConfig');
+        $this->runtimeConfigService->expects($this->never())->method('resetCaches');
+        $this->lifecycleService = new ThemeLifecycleService(
+            $this->pluginRegistry,
+            $this->themeRepository,
+            $this->mediaRepository,
+            $this->mediaFolderRepository,
+            $this->themeMediaRepository,
+            $this->fileSaver,
+            $this->createMock(FileNameProvider::class),
+            $this->themeFilesystemResolver,
+            $this->languageRepository,
+            $this->themeChildRepository,
+            $this->connection,
+            $this->createMock(StorefrontPluginConfigurationFactory::class),
+            $this->runtimeConfigService,
+            $this->logger,
+            'dev',
+        );
+
+        $this->expectExceptionObject($exception);
+
+        $this->lifecycleService->refreshTheme($this->configuration, $this->context);
     }
 }

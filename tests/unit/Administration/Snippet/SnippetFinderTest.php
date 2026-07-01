58282a2ca1464233437beda0d3f7bf8c262ea37c
Krzykawski <m.krzykawski@shopware.com>
2026-07-01 14:01:39 +0200

    test(administration): pass event dispatcher to TranslationLoader in SnippetFinderTest
    The TranslationLoader constructor now takes an EventDispatcherInterface; update
    this remaining call site so the unit suite and PHPStan pass.

--- tests/unit/Administration/Snippet/SnippetFinderTest.php
+++ tests/unit/Administration/Snippet/SnippetFinderTest.php
@@ -32,6 +32,7 @@ use Shopware\Core\System\Snippet\Struct\TranslationConfig;
 use Shopware\Core\Test\Stub\DataAbstractionLayer\StaticEntityRepository;
 use Shopware\Storefront\Storefront;
 use Shopware\Tests\Unit\Core\System\Snippet\Mock\TestPlugin;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * @internal
@@ -462,6 +463,7 @@ class SnippetFinderTest extends TestCase
             snippetSetRepository: $this->snippetSetRepository,
             client: $this->createMock(ClientInterface::class),
             config: $translationConfig,
+            eventDispatcher: new EventDispatcher(),
         );
     }
 }

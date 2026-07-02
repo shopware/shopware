0ffd6f4b541dc66f37b9855585b35e6c2dbe71d4
Nicolas Fortier <n.fortier@shopware.com>
2026-07-02 22:26:21 +0200

    test: enforce createStub rule for Framework/Adapter

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/NoCreateMockWithoutExpectationsRule.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/NoCreateMockWithoutExpectationsRule.php
@@ -61,6 +61,7 @@ class NoCreateMockWithoutExpectationsRule implements Rule
         'Shopware\\Tests\\Unit\\Core\\System\\',
         'Shopware\\Tests\\Unit\\Core\\Content\\',
         'Shopware\\Tests\\Unit\\Core\\Framework\\Mcp\\',
+        'Shopware\\Tests\\Unit\\Core\\Framework\\Adapter\\',
     ];
 
     public function getNodeType(): string

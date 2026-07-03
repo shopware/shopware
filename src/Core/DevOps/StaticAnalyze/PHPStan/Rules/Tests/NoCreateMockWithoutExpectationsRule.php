175978035cb52aa38bf6eb53ae364903d8ef7fc3
Nicolas Fortier <164332867+nfortier-shopware@users.noreply.github.com>
2026-07-03 12:23:35 +0200

    test: enforce createStub rule for Core/Checkout (part 2) (#17867)

--- src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/NoCreateMockWithoutExpectationsRule.php
+++ src/Core/DevOps/StaticAnalyze/PHPStan/Rules/Tests/NoCreateMockWithoutExpectationsRule.php
@@ -62,7 +62,10 @@ class NoCreateMockWithoutExpectationsRule implements Rule
         'Shopware\\Tests\\Unit\\Core\\Content\\',
         'Shopware\\Tests\\Unit\\Core\\Framework\\Mcp\\',
         'Shopware\\Tests\\Unit\\Core\\Checkout\\Cart\\',
+        'Shopware\\Tests\\Unit\\Core\\Checkout\\Customer\\',
         'Shopware\\Tests\\Unit\\Core\\Checkout\\DependencyInjection\\',
+        'Shopware\\Tests\\Unit\\Core\\Checkout\\Rule\\',
+        'Shopware\\Tests\\Unit\\Core\\Checkout\\Validation\\',
         'Shopware\\Tests\\Unit\\Core\\Framework\\DataAbstractionLayer\\',
         'Shopware\\Tests\\Unit\\Elasticsearch\\',
     ];

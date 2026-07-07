8c1d2cb0b17344e0a7624ef3892e7cd1e13c7628
bneumann97 <monkeyfizt@gmail.com>
2026-07-07 11:50:29 +0200

    feat(framework): add configurable last-modified version strategy for theme assets

---
+++ src/Core/Framework/DependencyInjection/CompilerPass/ThemeAssetVersionStrategyCompilerPass.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Framework\DependencyInjection\CompilerPass;
+
+use Shopware\Core\Framework\Log\Package;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+#[Package('framework')]
+class ThemeAssetVersionStrategyCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if ($container->hasParameter('shopware.filesystem.theme.use_last_modified_version_strategy')
+            && !$container->getParameter('shopware.filesystem.theme.use_last_modified_version_strategy')) {
+            $container->removeDefinition('shopware.asset.theme.version_strategy');
+            $container->setAlias('shopware.asset.theme.version_strategy', 'assets.empty_version_strategy');
+        }
+    }
+}

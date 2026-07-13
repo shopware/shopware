5d278231168353f15240044d3fa2230156ce8c8e
Aydin Hassan <a.hassan@shopware.com>
2026-07-13 13:23:34 +0200

    feat: shopware account service protection (#16843)
    Co-authored-by: Magda Kokot <61601293+magdakok@users.noreply.github.com>

--- src/Core/Framework/App/Manifest/ManifestFactory.php
+++ src/Core/Framework/App/Manifest/ManifestFactory.php
@@ -2,6 +2,8 @@
 
 namespace Shopware\Core\Framework\App\Manifest;
 
+use Shopware\Core\Framework\App\AppEntity;
+use Shopware\Core\Framework\App\Source\SourceResolver;
 use Shopware\Core\Framework\Log\Package;
 
 /**
@@ -10,8 +12,19 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('framework')]
 class ManifestFactory
 {
+    public function __construct(private readonly SourceResolver $sourceResolver)
+    {
+    }
+
     public function createFromXmlFile(string $file): Manifest
     {
         return Manifest::createFromXmlFile($file);
+    }
+
+    public function createFromApp(AppEntity $app): Manifest
+    {
+        $filesystem = $this->sourceResolver->filesystemForApp($app);
+
+        return $this->createFromXmlFile($filesystem->path('manifest.xml'));
     }
 }

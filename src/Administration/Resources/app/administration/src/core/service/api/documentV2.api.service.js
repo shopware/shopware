3d2feb3363f2aad6107a319f320e3b92d12a130f
Jozsef Damokos <j.damokos@shopware.com>
2026-07-14 10:13:41 +0200

    split js api services

---
+++ src/Administration/Resources/app/administration/src/core/service/api/documentV2.api.service.js
@@ -0,0 +1,178 @@
+import ApiService from '../api.service';
+import { DocumentEvents } from './document.api.service';
+
+/**
+ * @sw-package after-sales
+ * @class
+ * @extends ApiService
+ */
+class DocumentV2ApiService extends ApiService {
+    constructor(httpClient, loginService, apiEndpoint = 'document-v2') {
+        super(httpClient, loginService, apiEndpoint);
+        this.name = 'documentV2Service';
+        this.$listener = () => {};
+    }
+
+    getAvailableTypes() {
+        return this.httpClient.get('/_action/order/document-v2/available-types', {
+            headers: this.getBasicHeaders(),
+        });
+    }
+
+    createDocument(
+        orderId,
+        orderVersionId,
+        documentTypeName,
+        fileTypes,
+        documentNumber,
+        documentDate,
+        documentComment = '',
+        additionalHeaders = {},
+    ) {
+        const headers = this.getBasicHeaders(additionalHeaders);
+
+        return this.httpClient
+            .post(
+                '/_action/order/document-v2/create',
+                {
+                    orderId,
+                    orderVersionId,
+                    documentType: documentTypeName,
+                    fileTypes,
+                    documentNumber,
+                    documentDate,
+                    documentComment,
+                },
+                {
+                    headers,
+                },
+            )
+            .then((response) => {
+                this.$listener(this.createDocumentEvent(DocumentEvents.DOCUMENT_FINISHED));
+
+                return Promise.resolve(response);
+            })
+            .catch((error) => {
+                if (error.response?.data?.errors) {
+                    this.$listener(
+                        this.createDocumentEvent(DocumentEvents.DOCUMENT_FAILED, error.response.data.errors.pop()),
+                    );
+                }
+            });
+    }
+
+    uploadDocument(
+        orderId,
+        orderVersionId,
+        documentTypeName,
+        fileType,
+        documentNumber,
+        documentDate,
+        documentComment = '',
+        mediaId = null,
+        file = null,
+        referencedDocumentId = null,
+        additionalHeaders = {},
+    ) {
+        const headers = this.getBasicHeaders(additionalHeaders);
+        const payload = {
+            orderId,
+            orderVersionId,
+            documentType: documentTypeName,
+            fileType,
+            documentNumber,
+            documentDate,
+            documentComment,
+            mediaId,
+            referencedDocumentId,
+        };
+
+        let request;
+
+        if (typeof File !== 'undefined' && file instanceof File) {
+            headers['Content-Type'] = file.type;
+
+            request = this.httpClient.post('/_action/order/document-v2/upload', file, {
+                params: {
+                    ...payload,
+                    extension: file.name.split('.').pop(),
+                    fileName: file.name.split('.').shift(),
+                },
+                headers,
+            });
+        } else {
+            request = this.httpClient.post('/_action/order/document-v2/upload', payload, {
+                headers,
+            });
+        }
+
+        return request
+            .then((response) => {
+                this.$listener(this.createDocumentEvent(DocumentEvents.DOCUMENT_FINISHED));
+
+                return Promise.resolve(response);
+            })
+            .catch((error) => {
+                if (error.response?.data?.errors) {
+                    this.$listener(
+                        this.createDocumentEvent(DocumentEvents.DOCUMENT_FAILED, error.response.data.errors.pop()),
+                    );
+                }
+            });
+    }
+
+    previewDocument(
+        orderId,
+        orderVersionId,
+        documentTypeName,
+        fileType,
+        documentNumber,
+        documentDate,
+        documentComment = '',
+        additionalHeaders = {},
+    ) {
+        const headers = this.getBasicHeaders(additionalHeaders);
+
+        return this.httpClient
+            .post(
+                '/_action/order/document-v2/preview',
+                {
+                    orderId,
+                    orderVersionId,
+                    documentType: documentTypeName,
+                    fileType,
+                    documentNumber,
+                    documentDate,
+                    documentComment,
+                },
+                {
+                    responseType: 'blob',
+                    headers,
+                },
+            )
+            .catch(async (error) => {
+                const errorObject = JSON.parse(await error.response.data.text());
+                if (errorObject.errors) {
+                    this.$listener(this.createDocumentEvent(DocumentEvents.DOCUMENT_FAILED, errorObject.errors.pop()));
+                }
+            });
+    }
+
+    getDocument(documentId, deepLinkCode, fileType = 'pdf') {
+        return this.httpClient.get(`/_action/order/document-v2/${documentId}/${deepLinkCode}/download/${fileType}`, {
+            responseType: 'blob',
+            headers: this.getBasicHeaders(),
+        });
+    }
+
+    createDocumentEvent(action, payload) {
+        return { action, payload };
+    }
+
+    setListener(callback) {
+        this.$listener = callback;
+    }
+}
+
+// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
+export default DocumentV2ApiService;
---
+++ src/Administration/Resources/app/administration/src/core/service/api/documentV2.api.service.spec.js
@@ -0,0 +1,233 @@
+import DocumentV2ApiService from 'src/core/service/api/documentV2.api.service';
+import { DocumentEvents } from 'src/core/service/api/document.api.service';
+import createLoginService from 'src/core/service/login.service';
+import createHTTPClient from 'src/core/factory/http.factory';
+import MockAdapter from 'axios-mock-adapter';
+
+/**
+ * @sw-package after-sales
+ */
+
+function createDocumentV2ApiService() {
+    const client = createHTTPClient();
+    const clientMock = new MockAdapter(client);
+    const loginService = createLoginService(client, Shopware.Context.api);
+    const documentV2ApiService = new DocumentV2ApiService(client, loginService);
+
+    return { documentV2ApiService, clientMock };
+}
+
+describe('documentV2Service', () => {
+    it('is registered correctly', async () => {
+        const { documentV2ApiService } = createDocumentV2ApiService();
+
+        expect(documentV2ApiService).toBeInstanceOf(DocumentV2ApiService);
+        expect(documentV2ApiService.name).toBe('documentV2Service');
+    });
+
+    it('loads the support metadata', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+
+        clientMock.onGet('/_action/order/document-v2/available-types').reply(200, {
+            documentTypes: {
+                invoice: {
+                    formats: [
+                        'html',
+                        'zugferd_xml',
+                    ],
+                },
+            },
+        });
+
+        const response = await documentV2ApiService.getAvailableTypes();
+
+        expect(clientMock.history.get[0].url).toBe('/_action/order/document-v2/available-types');
+        expect(response.data).toEqual({
+            documentTypes: {
+                invoice: {
+                    formats: [
+                        'html',
+                        'zugferd_xml',
+                    ],
+                },
+            },
+        });
+    });
+
+    it('creates a document with the selected formats', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+        const listener = jest.fn();
+
+        documentV2ApiService.setListener(listener);
+
+        const orderId = '4a4a687257644d52bf481b4c20e59213';
+        const orderVersionId = '4d03324edcd0490b9180df8161c9167f';
+
+        clientMock.onPost('/_action/order/document-v2/create').reply(200, {
+            documentId: '4d03324edcd0490b9180df8161c9167f',
+            deepLinkCode: 'COp6DlWc2JgUn3XOb7QzKXWcWIVrH8XN',
+            fileTypes: [
+                'html',
+                'zugferd_xml',
+            ],
+        });
+
+        await documentV2ApiService.createDocument(
+            orderId,
+            orderVersionId,
+            'invoice',
+            [
+                'html',
+                'zugferd_xml',
+            ],
+            '1000',
+            '2021-02-22T04:34:56.441Z',
+            '',
+        );
+
+        expect(clientMock.history.post[0].url).toBe('/_action/order/document-v2/create');
+        expect(JSON.parse(clientMock.history.post[0].data)).toEqual({
+            orderId,
+            orderVersionId,
+            documentType: 'invoice',
+            fileTypes: [
+                'html',
+                'zugferd_xml',
+            ],
+            documentNumber: '1000',
+            documentDate: '2021-02-22T04:34:56.441Z',
+            documentComment: '',
+        });
+        expect(listener).toHaveBeenCalledWith({
+            action: DocumentEvents.DOCUMENT_FINISHED,
+            payload: undefined,
+        });
+    });
+
+    it('uploads a document from an existing media file', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+
+        clientMock.onPost('/_action/order/document-v2/upload').reply(200, {
+            documentId: '4d03324edcd0490b9180df8161c9167f',
+            deepLinkCode: 'COp6DlWc2JgUn3XOb7QzKXWcWIVrH8XN',
+            fileTypes: ['pdf'],
+        });
+
+        await documentV2ApiService.uploadDocument(
+            '4a4a687257644d52bf481b4c20e59213',
+            '4d03324edcd0490b9180df8161c9167f',
+            'invoice',
+            'pdf',
+            '1000',
+            '2021-02-22T04:34:56.441Z',
+            '',
+            'media-id',
+            null,
+            'referenced-document-id',
+        );
+
+        expect(clientMock.history.post[0].url).toBe('/_action/order/document-v2/upload');
+        expect(JSON.parse(clientMock.history.post[0].data)).toEqual({
+            orderId: '4a4a687257644d52bf481b4c20e59213',
+            orderVersionId: '4d03324edcd0490b9180df8161c9167f',
+            documentType: 'invoice',
+            fileType: 'pdf',
+            documentNumber: '1000',
+            documentDate: '2021-02-22T04:34:56.441Z',
+            documentComment: '',
+            mediaId: 'media-id',
+            referencedDocumentId: 'referenced-document-id',
+        });
+    });
+
+    it('uploads a document from a binary file body', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+        const file = new File(['test document'], 'invoice.pdf', {
+            type: 'application/pdf',
+        });
+
+        clientMock.onPost('/_action/order/document-v2/upload').reply(200, {
+            documentId: '4d03324edcd0490b9180df8161c9167f',
+            deepLinkCode: 'COp6DlWc2JgUn3XOb7QzKXWcWIVrH8XN',
+            fileTypes: ['pdf'],
+        });
+
+        await documentV2ApiService.uploadDocument(
+            '4a4a687257644d52bf481b4c20e59213',
+            '4d03324edcd0490b9180df8161c9167f',
+            'invoice',
+            'pdf',
+            '1000',
+            '2021-02-22T04:34:56.441Z',
+            '',
+            null,
+            file,
+            'referenced-document-id',
+        );
+
+        expect(clientMock.history.post[0].url).toBe('/_action/order/document-v2/upload');
+        expect(clientMock.history.post[0].data).toBe(file);
+        expect(clientMock.history.post[0].headers['Content-Type']).toBe('application/pdf');
+        expect(clientMock.history.post[0].params).toEqual({
+            orderId: '4a4a687257644d52bf481b4c20e59213',
+            orderVersionId: '4d03324edcd0490b9180df8161c9167f',
+            documentType: 'invoice',
+            fileType: 'pdf',
+            documentNumber: '1000',
+            documentDate: '2021-02-22T04:34:56.441Z',
+            documentComment: '',
+            mediaId: null,
+            referencedDocumentId: 'referenced-document-id',
+            extension: 'pdf',
+            fileName: 'invoice',
+        });
+    });
+
+    it('previews a document with the selected format', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+
+        clientMock.onPost('/_action/order/document-v2/preview').reply(200, {
+            content: '<html></html>',
+        });
+
+        await documentV2ApiService.previewDocument(
+            '4a4a687257644d52bf481b4c20e59213',
+            '4d03324edcd0490b9180df8161c9167f',
+            'invoice',
+            'html',
+            '1000',
+            '2021-02-22T04:34:56.441Z',
+            '',
+            {
+                'sw-language-id': 'language-id',
+            },
+        );
+
+        expect(clientMock.history.post[0].url).toBe('/_action/order/document-v2/preview');
+        expect(clientMock.history.post[0].headers['sw-language-id']).toBe('language-id');
+        expect(JSON.parse(clientMock.history.post[0].data)).toEqual({
+            orderId: '4a4a687257644d52bf481b4c20e59213',
+            orderVersionId: '4d03324edcd0490b9180df8161c9167f',
+            documentType: 'invoice',
+            fileType: 'html',
+            documentNumber: '1000',
+            documentDate: '2021-02-22T04:34:56.441Z',
+            documentComment: '',
+        });
+    });
+
+    it('downloads a document file', async () => {
+        const { documentV2ApiService, clientMock } = createDocumentV2ApiService();
+        const documentId = '4a4a687257644d52bf481b4c20e59213';
+        const deepLinkCode = 'DEEP_LINK';
+        const fileType = 'pdf';
+
+        clientMock.onGet(`/_action/order/document-v2/${documentId}/${deepLinkCode}/download/${fileType}`).reply(200, '');
+
+        await documentV2ApiService.getDocument(documentId, deepLinkCode, fileType);
+
+        expect(clientMock.history.get[0].url).toBe(
+            `/_action/order/document-v2/${documentId}/${deepLinkCode}/download/${fileType}`,
+        );
+    });
+});

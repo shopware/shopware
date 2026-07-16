cc1b9c6ab52d0a76d8569be5fd418ebbdca38019
Elias Lackner <lackner.elias@gmail.com>
2026-07-16 12:29:32 +0200

    fix: misleading rule method names

--- src/Core/Checkout/Cart/Rule/LineItemDimensionHeightRule.php
+++ src/Core/Checkout/Cart/Rule/LineItemDimensionHeightRule.php
@@ -33,7 +33,7 @@ class LineItemDimensionHeightRule extends Rule
     public function match(RuleScope $scope): bool
     {
         if ($scope instanceof LineItemScope) {
-            return $this->matchWeightDimension($scope->getLineItem());
+            return $this->matchHeightDimension($scope->getLineItem());
         }
 
         if (!$scope instanceof CartRuleScope) {
@@ -41,7 +41,7 @@ class LineItemDimensionHeightRule extends Rule
         }
 
         foreach ($scope->getCart()->getLineItems()->filterGoodsFlat() as $lineItem) {
-            if ($this->matchWeightDimension($lineItem)) {
+            if ($this->matchHeightDimension($lineItem)) {
                 return true;
             }
         }
@@ -74,7 +74,7 @@ class LineItemDimensionHeightRule extends Rule
     /**
      * @throws UnsupportedOperatorException
      */
-    private function matchWeightDimension(LineItem $lineItem): bool
+    private function matchHeightDimension(LineItem $lineItem): bool
     {
         $deliveryInformation = $lineItem->getDeliveryInformation();
 

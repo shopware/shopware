16a18011e0586931098cd305894d5849d4cebcac
umut <u.dogan@shopware.com>
2026-07-20 15:38:29 +0200

    chore: migrate checkout service definitions to PHP

--- src/Core/System/System.php
+++ src/Core/System/System.php
@@ -34,24 +34,24 @@ class System extends Bundle
         $configLocator = new FileLocator(__DIR__ . '/DependencyInjection/');
 
         $loader = new XmlFileLoader($container, $configLocator);
+        $phpLoader = new PhpFileLoader($container, $configLocator);
         $loader->load('sales_channel.xml');
         $loader->load('country.xml');
         $loader->load('currency.xml');
         $loader->load('custom_entity.xml');
         $loader->load('locale.xml');
         $loader->load('snippet.xml');
-        $loader->load('salutation.xml');
-        $loader->load('tax.xml');
-        $loader->load('tax_provider.xml');
+        $phpLoader->load('salutation.php');
+        $phpLoader->load('tax.php');
+        $phpLoader->load('tax_provider.php');
         $loader->load('unit.xml');
         $loader->load('user.xml');
         $loader->load('integration.xml');
-        $loader->load('state_machine.xml');
+        $phpLoader->load('state_machine.php');
         $loader->load('configuration.xml');
         $loader->load('number_range.xml');
         $loader->load('tag.xml');
 
-        $phpLoader = new PhpFileLoader($container, $configLocator);
         $phpLoader->load('consent.php');
         $phpLoader->load('usage_data.php');
 

97e8cc96c16abf7ff65940e91fd779dc99aaebfd
Nicolas Fortier <164332867+nfortier-shopware@users.noreply.github.com>
2026-07-20 23:13:21 +0200

    test: add the package attribute to remaining Core/Framework tests (#18443)

--- tests/unit/Core/Framework/FrameworkTest.php
+++ tests/unit/Core/Framework/FrameworkTest.php
@@ -8,12 +8,14 @@ use Shopware\Core\Framework\Adapter\Cache\StampedeProtectionConfigurator;
 use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
 use Shopware\Core\Framework\Feature\FeatureFlagRegistry;
 use Shopware\Core\Framework\Framework;
+use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\SalesChannel\Entity\SalesChannelDefinitionInstanceRegistry;
 use Symfony\Component\DependencyInjection\Container;
 
 /**
  * @internal
  */
+#[Package('framework')]
 #[CoversClass(Framework::class)]
 class FrameworkTest extends TestCase
 {

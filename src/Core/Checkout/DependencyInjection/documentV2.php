427b55bb5f129071fe2da9be34e92dd4a3c6c712
Lars Kemper <l.kemper@shopware.com>
2026-07-21 10:40:45 +0200

    feat: implement cancellation invoice for document v2 (#18310)

--- src/Core/Checkout/DependencyInjection/documentV2.php
+++ src/Core/Checkout/DependencyInjection/documentV2.php
@@ -3,12 +3,14 @@
 namespace Shopware\Core\Checkout\DependencyInjection;
 
 use Doctrine\DBAL\Connection;
+use Shopware\Core\Checkout\Document\Service\ReferenceInvoiceLoader;
 use Shopware\Core\Checkout\DocumentV2\Aggregate\DocumentFile\DocumentFileDefinition;
 use Shopware\Core\Checkout\DocumentV2\Config\DocumentConfigLoader;
 use Shopware\Core\Checkout\DocumentV2\Config\DocumentNumberGenerator;
 use Shopware\Core\Checkout\DocumentV2\Generation\DocumentDependencyResolver;
 use Shopware\Core\Checkout\DocumentV2\Generation\DocumentGenerator;
 use Shopware\Core\Checkout\DocumentV2\Generation\DocumentPersister;
+use Shopware\Core\Checkout\DocumentV2\Provider\CancellationInvoiceDataProvider;
 use Shopware\Core\Checkout\DocumentV2\Provider\DeliveryNoteDataProvider;
 use Shopware\Core\Checkout\DocumentV2\Provider\DocumentDataProviderRegistry;
 use Shopware\Core\Checkout\DocumentV2\Provider\DocumentMetaProvider;
@@ -76,6 +78,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(DeliveryNoteDataProvider::class)
         ->public()
+        ->tag('shopware.document_v2.provider');
+
+    $services->set(CancellationInvoiceDataProvider::class)
+        ->public()
+        ->args([
+            service(InvoiceDataProvider::class),
+            service(ReferenceInvoiceLoader::class),
+        ])
         ->tag('shopware.document_v2.provider');
 
     $services->set(DocumentDataProviderRegistry::class)

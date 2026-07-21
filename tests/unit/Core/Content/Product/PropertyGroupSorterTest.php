cc33ada8835d3e7f959dd42628bab046a274d071
rittou <rittou.xiii@gmail.com>
2026-07-21 13:35:12 +0700

    test: add Package attribute to PropertyGroupSorterTest
    Danger's MissingPackageAttributeInTests rule fails new test classes
    that don't carry #[Package('...')]. Add it with the same value as the
    covered class (PropertyGroupSorter is #[Package('inventory')]).

--- tests/unit/Core/Content/Product/PropertyGroupSorterTest.php
+++ tests/unit/Core/Content/Product/PropertyGroupSorterTest.php
@@ -13,12 +13,14 @@ use Shopware\Core\Content\Property\PropertyGroupEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\Entity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\PartialEntity;
+use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\Framework\Uuid\Uuid;
 
 /**
  * @internal
  */
+#[Package('inventory')]
 #[CoversClass(PropertyGroupSorter::class)]
 class PropertyGroupSorterTest extends TestCase
 {

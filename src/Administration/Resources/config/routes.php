fc112c79d5a6fb1615e6bb601411771ab03abddd
umut <u.dogan@shopware.com>
2026-07-23 16:34:16 +0200

    chore: move Administration dev routes to routes_dev.php

--- src/Administration/Resources/config/routes.php
+++ src/Administration/Resources/config/routes.php
@@ -7,12 +7,4 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
  */
 return static function (RoutingConfigurator $routes): void {
     $routes->import('../../Controller/*Controller.php', 'attribute');
-
-    if ($routes->env() === 'dev') {
-        $routes->import('@PentatrionViteBundle/Resources/config/routing.yaml')
-            ->prefix('/build');
-
-        $routes->add('_profiler_vite', '/_profiler/vite')
-            ->controller('Pentatrion\ViteBundle\Controller\ProfilerController::info');
-    }
 };

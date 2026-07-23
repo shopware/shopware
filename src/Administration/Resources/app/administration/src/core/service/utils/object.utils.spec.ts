95f401b766284f8c71826bf7a04b4dc6d3bb34ac
Johannes Berthel <j.berthel@shopware.com>
2026-07-23 10:29:58 +0200

    refactor: parameterize object diff test cases

--- src/Administration/Resources/app/administration/src/core/service/utils/object.utils.spec.ts
+++ src/Administration/Resources/app/administration/src/core/service/utils/object.utils.spec.ts
@@ -6,43 +6,53 @@ import { getObjectDiff } from './object.utils';
 
 describe('src/core/service/utils/object.utils', () => {
     describe('getObjectDiff', () => {
-        it('returns an empty diff for unchanged values', () => {
-            expect(getObjectDiff({ value: 'image-id' }, { value: 'image-id' })).toEqual({});
-        });
+        const cases: [string, Record<string, unknown>, Record<string, unknown>, Record<string, unknown>][] = [
+            [
+                'returns an empty diff for unchanged values',
+                { value: 'image-id' },
+                { value: 'image-id' },
+                {},
+            ],
+            [
+                'reports changed scalar values',
+                { value: 'first-image-id' },
+                { value: 'second-image-id' },
+                { value: 'second-image-id' },
+            ],
+            [
+                'reports added properties',
+                { value: 'image-id' },
+                { value: 'image-id', source: 'static' },
+                { source: 'static' },
+            ],
+            [
+                'reports removed properties while retaining the remaining object',
+                { value: { imageId: 'image-id', source: 'static' } },
+                { value: { imageId: 'image-id' } },
+                { value: { imageId: 'image-id' } },
+            ],
+            [
+                'reports changed nested properties',
+                { value: { image: { id: 'first-image-id' } } },
+                { value: { image: { id: 'second-image-id' } } },
+                { value: { image: { id: 'second-image-id' } } },
+            ],
+            [
+                'reports an array cleared to empty as a change',
+                { value: ['image-id'] },
+                { value: [] },
+                { value: [] },
+            ],
+            [
+                'reports an object cleared to empty as a change',
+                { value: { imageId: 'image-id' } },
+                { value: {} },
+                { value: {} },
+            ],
+        ];
 
-        it('reports changed scalar values', () => {
-            expect(getObjectDiff({ value: 'first-image-id' }, { value: 'second-image-id' })).toEqual({
-                value: 'second-image-id',
-            });
-        });
-
-        it('reports added properties', () => {
-            expect(getObjectDiff({ value: 'image-id' }, { value: 'image-id', source: 'static' })).toEqual({
-                source: 'static',
-            });
-        });
-
-        it('reports removed properties while retaining the remaining object', () => {
-            expect(
-                getObjectDiff({ value: { imageId: 'image-id', source: 'static' } }, { value: { imageId: 'image-id' } }),
-            ).toEqual({ value: { imageId: 'image-id' } });
-        });
-
-        it('reports changed nested properties', () => {
-            expect(
-                getObjectDiff(
-                    { value: { image: { id: 'first-image-id' } } },
-                    { value: { image: { id: 'second-image-id' } } },
-                ),
-            ).toEqual({ value: { image: { id: 'second-image-id' } } });
-        });
-
-        it('reports an array cleared to empty as a change', () => {
-            expect(getObjectDiff({ value: ['image-id'] }, { value: [] })).toEqual({ value: [] });
-        });
-
-        it('reports an object cleared to empty as a change', () => {
-            expect(getObjectDiff({ value: { imageId: 'image-id' } }, { value: {} })).toEqual({ value: {} });
+        it.each(cases)('%s', (_description, currentValue, nextValue, expectedDiff) => {
+            expect(getObjectDiff(currentValue, nextValue)).toEqual(expectedDiff);
         });
     });
 });

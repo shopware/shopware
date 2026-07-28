042a93083f682c3fcff1320eeca050c276a5915d
Florian Ressel <florian@genxtreme.de>
2026-07-28 10:11:12 +0200

    test: add missing Package attribute to CacheTagCollectorTest

--- tests/unit/Core/Framework/Adapter/Cache/CacheTagCollectorTest.php
+++ tests/unit/Core/Framework/Adapter/Cache/CacheTagCollectorTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Adapter\Cache\CacheTagCollector;
 use Shopware\Core\Framework\Adapter\Cache\Event\AddCacheTagEvent;
+use Shopware\Core\Framework\Log\Package;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -13,6 +14,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 /**
  * @internal
  */
+#[Package('framework')]
 #[CoversClass(CacheTagCollector::class)]
 class CacheTagCollectorTest extends TestCase
 {

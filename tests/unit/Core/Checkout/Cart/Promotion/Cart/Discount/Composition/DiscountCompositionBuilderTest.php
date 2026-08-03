16ec2c76e07d0c0439e069554a9afcc9c0fe3819
Fabian Boensch <f.boensch@shopware.com>
2026-08-03 14:40:12 +0200

    lint

--- tests/unit/Core/Checkout/Cart/Promotion/Cart/Discount/Composition/DiscountCompositionBuilderTest.php
+++ tests/unit/Core/Checkout/Cart/Promotion/Cart/Discount/Composition/DiscountCompositionBuilderTest.php
@@ -59,21 +59,21 @@ class DiscountCompositionBuilderTest extends TestCase
             new CalculatedTaxCollection(),
             new TaxRuleCollection(),
         );
-    
+
         $items = [
             new DiscountCompositionItem('A', 1, 10.0),
             new DiscountCompositionItem('B', 1, 30.0),
         ];
-    
+
         $adjusted = (new DiscountCompositionBuilder())->adjustCompositionItemValues(
             $targetPrice,
             $items,
         );
-    
+
         static::assertCount(2, $adjusted);
         static::assertInstanceOf(DiscountCompositionItem::class, $adjusted[0]);
         static::assertInstanceOf(DiscountCompositionItem::class, $adjusted[1]);
-    
+
         static::assertSame(5.0, $adjusted[0]->getDiscountValue());
         static::assertSame(15.0, $adjusted[1]->getDiscountValue());
     }

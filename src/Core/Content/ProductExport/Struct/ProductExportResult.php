65a26784aee1f5f730f5d12532fa07f9903f79b0
Jonas Elfering <j.elfering@shopware.com>
2026-08-03 20:40:42 +0200

    fix(product-export): trigger total parameter deprecation

--- src/Core/Content/ProductExport/Struct/ProductExportResult.php
+++ src/Core/Content/ProductExport/Struct/ProductExportResult.php
@@ -21,6 +21,9 @@ class ProductExportResult
         private readonly int $offset = 0,
         private readonly bool $hasNextBatch = false
     ) {
+        if (\func_num_args() > 2) {
+            Feature::triggerDeprecationOrThrow('v6.8.0.0', 'Passing $total is deprecated');
+        }
     }
 
     public function getContent(): string

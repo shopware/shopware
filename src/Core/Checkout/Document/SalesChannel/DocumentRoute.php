c078b3cd9a03762e028d6f132a441de52aba2d5c
Dennis Garding <d.garding@shopware.com>
2026-08-04 09:12:50 +0200

    Remove strtolower

--- src/Core/Checkout/Document/SalesChannel/DocumentRoute.php
+++ src/Core/Checkout/Document/SalesChannel/DocumentRoute.php
@@ -214,7 +214,7 @@ final class DocumentRoute extends AbstractDocumentRoute
             throw DocumentException::customerNotLoggedIn();
         }
 
-        $cacheKey = strtolower($documentId) . '-' . ($request->getClientIp() ?? '');
+        $cacheKey = $documentId . '-' . ($request->getClientIp() ?? '');
 
         try {
             $this->rateLimiter->ensureAccepted(RateLimiter::GUEST_LOGIN, $cacheKey);

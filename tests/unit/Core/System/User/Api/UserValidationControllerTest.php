5443ac31b99d04d9eda92fa7e615a6e90a25cef8
Jonas Elfering <j.elfering@shopware.com>
2026-08-04 11:20:53 +0200

    test(user): use explicit validation methods

--- tests/unit/Core/System/User/Api/UserValidationControllerTest.php
+++ tests/unit/Core/System/User/Api/UserValidationControllerTest.php
@@ -36,17 +36,30 @@ class UserValidationControllerTest extends TestCase
     /**
      * @param array<string, string> $payload
      */
-    #[DataProvider('missingParameterProvider')]
-    public function testValidationRoutesRejectMissingParameters(
-        string $method,
-        array $payload,
-        string $missingParameter,
-    ): void {
+    #[DataProvider('missingEmailParameterProvider')]
+    public function testEmailValidationRejectsMissingParameters(array $payload, string $missingParameter): void
+    {
         $controller = new UserValidationController(static::createStub(UserValidationService::class));
 
         static::expectExceptionObject(UserException::missingRequestParameter($missingParameter));
 
-        $controller->{$method}(
+        $controller->isEmailUnique(
+            Request::create('/', Request::METHOD_POST, $payload),
+            Context::createDefaultContext(),
+        );
+    }
+
+    /**
+     * @param array<string, string> $payload
+     */
+    #[DataProvider('missingUsernameParameterProvider')]
+    public function testUsernameValidationRejectsMissingParameters(array $payload, string $missingParameter): void
+    {
+        $controller = new UserValidationController(static::createStub(UserValidationService::class));
+
+        static::expectExceptionObject(UserException::missingRequestParameter($missingParameter));
+
+        $controller->isUsernameUnique(
             Request::create('/', Request::METHOD_POST, $payload),
             Context::createDefaultContext(),
         );
@@ -62,13 +75,20 @@ class UserValidationControllerTest extends TestCase
     }
 
     /**
-     * @return \Generator<string, array{0: string, 1: array<string, string>, 2: string}>
+     * @return \Generator<string, array{0: array<string, string>, 1: string}>
      */
-    public static function missingParameterProvider(): \Generator
+    public static function missingEmailParameterProvider(): \Generator
     {
-        yield 'email is missing' => ['isEmailUnique', ['id' => 'id'], 'email'];
-        yield 'email id is missing' => ['isEmailUnique', ['email' => 'email'], 'id'];
-        yield 'username is missing' => ['isUsernameUnique', ['id' => 'id'], 'username'];
-        yield 'username id is missing' => ['isUsernameUnique', ['username' => 'username'], 'id'];
+        yield 'email is missing' => [['id' => 'id'], 'email'];
+        yield 'email id is missing' => [['email' => 'email'], 'id'];
+    }
+
+    /**
+     * @return \Generator<string, array{0: array<string, string>, 1: string}>
+     */
+    public static function missingUsernameParameterProvider(): \Generator
+    {
+        yield 'username is missing' => [['id' => 'id'], 'username'];
+        yield 'username id is missing' => [['username' => 'username'], 'id'];
     }
 }

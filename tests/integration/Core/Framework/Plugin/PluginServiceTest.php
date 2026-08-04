70919aaa6b0e40ae497f41ca1f6dc80d46be22c0
Jonas Elfering <j.elfering@shopware.com>
2026-08-04 14:04:31 +0200

    chore(framework): clean up plugin backport

--- tests/integration/Core/Framework/Plugin/PluginServiceTest.php
+++ tests/integration/Core/Framework/Plugin/PluginServiceTest.php
@@ -12,7 +12,6 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Plugin\Exception\PluginComposerJsonInvalidException;
 use Shopware\Core\Framework\Plugin\PluginCollection;
 use Shopware\Core\Framework\Plugin\PluginEntity;

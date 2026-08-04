b2645e87be78f10fb85c3aba3d3bc5c5514acd7e
Florian Robel <f.robel@shopware.com>
2026-08-04 16:10:51 +0200

    fix: produc details visual test in administration

--- tests/acceptance/tests/Visual/Administration/ProductDetail.spec.ts
+++ tests/acceptance/tests/Visual/Administration/ProductDetail.spec.ts
@@ -30,72 +30,102 @@ test(
         await test.step('Creates a screenshot of the product detail page General tab.', async () => {
             await ShopAdmin.goesTo(AdminProductDetail.url(product.id));
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/search/category',
+                waitForSelector: AdminProductDetail.releaseDateInput,
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+                AdminProductDetail.page.locator('.sw-select-selection-list__item'),
+                AdminProductDetail.page.locator('.sw-category-tree-field__label-property'),
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-General-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Specifications tab.', async () => {
             await AdminProductDetail.specificationsTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/search-ids/property-group-option',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Configure properties',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Specifications-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Advanced Pricing tab.', async () => {
             await AdminProductDetail.advancedPricingTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/app-system/action-button/product/detail',
+                waitForSelector: AdminProductDetail.page.locator('.sw-product-detail-context-prices__empty-state'),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Advanced-Pricing-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Variants tab.', async () => {
             await AdminProductDetail.variantsTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/search/product',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Configure properties',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Variants-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Layout tab.', async () => {
             await AdminProductDetail.layoutTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/app-system/action-button/product/detail',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Change layout',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Layout-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page SEO tab.', async () => {
             await AdminProductDetail.SEOTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/search/sales-channel',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Upload file',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-SEO-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Cross Selling tab.', async () => {
             await AdminProductDetail.crossSellingTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/app-system/action-button/product/detail',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Add new Cross Selling',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Cross-Selling-Tab.png');
         });
 
         await test.step('Creates a screenshot of the product detail page Reviews tab.', async () => {
             await AdminProductDetail.reviewsTabLink.click();
             await setViewport(AdminProductDetail.page, {
-                requestURL: 'api/app-system/action-button/product/detail',
+                waitForSelector: AdminProductDetail.page.getByRole('button', {
+                    name: 'Go to reviews',
+                }),
             });
-            await replaceElements(AdminProductDetail.page, [AdminProductDetail.productHeadline]);
+            await replaceElements(AdminProductDetail.page, [
+                AdminProductDetail.productHeadline,
+            ]);
             await assertScreenshot(AdminProductDetail.page, 'Product-Detail-Reviews-Tab.png');
         });
     },

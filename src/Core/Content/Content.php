shopware/core

feat: EU guarantee label (#18290)

Content/Content.php
@@ -47,6 +47,7 @@ class Content extends Bundle
         $phpLoader->load('landing_page.php');
         $phpLoader->load('flow.php');
         $phpLoader->load('measurement_system.php');
+        $phpLoader->load('legal_guarantee_notice.php');
         $phpLoader->load('shared.php');
 
         $phpLoader->load('product_export_tracking.php');

Content/Product/SalesChannel/Garan/GaranLabelRoute.php
@@ -1,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Product\SalesChannel\Garan;
+
+use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
+use Shopware\Core\Content\Product\Garan\GaranLabelResolver;
+use Shopware\Core\Content\Product\ProductCollection;
+use Shopware\Core\Content\Product\ProductException;
+use Shopware\Core\Content\Product\SalesChannel\ProductAvailableFilter;
+use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
+use Shopware\Core\Framework\Routing\StoreApiRouteScope;
+use Shopware\Core\PlatformRequest;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Package('inventory')]
+#[Route(defaults: [PlatformRequest::ATTRIBUTE_ROUTE_SCOPE => [StoreApiRouteScope::ID]])]
+class GaranLabelRoute extends AbstractGaranLabelRoute
+{
+    /**
+     * @internal
+     *
+     * @param SalesChannelRepository<ProductCollection> $productRepository
+     */
+    public function __construct(
+        private readonly SalesChannelRepository $productRepository,
+        private readonly GaranLabelResolver $resolver,
+    ) {
+    }
+
+    public function getDecorated(): AbstractGaranLabelRoute
+    {
+        throw new DecorationPatternException(self::class);
+    }
+
+    #[Route(
+        path: '/store-api/product/{productId}/garan-label',
+        name: 'store-api.product.garan-label',
+        methods: [Request::METHOD_GET]
+    )]
+    public function load(string $productId, SalesChannelContext $context): GaranLabelRouteResponse
+    {
+        $product = $this->loadProduct($productId, $context);
+
+        return new GaranLabelRouteResponse(
+            $this->resolver->resolve($product, GaranLabelResolver::LABEL_TYPE_FULL),
+            $this->resolver->resolve($product, GaranLabelResolver::LABEL_TYPE_NESTED),
+        );
+    }
+
+    private function loadProduct(string $productId, SalesChannelContext $context): SalesChannelProductEntity
+    {
+        $criteria = new Criteria([$productId]);
+        $criteria->setTitle('product-garan-label-route');
+        $criteria->addAssociation('manufacturer');
+        $criteria->addFilter(new ProductAvailableFilter($context->getSalesChannelId(), ProductVisibilityDefinition::VISIBILITY_LINK));
+
+        $product = $this->productRepository->search($criteria, $context)->getEntities()->first();
+
+        if (!$product instanceof SalesChannelProductEntity) {
+            throw ProductException::productNotFound($productId);
+        }
+
+        return $product;
+    }
+}

Migration/V6_7/Migration1783944800AddGaranLabel.php
@@ -1,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_7;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Migration\Traits\MailUpdate;
+use Shopware\Core\Migration\Traits\UpdateMailTrait;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @internal
+ */
+#[Package('inventory')]
+class Migration1783944800AddGaranLabel extends MigrationStep
+{
+    use UpdateMailTrait;
+
+    public function getCreationTimestamp(): int
+    {
+        return 1783944800;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->updateProductWithHarmonisedLabelFields($connection);
+        $this->updateConfirmationMailsWithHarmonisedLabels($connection);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+
+    private function updateProductWithHarmonisedLabelFields(Connection $connection): void
+    {
+        if ($this->columnExists($connection, 'product', 'guarantee_confirmed')) {
+            return;
+        }
+
+        $this->addColumn($connection, 'product', 'guarantee_confirmed', 'TINYINT(1)', false, '0');
+
+        if ($this->columnExists($connection, 'product', 'guarantee_months')) {
+            return;
+        }
+
+        $this->addColumn($connection, 'product', 'guarantee_months', 'INT');
+    }
+
+    private function updateConfirmationMailsWithHarmonisedLabels(Connection $connection): void
+    {
+        $filesystem = new Filesystem();
+
+        $update = new MailUpdate(
+            'order_confirmation_mail',
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/en-plain.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/en-html.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/de-plain.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/de-html.html.twig'),
+        );
+
+        $this->updateMail($update, $connection);
+    }
+}

shopware/shopware

src/Core/Content/Content.php
@@ -42,6 +42,7 @@ class Content extends Bundle
         $loader->load('revocation_request_form.xml');
         $loader->load('sitemap.xml');
         $loader->load('landing_page.xml');
+        $loader->load('legal_guarantee_notice.xml');
         $loader->load('flow.xml');
 
         $container->addCompilerPass(new MailerConfigurationCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 0);

src/Core/Content/Product/SalesChannel/Garan/GaranLabelRoute.php
@@ -1,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Product\SalesChannel\Garan;
+
+use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
+use Shopware\Core\Content\Product\Garan\GaranLabelResolver;
+use Shopware\Core\Content\Product\ProductCollection;
+use Shopware\Core\Content\Product\ProductException;
+use Shopware\Core\Content\Product\SalesChannel\ProductAvailableFilter;
+use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
+use Shopware\Core\Framework\Routing\StoreApiRouteScope;
+use Shopware\Core\PlatformRequest;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Package('inventory')]
+#[Route(defaults: [PlatformRequest::ATTRIBUTE_ROUTE_SCOPE => [StoreApiRouteScope::ID]])]
+class GaranLabelRoute extends AbstractGaranLabelRoute
+{
+    /**
+     * @internal
+     *
+     * @param SalesChannelRepository<ProductCollection> $productRepository
+     */
+    public function __construct(
+        private readonly SalesChannelRepository $productRepository,
+        private readonly GaranLabelResolver $resolver,
+    ) {
+    }
+
+    public function getDecorated(): AbstractGaranLabelRoute
+    {
+        throw new DecorationPatternException(self::class);
+    }
+
+    #[Route(
+        path: '/store-api/product/{productId}/garan-label',
+        name: 'store-api.product.garan-label',
+        methods: [Request::METHOD_GET]
+    )]
+    public function load(string $productId, SalesChannelContext $context): GaranLabelRouteResponse
+    {
+        $product = $this->loadProduct($productId, $context);
+
+        return new GaranLabelRouteResponse(
+            $this->resolver->resolve($product, GaranLabelResolver::LABEL_TYPE_FULL),
+            $this->resolver->resolve($product, GaranLabelResolver::LABEL_TYPE_NESTED),
+        );
+    }
+
+    private function loadProduct(string $productId, SalesChannelContext $context): SalesChannelProductEntity
+    {
+        $criteria = new Criteria([$productId]);
+        $criteria->setTitle('product-garan-label-route');
+        $criteria->addAssociation('manufacturer');
+        $criteria->addFilter(new ProductAvailableFilter($context->getSalesChannelId(), ProductVisibilityDefinition::VISIBILITY_LINK));
+
+        $product = $this->productRepository->search($criteria, $context)->getEntities()->first();
+
+        if (!$product instanceof SalesChannelProductEntity) {
+            throw ProductException::notFound($productId);
+        }
+
+        return $product;
+    }
+}

src/Core/Migration/V6_6/Migration1783944800AddGaranLabel.php
@@ -1,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration\V6_6;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Migration\Traits\MailUpdate;
+use Shopware\Core\Migration\Traits\UpdateMailTrait;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @internal
+ */
+#[Package('inventory')]
+class Migration1783944800AddGaranLabel extends MigrationStep
+{
+    use UpdateMailTrait;
+
+    public function getCreationTimestamp(): int
+    {
+        return 1783944800;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->updateProductWithHarmonisedLabelFields($connection);
+        $this->updateConfirmationMailsWithHarmonisedLabels($connection);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+
+    private function updateProductWithHarmonisedLabelFields(Connection $connection): void
+    {
+        if ($this->columnExists($connection, 'product', 'guarantee_confirmed')) {
+            return;
+        }
+
+        $this->addColumn($connection, 'product', 'guarantee_confirmed', 'TINYINT(1)', false, '0');
+
+        if ($this->columnExists($connection, 'product', 'guarantee_months')) {
+            return;
+        }
+
+        $this->addColumn($connection, 'product', 'guarantee_months', 'INT');
+    }
+
+    private function updateConfirmationMailsWithHarmonisedLabels(Connection $connection): void
+    {
+        $filesystem = new Filesystem();
+
+        $update = new MailUpdate(
+            'order_confirmation_mail',
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/en-plain.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/en-html.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/de-plain.html.twig'),
+            $filesystem->readFile(__DIR__ . '/../Fixtures/mails/order_confirmation_mail/de-html.html.twig'),
+        );
+
+        $this->updateMail($update, $connection);
+    }
+}
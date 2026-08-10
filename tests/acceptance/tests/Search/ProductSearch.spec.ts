fa6ac0c03cea47f6fbb10731e4655afd00108561
Jonas Elfering <j.elfering@shopware.com>
2026-08-10 15:29:40 +0200

    test(acceptance): make product search assertion portable

--- tests/acceptance/tests/Search/ProductSearch.spec.ts
+++ tests/acceptance/tests/Search/ProductSearch.spec.ts
@@ -10,20 +10,22 @@ test(
     },
     async ({ ShopCustomer, TestDataService, StorefrontHome, StorefrontSearchSuggest, SearchForTerm, IdProvider }) => {
         const productNameSuffix1 = IdProvider.getIdPair().uuid;
-        await TestDataService.createBasicProduct({
-            name: `Bottle${productNameSuffix1}`,
+        const createBottle = TestDataService.createBasicProduct({
+            name: `Bottle ${productNameSuffix1}`,
         });
-        await TestDataService.createBasicProduct({
-            name: `Bowl${productNameSuffix1}`,
+        const createBowl = TestDataService.createBasicProduct({
+            name: `Bowl ${productNameSuffix1}`,
         });
+
+        await Promise.all([createBottle, createBowl]);
 
         await TestDataService.clearCaches();
 
         await test.step('Products are visible.', async () => {
             await ShopCustomer.goesTo(StorefrontHome.url());
-            const productLocator1 = await StorefrontHome.getListingItemByProductName(`Bottle${productNameSuffix1}`);
+            const productLocator1 = await StorefrontHome.getListingItemByProductName(`Bottle ${productNameSuffix1}`);
             await ShopCustomer.expects(productLocator1.productName).toBeVisible();
-            const productLocator2 = await StorefrontHome.getListingItemByProductName(`Bowl${productNameSuffix1}`);
+            const productLocator2 = await StorefrontHome.getListingItemByProductName(`Bowl ${productNameSuffix1}`);
             await ShopCustomer.expects(productLocator2.productName).toBeVisible();
         });
 
@@ -34,9 +36,12 @@ test(
         });
 
         await test.step('Customer searches term and sees a single matching product', async () => {
-            await ShopCustomer.attemptsTo(SearchForTerm(`Bottle${productNameSuffix1}`));
+            await ShopCustomer.attemptsTo(SearchForTerm('Bottle'));
             const totalCount1 = await StorefrontSearchSuggest.getTotalSearchResultCount();
             await ShopCustomer.expects(totalCount1).toBe(1);
+
+            const lineItemText = await StorefrontSearchSuggest.searchSuggestLineItemName.first().textContent();
+            ShopCustomer.expects(lineItemText).toContain(`Bottle ${productNameSuffix1}`);
         });
 
         await test.step('Customer searches for a partial term and sees multiple matching products', async () => {

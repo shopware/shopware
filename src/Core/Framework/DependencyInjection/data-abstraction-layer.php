d9818bf8f2f3d7eb9af35c621eb8c13190a310f1
Andrii Havryliuk <a.havryliuk@shopware.com>
2026-08-12 12:59:34 +0200

    feat: sync api metrics (#18929)

--- src/Core/Framework/DependencyInjection/data-abstraction-layer.php
+++ src/Core/Framework/DependencyInjection/data-abstraction-layer.php
@@ -9,6 +9,7 @@ use Shopware\Core\Content\Product\SearchKeyword\ProductSearchTermInterpreter;
 use Shopware\Core\Framework\Api\Acl\AclCriteriaValidator;
 use Shopware\Core\Framework\Api\Sync\SyncFkResolver;
 use Shopware\Core\Framework\Api\Sync\SyncService;
+use Shopware\Core\Framework\Api\Sync\Telemetry\SyncMetricsInstrumentor;
 use Shopware\Core\Framework\DataAbstractionLayer\Cache\EntityCacheKeyGenerator;
 use Shopware\Core\Framework\DataAbstractionLayer\Command\CreateEntitiesCommand;
 use Shopware\Core\Framework\DataAbstractionLayer\Command\CreateHydratorCommand;
@@ -873,6 +874,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             service(RequestCriteriaBuilder::class),
             service(AclCriteriaValidator::class),
             service(SyncFkResolver::class),
+            service(SyncMetricsInstrumentor::class),
+        ]);
+
+    $services->set(SyncMetricsInstrumentor::class)
+        ->args([
+            service(Meter::class),
+            service(EntityGroupResolver::class),
         ]);
 
     $services->set(SyncFkResolver::class)

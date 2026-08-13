2e390a617f73d047f39d90a97d56f7930bce38e9
Jonas Elfering <j.elfering@shopware.com>
2026-08-13 11:31:02 +0200

    fix(framework): remove ignored cache controller phpstan errors

--- src/Core/Framework/Api/Controller/CacheController.php
+++ src/Core/Framework/Api/Controller/CacheController.php
@@ -62,8 +62,8 @@ class CacheController extends AbstractController
     {
         $data = $dataBag->all();
 
-        $skip = !empty($data['skip']) && \is_array($data['skip']) ? array_values($data['skip']) : [];
-        $only = !empty($data['only']) && \is_array($data['only']) ? array_values($data['only']) : [];
+        $skip = isset($data['skip']) && \is_array($data['skip']) && $data['skip'] !== [] ? array_values($data['skip']) : [];
+        $only = isset($data['only']) && \is_array($data['only']) && $data['only'] !== [] ? array_values($data['only']) : [];
 
         $this->indexerRegistry->sendFullIndexingMessage($skip, $only);
 

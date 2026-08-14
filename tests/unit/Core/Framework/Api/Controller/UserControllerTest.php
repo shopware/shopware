060a1835d91d136f8971666ab92d1a3e1a0c0f61
Jonas Elfering <j.elfering@shopware.com>
2026-08-14 11:24:35 +0200

    fix(framework): allow users to change their timezone

--- tests/unit/Core/Framework/Api/Controller/UserControllerTest.php
+++ tests/unit/Core/Framework/Api/Controller/UserControllerTest.php
@@ -8,11 +8,13 @@ use Shopware\Core\Framework\Api\ApiException;
 use Shopware\Core\Framework\Api\Context\AdminApiSource;
 use Shopware\Core\Framework\Api\Context\ShopApiSource;
 use Shopware\Core\Framework\Api\Controller\UserController;
+use Shopware\Core\Framework\Api\Response\ResponseFactoryInterface;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Sso\SsoService;
 use Shopware\Core\System\User\UserDefinition;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -57,6 +59,44 @@ class UserControllerTest extends TestCase
         $context = Context::createDefaultContext(new AdminApiSource(null));
 
         $controller->logout($context);
+    }
+
+    public function testUpdateMeAllowsChangingTimezone(): void
+    {
+        $userId = 'test-user-id';
+        $context = Context::createDefaultContext(new AdminApiSource($userId));
+        $request = Request::create('/', Request::METHOD_PATCH, ['timeZone' => 'Europe/Berlin']);
+        $responseFactory = static::createStub(ResponseFactoryInterface::class);
+        $response = new Response();
+
+        $controller = $this->getMockBuilder(UserController::class)
+            ->setConstructorArgs([
+                static::createStub(EntityRepository::class),
+                static::createStub(EntityRepository::class),
+                static::createStub(EntityRepository::class),
+                static::createStub(EntityRepository::class),
+                static::createStub(UserDefinition::class),
+                static::createStub(SsoService::class),
+            ])
+            ->onlyMethods(['upsertUser'])
+            ->getMock();
+        $controller->expects($this->once())
+            ->method('upsertUser')
+            ->with($userId, $request, $context, $responseFactory)
+            ->willReturn($response);
+
+        static::assertSame($response, $controller->updateMe($context, $request, $responseFactory));
+    }
+
+    public function testUpdateMeRejectsFieldsOutsideTheSelfProfileAllowlist(): void
+    {
+        static::expectExceptionObject(ApiException::missingPrivileges(['user:update']));
+
+        $controller = $this->createController();
+        $context = Context::createDefaultContext(new AdminApiSource('test-user-id'));
+        $request = Request::create('/', Request::METHOD_PATCH, ['title' => 'Dr.']);
+
+        $controller->updateMe($context, $request, static::createStub(ResponseFactoryInterface::class));
     }
 
     private function createController(?SsoService $ssoService = null): UserController

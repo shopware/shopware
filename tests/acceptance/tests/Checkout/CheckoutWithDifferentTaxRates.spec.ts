4aff553d4f90b3a70df90400770996c885848848
Jonas Elfering <j.elfering@shopware.com>
2026-08-14 10:21:31 +0200

    test(acceptance): clear cache before tax checkout

--- tests/acceptance/tests/Checkout/CheckoutWithDifferentTaxRates.spec.ts
+++ tests/acceptance/tests/Checkout/CheckoutWithDifferentTaxRates.spec.ts
@@ -26,6 +26,7 @@ test(
     }) => {
         const taxRate19 = await TestDataService.createTaxRate({ taxRate: 19.0 });
         const productWithTaxRate19 = await TestDataService.createBasicProduct({}, taxRate19.id);
+        await TestDataService.clearCaches();
         let orderNumber: string;
 
         await test.step('Add 2 identical products to chart, proceed to checkout and validate on confirm page the tax price.', async () => {
@@ -84,6 +85,7 @@ test(
         const taxRate7 = await TestDataService.createTaxRate({ taxRate: 7.0 });
         const product1WithTaxRate7 = await TestDataService.createBasicProduct({}, taxRate7.id);
         const product2WithTaxRate7 = await TestDataService.createBasicProduct({}, taxRate7.id);
+        await TestDataService.clearCaches();
         let orderNumber: string;
 
         await test.step('Add 2 different products with same tax rate to chart, proceed to checkout and validate on confirm page the tax price.', async () => {
@@ -145,6 +147,7 @@ test(
         const taxRate7 = await TestDataService.createTaxRate({ taxRate: 7.0 });
         const productWithTaxRate19 = await TestDataService.createBasicProduct({}, taxRate19.id);
         const product1WithTaxRate7 = await TestDataService.createBasicProduct({}, taxRate7.id);
+        await TestDataService.clearCaches();
         let orderNumber: string;
 
         await test.step('Add 2 different products with different tax rates to chart, proceed to checkout and validate on confirm page the tax price.', async () => {
@@ -207,6 +210,7 @@ test(
     }) => {
         const taxRate0 = await TestDataService.createTaxRate({ taxRate: 0 });
         const productWithTaxRate0 = await TestDataService.createBasicProduct({}, taxRate0.id);
+        await TestDataService.clearCaches();
         let orderNumber: string;
 
         await test.step('Add one product with tax rate to chart, proceed to checkout and validate on confirm page the tax price.', async () => {

16f65133c6eb05c90ab7fe4e0ddb61aba9003949
Nicolas Fortier <n.fortier@shopware.com>
2026-08-20 01:47:36 +0200

    feat(framework): enforce test package attributes matching covered classes

---
+++ tests/devops/Core/DevOps/StaticAnalyse/PHPStan/Rules/data/CoversPackageMatchRule/Covered/CheckoutService.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\DevOps\Core\DevOps\StaticAnalyse\PHPStan\Rules\data\CoversPackageMatchRule\Covered;
+
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('checkout')]
+class CheckoutService
+{
+}
---
+++ tests/devops/Core/DevOps/StaticAnalyse/PHPStan/Rules/data/CoversPackageMatchRule/Tests/MissingTestPackageFixture.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Tests\DevOps\Core\DevOps\StaticAnalyse\PHPStan\Rules\data\CoversPackageMatchRule\Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Shopware\Tests\DevOps\Core\DevOps\StaticAnalyse\PHPStan\Rules\data\CoversPackageMatchRule\Covered\CheckoutService;
+
+#[CoversClass(CheckoutService::class)]
+class MissingTestPackageFixture extends TestCase
+{
+}

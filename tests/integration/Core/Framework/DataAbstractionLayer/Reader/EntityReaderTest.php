4e4ede13d09699bee94c667553f7d2e41cda2d72
Jonas Elfering <j.elfering@shopware.com>
2026-08-20 09:59:08 +0200

    Update tests/integration/Core/Framework/DataAbstractionLayer/Reader/EntityReaderTest.php
    Co-authored-by: Michael Telgmann <mitelg@users.noreply.github.com>

--- tests/integration/Core/Framework/DataAbstractionLayer/Reader/EntityReaderTest.php
+++ tests/integration/Core/Framework/DataAbstractionLayer/Reader/EntityReaderTest.php
@@ -1767,7 +1767,7 @@ class EntityReaderTest extends TestCase
         static::assertSame([$id1], array_values($category1->getProducts()?->getIds() ?? []));
 
         static::assertInstanceOf(CategoryEntity::class, $category2);
-        static::assertEmpty($category2->getProducts()?->getIds() ?? []);
+        static::assertSame([], $category2->getProducts()?->getIds() ?? []);
     }
 
     public function testLoadManyToManySupportsFilter(): void

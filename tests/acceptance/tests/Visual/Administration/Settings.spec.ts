ab7e1c33ab33c167ae824d5afb48968e7a7e9023
Orkun Cavli <o.cavli@shopware.com>
2026-08-21 12:04:25 +0200

    Revert "test(administration): force collapsed admin menu before visual screenshots"
    This reverts commit 080fe50abec6ebb10b946c90ce1531b7972cfaa4.

--- tests/acceptance/tests/Visual/Administration/Settings.spec.ts
+++ tests/acceptance/tests/Visual/Administration/Settings.spec.ts
@@ -1,10 +1,8 @@
 import { test, setViewport, assertScreenshot } from '@fixtures/AcceptanceTest';
-import { collapseAdminMenu } from '@helpers/admin-menu-helpers';
 
 test('Visual: Administration settings page', { tag: '@Visual' }, async ({ ShopAdmin, AdminSettingsListing }) => {
     await test.step('Creates a screenshot of the settings overview page.', async () => {
         await ShopAdmin.goesTo(AdminSettingsListing.url());
-        await collapseAdminMenu(AdminSettingsListing.page);
         await setViewport(AdminSettingsListing.page, {
             waitForSelector: AdminSettingsListing.shopwareServicesLink,
         });

0858fcd1aa2acf9dd46d2c60b1b6fafffd68addd
Nicolas Fortier <n.fortier@shopware.com>
2026-08-21 17:02:56 +0200

    chore: type the theme indexer event arrays and drop their baseline entries

--- src/Storefront/Theme/Event/ThemeIndexerEvent.php
+++ src/Storefront/Theme/Event/ThemeIndexerEvent.php
@@ -12,6 +12,10 @@ use Shopware\Core\Framework\Log\Package;
 #[Package('discovery')]
 class ThemeIndexerEvent extends NestedEvent
 {
+    /**
+     * @param array<string> $ids
+     * @param array<string> $skip
+     */
     public function __construct(
         private readonly array $ids,
         private readonly Context $context,
@@ -24,11 +28,17 @@ class ThemeIndexerEvent extends NestedEvent
         return $this->context;
     }
 
+    /**
+     * @return array<string>
+     */
     public function getIds(): array
     {
         return $this->ids;
     }
 
+    /**
+     * @return array<string>
+     */
     public function getSkip(): array
     {
         return $this->skip;

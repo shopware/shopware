ad4b9daab5de550fe6cf208984464733b44b286c
Nicolas Fortier <n.fortier@shopware.com>
2026-08-21 19:34:32 +0200

    test: mark checkout-owned Core Framework classes codeCoverageIgnore

--- src/Core/Framework/Store/Struct/LicenseDomainCollection.php
+++ src/Core/Framework/Store/Struct/LicenseDomainCollection.php
@@ -7,6 +7,8 @@ use Shopware\Core\Framework\Struct\Collection;
 
 /**
  * @extends Collection<LicenseDomainStruct>
+ *
+ * @codeCoverageIgnore
  */
 #[Package('checkout')]
 class LicenseDomainCollection extends Collection

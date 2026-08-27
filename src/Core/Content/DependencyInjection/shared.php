ba28b52d4314e3a98bf0b2652d28e2df814761cb
Jozsef Damokos <j.damokos@shopware.com>
2026-08-27 15:42:50 +0300

    remove comment

--- src/Core/Content/DependencyInjection/shared.php
+++ src/Core/Content/DependencyInjection/shared.php
@@ -20,7 +20,6 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    // Shared between the mail attachment builder and the a11y flow storer
     $services->set(DocumentResolver::class)
         ->args([
             service('document.repository'),

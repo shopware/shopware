352abfa977103197b7c7c9ddce5feda2679bb632
Nicolas Fortier <n.fortier@shopware.com>
2026-08-28 19:39:17 +0200

    test: drop the coverage ignore from the tax provider collection

--- src/Core/System/TaxProvider/TaxProviderCollection.php
+++ src/Core/System/TaxProvider/TaxProviderCollection.php
@@ -7,8 +7,6 @@ use Shopware\Core\Framework\Log\Package;
 
 /**
  * @extends EntityCollection<TaxProviderEntity>
- *
- * @codeCoverageIgnore
  */
 #[Package('checkout')]
 class TaxProviderCollection extends EntityCollection

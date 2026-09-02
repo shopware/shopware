26bc9a03b53dbaf3c57fc4466cdbd77704b121e3
Simon Fiebranz <s.fiebranz@shopware.com>
2026-09-02 13:55:07 +0200

    Adjust backfill migration for type_name fields

--- src/Core/Migration/V6_7/Migration1787130172BackfillDocumentTypeName.php
+++ src/Core/Migration/V6_7/Migration1787130172BackfillDocumentTypeName.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\ParameterType;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Util\Database\TableHelper;
 
 /**
  * @internal
@@ -30,7 +31,9 @@ class Migration1787130172BackfillDocumentTypeName extends MigrationStep
     public function update(Connection $connection): void
     {
         foreach (self::DOCUMENT_TYPE_TABLES as $table) {
-            $this->backfillTable($connection, $table);
+            if (TableHelper::columnExists($connection, $table, 'type_name')) {
+                $this->backfillTable($connection, $table);
+            }
         }
     }
 

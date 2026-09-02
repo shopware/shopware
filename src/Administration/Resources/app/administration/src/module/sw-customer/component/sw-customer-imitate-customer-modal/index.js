f20da58fc74bd720d1645021e278458d1e79e56a
Max Stegmeyer <m.stegmeyer@shopware.com>
2026-09-02 15:02:06 +0200

    fix(administration): default isLoading to false in imitate customer modal

--- src/Administration/Resources/app/administration/src/module/sw-customer/component/sw-customer-imitate-customer-modal/index.js
+++ src/Administration/Resources/app/administration/src/module/sw-customer/component/sw-customer-imitate-customer-modal/index.js
@@ -35,7 +35,7 @@ export default {
         return {
             salesChannelDomains: [],
             imitateCustomerTokens: {},
-            isLoading: true,
+            isLoading: false,
         };
     },
 

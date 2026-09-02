0fe5c44a12176721d49e6f8854bfb8160cd2a194
Simon Fiebranz <s.fiebranz@shopware.com>
2026-09-02 10:49:45 +0200

    Add missing package annotation

--- tests/migration/Core/V6_8/Migration1788337044RemoveTypeNameFromDocumentBaseConfigSalesChannelTest.php
+++ tests/migration/Core/V6_8/Migration1788337044RemoveTypeNameFromDocumentBaseConfigSalesChannelTest.php
@@ -5,10 +5,12 @@ namespace Shopware\Tests\Migration\Core\V6_8;
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
 use Shopware\Core\Framework\Util\Database\TableHelper;
 use Shopware\Core\Migration\V6_8\Migration1788337044RemoveTypeNameFromDocumentBaseConfigSalesChannel;
 
+#[Package('after-sales')]
 #[CoversClass(Migration1788337044RemoveTypeNameFromDocumentBaseConfigSalesChannel::class)]
 class Migration1788337044RemoveTypeNameFromDocumentBaseConfigSalesChannelTest extends TestCase
 {

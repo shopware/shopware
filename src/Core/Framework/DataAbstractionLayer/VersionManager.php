65577161543f28613a2bb5a08599ed17eb0205b4
Jonas Elfering <j.elfering@shopware.com>
2026-09-03 11:24:28 +0200

    docs(framework): explain clone association protection

--- src/Core/Framework/DataAbstractionLayer/VersionManager.php
+++ src/Core/Framework/DataAbstractionLayer/VersionManager.php
@@ -348,6 +348,7 @@ class VersionManager
                 continue;
             }
 
+            // Autoloaded associations are not added to the clone criteria, but can still be present in the serialized entity.
             if ($field instanceof AssociationField && $this->isCloneProtected($this->getCloneReferenceDefinition($field), $context)) {
                 continue;
             }

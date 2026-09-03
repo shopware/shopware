74f4dc0b7030c53d89f013d89895e7b0e8948e6a
Simon Fiebranz <s.fiebranz@shopware.com>
2026-09-03 13:29:54 +0200

    Revert "Skip test that tests for the type name field"
    This reverts commit edddfca61601d6f86d522e671203af026855ea8b.

--- tests/integration/Core/Checkout/DocumentV2/Subscriber/DocumentTypeNameSyncSubscriberTest.php
+++ tests/integration/Core/Checkout/DocumentV2/Subscriber/DocumentTypeNameSyncSubscriberTest.php
@@ -8,7 +8,6 @@ use Shopware\Core\Checkout\Document\Aggregate\DocumentBaseConfigSalesChannel\Doc
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\Framework\Uuid\Uuid;
@@ -44,10 +43,6 @@ class DocumentTypeNameSyncSubscriberTest extends TestCase
 
     public function testTypeNameIsWrittenForSalesChannelConfigWrite(): void
     {
-        if (Feature::isActive('v6.8.0.0')) {
-            static::markTestSkipped('type_name of document_base_config_sales_channel table was removed.');
-        }
-
         $context = Context::createDefaultContext();
         $documentTypeId = $this->getDocumentTypeId('invoice', $context);
 

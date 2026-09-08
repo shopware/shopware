35bdd2dc8c23576332c514a32b46faaf95cf1176
Martin Bens <m.bens@shopware.com>
2026-09-08 13:06:20 +0200

    fix(content-system): keep mirrored consumer wiring decode-legal (#20180)

--- src/Core/Framework/ContentSystem/Mutation/Op/AttachElement.php
+++ src/Core/Framework/ContentSystem/Mutation/Op/AttachElement.php
@@ -37,7 +37,8 @@ final class AttachElement extends AbstractLayoutMutation
 
         $clone = $this->cloneWithNewIds($this->element);
         $this->affected = $this->subtreeIds($clone);
-        $this->created = $this->affected;
+        // created and affected are the same set here: every node of the fresh subtree.
+        $this->created = $this->subtreeIds($clone);
 
         if ($this->parentElementId === null) {
             return $tree->insertAtRoot($this->index, [$clone]);

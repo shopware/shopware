7c67aca93760397f26ed1641d4202f06b011ed7d
Jonas Elfering <j.elfering@shopware.com>
2026-09-08 13:25:42 +0200

    test(administration): fix deprecated test annotation

--- src/Administration/Resources/app/administration/src/app/component/base/sw-tabs/sw-tabs.spec.js
+++ src/Administration/Resources/app/administration/src/app/component/base/sw-tabs/sw-tabs.spec.js
@@ -18,7 +18,7 @@ async function createWrapper(additionalOptions = {}) {
 }
 
 describe('src/app/component/base/sw-tabs', () => {
-    // @deprecated tag:v6.8.0.0 - The test will be removed with the legacy sw-tabs branch.
+    // @deprecated tag:v6.8.0 - The test will be removed with the legacy sw-tabs branch.
     it.deprecated('v6.8.0.0')('should render the deprecated tabs by default', async () => {
         const wrapper = await createWrapper();
 

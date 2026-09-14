9b6bfe1a1e564c83825071684bd6d2a3e96646b7
Michael Telgmann <m.telgmann@shopware.com>
2026-09-14 15:57:27 +0200

    add comment to new method

--- src/Core/Framework/DataAbstractionLayer/Search/IdSearchResult.php
+++ src/Core/Framework/DataAbstractionLayer/Search/IdSearchResult.php
@@ -92,6 +92,9 @@ class IdSearchResult extends Struct
     }
 
     /**
+     * Returns a list of associative arrays. For regular entities it will only use `id` as the key.
+     * For mapping entities which have combined primary keys, it will just return the full primary key structure.
+     *
      * @return list<(IDStructure is string ? array{id: string} : IDStructure)>
      */
     public function getPrimaryKeyData(): array

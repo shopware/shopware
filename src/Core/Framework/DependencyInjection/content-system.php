26719be796feceb8f5f55ca0c6d5dba24a44513e
David Neustadt <d.neustadt@shopware.com>
2026-09-14 09:18:57 +0200

    feat(content-system): layout presets from extensions (#20220)

--- src/Core/Framework/DependencyInjection/content-system.php
+++ src/Core/Framework/DependencyInjection/content-system.php
@@ -68,6 +68,7 @@ use Shopware\Core\Framework\ContentSystem\Layout\Field\StoredElementListFieldSer
 use Shopware\Core\Framework\ContentSystem\Layout\LayoutDefaultSeeder;
 use Shopware\Core\Framework\ContentSystem\Layout\LayoutWriteBoundary;
 use Shopware\Core\Framework\ContentSystem\Layout\Preset\LayoutPresetPayloadCompiler;
+use Shopware\Core\Framework\ContentSystem\Layout\Preset\Loader\DatabaseLayoutPresetLoader;
 use Shopware\Core\Framework\ContentSystem\Layout\Preset\Loader\LayoutPresetNameResolver;
 use Shopware\Core\Framework\ContentSystem\Layout\Preset\Loader\YamlLayoutPresetLoader;
 use Shopware\Core\Framework\ContentSystem\Layout\Preset\Registry\CachedContentSystemLayoutPresetRegistry;
@@ -498,6 +499,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             service(LayoutPresetNameResolver::class),
         ])
         ->arg('$directories', [])
+        ->tag('content_system.layout_preset_loader');
+
+    $services->set(DatabaseLayoutPresetLoader::class)
+        ->args([
+            service(LayoutPresetSpecificationSerializer::class),
+            service(LayoutPresetPayloadCompiler::class),
+            service('validator'),
+            service(Connection::class),
+            param('kernel.environment'),
+            service('logger'),
+        ])
         ->tag('content_system.layout_preset_loader');
 
     $services->set(ContentSystemLayoutPresetRegistry::class)

ef3a9dfa15b59d46dcb48c0b0ba65ab755c2f13d
Daniel Vien <d.vienphamtri@shopware.com>
2026-09-16 17:52:37 +0700

    fix(administration): restore customer link URLs in 6.7.14

--- src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-list/sw-customer-list.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-customer/page/sw-customer-list/sw-customer-list.spec.js
@@ -3,12 +3,14 @@
  */
 
 import { mount } from '@vue/test-utils';
+import { createMemoryHistory, createRouter, RouterLink } from 'vue-router';
 import { searchRankingPoint } from 'src/app/service/search-ranking.service';
 import Criteria from 'src/core/data/criteria.data';
 
-async function createWrapper(privileges = []) {
+async function createWrapper(privileges = [], router = null) {
     return mount(await wrapTestComponent('sw-customer-list', { sync: true }), {
         global: {
+            plugins: router ? [router] : [],
             mocks: {
                 $route: {
                     query: {
@@ -56,6 +58,8 @@ async function createWrapper(privileges = []) {
                                                   salesChannelId: 'd4c3b2a1',
                                                   sourceEntitiy: 'customer',
                                                   createdById: '123213132',
+                                                  firstName: 'John',
+                                                  lastName: 'Doe',
                                               },
                                           ],
                                           total: 1,
@@ -106,14 +110,15 @@ async function createWrapper(privileges = []) {
                     ],
                     template: `
                         <div>
-                            <template v-for="item in (dataSource || items)">
+                            <template v-for="item in (dataSource?.data || dataSource || items)">
+                                <slot name="column-firstName" v-bind="{ item, isInlineEdit: false }"></slot>
                                 <slot name="actions" v-bind="{ item }"></slot>
                             </template>
                         </div>`,
                 },
                 'sw-language-switch': true,
                 'sw-context-menu-item': true,
-                'router-link': true,
+                'router-link': router ? RouterLink : true,
                 'sw-avatar': true,
                 'sw-text-field': true,
                 'sw-label': true,
@@ -136,6 +141,75 @@ Shopware.Service().register('filterService', () => {
 });
 
 describe('module/sw-customer/page/sw-customer-list', () => {
+    describe('customer name link', () => {
+        let wrapper;
+        let router;
+
+        beforeEach(async () => {
+            router = createRouter({
+                history: createMemoryHistory('/admin#'),
+                routes: [
+                    { path: '/', component: { template: '<div />' } },
+                    { path: '/sw/customer/index', name: 'sw.customer.index', component: { template: '<div />' } },
+                    { path: '/sw/customer/detail/:id', name: 'sw.customer.detail', component: { template: '<div />' } },
+                ],
+            });
+            await router.push({ name: 'sw.customer.index' });
+            wrapper = await createWrapper([], router);
+            await flushPromises();
+        });
+
+        afterEach(() => {
+            wrapper?.unmount();
+        });
+
+        it('renders a customer detail URL that can be copied or opened in a new tab', () => {
+            const link = wrapper.get('a.mt-link');
+
+            expect(link.text()).toBe('Doe, John');
+            expect(link.attributes('href')).toBe('#/sw/customer/detail/1a2b3c?edit=false');
+        });
+
+        it('navigates to the customer detail on a normal click', async () => {
+            await wrapper.get('a.mt-link').trigger('click');
+            await flushPromises();
+
+            expect(router.currentRoute.value.fullPath).toBe('/sw/customer/detail/1a2b3c?edit=false');
+        });
+
+        it.each([
+            [
+                'Ctrl-click',
+                'click',
+                { ctrlKey: true },
+            ],
+            [
+                'Meta-click',
+                'click',
+                { metaKey: true },
+            ],
+            [
+                'middle-click',
+                'auxclick',
+                { button: 1 },
+            ],
+        ])('preserves native browser handling for %s', async (_name, eventType, options) => {
+            const link = wrapper.get('a.mt-link').element;
+            let navigationPrevented;
+            link.addEventListener(eventType, (event) => {
+                navigationPrevented = event.defaultPrevented;
+                // JSDOM cannot perform native navigation after the component has handled the event.
+                event.preventDefault();
+            });
+
+            link.dispatchEvent(new MouseEvent(eventType, { bubbles: true, cancelable: true, ...options }));
+            await flushPromises();
+
+            expect(navigationPrevented).toBe(false);
+            expect(router.currentRoute.value.name).toBe('sw.customer.index');
+        });
+    });
+
     it('should not be able to create a new customer', async () => {
         const wrapper = await createWrapper();
         await flushPromises();

fc1a5b25ed0e4db9b6c77dc888cc36088fb83a90
Jonas Elfering <j.elfering@shopware.com>
2026-09-18 08:41:41 +0000

    fix(core): reset derived fields when cloning (#20575)

--- src/Administration/Resources/app/administration/src/module/sw-promotion-v2/page/sw-promotion-v2-list/sw-promotion-v2-list.spec.js
+++ src/Administration/Resources/app/administration/src/module/sw-promotion-v2/page/sw-promotion-v2-list/sw-promotion-v2-list.spec.js
@@ -249,8 +249,6 @@ describe('src/module/sw-promotion-v2/page/sw-promotion-v2-list', () => {
                     individualCodePattern: '',
                     individualCodes: null,
                     active: false,
-                    orderCount: 0,
-                    ordersPerCustomerCount: null,
                 },
             },
             Shopware.Context.api,

shopware/storefront

fix(a11y): bootstrap focus-trap for ajaxModals and OffCanvas (#17501)

Resources/app/storefront/src/plugin/offcanvas/offcanvas.plugin.js
@@ -121,6 +121,10 @@ class OffCanvasSingleton {
     _openOffcanvas(_offCanvas, callback) {
         window.focusHandler.saveFocusState('offcanvas');
 
+        // Keep the Bootstrap focus-trap working when the offcanvas is the last element before `</body>`.
+        // @todo: Remove when upstream issue https://github.com/twbs/bootstrap/issues/42503 is resolved.
+        window.focusHandler._addFocusTrapGuard(_offCanvas);
+
         OffCanvasSingleton.bsOffcanvas.show();
         window.history.pushState('offcanvas-open', '');
 
@@ -145,6 +149,8 @@ class OffCanvasSingleton {
                 setTimeout(() => {
                     offCanvas.remove();
 
+                    // @todo: Remove when upstream issue https://github.com/twbs/bootstrap/issues/42503 is resolved.
+                    window.focusHandler._removeFocusTrapGuard();
                     window.focusHandler.resumeFocusState('offcanvas');
 
                     this.$emitter.publish('onCloseOffcanvas', {
@@ -188,7 +194,10 @@ class OffCanvasSingleton {
             if (offCanvasInstance && typeof offCanvasInstance.dispose === 'function') {
                 offCanvasInstance.dispose();
             }
+
             offCanvas.remove();
+            // @todo: Remove when upstream issue https://github.com/twbs/bootstrap/issues/42503 is resolved.
+            window.focusHandler._removeFocusTrapGuard();
         });
 
         // Clear the singleton reference after disposal

shopware/shopware

(cherry picked from commit 98553ed7a61c6eba6faad798f38158a5e6be6118)
Conflict resolutions against the 6.6.x shape:
- focus-handler.helper.js: only the _focusTrapGuard reference and the two guard
  methods are taken. The bundled _focusAbleElements selector list and the
  getFocusableElements/getFirstFocusableElement/getLastFocusableElement helpers
  are a trunk-only addition the focus trap does not depend on.
- offcanvas.plugin.js: 6.6.x already carries the later commit #18866 (as #19501),
  so the guard is added after _disableBackgroundAccessibility() in the order
  trunk HEAD uses, and _removeFocusTrapGuard() is added inside the existing
  Iterator.iterate() callback instead of pulling in trunk's dispose() loop.
- focus-handler.helper.test.js: only the two guard describes are taken, the
  getFocusableElements describe belongs to the helpers that were not backported.
- offcanvas-cart.plugin.test.js: needs no change and is left untouched.
6.6.x-only adaptations:
- jest.init.js: the jsdom bundled with jest 24 does not implement
  Element.insertAdjacentElement, which the guard relies on. Without the polyfill
  the specs for the guard and for the offcanvas cart fail with a TypeError.
- address-editor.plugin.test.js: this spec no longer exists on trunk, so its
  focusHandler stub never received the two new methods.
- The @todo markers of the original commit are dropped for consistency and the
  upstream issue is referenced with @see instead. The 6.6.x storefront sources
  carry no such marker; the Storefront eslint config does not enforce this, the
  Administration one does.

src/Storefront/Resources/app/storefront/src/plugin/offcanvas/offcanvas.plugin.js
@@ -125,6 +125,9 @@ class OffCanvasSingleton {
 
         this._disableBackgroundAccessibility(offCanvas);
 
+        // Keep the Bootstrap focus-trap working when the offcanvas is the last element before `</body>`.
+        window.focusHandler._addFocusTrapGuard(offCanvas);
+
         OffCanvasSingleton.bsOffcanvas.show();
         window.history.pushState('offcanvas-open', '');
 
@@ -150,6 +153,7 @@ class OffCanvasSingleton {
                     this._restoreBackgroundState();
                     offCanvas.remove();
 
+                    window.focusHandler._removeFocusTrapGuard();
                     window.focusHandler.resumeFocusState('offcanvas');
 
                     this.$emitter.publish('onCloseOffcanvas', {
@@ -235,7 +239,10 @@ class OffCanvasSingleton {
         // Clear the singleton reference after disposal
         OffCanvasSingleton.bsOffcanvas = null;
         const offCanvasElements = this.getOffCanvas();
-        return Iterator.iterate(offCanvasElements, offCanvas => offCanvas.remove());
+        return Iterator.iterate(offCanvasElements, offCanvas => {
+            offCanvas.remove();
+            window.focusHandler._removeFocusTrapGuard();
+        });
     }
 
     /**
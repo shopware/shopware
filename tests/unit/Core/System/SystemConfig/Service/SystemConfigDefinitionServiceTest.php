a7f170d8398275741b60bcb6cb2f151dcbfd8e14
Elias Lackner <lackner.elias@gmail.com>
2026-09-21 14:17:27 +0200

    test: apply ConfigurationServiceTest changes to SystemConfigDefinitionServiceTest

--- tests/unit/Core/System/SystemConfig/Service/SystemConfigDefinitionServiceTest.php
+++ tests/unit/Core/System/SystemConfig/Service/SystemConfigDefinitionServiceTest.php
@@ -48,9 +48,22 @@ class SystemConfigDefinitionServiceTest extends TestCase
             new NullLogger()
         );
 
-        static::assertFalse($configService->checkConfiguration('invalid!', Context::createDefaultContext()));
-
         $configService->getConfiguration('invalid!', Context::createDefaultContext());
+    }
+
+    public function testCheckConfigurationWithInvalidDomain(): void
+    {
+        $appRepository = new StaticEntityRepository([]);
+        $configService = new SystemConfigDefinitionService(
+            [],
+            new ConfigReader(),
+            static::createStub(AppConfigReader::class),
+            $appRepository,
+            new StaticSystemConfigService([]),
+            new NullLogger()
+        );
+
+        static::assertFalse($configService->checkConfiguration('invalid!', Context::createDefaultContext()));
     }
 
     public function testMissingConfig(): void

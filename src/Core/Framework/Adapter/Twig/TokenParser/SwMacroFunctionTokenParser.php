9be6d936be67f8c514d683b0b342241bf87aa2dd
Michael Telgmann <m.telgmann@shopware.com>
2026-09-23 11:31:39 +0200

    fix: Ensure compatibility with Twig 3.29

--- src/Core/Framework/Adapter/Twig/TokenParser/SwMacroFunctionTokenParser.php
+++ src/Core/Framework/Adapter/Twig/TokenParser/SwMacroFunctionTokenParser.php
@@ -2,30 +2,33 @@
 
 namespace Shopware\Core\Framework\Adapter\Twig\TokenParser;
 
-use Shopware\Core\Framework\Adapter\Twig\Node\MacroOverrideNode;
 use Shopware\Core\Framework\Log\Package;
 use Twig\Error\SyntaxError;
 use Twig\Node\BodyNode;
-use Twig\Node\EmptyNode;
 use Twig\Node\Expression\ArrayExpression;
 use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Expression\TempNameExpression;
 use Twig\Node\Expression\Unary\NegUnary;
 use Twig\Node\Expression\Unary\PosUnary;
 use Twig\Node\Expression\Variable\LocalVariable;
+use Twig\Node\MacroDeclarationNode;
+use Twig\Node\MacroNode;
 use Twig\Node\Node;
+use Twig\Node\NodeDocumentation;
 use Twig\Token;
 use Twig\TokenParser\AbstractTokenParser;
 use Twig\TokenParser\MacroTokenParser;
 
-#[Package('framework')]
 /**
  * @internal
  *
- * @see MacroTokenParser -> basically copied, we use our own Macro node,
- * that returns the actual instance of returned value instead of the markup
+ * @see MacroTokenParser
  *
- * @codeCoverageIgnore - Covered by @see \Shopware\Tests\Integration\Core\Framework\Adapter\Twig\ReturnNodeTest
+ * @codeCoverageIgnore
+ *
+ * @see \Shopware\Tests\Integration\Core\Framework\Adapter\Twig\ReturnNodeTest
  */
+#[Package('framework')]
 class SwMacroFunctionTokenParser extends AbstractTokenParser
 {
     public function parse(Token $token): Node
@@ -33,11 +36,11 @@ class SwMacroFunctionTokenParser extends AbstractTokenParser
         $lineno = $token->getLine();
         $stream = $this->parser->getStream();
         $name = $stream->expect(Token::NAME_TYPE)->getValue();
-        $arguments = $this->parseDefinition();
+        [$arguments, $variadicName] = $this->parseDefinition($name);
 
         $stream->expect(Token::BLOCK_END_TYPE);
         $this->parser->pushLocalScope();
-        $body = $this->parser->subparse([$this, 'decideBlockEnd'], true);
+        $body = $this->parser->subparse($this->decideBlockEnd(...), true);
         if ($token = $stream->nextIf(Token::NAME_TYPE)) {
             $value = $token->getValue();
 
@@ -48,9 +51,10 @@ class SwMacroFunctionTokenParser extends AbstractTokenParser
         $this->parser->popLocalScope();
         $stream->expect(Token::BLOCK_END_TYPE);
 
-        $this->parser->setMacro($name, new MacroOverrideNode($name, new BodyNode([$body]), $arguments, $lineno));
+        $this->parser->setMacro($name, $macro = new MacroNode($name, new BodyNode([$body]), $arguments, $lineno, $variadicName));
+        $this->parser->setDocumentationTarget($macro);
 
-        return new EmptyNode($lineno);
+        return new MacroDeclarationNode($name, $lineno);
     }
 
     public function decideBlockEnd(Token $token): bool
@@ -63,9 +67,13 @@ class SwMacroFunctionTokenParser extends AbstractTokenParser
         return 'sw_macro_function';
     }
 
-    private function parseDefinition(): ArrayExpression
+    /**
+     * @return array{ArrayExpression, string|null}
+     */
+    private function parseDefinition(string $macroName): array
     {
         $arguments = new ArrayExpression([], $this->parser->getCurrentToken()->getLine());
+        $variadicName = null;
         $stream = $this->parser->getStream();
         $stream->expect(Token::OPERATOR_TYPE, '(', 'A list of arguments must begin with an opening parenthesis');
         while (!$stream->test(Token::PUNCTUATION_TYPE, ')')) {
@@ -78,8 +86,25 @@ class SwMacroFunctionTokenParser extends AbstractTokenParser
                 }
             }
 
+            if ($stream->nextIf(Token::OPERATOR_TYPE, '...')) {
+                $token = $stream->expect(Token::NAME_TYPE, null, 'A variadic argument must be a name');
+                $variadicName = (new LocalVariable($token->getValue(), $token->getLine()))->getAttribute('name');
+                if (str_starts_with($variadicName, TempNameExpression::RESERVED_NAME_PREFIX)) {
+                    $variadicName = substr($variadicName, \strlen(TempNameExpression::RESERVED_NAME_PREFIX));
+                }
+                if ($stream->test(Token::OPERATOR_TYPE, '=')) {
+                    throw new SyntaxError(\sprintf('The variadic argument "%s" in macro "%s" cannot have a default value.', $variadicName, $macroName), $token->getLine(), $stream->getSourceContext());
+                }
+                if ($stream->nextIf(Token::PUNCTUATION_TYPE, ',') && !$stream->test(Token::PUNCTUATION_TYPE, ')')) {
+                    throw new SyntaxError(\sprintf('The variadic argument "%s" in macro "%s" must be the last one.', $variadicName, $macroName), $token->getLine(), $stream->getSourceContext());
+                }
+
+                break;
+            }
+
             $token = $stream->expect(Token::NAME_TYPE, null, 'An argument must be a name');
-            $name = new LocalVariable($token->getValue(), $this->parser->getCurrentToken()->getLine());
+            $name = new LocalVariable($token->getValue(), $token->getLine());
+            NodeDocumentation::add($name, $token);
             if ($token = $stream->nextIf(Token::OPERATOR_TYPE, '=')) {
                 $default = $this->parser->parseExpression();
             } else {
@@ -94,7 +119,7 @@ class SwMacroFunctionTokenParser extends AbstractTokenParser
         }
         $stream->expect(Token::PUNCTUATION_TYPE, ')', 'A list of arguments must be closed by a parenthesis');
 
-        return $arguments;
+        return [$arguments, $variadicName];
     }
 
     // checks that the node only contains "constant" elements

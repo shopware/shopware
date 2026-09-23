4fc3048846919b620ef8754cc58acf646d3336a0
David Traum <d.traum@shopware.com>
2026-09-23 16:49:24 +0200

    fix: remove duplicate template tag from test

--- src/Administration/Resources/app/administration/src/app/adapter/composition-extension-system/options-api-setup-shim.spec/override-local-state.spec.ts
+++ src/Administration/Resources/app/administration/src/app/adapter/composition-extension-system/options-api-setup-shim.spec/override-local-state.spec.ts
@@ -42,7 +42,6 @@ describe('src/app/adapter/composition-extension-system/options-api-setup-shim - 
             },
             template: `
                 <sw-block name="sw-shim-local-block" :data="$dataScope">
-                    <template #default="{ __swOverride }">
                     <template #default="{ __swOverride: { [namespace]: { margin } } }">
                         <margin-hint :margin="margin" />
                     </template>
